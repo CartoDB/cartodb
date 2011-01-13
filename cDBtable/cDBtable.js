a9a06701a7ed5520b0aff3a6a029adc68c74a789
Javier Álvarez Medina <xavijam@gmail.com>
2011-01-13 19:02:39 +0100

    chagnes in tables

---
+++ cDBtable/cDBtable.js
@@ -0,0 +1,164 @@
+
+    // FUNCIONALITIES
+    //   - Editing table data with events
+    //   - Resize columns
+    //   - Pagination with ajax
+    //   - Custom style
+    //   - jScrollPane
+    //   - Update table (remove columns and rows, add columns and rows, move columns, sort columns)
+    //   - Validate fields
+    //   - Rows selection for multiple edition
+    //   - Floating tHead 
+    //   - Floating first column 
+
+
+    (function( $ ){
+      
+      var table;
+      var loading = false;
+      var page = 0;
+      var defaults;
+      var total = 10000;
+
+      var methods = {
+        init : function(options) {
+          return this.each(function(){
+      
+            defaults = {  
+              height: '200',  
+              width: '200',
+              getDataUrl: 'http://bioblitz.tdwg.org/api/taxonomy',  
+              style: "cDBtable.css",
+              paginateParam: "page",
+              resultsPerPage: 30
+            };
+            
+            table = $(this)[0];
+            methods.getData(defaults);
+            
+          });
+        },
+
+        getData : function(options) {
+         //Pagination AJAX adding rows
+         if (!loading) {
+           loading = true;
+
+           $.ajax({
+             dataType: 'jsonp',
+             method: "GET",
+             url: options.getDataUrl,
+             data: {
+               query: 'a',
+               limit: options.resultsPerPage,
+               page: page
+             },
+             success: function(data) {
+               methods.draw(options,data);
+             }
+           });
+         }
+        },
+
+        draw: function(options,data) {
+          //Draw the data
+          if ($(table).html()=='') {
+            var thead = '<thead><tr>';
+            $.each(data[0], function(index,element){
+              thead += '<th width="100"><div><h3>'+index+'</h3><p>String</p><a href="#">options</a></div></th>';
+            });
+            thead += "</thead></tr>";
+            $(table).append(thead);
+            
+            //Scroll event
+            methods.addScroll();
+            
+            //Cell click event
+            //methods.bindCellEvent();
+            
+          }
+          
+          if ($(table).children('tbody').length==0) {
+            var tbody = '<tbody>';
+          } else {
+            var tbody = '';
+          }
+          
+          //Loop all the data
+          $.each(data, function(i,element){
+            tbody += '<tr>';
+            $.each(element, function(j,elem){
+              tbody += '<td width="100" r="'+i+'" c="'+j+'"><div>'+elem+'</div></td>';
+            });
+            tbody += '</tr>';
+          });
+          $(table).append(tbody);
+          methods.hideLoader();
+          
+        },
+        
+        addScroll: function() {
+          $(window).scroll(function(ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+            if (!loading) {
+              $(window).queue([]).stop(); 
+              var difference = $(document).height() - $(window).height();
+              if ($(window).scrollTop()==difference) {
+                methods.showLoader();
+                page++;
+                setTimeout(function(){methods.getData(defaults)},500);
+              }
+            }
+          });
+          // $(table).parent().addClass('scroll-pane');
+          // $('.scroll-pane').jScrollPane({showArrows: false});
+          // $(window).resize(function(){$('.scroll-pane').jScrollPane({showArrows: false});});
+        },
+        
+        showLoader: function(){
+          $('div.loading_more').show();
+        },
+        
+        hideLoader: function() {
+          loading = false;
+          $('div.loading_more').hide();
+        },
+        
+        bindCellEvent: function() {
+          document.ondblclick = function(event) {
+
+              //Cross-browser event object
+              event = event || window.event;
+              //Cross-browser event target
+              var target = event.target || event.srcElement;
+              //Get target element type
+              var targetElement = target.nodeName.toLowerCase();
+
+              //Is it a link?
+              if (targetElement == "td") {
+                alert("td");
+                //alert(targetElement.getAttribute('r'));
+              }
+
+              //Cross-browser prevent default action
+              if (event.preventDefault) {
+                event.preventDefault();
+              } else {
+                event.returnValue = false;
+              }
+            };
+        }
+      };
+
+      
+      $.fn.cDBtable = function(method) {
+        if (methods[method]) {
+          return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+          return methods.init.apply( this, arguments );
+        } else {
+          return methods.init.apply( this, arguments );
+        }
+      };
+    })( jQuery );
--- javascripts/table.js
+++ javascripts/table.js
@@ -2,7 +2,7 @@
     
     $(document).ready(function(){
       //Fix tabs content position
-      fixContentPosition();
+      //fixContentPosition();
       
       //$.getJSON('/javascripts/test.json',function(result){
         //console.log(result);
@@ -10,45 +10,70 @@
 
       //$('table').flexigrid({height:'auto',striped:false});
       
-      $("#flex1").flexigrid({height:'auto',width:'auto',striped: true});
+      // for (var i=0; i<2000; i++) {
+      //   $('table tbody').append(
+      //   '<tr>' +
+      //     '<td c="0" r="'+i+'">data 1</td>'+ 
+      //     '<td c="1" r="'+i+'">data 2</td>'+ 
+      //     '<td c="2" r="'+i+'">data 3</td>'+ 
+      //     '<td c="3" r="'+i+'">data 4</td>'+
+      //     '<td c="4" r="'+i+'">data 5</td>'+ 
+      //     '<td c="5" r="'+i+'">data 6</td>'+ 
+      //     '<td c="6" r="'+i+'">data 7</td>'+ 
+      //     '<td c="7" r="'+i+'">data 8</td>'+
+      //     '<td c="8" r="'+i+'">data 9</td>'+ 
+      //     '<td c="9" r="'+i+'">data 10</td>'+ 
+      //     '<td c="10" r="'+i+'">data 11</td>'+ 
+      //     '<td c="11" r="'+i+'">data 12</td>'+
+      //   '</tr>');
+      // }
+
+
+      $("table#cDBtable").cDBtable();
       
       
-      $(window).resize(function(){fixContentPosition()});
+      // $('table tr td').live('dblclick',function(){
+      //   alert(
+      //     $(this).attr('c') + '->' + $(this).attr('r') + '->' + $(this).text()
+      //   );
+      // });
+      
+      // $(window).resize(function(){fixContentPosition()});
     });
     
-    function fixContentPosition() {
-      if ($('table').is(':visible')) {
-        //$('table').height($(window).height()-162);
-      } else {
-        $('div#map').height($(window).height()-162);
-      }
-    }
+    // function fixContentPosition() {
+    //   if ($('table').is(':visible')) {
+    //     //$('table').height($(window).height()-162);
+    //   } else {
+    //     $('div#map').height($(window).height()-162);
+    //   }
+    // }
     
     
-    function changeTo(ev,kind){
-      ev.stopPropagation();
-      ev.preventDefault();
-      
-      if (kind=="table") {
-        if (!$('table').is(':visible')) {
-          $("ul.tab_menu li").removeClass('selected');
-          $("ul li a:contains('Table')").parent().addClass('selected');
-          $('table').toggle();
-          $('div#map').toggle();
-          fixContentPosition()
-        }
-      } else {
-        if (!$('div#map').is(':visible')) {
-          $("ul.tab_menu li").removeClass('selected');
-          $("ul li a:contains('Map')").parent().addClass('selected');
-          $('table').toggle();
-          $('div#map').toggle();
-          
-          if ($('div#map svg').size()==0) {
-            $('div#map').append('<script type="text/javascript" src="/javascripts/maps/map.js"></script>');
-          }
-          
-        }
-      }
-      
-    }
+    // function changeTo(ev,kind){
+    //   ev.stopPropagation();
+    //   ev.preventDefault();
+    //   
+    //   if (kind=="table") {
+    //     if (!$('table').is(':visible')) {
+    //       $("ul.tab_menu li").removeClass('selected');
+    //       $("ul li a:contains('Table')").parent().addClass('selected');
+    //       $('table').toggle();
+    //       $('div#map').toggle();
+    //       fixContentPosition()
+    //     }
+    //   } else {
+    //     if (!$('div#map').is(':visible')) {
+    //       $("ul.tab_menu li").removeClass('selected');
+    //       $("ul li a:contains('Map')").parent().addClass('selected');
+    //       $('table').toggle();
+    //       $('div#map').toggle();
+    //       
+    //       if ($('div#map svg').size()==0) {
+    //         $('div#map').append('<script type="text/javascript" src="/javascripts/maps/map.js"></script>');
+    //       }
+    //       
+    //     }
+    //   }
+    //   
+    // }

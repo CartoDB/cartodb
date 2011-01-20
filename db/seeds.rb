29ff54872006d26824f966e9b06acbd087ef56a9
Fernando Blat <ferblape@gmail.com>
2011-01-20 12:52:08 +0100

    More seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,14 @@ Rails::Sequel.connection[tables.first].with_sql(
 ## Create users
 
 User.create :email => 'admin@example.com', :password => 'example'
+User.create :email => 'user1@example.com', :password => 'user1'
 
 ## Development demo data
 
 user = User.first
-
 Table.create :user_id => user.id, :privacy => Table::PUBLIC, :name => '4sq check-ins'
-
 Table.create :user_id => user.id, :privacy => Table::PRIVATE, :name => 'Downloaded movies'
+
+user = User.order(:id).last
+Table.create :user_id => user.id, :privacy => Table::PUBLIC, :name => 'Twitter followers'
+Table.create :user_id => user.id, :privacy => Table::PRIVATE, :name => 'Recipes'

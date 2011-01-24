bb8efa00df760465553c37e8126a51544fadaad9
Fernando Blat <ferblape@gmail.com>
2011-01-24 10:56:03 +0100

    Fixed login acceptance

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -2,20 +2,15 @@ module HelperMethods
 
   def login_as(user)
     visit login_path
-    fill_in 'E-mail', :with => user.email
-    fill_in 'Password', :with => user.email.split('@').first
-    click_link_or_button 'Login'
+    fill_in 'your e-mail', :with => user.email
+    fill_in 'your password', :with => user.email.split('@').first
+    click_link_or_button 'Sign in'
   end
 
   def get_json(path)
-    # set_json_headers
     visit path
   end
 
-  def set_json_headers
-    header 'Accept', 'application/json'
-    header 'Content-Type', 'application/json'
-  end
 end
 
 RSpec.configuration.include HelperMethods, :type => :acceptance

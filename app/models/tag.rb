92e1cbf63d7c8f056d5ea2dbe130d34c68443510
Fernando Blat <ferblape@gmail.com>
2011-01-27 12:39:19 +0100

    Public tables tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,4 +6,23 @@ class Tag < Sequel::Model
   # Allowed columns
   set_allowed_columns(:name)
 
+  def self.load_user_tags(user_id, options = {})
+    options[:limit] ||= 5
+    fetch("select tags.name, count(*) as count
+                        from tags
+                        where tags.user_id = ?
+                        group by tags.name
+                        order by count desc limit 5", user_id).all
+  end
+
+  def self.load_public_tags(user_id, options = {})
+    options[:limit] ||= 5
+    fetch("select tags.name, count(*) as count
+                        from tags
+                        inner join user_tables on user_tables.id = tags.table_id and user_tables.privacy = #{Table::PUBLIC}
+                        where tags.user_id != ?
+                        group by tags.name
+                        order by count desc limit 5", user_id).all
+  end
+
 end

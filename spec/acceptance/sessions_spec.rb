4de43c44a59e847e8b0fb28694d25b1f4bed8b72
Fernando Blat <ferblape@gmail.com>
2011-01-31 10:48:35 +0100

    Error class when login fails 

--- spec/acceptance/sessions_spec.rb
+++ spec/acceptance/sessions_spec.rb
@@ -10,6 +10,8 @@ feature "Sessions" do
     fill_in 'your password', :with => 'blablapassword'
     click_link_or_button 'Sign in'
 
+    page.should have_css("input[@type=text].error")
+    page.should have_css("input[@type=password].error")
     page.should have_content("Your account or your password is not ok")
 
     fill_in 'your e-mail', :with => user.email

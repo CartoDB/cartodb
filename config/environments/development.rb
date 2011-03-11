b46aa7e497867db47ab9a3eae5cbdf775945a7fc
Fernando Espinosa <fer@ferdev.com>
2011-03-11 12:24:41 +0100

    Disables ses email service and error reporting in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,8 @@ CartoDB::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.delivery_method = :ses
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

76a0d2c6a1347bf66f78a74328c6dc2a16b9c8c4
Javier Álvarez Medina <xavijam@gmail.com>
2011-03-18 18:00:46 +0100

    last changes

--- public/javascripts/admin/table/geocoding.js
+++ public/javascripts/admin/table/geocoding.js
@@ -16,56 +16,58 @@
 	  var me = this;
 		$.ajax({
       method: "GET",
-      url: '/api/json/tables/'+this.table+'/addresses',
+      url: '/v1/tables/'+table_name+'/records/pending_addresses',
       success: function(data) {
-        var rows = data.rows;
-        var directions = [];
-        for (var i=0; i<rows.length; i++) {
-          var elem = {};
-          elem.address = rows[i].address;
-          elem.cartodb_id = rows[i].cartodb_id;
-          directions.push(elem);
-        }
-        
-        var worker = new Worker("/javascripts/admin/table/worker_geocoding.js");
-
-        worker.onmessage = function(event){
-          if (event.data == "Finish" || event.data == "Stopped") {
-            if (event.data == "Finish") {
-              me.finishGeocoding();
-              delete worker;
-            } else {
-              me.stoppedGeocoding();
-              delete worker;
-            }
-          } else {
-            var params = {};
-            if (event.data.Placemark!=undefined) {
-              params['lat'] = event.data.Placemark[0].Point.coordinates[1];
-              params['lon'] = event.data.Placemark[0].Point.coordinates[0];
-            } else {
-              params['lat'] = -0;
-              params['lon'] = -0;
-            }
-
-            $.ajax({
-              type: "PUT",
-              url: '/api/json/tables/'+me.table+'/update_geometry/'+event.data.cartodb_id,
-              data: params,
-              success: function(data) {
-                //console.log(data);
-              },
-              error: function(e) {
-                //console.log(e);
-              }
-            });
-          }
-        };
+        console.log(data);
+                // 
+                // var rows = data.rows;
+                // var directions = [];
+                // for (var i=0; i<rows.length; i++) {
+                //   var elem = {};
+                //   elem.address = rows[i].address;
+                //   elem.cartodb_id = rows[i].cartodb_id;
+                //   directions.push(elem);
+                // }
+                // 
+                // var worker = new Worker("/javascripts/admin/table/worker_geocoding.js");
+                // 
+                // worker.onmessage = function(event){
+                //   if (event.data == "Finish" || event.data == "Stopped") {
+                //     if (event.data == "Finish") {
+                //       me.finishGeocoding();
+                //       delete worker;
+                //     } else {
+                //       me.stoppedGeocoding();
+                //       delete worker;
+                //     }
+                //   } else {
+                //     var params = {};
+                //     if (event.data.Placemark!=undefined) {
+                //       params['lat'] = event.data.Placemark[0].Point.coordinates[1];
+                //       params['lon'] = event.data.Placemark[0].Point.coordinates[0];
+                //     } else {
+                //       params['lat'] = -0;
+                //       params['lon'] = -0;
+                //     }
+                // 
+                //     $.ajax({
+                //       type: "PUT",
+                //       url: '/api/json/tables/'+me.table+'/update_geometry/'+event.data.cartodb_id,
+                //       data: params,
+                //       success: function(data) {
+                //         //console.log(data);
+                //       },
+                //       error: function(e) {
+                //         //console.log(e);
+                //       }
+                //     });
+                //   }
+                // };
    
-        worker.postMessage({process: 'start', places: directions});
-        $(window).bind('stopGeo',function(ev){
-          worker.postMessage({process: 'stop', places: null});
-        });
+        // worker.postMessage({process: 'start', places: directions});
+        // $(window).bind('stopGeo',function(ev){
+        //   worker.postMessage({process: 'stop', places: null});
+        // });
        }
     });
 	}

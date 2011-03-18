1276a849580a5f3e278200d0f6f3bee5cbe2185f
Fernando Blat <ferblape@gmail.com>
2011-03-18 18:14:06 +0100

    Fixed columns shown in table index

--- app/controllers/admin/tables_controller.rb
+++ app/controllers/admin/tables_controller.rb
@@ -10,12 +10,12 @@ class Admin::TablesController < ApplicationController
     per_page = 10
     unless params[:public]
       @tags   = Tag.load_user_tags(current_user.id, :limit => 5)
-      @tables = Table.filter(:user_id => current_user.id).order(:id).reverse.select(:id,:name,:privacy,:updated_at,:tags).
+      @tables = Table.filter(:user_id => current_user.id).order(:id).reverse.select(:id,:name,:privacy,:updated_at,:tags,:user_id).
                         paginate(current_page, per_page, current_user.tables_count)
     else
       @tags = Tag.load_public_tags(current_user.id, :limit => 5)
       @tables = Table.filter(~{:user_id => current_user.id} & {:privacy => Table::PUBLIC}).order(:id).reverse.
-                      select(:id,:name,:privacy,:updated_at,:tags).paginate(current_page, per_page)
+                      select(:id,:name,:privacy,:updated_at,:tags,:user_id).paginate(current_page, per_page)
       render :template => 'admin/tables/index_public' and return
     end
   end

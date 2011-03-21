1a22ec92355e972b69ec28bb888ce06a8227dc93
Fernando Blat <ferblape@gmail.com>
2011-03-21 13:09:10 +0100

    Fixed a bug with the url of developers Removed layout from superadmin

--- app/controllers/superadmin/superadmin_controller.rb
+++ app/controllers/superadmin/superadmin_controller.rb
@@ -1,8 +1,6 @@
 class Superadmin::SuperadminController < ApplicationController
   before_filter :check_admin_user
 
-  layout 'superadmin'
-
   def check_admin_user
     redirect_to login_path and return unless logged_in?
     redirect_to dashboard_path and return unless current_user.admin

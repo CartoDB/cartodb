d0b5426da43d1442000ec370823b480db4df6a09
Fernando Blat <ferblape@gmail.com>
2011-03-22 16:08:12 +0100

    Created fron documentation page Include galleries link

--- lib/cartodb/api_documentation_server.rb
+++ lib/cartodb/api_documentation_server.rb
@@ -9,13 +9,15 @@ module CartoDB
     
     def call(env)
       if env['HTTP_HOST'] =~ /^developers\./
-        layout_path = "#{Rails.root}/app/views/layouts/api_doc.html.erb"
-        file_path = "#{Rails.root}/app/views/api_doc/#{env['PATH_INFO']}"
+        layout_path = "#{Rails.root}/app/views/layouts/developers.html.erb"
+        file_path = "#{Rails.root}/app/views/developers/#{env['PATH_INFO']}"
         file_path =~ /\/$/ ? file_path += 'index.html' : file_path += '.html'
+        html = File.read(layout_path).gsub(/\{\{yield\}\}/,File.open(file_path).read).
+                                      gsub(/\{\{sidebar\}\}/,File.open("#{Rails.root}/app/views/developers/api/_sidebar.html").read)
         unless File.file?(layout_path) and File.file?(file_path)
           [ 404, {'Content-Type' => 'text/html'}, File.read("#{Rails.root}/public/404.html") ]
         else
-          [ 200, {'Content-Type' => 'text/html'}, ERB.new(File.read(layout_path).gsub(/\{\{yield\}\}/,File.open(file_path).read)).result(binding) ]
+          [ 200, {'Content-Type' => 'text/html'}, ERB.new(html).result(binding) ]
         end
       else
         @app.call(env)

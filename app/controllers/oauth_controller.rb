96e8338b11b4a4d229b79ff93ff3b37613f3ca6f
Fernando Blat <ferblape@gmail.com>
2011-03-28 12:52:51 +0200

    Small refactor in authorize method

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -10,32 +10,29 @@ class OauthController < ApplicationController
   skip_before_filter :login_required, :only => :authorize
 
   def authorize
-    if params[:oauth_token]
-      unless @token = ::RequestToken.find_by_token(params[:oauth_token])
-        render :action => "authorize_failure" and return
-      end
-      unless @token.invalidated?
-        if logged_in?
-          @token.authorize!(current_user)
-        else
-          @token.authorize!(@token.client_application.user)
-        end
-        @redirect_url = URI.parse(@token.oob? ? @token.client_application.callback_url || "http://cartodb.com" : @token.callback_url)
+    unless params[:oauth_token]
+      render :nothing => true, :status => 404 and return
+    end
+    unless @token = ::RequestToken.find_by_token(params[:oauth_token])
+      render :action => "authorize_failure" and return
+    end
+    if @token.invalidated?
+      render :action => "authorize_failure" and return
+    end
+    if logged_in?
+      @token.authorize!(current_user)
+    else
+      @token.authorize!(@token.client_application.user)
+    end
+    @redirect_url = URI.parse(@token.oob? ? @token.client_application.callback_url || APP_CONFIG[:app_host] : @token.callback_url)
 
-        unless @redirect_url.to_s.blank?
-          @redirect_url.query = @redirect_url.query.blank? ?
-                                "oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}" :
-                                @redirect_url.query + "&oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}"
-          redirect_to @redirect_url.to_s
-        else
-          render :action => "authorize_success"
-        end
-      else
-        @token.invalidate!
-        render :action => "authorize_failure"
-      end
-    elsif ["code","token"].include?(params[:response_type]) # pick flow
-      send "oauth2_authorize_#{params[:response_type]}"
+    unless @redirect_url.to_s.blank?
+      @redirect_url.query = @redirect_url.query.blank? ?
+                            "oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}" :
+                            @redirect_url.query + "&oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}"
+      redirect_to @redirect_url.to_s
+    else
+      render :action => "authorize_success"
     end
   end
 

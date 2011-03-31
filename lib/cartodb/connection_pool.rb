85187a6bcd2bb193dad2483eb34d41ef52ca5135
Fernando Blat <ferblape@gmail.com>
2011-03-31 16:45:26 +0200

    Implementing a limit of connections and a timestamp on every connection. When the limit is reached the oldest connection is removed

--- lib/cartodb/connection_pool.rb
+++ lib/cartodb/connection_pool.rb
@@ -1,23 +1,48 @@
+require 'fiber'
+
 module CartoDB
   class ConnectionPool
+    
+    MAX_POOL_SIZE = 100
+    
     def initialize
       @pool = {}
     end
     
     def fetch(configuration, &block)
-      if connection = @pool[connection_id(configuration)]
-        Rails.logger.info "[pool] Found a connection for #{connection_id(configuration)}"
-        connection
+      if @pool[connection_id(configuration)]
+        Rails.logger.info "[pool] Found a connection for #{connection_id(configuration)} (#{@pool.keys.size})"
+        @pool[connection_id(configuration)][:last_accessed] = Time.now
+        @pool[connection_id(configuration)][:connection]
       else
+        if @pool.size >= MAX_POOL_SIZE
+          close_oldest_connection!
+        end
         connection = yield
-        @pool[connection_id(configuration)] = connection
-        Rails.logger.info "[pool]  Creating a new connection for #{connection_id(configuration)} (#{@pool.keys.size})"
+        @pool[connection_id(configuration)] = { :connection => connection, :last_accessed => Time.now }
+        Rails.logger.info "[pool] Creating a new connection for #{connection_id(configuration)} (#{@pool.keys.size})"
         connection
       end
     end
     
     def close_connections!
-      @pool.values.each{ |connection| connection.disconnect }
+      @pool.each do |connection_id, conn|
+        conn[:connection].disconnect
+      end
+      @pool = {}
+    end
+    
+    def close_oldest_connection!
+      older = nil
+      oldest_access = nil
+      @pool.each do |connection_id, conn|
+        if oldest_access.nil? || oldest_access < conn[:last_accessed]
+          oldest_access = conn[:last_accessed]
+          older = connection_id
+        end
+      end
+      @pool[older][:connection].disconnect
+      @pool[older] = nil
     end
     
     private

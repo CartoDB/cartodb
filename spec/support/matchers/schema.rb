f0f423042986c09fa784da6065f3dfeb62dea931
Fernando Blat <ferblape@gmail.com>
2011-04-06 17:32:13 +0200

    Store the_geom_type in the metadata model

--- spec/support/matchers/schema.rb
+++ spec/support/matchers/schema.rb
@@ -4,7 +4,7 @@ RSpec::Matchers.define :be_equal_to_default_cartodb_schema do |expected|
   match do |actual|
     actual.should == [
       [:cartodb_id, "number"], [:name, "string"], [:description, "string"],
-      [:the_geom, "geometry", "geometry"], [:created_at, "date"], [:updated_at, "date"]
+      [:the_geom, "geometry", "geometry", "point"], [:created_at, "date"], [:updated_at, "date"]
     ]
   end
 end
@@ -13,7 +13,7 @@ RSpec::Matchers.define :be_equal_to_default_db_schema do |expected|
   match do |actual|
     actual.should == [
       [:cartodb_id, "integer"], [:name, "text"], [:description, "text"],
-      [:the_geom, "geometry", "geometry"], [:created_at, "timestamp"], [:updated_at, "timestamp"]
+      [:the_geom, "geometry", "geometry", "point"], [:created_at, "timestamp"], [:updated_at, "timestamp"]
     ]
   end
 end

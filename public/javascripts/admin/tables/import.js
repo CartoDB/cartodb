c833db749245a89f428d302becf24d922d2e1fab
Javier Álvarez Medina <xavijam@gmail.com>
2011-04-07 15:57:51 +0200

    last changes

--- public/javascripts/admin/tables/import.js
+++ public/javascripts/admin/tables/import.js
@@ -5,6 +5,23 @@
 
     head(function(){
 
+      $('div.geom_type span').click(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+        if (!$(this).hasClass('selected')) {
+          $('div.geom_type span').removeClass('selected');
+          $(this).addClass('selected');
+        }
+      });
+      
+      $('div.geom_type span a').click(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+        $(this).parent().trigger('click');
+      });
+
+
+
       //Create new table
       $('a.new_table').click(function(ev){
          ev.preventDefault();
@@ -19,12 +36,13 @@
       $('div.create_window ul li a').click(function(ev){
         ev.stopPropagation();
         ev.preventDefault();
-        if (!$(this).parent().hasClass('selected') && !$(this).parent().hasClass('disabled')) {
+        if (!$(this).parent().hasClass('selected') && !$(this).parent().hasClass('disabled') && !$(this).parent().is("span")) {
           $('div.create_window ul li').removeClass('selected');
           $(this).parent().addClass('selected');
           (create_type==0)?create_type++:create_type--;
         }
       });
+      
 
 
       $('span.file input').hover(function(ev){
@@ -54,7 +72,7 @@
           $('span.progress').width((346*percentage)/1);
         },
         onComplete: function(id, fileName, responseJSON){
-          createNewToFinish(responseJSON.file_uri);
+          createNewToFinish('',responseJSON.file_uri);
         },
         onCancel: function(id, fileName){},
         showMessage: function(message){
@@ -67,7 +85,8 @@
         ev.stopPropagation();
         ev.preventDefault();
         if (create_type==0) {
-          createNewToFinish('');
+          var geom_type = $('div.geom_type span.selected a').text();
+          createNewToFinish(geom_type,'');
         }
       });
     });
@@ -78,6 +97,12 @@
       $('div.create_window ul li:eq(0)').removeClass('disabled');
       $('div.create_window ul li').removeClass('selected');
       $('div.create_window ul li:eq(0)').addClass('selected');
+      $('div.create_window div.inner_ form').show();
+      $('div.create_window div.inner_ form').css('opacity',1);
+      $('div.create_window div.inner_').css('border-color','#FFC209');
+      $('div.create_window div.inner_').css('height','auto');
+      $('div.create_window div.inner_ span.loading').hide();
+      $('div.create_window div.inner_ span.loading').css('opacity',0);
       $('form input[type="submit"]').removeClass('disabled');
       $('span.file').removeClass('uploading');
       $('span.file input[type="file"]').attr('value','');
@@ -86,10 +111,12 @@
       $('span.progress').width(5);
       $('div.create_window ul li:eq(1)').removeClass('finished');
       $('div.create_window').removeClass('georeferencing');
+      $('div.create_window div.inner_ span.loading p').html('It\'s not gonna be a lot of time. Just a few seconds, ok?');
+      $('div.create_window div.inner_ span.loading h5').html('We are creating your table...');
     }
 
 
-    function createNewToFinish (url) {
+    function createNewToFinish (type,url) {
       $('div.create_window div.inner_').animate({borderColor:'#FFC209', height:'68px'},500);
       $('div.create_window div.inner_ form').animate({opacity:0},300,function(){
         $('div.create_window div.inner_ span.loading').show();
@@ -97,6 +124,8 @@
           var params = {}
           if (url!='') {
             params = {file:'http://'+window.location.host + url};
+          } else {
+            params = {the_geom_type:type}
           }
           $.ajax({
             type: "POST",
@@ -107,10 +136,34 @@
               window.location.href = "/tables/"+data.id;
             },
             error: function(e) {
-              console.debug(e);
+              $('div.create_window div.inner_ span.loading').addClass('error');
+              $('div.create_window div.inner_ span.loading p').html('Something weird has occurred when creating your table. Do you want to <a onclick="retryImportTable()">retry</a>?');
+              $('div.create_window div.inner_ span.loading h5').text('Ops! There has been an error');
+              $('div.create_window div.inner_').height(78);
             }
           });
         });
       });
       setTimeout(function(){$('div.create_window a.close_create').addClass('last');},250);
     }
+    
+    
+    function retryImportTable() {
+      $('div.create_window a.close_create').removeClass('last');
+      $('div.create_window div.inner_').animate({borderColor:'#CCCCCC', height:'254px'},500,function(){
+        $('div.create_window div.inner_').css('height','auto');
+      });
+      $('div.create_window ul li:eq(0)').removeClass('disabled');
+      $('form input[type="submit"]').removeClass('disabled');
+      $('span.file').removeClass('uploading');
+      $('div.create_window div.inner_ span.loading').animate({opacity:0},300,function(){
+        $('div.create_window div.inner_ span.loading').hide();
+        $('div.create_window div.inner_ span.loading').removeClass('error');
+        $('div.create_window div.inner_ span.loading p').html('It\'s not gonna be a lot of time. Just a few seconds, ok?');
+        $('div.create_window div.inner_ span.loading h5').html('We are creating your table...');
+        $('div.create_window div.inner_ form').show();
+        $('div.create_window div.inner_ form').animate({opacity:1},200);
+      });
+    }
+
+

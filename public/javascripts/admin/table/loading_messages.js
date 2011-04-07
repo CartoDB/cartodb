a48594f61a4a04defb4140965875be02786194db
Simon Tokumine <simon@japancentre.com>
2011-04-07 17:19:27 +0100

    simplify loading messages

--- public/javascripts/admin/table/loading_messages.js
+++ public/javascripts/admin/table/loading_messages.js
@@ -1,16 +1,16 @@
   
   var loadingMessages = [];
-    loadingMessages["tags"] = "Your table tags have been updated";
-    loadingMessages["name"] = "The name of your table has been successfully changed";
-    loadingMessages["privacy"] = "Your table privacy has been changed";
-    loadingMessages["update_cell"] = "The value of the cell has been changed";
-    loadingMessages["rename_column"] = "The name of the column has been renamed";
-    loadingMessages["update_geometry"] = "The geometry has been updated";
-    loadingMessages["column_type"] = "The type of the column has been changed";
-    loadingMessages["new_column"] = "New column added!";
-    loadingMessages["georeference_address"] = "Your address column has been geocordered";
-    loadingMessages["delete_column"] = "Your column has been deleted";
-    loadingMessages["delete_row"] = "This row has been deleted";
-    loadingMessages["add_row"] = "New row added in your table";
-    loadingMessages["change_latlng"] = "Occurrence position changed";
+    loadingMessages["tags"] = "table tags updated";
+    loadingMessages["name"] = "table name updated";
+    loadingMessages["privacy"] = "table privacy updated";
+    loadingMessages["update_cell"] = "cell value changed";
+    loadingMessages["rename_column"] = "column renamed";
+    loadingMessages["update_geometry"] = "geometry updated";
+    loadingMessages["column_type"] = "column type changed";
+    loadingMessages["new_column"] = "column added";
+    loadingMessages["georeference_address"] = "address column geocoded";
+    loadingMessages["delete_column"] = "column deleted";
+    loadingMessages["delete_row"] = "row deleted";
+    loadingMessages["add_row"] = "row added";
+    loadingMessages["change_latlng"] = "position updated";
     

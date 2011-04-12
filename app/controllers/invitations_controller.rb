8716460b9957c203c3957b1664c10c0ad2a4de18
Fernando Blat <ferblape@gmail.com>
2011-04-12 15:52:15 +0200

    Create method without SSL Some refactoring

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,5 @@
 class InvitationsController < ApplicationController
-  ssl_required :create, :edit, :update
+  ssl_required :edit, :update
 
   layout 'front_layout'
 
@@ -20,7 +20,6 @@ class InvitationsController < ApplicationController
   end
 
   def update
-
     render :action => :edit and return unless params[:user]
 
     @user = User[:id => params[:id]]
@@ -36,16 +35,13 @@ class InvitationsController < ApplicationController
   end
 
   def invite_token_valid?
-    redirect_to root_path and return if params.blank?
+    redirect_to root_path and return false if params.blank? || (params[:invite_token].blank? && params[:user].blank?)
 
     invite_token = params[:invite_token] || params[:user][:invite_token]
-    redirect_to root_path and return if params[:id].blank? || invite_token.blank?
+    redirect_to root_path and return false if params[:id].blank? || invite_token.blank?
 
     @user = User.filter({:id => params[:id]} & {:invite_token => invite_token}).first
-
-    redirect_to root_path and return if @user.nil? || @user.invite_token_date < 30.days.ago.to_date
-  rescue
-    redirect_to root_path
+    redirect_to root_path and return false if @user.nil? || @user.invite_token_date < 30.days.ago.to_date
   end
   private :invite_token_valid?
 

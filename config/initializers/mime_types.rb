e37e4488da059848d9fa1f90ba0b8b04071c6194
Fernando Espinosa <fer@ferdev.com>
2011-04-13 17:59:59 +0200

    Allows to export tables as a zipped csv file

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,5 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
+Mime::Type.register "application/octet-stream", :shp

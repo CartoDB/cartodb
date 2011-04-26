9041cda758e2ea14005021cdd5a6180d470f7513
Fernando Blat <ferblape@gmail.com>
2011-04-26 18:12:57 +0200

    Improved log

--- app/controllers/api/json/queries_controller.rb
+++ app/controllers/api/json/queries_controller.rb
@@ -15,8 +15,13 @@ class Api::Json::QueriesController < Api::ApplicationController
   #       "rows" => [{:id=>1, :name=>"name 1", :location=>"...", :description=>"description 1"}]
   #     }
   def run
-    render :json => { :errors => ["You must indicate a sql query"] }.to_json, :status => 400,
-           :callback => params[:callback] and return if params[:sql].blank?
+    if params[:sql].blank?
+      Rails.logger.info "============== exception on queries#run ======================"
+      Rails.logger.info "You must indicate a sql query (query parameter is nil or blank)"
+      Rails.logger.info "=============================================================="
+      render :json => { :errors => ["You must indicate a sql query"] }.to_json, :status => 400,
+             :callback => params[:callback] and return
+    end
     respond_to do |format|
       format.json do
         @to_log = params[:sql]
@@ -24,9 +29,15 @@ class Api::Json::QueriesController < Api::ApplicationController
       end
     end
   rescue CartoDB::ErrorRunningQuery => e
+    Rails.logger.info "============== exception on queries#run ====================="
+    Rails.logger.info $!
+    Rails.logger.info "============================================================="
     render :json => { :errors => [e.db_message, e.syntax_message] }.to_json, :status => 400,
            :callback => params[:callback]
   rescue
+    Rails.logger.info "============== exception on queries#run ====================="
+    Rails.logger.info $!
+    Rails.logger.info "============================================================="
     render :json => { :errors => [$!] }.to_json, :status => 400,
           :callback => params[:callback]  
   end

096364d986bcb308820b3c862d865eb0c8bd0382
Simon Tokumine <simon@japancentre.com>
2011-04-28 00:37:42 +0100

    updated maps with basic level CSS editing

--- public/javascripts/admin/maps/maps.js
+++ public/javascripts/admin/maps/maps.js
@@ -9,7 +9,8 @@
   var map;
   var radius = 10, tips = {};
   var layer;
-  var tile_url = 'http://ec2-50-16-103-51.compute-1.amazonaws.com/tiles/{X}/{Y}/{Z}/'+user_id+'/';
+  var tile_url = TILESERVER + '/tiles/{X}/{Y}/{Z}/users/'+user_id+'/';
+  
   var map_type;
 
   function initMap() {
@@ -36,15 +37,15 @@
 
           map.add(po.image()
               .url(po.url("http://{S}tile.cloudmade.com"
-              + "/1a1b06b230af4efdbb989ea99e9841af"
+              + "/BC9A493B41014CAABB98F0471D759707"
               + "/998/256/{Z}/{X}/{Y}.png")
               .hosts(["a.", "b.", "c.", ""])));
 
-          layer = po.image().url(po.url(tile_url + table_name + '/'+map_type+'/?sql='+sql+'&style=' + encodeURIComponent(JSON.stringify(style))));
+          layer = po.image().url(generateURL(table_name, sql)); 
           map.add(layer);
     } else {
       closeMapElements();
-      layer.url(po.url(tile_url + table_name + '/'+ map_type +'/?sql='+sql+'&style=' + encodeURIComponent(JSON.stringify(style))));
+      layer.url(po.url(generateURL(table_name, sql))); 
     }
   }
 
@@ -57,6 +58,149 @@
   
   
   function refreshLayer(_style) {
-    layer.url(po.url(tile_url + table_name + '/'+ map_type +'/?sql='+sql+'&style=' + encodeURIComponent(JSON.stringify(_style))));
+    layer.url(po.url(generateURL(table_name, sql))); 
     layer.reload();
+  }
+  
+  function generateURL(_table_name, _sql){
+    var url = tile_url + 'layers/' + _table_name;
+    if (_sql) url = url + '/?sql='+ Base64.encode(_sql);
+    return url;        
+  }
+
+
+  var Base64 = {
+
+  // private property
+  _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+  // public method for encoding
+  encode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = Base64._utf8_encode(input);
+
+      while (i < input.length) {
+
+          chr1 = input.charCodeAt(i++);
+          chr2 = input.charCodeAt(i++);
+          chr3 = input.charCodeAt(i++);
+
+          enc1 = chr1 >> 2;
+          enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+          enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+          enc4 = chr3 & 63;
+
+          if (isNaN(chr2)) {
+              enc3 = enc4 = 64;
+          } else if (isNaN(chr3)) {
+              enc4 = 64;
+          }
+
+          output = output +
+          this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+          this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+      }
+
+      return output;
+  },
+
+  // public method for decoding
+  decode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+      while (i < input.length) {
+
+          enc1 = this._keyStr.indexOf(input.charAt(i++));
+          enc2 = this._keyStr.indexOf(input.charAt(i++));
+          enc3 = this._keyStr.indexOf(input.charAt(i++));
+          enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+
+          output = output + String.fromCharCode(chr1);
+
+          if (enc3 != 64) {
+              output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+              output = output + String.fromCharCode(chr3);
+          }
+
+      }
+
+      output = Base64._utf8_decode(output);
+
+      return output;
+
+  },
+
+  // private method for UTF-8 encoding
+  _utf8_encode : function (string) {
+      string = string.replace(/\r\n/g,"\n");
+      var utftext = "";
+
+      for (var n = 0; n < string.length; n++) {
+
+          var c = string.charCodeAt(n);
+
+          if (c < 128) {
+              utftext += String.fromCharCode(c);
+          }
+          else if((c > 127) && (c < 2048)) {
+              utftext += String.fromCharCode((c >> 6) | 192);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+          else {
+              utftext += String.fromCharCode((c >> 12) | 224);
+              utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+
+      }
+
+      return utftext;
+  },
+
+  // private method for UTF-8 decoding
+  _utf8_decode : function (utftext) {
+      var string = "";
+      var i = 0;
+      var c = c1 = c2 = 0;
+
+      while ( i < utftext.length ) {
+
+          c = utftext.charCodeAt(i);
+
+          if (c < 128) {
+              string += String.fromCharCode(c);
+              i++;
+          }
+          else if((c > 191) && (c < 224)) {
+              c2 = utftext.charCodeAt(i+1);
+              string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+              i += 2;
+          }
+          else {
+              c2 = utftext.charCodeAt(i+1);
+              c3 = utftext.charCodeAt(i+2);
+              string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+              i += 3;
+          }
+
+      }
+
+      return string;
+  }
+
   }
---
+++ public/javascripts/environments/development.js
@@ -0,0 +1,1 @@
+var TILESERVER = "http://localhost:8080"

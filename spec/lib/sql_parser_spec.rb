9b65380c9c57e1da421c211f0442edb42bd856c7
Fernando Blat <ferblape@gmail.com>
2011-05-05 15:43:54 +0200

    When parsing '*' fails, try to keep '*'

--- spec/lib/sql_parser_spec.rb
+++ spec/lib/sql_parser_spec.rb
@@ -111,4 +111,17 @@ describe CartoDB::SqlParser do
       "select ST_AsGeoJSON(ST_Union(the_geom),6) as the_geom from cp_vizzuality WHERE cod_postal in ('01001','01002')"
   end
   
+  it "should parse a query with subqueries" do
+    CartoDB::SqlParser.parse("SELECT id_4 AS id,name_4 AS municipio,name_2 AS provincia,censo_total,((votantes_totales::NUMERIC / censo_total::NUMERIC) * 100)::INTEGER AS percen_participacion,primer_partido_percent,pp1.name AS primer_partido_name,segundo_partido_per
+    cent,pp2.name AS segundo_partido_name,tercer_partido_percent,pp3.name AS tercer_partido_name,0 AS otros_partido_percent,center_longitude,center_latitude,vsm.paro_normalizado_1996,vsm.paro_normalizado_1997,vsm.paro_normalizado_1998,vsm.paro_normalizado_1999,vsm.paro_normalizado_2000,vsm.paro_normalizado_2001,vsm.pa
+    ro_normalizado_2002,vsm.paro_normalizado_2003,vsm.paro_normalizado_2004,vsm.paro_normalizado_2005,vsm.paro_normalizado_2006,vsm.paro_normalizado_2007,vsm.paro_normalizado_2008,vsm.paro_normalizado_2009,p_n_min_max.* FROM (SELECT max(paro_normalizado_1996) as paro_normalizado_1996_max,min(paro_normalizado_1996) as 
+    paro_normalizado_1996_min, max(paro_normalizado_1997) as paro_normalizado_1997_max,min(paro_normalizado_1997) as paro_normalizado_1997_min,max(paro_normalizado_1998) as paro_normalizado_1998_max,min(paro_normalizado_1998) as paro_normalizado_1998_min,max(paro_normalizado_1999) as paro_normalizado_1999_max,min(paro
+    _normalizado_1999) as paro_normalizado_1999_min,max(paro_normalizado_2000) as paro_normalizado_2000_max,min(paro_normalizado_2000) as paro_normalizado_2000_min,max(paro_normalizado_2001) as paro_normalizado_2001_max,min(paro_normalizado_2001) as paro_normalizado_2001_min,max(paro_normalizado_2002) as paro_normaliz
+    ado_2002_max,min(paro_normalizado_2002) as paro_normalizado_2002_min,max(paro_normalizado_2003) as paro_normalizado_2003_max,min(paro_normalizado_2003) as paro_normalizado_2003_min,max(paro_normalizado_2004) as paro_normalizado_2004_max,min(paro_normalizado_2004) as paro_normalizado_2004_min,max(paro_normalizado_2
+    005) as paro_normalizado_2005_max,min(paro_normalizado_2005) as paro_normalizado_2005_min,max(paro_normalizado_2006) as paro_normalizado_2006_max,min(paro_normalizado_2006) as paro_normalizado_2006_min,max(paro_normalizado_2007) as paro_normalizado_2007_max,min(paro_normalizado_2007) as paro_normalizado_2007_min,m
+    ax(paro_normalizado_2008) as paro_normalizado_2008_max,min(paro_normalizado_2008) as paro_normalizado_2008_min,max(paro_normalizado_2009) as paro_normalizado_2009_max,min(paro_normalizado_2009) as paro_normalizado_2009_min FROM vars_socioeco_x_municipio) AS p_n_min_max,gadm4 AS g INNER JOIN votaciones_por_municipi
+    o AS v ON g.cartodb_id = v.gadm4_cartodb_id INNER JOIN vars_socioeco_x_municipio AS vsm ON vsm.gadm4_cartodb_id = v.gadm4_cartodb_id INNER JOIN partidos_politicos AS pp1 ON pp1.cartodb_id = v.primer_partido_id INNER JOIN partidos_politicos AS pp2 ON pp2.cartodb_id = v.segundo_partido_id INNER JOIN partidos_politic
+    os AS pp3 ON pp3.cartodb_id = v.tercer_partido_id WHERE v_get_tile(1014,757,11) && centre_geom_webmercator and proceso_electoral_id=73")
+  end
+  
 end

a9d2a9815fc72c59c490229a3db11b81633e5c75
Fernando Blat <ferblape@gmail.com>
2011-06-08 17:14:06 +0200

    Removing unused SQL Parser

--- lib/sql_parser/sql.rb
+++
@@ -1,4 +0,0 @@
-require File.dirname(__FILE__) + '/sql/version'
-require File.dirname(__FILE__) + '/sql/statement'
-require File.dirname(__FILE__) + '/sql/sql_visitor'
-require File.dirname(__FILE__) + '/sql/parser.racc.rb'

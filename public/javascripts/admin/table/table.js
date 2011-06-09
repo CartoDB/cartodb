9cff9dec87396450727d4cfe7f7671b38e43e2a1
Javier Álvarez Medina <xavijam@gmail.com>
2011-06-09 19:09:19 +0200

    millions of commits

--- public/javascripts/admin/table/table.js
+++ public/javascripts/admin/table/table.js
@@ -1,13 +1,22 @@
     var requests_queue = new loaderQueue();
     var geolocating = false;
 
-
-    function startTable(){
-      initMap();
+    
+    head.ready(function(){
+      head.js(
+        "/javascripts/admin/maps/map.js",
+        "/javascripts/admin/maps/map_elements.js",
+        "/javascripts/admin/maps/header_elements.js",
+        "/javascripts/admin/maps/map_functions.js",
+        "/javascripts/admin/maps/style_functions.js",
+        "/javascripts/admin/maps/map_functions.js",
+        "/javascripts/admin/maps/CartoMarker.js",
+        "/javascripts/admin/maps/CartoInfowindow.js",
+        "http://maps.google.com/maps/api/js?sensor=true&callback=initMap");
       
       $("table#carto_table").cDBtable(
         'start',{
-          getDataUrl: '/v1/tables/', //-query +table_id
+          getDataUrl: '/v1/tables/',
           resultsPerPage: 20,
           reuseResults: 600,
           total: 5000,
@@ -16,4 +25,6 @@
           mode: 'asc'
         }
       );
-    }
+    });
+
+    

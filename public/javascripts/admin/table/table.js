86a077ec9256a8ff508dc99a71af1d0f9ee74dca
Javier Álvarez Medina <xavijam@gmail.com>
2011-06-10 19:05:49 +0200

    thousands of changes in css

--- public/javascripts/admin/table/table.js
+++ public/javascripts/admin/table/table.js
@@ -1,4 +1,4 @@
-    var requests_queue = new loaderQueue();
+    var requests_queue;
     var geolocating = false;
 
     
@@ -7,24 +7,24 @@
         "/javascripts/admin/maps/map.js",
         "/javascripts/admin/maps/map_elements.js",
         "/javascripts/admin/maps/header_elements.js",
-        "/javascripts/admin/maps/map_functions.js",
-        "/javascripts/admin/maps/style_functions.js",
-        "/javascripts/admin/maps/map_functions.js",
-        "/javascripts/admin/maps/CartoMarker.js",
-        "/javascripts/admin/maps/CartoInfowindow.js",
+        // "/javascripts/admin/maps/map_functions.js",
+        // "/javascripts/admin/maps/style_functions.js",
+        // "/javascripts/admin/maps/map_functions.js",
+        // "/javascripts/admin/maps/CartoMarker.js",
+        // "/javascripts/admin/maps/CartoInfowindow.js",
         "http://maps.google.com/maps/api/js?sensor=true&callback=initMap");
-      
-      $("table#carto_table").cDBtable(
-        'start',{
-          getDataUrl: '/v1/tables/',
-          resultsPerPage: 20,
-          reuseResults: 600,
-          total: 5000,
-          query: "SELECT * FROM "+ table_name,
-          order_by: 'cartodb_id',
-          mode: 'asc'
-        }
-      );
+        
+        $("table#carto_table").cDBtable(
+          'start',{
+            getDataUrl: '/v1/tables/',
+            resultsPerPage: 20,
+            reuseResults: 600,
+            total: 5000,
+            query: "SELECT * FROM "+ table_name,
+            order_by: 'cartodb_id',
+            mode: 'asc'
+          }
+        );
+        
+        requests_queue = new loaderQueue();
     });
-
-    

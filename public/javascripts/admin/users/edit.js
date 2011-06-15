32ad5314340be38a20cc5601ab889d527f6800e4
Javier Álvarez Medina <xavijam@gmail.com>
2011-06-15 17:53:02 +0200

    forget password animation

--- public/javascripts/admin/users/edit.js
+++ public/javascripts/admin/users/edit.js
@@ -6,11 +6,12 @@
       $('input#user_password').val('lalalala');
       $('input#user_password_confirmation').val('lalalala');
       
-
+      // Focus hide label
       $('div.unlock_window input[type="password"]').focusin(function(){
         $('span.top label').hide();
       });
 
+      // Focus show label
       $('div.unlock_window input[type="password"]').focusout(function(){
         var value = $(this).val();
         if (value=="") {
@@ -18,6 +19,7 @@
         }
       });
 
+      // Unlock form
       $('div.unlock_window form').submit(function(ev){
         ev.preventDefault();
         $.post($(this).attr('action'), $(this).serialize())
@@ -66,10 +68,18 @@
         $('div.mamufas').fadeIn('fast');
       });
 
+      // Show delete window
       $('a.delete_account:not(.disabled)').live('click', function(ev){
         $('div.mamufas div.delete_window').show();
         $('div.mamufas').fadeIn('fast');
         bindESC();
+      });
+      
+      // Show forget window effect
+      $('a.forget').click(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+        showForget();
       });
     });
 
@@ -92,7 +102,29 @@
 
     function close_mamufas(){
       $('div.mamufas').fadeOut('fast',function(){
-        $('div.mamufas div.unlock_window').hide();
+        $('div.mamufas div.unlock_window').hide(function(){
+          resetUnlock();
+        });
       });
       unbindESC();
     };
+    
+    function showForget() {
+      $('div.unlock_window .inner_').animate({height:'72px'},300);
+      $('div.unlock_window form').animate({opacity:0,},300,function(){
+        $('div.unlock_window form').hide();
+        $('div.unlock_window div.inner_').css('background-position','-200px -200px');
+        $('div.email_sent').show();
+        $('div.email_sent').animate({opacity:1},300);
+      });
+    };
+    
+    
+    function resetUnlock() {
+      $('div.unlock_window .inner_').animate({height:'120px'},300);
+      $('div.unlock_window form').css('opacity',1);
+      $('div.unlock_window form').show();
+      $('div.unlock_window div.inner_').css('background-position','15px 15px');
+      $('div.email_sent').hide();
+      $('div.email_sent').css('opacity',1);
+    }

f9113931a841390cf7f0dda9c951836766d63b02
Fernando Blat <ferblape@gmail.com>
2011-06-15 16:19:49 +0200

    Some more specs about the consistency of the data when an exception occurs

--- vendor/cartodb-importer/spec/import_spec.rb
+++ vendor/cartodb-importer/spec/import_spec.rb
@@ -7,7 +7,7 @@ describe CartoDB::Importer do
   it "should raise an error if :import_from_file option is blank" do
     lambda { 
       CartoDB::Importer.new 
-    }.should raise_error #("import_from_file value can't be nil")
+    }.should raise_error("import_from_file value can't be nil")
   end
   
   it "should get the name from the options" do
@@ -18,6 +18,22 @@ describe CartoDB::Importer do
     result.name.should == 'prefered_name'
     result.rows_imported.should == 2003
     result.import_type.should == '.csv'
+  end
+  
+  it "should remove the table from the database if an exception happens" do
+    options = { :import_from_file => File.expand_path("../cartodb-importer/spec/support/data/empty.csv"),
+                :database => "cartodb_importer_test", :username => 'postgres', :password => '',
+                :host => 'localhost', :port => 5432 }
+    importer = CartoDB::Importer.new options
+    lambda { 
+      importer.import!
+    }.should raise_error
+    
+    db_configuration = options.slice(:database, :username, :password, :host, :port)
+    db_configuration[:port] ||= 5432
+    db_configuration[:host] ||= '127.0.0.1'
+    db_connection = Sequel.connect("postgres://#{db_configuration[:username]}:#{db_configuration[:password]}@#{db_configuration[:host]}:#{db_configuration[:port]}/#{db_configuration[:database]}")
+    db_connection.tables.should_not include(:empty)
   end
   
   it "should suggest a new table name of the format _n if the previous table exists" do
@@ -70,6 +86,16 @@ describe CartoDB::Importer do
       result = importer.import!
       result.name.should == 'vizzuality_shp'
       result.rows_imported.should == 11
+      result.import_type.should == '.shp'
+    end
+    
+    it "should import SHP file TM_WORLD_BORDERS_SIMPL-0.3.zip" do
+      importer = CartoDB::Importer.new :import_from_file => File.expand_path("../cartodb-importer/spec/support/data/TM_WORLD_BORDERS_SIMPL-0.3.zip"),
+                                       :database => "cartodb_importer_test", :username => 'postgres', :password => '',
+                                       :host => 'localhost', :port => 5432
+      result = importer.import!
+      result.name.should == 'tm_world_borders_simpl_0_3_shp'
+      result.rows_imported.should == 246
       result.import_type.should == '.shp'
     end
   end

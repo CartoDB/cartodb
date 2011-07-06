2e3f6d445e03ac07819e65986659b2590724432c
Javier Alvarez <xavijam@gmail.com>
2011-07-06 15:20:49 +0200

    map tab enabled done

--- public/javascripts/admin/table/out_table_elements.js
+++ public/javascripts/admin/table/out_table_elements.js
@@ -668,35 +668,35 @@
     ///////////////////////////////////////
     $('section.subheader ul.tab_menu li a').click(function(ev){
       stopPropagation(ev);
-      //closeOutTableWindows();
-					//       if (!$(this).parent().hasClass('selected')) {
-					//         if ($(this).text()=="Table") {
-					// window.location.hash = "#table";
-					//           $('section.subheader ul.tab_menu li').removeClass('selected');
-					//           $(this).parent().addClass('selected');
-					//           $(document).trigger('click');
-					//           $('body').trigger('refresh');
-					//           $('body').trigger('enabled',[true]);
-					//					 $('body').attr('view_mode','table');
-					//           $('div.general_options').removeClass('map').addClass('table');
-					//           $('div.table_position').show();
-					//           hideMap();
-					//         } else {
-					// window.location.hash = "#map";
-					//           if (geolocating) {
-					//             $('p.geo').trigger('click');
-					//           } else {
-					//             $('section.subheader ul.tab_menu li').removeClass('selected');
-					//             $('div.general_options').removeClass('table end').addClass('map');
-					//             $(this).parent().addClass('selected');
-					//             $('div.table_position').hide();
-					//						 $('body').attr('view_mode','map');
-					//             $(document).trigger('click');
-					//             $('body').trigger('enabled',[false]);
-					//             showMap();
-					//           }
-					//         }
-					//       }
+      closeOutTableWindows();
+      if (!$(this).parent().hasClass('selected') && !$(this).parent().hasClass('disabled')) {
+        if ($(this).text()=="Table") {
+ 					window.location.hash = "#table";
+          $('section.subheader ul.tab_menu li').removeClass('selected');
+          $(this).parent().addClass('selected');
+          $(document).trigger('click');
+          $('body').trigger('refresh');
+          $('body').trigger('enabled',[true]);
+					 $('body').attr('view_mode','table');
+          $('div.general_options').removeClass('map').addClass('table');
+          $('div.table_position').show();
+          hideMap();
+        } else {
+ 					window.location.hash = "#map";
+          if (geolocating) {
+            $('p.geo').trigger('click');
+          } else {
+            $('section.subheader ul.tab_menu li').removeClass('selected');
+            $('div.general_options').removeClass('table end').addClass('map');
+            $(this).parent().addClass('selected');
+            $('div.table_position').hide();
+						 $('body').attr('view_mode','map');
+            $(document).trigger('click');
+            $('body').trigger('enabled',[false]);
+            showMap();
+          }
+        }
+      }
     });
         
   });

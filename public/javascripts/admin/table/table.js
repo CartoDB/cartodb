4b5191f619cb8e7c67f209543282db6baa03e9ce
Javier Alvarez <xavijam@gmail.com>
2011-07-06 13:38:33 +0200

    new comments for js code and map tab enabled first steps

--- public/javascripts/admin/table/table.js
+++ public/javascripts/admin/table/table.js
@@ -3,14 +3,16 @@
 	  
     
     head.ready(function(){
-			//       head.js(
-			//         "/javascripts/admin/maps/map.js",
-			//         "/javascripts/admin/maps/map_elements.js",
-			//         "/javascripts/admin/maps/CartoMap.js",
-			//         "http://maps.google.com/maps/api/js?sensor=true&callback=initApp"
-			// );
-			
-			initApp();
+			if (map_enabled) {
+				head.js(
+	        "/javascripts/admin/maps/map.js",
+	        "/javascripts/admin/maps/map_elements.js",
+	        "/javascripts/admin/maps/CartoMap.js",
+	        "http://maps.google.com/maps/api/js?sensor=true&callback=initApp"
+				);
+			} else {
+				initApp();
+			}
     });
 
 
@@ -18,8 +20,8 @@
 			// Inits loader queue
 			requests_queue = new loaderQueue();
 			
-			// Inits map
-			// initMap();
+			// Inits map if map_enabled = true
+			if (map_enabled) initMap();
 			
 			// Inits carto table
 			$("table#carto_table").cDBtable(
@@ -35,14 +37,24 @@
       );
 
 			// Manage tabs with url hash
-			// var hash = window.location.hash;
-			// if (hash == "#map") {
-			// 	$('section.subheader ul.tab_menu li a:contains("Map")').click();
-			// 	setTimeout(function(){$('body').trigger('enabled',[false])},500);
-			// 	$('body').attr('view_mode','map');
-			// } else {
+			manageHash();
+		}
+		
+		
+		function manageHash() {
+			if (map_enabled) {
+				var hash = window.location.hash;
+				if (hash == "#map") {
+					$('section.subheader ul.tab_menu li a:contains("Map")').click();
+					setTimeout(function(){$('body').trigger('enabled',[false])},500);
+					$('body').attr('view_mode','map');
+				} else {
+					$('body').attr('view_mode','table');
+					window.location.hash = "#table";
+				}
+			} else {
 				$('body').attr('view_mode','table');
 				window.location.hash = "#table";
-			//}
+			}
 			$('body').attr('query_mode','false');
 		}

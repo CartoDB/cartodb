9581f4c343b5534b5da4103384c3bf69e9d79cd0
Javier Alvarez <xavijam@gmail.com>
2011-08-23 15:39:11 +0200

    map works again with sql

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -5,15 +5,15 @@ class Api::ApplicationController < ApplicationController
   before_filter :api_authorization_required
   # TO ACTIVATE when decided how to do it more efficiently without filling the Redis
   # after_filter :log_request
-  
+
   protected
 
   # def log_request
   #   # $queries_log.lpush "log-#{Date.today.strftime("%Y-%m-%d")}", request.remote_ip + '#' + (@to_log || "#{controller_name}##{action_name}")
   # end
-  
+
   def set_start_time
     @time_start = Time.now
   end
-  
+
 end

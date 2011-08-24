675bc64fec53f0eae0aec7a94bd8cb3ebb1fb276
Alvaro Bautista <alvarobp@gmail.com>
2011-08-24 19:07:34 +0200

    Static 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def render_404
     respond_to do |format|
       format.html do
-        render :file => "public/404.html.erb", :status => 404, :layout => false
+        render :file => "public/404.html", :status => 404, :layout => false
       end
       format.json do
         render :nothing => true, :status => 404

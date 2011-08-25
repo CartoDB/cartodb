54b622980d800cd357bca45b5f8424aaf59638c3
Alvaro Bautista <alvarobp@gmail.com>
2011-08-25 12:22:27 +0200

    Point remember password link to account host if it's configured

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,2 +1,9 @@
 module LoginHelper
+  
+  def forget_password_url
+    if APP_CONFIG[:account_host]
+      "#{request.protocol}#{APP_CONFIG[:account_host]}/password_resets/new"
+    end
+  end
+  
 end

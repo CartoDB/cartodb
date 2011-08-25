58f1092469c6b2c0dea1b90a555901367f99cb4a
Alvaro Bautista <alvarobp@gmail.com>
2011-08-25 12:07:49 +0200

    Remove Home actions and views no longer used

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,6 @@ class HomeController < ApplicationController
 
   layout 'front_layout'
 
-  def index
-    if logged_in?
-      redirect_to dashboard_path and return
-    else
-      @user = User.new
-    end
-  end
-
   def app_status
     db_ok    = Rails::Sequel.connection.select('OK').first.values.include?('OK')
     redis_ok = $tables_metadata.dbsize

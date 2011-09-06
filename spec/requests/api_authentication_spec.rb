050619f0c1b4f1c49eac3ac5baa33c74b9eaface
Alvaro Bautista <alvarobp@gmail.com>
2011-09-06 13:48:33 +0200

    Upgrade to capybara 1.1.1

--- spec/requests/api_authentication_spec.rb
+++ spec/requests/api_authentication_spec.rb
@@ -68,16 +68,4 @@ describe "API Authentication" do
     end
   end
   
-  def prepare_oauth_request(consumer, url, options={})
-    url = URI.parse(url)
-    http = Net::HTTP.new(url.host, url.port)
-    if options[:form_data]
-      req = Net::HTTP::Post.new(url.request_uri)
-      req.set_form_data(options[:form_data])
-    else
-      req = Net::HTTP::Get.new(url.request_uri)
-    end
-    req.oauth!(http, consumer, options[:token])
-    req
-  end
 end

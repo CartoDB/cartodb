967e2f233d05bf4719247c554901c013a7fdf932
Alvaro Bautista <alvarobp@gmail.com>
2011-09-07 19:01:10 +0200

    Decapified

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-role :app, appserver_production
-role :web, appserver_production
-role :db,  appserver_production, :primary => true
-
-set :branch, "production"
-
-task :set_staging_flag, :roles => [:app] do
-end
--- deploy_production.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git pull
-git checkout staging
-git merge master
-git checkout production
-git merge staging
-git push
-git checkout master
-cap production deploy

e522bad2c49dd698350baaf29344b1f55d7befba
Alvaro Bautista <alvarobp@gmail.com>
2011-09-07 18:06:02 +0200

    Adapt failing specs to new importer

--- spec/acceptance/superadmin/users_spec.rb
+++ spec/acceptance/superadmin/users_spec.rb
@@ -19,12 +19,10 @@ feature "Superadmin's users API" do
   scenario "user create fail" do
     @user_atts[:email] = nil
 
-    basic_auth do
-      post_json superadmin_users_path, { :user => @user_atts } do |response|
-        response.status.should == 422
-        response.body[:email].should be_present
-        response.body[:email].should include("is not present")
-      end
+    post_json superadmin_users_path, { :user => @user_atts }, default_headers do |response|
+      response.status.should == 422
+      response.body[:email].should be_present
+      response.body[:email].should include("is not present")
     end
   end
 
@@ -32,57 +30,50 @@ feature "Superadmin's users API" do
     @user_atts.delete(:crypted_password)
     @user_atts.delete(:salt)
     @user_atts.merge!(:password => "this_is_a_password")
-    basic_auth do
-      post_json superadmin_users_path, { :user => @user_atts } do |response|
-        response.status.should == 201
-        response.body[:email].should == @user_atts[:email]
-        response.body[:username].should == @user_atts[:username]
-        response.body.should_not have_key(:crypted_password)
-        response.body.should_not have_key(:salt)
 
-        # Double check that the user has been created properly
-        user = User.filter(:email => @user_atts[:email]).first
-        user.should be_present
-        user.id.should == response.body[:id]
-        User.authenticate(user.username, "this_is_a_password").should == user
-      end
+    post_json superadmin_users_path, { :user => @user_atts }, default_headers do |response|
+      response.status.should == 201
+      response.body[:email].should == @user_atts[:email]
+      response.body[:username].should == @user_atts[:username]
+      response.body.should_not have_key(:crypted_password)
+      response.body.should_not have_key(:salt)
+
+      # Double check that the user has been created properly
+      user = User.filter(:email => @user_atts[:email]).first
+      user.should be_present
+      user.id.should == response.body[:id]
+      User.authenticate(user.username, "this_is_a_password").should == user
     end
   end
   
   scenario "user create with crypted_password and salt success" do
-    basic_auth do
-      post_json superadmin_users_path, { :user => @user_atts } do |response|
-        response.status.should == 201
-        response.body[:email].should == @user_atts[:email]
-        response.body[:username].should == @user_atts[:username]
-        response.body.should_not have_key(:crypted_password)
-        response.body.should_not have_key(:salt)
-        
-        # Double check that the user has been created properly
-        user = User.filter(:email => @user_atts[:email]).first
-        user.should be_present
-        user.id.should == response.body[:id]
-        User.authenticate(user.username, "this_is_a_password").should == user
-      end
+    post_json superadmin_users_path, { :user => @user_atts }, default_headers do |response|
+      response.status.should == 201
+      response.body[:email].should == @user_atts[:email]
+      response.body[:username].should == @user_atts[:username]
+      response.body.should_not have_key(:crypted_password)
+      response.body.should_not have_key(:salt)
+      
+      # Double check that the user has been created properly
+      user = User.filter(:email => @user_atts[:email]).first
+      user.should be_present
+      user.id.should == response.body[:id]
+      User.authenticate(user.username, "this_is_a_password").should == user
     end
   end
   
   scenario "user update fail" do
     user = create_user
     
-    basic_auth do
-      put_json superadmin_user_path(user), { :user => { :email => "" } } do |response|
-        response.status.should == 422
-      end
+    put_json superadmin_user_path(user), { :user => { :email => "" } }, default_headers do |response|
+      response.status.should == 422
     end
   end
   
   scenario "user update success" do
     user = create_user
-    basic_auth do
-      put_json superadmin_user_path(user), { :user => { :email => "newmail@test.com" } } do |response|
-        response.status.should == 200
-      end
+    put_json superadmin_user_path(user), { :user => { :email => "newmail@test.com" } }, default_headers do |response|
+      response.status.should == 200
     end
     user = User[user.id]
     user.email.should == "newmail@test.com"
@@ -90,20 +81,18 @@ feature "Superadmin's users API" do
   
   scenario "user delete success" do
     user = create_user
-    basic_auth do
-      delete_json superadmin_user_path(user) do |response|
-        response.status.should == 200
-      end
+    delete_json superadmin_user_path(user), default_headers do |response|
+      response.status.should == 200
     end
     User[user.id].should be_nil
   end
   
   private
   
-  # set http auth and json headers for request
-  def basic_auth(user = APP_CONFIG[:superadmin]["username"], password = APP_CONFIG[:superadmin]["password"])
-    header('AUTHORIZATION', ActionController::HttpAuthentication::Basic.encode_credentials(user, password))
-    header('accept', "application/json")
-    yield if block_given?
+  def default_headers(user = APP_CONFIG[:superadmin]["username"], password = APP_CONFIG[:superadmin]["password"])
+    { 
+      'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(user, password),
+      'HTTP_ACCEPT' => "application/json"
+    }
   end
 end

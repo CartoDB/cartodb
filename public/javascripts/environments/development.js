4b3d0cc8964f533d2e3f140b07dcfc806f7d7d21
Simon Tokumine <simon@japancentre.com>
2011-09-13 22:56:17 -0600

    ssl maps in production

--- public/javascripts/environments/development.js
+++ public/javascripts/environments/development.js
@@ -1,2 +1,3 @@
+var TILEHTTP = "http";
 var TILESERVER = "localhost.lan:8181"
 var global_api_url = '/api/v1/';
--- public/javascripts/environments/production.js
+++ public/javascripts/environments/production.js
@@ -1,2 +1,3 @@
+var TILEHTTP = "https";
 var TILESERVER = "cartodb.com";
 var global_api_url = '/api/v1/';

8c24c12961088e45b58a6d06e7f36a780b48d03d
Simon Tokumine <simon@japancentre.com>
2011-09-13 22:35:18 -0600

    use proper tileserver urls

--- public/javascripts/admin/maps/CartoMap.js
+++ public/javascripts/admin/maps/CartoMap.js
@@ -689,8 +689,8 @@
       return {
         tilejson: '1.0.0',
         scheme: 'xyz',
-        tiles: ['http://'+user_name+'.localhost.lan:8181/tiles/'+table_name+'/{z}/{x}/{y}.png8?cache_buster={cache}'+query],
-        grids: ['http://'+user_name+'.localhost.lan:8181/tiles/'+table_name+'/{z}/{x}/{y}.grid.json'],
+        tiles: ['http://' + user_name + '.' + TILESERVER + '/tiles/' + table_name + '/{z}/{x}/{y}.png8?cache_buster={cache}'+query],
+        grids: ['http://' + user_name + '.' + TILESERVER + '/tiles/' + table_name + '/{z}/{x}/{y}.grid.json'],
         formatter: function(options, data) { 
           currentCartoDbId = data.cartodb_id;
           return data.cartodb_id; 
--- public/javascripts/environments/development.js
+++ public/javascripts/environments/development.js
@@ -1,2 +1,2 @@
-var TILESERVER = "http://localhost:8080"
+var TILESERVER = "localhost.lan:8181"
 var global_api_url = '/api/v1/';

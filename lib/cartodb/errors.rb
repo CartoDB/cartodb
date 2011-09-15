8e4aa189528973234222965fd5da312e9ee08a39
Alvaro Bautista <alvarobp@gmail.com>
2011-09-15 14:00:13 +0200

    Change parsing of postgres errors Separate column not existing errors

--- lib/cartodb/errors.rb
+++ lib/cartodb/errors.rb
@@ -9,19 +9,29 @@ class CartoDB::InvalidSRID < StandardError; end
 class CartoDB::InvalidGeoJSONFormat < StandardError; end
 class CartoDB::QueryNotAllowed < StandardError; end
 
-class CartoDB::TableNotExists < StandardError
+class CartoDB::DbError < StandardError
   attr_accessor :message
-  def initialize(table_name)
-    @message = "Table #{table_name} doesn't exist"
+  def initialize(msg)
+    # Example of an error (newline included):
+    # Pgerror: error:  relation "antantaric_species" does not exist
+    # LINE 1: insert into antantaric_species (name_of_species,family) valu...
+    
+    # So we suppose everything we need is in the first line
+    @message = msg.split("\n").first.gsub(/pgerror: error:/i,'').strip
   end
+  def to_s; @message; end
 end
 
+class CartoDB::TableNotExists < CartoDB::DbError; end
 
-class CartoDB::ErrorRunningQuery < StandardError
+class CartoDB::ColumnNotExists < CartoDB::DbError; end
+
+class CartoDB::ErrorRunningQuery < CartoDB::DbError
   attr_accessor :db_message # the error message from the database
   attr_accessor :syntax_message # the query and a marker where the error is
 
   def initialize(message)
+    super(message)
     @db_message = message.split("\n")[0]
     @syntax_message = message.split("\n")[1..-1].join("\n")
   end
@@ -29,13 +39,12 @@ end
 
 class CartoDB::InvalidQuery < StandardError
   attr_accessor :message
-
   def initialize
     @message = "Only SELECT statement is allowed"
   end
 end
 
-class CartoDB::InvalidType < StandardError
+class CartoDB::InvalidType < CartoDB::DbError
   attr_accessor :db_message # the error message from the database
   attr_accessor :syntax_message # the query and a marker where the error is
 

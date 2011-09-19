b1dd08cd55e82b2e7d2b9eb4a5532d503e2bc3be
Javier de la Torre <jatorre@gmail.com>
2011-09-19 18:10:49 -0400

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ CartoDB::Application.routes.draw do
       get    '/column_types'                         => 'meta#column_types'
       get    '/tables'                               => 'tables#index'
       post   '/tables'                               => 'tables#create'
+      get    '/tables/lala'                          => 'tables#create'
       get    '/tables/tags/:tag_name'                => 'tables#index'
       get    '/tables/tags'                          => 'tags#index'
       get    '/tables/:id'                           => 'tables#show'

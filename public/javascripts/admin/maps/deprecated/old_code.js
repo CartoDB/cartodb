5bef621d3e8af9597802208c2a8984c6a46cd418
Javier Alvarez <xavijam@gmail.com>
2011-09-19 10:11:34 -0400

    adding polygon staff in the map and some bugs solved in tables index

--- public/javascripts/admin/maps/deprecated/old_code.js
+++ public/javascripts/admin/maps/deprecated/old_code.js
@@ -1,178 +1,163 @@
+////////////////////////////////////////
+//  POLYGON OPERATIONS OVER THE MAP		//
+////////////////////////////////////////
+/* Disable selection area  */
+CartoMap.prototype.disableSelectionTool = function() {
+	this.map_.setOptions({draggable:true});
+  $('div#map').unbind('mousedown');
+  if (this.selection_area_!=null) this.selection_area_.setMap(null);
+}
 
-// var markers = [];
-// var bounds;
-// var geocoder;
-// var image;
-// var globalZindex = 1;
-// var po;
+/* Enable selection area  */	
+CartoMap.prototype.enableSelectionTool = function() {
+	var me = this;
+	this.map_.setOptions({draggable:false});
+   
+  // Selection tool
+  $('div#map').mousedown(function(ev){
+   	if (me.status_=="select_area") {
+   		google.maps.event.clearListeners(me.selection_area_, 'mouseover');
+      google.maps.event.clearListeners(me.selection_area_, 'mouseout');
+      me.tooltip_.hide();
+            
+      var position = {};
+      position.x = ev.pageX-($('div#map').offset().left);
+      position.y = ev.pageY-($('div#map').offset().top);
+      var latlng = me.map_canvas_.transformCoordinates(new google.maps.Point(position.x,position.y));
+      
+      me.selection_area_.setOptions({fillOpacity: 0});
+      me.drawing = true;
+      me.selection_area_.setPath([latlng,latlng,latlng,latlng]);
+      me.selection_area_.setMap(me.map_);
+      
+      $('div#map').mousemove(function(ev){
+        position.x = ev.pageX-($('div#map').offset().left);
+        position.y = ev.pageY-($('div#map').offset().top);
+        var latlng = me.map_canvas_.transformCoordinates(new google.maps.Point(position.x,position.y));
+             
+       	me.selection_area_.setPath([
+					me.selection_area_.getPath().getAt(0),
+          new google.maps.LatLng(me.selection_area_.getPath().getAt(0).lat(),latlng.lng()),
+          latlng,
+          new google.maps.LatLng(latlng.lat(),me.selection_area_.getPath().getAt(0).lng()),
+          me.selection_area_.getPath().getAt(0)]);
+ 				});
+      
+       	$('div#map').mouseup(function(ev){
+        	var position = {};
+         	position.x = ev.pageX-($('div#map').offset().left);
+         	position.y = ev.pageY-($('div#map').offset().top);
+         	var latlng = me.map_canvas_.transformCoordinates(new google.maps.Point(position.x,position.y));
+         
+         	$('div#map').unbind('mouseup');
+         	$('div#map').unbind('mousemove');
 
-// var radius = 10, tips = {};
-// var layer;
-// var tile_url = TILESERVER + '/tiles/{X}/{Y}/{Z}/users/'+user_id+'/';
+        	drawing = false;
+          me.selection_area_.setOptions({fillOpacity: 0.40});
+          google.maps.event.clearListeners(me.map_, 'mousemove');
+          google.maps.event.clearListeners(me.selection_area_, 'click');
+
+					var markers = me.markersInPolygon(me.selection_area_);
+  
+          google.maps.event.addListener(me.selection_area_,'mouseover',function(){
+            me.tooltip_.openPolgyon(latlng,markers);
+           	me.over_marker_ = true;
+						me.tooltip_.show();
+          });
+        
+          google.maps.event.addListener(me.selection_area_,'mouseout',function(){
+           	me.over_marker_ = false;
+          });
+
+					me.tooltip_.show();
+       	});
+     	}
+   });
+}
+
+/* Calculate markers in selection area  */			
+CartoMap.prototype.markersInPolygon = function(selection_polygon) {
+ 	//Check if the polygon contains this point
+  function Contains(polygon, point) { 
+     var j=0; 
+     var oddNodes = false; 
+     var x = point.lng(); 
+     var y = point.lat(); 
+     for (var i=0; i < polygon.getPath().getLength(); i++) { 
+       j++; 
+       if (j == polygon.getPath().getLength()) {j = 0;} 
+       if (((polygon.getPath().getAt(i).lat() < y) && (polygon.getPath().getAt(j).lat() >= y)) || ((polygon.getPath().getAt(j).lat() < y) && (polygon.getPath().getAt(i).lat() >= y))) { 
+         if ( polygon.getPath().getAt(i).lng() + (y - polygon.getPath().getAt(i).lat()) /  (polygon.getPath().getAt(j).lat()-polygon.getPath().getAt(i).lat()) *  (polygon.getPath().getAt(j).lng() - polygon.getPath().getAt(i).lng())<x ) { 
+           oddNodes = !oddNodes; 
+         } 
+       } 
+     } 
+     return oddNodes; 
+   };
+   
+  var markers_polygon = [];
+	_.each(this.points_, function(element){
+    if (Contains(selection_polygon,element.getPosition())) {
+      markers_polygon.push(element);
+    }
+   });
+   return markers_polygon;
+}
 
 
 
-// layer = new google.maps.ImageMapType({
-//    getTileUrl: function(tile, zoom) {
-//      return generateURL(table_name,sql).replace('{X}',tile.x).replace('{Y}',tile.y).replace('{Z}',zoom);
-//    },
-//    tileSize: new google.maps.Size(256, 256),
-//    opacity: 1,
-//    isPng: true,
-//    name: "User layer",
-//    alt: ""
-// });
-// map.overlayMapTypes.push(layer);
+////////////////////////////////////////
+//  REQUEST POINTS TO DRAW IN THE MAP	//
+////////////////////////////////////////
+CartoMap.prototype.getPoints = function() {
+  var me = this;
+  
+  var api_key = "",
+			query_url = "";
 
+	if (this.query_mode) {
+	  var value = editor.getValue();
+	  var sql_ = value.split(/(from|FROM|From)/,3);
+	  sql_[1] = ", ST_AsGeoJSON(the_geom) as coordinates_ FROM ";
+	  value = sql_.join('');
+	  
+		query_url = global_api_url+'queries?sql='+ escape(value);
+		var now = new Date();
+	} else {
+		query_url = global_api_url+'queries?sql='+ escape("select *,ST_AsGeoJSON(the_geom) as coordinates_ from " + table_name);
+	}
+	
+	
 
+  $.ajax({
+    method: 'GET',
+    url: query_url,
+    headers: {'cartodbclient':true},
+    success: function(result) {
+			if (me.query_mode) {
+				var arrived = new Date();
+				var total = result.total_rows;
+        $('div.sql_window p.errors').fadeOut();
+				$('span.query h3').html(total + ' row' + ((total>1)?'s':'') + ' matching your query <a class="clear_table" href="#clear">CLEAR VIEW</a>');
+				$('span.query p').text('This query took '+(arrived - now)/1000+' seconds');
+			}
+			me.drawMarkers(result.rows);
+    },
+    error: function(e) {
+			if (me.query_mode) {
+				var json = $.parseJSON(e.responseText);
+        var msg = '';
 
-
-
-
-// function refreshLayer(_style) {
-//   map.overlayMapTypes.clear();
-//   map.overlayMapTypes.push(layer);
-// }
-// 
-// function generateURL(_table_name, _sql){
-//   var url = tile_url + 'layers/' + _table_name;
-//   if (_sql) url = url + '/?sql='+ Base64.encode(_sql);
-//   return url;        
-// }
-// 
-// 
-// var Base64 = {
-// 
-//   // private property
-//   _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-// 
-//   // public method for encoding
-//   encode : function (input) {
-//       var output = "";
-//       var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-//       var i = 0;
-// 
-//       input = Base64._utf8_encode(input);
-// 
-//       while (i < input.length) {
-// 
-//           chr1 = input.charCodeAt(i++);
-//           chr2 = input.charCodeAt(i++);
-//           chr3 = input.charCodeAt(i++);
-// 
-//           enc1 = chr1 >> 2;
-//           enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-//           enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-//           enc4 = chr3 & 63;
-// 
-//           if (isNaN(chr2)) {
-//               enc3 = enc4 = 64;
-//           } else if (isNaN(chr3)) {
-//               enc4 = 64;
-//           }
-// 
-//           output = output +
-//           this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-//           this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-// 
-//       }
-// 
-//       return output;
-//   },
-// 
-//   // public method for decoding
-//   decode : function (input) {
-//       var output = "";
-//       var chr1, chr2, chr3;
-//       var enc1, enc2, enc3, enc4;
-//       var i = 0;
-// 
-//       input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-// 
-//       while (i < input.length) {
-// 
-//           enc1 = this._keyStr.indexOf(input.charAt(i++));
-//           enc2 = this._keyStr.indexOf(input.charAt(i++));
-//           enc3 = this._keyStr.indexOf(input.charAt(i++));
-//           enc4 = this._keyStr.indexOf(input.charAt(i++));
-// 
-//           chr1 = (enc1 << 2) | (enc2 >> 4);
-//           chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-//           chr3 = ((enc3 & 3) << 6) | enc4;
-// 
-//           output = output + String.fromCharCode(chr1);
-// 
-//           if (enc3 != 64) {
-//               output = output + String.fromCharCode(chr2);
-//           }
-//           if (enc4 != 64) {
-//               output = output + String.fromCharCode(chr3);
-//           }
-// 
-//       }
-// 
-//       output = Base64._utf8_decode(output);
-// 
-//       return output;
-// 
-//   },
-// 
-//   // private method for UTF-8 encoding
-//   _utf8_encode : function (string) {
-//       string = string.replace(/\r\n/g,"\n");
-//       var utftext = "";
-// 
-//       for (var n = 0; n < string.length; n++) {
-// 
-//           var c = string.charCodeAt(n);
-// 
-//           if (c < 128) {
-//               utftext += String.fromCharCode(c);
-//           }
-//           else if((c > 127) && (c < 2048)) {
-//               utftext += String.fromCharCode((c >> 6) | 192);
-//               utftext += String.fromCharCode((c & 63) | 128);
-//           }
-//           else {
-//               utftext += String.fromCharCode((c >> 12) | 224);
-//               utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-//               utftext += String.fromCharCode((c & 63) | 128);
-//           }
-// 
-//       }
-// 
-//       return utftext;
-//   },
-// 
-//   // private method for UTF-8 decoding
-//   _utf8_decode : function (utftext) {
-//       var string = "";
-//       var i = 0;
-//       var c = c1 = c2 = 0;
-// 
-//       while ( i < utftext.length ) {
-// 
-//           c = utftext.charCodeAt(i);
-// 
-//           if (c < 128) {
-//               string += String.fromCharCode(c);
-//               i++;
-//           }
-//           else if((c > 191) && (c < 224)) {
-//               c2 = utftext.charCodeAt(i+1);
-//               string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-//               i += 2;
-//           }
-//           else {
-//               c2 = utftext.charCodeAt(i+1);
-//               c3 = utftext.charCodeAt(i+2);
-//               string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-//               i += 3;
-//           }
-// 
-//       }
-// 
-//       return string;
-//   }
-// 
-// }
+        _.each(json.errors,function(text,pos){
+          msg += text + ', ';
+        });
+        msg = msg.substr(0,msg.length-2);
+				
+				$('span.query h3').html('No results for this query <a class="clear_table" href="#clear">CLEAR VIEW</a>');
+				$('span.query p').text('');
+				$('div.sql_window p.errors').text(msg).fadeIn();
+			}
+      me.hideLoader();
+    }
+  });
+}

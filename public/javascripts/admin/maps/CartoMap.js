8307a90a034b2d4c661060a39ad1e2a13b12be22
Simon Tokumine <simon@japancentre.com>
2011-09-20 13:26:28 -0400

    update basemap

--- public/javascripts/admin/maps/CartoMap.js
+++ public/javascripts/admin/maps/CartoMap.js
@@ -45,70 +45,70 @@ function CartoMap (latlng,zoom) {
 CartoMap.prototype.createMap = function () {
 
     var mapStyles = [
-  {
-    stylers: [
-      { saturation: -65 },
-      { gamma: 1.52 }
-    ]
-  },{
-    featureType: "administrative",
-    stylers: [
-      { saturation: -95 },
-      { gamma: 2.58 }
-    ]
-  },{
-    featureType: "water",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "administrative.locality",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "road",
-    stylers: [
-      { visibility: "simplified" },
-      { saturation: -99 },
-      { gamma: 2.04 }
-    ]
-  },{
-    featureType: "poi",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "road.arterial",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "road.local",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "transit",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "poi",
-    stylers: [
-      { saturation: -55 }
-    ]
-  }
-];
+        {
+            stylers: [
+                { saturation: -65 },
+                { gamma: 1.52 }
+            ]
+        },{
+            featureType: "administrative",
+            stylers: [
+                { saturation: -95 },
+                { gamma: 2.26 }
+            ]
+        },{
+            featureType: "water",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "administrative.locality",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "road",
+            stylers: [
+                { visibility: "simplified" },
+                { saturation: -99 },
+                { gamma: 2.22 }
+            ]
+        },{
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "road.arterial",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "road.local",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "transit",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "road",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "poi",
+            stylers: [
+                { saturation: -55 }
+            ]
+        }
+    ];
 
 
     // Generate a google map

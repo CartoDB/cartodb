a9eeca8dc891fa318d88fb946aae50db0d0b0750
Fernando Espinosa <fer@ferdev.com>
2011-09-21 14:27:40 +0200

    Fix stupid typo in specs support file

--- spec/support/setup_dirs.rb
+++ spec/support/setup_dirs.rb
@@ -1,5 +1,5 @@
 dirs = []
 dirs << Rails.root.join('public', 'uploads', 'failed_imports')
 
-dirs.each{|dir| Fileutils.mkdir_p dir unless Dir.exist? dir}
+dirs.each{|dir| FileUtils.mkdir_p dir unless Dir.exist? dir}
 

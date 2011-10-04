2a4d2c7ea61d49cfd27e0c492bc7a1fc610c16ce
Simon Tokumine <simon@japancentre.com>
2011-10-04 03:03:33 +0100

    added sane logging, exception handling and reviewed/refactored core codebase.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,27 +5,14 @@ class Tag < Sequel::Model
   # Ignore mass-asigment on not allowed columns
   self.strict_param_setting = false
 
-  # Allowed columns
   set_allowed_columns(:name)
 
   def self.load_user_tags(user_id, options = {})
     options[:limit] ||= 5
-    fetch("select tags.name, count(*) as count
-                        from tags
-                        where tags.user_id = ?
-                        group by tags.name
-                        order by count desc limit ?", user_id, options[:limit]).all.map{|t| t.values }
+    fetch("SELECT tags.name, count(*) AS count
+                        FROM tags
+                        WHERE tags.user_id = ?
+                        GROUP BY tags.name
+                        ORDER BY count DESC LIMIT ?", user_id, options[:limit]).all.map{|t| t.values }
   end
-
-  # TODO: V2
-  # def self.load_public_tags(user_id, options = {})
-  #   options[:limit] ||= 5
-  #   fetch("select tags.name, count(*) as count
-  #                       from tags
-  #                       inner join user_tables on user_tables.id = tags.table_id and user_tables.privacy = #{Table::PUBLIC}
-  #                       where tags.user_id != ?
-  #                       group by tags.name
-  #                       order by count desc limit ?", user_id, options[:limit]).all.map{|t| t.values }
-  # end
-
 end

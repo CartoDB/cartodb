bc0a7cb5c460f2ac3ac260c11d55c60c678ef65e
Javier Alvarez <xavijam@gmail.com>
2011-11-07 11:46:24 +0100

    close #237

--- public/javascripts/plugins/general_functions.js
+++ public/javascripts/plugins/general_functions.js
@@ -220,3 +220,10 @@
       str += ']}';
       return str;
     }
+
+
+
+		function isURL(s) {
+			var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+			return regexp.test(s);
+		}

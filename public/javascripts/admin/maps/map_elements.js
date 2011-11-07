025b4bd8af333f91dcb77efca550495ce7f2b4f8
Javier Alvarez <xavijam@gmail.com>
2011-11-07 13:47:36 +0100

    close #182

--- public/javascripts/admin/maps/map_elements.js
+++ public/javascripts/admin/maps/map_elements.js
@@ -282,9 +282,8 @@
                   '<li>'+
                     '<a href="#custom">Custom</a>'+
                     '<div class="suboptions">'+
-                      '<label>Custom vars</label>'+
-                      '<ul class="column_names scrollPane">'+
-                      '</ul>'+
+                      '<span class="info_tools"><a class="mark_all" href="#mark_all">mark all</a><a class="clear_all" href="#clear_all">clear all</a></span>'+
+                      '<ul class="column_names scrollPane"></ul>'+
                     '</div>'+
                   '</li>'+
                 '</ul>'+

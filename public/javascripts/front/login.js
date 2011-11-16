ff94e004cda781cdfe42570e0a263b9d0eca9f7f
Javier de la Torre <jatorre@gmail.com>
2011-11-16 15:06:02 -0500

    When going directly to a subdomain autofill the username and focus on the password

--- public/javascripts/front/login.js
+++ public/javascripts/front/login.js
@@ -1,4 +1,13 @@
 
-	head(function(){    
+head(function(){    
     $('div.error_content').delay(2000).fadeOut();
-  });
+    
+    
+    //Check if the username field is empty and if we have a subdomain and fill it automatically and focus
+    //on the password
+    if ($("#email").val()=="" && window.location.host.split(".").length>1) {
+        $("#email").val(window.location.host.split(".")[0]);
+        $('#password').focus();
+    }
+    
+});

6b3221ab8e8fe727bd25777fba859117bdc66260
Javier Alvarez <xavijam@gmail.com>
2012-02-14 12:37:54 +0100

    Confirmation window when renaming a table cannot be closed using x

--- public/javascripts/admin/show/view/view_templates.js
+++ public/javascripts/admin/show/view/view_templates.js
@@ -173,7 +173,6 @@
         '</span>',
       warning_window : 
         '<div class="warning_window">'+
-          '<a href="#close_window" class="close"></a>'+
           '<div class="inner_">'+
             '<form>'+
               '<span class="top">'+

fa10ef213bfae8eaa1ca7611783b2dcbb56573cd
Andrew W Hill <andrewxhill@gmail.com>
2012-02-14 11:49:01 +0100

    kml/sql working

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,3 +5,4 @@
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
 Mime::Type.register "application/octet-stream", :shp
+Mime::Type.register "application/octet-stream", :sql

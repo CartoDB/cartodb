7826b0b9eb850cd6b3ed679a3d483736e572682b
Andrew W Hill <andrewxhill@gmail.com>
2012-02-15 18:06:50 +0100

    updated test on 812 to use st_x, st_y instead of x,y

--- lib/importer/lib/cartodb-importer/loaders/shp.rb
+++ lib/importer/lib/cartodb-importer/loaders/shp.rb
@@ -25,6 +25,7 @@ module CartoDB
         end
 
         full_shp_command = "#{shp2pgsql_bin_path} -s #{shp_args_command[0]} -D -i -g the_geom -W #{shp_args_command[1]} \"#{shp_args_command[2]}\" #{shp_args_command[3].strip} | #{@psql_bin_path} #{host} #{port} -U #{@db_configuration[:username]} -w -d #{@db_configuration[:database]}"
+        p full_shp_command
         log "Running shp2pgsql: #{full_shp_command}"
         
         out = `#{full_shp_command}`

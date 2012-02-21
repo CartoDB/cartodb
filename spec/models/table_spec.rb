d4dad416da69f27ccd2385f806c1b88877f292b1
Andrew W Hill <andrewxhill@gmail.com>
2012-02-21 11:54:55 +0100

    gropuing continued

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -85,8 +85,12 @@ describe Table do
         user_database.table_exists?('wadus_table_23'.to_sym).should be_true
       end
     end
+    it "should store the identifier of its owner when created" do
+      table = create_table
+      $tables_metadata.hget(table.key,"user_id").should == table.user_id.to_s
+    end
   end
-  context "redis setup" do
+  context "redis sync tests" do
     it "should have a unique key to be identified in Redis" do
       table = create_table
       user = User[table.user_id]
@@ -108,234 +112,233 @@ describe Table do
       $tables_metadata.hget(table.key, "user_id").should be_present
       $tables_metadata.hget(table.key,"the_geom_type").should == original_the_geom_type
     end
-  end
-  it "should store the identifier of its owner when created" do
-    table = create_table
-    $tables_metadata.hget(table.key,"user_id").should == table.user_id.to_s
-  end
-  
-  it "should store the_geom_type in Redis" do
-    table = create_table
-    table.the_geom_type.should == "point"
-    $tables_metadata.hget(table.key,"the_geom_type").should == "point"
+    it "should store the_geom_type in Redis" do
+      table = create_table
+      table.the_geom_type.should == "point"
+      $tables_metadata.hget(table.key,"the_geom_type").should == "point"
     
-    table.the_geom_type = "multipolygon"
-    $tables_metadata.hget(table.key,"the_geom_type").should == "multipolygon"
-  end
+      table.the_geom_type = "multipolygon"
+      $tables_metadata.hget(table.key,"the_geom_type").should == "multipolygon"
+    end
   
-  it "should remove the table from Redis when removing the table" do
-    table = create_table
-    $tables_metadata.exists(table.key).should be_true
-    table.destroy
-    $tables_metadata.exists(table.key).should be_false
+    it "should remove the table from Redis when removing the table" do
+      table = create_table
+      $tables_metadata.exists(table.key).should be_true
+      table.destroy
+      $tables_metadata.exists(table.key).should be_false
+    end
   end
 
-  it "has a default schema" do
-    table = create_table
-    table.reload
-    table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
-    table.schema.should be_equal_to_default_cartodb_schema
-  end
+  context "schema and column tests" do
+    it "has a default schema" do
+      table = create_table
+      table.reload
+      table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
+      table.schema.should be_equal_to_default_cartodb_schema
+    end
   
-  it "can be associated to many tags" do
-    user = create_user
-    table = create_table :user_id => user.id, :tags => "tag 1, tag 2,tag 3, tag 3"
-    Tag.count.should == 3
-    tag1 = Tag[:name => 'tag 1']
-    tag1.table_id.should == table.id
-    tag1.user_id.should == user.id
-    tag2 = Tag[:name => 'tag 2']
-    tag2.user_id.should == user.id
-    tag2.table_id.should == table.id
-    tag3 = Tag[:name => 'tag 3']
-    tag3.user_id.should == user.id
-    tag3.table_id.should == table.id
+    it "can be associated to many tags" do
+      user = create_user
+      table = create_table :user_id => user.id, :tags => "tag 1, tag 2,tag 3, tag 3"
+      Tag.count.should == 3
+      tag1 = Tag[:name => 'tag 1']
+      tag1.table_id.should == table.id
+      tag1.user_id.should == user.id
+      tag2 = Tag[:name => 'tag 2']
+      tag2.user_id.should == user.id
+      tag2.table_id.should == table.id
+      tag3 = Tag[:name => 'tag 3']
+      tag3.user_id.should == user.id
+      tag3.table_id.should == table.id
 
-    table.tags = "tag 1"
-    table.save_changes
-    Tag.count.should == 1
-    tag1 = Tag[:name => 'tag 1']
-    tag1.table_id.should == table.id
-    tag1.user_id.should == user.id
+      table.tags = "tag 1"
+      table.save_changes
+      Tag.count.should == 1
+      tag1 = Tag[:name => 'tag 1']
+      tag1.table_id.should == table.id
+      tag1.user_id.should == user.id
 
-    table.tags = "    "
-    table.save_changes
-    Tag.count.should == 0
-  end
+      table.tags = "    "
+      table.save_changes
+      Tag.count.should == 0
+    end
 
-  it "can add a column of a CartoDB::TYPE type" do
-    table = create_table
-    table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
+    it "can add a column of a CartoDB::TYPE type" do
+      table = create_table
+      table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
 
-    resp = table.add_column!(:name => "my new column", :type => "number")
-    resp.should == {:name => "my_new_column", :type => "double precision", :cartodb_type => "number"}
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column, "double precision"])
-  end
+      resp = table.add_column!(:name => "my new column", :type => "number")
+      resp.should == {:name => "my_new_column", :type => "double precision", :cartodb_type => "number"}
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column, "double precision"])
+    end
 
-  it "can modify a column using a CartoDB::TYPE type" do
-    table = create_table
-    resp = table.modify_column!(:name => "name", :type => "number")
-    resp.should == {:name => "name", :type => "double precision", :cartodb_type => "number"}
-  end
+    it "can modify a column using a CartoDB::TYPE type" do
+      table = create_table
+      resp = table.modify_column!(:name => "name", :type => "number")
+      resp.should == {:name => "name", :type => "double precision", :cartodb_type => "number"}
+    end
   
-  it "should not modify the name of a column to a number" do
-    table = create_table
-    lambda {
-      table.modify_column!(:old_name => "name", :new_name => "1")
-    }.should raise_error(CartoDB::InvalidColumnName)
-  end
+    it "should not modify the name of a column to a number" do
+      table = create_table
+      lambda {
+        table.modify_column!(:old_name => "name", :new_name => "1")
+      }.should raise_error(CartoDB::InvalidColumnName)
+    end
 
-  it "can modify it's schema" do
-    table = create_table
-    table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
+    it "can modify it's schema" do
+      table = create_table
+      table.schema(:cartodb_types => false).should be_equal_to_default_db_schema
 
-    lambda {
-      table.add_column!(:name => "my column with bad type", :type => "textttt")
-    }.should raise_error(CartoDB::InvalidType)
+      lambda {
+        table.add_column!(:name => "my column with bad type", :type => "textttt")
+      }.should raise_error(CartoDB::InvalidType)
 
-    resp = table.add_column!(:name => "my new column", :type => "integer")
-    resp.should == {:name => 'my_new_column', :type => 'integer', :cartodb_type => 'number'}
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column, "integer"])
+      resp = table.add_column!(:name => "my new column", :type => "integer")
+      resp.should == {:name => 'my_new_column', :type => 'integer', :cartodb_type => 'number'}
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column, "integer"])
 
-    resp = table.modify_column!(:old_name => "my_new_column", :new_name => "my new column new name", :type => "text")
-    resp.should == {:name => 'my_new_column_new_name', :type => 'text', :cartodb_type => 'string'}
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column_new_name, "text"])
+      resp = table.modify_column!(:old_name => "my_new_column", :new_name => "my new column new name", :type => "text")
+      resp.should == {:name => 'my_new_column_new_name', :type => 'text', :cartodb_type => 'string'}
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column_new_name, "text"])
 
-    resp = table.modify_column!(:old_name => "my_new_column_new_name", :new_name => "my new column")
-    resp.should == {:name => 'my_new_column', :type => "text", :cartodb_type => "string"}
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
+      resp = table.modify_column!(:old_name => "my_new_column_new_name", :new_name => "my new column")
+      resp.should == {:name => 'my_new_column', :type => "text", :cartodb_type => "string"}
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
 
-    resp = table.modify_column!(:name => "my_new_column", :type => "text")
-    resp.should == {:name => 'my_new_column', :type => 'text', :cartodb_type => 'string'}
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
+      resp = table.modify_column!(:name => "my_new_column", :type => "text")
+      resp.should == {:name => 'my_new_column', :type => 'text', :cartodb_type => 'string'}
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
 
-    table.drop_column!(:name => "description")
-    table.reload
-    table.schema(:cartodb_types => false).should_not include([:description, "text"]) 
-
-    lambda {
       table.drop_column!(:name => "description")
-    }.should raise_error
+      table.reload
+      table.schema(:cartodb_types => false).should_not include([:description, "text"]) 
+
+      lambda {
+        table.drop_column!(:name => "description")
+      }.should raise_error
+    end
+
+    it "cannot modify :cartodb_id column" do
+      table = create_table
+      original_schema = table.schema(:cartodb_types => false)
+
+      lambda {
+        table.modify_column!(:old_name => "cartodb_id", :new_name => "new_id", :type => "integer")
+      }.should raise_error
+      table.reload
+      table.schema(:cartodb_types => false).should == original_schema
+
+      lambda {
+        table.modify_column!(:old_name => "cartodb_id", :new_name => "cartodb_id", :type => "float")
+      }.should raise_error
+      table.reload
+      table.schema(:cartodb_types => false).should == original_schema
+
+      lambda {
+        table.drop_column!(:name => "cartodb_id")
+      }.should raise_error
+      table.reload
+      table.schema(:cartodb_types => false).should == original_schema
+    end
+
+    it "should be able to modify it's schema with castings that the DB engine doesn't support" do
+      table = create_table
+      table.add_column!(:name => "my new column", :type => "text")
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
+
+      pk = table.insert_row!(:name => "Text", :my_new_column => "1")
+      table.modify_column!(:old_name => "my_new_column", :new_name => "my new column new name", :type => "integer", :force_value => "NULL")
+      table.reload
+      table.schema(:cartodb_types => false).should include([:my_new_column_new_name, "integer"])
+
+      rows = table.records
+      rows[:rows][0][:my_new_column_new_name].should == 1
+    end
+
   end
+  context "insert and row tests" do
+    it "should be able to insert a new row" do
+      table = create_table
+      table.rows_counted.should == 0
+      primary_key = table.insert_row!({:name => String.random(10), :description => "bla bla bla"})
+      table.reload
+      table.rows_counted.should == 1
+      primary_key.should == table.records(:rows_per_page => 1)[:rows].first[:cartodb_id]
 
-  it "cannot modify :cartodb_id column" do
-    table = create_table
-    original_schema = table.schema(:cartodb_types => false)
+      lambda {
+        table.insert_row!({})
+      }.should_not raise_error(CartoDB::EmptyAttributes)
 
-    lambda {
-      table.modify_column!(:old_name => "cartodb_id", :new_name => "new_id", :type => "integer")
-    }.should raise_error
-    table.reload
-    table.schema(:cartodb_types => false).should == original_schema
+      lambda {
+        table.insert_row!({:non_existing => "bad value"})
+      }.should raise_error(CartoDB::InvalidAttributes)
+    end
 
-    lambda {
-      table.modify_column!(:old_name => "cartodb_id", :new_name => "cartodb_id", :type => "float")
-    }.should raise_error
-    table.reload
-    table.schema(:cartodb_types => false).should == original_schema
+    it "should be able to insert a row with a geometry value" do
+      user = create_user
+      table = new_table :user_id => user.id
+      table.save.reload
 
-    lambda {
-      table.drop_column!(:name => "cartodb_id")
-    }.should raise_error
-    table.reload
-    table.schema(:cartodb_types => false).should == original_schema
-  end
+      lat = -43.941
+      lon = 3.429
+      the_geom = %Q{\{"type":"Point","coordinates":[#{lon},#{lat}]\}}
+      pk = table.insert_row!({:name => "First check_in", :the_geom => the_geom})
 
-  it "should be able to modify it's schema with castings that the DB engine doesn't support" do
-    table = create_table
-    table.add_column!(:name => "my new column", :type => "text")
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column, "text"])
-
-    pk = table.insert_row!(:name => "Text", :my_new_column => "1")
-    table.modify_column!(:old_name => "my_new_column", :new_name => "my new column new name", :type => "integer", :force_value => "NULL")
-    table.reload
-    table.schema(:cartodb_types => false).should include([:my_new_column_new_name, "integer"])
-
-    rows = table.records
-    rows[:rows][0][:my_new_column_new_name].should == 1
-  end
-
-  it "should be able to insert a new row" do
-    table = create_table
-    table.rows_counted.should == 0
-    primary_key = table.insert_row!({:name => String.random(10), :description => "bla bla bla"})
-    table.reload
-    table.rows_counted.should == 1
-    primary_key.should == table.records(:rows_per_page => 1)[:rows].first[:cartodb_id]
-
-    lambda {
-      table.insert_row!({})
-    }.should_not raise_error(CartoDB::EmptyAttributes)
-
-    lambda {
-      table.insert_row!({:non_existing => "bad value"})
-    }.should raise_error(CartoDB::InvalidAttributes)
-  end
-
-  it "should be able to insert a row with a geometry value" do
-    user = create_user
-    table = new_table :user_id => user.id
-    table.save.reload
-
-    lat = -43.941
-    lon = 3.429
-    the_geom = %Q{\{"type":"Point","coordinates":[#{lon},#{lat}]\}}
-    pk = table.insert_row!({:name => "First check_in", :the_geom => the_geom})
-
-    query_result = user.run_query("select ST_X(the_geom) as lon, ST_Y(the_geom) as lat from #{table.name} where cartodb_id = #{pk} limit 1")
-    ("%.3f" % query_result[:rows][0][:lon]).should == ("%.3f" % lon)
-    ("%.3f" % query_result[:rows][0][:lat]).should == ("%.3f" % lat)
-  end
+      query_result = user.run_query("select ST_X(the_geom) as lon, ST_Y(the_geom) as lat from #{table.name} where cartodb_id = #{pk} limit 1")
+      ("%.3f" % query_result[:rows][0][:lon]).should == ("%.3f" % lon)
+      ("%.3f" % query_result[:rows][0][:lat]).should == ("%.3f" % lat)
+    end
   
-  it "should update null value to nil when inserting and updating" do
-    user = create_user
-    table = new_table :user_id => user.id
-    table.force_schema = "valid boolean"
-    table.save.reload
-    pk = table.insert_row!({:valid => "null"})
-    table.record(pk)[:valid].should be_nil
+    it "should update null value to nil when inserting and updating" do
+      user = create_user
+      table = new_table :user_id => user.id
+      table.force_schema = "valid boolean"
+      table.save.reload
+      pk = table.insert_row!({:valid => "null"})
+      table.record(pk)[:valid].should be_nil
     
-    pk = table.insert_row!({:valid => true})
-    table.update_row!(pk, {:valid => "null"})
-    table.record(pk)[:valid].should be_nil    
+      pk = table.insert_row!({:valid => true})
+      table.update_row!(pk, {:valid => "null"})
+      table.record(pk)[:valid].should be_nil    
+    end
+
+    it "should be able to update a row" do
+      table = create_table
+      pk = table.insert_row!({:name => String.random(10), :description => ""})
+      table.update_row!(pk, :description => "Description 123")
+
+      row = table.records(:rows_per_page => 1, :page => 0)[:rows].first
+      row[:description].should == "Description 123"
+
+      lambda {
+        table.update_row!(pk, :non_existing => 'ignore it, please', :description => "Description 123")
+      }.should raise_error(CartoDB::InvalidAttributes)
+    end
+
+    it "should be able to update a row with a geometry value" do
+      user = create_user
+      table = new_table :user_id => user.id
+      table.save.reload
+
+      lat = -43.941
+      lon = 3.429
+      pk = table.insert_row!({:name => "First check_in"})
+
+      the_geom = %Q{\{"type":"Point","coordinates":[#{lon},#{lat}]\}}
+      table.update_row!(pk, {:the_geom => the_geom})
+
+      query_result = user.run_query("select ST_X(the_geom) as lon, ST_Y(the_geom) as lat from #{table.name} where cartodb_id = #{pk} limit 1")
+      ("%.3f" % query_result[:rows][0][:lon]).should == ("%.3f" % lon)
+      ("%.3f" % query_result[:rows][0][:lat]).should == ("%.3f" % lat)
+    end
+
   end
-
-  it "should be able to update a row" do
-    table = create_table
-    pk = table.insert_row!({:name => String.random(10), :description => ""})
-    table.update_row!(pk, :description => "Description 123")
-
-    row = table.records(:rows_per_page => 1, :page => 0)[:rows].first
-    row[:description].should == "Description 123"
-
-    lambda {
-      table.update_row!(pk, :non_existing => 'ignore it, please', :description => "Description 123")
-    }.should raise_error(CartoDB::InvalidAttributes)
-  end
-
-  it "should be able to update a row with a geometry value" do
-    user = create_user
-    table = new_table :user_id => user.id
-    table.save.reload
-
-    lat = -43.941
-    lon = 3.429
-    pk = table.insert_row!({:name => "First check_in"})
-
-    the_geom = %Q{\{"type":"Point","coordinates":[#{lon},#{lat}]\}}
-    table.update_row!(pk, {:the_geom => the_geom})
-
-    query_result = user.run_query("select ST_X(the_geom) as lon, ST_Y(the_geom) as lat from #{table.name} where cartodb_id = #{pk} limit 1")
-    ("%.3f" % query_result[:rows][0][:lon]).should == ("%.3f" % lon)
-    ("%.3f" % query_result[:rows][0][:lat]).should == ("%.3f" % lat)
-  end
-
   it "should increase the tables_count counter from owner" do
     user = create_user
     user.tables_count.should == 0

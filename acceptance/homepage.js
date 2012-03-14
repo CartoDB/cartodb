b9cda219f70861522b6aad3f7ae5bed162a50b19
Simon Tokumine <si@tinypla.net>
2012-03-14 20:58:43 +0100

    more acceptance testing

--- acceptance/homepage.js
+++ acceptance/homepage.js
@@ -1,10 +1,21 @@
 var Browser = require("zombie");
-var assert = require("assert");
+var assert  = require("assert");
+var util    = require('util');
 
 // Load the page from localhost
-browser = new Browser()
+browser = new Browser({
+  debug : true,
+  runScripts : true,
+  waitFor : 0,
+  
+  userAgent : 'Mozilla/5.0 (Ubuntu; X11; Linux i686; rv:9.0) Gecko/20100101 Firefox/9.0'
+});
+  
 browser.visit("http://toktest.localhost.lan:3000/", function () {
-
+  browser.dump();
+  console.log(util.inspect(browser.lastResponse.headers));
+  console.log(browser.cookies().all())
+  
   // Fill email, password and submit form
   browser.
     fill("email", "toktest").
@@ -12,10 +23,7 @@ browser.visit("http://toktest.localhost.lan:3000/", function () {
     pressButton("Log in", function() {
 
       // Form submitted, new page loaded.
-      //assert.equal(browser.location.pathname, "/sessions/create");
-      console.log(browser.dump());
-      //assert.equal(browser.text("title"), "Welcome To Brains Depot");
-
+      browser.dump();
+      console.log(browser.cookies().all())
     });
-
 });

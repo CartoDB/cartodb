26cc7abbb1508c3fd1986b333722506d71950ba3
Andrew W Hill <andrewxhill@gmail.com>
2012-03-20 11:57:56 -0400

    first loader complete

--- lib/importer/lib/cartodb-importer/loaders/osm.rb
+++ lib/importer/lib/cartodb-importer/loaders/osm.rb
@@ -34,46 +34,17 @@ module CartoDB
         if $?.exitstatus != 0  
           @data_import.set_error_code(3005)
           @data_import.log_error(stderr)
-          @data_import.log_error("ERROR: failed to generate SQL from #{@path}")
-          raise "ERROR: failed to generate SQL from #{@path}"
+          @data_import.log_error("ERROR: failed to import #{@path}")
+          raise "ERROR: failed to import #{@path}"
         end
         
         unless (reg = stdout.read).empty?
           @runlog.stdout << reg
         end
         
-        # out = `#{full_shp_command}`
-        # 
-        # if $?.exitstatus != 0
-        #   @data_import.log_error("failed to convert shp to sql")
-        #   raise "failed to convert shp to sql"
-        # end
-        # 
-        # if 0 < out.strip.length
-        #   @data_import.log_update(out)
-        #   @runlog.stdout << out
-        # end
-
-        # TODO: THIS SHOULD BE UPDATE IF NOT NULL TO PREVENT CRASHING
-        if shp_args_command[0] != '4326'
-          @data_import.log_update("reprojecting the_geom column from #{shp_args_command[0]} to 4326")
-          begin  
-            @db_connection.run("ALTER TABLE #{random_table_name} RENAME COLUMN the_geom TO the_geom_orig;")
-            geom_type = @db_connection["SELECT GeometryType(the_geom_orig) as type from #{random_table_name} LIMIT 1"].first[:type]
-            @db_connection.run("SELECT AddGeometryColumn('#{random_table_name}','the_geom',4326, '#{geom_type}', 2);")
-            @db_connection.run("UPDATE \"#{random_table_name}\" SET the_geom = ST_Force_2D(ST_Transform(the_geom_orig, 4326))")
-            @db_connection.run("ALTER TABLE #{random_table_name} DROP COLUMN the_geom_orig")
-            @db_connection.run("CREATE INDEX \"#{random_table_name}_the_geom_gist\" ON \"#{random_table_name}\" USING GIST (the_geom)")
-          rescue Exception => msg  
-            @runlog.err << msg
-            @data_import.set_error_code(2000)
-            @data_import.log_error(msg)
-            @data_import.log_error("ERROR: unable to convert EPSG:#{shp_args_command[0]} to EPSG:4326")
-          end  
-        end        
-
+        
         begin
-          @db_connection.run("ALTER TABLE \"#{random_table_name}\" RENAME TO \"#{@suggested_name}\"")
+          @db_connection.run("ALTER TABLE \"#{random_table_prefix}_point\" RENAME TO #{@suggested_name}_point")
           @table_created = true
         rescue Exception => msg  
           @runlog.err << msg
@@ -83,11 +54,11 @@ module CartoDB
         end  
 
         @entries.each{ |e| FileUtils.rm_rf(e) } if @entries.any?
-        rows_imported = @db_connection["SELECT count(*) as count from \"#{@suggested_name}\""].first[:count]
+        rows_imported = @db_connection["SELECT count(*) as count from #{@suggested_name}_point"].first[:count]
         @import_from_file.unlink
 
         payload = OpenStruct.new({
-                                :name => @suggested_name, 
+                                :name => "#{@suggested_name}_point", 
                                 :rows_imported => rows_imported,
                                 :import_type => @import_type,
                                 :log => @runlog

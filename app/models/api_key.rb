bf3e81cacbd8d5bed8bcc8d7422445af4931d04b
Simon Tokumine <si@tinypla.net>
2012-04-24 17:38:43 -0400

    refactor our APIKey code

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,25 +1,4 @@
 # coding: UTF-8
 
 class APIKey < Sequel::Model(:api_keys)
-
-  def before_validation
-    self.domain = "http://#{domain}" if domain !~ /^http:\/\// && !domain.blank?
-    super
-  end
-
-  def validate
-    super
-    errors.add(:domain, "cannot be blank") if domain.blank?
-    errors.add(:domain, "already taken") if APIKey.filter(:domain => domain, :user_id => self.user_id).count > 0
-  end
-
-  def before_create
-    self.api_key = User.secure_digest(self.domain)
-    super
-  end
-
-  def user
-    User[:id => user_id]
-  end
-
 end

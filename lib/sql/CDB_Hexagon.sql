6f787b2919a011e1a396726597222484639d0aa8
Sandro Santilli <strk@keybit.net>
2012-04-25 16:44:37 +0200

    CDB_HexagonGrid: take tiling into account
    This commit adds an optional "origin" parameter to the function
    which defaults to (0,0) allowing control over the grid origin and
    thus letting you produce big coverages using multiple small calls.
    
    NOTE: snapping to a precision grid for perfect side matching is
    still to be implemented. Why ? Take a look at this:
    http://trac.osgeo.org/postgis/ticket/1789

--- lib/sql/CDB_Hexagon.sql
+++ lib/sql/CDB_Hexagon.sql
@@ -10,56 +10,127 @@ AS $$
     WHERE path[1] % 2 != 0
 $$ LANGUAGE 'sql' IMMUTABLE STRICT;
 
--- Return an hexagon grid covering given extent 
 --
--- TODO: use snapping to ensure full coverage!
--- 
-CREATE OR REPLACE FUNCTION CDB_HexagonGrid(ext GEOMETRY, side FLOAT8)
+-- Fill given extent with an hexagonal coverage
+--
+-- @param ext Extent to fill. Only hexagons with center point falling
+--            inside the extent (or at the lower or leftmost edge) will
+--            be emitted. The returned hexagons will have the same SRID
+--            as this extent.
+--
+-- @param side Side measure for the hexagon.
+--             Maximum diameter will be 2 * side.
+--
+-- @param origin Optional origin to allow for exact tiling.
+--               If omitted the origin will be 0,0.
+--               The parameter is checked for having the same SRID
+--               as the extent.
+--  
+--
+DROP FUNCTION IF EXISTS CDB_HexagonGrid(ext GEOMETRY, side FLOAT8);
+CREATE OR REPLACE FUNCTION CDB_HexagonGrid(ext GEOMETRY, side FLOAT8, origin GEOMETRY DEFAULT NULL)
 RETURNS SETOF GEOMETRY
 AS $$
 DECLARE
   h GEOMETRY; -- hexagon
   c GEOMETRY; -- center point
   rec RECORD;
-  min_diameter FLOAT8;
-  max_diameter FLOAT8;
   hstep FLOAT8; -- horizontal step
   vstep FLOAT8; -- vertical step
-  vstart FLOAT8[];
+  vstart FLOAT8;
+  vstartary FLOAT8[];
   vstartidx INTEGER;
+  hskip INTEGER;
+  hstart FLOAT8;
+  hend FLOAT8;
+  vend FLOAT8;
+  xoff FLOAT8;
+  yoff FLOAT8;
+  srid INTEGER;
 BEGIN
 
-  max_diameter := side;
-  min_diameter := sqrt(3)*side;
-  hstep := side * 3.0/2.0;
-  vstep := min_diameter;
-  vstart := ARRAY[ ST_Ymin(ext) + (vstep/2.0), ST_Ymin(ext) ];
-  vstartidx := 0;
+  --            |     |     
+  --            |hstep|
+  --  ______   ___    |    
+  --  vstep  /     \ ___ /
+  --  ______ \ ___ /     \  
+  --         /     \ ___ / 
+  --
+  --
+
+  vstep := side * sqrt(3); -- x 2 ?
+  hstep := side * 1.5;
+
+  srid := ST_SRID(ext);
+
+  xoff := 0; 
+  yoff := 0;
+
+  IF origin IS NOT NULL THEN
+    IF ST_SRID(origin) != srid THEN
+      RAISE EXCEPTION 'SRID mismatch between extent (%) and origin (%)', srid, ST_SRID(origin);
+    END IF;
+    xoff := ST_X(origin);
+    yoff := ST_Y(origin);
+  END IF;
 
   RAISE DEBUG 'Side: %', side;
-  RAISE DEBUG 'min_diameter: %', min_diameter;
+
+  -- Tweak horizontal start on hstep*2 grid from origin 
+  hskip := ceil((ST_XMin(ext)-xoff)/hstep);
+  RAISE DEBUG 'hskip: %', hskip;
+  hstart := xoff + hskip*hstep;
+  -- Tweak vertical start on hstep grid from origin 
+  vstart := yoff + ceil((ST_Ymin(ext)-yoff)/vstep)*vstep; 
+
+  hend := ST_XMax(ext);
+  vend := ST_YMax(ext);
+
+  -- TODO: snap to grid {h,v}{start,end} ?
+
+  vstartidx := abs(hskip)%2;
+  IF vstartidx = 0 THEN
+    vstartary := ARRAY[ vstart + (vstep/2.0) - vstep, vstart ];
+  ELSE
+    vstartary := ARRAY[ vstart - (vstep/2.0), vstart ];
+  END IF;
+
+  RAISE DEBUG 'hstart: %', hstart;
+  RAISE DEBUG 'vstart: % : %', vstartary[1], vstartary[2];
+  RAISE DEBUG 'vstartidx: %', vstartidx;
+
+
+  RAISE DEBUG 'hstep: %', hstep;
   RAISE DEBUG 'vstep: %', vstep;
 
-  c := ST_MakePoint(ST_XMin(ext), vstart[vstartidx+1]);
+  c := ST_SetSRID(ST_MakePoint(hstart, vstartary[vstartidx+1]), srid);
   vstartidx := (vstartidx + 1) % 2;
   LOOP -- over X
     --RAISE DEBUG 'X loop starts, center point: %', ST_AsText(c);
     LOOP -- over Y
-      h := CDB_MakeHexagon(c, side);
-      RETURN NEXT h;
-      c := ST_Translate(c, 0, vstep);
       --RAISE DEBUG 'Center: %', ST_AsText(c);
-      IF c |>> ext THEN
+      -- this one should only deal with the start, end is taken care of
+      -- by the final check (correct that if we start at the upmost
+      -- or rightmost boundary we end up emitting an hexagon which should
+      -- not be there, but this should only happen for extents which
+      -- are very narrow either dimension).
+      IF ST_Intersects(c, ext) THEN
+        h := CDB_MakeHexagon(c, side);
+        -- TODO: snap to grid !
+        RETURN NEXT h;
+      END IF;
+      c := ST_Translate(c, 0, vstep);
+      IF ST_Y(c) >= vend THEN
         EXIT;
       END IF;
     END LOOP;
-    c := ST_MakePoint(ST_X(c)+hstep, vstart[vstartidx+1]);
+    c := ST_SetSRID(ST_MakePoint(ST_X(c)+hstep, vstartary[vstartidx+1]), srid);
     vstartidx := (vstartidx + 1) % 2;
-    IF c >> ext THEN
+    IF ST_X(c) >= hend THEN
         EXIT;
     END IF;
   END LOOP;
 
   RETURN;
 END
-$$ LANGUAGE 'plpgsql' IMMUTABLE STRICT;
+$$ LANGUAGE 'plpgsql' IMMUTABLE;

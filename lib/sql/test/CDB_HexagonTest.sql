e0e76843f785a420c277a6fb2f762601570ffb98
Sandro Santilli <strk@keybit.net>
2012-05-03 18:01:48 +0200

    Another test for hexagon grid (cell centroids must intersect extent)

--- lib/sql/test/CDB_HexagonTest.sql
+++ lib/sql/test/CDB_HexagonTest.sql
@@ -5,7 +5,8 @@
 --
 
 WITH
- grid AS ( SELECT * FROM CDB_HexagonGrid(ST_MakeEnvelope(10, 10, 20, 20), 2) as cell ),
+ params AS ( SELECT ST_MakeEnvelope(10, 10, 20, 20) as env, 2 as radius ),
+ grid AS ( SELECT CDB_HexagonGrid(env, radius) AS cell from params),
  igrid AS ( SELECT row_number() over (), cell from grid )
 
  SELECT count(row_number) as r1, sum(st_npoints(cell)) as r2, 'count / npoints' as err
@@ -26,5 +27,11 @@ UNION ALL
        ST_Intersects(g1.cell, g2.cell)
        GROUP BY g1.row_number
        HAVING count(g2.row_number) > 6 OR count(g2.row_number) < 2
+
+UNION ALL
+
+ SELECT g1.row_number, null::integer, 'centroid out of extent' as err
+ FROM igrid g1, params
+ WHERE NOT ST_Intersects(ST_Centroid(g1.cell), params.env)
 
 	;

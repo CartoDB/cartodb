401541221836024dbf8a8231b07a62adde2243e5
Sandro Santilli <strk@keybit.net>
2012-05-10 12:35:32 +0200

    Have CDB_QueryTables return NULL on invalid query
    Invalid queries may be those that are not valid to explain (like
    create or insert) and those containing syntax errors or those
    referencing non-existing tables...
    
    The function would return NULL for them, and raise a WARNING.
    Hopefully this way it'll be easier to collect tables affected by
    different portions of a concatenated-statement query.

--- lib/sql/test/CDB_QueryTablesTest.sql
+++ lib/sql/test/CDB_QueryTablesTest.sql
@@ -1,5 +1,13 @@
+
 WITH inp AS ( select 'SELECT * FROM geometry_columns'::text as q )
  SELECT q, CDB_QueryTables(q) from inp;
 
 WITH inp AS ( select 'SELECT a.attname FROM pg_class c JOIN pg_attribute a on (a.attrelid = c.oid)'::text as q )
  SELECT q, CDB_QueryTables(q) from inp;
+
+WITH inp AS ( select $quote$CREATE table "my'tab;le" as select 1$quote$::text as q )
+ SELECT q, CDB_QueryTables(q) from inp;
+
+-- Cannot automate this until I figure how to omit filename from the WARNING output..
+--WITH inp AS ( select 'select 1 from nonexistent'::text as q )
+-- SELECT q, CDB_QueryTables(q) from inp;

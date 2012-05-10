1e7345de9fcf3f21f3e63ba333e0de650148f0cd
Sandro Santilli <strk@keybit.net>
2012-05-10 15:46:50 +0200

    Slighly modify the query-parsing regexp to reduce matches

--- lib/sql/CDB_QueryStatements.sql
+++ lib/sql/CDB_QueryStatements.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION CDB_QueryStatements(query text)
 RETURNS SETOF TEXT AS $$
    SELECT stmt FROM (
      SELECT btrim(q[1], E' \n\t\r;') as stmt FROM (
-       SELECT regexp_matches( $1, $REG$((?:[^'"$;]*|"[^"]*"|'(?:[^']*|'')*'|(\$[^$]*\$).*?\2)+)$REG$, 'g' ) as q
+       SELECT regexp_matches( $1, $REG$((?:[^'"$;]+|"[^"]*"|'(?:[^']*|'')*'|(\$[^$]*\$).*?\2)+)$REG$, 'g' ) as q
      ) i
    ) j
    WHERE stmt <> '';

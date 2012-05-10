eec468ae585d424c51d17200a9e32402fbf6c213
Sandro Santilli <strk@keybit.net>
2012-05-10 17:22:06 +0200

    Have CDB_QueryTables accept concatenated statements
    It will use CDB_QueryStatements internally

--- lib/sql/test/CDB_QueryTablesTest.sql
+++ lib/sql/test/CDB_QueryTablesTest.sql
@@ -11,6 +11,9 @@ WITH inp AS ( select $quote$CREATE table "my'tab;le" as select 1$quote$::text as
 WITH inp AS ( select 'SELECT a.oid, b.oid FROM pg_class a, pg_class b'::text as q )
  SELECT q, CDB_QueryTables(q) from inp;
 
+WITH inp AS ( select 'SELECT 1 as col1; select 2 as col2'::text as q )
+ SELECT q, CDB_QueryTables(q) from inp;
+
 -- Cannot automate this until I figure how to omit filename from the WARNING output..
 --WITH inp AS ( select 'select 1 from nonexistent'::text as q )
 -- SELECT q, CDB_QueryTables(q) from inp;

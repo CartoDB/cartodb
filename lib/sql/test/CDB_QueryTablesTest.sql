c470bdeaa693756028cdd4472aa1a2d3632d7edf
Sandro Santilli <strk@keybit.net>
2012-05-10 13:57:30 +0200

    Fix CDB_QueryTables to not return duplicated names
    Also, change return on invalid query to be the empty array.

--- lib/sql/test/CDB_QueryTablesTest.sql
+++ lib/sql/test/CDB_QueryTablesTest.sql
@@ -8,6 +8,9 @@ WITH inp AS ( select 'SELECT a.attname FROM pg_class c JOIN pg_attribute a on (a
 WITH inp AS ( select $quote$CREATE table "my'tab;le" as select 1$quote$::text as q )
  SELECT q, CDB_QueryTables(q) from inp;
 
+WITH inp AS ( select 'SELECT a.oid, b.oid FROM pg_class a, pg_class b'::text as q )
+ SELECT q, CDB_QueryTables(q) from inp;
+
 -- Cannot automate this until I figure how to omit filename from the WARNING output..
 --WITH inp AS ( select 'select 1 from nonexistent'::text as q )
 -- SELECT q, CDB_QueryTables(q) from inp;

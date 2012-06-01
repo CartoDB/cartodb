9cabbbf9870537b3d9d365390dab898bf7e4cc4d
Andrew W Hill <andrewxhill@gmail.com>
2012-06-01 18:00:39 -0400

    added table check and metadata delete if a drop table is detected

--- app/controllers/api/json/queries_controller.rb
+++ app/controllers/api/json/queries_controller.rb
@@ -27,6 +27,25 @@ class Api::Json::QueriesController < Api::ApplicationController
         CartoDB::Logger.info "exception automatically creating new cartodb table", errors
       end
     end
+    if params[:sql].downcase.include? "drop table "
+      begin
+        #get all tables in user_tables
+        #check each if exists in database
+        #drop each that doesn't exist
+        Table.fetch("select name,id from user_tables
+                          where user_tables.user_id = ?", current_user.id).all do |row|
+          if current_user.run_pg_query("SELECT relname FROM pg_class WHERE relname='#{row.name}'")[:rows].empty?
+            Table.filter(:user_id => current_user.id, :id => row.id).delete
+            Tag.filter(:user_id => current_user.id, :table_id => row.id).delete
+            User.filter(:id => current_user.id).update(:tables_count => :tables_count - 1)
+          end
+        end
+      # or, should the process of 'creating' the table fail if we can't register the table?
+      rescue => e
+        errors = e.is_a?(CartoDB::ErrorRunningQuery) ? [e.db_message, e.syntax_message] : [e.message]
+        CartoDB::Logger.info "exception automatically dropping cartodb table components", errors
+      end
+    end
     
     # log results of query
     @to_log = params[:sql]          

464268af83d960634c67b9d51bdc9f31d50293f1
Javier Alvarez <xavijam@gmail.com>
2012-06-07 13:39:22 +0200

    compass running properly and useless files removed

--- config/compass.rb
+++ config/compass.rb
@@ -1,2 +1,8 @@
 # Require any additional compass plugins here.
 project_type = :rails
+sass_dir = "scss"
+sass_path = "app/assets"
+#css_dir = "public/css"
+css_path = "public"
+images_dir = "public/img"
+output_style = :compressed

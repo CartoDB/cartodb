6f288bac6f2687f3f9db7e398fd93351ca2ad0b6
javi <qualopec@gmail.com>
2012-06-07 12:23:34 +0200

    some changes

--- public/javascripts/v2/test/spec/view.spec.js
+++ public/javascripts/v2/test/spec/view.spec.js
@@ -41,11 +41,28 @@ describe("core.view", function() {
   });
 
   it("clean should unbind all events", function() {
-      var model = new Backbone.Model();
       view.bind('meh', function(){});
       expect(_.size(view._callbacks)).toEqual(1);
       view.clean();
-      expect(model._callbacks).toEqual(undefined);
+      expect(view._callbacks).toEqual(undefined);
+  });
+
+  it("should unlink linked models", function() {
+      var called = false;
+      var model = new Backbone.Model();
+      model.bind('change', view.test_method, this);
+      model.bind('change', function() { called= true;});
+      view.add_related_model(model);
+      expect(model._callbacks.change.tail).not.toEqual(model._callbacks.change.next);
+
+      model.trigger('change');
+      expect(called).toEqual(true);
+      called = false;
+      view.clean();
+      model.trigger('change');
+      expect(called).toEqual(true);
+      // there is only one event
+      expect(model._callbacks.change.tail).toEqual(model._callbacks.change.next);
   });
 
 });

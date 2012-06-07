24f7eb876cf05b9fab244ca027e49302c369645f
javi <qualopec@gmail.com>
2012-06-07 12:33:03 +0200

    fixes

--- public/javascripts/v2/test/spec/view.spec.js
+++ public/javascripts/v2/test/spec/view.spec.js
@@ -50,19 +50,22 @@ describe("core.view", function() {
   it("should unlink linked models", function() {
       var called = false;
       var model = new Backbone.Model();
-      model.bind('change', view.test_method, this);
+      spyOn(view, 'test_method');
+      model.bind('change', view.test_method, view);
       model.bind('change', function() { called= true;});
       view.add_related_model(model);
-      expect(model._callbacks.change.tail).not.toEqual(model._callbacks.change.next);
 
       model.trigger('change');
       expect(called).toEqual(true);
+      expect(view.test_method).toHaveBeenCalled();
+      expect(view.test_method.callCount).toEqual(1);
       called = false;
       view.clean();
+      expect(_.size(view._models)).toEqual(0);
+      //trigger again
       model.trigger('change');
       expect(called).toEqual(true);
-      // there is only one event
-      expect(model._callbacks.change.tail).toEqual(model._callbacks.change.next);
+      expect(view.test_method.callCount).toEqual(1);
   });
 
 });

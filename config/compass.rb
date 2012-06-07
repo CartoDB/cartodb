e74596487470c3b27c19530d07398806030af40b
Javier Alvarez <xavijam@gmail.com>
2012-06-07 14:00:31 +0200

    compass sprites running

--- config/compass.rb
+++ config/compass.rb
@@ -2,7 +2,9 @@
 project_type = :rails
 sass_dir = "scss"
 sass_path = "app/assets"
-#css_dir = "public/css"
 css_path = "public"
 images_dir = "public/img"
+images_path = "public"
+generated_images_dir = "img"
+generated_images_path = "public"
 output_style = :compressed

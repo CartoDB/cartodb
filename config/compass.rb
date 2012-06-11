521e2425dc2bffcdea300c3d5be0969192336625
Javier Alvarez <xavijam@gmail.com>
2012-06-11 17:04:23 +0200

    some changes in dashboard

--- config/compass.rb
+++ config/compass.rb
@@ -1,10 +1,14 @@
 # Require any additional compass plugins here.
-project_type = :rails
-sass_dir = "scss"
-sass_path = "app/assets"
-css_path = "public"
-images_dir = "public/img"
-images_path = "public"
-generated_images_dir = "img"
+project_type          = :rails
+sass_dir              = "scss"
+sass_path             = "app/assets"
+css_path              = "public"
+images_dir            = "img"
+images_path           = "public"
+generated_images_dir  = "img"
 generated_images_path = "public"
-output_style = :compressed
+fonts_dir             = "fonts"
+fonts_path            = "public"
+http_images_path      = "/img"
+relative_assets       = "false"
+output_style          = :compressed

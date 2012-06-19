6a18944db7fb2d35652e7239a3d4bb84a487233a
javi <qualopec@gmail.com>
2012-06-19 17:09:52 +0200

    table widget

---
+++ public/js/lib/test/spec/ui/common/table.spec.js
@@ -0,0 +1,95 @@
+describe("common.ui.Table", function() {
+
+  var cols; 
+  var tableMetadata;
+  describe("Row", function() {
+    beforeEach(function() {
+    });
+
+    it("", function() {
+
+    });
+  });
+
+  describe("TableData", function() {
+    beforeEach(function() {
+      cols = new cdb.ui.common.TableData();
+      cols.reset([
+        {'id': 1, 'col1': 1, 'col2': 2, 'col3': 3},
+        {'id': 2, 'col1': 4, 'col2': 5, 'col3': 6}
+      ]);
+    });
+    it("should return the value for cell", function() {
+      expect(cols.getCell(0, 'col1')).toEqual(1);
+    });
+
+    it("should return null for non existing cell", function() {
+      expect(cols.getCell(10, 'col1')).toEqual(null);
+    });
+  });
+
+  describe("Table", function() {
+    var table;
+    beforeEach(function() {
+      cols = new cdb.ui.common.TableData();
+
+      tableMetadata = new cdb.ui.common.TableProperties({
+        schema: [
+          ['id', 'number'],
+          ['col1','number'], 
+          ['col2','number'], 
+          ['col3','number']
+        ]
+      });
+
+      table = new cdb.ui.common.Table({
+        dataModel: cols,
+        model: tableMetadata
+      });
+
+      cols.reset([
+        {'id': 1, 'col1': 1, 'col2': 2, 'col3': 3},
+        {'id': 2, 'col1': 4, 'col2': 5, 'col3': 6}
+      ]);
+
+    });
+
+    it("should render a table", function() {
+      expect(table.render().$el.is('table')).toEqual(true);
+    });
+
+    it("should render a header", function() {
+      expect(table.render().$('thead')).toBeTruthy();
+    });
+
+    it("should have 2 rows and the header", function() {
+      expect(table.render().$('tr').length).toEqual(3);
+    });
+
+    it("should have 6 cells and header", function() {
+      expect(table.render().$('td').length).toEqual(3*4);
+    });
+
+    it("each row has an id", function() {
+      expect($(table.render().$('tr')[1]).attr('id')).toEqual('row_' + cols.at(0).id);
+    });
+
+    it("should change value when model changes", function() {
+      table.render();
+      cols.at(0).set('col1', 10);
+      expect(table.$('#cell_1_col1').html()).toEqual(cols.at(0).get('col1').toString());
+    });
+
+    it("should rerender on data reset", function() {
+      table.render();
+      expect(table.render().$('tr').length).toEqual(3);
+      cols.reset([
+        {'id': 1, 'col1': 1, 'col2': 2, 'col3': 3}
+      ]);
+      expect(table.render().$('tr').length).toEqual(2);
+    });
+
+  });
+
+});
+

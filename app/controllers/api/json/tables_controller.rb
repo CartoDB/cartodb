8c3abc54fa8b4a95d4d37a84aaa9302bd43702b3
andrewxhill <andrewxhill@gmail.com>
2012-06-19 14:55:07 -0400

    importer reconfigured to be called from the tables_controller, not the table

--- app/controllers/api/json/tables_controller.rb
+++ app/controllers/api/json/tables_controller.rb
@@ -14,27 +14,27 @@ class Api::Json::TablesController < Api::ApplicationController
       tag_name = params[:tag_name].sanitize.tr('_',' ')
       tables_count = Table.fetch("select count(user_tables.id) as count
                           from user_tables, tags
-                          where user_tables.user_id = ? 
+                          where user_tables.user_id = ?
                             and user_tables.id = tags.table_id
                             and tags.name = ?", current_user.id, tag_name).first[:count]
-      Table.fetch("select user_tables.*, 
+      Table.fetch("select user_tables.*,
                       array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
                           from user_tables, tags
-                          where user_tables.user_id = ? 
+                          where user_tables.user_id = ?
                             and user_tables.id = tags.table_id
                             and tags.name = ?
-                          order by user_tables.id DESC 
-                          limit ? offset ?", current_user.id, tag_name, limit, offset).all      
+                          order by user_tables.id DESC
+                          limit ? offset ?", current_user.id, tag_name, limit, offset).all
     else
       Table.fetch("select *, array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
                           from user_tables
                           where user_tables.user_id = ? order by id DESC
                           limit ? offset ?", current_user.id, limit, offset).all
     end
-    
+
     render_jsonp({ :total_entries => params[:tag_name] ? tables_count : current_user.tables_count,
                     :tables => @tables.map { |table|
-                        { :id => table.id,            
+                        { :id => table.id,
                           :name => table.name,
                           :privacy => table_privacy_text(table),
                           :tags => table[:tags_names],
@@ -44,7 +44,7 @@ class Api::Json::TablesController < Api::ApplicationController
                       }
                     })
   end
-  
+
   def show
     respond_to do |format|
       format.csv do
@@ -75,7 +75,7 @@ class Api::Json::TablesController < Api::ApplicationController
   def update
     @table = Table.filter(:user_id => current_user.id, :name => params[:id]).first
     warnings = []
-    
+
     @table.set_all(params)
     if params.keys.include?("latitude_column") && params.keys.include?("longitude_column")
       latitude_column  = params[:latitude_column]  == "nil" ? nil : params[:latitude_column].try(:to_sym)
@@ -87,9 +87,9 @@ class Api::Json::TablesController < Api::ApplicationController
       @table = Table.fetch("select *, array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
                             from user_tables
                             where id=?",@table.id).first
-                            
+
       # wont allow users to set a table to same name, sends error
-      unless params[:name].nil? 
+      unless params[:name].nil?
         if params[:name].downcase != @table.name
           owner = User.select(:id,:database_name,:crypted_password,:quota_in_bytes,:username, :private_tables_enabled, :table_quota).filter(:id => current_user.id).first
           if params[:name][0].match(/[^0-9]/).nil?
@@ -100,7 +100,7 @@ class Api::Json::TablesController < Api::ApplicationController
           end
         end
       end
-      render_jsonp({ :id => @table.id,                 
+      render_jsonp({ :id => @table.id,
                      :name => @table.name,
                      :warnings => warnings,
                      :privacy => table_privacy_text(@table),
@@ -118,13 +118,13 @@ class Api::Json::TablesController < Api::ApplicationController
     @table.destroy
     head :ok
   end
-  
+
   # expects the infowindow data in the infowindow parameter
   def set_infowindow
     @table.infowindow = params[:infowindow]
     head :ok
   end
-  
+
   def set_map_metadata
     @table.map_metadata = params[:map_metadata]
     head :ok
@@ -134,13 +134,107 @@ class Api::Json::TablesController < Api::ApplicationController
   def get_map_metadata
     render_jsonp({:map_metadata => @table.map_metadata})
   end
-  
+
   protected
+
+  def import_to_cartodb method, import_source
+    if method == 'file'
+      hash_in = ::Rails::Sequel.configuration.environment_for(Rails.env).merge(
+        "database" => current_user.database_name,
+        :logger => ::Rails.logger,
+        "username" => current_user.database_username,
+        "password" => current_user.database_password,
+        :import_from_file => import_source,
+        :debug => (Rails.env.development?),
+        :remaining_quota => current_user.remaining_quota,
+        :data_import_id => @data_import.id
+      ).symbolize_keys
+      importer = CartoDB::Importer.new hash_in
+      importer = importer.import!
+      @data_import.reload
+      @data_import.imported
+      return importer
+    end
+    #import from URL
+    if method == 'url'
+      @data_import.data_type = 'url'
+      @data_import.data_source = import_from_url
+      @data_import.download
+      @data_import.save
+      importer = CartoDB::Importer.new ::Rails::Sequel.configuration.environment_for(Rails.env).merge(
+        "database" => database_name,
+        :logger => ::Rails.logger,
+        "username" => owner.database_username,
+        "password" => owner.database_password,
+        :import_from_url => import_from_url,
+        :debug => (Rails.env.development?),
+        :remaining_quota => owner.remaining_quota,
+        :data_import_id => @data_import.id
+      ).symbolize_keys
+      importer = importer.import!
+      @data_import.reload
+      @data_import.imported
+      @data_import.save
+      return importer.name
+    end
+    #Import from the results of a query
+    if method == 'from_query'
+      @data_import.data_type = 'query'
+      @data_import.data_source = import_from_query
+      @data_import.migrate
+      @data_import.save
+
+      # ensure unique name
+      uniname = get_valid_name(self.name)
+
+      # create a table based on the query
+      owner.in_database.run("CREATE TABLE #{uniname} AS #{self.import_from_query}")
+
+      # with table #{uniname} table created now run migrator to CartoDBify
+      hash_in = ::Rails::Sequel.configuration.environment_for(Rails.env).merge(
+        "database" => database_name,
+        :logger => ::Rails.logger,
+        "username" => owner.database_username,
+        "password" => owner.database_password,
+        :current_name => uniname,
+        :suggested_name => uniname,
+        :debug => (Rails.env.development?),
+        :remaining_quota => owner.remaining_quota,
+        :data_import_id => @data_import.id
+      ).symbolize_keys
+      importer = CartoDB::Migrator.new hash_in
+      importer = importer.migrate!
+      @data_import.reload
+      @data_import.migrated
+      @data_import.save
+      return importer.name
+    end
+
+    #Import from copying another table
+    if method == 'table_copy'
+      @data_import.data_type = 'table'
+      @data_import.data_source = migrate_existing_table
+      @data_import.migrate
+      @data_import.save
+      # ensure unique name
+      uniname = get_valid_name(self.name)
+      owner.in_database.run("CREATE TABLE #{uniname} AS SELECT * FROM #{import_from_table_copy}")
+      @data_import.imported
+      owner.in_database.run("CREATE INDEX ON #{uniname} USING GIST(the_geom)")
+      owner.in_database.run("CREATE INDEX ON #{uniname} USING GIST(#{THE_GEOM_WEBMERCATOR})")
+      owner.in_database.run("UPDATE #{uniname} SET created_at = now()")
+      owner.in_database.run("UPDATE #{uniname} SET updated_at = now()")
+      owner.in_database.run("ALTER TABLE #{uniname} ALTER COLUMN created_at SET DEFAULT now()")
+      set_trigger_the_geom_webmercator
+      @data_import.migrated
+      return uniname
+    end
+  end
 
   def load_table
     @table = Table.find_by_identifier(current_user.id, params[:id])
   end
-  
+
   def record_query_threshold
     if response.ok?
       case action_name
--- lib/importer/lib/cartodb-importer/importer.rb
+++ lib/importer/lib/cartodb-importer/importer.rb
@@ -6,29 +6,29 @@ module CartoDB
     class << self
       attr_accessor :debug
     end
-    
+
     include CartoDB::Import::Util
-    
+
     @@debug = true
     RESERVED_COLUMN_NAMES = %W{ oid tableoid xmin cmin xmax cmax ctid }
     SUPPORTED_FORMATS     = %W{ .csv .shp .ods .xls .xlsx .tif .tiff .kml .kmz .js .json .tar .gz .tgz .osm .bz2 .geojson .gpx }
-    
-    attr_accessor :import_from_file,              
-                  :db_configuration, 
-                  :db_connection, 
+
+    attr_accessor :import_from_file,
+                  :db_configuration,
+                  :db_connection,
                   :append_to_table,
-                  :suggested_name,                      
-                  :ext                   
-    attr_reader   :table_created, 
+                  :suggested_name,
+                  :ext
+    attr_reader   :table_created,
                   :force_name
-    
-    # Initialiser has to get the file in a standard location on the filesystem    
+
+    # Initialiser has to get the file in a standard location on the filesystem
     def initialize(options = {})
       @entries          = [] #will contain all files created on disk
       @table_entries    = [] #will contain all tables attempted to be created on disk
       @python_bin_path  = `which python`.strip
-      @psql_bin_path    = `which psql`.strip   
-      @runlog           = OpenStruct.new :log => [], :stdout => [], :err => []   
+      @psql_bin_path    = `which psql`.strip
+      @runlog           = OpenStruct.new :log => [], :stdout => [], :err => []
       @@debug           = options[:debug]
       @data_import_id   = options[:data_import_id]
       @data_import      = DataImport.find(:id=>@data_import_id)
@@ -36,32 +36,32 @@ module CartoDB
       @append_to_table  = options[:append_to_table] || nil
       @db_configuration = options.slice :database, :username, :password, :host, :port
       @db_configuration = {:port => 5432, :host => '127.0.0.1'}.merge @db_configuration
-      @db_connection = Sequel.connect("postgres://#{@db_configuration[:username]}:#{@db_configuration[:password]}@#{@db_configuration[:host]}:#{@db_configuration[:port]}/#{@db_configuration[:database]}")     
-      @working_data = nil; 
-      
+      @db_connection = Sequel.connect("postgres://#{@db_configuration[:username]}:#{@db_configuration[:password]}@#{@db_configuration[:host]}:#{@db_configuration[:port]}/#{@db_configuration[:database]}")
+      @working_data = nil;
+
       # Setup candidate file
       @import_from_file = options[:import_from_file]
-      if options[:import_from_url]        
+      if options[:import_from_url]
         begin
-          @import_from_file = temporary_filename() + File.basename(options[:import_from_url])                
+          @import_from_file = temporary_filename() + File.basename(options[:import_from_url])
           `wget \"#{options[:import_from_url]}\" -O #{@import_from_file}`
         rescue => e
           log e
           raise e
-        end    
-      end      
+        end
+      end
       raise "import_from_file value can't be nil" unless @import_from_file
-      
-      # Setup suggested name  
+
+      # Setup suggested name
       if options[:suggested_name]
         @force_name     = true
         @suggested_name = get_valid_name(options[:suggested_name])
       else
         @force_name = false
       end
-      
+
       # TODO: Explain THIS!
-      if @import_from_file.is_a?(String) 
+      if @import_from_file.is_a?(String)
         @filesrc = nil
         @fromuri = false
         if @import_from_file =~ /^http/ # Tells us it is a URL
@@ -78,8 +78,8 @@ module CartoDB
               @data_import.file_ready
               file_name = File.basename(@import_from_file)
               @ext = File.extname(file_name).downcase
-              
-              
+
+
               # Fix for extensionless fusiontables files
               if @filesrc == "fusiontables"
                 @ext = ".kml"
@@ -88,10 +88,10 @@ module CartoDB
               elsif @ext==".gz" and @import_from_file.include?(".tar.gz")
                 @ext=".tgz"
               end
-              
+
               @iconv ||= Iconv.new('UTF-8//IGNORE', 'UTF-8')
               @original_name ||= get_valid_name(File.basename(@iconv.iconv(@import_from_file), @ext).downcase.sanitize)
-              
+
               @import_from_file = Tempfile.new([@original_name, @ext])
               @import_from_file.write res.read.force_encoding("UTF-8")
               @import_from_file.close
@@ -104,7 +104,7 @@ module CartoDB
               @data_import.log_error(e)
             end
           end
-        
+
       else
         original_filename = if @import_from_file.respond_to?(:original_filename)
           @import_from_file.original_filename
@@ -114,10 +114,10 @@ module CartoDB
         @ext = File.extname(original_filename)
         @suggested_name ||= get_valid_name(File.basename(original_filename,@ext).tr('.','_').downcase.sanitize)
       end
-      
+
       # finally setup current path
       @path = @import_from_file.respond_to?(:tempfile) ? @import_from_file.tempfile.path : @import_from_file.path
-      
+
       #final ext check in case the file was falsely transfered as .SHP for example
       if ['','.shp','.csv'].include? @ext
         @ext = check_if_archive(@path, @ext)
@@ -132,8 +132,8 @@ module CartoDB
       log e.inspect
       raise e
     end
-        
-    # core import method    
+
+    # core import method
     #
     # Has 3 expansion points
     #
@@ -147,44 +147,44 @@ module CartoDB
         if fs.to_i == 0
           @data_import.set_error_code(1005)
           @data_import.log_error("File contains no information, check it locally" )
-          raise "File contains no information, check it locally" 
+          raise "File contains no information, check it locally"
         elsif @remaining_quota < (0.3*fs)
           disk_quota_overspend = (File.size(@path) - @remaining_quota).to_int
           @data_import.set_error_code(8001)
           @data_import.log_error("#{disk_quota_overspend / 1024}KB more space is required" )
-          raise CartoDB::QuotaExceeded, "#{disk_quota_overspend / 1024}KB more space is required" 
+          raise CartoDB::QuotaExceeded, "#{disk_quota_overspend / 1024}KB more space is required"
         end
-        
+
         errors = Array.new
         suggested = @suggested_name.nil? ? get_valid_name(File.basename(@original_name,@ext).tr('.','_').downcase.sanitize) : @suggested_name
-        
-        
-        # All imports start with only one file, so 'import_data' is an array of 
+
+
+        # All imports start with only one file, so 'import_data' is an array of
         # on length = 1
         import_data = [{
           :ext            => @ext,
           :path           => @path,
           :suggested_name => suggested
         }]
-        
+
         # A record of all file paths for cleanup
         @entries << @path
-        
-        # TODO the problem with this Factory method, is that if a Zip -> KMZ/Zip 
+
+        # TODO the problem with this Factory method, is that if a Zip -> KMZ/Zip
         # it will fail because it wont know to go back and do the Decompressor
         # stage again
-        
+
         # set our multi file handlers
         # decompress data and update self with results
-        decompressor = CartoDB::Import::Decompressor.create(@ext, self.to_import_hash) 
+        decompressor = CartoDB::Import::Decompressor.create(@ext, self.to_import_hash)
         @data_import.log_update('file unzipped') if decompressor
         import_data = decompressor.process! if decompressor
-        
+
         @data_import.reload
-        
+
         # Preprocess data and update self with results
         # preprocessors are expected to return a hash datastructure
-        
+
         processed_imports = Array.new
         import_data.each { |data|
           @entries << data[:path]
@@ -195,7 +195,7 @@ module CartoDB
           if preproc
             begin
               out = preproc.process!
-              out.each{ |d| 
+              out.each{ |d|
                 processed_imports << d
               }
               @data_import.log_update('file preprocessed')
@@ -209,26 +209,26 @@ module CartoDB
             processed_imports << data
           end
         }
-        
+
         # Load data in
         payloads = Array.new
         processed_imports.each { |data|
           @entries << data[:path]
           @working_data = data
           # re-check suggested_name in the case that it has been taken by another in this import
-          @working_data[:suggested_name] = @suggested_name.nil? ? get_valid_name(@working_data[:suggested_name]) : get_valid_name(@suggested_name) 
-          
+          @working_data[:suggested_name] = @suggested_name.nil? ? get_valid_name(@working_data[:suggested_name]) : get_valid_name(@suggested_name)
+
           loader = CartoDB::Import::Loader.create(data[:ext], self.to_import_hash)
-          
+
           if !loader
             @data_import.log_update("no importer for this type of data, #{@ext}")
             @data_import.set_error_code(1002)
-            #raise "no importer for this type of data"  
+            #raise "no importer for this type of data"
           else
             begin
               out = loader.process!
               out.each{ |d| payloads << d }
-              @data_import.log_update("#{data[:ext]} successfully loaded")  
+              @data_import.log_update("#{data[:ext]} successfully loaded")
             rescue
               @data_import.reload
               errors << OpenStruct.new({ :description => @data_import.get_error_text,
@@ -237,13 +237,13 @@ module CartoDB
             end
           end
         }
-        
+
         @data_import.refresh
         @data_import.log_update("file successfully imported")
-        
+
         #remove all files from disk
         cleanup_disk
-        
+
         @data_import.save
         return [payloads, errors]
       rescue => e
@@ -255,9 +255,9 @@ module CartoDB
         log "====================="
         begin  # TODO: Do we really mean nil here? What if a table is created?
           @db_connection.drop_table @suggested_name
-        rescue # silent try to drop the table            
+        rescue # silent try to drop the table
         end
-        
+
         raise e
       ensure
         @db_connection.disconnect
@@ -270,8 +270,8 @@ module CartoDB
         if @data_import
           @data_import.save
         end
-      end        
-    end  
+      end
+    end
     #https://viz2.cartodb.com/tables/1255.shp
   def cleanup_disk
     @entries = @entries.sort_by {|x| x.length}.reverse

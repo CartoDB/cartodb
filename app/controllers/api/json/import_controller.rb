b30dbc4cadeb36bd2a796847917c20dfbab88a44
andrewxhill <andrewxhill@gmail.com>
2012-06-25 12:22:33 -0400

    import_contoller from table_controller;

--- app/controllers/api/json/import_controller.rb
+++ app/controllers/api/json/import_controller.rb
@@ -1,50 +1,11 @@
 # coding: UTF-8
 
-class Api::Json::TablesController < Api::ApplicationController
-  ssl_required :index, :show, :create, :update, :destroy, :set_infowindow, :duplicate, :set_map_metadata, :get_map_metadata
-
-  before_filter :load_table, :except => [:index, :create]
+class Api::Json::ImportController < Api::ApplicationController
+  :create
+  
   before_filter :set_start_time
   after_filter  :record_query_threshold
-
-  def index
-    limit  = params[:per_page].nil? || params[:per_page].to_i > 100 ? 100 : params[:per_page].to_i
-    offset = params[:page].nil? || params[:page].to_i < 0 ? 0 : limit*(params[:page].to_i - 1)
-    @tables = if !params[:tag_name].blank?
-      tag_name = params[:tag_name].sanitize.tr('_',' ')
-      tables_count = Table.fetch("select count(user_tables.id) as count
-                          from user_tables, tags
-                          where user_tables.user_id = ? 
-                            and user_tables.id = tags.table_id
-                            and tags.name = ?", current_user.id, tag_name).first[:count]
-      Table.fetch("select user_tables.*, 
-                      array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
-                          from user_tables, tags
-                          where user_tables.user_id = ? 
-                            and user_tables.id = tags.table_id
-                            and tags.name = ?
-                          order by user_tables.id DESC 
-                          limit ? offset ?", current_user.id, tag_name, limit, offset).all      
-    else
-      Table.fetch("select *, array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
-                          from user_tables
-                          where user_tables.user_id = ? order by id DESC
-                          limit ? offset ?", current_user.id, limit, offset).all
-    end
-    
-    render_jsonp({ :total_entries => params[:tag_name] ? tables_count : current_user.tables_count,
-                    :tables => @tables.map { |table|
-                        { :id => table.id,            
-                          :name => table.name,
-                          :privacy => table_privacy_text(table),
-                          :tags => table[:tags_names],
-                          :schema => table.schema,
-                          :updated_at => table.updated_at,
-                          :rows_counted => table.rows_estimated }
-                      }
-                    })
-  end
-
+  
   def create
     @data_import  = DataImport.new(:user_id => current_user.id)
     @data_import.updated_at = Time.now
@@ -181,96 +142,6 @@ class Api::Json::TablesController < Api::ApplicationController
     render_jsonp({ :description => @data_import.get_error_text, :stack =>  @data_import.log_json, :code => @data_import.error_code }, 400)
   end
 
-  def show
-    respond_to do |format|
-      format.csv do
-        send_data @table.to_csv,
-          :type => 'application/zip; charset=binary; header=present',
-          :disposition => "attachment; filename=#{@table.name}.zip"
-      end
-      format.shp do
-        send_data @table.to_shp,
-          :type => 'application/octet-stream; charset=binary; header=present',
-          :disposition => "attachment; filename=#{@table.name}.zip"
-      end
-      format.kml or format.kmz do
-        send_data @table.to_kml,
-          :type => 'application/vnd.google-earth.kml+xml; charset=binary; header=present',
-          :disposition => "attachment; filename=#{@table.name}.kmz"
-      end
-      format.json do
-        render_jsonp({ :id => @table.id,
-                       :name => @table.name,
-                       :privacy => table_privacy_text(@table),
-                       :tags => @table[:tags_names],
-                       :schema => @table.schema(:reload => true) })
-      end
-    end
-  end
-
-  def update
-    @table = Table.filter(:user_id => current_user.id, :name => params[:id]).first
-    warnings = []
-    
-    @table.set_all(params)
-    if params.keys.include?("latitude_column") && params.keys.include?("longitude_column")
-      latitude_column  = params[:latitude_column]  == "nil" ? nil : params[:latitude_column].try(:to_sym)
-      longitude_column = params[:longitude_column] == "nil" ? nil : params[:longitude_column].try(:to_sym)
-      @table.georeference_from!(:latitude_column => latitude_column, :longitude_column => longitude_column)
-    end
-    @table.tags = params[:tags] if params[:tags]
-    if @table.save
-      @table = Table.fetch("select *, array_to_string(array(select tags.name from tags where tags.table_id = user_tables.id),',') as tags_names
-                            from user_tables
-                            where id=?",@table.id).first
-                            
-      # wont allow users to set a table to same name, sends error
-      unless params[:name].nil? 
-        if params[:name].downcase != @table.name
-          owner = User.select(:id,:database_name,:crypted_password,:quota_in_bytes,:username, :private_tables_enabled, :table_quota).filter(:id => current_user.id).first
-          if params[:name][0].match(/[^0-9]/).nil?
-            warnings << "Table names can't start with a number."
-          elsif owner.tables.filter(:name.like(/^#{params[:name]}/)).select_map(:name).include?(params[:name].downcase)
-          #raise "Table name already exists"
-            warnings << "Table '#{params[:name].downcase}' already existed"
-          end
-        end
-      end
-      render_jsonp({ :id => @table.id,                 
-                     :name => @table.name,
-                     :warnings => warnings,
-                     :privacy => table_privacy_text(@table),
-                     :tags => @table[:tags_names],
-                     :schema => @table.schema })
-    else
-      render_jsonp({ :errors => @table.errors.full_messages}, 400)
-    end
-  rescue => e
-    CartoDB::Logger.info e.class.name, e.message
-    render_jsonp({ :errors => [translate_error(e.message.split("\n").first)] }, 400) and return
-  end
-
-  def destroy
-    @table.destroy
-    head :ok
-  end
-  
-  # expects the infowindow data in the infowindow parameter
-  def set_infowindow
-    @table.infowindow = params[:infowindow]
-    head :ok
-  end
-  
-  def set_map_metadata
-    @table.map_metadata = params[:map_metadata]
-    head :ok
-  end
-
-  #todo: replace with windshaft
-  def get_map_metadata
-    render_jsonp({:map_metadata => @table.map_metadata})
-  end
-  
   protected
 
   def import_to_cartodb method, import_source
@@ -373,12 +244,7 @@ class Api::Json::TablesController < Api::ApplicationController
   
   def record_query_threshold
     if response.ok?
-      case action_name
-        when "create"
-          CartoDB::QueriesThreshold.incr(current_user.id, "other", Time.now - @time_start)
-        when "destroy"
-          CartoDB::QueriesThreshold.incr(current_user.id, "other", Time.now - @time_start)
-      end
+      CartoDB::QueriesThreshold.incr(current_user.id, "other", Time.now - @time_start)
     end
   end
   def fix_openstreetmap_url url

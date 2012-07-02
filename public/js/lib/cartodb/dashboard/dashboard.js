481f68de546a1d81f9b1df4fc5b236f23ff34878
javi <qualopec@gmail.com>
2012-07-02 18:36:46 +0200

    dashboad app wip

--- public/js/lib/cartodb/dashboard/dashboard.js
+++ public/js/lib/cartodb/dashboard/dashboard.js
@@ -6,25 +6,50 @@
 $(function() {
 
     var Dashboard = cdb.core.View.extend({
-        el: document.body
+        el: document.body,
+
+        initialize: function() {
+          this._initModels();
+          this._initViews();
+
+          this.tables.fetch();
+        },
+
+        _initModels: function() {
+          this.tables = new cbd.admin.Tables();
+        },
+
+        _initViews: function() {
+
+          this.settings = new Settings({
+            template_base: $('#settings_template').html(),
+            speed: 300
+          });
+          this.$el.append(settings.render().el);
+
+          setTimeout(function(){
+            settings.open();  
+          },3);
+        }
+    });
+
+    var DashboardRouter = Backbone.Router.extend({
+
+        routes: {
+            '/': 'index'
+        },
+
+        index: function() {
+        }
+
     });
 
     var dashboard = new Dashboard();
+    var router = new DashboardRouter();
 
     // expose to debug
     window.dashboard = dashboard;
 
 
-    var Settings = cdb.ui.common.Settings.extend();
 
-    var settings = new Settings({
-      template_base: $('#settings_template').html(),
-      speed: 300
-    });
-
-    $('body').append(settings.render().el);
-
-    setTimeout(function(){
-      settings.open();  
-    },3
 });

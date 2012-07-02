2ae88dbb58775021565bf2a0781176c26c0f678e
javi <qualopec@gmail.com>
2012-07-02 19:47:24 +0200

    fixed tempaltes

--- public/js/lib/ui/common/settings.js
+++ public/js/lib/ui/common/settings.js
@@ -30,7 +30,7 @@ cdb.ui.common.Settings = cdb.core.View.extend({
   initialize: function() {
     this.closeTimeout = -1;
     _.defaults(this.options, this.default_options);
-    this.template_base = _.template(this.options.template_base || cdb.templates.getTemplate('common/notification') || '');
+    this.template_base = this.options.template_base ? _.template(this.options.template_base) : cdb.templates.getTemplate('common/notification');
     this.$el.hide();
   },
 

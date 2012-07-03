7d587a4aaaee8c24db3464a902c4bec6b24eb55f
Javier Arce <javierarce@gmail.com>
2012-07-03 16:33:28 +0200

    Adds comment to configure the asset pipeline

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-//= require cdb/src/cartodb.js
-//= require cdb/src/core/config.js
-//= require cdb/src/core/log.js
-//= require cdb/src/core/profiler.js
-//= require cdb/src/core/template.js
-//= require cdb/src/core/view.js
-//= require cdb/src/geo/map.js
-//= require cdb/src/geo/ui/zoom.js
-//= require cdb/src/ui/common/dialog.js
-//= require cdb/src/ui/common/notification.js
-//= require cdb/src/ui/common/settings.js
-//= require cdb/src/ui/common/table.js
+/**
+/* Manifest for the rails asset pipeline
+*
+* use: add '//= require cdb' in application.js
+*
+**/
+
+//= require cdb/src/cartodb
+//= require cdb/src/core/config
+//= require cdb/src/core/log
+//= require cdb/src/core/profiler
+//= require cdb/src/core/template
+//= require cdb/src/core/view
+//= require cdb/src/geo/map
+//= require cdb/src/geo/ui/zoom
+//= require cdb/src/ui/common/dialog
+//= require cdb/src/ui/common/notification
+//= require cdb/src/ui/common/settings
+//= require cdb/src/ui/common/table

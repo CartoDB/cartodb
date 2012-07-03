fc92aa2a35bf1e3795f3c6e582dbe5d94c9bd307
Javier Arce <javierarce@gmail.com>
2012-07-03 16:24:32 +0200

    Adds js path for rails 3.2.3

---
+++ index.js
@@ -0,0 +1,12 @@
+//= require cdb/src/cartodb.js
+//= require cdb/src/core/config.js
+//= require cdb/src/core/log.js
+//= require cdb/src/core/profiler.js
+//= require cdb/src/core/template.js
+//= require cdb/src/core/view.js
+//= require cdb/src/geo/map.js
+//= require cdb/src/geo/ui/zoom.js
+//= require cdb/src/ui/common/dialog.js
+//= require cdb/src/ui/common/notification.js
+//= require cdb/src/ui/common/settings.js
+//= require cdb/src/ui/common/table.js

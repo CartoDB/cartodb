847df1fd83ff5be8e7e4f224d91ff5508c9183d4
Javier Alvarez <xavijam@gmail.com>
2012-07-04 12:04:17 +0200

    plugins and css for login done

--- public/js/lib/cartodb/login/error.js
+++ public/js/lib/cartodb/login/error.js
@@ -1,196 +1,123 @@
 /**
- * INPUT PLACEHOLDER FOR THE MASSES
+ * INPUT ERROR FOR THE MASSES
  *
  * Simulates a placeholder for a text input
  *
  * usage example:
  *
- *    var settings = new cdb.ui.common.Settings({
- *        el: "#settings_element",
- *        speed: 300
+ *    var error = new cdb.admin.InputError({
+ *        el: "div.error",
+ *        speedIn: 200,
+ *        speedOut: 200
  *    });
- *    settings.show();
- *    // close it
- *    settings.close();
+ *    // Remove it
+ *    error.clear();
 */
 
 
 cdb.admin.InputError = cdb.core.View.extend({
 
   tagName: 'div',
-  className: 'error_field',
+  className: 'error',
 
-  // events: {
-  //   'click label': 'onLabelClick',
-  //   'keyup input': 'onChange'
-  // },
+  events: {
+    'mouseover a' : '_onMouseOver',
+    'mouseout a'  : '_onMouseOut',
+    'click a'     : '_onClick'
+  },
 
-  // default_options: {
-  //     speed: 300
-  // },
+  default_options: {
+    speedIn: 200,
+    speedOut: 200
+  },
 
-  // initialize: function() {
-  //   this.input = this.$el.find('input');
+  initialize: function() {
+    // Set options
+    _.defaults(this.options, this.default_options);
 
-  //   // Render
-  //   this.render();
-    
-  //   // Check
-  //   this.check();
-  // },
+    // Render
+    this.render();
 
-
-  // render: function() {
-  //   var $el = this.$el;
-
-  //   // Get label text
-  //   var text = this.input.attr("data-label");
-
-  //   if (!text) {
-  //     return false;
-  //   }
-
-  //   // Prepend label
-  //   $el.find("input").before("<label>" + text + "</label>");
-
-  //   return this;
-  // },
+    // Show error
+    this.$el.show();
+  },
 
 
-  // check: function() {
-  //   if (this.input.val() != '') {
-  //     this.$el.find('label').hide();
-  //   }
-  // },
+  render: function() {
+    // Get error text
+    var text = this.$el.text();
+
+    // Create the error tooltip
+    this.$p = $("<p>").text(text).append("<span>").addClass("tail");
+    this.$a = $("<a>").addClass("error").attr("tabindex","-1");
+
+    // Add the tooltip to the view
+    this.$el
+      .html('')
+      .append(this.$a)
+      .append(this.$p);
+
+    return this;
+  },
 
 
-  // onLabelClick: function() {
-  //   this.input.focus();
-  // },
+  clear: function() {
+    // Remove error and bindings
+    this.$a.unbind("click")
+    this.$a.unbind("mouseover")
+    this.$a.unbind("mouseout")
+
+    this.remove();
+  },
 
 
-  // onChange: function(ev) {
-  //   var value = $(ev.target).val()
-  //     , $label = this.$el.find('label');
-
-  //   if (value.length>0) {
-  //     $label.fadeOut(10);
-  //   } else {
-  //     $label.fadeIn(300);
-  //   }
-  // }
+  _onClick: function(ev) {
+    ev.preventDefault();
+  },
 
 
+  _onMouseOver: function(ev) {
+    var height_ = this.$p.outerHeight()
+      , options = this.options;
+
+    // Show it
+    this.$p.css({
+      opacity:0,
+      display:'inline-block',
+      top: '-' + (height_ + 10 ) + 'px'
+    }).show();
+
+    if ($.browser.msie && $.browser.version<9) {
+      this.$p.css({
+        top: '-=5px',
+        opacity: 1,
+        display: 'block'
+      });
+    } else {
+      this.$p.animate({
+        top: '-=5px',
+        opacity: 1
+      },options.speedIn);
+    }
+  },
+
+  _onMouseOut: function(ev) {
+    var options = this.options;
+
+    // Hide it
+    if ($.browser.msie && $.browser.version<9) {
+      this.$p.css({
+        top: '+=5px',
+        opacity: 0,
+        display: 'none'
+      });
+    } else {
+      this.$p.animate({
+        top: '+=5px',
+        opacity: 0
+      },options.speedOut,function(ev){
+        $(this).hide();
+      });
+    }
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Core = {
-//   pluginName : name,
-//   options : null,
-
-//   _init : function (options) {
-//     // take user options in consideration
-//     Core.options = $.extend( true, defaultOptions, options );
-//     return this.each( function () {
-//       var $el = $(this);
-
-//       // Append necessary html
-//       Core._addCustom($el);
-
-//       // Check domain
-//       Core._checkDomain($el);
-
-//       // Bind events
-//       Core._bind($el);
-//     });
-//   },
-
-
-//   _bind: function($el) {
-//     $el.find('a').bind({'click':Core._stopPropagation,'mouseover':Core._onMouseover,'mouseout':Core._onMouseout})
-//   },
-
-
-//   _trigger : function ( eventName, data, $el ) {
-//     var isGlobal = $.inArray( eventName, Core.options.globalEvents ) >= 0, eventName = eventName + '.' +  Core.pluginName;
-
-//     if (!isGlobal) {
-//       $el.trigger( eventName, data );
-//     } else {
-//       $.event.trigger( eventName, data );
-//     }
-//   },
-
-
-//   // PRIVATE LOGIC
-//   _stopPropagation: function(ev) {
-//     ev.stopPropagation();
-//     ev.preventDefault();
-//   },
-
-//   _addCustom: function($el) {
-//     var text = $el.text();
-//     $el.html('<a class="error" tabindex="-1"></a>' + '<p>' + text + '<span class="tail"></span></p>');
-//     $el.show();
-//   },
-
-//   _checkDomain: function($el) {
-//     /*
-//       Check if the field is the domain type and
-//       move the .cartodb.com a little more :)
-//     */
-//     $el.parent().find('span.domain').css('right','42px');
-//   },
-
-//   _onMouseover: function(ev) {
-//     var $p = $(ev.target).parent().find('p')
-//       , height_ = $p.outerHeight();
-
-//     $p.css({opacity:0,display:'inline-block',top: '-' + (height_ + 10 ) + 'px'}).show();
-
-//     if ($.browser.msie && $.browser.version<9) {
-//       $p.css({
-//         top: '-=5px',
-//         opacity: 1,
-//         display: 'block'
-//       });
-//     } else {
-//       $p.animate({
-//         top: '-=5px',
-//         opacity: 1
-//       },200);
-//     }
-//   },
-
-//   _onMouseout: function(ev) {
-//     var $p = $(ev.target).parent().find('p');
-
-//     if ($.browser.msie && $.browser.version<9) {
-//       $p.css({
-//         top: '+=5px',
-//         opacity: 0,
-//         display: 'none'
-//       });
-//     } else {
-//       $p.animate({
-//         top: '+=5px',
-//         opacity: 0
-//       },200,function(ev){
-//         $(this).hide();
-//       });
-//     }
-
-//   }
-// };

8533a8e0fe245595e544f92ba2e97c2aff538274
javi <qualopec@gmail.com>
2012-07-05 11:44:58 +0200

    table view wii

--- public/js/lib/cartodb/table/tableview.js
+++ public/js/lib/cartodb/table/tableview.js
@@ -3,12 +3,39 @@
  * view used to render each row
  */
 cdb.admin.RowView = cdb.ui.common.RowView.extend({
+
+  initialize: function() {
+     this.constructor.__super__.initialize.apply(this);
+     this.options.row_header = true;
+  },
   /**
    * return each cell view
    */
   valueView: function(colName, value) {
     return $('<div>').append(value);
   }
+});
+
+
+/**
+ * header cell view, manages operations on table columns
+ */
+
+cdb.admin.HeaderView = cdb.core.View.extend({
+
+  initialize: function() {
+    this.column = this.options.column;
+    this.template = this.getTemplate('table/views/table_header_view');
+  },
+
+  render: function() {
+    this.$el.append(this.template({
+      col_name: this.column[0],
+      col_type: this.column[1]
+    }));
+    return this;
+  }
+
 });
 
 /**
@@ -18,12 +45,17 @@ cdb.admin.TableView = cdb.ui.common.Table.extend({
 
   rowView: cdb.admin.RowView,
 
+  initialize: function() {
+     this.constructor.__super__.initialize.apply(this);
+     this.options.row_header = true;
+  },
+
   headerView: function(column) {
-    var l = $('<label>')
-      .addClass('strong')
-      .addClass('small')
-      .html(column[0]);
-    return $('<div>').append(l);
+    if(column[1] !== 'header') {
+      var v = new cdb.admin.HeaderView({ column: column });
+      return v.render().el;
+    }
+    return '';
   }
 });
 

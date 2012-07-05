9145dfbf0160f5de04b2f76cadabb47243a0a7cb
javi <qualopec@gmail.com>
2012-07-05 13:10:41 +0200

    added table view

--- public/js/lib/cartodb/table/table.js
+++ public/js/lib/cartodb/table/table.js
@@ -13,8 +13,15 @@ $(function() {
           this._initModels();
           this._initViews();
 
+          // init data
           this.table.fetch();
           this.columns.fetch();
+          var URL = 'http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png';
+          this.map.setCenter([34.30714385628804, 11.6015625]);
+          this.map.setZoom(2);
+          this.map.addLayer(new cdb.geo.TileLayer({
+            urlTemplate: URL
+          }));
         },
 
         _initModels: function() {
@@ -22,34 +29,59 @@ $(function() {
             name: table_name
           });
           this.columns = this.table.data();
+          this.map = new cdb.geo.Map();
         },
 
         _initViews: function() {
-          this.tableView = new cdb.admin.TableView({
-            dataModel: this.columns,
+
+          this.workView = new cdb.ui.common.TabPane({
+            el: this.$('.panes')
+          });
+
+          this.tableTab = new cdb.admin.TableTab({
             model: this.table
           });
 
-          this.$('.table').append(this.tableView.render().el);
+          this.mapTab = new cdb.admin.MapTab({
+            model: this.map
+          });
+
+
+          this.workView.addTab('table', this.tableTab.render());
+          this.workView.addTab('map', this.mapTab.render());
+          this.mapTab.enableMap();
+
         }
     });
 
     var TableRouter = Backbone.Router.extend({
 
+        initialize: function(table) {
+          this.table = table;
+        },
+
         routes: {
-            '/': 'index'
+            '': 'index',
+            'table': 'index',
+            'map': 'map'
         },
 
         index: function() {
+          this.table.workView.active('table');
+        },
+
+        map: function() {
+          this.table.workView.active('map');
         }
 
     });
 
     cdb.init(function() {
       var table = new Table();
-      var router = new TableRouter();
+      var router = new TableRouter(table);
       // expose to debug
       window.table = table;
+      Backbone.history.start();
     });
 
 });

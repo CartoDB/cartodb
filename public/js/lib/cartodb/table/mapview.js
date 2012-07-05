5b17f932f75d054d5ef3cf34dbf8bb4c4d91be55
javi <qualopec@gmail.com>
2012-07-05 14:54:24 +0200

    map table switch

--- public/js/lib/cartodb/table/mapview.js
+++ public/js/lib/cartodb/table/mapview.js
@@ -5,20 +5,24 @@ cdb.admin.MapTab = cdb.core.View.extend({
 
   initialize: function() {
     this.map = this.model;
+    this.map_enabled = false;
   },
 
   enableMap: function() {
-    var div = $('<div>');
-    div.css({'height': '100%'});
-    this.$el.append(div);
-    this.mapView = new cdb.geo.LeafletMapView({
-      el: div,
-      map: this.map
-    });
+    if(!this.map_enabled) {
+        var div = $('<div>');
+        div.css({'height': '100%'});
+        this.$el.append(div);
+        this.mapView = new cdb.geo.LeafletMapView({
+          el: div,
+          map: this.map
+        });
+        this.map_enabled = true;
+    }
   },
 
   render: function() {
-    this.$el.css({'height': '700px'});
+    this.$el.css({'height': '900px'});
     return this;
   }
 

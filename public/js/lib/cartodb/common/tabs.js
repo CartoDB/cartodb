28174eeba478dc1cbde966d448e26d303c7c881e
javi <qualopec@gmail.com>
2012-07-05 15:06:35 +0200

    added tab enabler

---
+++ public/js/lib/cartodb/common/tabs.js
@@ -0,0 +1,12 @@
+
+cdb.admin.Tabs = cdb.core.View.extend({
+
+    initialize: function() {
+      _.bindAll(this, 'activate');
+    },
+
+    activate: function(name) {
+      this.$('a').removeClass('selected');
+      this.$('a[href$="#' + name + '"]').addClass('selected');
+    }
+});

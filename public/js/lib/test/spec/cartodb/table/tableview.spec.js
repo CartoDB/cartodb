063545f02498e0ff454a592ec85b5ae903155c79
javi <qualopec@gmail.com>
2012-07-05 13:10:41 +0200

    added table view

--- public/js/lib/test/spec/cartodb/table/tableview.spec.js
+++ public/js/lib/test/spec/cartodb/table/tableview.spec.js
@@ -7,7 +7,8 @@ describe("tableview", function() {
     var view;
     beforeEach(function() {
       view = new cdb.admin.HeaderView({
-        column: ['name', 'type']
+        column: ['name', 'type'],
+        template: "<label><a><%= col_name %></a></label><p><a><%= col_type %></a></p>"
       });
     });
 
@@ -18,7 +19,47 @@ describe("tableview", function() {
     });
   });
 
-  it('ok', function() {
+  describe('TableView', function() {
+    var tview;
+    var model;
+    beforeEach(function() {
+      model = new cdb.admin.CartoDBTableMetadata({
+        name: 'test'
+      });
+
+      tview = new cdb.admin.TableView({
+        dataModel: model.data(),
+        model: model
+      });
+    });
+
+    it('should a table', function() {
+      model.set({
+        columns: [['test', 'integer']]
+      });
+      tview.render();
+      expect(tview.$('thead').length).toEqual(1);
+    });
+
+  });
+
+  describe('TableTab', function() {
+    var tview;
+    var model;
+    beforeEach(function() {
+      model = new cdb.admin.CartoDBTableMetadata({
+        name: 'test'
+      });
+
+      tview = new cdb.admin.TableTab({
+        model: model
+      });
+    });
+
+    it('should render a div', function() {
+      tview.render();
+      expect(tview.$el.hasClass('table')).toBeTruthy();
+    });
 
   });
 });

82079474396511849aaf03789ae3e242f1818f55
Javier Alvarez <xavijam@gmail.com>
2012-07-09 15:34:46 +0200

    modal style

--- public/js/lib/cartodb/dashboard/dashboard.js
+++ public/js/lib/cartodb/dashboard/dashboard.js
@@ -26,15 +26,15 @@ $(function() {
           });
 
           // User settings
-          var settings = this.settings = new cdb.ui.common.Dropdown({
-            target: 'a.account',
-            model: {username: username},
-            template_base: "dashboard/views/settings_item",
-            onClick: function() {
-              console.log("how does it look?");
-            }
-          });
-          this.$el.append(this.settings.render().el);
+          // var settings = this.settings = new cdb.ui.common.Dropdown({
+          //   target: 'a.account',
+          //   model: {username: username},
+          //   template_base: "dashboard/views/settings_item",
+          //   onClick: function() {
+          //     console.log("how does it look?");
+          //   }
+          // });
+          // this.$el.append(this.settings.render().el);
         }
     });
 

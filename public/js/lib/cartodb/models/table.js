2730412dafc7fe625814fb19f785cc432a044478
javi <qualopec@gmail.com>
2012-07-09 21:47:34 +0200

    column models

--- public/js/lib/cartodb/models/table.js
+++ public/js/lib/cartodb/models/table.js
@@ -11,9 +11,30 @@
   cdb.admin.User = Backbone.Model.extend({});
 
 
+  cdb.admin.Column = Backbone.Model.extend({
+
+    url: function() {
+      return '/api/v1/tables/' + this.table.get('name') + '/' + this.get('name');
+    },
+
+    initialize: function() {
+      this.table = this.get('table');
+      if(!this.table) {
+        throw new Exception("you should specify a table model");
+      }
+      this.unset('table', { silent: true });
+    },
+
+  });
   /**
    * contains table meta information like name
    * rows and so on
+   * table api:
+   *
+   * delete a column: DELETE /api/v1/tables/_2/columns/color
+   * rename column: PUT /api/v1/tables/_2/columns/name
+   *    params: new_name:name2
+   *    index:3
    */
   cdb.admin.Table = cdb.ui.common.TableProperties.extend({
     url: function() {
@@ -22,6 +43,18 @@
       return '/api/v1/tables';
     },
     //idAttribute: 'name'
+    deleteColumn: function(columnName) {
+      var c = new cdb.admin.Column({
+        table: this,
+      })
+      c.delete();
+    },
+
+    renameColumn: function(columnName, newName) {
+    },
+
+    changeColumnType: function(columnName, newType) {
+    }
   });
 
   /**

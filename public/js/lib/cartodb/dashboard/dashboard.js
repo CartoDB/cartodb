7ddef328911b578fcd014ca90173437435985831
saleiva <saleiva@gmail.com>
2012-07-10 16:26:35 +0200

    finishing dropdown.js

--- public/js/lib/cartodb/dashboard/dashboard.js
+++ public/js/lib/cartodb/dashboard/dashboard.js
@@ -45,7 +45,7 @@ $(function() {
           })
           .on("optionClicked",function(ev){})
 
-          cdb.god.bind("closeDialogs", settings.handle_click, settings);
+          cdb.god.bind("closeDialogs", settings.hide, settings);
 
           this.$el.append(this.settings.render().el);
         },

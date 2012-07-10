c747a46ee077d2dacf1688cd4477085e67c539f2
javi <qualopec@gmail.com>
2012-07-10 14:23:17 +0200

    table models fixed

--- public/js/lib/cartodb/models/table.js
+++ public/js/lib/cartodb/models/table.js
@@ -13,8 +13,10 @@
 
   cdb.admin.Column = Backbone.Model.extend({
 
+    idAttribute: 'name',
+
     url: function() {
-      return '/api/v1/tables/' + this.table.get('name') + '/' + this.get('name');
+      return '/api/v1/tables/' + this.table.get('name') + '/columns/' + this.get('name');
     },
 
     initialize: function() {
@@ -23,77 +25,74 @@
         throw new Exception("you should specify a table model");
       }
       this.unset('table', { silent: true });
-    },
+    }
 
   });
+
+
+
   /**
-   * contains table meta information like name
-   * rows and so on
-   * table api:
-   *
-   * delete a column: DELETE /api/v1/tables/_2/columns/color
-   * rename column: PUT /api/v1/tables/_2/columns/name
-   *    params: new_name:name2
-   *    index:3
+   * contains information about the table, not the data itself
    */
-  cdb.admin.Table = cdb.ui.common.TableProperties.extend({
-    url: function() {
-      //TODO: use current host
-      //var name = this.options.user.get('name');
-      return '/api/v1/tables';
+  cdb.admin.CartoDBTableMetadata = cdb.ui.common.TableProperties.extend({
+
+    initialize: function() {
+      this.bind('change:schema', this._prepareSchema, this);
+      this._prepareSchema();
     },
-    //idAttribute: 'name'
-    deleteColumn: function(columnName) {
+
+    urlRoot: function() {
+      return '/api/v1/tables/';
+    },
+
+    _prepareSchema: function() {
+      var self = this;
+      this._columnType = {};
+      _(this.get('schema')).each(function(s) {
+        self._columnType[s[0]] = s[1];
+      });
+    },
+
+    _getColumn: function(columnName) {
+      if(this._columnType[columnName] === undefined) {
+        throw new Exception("the column does not exists");
+      }
       var c = new cdb.admin.Column({
         table: this,
-      })
-      c.delete();
+        name: columnName,
+        type: this._columnType[columnName]
+      });
+      return c;
+    },
+
+    deleteColumn: function(columnName) {
+      var c = this._getColumn(columnName);
+      c.destroy();
     },
 
     renameColumn: function(columnName, newName) {
+      var c = this._getColumn(columnName);
+      c.set({
+        new_name: newName,
+        old_name: c.get('name')
+      });
+      c.save();
     },
 
     changeColumnType: function(columnName, newType) {
-    }
-  });
-
-  /**
-   * tables available for given user
-   * usage:
-   * var tables = new cbd.admin.Tables()
-   * tables.fetch();
-   */
-  cdb.admin.Tables = Backbone.Collection.extend({
-
-    model: cdb.admin.Table,
-
-    url: function() {
-      //TODO: use current host
-      //var name = this.options.user.get('name');
-      return '/api/v1/tables';
+      var c = this._getColumn(columnName);
+      c.set({ type: newType});
+      c.save();
     },
 
-    parse: function(response) {
-      return response.tables;
-    },
-
-    initialize: function() {
-    }
-  });
-  
-  cdb.admin.CartoDBTableMetadata = cdb.ui.common.TableProperties.extend({
-      url: function() {
-        return '/api/v1/tables/' + this.get('name');
-      },
-
-      data: function() {
-        if(this._data === undefined) {
-          this._data = new cdb.admin.CartoDBTableData(null, {
-            name: this.get('name')
-          });
-        }
-        return this._data;
+    data: function() {
+      if(this._data === undefined) {
+        this._data = new cdb.admin.CartoDBTableData(null, {
+          name: this.get('name')
+        });
       }
+      return this._data;
+    }
   });
 
   cdb.admin.CartoDBTableData = cdb.ui.common.TableData.extend({
@@ -111,6 +110,34 @@
       return '/api/v1/tables/' + this.table + '/records';
     }
 
+  });
+
+  /**
+   * tables available for given user
+   * usage:
+   * var tables = new cbd.admin.Tables()
+   * tables.fetch();
+   */
+  cdb.admin.Tables = Backbone.Collection.extend({
+
+    model: cdb.admin.CartoDBTableMetadata,
+
+    url: function() {
+      //TODO: use current host
+      //var name = this.options.user.get('name');
+      return '/api/v1/tables';
+    },
+
+    parse: function(response) {
+      return response.tables;
+    },
+
+    initialize: function() {
+    },
+
+    create: function(m) {
+      Backbone.Collection.prototype.create.call(this, m, {wait: true});
+    }
   });
 
 })();

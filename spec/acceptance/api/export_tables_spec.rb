6571402f8c0086c93102d166d1513178bf7c6871
David Arango <arango@gmail.com>
2012-07-11 14:28:07 +0200

    Finally fixing the export_tables spec, we should have more coverage here btw

--- spec/acceptance/api/export_tables_spec.rb
+++ spec/acceptance/api/export_tables_spec.rb
@@ -6,11 +6,11 @@ feature "API 1.0 tables export" do
 
   background do
     Capybara.current_driver = :rack_test
-    @user = create_user
+    @user = create_user({:username => 'test'})
 
     @data_import = DataImport.create(:queue_id => '',
       :user_id => @user.id,
-      :data_source => "/../spec/support/data/elecciones2008.csv",
+      :data_source => "/../spec/support/data/clubbing.csv",
       :updated_at => Time.now)
     @table = Table[@data_import.table_id]
   end
@@ -19,22 +19,26 @@ feature "API 1.0 tables export" do
     get "#{api_table_export_to_csv_url(@table.name)}" do |response|
       response.status.should == 200 
     end
-    
-    csv = CSV.parse(response.body)
-    debugger
-    csv.first.should == ['id','name of species','kingdom','family','lat','lon','views']
-  end
-
-  scenario "Get table in SHP format" do
-    get "#{api_table_export_to_shp_url(@table.name)}"
-    response.status.should == 200
-    
-    path = "/tmp/temp_shp.zip"
+    path = "/tmp/temp_csv.zip"
     fd = File.open(path,'w+')
     fd.write(response.body)
     fd.close
     Zip::ZipFile.foreach(path) do |entry|
-      %W{ import_csv_1_export.shx import_csv_1_export.shp import_csv_1_export.dbf import_csv_1_export.prj }.should include(entry.name)
+      entry.name.should == "clubbing_export.csv"
+    end
+    FileUtils.rm_rf(path)
+  end
+
+  scenario "Get table in SHP format" do
+    get "#{api_table_export_to_shp_url(@table.name)}" do |response|
+      response.status.should == 200
+    end
+    path = "/tmp/temp_csv.zip"
+    fd = File.open(path,'w+')
+    fd.write(response.body)
+    fd.close
+    Zip::ZipFile.foreach(path) do |entry|
+      ['dbf', 'shp', 'shx'].map { |ext| "clubbing_export.#{ext}" }.should include(entry.to_s)
     end
     FileUtils.rm_rf(path)
   end

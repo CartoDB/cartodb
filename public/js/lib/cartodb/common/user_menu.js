b66f1e452ce85e4d9406e6f3436e4c3e948e5e25
saleiva <saleiva@gmail.com>
2012-07-11 19:24:04 +0200

    changing closing effect to user_menu view

--- public/js/lib/cartodb/common/user_menu.js
+++ public/js/lib/cartodb/common/user_menu.js
@@ -36,7 +36,7 @@ cdb.admin.UserMenu = cdb.ui.common.Dropdown.extend({
     this.isOpen = false;
 
     this.$el.animate({
-      margin: "-10px 0 0 0",
+      margin: "10px 0 0 0",
       opacity: 0
     },this.options.speedOut, function(){
       // Remove selected class

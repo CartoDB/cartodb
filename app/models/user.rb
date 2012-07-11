11179c3523d2f4019332a307d2bf0c3f7faa834f
Simon Tokumine <si@tinypla.net>
2012-07-11 12:37:46 +0200

    make sql functions load on user create. These were not being loaded before as the paths were being joined incorrectly

--- app/models/user.rb
+++ app/models/user.rb
@@ -376,8 +376,8 @@ class User < Sequel::Model
     puts "Loading functions in db '#{database_name}' (#{username})"
     in_database(:as => :superuser) do |user_database|
       user_database.transaction do
-        glob = Rails.root.join('/lib/sql/*.sql')
 
+        glob = Rails.root.join('lib/sql/*.sql')
         Dir.glob(glob).each do |f|
           @sql = File.new(f).read
           user_database.run(@sql)

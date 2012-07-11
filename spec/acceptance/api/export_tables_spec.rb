0a0b9609a0b8af7858ee15218a77035e936977bc
David Arango <arango@gmail.com>
2012-07-11 11:52:25 +0200

    WIP fixing export_tables spec

--- spec/acceptance/api/export_tables_spec.rb
+++ spec/acceptance/api/export_tables_spec.rb
@@ -7,29 +7,22 @@ feature "API 1.0 tables export" do
   background do
     Capybara.current_driver = :rack_test
     @user = create_user
-    login_as @user
-    
-    @table = new_table
-    @table.import_from_file = "#{Rails.root}/db/fake_data/import_csv_1.csv"
-    @table.user_id = @user.id
-    @table.name = "antantaric_species"
-    @table.save
-    # FIXME: At some point georeferencing an imported table won't be necessary here
-    @table.georeference_from!(:latitude_column => "lat", :longitude_column => "lon")
+
+    @data_import = DataImport.create(:queue_id => '',
+      :user_id => @user.id,
+      :data_source => "/../spec/support/data/elecciones2008.csv",
+      :updated_at => Time.now)
+    @table = Table[@data_import.table_id]
   end
 
   scenario "Get table in CSV format" do
-    get "#{api_table_export_to_csv_url(@table.name)}"
-    response.status.should == 200
-    
-    path = "/tmp/temp_csv.zip"
-    fd = File.open(path,'w+')
-    fd.write(response.body)
-    fd.close
-    Zip::ZipFile.foreach(path) do |entry|
-      entry.name.should == "import_csv_1_export.csv"
+    get "#{api_table_export_to_csv_url(@table.name)}" do |response|
+      response.status.should == 200 
     end
-    FileUtils.rm_rf(path)
+    
+    csv = CSV.parse(response.body)
+    debugger
+    csv.first.should == ['id','name of species','kingdom','family','lat','lon','views']
   end
 
   scenario "Get table in SHP format" do

321010436c953edcf0246bd7cf106ba51f55906c
javi <qualopec@gmail.com>
2012-07-11 20:05:05 +0200

    importer processing checking

--- public/js/lib/cartodb/models/file.js
+++ public/js/lib/cartodb/models/file.js
@@ -9,8 +9,34 @@ cdb.admin.Files = Backbone.Collection.extend({
 });
 
 cdb.admin.Import = Backbone.Model.extend({
+
     idAttribute: 'item_queue_id',
-    urlRoot: '/api/v1/imports'
+    urlRoot: '/api/v1/imports',
+
+    initialize: function() {
+      this.bind('change', this._checkFinish, this);
+    },
+
+    /**
+     * checks for poll to finish
+     */
+    pollCheck: function(i) {
+      var self = this;
+      var tries = 0;
+      this.pollTimer = setInterval(function() {
+        cdb.log.debug("checking job for finish: " + tries);
+        self.fetch();
+        ++tries;
+      }, i || 1500);
+    },
+
+    _checkFinish: function() {
+      if(this.get('state') === 'complete') {
+        cdb.log.debug("job finished");
+        clearInterval(this.pollTimer);
+        this.trigger('importComplete');
+      }
+    }
 });
 
 cdb.admin.Imports = Backbone.Model.extend({

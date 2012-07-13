9abbb540c31f7ba16aec16d0ecb2335487e95f9d
Simon Tokumine <si@tinypla.net>
2012-07-13 14:42:01 +0200

    add user json path. /cc @javisantana, @xavijam

---
+++ app/controllers/api/json/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::Json::UsersController < Api::ApplicationController
+
+  if Rails.env.production?
+    ssl_required :show
+  end
+
+  def show
+  	user = User[params[:id]]
+    
+    data = {
+    	:username => user.username,
+    	:account_type => user.account_type,
+    	:private_tables => user.private_tables_enabled,
+    	:table_quota => user.table_quota,
+    	:table_count => user.table_count,
+    	:byte_quota => user.quota_in_bytes,
+    	:remaining_table_quota => user.remaining_table_quota,
+    	:remaining_byte_quota => user.remaining_quota.to_f,
+    	:api_calls => (1..30).map{|i|i}
+    }
+
+    render :json => data
+  end
+end

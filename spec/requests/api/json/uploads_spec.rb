aaee9023381dea95a704f2ff688dfebff1d3d639
Fernando Espinosa <fer@ferdev.com>
2012-07-13 14:33:55 +0200

    Moves requests specs to a more proper folder

--- spec/requests/api/json/uploads_spec.rb
+++ spec/requests/api/json/uploads_spec.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+#encoding: UTF-8
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe "Imports API uploads" do
 
@@ -9,7 +10,7 @@ describe "Imports API uploads" do
   end
 
   it 'allow to upload using the request body' do
-    file = File.open(Rails.root.join('db/fake_data/clubbing.csv'), 'r+')
+    file = File.open(Rails.root.join('db/fake_data/estaciones.csv'), 'r+')
 
     post v1_uploads_url(:host => 'test.localhost.lan'), {:body => file.read, :qqfile => File.basename(file.path), :api_key => @user.get_map_key}
 
@@ -17,13 +18,13 @@ describe "Imports API uploads" do
 
     response_json = JSON.parse(response.body)
     response_json.should_not be_nil
-    response_json['file_uri'].should match(/uploads\/.*\/clubbing.csv/)
+    response_json['file_uri'].should match(/uploads\/.*\/estaciones.csv/)
     response_json['success'].should be_true
   end
 
   it 'allow to upload using a file param' do
 
-    file = Rack::Test::UploadedFile.new(Rails.root.join('db/fake_data/clubbing.csv'), 'text/plain')
+    file = Rack::Test::UploadedFile.new(Rails.root.join('db/fake_data/estaciones.csv'), 'text/plain')
 
     post v1_uploads_url(:host => 'test.localhost.lan'), {:file => file, :api_key => @user.get_map_key}
 
@@ -31,7 +32,7 @@ describe "Imports API uploads" do
 
     response_json = JSON.parse(response.body)
     response_json.should_not be_nil
-    response_json['file_uri'].should match(/uploads\/.*\/clubbing.csv/)
+    response_json['file_uri'].should match(/uploads\/.*\/estaciones.csv/)
     response_json['success'].should be_true
   end
 

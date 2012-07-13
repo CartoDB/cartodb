058c2ca347f9b54a0f586470defba7e59e2dc11a
Fernando Espinosa <fer@ferdev.com>
2012-07-13 17:29:22 +0200

    wip

---
+++ spec/requests/admin/tables_controller_spec.rb
@@ -0,0 +1,18 @@
+#encoding: UTF-8
+
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
+
+describe "Tables controller" do
+
+  it 'should allow to append data to an existing table'
+
+  it 'should allow to duplicate an existing table' do
+  end
+
+  it 'should allow to duplicate an existing table'
+
+  it 'should allow to create a table from a query'
+
+  it 'should allow to copy data from another table'
+
+end

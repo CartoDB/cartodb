5aa3593708668c8666d183ae24fca0f988de4472
javi <qualopec@gmail.com>
2012-07-16 12:56:09 +0200

    showing sql errors

--- public/js/lib/cartodb/table/menu_modules/sql.js
+++ public/js/lib/cartodb/table/menu_modules/sql.js
@@ -25,13 +25,24 @@ cdb.admin.mod.SQL = cdb.core.View.extend({
       return this;
     },
 
+    _parseError: function(err) {
+      this.$('.error').html(err.errors.join('<br/>'));
+    },
+
+    _clearErrors: function() {
+      this.$('.error').html('');
+    },
+
     applyQuery: function() {
+      var self = this;
+      this._clearErrors();
       var sql = this.$('textarea').val();
       this.sqlView.setSQL(sql);
       this.model.useSQLView(this.sqlView);
       this.sqlView.fetch({
-        error: function(e) {
-          console.log(e);
+        error: function(e, resp) {
+          var errors = JSON.parse(resp.responseText);
+          self._parseError(errors);
         }
       });
       //this.trigger('sqlQuery', sql);

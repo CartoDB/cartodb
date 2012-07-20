8b413ee0013ce14f7ec45087635aa67ed32be9de
javi <qualopec@gmail.com>
2012-07-20 18:25:07 +0200

    profiler UI wip

--- src/core/profiler.js
+++ src/core/profiler.js
@@ -49,4 +49,61 @@
   };
 
   cdb.core.Profiler = Profiler;
+
+  Profiler.ui = function() {
+    var _$ied;
+    if(!_$ied){
+        _$ied = $('<div></div>').css({
+          'position': 'fixed',
+          'bottom': 10,
+          'left': 10,
+          'zIndex': 20000,
+          'width': $('body').width() - 80,
+          'border': '1px solid #000',
+          'padding': '30px',
+          'backgroundColor': '#fff',
+          'fontFamily': 'arial,helvetica,sans-serif',
+          'fontSize': '14px',
+          'lineHeight': '1.3em'
+        });
+        $('body').append(_$ied);
+    }
+    this.el = _$ied;
+    var update = function() {
+        for(k in Profiler.times) {
+          var pid = '_prof_time_' + k;
+          var p = $('#' + pid);
+          if(p.length === 0) {
+            p = $('<div>').attr('id', pid)
+            var t = Profiler.times[k];
+            var div = $('<div>').append('<h1>' + k + '</h1>').css({
+              'font-weight': 'bold',
+              'margin': '10px 0'
+            })
+            for(var c in t) {
+              p.append(div.append($('<div>').append('<span style="display: inline-block; width: 60px;font-weight:  300;">' + c + '</span><span class="'+  c + '"></span>')));
+            }
+            _$ied.append(p);
+          }
+          // update ir
+          var t = Profiler.times[k];
+          for(var c in t) {
+            p.find('.' + c).html(t[c]);
+          }
+        }
+    }
+    setInterval(function() {
+      update();
+    }, 1000);
+    /*var $message = $('<li>'+message+' - '+vars+'</li>').css({
+        'borderBottom': '1px solid #999999'
+      });
+      _$ied.find('ol').append($message);
+      _.delay(function() {
+        $message.fadeOut(500);
+      }, 2000);
+    };
+    */
+  };
+
 })();

0ac0b78e8322c3f51ed2f8ebad75bca5f8e64e6c
javi <qualopec@gmail.com>
2012-07-23 10:08:34 +0200

    map creation and save

--- public/js/lib/cartodb/models/map.js
+++ public/js/lib/cartodb/models/map.js
@@ -1,4 +1,50 @@
 
 cdb.admin.Map = cdb.geo.Map.extend({
-  url: '/api/v1/maps'
+
+  urlRoot: '/api/v1/maps',
+
+  relatedTo: function(o) {
+    this.table = o;
+    this.table.bind('change:map_id', this._fetchOrCreate, this);
+    this.delayedSave = _.debounce(this.save, 1000);
+    this.autoSave();
+  },
+
+  parse: function(data) {
+    data.bounding_box_ne = JSON.parse(data.bounding_box_ne);
+    data.bounding_box_sw = JSON.parse(data.bounding_box_sw);
+    data.center = JSON.parse(data.center);
+    return data;
+  },
+
+  _fetchOrCreate: function() {
+    var self = this;
+    var map_id = this.table.get('map_id');
+    if(!map_id) {
+      this.create();
+    } else {
+      this.set({ id: map_id });
+      this.fetch({
+        error: function() {
+          cdb.log.info("creating map for table");
+          self.create();
+        }
+      });
+    }
+  },
+
+  create: function() {
+    this.unset('id');
+    this.set({ table_id: this.table.id });
+    this.save();
+  },
+
+  /**
+   * enable save map each time the viewport changes
+   */
+  autoSave: function() {
+    this.bind('change:center', this.delayedSave);
+    this.bind('change:zoom', this.delayedSave);
+  }
+
 });

73c98108fdd0dea231a7973eecfaa1205d714a53
Javier Alvarez <xavijam@gmail.com>
2012-07-23 17:33:00 +0200

    adding more fields to the dashboard table list

--- public/js/lib/cartodb/dashboard/tablelist.js
+++ public/js/lib/cartodb/dashboard/tablelist.js
@@ -27,6 +27,7 @@ cdb.admin.dashboard = cdb.admin.dashboard || {};
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
+      console.log(this.model.attributes);
 
       return this;
     },

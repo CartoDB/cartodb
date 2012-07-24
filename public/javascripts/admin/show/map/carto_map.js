199921837fe74d2eaba4df680841718e79e964e7
Sandro Santilli <strk@keybit.net>
2012-07-24 13:30:03 +0200

    Always refresh map on style change, even on error (closes #833)
    This is to workaround cross-domain errors preventing the map
    from refreshing while still tolerated by the actual architecture.

--- public/javascripts/admin/show/map/carto_map.js
+++ public/javascripts/admin/show/map/carto_map.js
@@ -282,14 +282,23 @@
             me.refreshWax();
         },
         error:function(e) {
-          var errors = JSON.parse(e.responseText);
-          var msg = '';
-          _.each(errors,function(ele,i){msg += ele + '<br/>';});
-          $('.cartocss_editor span.errors p').html(msg);
 
-		      var errors_height = (errors.length * 16) + 23;
-		      $('.cartocss_editor').find('.outer_textarea').css({'bottom':errors_height+'px'});
-		      $('.cartocss_editor').addClass('error');
+          if ( e.responseText ) {
+            var errors = JSON.parse(e.responseText);
+            var msg = '';
+            _.each(errors,function(ele,i){msg += ele + '<br/>';});
+            $('.cartocss_editor span.errors p').html(msg);
+
+            var errors_height = (errors.length * 16) + 23;
+            $('.cartocss_editor').find('.outer_textarea').css({'bottom':errors_height+'px'});
+            $('.cartocss_editor').addClass('error');
+          }
+          // else it could be a cross-domain issue.
+          // TODO: how to deal with it ?
+
+          // see https://github.com/Vizzuality/cartodb/issues/833
+          if (refresh)
+            me.refreshWax();
         }
       });
     }

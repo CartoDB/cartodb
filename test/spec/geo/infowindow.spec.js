676a6524be8be2580eddb991d1ecc9a20ffa4a5c
javi <qualopec@gmail.com>
2012-07-26 18:26:49 +0200

    added infowindow field manipulation methods

---
+++ test/spec/geo/infowindow.spec.js
@@ -0,0 +1,33 @@
+
+describe("cdb.geo.ui.infowindow", function() {
+
+  var model;
+  beforeEach(function() {
+    model = new cdb.geo.ui.InfowindowModel();
+  });
+
+  it("should add a field", function() {
+    expect(model.containsField('test')).toEqual(false);
+    model.addField('test');
+    model.addField('test2');
+    expect(model.containsField('test')).toEqual(true);
+    model.removeField('test');
+    expect(model.containsField('test')).toEqual(false);
+    expect(model.containsField('test2')).toEqual(true);
+    model.clearFields();
+    expect(model.containsField('test2')).toEqual(false);
+  });
+
+  it("should allow modify field properties", function() {
+    var spy = sinon.spy();
+    model.addField('test');
+    var t = model.getFieldProperty('test', 'title');
+    expect(t).toEqual(true);
+    model.bind('change:fields', spy);
+    expect(spy.called).toEqual(false);
+    model.setFieldProperty('test', 'title', false);
+    t = model.getFieldProperty('test', 'title');
+    expect(t).toEqual(false);
+    expect(spy.called).toEqual(true);
+  });
+});

6d012f66601788639036c45cba5105bcdbcedb31
David Arango <arango@gmail.com>
2012-07-26 11:27:50 +0200

    Ordering map layers by id. Adding public_values method to map

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,6 +1,7 @@
 class Map < Sequel::Model
-  many_to_many :layers
+  many_to_many :layers, :order => :id
   plugin :association_dependencies, :layers => :nullify
+  PUBLIC_ATTRIBUTES = %W{ id user_id provider bounding_box_sw bounding_box_ne center zoom }
 
   # TODO remove this
   # We'll need to join maps and tables for this version
@@ -11,7 +12,10 @@ class Map < Sequel::Model
     Table.filter(user_id: self.user_id, id: table_id).
       update(map_id: self.id)
   end
-
+  
+  def public_values
+    Hash[PUBLIC_ATTRIBUTES.map{ |a| [a, self.send(a)] }]
+  end
 
   def validate
     super

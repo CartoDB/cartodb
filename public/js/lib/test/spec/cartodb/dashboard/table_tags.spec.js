c08dfad50dc8ce3e9a231212aa2628cba7566b8d
Javier Alvarez <xavijam@gmail.com>
2012-07-30 16:02:42 +0200

    adding files forgotted

---
+++ public/js/lib/test/spec/cartodb/dashboard/table_tags.spec.js
@@ -0,0 +1,38 @@
+
+describe("table-tags", function() {
+  var tags
+    , tables
+    , user
+    , tagsView;
+
+  beforeEach(function() {
+    tables = new cdb.admin.Tables();
+    user = new cdb.admin.User({ id : "1" });
+    tags = new cdb.admin.Tags();
+
+    tagsView = new cdb.admin.dashboard.TagsView({
+      tables: tables,
+      model: tags
+    });
+
+    tables.reset([{name: 'test'}]);
+  });
+
+  it("should update table tags when tables model is fetched", function() {
+    spyOn(tagsView.model, 'fetch');
+    tables.reset([{name: 'test'}]);
+    expect(tagsView.model.fetch).toHaveBeenCalled();
+  });
+
+  it("should update user tags when a table is removed", function() {
+    spyOn(tagsView.model, 'fetch');
+    tables.pop();
+    expect(tagsView.model.fetch).toHaveBeenCalled();
+  });
+
+  it("should update user tags when a table is added", function() {
+    spyOn(tagsView.model, 'fetch');
+    tables.add({id: 1, name: 'test', privacy: 'PRIVATE', rows_counted: 1, updated_at: new Date(), tags: 'a', table_size: 100});
+    expect(tagsView.model.fetch).toHaveBeenCalled();
+  });
+});

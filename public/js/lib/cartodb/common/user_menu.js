570791e4b4b09b2fafc6a4286bab83c6862dc2d4
javi <qualopec@gmail.com>
2012-07-31 18:43:31 +0200

    drop down menu in table header in correct position

--- public/js/lib/cartodb/common/user_menu.js
+++ public/js/lib/cartodb/common/user_menu.js
@@ -49,6 +49,27 @@
       },this.options.speedIn);
     },
 
+    openAt: function(x, y) {
+
+      this.$el.css({
+        top: 0, //x +  10,
+        left: 0,//y  - this.options.width + 15,
+        width: this.options.width,
+        margin: "-10px 0 0 0",
+        display: "block",
+        opacity: 0
+      });
+
+      // Add selected class to the target
+      //$(target).addClass("selected");
+
+      // Show
+      this.$el.animate({
+        margin: "0px 0 0 0",
+        opacity: 1
+      },this.options.speedIn);
+    },
+
     hide: function(done) {
       var self = this;
       this.isOpen = false;

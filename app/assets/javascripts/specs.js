46805834fb7f2812595f87c1140456e58c5b828b
javi <qualopec@gmail.com>
2012-08-02 17:46:02 +0200

    test running again (jammit -> assets pipeline)

--- app/assets/javascripts/specs.js
+++ app/assets/javascripts/specs.js
@@ -1,5 +1,5 @@
-//= require test/lib/jasmine-1.2.0/jasmine
-//= require test/lib/jasmine-1.2.0/jasmine-html
-//= require test/lib/sinon-1.3.4
-//= require test/spec/SpecHelper
-//= require_tree test/spec
+//= require ../../../lib/assets/test/lib/jasmine-1.2.0/jasmine
+//= require ../../../lib/assets/test/lib/jasmine-1.2.0/jasmine-html
+//= require ../../../lib/assets/test/lib/sinon-1.3.4
+//= require ../../../lib/assets/test/spec/SpecHelper
+//= require_tree ../../../lib/assets/test/spec/

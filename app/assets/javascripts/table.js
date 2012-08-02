66eb3268051236ae9aaeacf8c5cda38e1354490d
David Arango <arango@gmail.com>
2012-08-02 13:06:55 +0200

    Rewriting table assets

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,1 +1,5 @@
-//= require_tree cartodb
+//= require_tree ../../../lib/assets/javascripts/cartodb/models
+//= require_tree ../../../lib/assets/javascripts/cartodb/common
+//= require_tree ../../../lib/assets/javascripts/cartodb/table
+//= require ../../../lib/assets/javascripts/cartodb/table/table
+//= require_tree ../../../lib/assets/javascripts/cartodb/table/views

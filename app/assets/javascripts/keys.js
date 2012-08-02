dd72703e0bbad5bd2c8a04ea28dc6c7eed8c704d
David Arango <arango@gmail.com>
2012-08-02 18:46:30 +0200

    Fixing keys manifest

--- app/assets/javascripts/keys.js
+++ app/assets/javascripts/keys.js
@@ -1,2 +1,5 @@
 //= require ZeroClipboard
-//= require_tree cartodb
+//= require_tree ../../../lib/assets/javascripts/cartodb/models
+//= require_tree ../../../lib/assets/javascripts/cartodb/common
+//= require_tree ../../../lib/assets/javascripts/cartodb/keys
+//= require ../../../lib/assets/javascripts/cartodb/keys/keys

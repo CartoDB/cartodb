fed1adf3c99ac841250fec034d41cd8f48045ffb
David Arango <arango@gmail.com>
2012-08-10 13:15:29 +0200

    Loading assets gem group on all environments

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,7 @@ require "sequel-rails/railtie"
 require "action_mailer/railtie"
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 

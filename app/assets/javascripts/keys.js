8d44e72694799be21389dc2c1afa682fb9353c0c
Javier Alvarez <xavijam@gmail.com>
2012-08-13 16:04:50 +0200

    codemirror css added

--- app/assets/javascripts/keys.js
+++ app/assets/javascripts/keys.js
@@ -1,4 +1,4 @@
-//= require_tree ../../../lib/assets/javascripts/cartodb/dashboard/views
+//= require_tree ../../../lib/assets/javascripts/cartodb/common/views
 //= require ../../../lib/assets/javascripts/cartodb/common/search_form
 //= require ../../../lib/assets/javascripts/cartodb/common/user_menu
 //= require_tree ../../../lib/assets/javascripts/cartodb/keys

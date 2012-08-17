58ad333f175adb9472f75115774828783f50f5f6
Javier Alvarez <xavijam@gmail.com>
2012-08-17 17:56:54 +0200

    dropdown refactor

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,7 @@
 //= require d3.v2
 //= require fileuploader
 //= require_tree ../../../lib/assets/javascripts/cartodb/models
-//= require ../../../lib/assets/javascripts/cartodb/common/user_menu
+//= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
 //= require_tree ../../../lib/assets/javascripts/cartodb/common
 //= require_tree ../../../lib/assets/javascripts/cartodb/dashboard
 //= require ../../../lib/assets/javascripts/cartodb/dashboard/dashboard

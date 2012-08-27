12b4641a77c13c690c66d652ae4f765f9d8a32e3
David Arango <arango@gmail.com>
2012-08-27 18:05:35 +0200

    Single endpoint on the api for file imports. Only works asynchronously for the time being (see #32)

--- app/controllers/api/json/imports_controller.rb
+++ app/controllers/api/json/imports_controller.rb
@@ -1,3 +1,4 @@
+#encoding: UTF-8
 class Api::Json::ImportsController < Api::ApplicationController
   ssl_required :index, :show, :create
 
@@ -15,7 +16,46 @@ class Api::Json::ImportsController < Api::ApplicationController
   end
 
   def create
-    job_meta = Resque::ImporterJobs.enqueue(current_user[:id], params[:table_name], params[:file_uri]) 
-    render :json => {:item_queue_id => job_meta.meta_id, :success => true}
+    file_uri = upload_file
+
+    if synchronous_import?
+      #@data_import = Resque::ImporterJobs.process(current_user[:id], params[:table_name], file_uri)
+      #render :json => {:item_queue_id => job_meta.meta_id, :success => true}
+    else
+      job_meta = Resque::ImporterJobs.enqueue(current_user[:id], params[:table_name], file_uri)
+      render_jsonp({:item_queue_id => job_meta.meta_id, :success => true})
+    end
+  #rescue => e
+  #  render_jsonp({ :description => e.message, :code => (@data_import.error_code rescue '') }, 400)
   end
+
+  protected
+
+  def synchronous_import?
+    params[:synchronous].present?
+  end
+
+  def upload_file
+    temp_file = filename = filedata = nil
+
+    case
+    when params[:qqfile].present? && request.body.present?
+      filename = params[:qqfile]
+      filedata = request.body.read.force_encoding('utf-8')
+    when params[:file].present?
+      filename = params[:file].original_filename
+      filedata = params[:file].read.force_encoding('utf-8')
+    end
+
+    random_token = Digest::SHA2.hexdigest("#{Time.now.utc}--#{filename.object_id.to_s}").first(20)
+
+    FileUtils.mkdir_p(Rails.root.join('public/uploads').join(random_token))
+
+    file = File.new(Rails.root.join('public/uploads').join(random_token).join(File.basename(filename)), 'w')
+    file.write filedata
+    file.close
+
+    return file.path[/(\/uploads\/.*)/, 1]
+  end
+
 end

7a502d64d680e3a363d55073419ec10e2f630fda
David Arango <arango@gmail.com>
2012-09-03 13:07:29 +0200

    Raise error when important keys are missing fron app_config.yml

--- config/initializers/01_app_config.rb
+++ config/initializers/01_app_config.rb
@@ -1,11 +1,20 @@
 module Cartodb
   def self.config
     return @config if @config
+
+    begin
     config_file_hash = YAML.load_file("#{Rails.root}/config/app_config.yml")
+    rescue => e
+      raise "Missing or inaccessible config/app_config.yml: #{e.message}"
+    end
     @config ||= config_file_hash[Rails.env].try(:to_options!)
 
     if @config.blank?
-      raise "Can't find App configuration for #{Rails.env} environment on app_config.yml"
+      raise "Can't find App configuration for #{Rails.env} environment on config/app_config.yml"
+    end
+
+    unless @config[:mandatory_keys].present? && (@config[:mandatory_keys].map(&:to_sym) - @config.keys).blank?
+      raise "Missing the following config keys on config/app_config.yml: #{(@config[:mandatory_keys].map(&:to_sym) - @config.keys).join(', ')}"
     end
   end
 

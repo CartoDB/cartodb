073ce3d38feb5ce47057264d0c9c5b63fbe0363c
javi <qualopec@gmail.com>
2012-09-05 11:18:40 +0200

    updated cartodb-leaflet library

--- vendor/cartodb-leaflet.js
+++ vendor/cartodb-leaflet.js
@@ -383,6 +383,8 @@ if (typeof(L.CartoDBLayer) === "undefined") {
         var q = encodeURIComponent(
           params[k].replace(/\{\{table_name\}\}/g, this.options.table_name)
         );
+        //left template variables the samep
+        q = q.replace(/%7Bx%7D/g,"{x}").replace(/%7By%7D/g,"{y}").replace(/%7Bz%7D/g,"{z}");
         url_params.push(k + "=" + q);
       }
       cartodb_url += url_params.join('&');
@@ -478,7 +480,10 @@ if (typeof(L.CartoDBLayer) === "undefined") {
 
       // SQL?
       if (this.options.query) {
-        var query = 'sql=' + encodeURIComponent(this.options.query.replace(/\{\{table_name\}\}/g,this.options.table_name));
+        var q = encodeURIComponent(this.options.query.replace(/\{\{table_name\}\}/g,this.options.table_name));
+        //left template variables the samep
+        q = q.replace(/%7Bx%7D/g,"{x}").replace(/%7By%7D/g,"{y}").replace(/%7Bz%7D/g,"{z}");
+        var query = 'sql=' +  q
         tile_url = this._addUrlData(tile_url, query);
         grid_url = this._addUrlData(grid_url, query);
       }

76d6d7001c3b379a2825be18ac8ef81f5a586960
Fernando Espinosa <fer@ferdev.com>
2012-09-05 16:37:15 +0200

    Adds support for json files to the importer
    Now users can import json files. The file must contain an array with
    simple json objects. See json files in the spec/support/data folder.

--- lib/importer/lib/cartodb-importer/importer.rb
+++ lib/importer/lib/cartodb-importer/importer.rb
@@ -13,7 +13,7 @@ module CartoDB
 
       @@debug = true
       RESERVED_COLUMN_NAMES = %W{ oid tableoid xmin cmin xmax cmax ctid }
-      SUPPORTED_FORMATS     = %W{ .csv .shp .ods .xls .xlsx .tif .tiff .kml .kmz .js .json .tar .gz .tgz .osm .bz2 .geojson .gpx }
+      SUPPORTED_FORMATS     = %W{ .csv .shp .ods .xls .xlsx .tif .tiff .kml .kmz .js .json .tar .gz .tgz .osm .bz2 .geojson .gpx .json }
 
       attr_accessor :import_from_file,
                     :db_configuration,
@@ -201,7 +201,8 @@ module CartoDB
                   processed_imports << d
                 }
                 @data_import.log_update('file preprocessed')
-              rescue
+              rescue => ex
+                require 'debugger'; debugger
                 @data_import.reload
                 errors << OpenStruct.new({ :description => @data_import.get_error_text,
                                              :stack =>  @data_import.log_json,

d5ddcb93ac2fcc07b8772917207ed400ae3adafe
Luis Bosque <luisico@gmail.com>
2012-09-11 16:50:35 +0200

    re-enabled load function message

--- app/models/user.rb
+++ app/models/user.rb
@@ -361,6 +361,7 @@ class User < Sequel::Model
 
   # Cartodb functions
   def load_cartodb_functions
+    puts "Loading functions in db '#{database_name}' (#{username})"
     in_database(:as => :superuser) do |user_database|
       user_database.transaction do
         glob = ::Rails.root.to_s + '/lib/sql/*.sql'

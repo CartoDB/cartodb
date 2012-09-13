57d1663d1827333a52adc7bb18cd73cc2fdccbe8
David Arango <arango@gmail.com>
2012-09-13 12:39:21 +0200

    Render user base layers in user json

--- spec/acceptance/api/users_spec.rb
+++ spec/acceptance/api/users_spec.rb
@@ -9,18 +9,25 @@ feature "API 1.0 users management" do
     @user  = create_user({:username => 'test'})
   end
 
-  scenario "Get standard information for the user by id" do    
+  scenario "Get standard information for the user by id" do
+    layer = Layer.create :kind => 'carto', :order => 2
+    layer2 = Layer.create :kind => 'tiled', :order => 1
+    @user.add_layer layer
+    @user.add_layer layer2
+
     get_json api_user_url(@user.id) do |response|
-      response.body.should == {:username          => "test", 
-                               :account_type      => "FREE", 
-                               :private_tables    =>true,
-                               :table_quota       => 5, 
-                               :table_count       => 0,                               
+      response.body.should == {:id                    => @user.id,
+                               :username              => "test", 
+                               :account_type          => "FREE", 
+                               :private_tables        => true,
+                               :table_quota           => 5, 
+                               :table_count           => 0,                               
                                :remaining_table_quota => 5, 
-                               :byte_quota        => 104857600, 
-                               :remaining_byte_quota  => 103231488.0, 
-                               :api_key           => @user.get_map_key,
-                               :api_calls         => (1..30).map{|i| i}}       
+                               :byte_quota            => 104857600, 
+                               :remaining_byte_quota  => 104857600.0,
+                               :layers                => [{"options"=>nil, "kind"=>"tiled", "infowindow"=>nil, "id"=>layer2.id, "order"=>1}, {"options"=>nil, "kind"=>"carto", "infowindow"=>nil, "id"=>layer.id, "order"=>2}],
+                               :api_key               => @user.get_map_key,
+                               :api_calls             => (1..30).map{|i| i}}       
       response.status.should be_success      
     end
   end

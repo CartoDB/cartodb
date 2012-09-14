bfa0ecc635c7fc07358b47017156629266beb5e7
David Arango <arango@gmail.com>
2012-09-14 13:29:39 +0200

    Fail over latin 1 when importing a file with invalid utf-8 characters

--- lib/importer/lib/cartodb-importer/loaders/csv.rb
+++ lib/importer/lib/cartodb-importer/loaders/csv.rb
@@ -15,7 +15,7 @@ module CartoDB
           @data_import = DataImport.find(:id=>@data_import_id)
 
           # run Chardet + Iconv
-          fix_encoding
+          encoding_to_try = fix_encoding
 
           # Check if file has only one header, add a blank column
           # if so. See Vizzuality/CartoDB#202
@@ -29,8 +29,8 @@ module CartoDB
 
           @data_import.log_update("ogr2ogr #{@working_data[:suggested_name]}")
           ogr2ogr_bin_path = `which ogr2ogr`.strip
-          ogr2ogr_command = %Q{#{ogr2ogr_bin_path} -f "PostgreSQL" PG:"host=#{@db_configuration[:host]} port=#{@db_configuration[:port]} user=#{@db_configuration[:username]} dbname=#{@db_configuration[:database]}" #{@working_data[:path]} -nln #{@working_data[:suggested_name]}}
-
+          ogr2ogr_command = %Q{PGCLIENTENCODING=#{encoding_to_try} #{ogr2ogr_bin_path} -f "PostgreSQL" PG:"host=#{@db_configuration[:host]} port=#{@db_configuration[:port]} user=#{@db_configuration[:username]} dbname=#{@db_configuration[:database]}" #{@working_data[:path]} -nln #{@working_data[:suggested_name]}}
+          puts ogr2ogr_command
           stdin,  stdout, stderr = Open3.popen3(ogr2ogr_command)
 
           unless (err = stderr.read).empty?

813cbc188f8ebb87f6e91b6f1bab86c713e35aa9
David Arango <arango@gmail.com>
2012-10-10 19:05:23 +0200

    Invalidates varnish cache by http header, not by url. Avoids always updating table's map_id attribute.

--- app/models/map.rb
+++ app/models/map.rb
@@ -28,7 +28,7 @@ class Map < Sequel::Model
   attr_accessor :table_id
   def after_save
     Table.filter(user_id: self.user_id, id: self.table_id).
-      update(map_id: self.id)
+      update(map_id: self.id) unless self.table_id.blank?
 
     self.invalidate_varnish_cache
   end
@@ -41,11 +41,13 @@ class Map < Sequel::Model
     super
 
     errors.add(:user_id, "can't be blank") if user_id.blank?
+    #errors.add(:table_id, "can't be blank") if table_id.blank?
     #errors.add(:user_id, "does not exist") if user_id.present? && User[user_id].nil?
     #errors.add(:table_id, "table #{table_id} doesn't belong to user #{user_id}") if user_id.present? && !User[user_id].tables.select(:id).map(&:id).include?(table_id)
   end
 
   def invalidate_varnish_cache
-    CartoDB::Varnish.new.purge("wadus")
+    t = self.tables_dataset.select(:id, :user_id, :name).first
+    CartoDB::Varnish.new.purge("obj.http.X-Cache-Channel ~ #{t.varnish_key}:vizjson")
   end
 end

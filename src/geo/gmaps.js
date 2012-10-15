6179eb27f6f25f27a46af0c7637ec8bd5ad28189
Javier Arce <javierarce@gmail.com>
2012-10-15 12:39:59 +0200

    Reverts default_map_style change

--- src/geo/gmaps.js
+++ src/geo/gmaps.js
@@ -2,7 +2,7 @@
 // if google maps is not defined do not load the class
 if(typeof(google) != "undefined" && typeof(google.maps) != "undefined") {
 
-var DEFAULT_MAP_STYLE  = [];
+var DEFAULT_MAP_STYLE = [ { stylers: [ { saturation: -65 }, { gamma: 1.52 } ] },{ featureType: "administrative", stylers: [ { saturation: -95 }, { gamma: 2.26 } ] },{ featureType: "water", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "administrative.locality", stylers: [ { visibility: "off" } ] },{ featureType: "road", stylers: [ { visibility: "simplified" }, { saturation: -99 }, { gamma: 2.22 } ] },{ featureType: "poi", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "road.arterial", stylers: [ { visibility: "off" } ] },{ featureType: "road.local", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "transit", stylers: [ { visibility: "off" } ] },{ featureType: "road", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "poi", stylers: [ { saturation: -55 } ] } ];
 
 /**
 * base layer for all leaflet layers

fda39b7db36386e6bf7cd8744994175f1ebbc61a
javi <qualopec@gmail.com>
2012-10-23 11:28:46 +0200

    publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-BRANCH = `git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 
 git checkout gh-pages
-git merge $(BRANCH)
+git merge $BRANCH
 clean
 release
 git add v2
 git ci -a -m "publish"
-git checkout $(BRANCH)
+git checkout $BRANCH

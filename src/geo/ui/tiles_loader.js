7845af56a9a6f22a350d526bba0002dc3ab52b29
Javier Alvarez <xavijam@gmail.com>
2012-11-22 16:43:52 +0100

    zoom info added as a new component and tiles-loader now in cartodb.js and not core repository

---
+++ src/geo/ui/tiles_loader.js
@@ -0,0 +1,49 @@
+/**
+ * Show or hide tiles loader
+ *
+ * Usage:
+ *
+ * var tiles_loader = new cdb.geo.ui.TilesLoader();
+ * mapWrapper.$el.append(tiles_loader.render().$el);
+ *
+ */
+
+
+cdb.geo.ui.TilesLoader = cdb.core.View.extend({
+
+  id: "tiles_loader",
+
+  default_options: {
+    animationSpeed: 500
+  },
+
+  initialize: function() {
+    _.defaults(this.options, this.default_options);
+    this.isVisible = false;
+    this.template = this.options.template ? this.options.template : cdb.templates.getTemplate('geo/tiles_loader');
+  },
+
+  render: function() {
+    this.$el.html($(this.template(this.options)));
+    return this;
+  },
+
+  show: function(ev) {
+    this.isVisible = true;
+    if (!$.browser.msie || ($.browser.msie && $.browser.version.indexOf("9.") != 0)) {
+      this.$el.fadeTo(this.options.animationSpeed, 1)
+    } else {
+      this.$el.show();
+    }
+  },
+
+  hide: function(ev) {
+    this.isVisible = false;
+    if (!$.browser.msie || ($.browser.msie && $.browser.version.indexOf("9.") == 0)) {
+      this.$el.stop(true).fadeTo(this.options.animationSpeed, 0)
+    } else {
+      this.$el.hide();
+    }
+  }
+
+});

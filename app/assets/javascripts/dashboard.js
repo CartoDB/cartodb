4b913abfd013d3b685be3287e5241b28e4116585
Javier Alvarez <xavijam@gmail.com>
2012-12-26 12:23:12 +0100

    removed jquery.cookie and now using cdb.admin. LocalStorage

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,7 +3,6 @@
 //= require jquery.fileupload
 //= require jquery.fileupload-fp
 //= require jquery.fileupload-ui
-//= require jquery.cookie
 //= require_tree ../../../lib/assets/javascripts/cartodb/models
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
 //= require_tree ../../../lib/assets/javascripts/cartodb/common

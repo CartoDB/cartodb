85051d06afd437fb8a411307a013166ab3427f8a
javi <qualopec@gmail.com>
2013-01-11 17:30:55 +0100

    menu refactor

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -14,6 +14,7 @@
 //= require_tree ../../../lib/assets/javascripts/cartodb/models
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
 //= require_tree ../../../lib/assets/javascripts/cartodb/common
+//= require ../../../lib/assets/javascripts/cartodb/table/right_menu
 //= require_tree ../../../lib/assets/javascripts/cartodb/table
 //= require ../../../lib/assets/javascripts/cartodb/table/table
 //= require_tree ../../../lib/assets/javascripts/cartodb/table/views

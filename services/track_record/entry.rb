be5a75ee5c291678ea83289505f1d19f6e551e3a
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-02-07 18:49:13 +0100

    Make log entries support DataRepository (multiple backends)

--- services/track_record/entry.rb
+++ services/track_record/entry.rb
@@ -1,27 +1,86 @@
 # encoding: utf-8
+require 'virtus'
+require 'uuidtools'
+require 'json'
+require_relative '../data-repository/handler'
 
 module TrackRecord
   class Entry
-    attr_reader :timestamp, :text
+    include Virtus
 
-    def initialize(text)
-      @text       = text
-      @timestamp  = Time.now.to_f.to_s
+    PAYLOAD_TYPE_ERROR      = 'Payload must be a Hash'
+
+    attribute :id,          String
+    attribute :payload,     Hash, default: {}
+    attribute :timestamp,   Float
+    attribute :created_at,  Time
+
+    def initialize(payload, repository=DataRepository::Handler.new)
+      raise(ArgumentError, PAYLOAD_TYPE_ERROR) unless payload.is_a?(Hash)
+      @repository = repository
+      payload     = JSON.parse(payload.to_json)
+      self.id     = payload.delete('id')
+
+      initialize_attributes_with(payload)
     end #initialize
+
+    def payload
+      super.dup
+    end #payload
+
+    def created_at=(time)
+      super Time.parse(time.to_s).utc
+    end #time=
+
+    def timestamp=(time_or_float)
+      super time_or_float.to_f
+    end #timestamp=
 
     def <=>(other)
       timestamp <=> other.timestamp
     end #<=>
 
+    def to_hash
+      JSON.parse(attributes_without_id.to_json)
+    end #to_hash
+
     def to_s
-      [utc_for(timestamp), text].join(' :: ')
+      [created_at, payload.values].join(' || ')
     end #to_s
+
+    def persist
+      repository.store(storage_key, self.to_hash)
+      self
+    end #persist
+
+    def fetch
+      self.attributes = repository.fetch(storage_key)
+      self
+    end #fetch
+
+    def storage_key
+      "entry:#{id}"
+    end #storage_key
 
     private
 
-    def utc_for(timestamp)
-      Time.at(timestamp.to_f).utc
-    end #utc_for
+    attr_reader :repository
+
+    def initialize_attributes_with(payload)
+      return if id
+
+      self.id         = next_id
+      self.payload    = payload
+      self.timestamp  = self.created_at = Time.now
+    end #attributes
+
+    def next_id
+      UUIDTools::UUID.timestamp_create.to_s
+    end #next_id
+
+    def attributes_without_id
+      attributes.delete_if { |k, v| k =~ /^id$/ }
+    end #attributes_without_id
   end # Entry
 end # TrackRecord
 

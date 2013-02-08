7c3ad4dc34b8adfe509c406cda38b08f35173feb
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-02-08 16:35:20 +0100

    Add DataRepository.new with the ability to detect backend connection type (redis | memory)

--- services/data-repository/repository.rb
+++ services/data-repository/repository.rb
@@ -1,11 +1,21 @@
 # encoding: utf-8
+require_relative 'backend/detector'
 require_relative 'backend/memory'
 
 module DataRepository
+  def self.new(backend_or_connection=nil)
+    backend = Backend::Detector.new(backend_or_connection).detect
+    Repository.new(backend)
+  end # DataRepository.new
+
   class Repository
     def initialize(storage=Backend::Memory.new)
       @storage = storage
     end #initialize
+
+    def backend
+      @storage
+    end #backend
 
     def store(key, data, options={})
       storage.store(key.to_s, data, options)

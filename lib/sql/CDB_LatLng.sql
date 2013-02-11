929c54c063987a8c7f766e9643766b29b9cda233
andrewxhill <andrewxhill@gmail.com>
2013-02-11 23:02:15 +0100

    added doc for latlng

--- lib/sql/CDB_LatLng.sql
+++ lib/sql/CDB_LatLng.sql
@@ -1,3 +1,12 @@
+--
+-- Create a valid GEOMETRY in 4326 from a lat/lng pair
+--
+-- @param lat A numeric latitude value.
+--
+-- @param lng A numeric longitude value.
+--  
+--
+
 CREATE OR REPLACE FUNCTION CDB_LatLng (lat NUMERIC, lng NUMERIC) RETURNS geometry as $$ 
 BEGIN 
     -- this function is silly

86c3c1094121cb7fb1b9870b6032ac008d3bda4b
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-02-11 11:53:28 +0100

    Implement logging in Workman job dispatcher

--- services/workman/dispatcher.rb
+++ services/workman/dispatcher.rb
@@ -2,19 +2,24 @@
 require 'redis'
 require 'resque'
 require_relative './job/model'
+require_relative '../track_record/track_record'
 
 module Workman
   class Dispatcher
-    attr_reader :queue
+    attr_reader :queue, :log
 
-    def initialize(queue=Resque)
-      @queue = queue
+    MESSAGE_TEMPLATE = "Job {job_id} changed to '{state}'"
+
+    def initialize(queue=Resque, log=TrackRecord::Log.new)
+      @queue  = queue
+      @log    = log
     end #initialize
 
     def schedule(attributes)
       job = Job::Model.new(attributes)
       job.transition_to(:queued)
       queue.enqueue(job.class, job.id)
+      log.append entry_for(job, :queued)
       job
     end #schedule
 
@@ -22,12 +27,24 @@ module Workman
       job = Job::Model.new(id: job_id).fetch
       job.transition_to(:aborted)
       queue.dequeue(job.class, job.id)
+      log.append entry_for(job, :aborted)
       job
     end #abort
 
     def query(job_id)
       Job::Model.new(id: job_id).fetch
     end #query
+    private
+
+    def entry_for(job, state)
+      { job_id: job.id, message: message_for(job.id, state) }
+    end
+
+    def message_for(job_id, state)
+      MESSAGE_TEMPLATE.dup
+        .gsub(/{job_id}/, job_id)
+        .gsub(/{state}/, state.to_s)
+    end #message_for
   end # Dispatcher
 end # Workman
 

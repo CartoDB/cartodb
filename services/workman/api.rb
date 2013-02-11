deda12e33174d3db908831b90d68fec9d46a1e1b
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-02-11 14:01:55 +0100

    Workman::Dispatcher methods now accept a job instance insted of a job_id

--- services/workman/api.rb
+++ services/workman/api.rb
@@ -7,17 +7,20 @@ require_relative './dispatcher'
 module Workman
   class API < Sinatra::Base
     post '/jobs' do
-      job = Dispatcher.new.schedule(payload)
+      job = Job::Model.new(payload)
+      Dispatcher.new.schedule(job)
       [201, present(job)]
     end # post /jobs
 
     get '/jobs/:job_id' do
-      job = Dispatcher.new.query(params.fetch('job_id'))
+      job = job_from(params.fetch('job_id'))
+      Dispatcher.new.query(job)
       [200, present(job)]
     end # get /jobs
 
     delete '/jobs/:job_id' do
-      job = Dispatcher.new.abort(params.fetch('job_id'))
+      job = job_from(params.fetch('job_id'))
+      Dispatcher.new.abort(job)
       [200, present(job)]
     end # delete /jobs
 
@@ -26,6 +29,10 @@ module Workman
     def present(job)
       Job::Presenter.new(job).as_json
     end #present
+
+    def job_from(job_id)
+      Job::Model.new(id: params.fetch('job_id')).fetch
+    end #job_from
 
     def payload
       @payload ||= JSON.parse(raw_payload).delete_if { |k, v| k =~ /^id/ }

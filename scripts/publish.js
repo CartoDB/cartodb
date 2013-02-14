3de0665c97e1ce10865345fc670524d19f20d499
javi <qualopec@gmail.com>
2013-02-14 18:46:06 +0100

    script to publish only development version

--- scripts/publish.js
+++ scripts/publish.js
@@ -83,12 +83,15 @@ var CSS_IMAGE_FILES = fs.readdirSync('themes/css/images')
 var IMG_FILES = fs.readdirSync('themes/img')
 
 var only_invalidate = process.argv.length > 2 && process.argv[2] == '--invalidate';
+var only_current_version = process.argv.length > 2 && process.argv[2] == '--current_version';
 
 if(!only_invalidate) {
-  put_files(JS_FILES, 'v2', 'cartodb.js/v2')
-  put_files(CSS_FILES, 'v2/themes/css', 'cartodb.js/v2/themes/css')
-  put_files(CSS_IMAGE_FILES, 'v2/themes/css/images', 'cartodb.js/v2/themes/css/images')
-  put_files(IMG_FILES, 'v2/themes/img', 'cartodb.js/v2/themes/img')
+  if(!only_current_version) {
+    put_files(JS_FILES, 'v2', 'cartodb.js/v2')
+    put_files(CSS_FILES, 'v2/themes/css', 'cartodb.js/v2/themes/css')
+    put_files(CSS_IMAGE_FILES, 'v2/themes/css/images', 'cartodb.js/v2/themes/css/images')
+    put_files(IMG_FILES, 'v2/themes/img', 'cartodb.js/v2/themes/img')
+  }
 
   put_files(JS_FILES, 'v2', 'cartodb.js/v2/' + package_.version)
   put_files(CSS_FILES, 'v2/themes/css', 'cartodb.js/v2/' + package_.version + '/themes/css')
@@ -98,10 +101,12 @@ if(!only_invalidate) {
 
 
 console.log(" *** flushing cdn cache")
-invalidate_files(JS_FILES,  'cartodb.js/v2')
-invalidate_files(CSS_FILES, 'cartodb.js/v2/themes/css')
-invalidate_files(CSS_IMAGE_FILES, 'cartodb.js/v2/themes/css/images')
-invalidate_files(IMG_FILES, 'cartodb.js/v2/themes/img')
+if(!only_current_version) {
+  invalidate_files(JS_FILES,  'cartodb.js/v2')
+  invalidate_files(CSS_FILES, 'cartodb.js/v2/themes/css')
+  invalidate_files(CSS_IMAGE_FILES, 'cartodb.js/v2/themes/css/images')
+  invalidate_files(IMG_FILES, 'cartodb.js/v2/themes/img')
+}
 invalidate_files(JS_FILES , 'cartodb.js/v2/' + package_.version)
 invalidate_files(CSS_FILES, 'cartodb.js/v2/' + package_.version + '/themes/css')
 invalidate_files(CSS_IMAGE_FILES, 'cartodb.js/v2/' + package_.version + '/themes/css/images')

ecbf4a028a706d4e0c6cde50cb482d106a17b45e
javi <qualopec@gmail.com>
2013-03-01 15:38:09 +0100

    fixed updated_at -> cache_buster

--- src/api/layers.js
+++ src/api/layers.js
@@ -94,8 +94,9 @@
         layerData = visData.layers[1];
         // add the timestamp to options
         layerData.options.extra_params = layerData.options.extra_params || {};
-        layerData.options.extra_params.updated_at = visData.updated_at;
-        delete layerData.options.cache_buster;
+        //layerData.options.extra_params.updated_at = visData.updated_at;
+        layerData.options.extra_params.cache_buster = visData.updated_at;
+        //delete layerData.options.cache_buster;
       } else {
         layerData = visData;
       }

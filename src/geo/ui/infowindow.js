dfd0a56d290c24973f209d194eebef2533a5a886
xavijam <xavijam@gmail.com>
2013-03-04 16:36:33 +0100

    added template_name in the infowindow model + problems with fields index

--- src/geo/ui/infowindow.js
+++ src/geo/ui/infowindow.js
@@ -286,9 +286,9 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
   _fieldsToString: function(attrs) {
     if (attrs.content && attrs.content.fields) {
       var self = this;
-      attrs.content.fields = _.map(attrs.content.fields, function(attr) {
+      attrs.content.fields = _.map(attrs.content.fields, function(attr,i) {
         // Return whole attribute sanitized
-        return self._sanitizeField(attr, attrs.template_name);
+        return self._sanitizeField(attr, attrs.template_name, attr.index || i);
       });
     }
 
@@ -303,7 +303,7 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
    *  - If the value is a valid url, let's make it a link.
    *  - More to come...
    */                                                                                                                
-  _sanitizeField: function(attr, template_name) {
+  _sanitizeField: function(attr, template_name, pos) {
     // Check null or undefined :| and set both to empty == ''
     if (attr.value == null || attr.value == undefined) {
       attr.value = '';
@@ -323,17 +323,17 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
     }
 
     // If it is index 0, not loading, header template type and length bigger than 30... cut off the text!
-    if (!attr.loading && attr.index==0 && attr.value.length > 35 && template_name && template_name.search('_header_') != -1) {
+    if (!attr.loading && pos==0 && attr.value.length > 35 && template_name && template_name.search('_header_') != -1) {
       new_value = attr.value.substr(0,32) + "...";
     }
 
     // If it is index 0, not loading, header image template type... don't cut off the text!
-    if (attr.index==0 && template_name.search('_header_with_image') != -1) {
+    if (pos==0 && template_name.search('_header_with_image') != -1) {
       new_value = attr.value;
     }
 
     // If it is index 1, not loading, header image template type and length bigger than 30... cut off the text!
-    if (!attr.loading && attr.index==1 && attr.value.length > 35 && template_name && template_name.search('_header_with_image') != -1) {
+    if (!attr.loading && pos==1 && attr.value.length > 35 && template_name && template_name.search('_header_with_image') != -1) {
       new_value = attr.value.substr(0,32) + "...";
     }
 

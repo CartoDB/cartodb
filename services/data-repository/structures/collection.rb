f9281e50c10ec9c899b6193a164126cba7aadbaf
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-13 18:15:14 +0100

    preliminary implementation for DataRepository::Collection

---
+++ services/data-repository/structures/collection.rb
@@ -0,0 +1,65 @@
+# encoding: utf-8
+require 'ostruct'
+require 'set'
+require_relative '../repository'
+
+module DataRepository
+  class Collection
+    include Enumerable
+
+    INTERFACE  = %w{ id add delete store fetch each to_json repository } +
+                  Enumerable.instance_methods
+
+    attr_reader :id
+
+    def initialize(attributes={}, member_class=nil, repository=nil)
+      @storage      = Set.new
+      @member_class = member_class  || OpenStruct
+      @repository   = repository    || Repository.new
+      @id           = attributes.fetch(:id, @repository.next_id)
+    end #initialize
+
+    def add(member)
+      storage.add(member.id)
+      self
+    end #add
+
+    def delete(member)
+      storage.delete(member.id)
+      self
+    end #delete
+
+    def each(&block)
+      return members(&block) if block
+      Enumerator.new(self, :each)
+    end #each
+
+    def fetch
+      self.storage = Set[*repository.fetch(id)]
+      self
+    rescue => exception
+      storage.clear
+      self
+    end #fetch
+
+    def store
+      repository.store(id, storage.to_a)
+      self
+    end #store
+
+    def to_json(*args)
+      fetch
+      map { |member| member.fetch.to_hash }.to_json(*args)
+    end #to_json
+
+    private
+
+    attr_accessor :storage
+    attr_reader   :repository, :member_class
+
+    def members
+      storage.each { |member_id| yield member_class.new(id: member_id) }
+    end #members
+  end # Collection
+end # DataRepository
+

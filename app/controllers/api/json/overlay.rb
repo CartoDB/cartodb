071813c7b534f77ed0c6889c1e63bde64ecbad7a
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-14 11:39:00 +0100

    implement GET /visualizations/:visualization_id/overlays

--- app/controllers/api/json/overlay.rb
+++ app/controllers/api/json/overlay.rb
@@ -2,16 +2,36 @@
 require 'sinatra/base'
 require 'json'
 require_relative '../../../models/overlay/member'
+require_relative '../../../models/overlay/collection'
 
 module CartoDB
   module Overlay
     class API < Sinatra::Base
+      OVERLAY_COLLECTION_ID = 'overlays'
+
+      get '/api/v1/visualizations/:visualization_id/overlays' do
+        begin
+          collection  = Overlay::Collection.new(
+                         { id: OVERLAY_COLLECTION_ID }
+                       ).fetch
+          response    = { overlays: collection }.to_json
+          [200, response]
+        rescue KeyError => exception
+          [404]
+        end
+      end # get /api/v1/visualizations/:visualization_id/overlays
+
       post '/api/v1/visualizations/:visualization_id/overlays' do
-        member          = Overlay::Member.new(payload)
-        response        = member.store.attributes.to_json
+        collection  = Overlay::Collection.new(
+                        { id: OVERLAY_COLLECTION_ID }
+                      ).fetch
+        member      = Overlay::Member.new(payload)
+        collection.add(member)
+        collection.store
+        response    = member.store.attributes.to_json
 
         [201, response]
-      end # POST /api/v1/visualizations/:visualization_id/overlays
+      end # post /api/v1/visualizations/:visualization_id/overlays
 
       get '/api/v1/visualizations/:visualization_id/overlays/:id' do
         begin
@@ -22,20 +42,30 @@ module CartoDB
         rescue KeyError
           [404]
         end 
-      end # GET /api/v1/visualizations/:visualization_id/overlays/:id
+      end # get /api/v1/visualizations/:visualization_id/overlays/:id
 
       put '/api/v1/visualizations/:visualization_id/overlays/:id' do
-        member            = Overlay::Member.new(id: params.fetch('id')).fetch
-        member.attributes = payload
-
-        response  = member.store.attributes.to_json
-        [200, response]
-      end # PUT /api/v1/visualizations/:visualization_id/overlays/:id
+        begin
+          member            = Overlay::Member.new(id: params.fetch('id')).fetch
+          member.attributes = payload
+          member.store
+          response  = member.store.attributes.to_json
+          [200, response]
+        rescue KeyError
+          [404]
+        end
+      end # put /api/v1/visualizations/:visualization_id/overlays/:id
 
       delete '/api/v1/visualizations/:visualization_id/overlays/:id' do
-        member = Overlay::Member.new(id: params.fetch('id')).delete
+        collection  = Overlay::Collection.new(
+                        { id: OVERLAY_COLLECTION_ID }
+                      ).fetch
+        member      = Overlay::Member.new(id: params.fetch('id'))
+        collection.delete(member)
+        member.delete
+        collection.store
         [204]
-      end # DELETE /api/v1/visualizations/:visualization_id/overlays/:id
+      end # delete /api/v1/visualizations/:visualization_id/overlays/:id
 
       helpers do
         def payload

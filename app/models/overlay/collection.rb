649c51f93f106bd0e7de4c1a392288c1fe61e4d2
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-14 11:35:54 +0100

    implement Overlay::Collection

---
+++ app/models/overlay/collection.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8 require 'set'
+require_relative '../visualization'
+require_relative './member'
+require_relative '../../../services/data-repository/structures/collection'
+
+module CartoDB
+  module Overlay
+    class Collection
+      def initialize(attributes={}, options={})
+        options     = defaults.merge(options)
+        @collection = DataRepository::Collection.new(attributes, options)
+      end #initialize
+
+      DataRepository::Collection::INTERFACE.each do |method_name|
+        define_method(method_name) do |*arguments, &block|
+          result = collection.send(method_name, *arguments, &block)
+          return self if result.is_a?(DataRepository::Collection)
+          result
+        end
+      end
+
+      private
+
+      attr_reader :collection
+
+      def defaults
+        { 
+          repository:   Visualization.default_repository,
+          member_class: Member
+        }
+      end #defautls
+    end # Collection
+  end # Overlay
+end # CartoDB
+

48cd99131371f438593565d7640f638703127650
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-20 15:44:00 +0100

    Refactor CartoDB::KMZ

--- lib/importer/lib/cartodb-importer/decompressors/kmz.rb
+++ lib/importer/lib/cartodb-importer/decompressors/kmz.rb
@@ -1,65 +1,92 @@
 # encoding: utf-8
+require 'zip/zip'
 require 'tempfile'
+require 'fileutils'
+require_relative '../constants'
+require_relative '../exceptions'
 
 module CartoDB
   class KMZ
-    # TODO: do we really need this still?
-    attr_accessor :data_import, :path, :suggested_name, :tmp_dir
-
     def initialize(arguments)
       @path           = arguments.fetch(:path)
       @suggested_name = arguments.fetch(:suggested_name)
-      @data_import    = arguments.fetch(:data_import)
-      tempfile        = Tempfile.new("")
-      @tmp_dir        = tempfile.path
-      tempfile.close!
+      @log            = arguments.fetch(:log, String.new)
+      @import         = []
     end #initialize
 
     def process!
-      data_import.log_update("Importing zip file: #{path}")
-      import_data     = []
+      log("Importing zip file: #{path}")
+      log("Extracting from file: #{path}")
+      Zip::ZipFile.foreach(path) { |entry| extract(entry) }
+      log("Extraction finished for file: #{path}")
 
-      data_import.log_update("Decompressing file: #{path}")
-      Zip::ZipFile.foreach(path) do |entry|
-        name = entry.name.split('/').last
-        orig = name
-        next if name =~ /^(\.|\_{2})/
-
-        # cleans spaces out of archived file names
-        name = name.gsub(' ','_')
-
-        #fixes problem of different SHP archive files 
-        # with different case patterns
-        unless name == orig.downcase
-          FileUtils.mv("#{path}/#{orig}", "#{path}/#{name.downcase}") 
-        end 
-        name = name.downcase
-
-        # temporary filename. no collisions.
-        tmp_path = "#{tmp_dir}.#{name}"
-
-        if CartoDB::Importer::SUPPORTED_FORMATS.include?(File.extname(name))
-          import_data << {
-            ext:            File.extname(name),
-            suggested_name: name_from(name, suggested_name),
-            path:           tmp_path
-          }
-          data_import.log_update(
-            "Found original @ext file named #{name} in path #{path}"
-          )
-        end
-        entry.extract(tmp_path)
-      end
-
-      import_data
+      import
+    rescue
+      raise ExtractionError
     end #process!
 
     private
 
+    attr_reader :path, :suggested_name, :import
+
+    def extract(entry)
+      return if hidden?(entry.name)
+      filename       = normalize_filename(entry)
+      temporary_path = "#{generate_tempfile}.#{filename}"
+
+      import.push(data_for(filename, temporary_path)) if supported?(entry.name)
+      log("Found original file #{filename} in path #{temporary_path}")
+      entry.extract(temporary_path)
+    end #extract
+
+
+    def data_for(name, path)
+      {
+        ext:            File.extname(name),
+        suggested_name: name_from(name, suggested_name),
+        path:           path
+      }
+    end #data_for
+
+    def log(message)
+      #@log.append("KMZ: #{message}")
+    end #log
+
+    def supported?(filename)
+      Importer::SUPPORTED_FORMATS.include?(File.extname(filename))
+    end #supported?
+
+    def normalize_filename(entry)
+      filename    = entry.name.split('/').last
+      normalized  = normalize(filename)
+      rename_file(filename, normalized)
+      normalized
+    end #normalize_filename
+
+    def normalize(filename)
+      filename.gsub(' ','_').downcase
+    end #normalize
+
     def name_from(name, suggested_name=nil )
-      return suggested_name unless suggested_name.nil?
-      File.basename(name, File.extname(name)).sanitize
+      suggested_name || File.basename(name, File.extname(name)).sanitize
     end #name_from
+
+    def generate_tempfile
+      tempfile    = Tempfile.new("")
+      path        = tempfile.path
+      tempfile.close!
+      path
+    end #generate_tempfile
+
+    def rename_file(filename, normalized)
+      return false if filename == normalized
+      FileUtils.mv("#{path}/#{filename}", "#{path}/#{normalized}")
+      normalized
+    end #rename_file
+
+    def hidden?(name)
+      name =~ /^(\.|\_{2})/
+    end #dot_directory?
   end # KMZ
 end # CartoDB
 

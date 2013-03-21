767e9334f1caa843ba1eba9d684467f337933745
David Arango <arango@gmail.com>
2013-03-21 15:23:27 +0100

    Undo last commit

--- app/controllers/api/json/imports_controller.rb
+++ app/controllers/api/json/imports_controller.rb
@@ -9,7 +9,7 @@ class Api::Json::ImportsController < Api::ApplicationController
 
   def show
     import        = DataImport[params[:id]]
-    import_values = import.public_values
+    import_values = import.public_values.merge(success: import.state != 'failure')
 
     render json: import_values
   end

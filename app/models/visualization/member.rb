4e461bf49398900610bcba5ab353fb6befbde7dd
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-28 20:37:30 +0100

    One step back in Visualization::Member to prepare for Tag::Collection association

--- app/models/visualization/member.rb
+++ app/models/visualization/member.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'virtus'
-require 'virtus/attribute/writer/coercible'
 require_relative '../visualization'
 
 module CartoDB
@@ -15,14 +14,16 @@ module CartoDB
       attribute :tags,          Array[String]
       attribute :description,   String
 
-      def initialize(attributes={}, repository=nil)
+      def initialize(attributes={}, repository=Visualization.default_repository)
+        @repository     = repository
         self.attributes = attributes
-        @repository     = repository || Visualization.default_repository
         self.id         ||= @repository.next_id
       end #initialize
 
       def store
-        repository.store(id, attributes.to_hash)
+        data = attributes.to_hash
+        (data.delete(:tags) || []).each { |tag| store_tag(id, tag) }
+        repository.store(id, data)
         self
       end #store
 
@@ -40,7 +41,24 @@ module CartoDB
       private
 
       attr_reader :repository
+
+      def store_tag(visualization_id, tag)
+        Tag::Member.new(
+          name:             tag, 
+          visualization_id: visualization_id
+        ).store
+      end #store_ta:
     end # Member
   end # Visualization
+
+  module Tag
+    class Member
+      def initialize(attributes={})
+      end #initilize
+
+      def store(*args)
+      end #store
+    end # Member
+  end # Tag
 end # CartoDB
 

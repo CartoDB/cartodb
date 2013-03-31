31cdba14529e24e1619cc8e6a86a03c042bd730a
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-31 15:59:04 +0200

    Refactor Visualization::Repository

--- app/models/visualization/repository.rb
+++ app/models/visualization/repository.rb
@@ -5,21 +5,27 @@ require 'uuidtools'
 module CartoDB
   module Visualization
     class Repository
+      TABLE = :visualizations
+
       def initialize(db=Sequel.sqlite)
         @db = db
       end #initialize
 
       def store(key, data={})
-        @db[:visualizations].insert(data)
+        db[TABLE].insert(data)
       end #store
 
       def fetch(key)
-        data = @db[:visualizations].where(id: key).first
+        db[TABLE].where(id: key).first
       end #fetch
 
       def next_id
         UUIDTools::UUID.timestamp_create
       end #next_id
+
+      private
+
+      attr_reader :db
     end # Repository
   end # Visualization
 end # CartoDB

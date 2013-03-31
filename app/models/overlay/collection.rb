a3c0c14bca7fc70090a740dd9f77d91add9f148b
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-31 17:43:10 +0200

    Require a visualization_id for Overlay::Collection

--- app/models/overlay/collection.rb
+++ app/models/overlay/collection.rb
@@ -1,13 +1,22 @@
-# encoding: utf-8 require 'set'
-require_relative '../visualization'
+# encoding: utf-8
+require 'set'
+require 'virtus'
+require 'aequitas'
 require_relative './member'
+require_relative '../visualization'
 require_relative '../../../services/data-repository/structures/collection'
 
 module CartoDB
   module Overlay
     SIGNATURE = 'overlays'
-
+    
     class Collection
+      include Virtus
+      include Aequitas
+
+      attribute :visualization_id,  String
+      validates_presence_of         :visualization_id
+
       def initialize
         @collection = 
           DataRepository::Collection.new({ signature: SIGNATURE }, defaults)

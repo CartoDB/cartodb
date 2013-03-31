46fab261500ca9f82fc7350ba7eef3c67764934a
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-03-31 19:07:45 +0200

    Make id the primary key in visualizations and overlays

--- db/migrate/20130331112949_visualizations.rb
+++ db/migrate/20130331112949_visualizations.rb
@@ -3,7 +3,7 @@
 class VisualizationsMigration < Sequel::Migration
   def up
     create_table :visualizations do
-      String      :id,          null: false
+      String      :id,          null: false, primary_key: true
       String      :name,        text: true
       String      :description, text: true
       String      :map_id,      index: true

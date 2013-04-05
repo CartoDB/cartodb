0767611d14f0fbb1b0689312ca50e5b75931ce48
David Arango <arango@gmail.com>
2013-04-05 18:42:36 +0200

    Using Rollbar instead of Bugsnag

--- app/models/map.rb
+++ app/models/map.rb
@@ -65,7 +65,7 @@ class Map < Sequel::Model
       result = get_map_bounds
       self.update(view_bounds_ne: "[#{result[:maxy]}, #{result[:maxx]}]", view_bounds_sw: "[#{result[:miny]}, #{result[:minx]}]")
     rescue Sequel::DatabaseError => ex
-      Bugsnag.notify(ex)
+      CartoDB::notify_exception(ex, nil)
     end
   end
 

80bf5bfc60158bbd3e152bc74ded5783292d4212
David Arango <arango@gmail.com>
2013-04-11 19:22:09 +0200

    WIP 2.1 migrator

---
+++ lib/cartodb/generic_migrator.rb
@@ -0,0 +1,31 @@
+module CartoDB
+  class GenericMigrator
+    
+    def initialize(version)
+      @logger = ::Logger.new(STDOUT)
+      @stats  = {
+        tables_skipped:     0,
+        tables_migrated:    0,
+        tables_with_errors: {}
+      }
+      @tables_to_migrate = Table.select(:id, :database_name, :name, :user_id)
+      @version           = version.gsub(/\D/, '')
+
+      require Rails.root.join("lib/cartodb/migrator#{@version}")
+      self.class.send(:include, CartoDB::DataMigrator)
+    end #initialize
+
+    def already_migrated?(table)
+      $tables_metadata.hget(table.key, "migrated_to_#{@version}").to_s == "true"
+    end
+
+    def migrated!(table)
+      @stats[:tables_migrated] += 1
+      $tables_metadata.hset(table.key, "migrated_to_#{@version}", true)
+    end
+
+    def log msg
+      @logger.debug(msg)
+    end
+  end
+end

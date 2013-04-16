9ae84a0e96dad8a186e89be8b964e78529b9a6b1
David Arango <arango@gmail.com>
2013-04-16 19:07:12 +0200

    Fixes SqlParser spec

--- spec/lib/sql_parser_spec.rb
+++ spec/lib/sql_parser_spec.rb
@@ -3,8 +3,13 @@
 require 'spec_helper'
 
 describe CartoDB::SqlParser do
+  before do
+    @user = create_user(username: 'test', email: "client@example.com", password: "clientex")
+    @connection = @user.in_database
+  end
+
   it "should return the affected tables" do
-    sql = ""
-    CartoDB::SqlParser.new(sql).affected_tables.should == ['restaurants']
+    sql = "select * from cdb_tablemetadata;select cartodb_id from unexisting_table;selecterror;select 1;select * from spatial_ref_sys"
+    CartoDB::SqlParser.new(sql, connection: @connection).affected_tables.should =~ ["cdb_tablemetadata", "spatial_ref_sys"]
   end
 end

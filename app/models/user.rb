6c74b68ecf6f6ef603c64865e4d0cb60b1cf59dc
Luis Bosque <luisico@gmail.com>
2013-04-24 13:26:59 +0200

    Load functions in alphabetical order
    Also prefixed 00- to CDB_UserTables since it's a dependency to other
    functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -550,7 +550,7 @@ class User < Sequel::Model
     in_database(as: :superuser) do |user_database|
       user_database.transaction do
         glob = Rails.root.join('lib/sql/*.sql')
-        Dir.glob(glob).each do |f|
+        Dir.glob(glob).sort.each do |f|
           CartoDB::Logger.info "Loading CartoDB SQL function #{File.basename(f)} into #{database_name}"
           @sql = File.new(f).read
           user_database.run(@sql)

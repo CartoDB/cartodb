a94ca76dbd72351534bbea159b111b8006342d01
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-25 13:24:03 +0200

    Stub for Visualization::NameGenerator

---
+++ app/models/visualization/name_generator.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+module CartoDB
+  module Visualization
+    class NameGenerator
+      def initialize(user, candidate)
+      end #initialize
+
+      def generate
+        ''
+      end #generate
+    end # NameGenerator
+  end # Visualization
+end # CartoDB
+

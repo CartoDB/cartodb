6b834e2e8682d590309d38ae6e3e564878ca8048
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-25 18:42:04 +0200

    Implement Visualization::NameGenerator

--- app/models/visualization/name_generator.rb
+++ app/models/visualization/name_generator.rb
@@ -3,13 +3,33 @@
 module CartoDB
   module Visualization
     class NameGenerator
-      def initialize(user, candidate)
+      PATTERN = 'Untitled visualization'
+
+      def initialize(user, checker=NameChecker.new)
+        @user     = user
+        @checker  = checker
       end #initialize
 
-      def generate
-        ''
-      end #generate
+      def name(candidate=PATTERN, iteration=0)
+        return candidate if checker.available?(user, candidate)
+
+        new_candidate = "#{candidate} #{iteration}"
+        name(new_candidate, iteration + 1)
+      end #name
+
+      private
+
+      attr_reader :checker, :user, :candidate
     end # NameGenerator
+
+    def NameChecker
+      def available?(candidate)
+        db[:visualizations]
+          .select(:name)
+          .where(map_id: user.maps.map(&:id))
+          .include?(candidate)
+      end #available?
+    end #NameChecker
   end # Visualization
 end # CartoDB
 

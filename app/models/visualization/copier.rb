8ba7f5b03a89dad9d13c81bb274d7b0c6747d019
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-25 11:46:53 +0200

    wip - preliminary copier 1

---
+++ app/models/visualization/copier.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+require 'ostruct'
+require_relative './name_generator'
+require_relative '../map/copier'
+
+module CartoDB
+  module Visualization
+    class Copier
+      def initialize(user, visualization, name=nil)
+        @user           = user
+        @visualization  = visualization
+        @name           = name
+      end #initialize
+
+      def copy
+        Member.new(
+          name:         new_name,
+          tags:         visualization.tags,
+          description:  visualization.description,
+          type:         'derived',
+          map_id:       map_copy.id
+        )
+      end #copy
+
+      private
+
+      attr_reader :visualization, :user, :name
+
+      def map_copy
+        @map_copy ||= Map::Copier.new(visualization.map).copy
+      end #map
+
+      def new_name
+        @new_name ||= NameGenerator.new(user, name || visualization.name).generate
+      end #name
+    end # Copier
+  end # Visualization
+end # CartoDB
+

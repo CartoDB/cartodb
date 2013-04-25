779893bfbe603c0c3572779ab47ea90cf3ea53b4
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-25 13:23:44 +0200

    Stub for Map::Copier

---
+++ app/models/map/copier.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+module CartoDB
+  module Map
+    class Copier
+      def initialize(map)
+      end #initialize
+
+      def copy
+        OpenStruct.new
+      end #copy
+    end # Copier
+  end # Map
+end # CartoDB
+

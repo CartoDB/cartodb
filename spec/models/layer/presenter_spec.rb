97182837f9a5a2225d827a92212f3444be4f7fe2
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-26 10:55:46 +0200

    Return wrapped layer query in the "query" attribure in the JSON representation

---
+++ spec/models/layer/presenter_spec.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+require 'rspec'
+require 'ostruct'
+require_relative '../../../app/models/layer/presenter'
+
+include CartoDB
+
+describe Layer::Presenter do
+  describe '#to_poro' do
+    it 'wraps the sql if query_wrapper available' do
+      layer = OpenStruct.new(
+        public_values:  {}, 
+        kind:           'carto',
+        options: {
+          'query'         => '',
+          'tile_style'    => '',
+          'interactivity' => '',
+          'table_name'    => 'bogus_table',
+          'query_wrapper' => 'bogus template <%= sql %>'
+        }
+      )
+
+      poro = Layer::Presenter.new(layer).to_poro
+      poro.fetch(:options).fetch(:query)
+        .should == 'bogus template select * from bogus_table'
+
+      layer = OpenStruct.new(
+        public_values:  {}, 
+        kind:           'carto',
+        options: {
+          'query'         => 'select the_geom from bogus_table',
+          'tile_style'    => '',
+          'interactivity' => '',
+          'table_name'    => 'bogus_table',
+          'query_wrapper' => 'bogus template <%= sql %>'
+        }
+      )
+
+      poro = Layer::Presenter.new(layer).to_poro
+      poro.fetch(:options).fetch(:query)
+        .should == 'bogus template select the_geom from bogus_table'
+    end
+  end
+end # Layer::Presenter

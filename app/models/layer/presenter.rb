c9f69a392f5b38037b73d69bafb2bfcff5fcf99e
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-04-30 07:38:28 +0000

    Return old tiler options in VizJSON v1

--- app/models/layer/presenter.rb
+++ app/models/layer/presenter.rb
@@ -30,8 +30,11 @@ module CartoDB
         representation  = to_poro
         options         = representation.fetch(:options, {})
 
-        options.store(:tile_style, options.delete(:cartocss))
-        representation.store(:options, options)
+        options.store(:tile_style,      options.delete(:cartocss))
+        options.store('tile_host',      options.delete('tiler_domain'))
+        options.store('tile_port',      options.delete('tiler_port'))
+        options.store('tile_protocol',  options.delete('tiler_protocol'))
+        representation.store(:options,  options)
 
         representation
       end #to_vizjson_v1

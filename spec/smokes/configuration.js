68954ee1b0d3a5358f110e7f06d34c99e65ef405
Alejandro Martínez <alejandro@vizzuality.com>
2013-05-13 10:39:55 +0200

    comment configuration fields

--- spec/smokes/configuration.js
+++ spec/smokes/configuration.js
@@ -1,12 +1,12 @@
 var configuration = {}
 var options       =  casper.cli.options
 
-configuration.LOGIN_URL     = options['cartodb_login_url']
-configuration.BASE_URL      = options['cartodb_base_url']
-configuration.HOST          = options['cartodb_host']
-configuration.SUBDOMAIN     = options['cartodb_subdomain']
-configuration.API_KEY       = options['cartodb_api_key']
-configuration.USER_EMAIL    = options['cartodb_user_email']
-configuration.USER_PASSWORD = options['cartodb_user_password']
+configuration.LOGIN_URL     = options['cartodb_login_url']			// http://testuser.localhost.lan:3000
+configuration.BASE_URL      = options['cartodb_base_url']			// http://testuser.localhost.lan:3000
+configuration.HOST          = options['cartodb_host']				// testuser.localhost.lan:3000
+configuration.SUBDOMAIN     = options['cartodb_subdomain']			// testuser
+configuration.API_KEY       = options['cartodb_api_key']			// API key
+configuration.USER_EMAIL    = options['cartodb_user_email']			// email
+configuration.USER_PASSWORD = options['cartodb_user_password']		// password
 module.exports = configuration
 

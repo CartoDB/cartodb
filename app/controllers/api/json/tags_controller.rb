4231db6fe77fa771be2c300e4f67a434982ef31e
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-05-16 12:54:46 +0200

    Allow to filter tag counters by visualization type

--- app/controllers/api/json/tags_controller.rb
+++ app/controllers/api/json/tags_controller.rb
@@ -3,26 +3,44 @@
 class Api::Json::TagsController < Api::ApplicationController
   ssl_required :index
 
+  DEFAULT_LIMIT = 500
+
   def index
-    limit = (params.fetch(:limit, 500) || 500).to_i
-    render_jsonp(tag_counts_for(current_user, limit))
+    render_jsonp(tag_counts_for(current_user, params))
   end
 
-  def tag_counts_for(current_user, limit=5)
-    map_ids = current_user.maps.map(&:id).join(',')
+  private
 
+  def tag_counts_for(user, params)
     Tag.fetch(%Q{
-      WITH tags as (
-        SELECT unnest(tags) as name
-        FROM visualizations
-        WHERE map_id IN (#{map_ids})
-        LIMIT #{limit}
-      )
-      SELECT name, count(*) as count
-      FROM tags
-      GROUP BY name
-      ORDER BY count(*)
-    }).all.map(&:values)
+        WITH tags as (
+          SELECT unnest(tags) as name
+          FROM visualizations
+          WHERE map_id IN ?
+          AND type IN ?
+          LIMIT ?
+        )
+        SELECT name, count(*) as count
+        FROM tags
+        GROUP BY name
+        ORDER BY count(*)
+      }, map_ids_for(user), types_from(params), limit_from(params)
+    ).all.map(&:values)
   end # tag_counts
+
+  def map_ids_for(user)
+    user.maps.map(&:id)
+  end #map_ids
+
+  def types_from(params={})
+    default = %w{ table derived }
+    type    = params.fetch(:type, nil)
+    return default if type.nil? || type.empty?
+    [type]
+  end #type_filter_from
+
+  def limit_from(params={})
+    (params.fetch(:limit, DEFAULT_LIMIT) || DEFAULT_LIMIT).to_i
+  end #limit_from
 end
 

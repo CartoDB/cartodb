1c29e3aca9b4d87594022bb320f2ac7525694190
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-06-04 19:48:58 +0200

    Raise an error when trying to convert a column with timestamp strings not supported by PG to date

---
+++ spec/models/table/column_typecaster_spec.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+require 'rspec'
+require_relative '../../spec_helper'
+
+describe CartoDB::ColumnTypecaster do
+  before do
+    quota_in_bytes  = 524288000
+    table_quota     = 500
+    @user           = create_user(
+                        quota_in_bytes: quota_in_bytes,
+                        table_quota:    table_quota
+                      )
+  end
+
+  it 'raises NonConvertibleData when trying to cast 
+  a non-supported time format to date' do
+    @user.in_database { |database| @db = database }
+
+    table_name  = "test_#{rand(999)}"
+    dataset     = @db[table_name.to_sym]
+    time_text   = 'Mon Oct 13 1997 15:32:18 GMT+0200 (CEST)'
+
+    @db.create_table?(table_name) do
+      String  :time_with_timezone
+    end
+
+    5.times { dataset.insert(time_with_timezone: time_text) }
+
+    typecaster  = CartoDB::ColumnTypecaster.new(
+      user_database:  @db,
+      table_name:     table_name,
+      column_name:    :time_with_timezone,
+      new_type:       'date'
+    )
+
+    expect { typecaster.run }.to raise_error CartoDB::NonConvertibleData
+    dataset.first.fetch(:time_with_timezone).should_not be_nil
+
+    @db.drop_table?(table_name)
+  end
+end
+

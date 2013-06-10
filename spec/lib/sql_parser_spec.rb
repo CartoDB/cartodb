bf7d76b04b0aa7dfec77ec9eb452044f6ec41f24
David Arango <arango@gmail.com>
2013-06-10 13:40:28 +0200

    Handle CDB_QueryTables errors on SqlParser

--- spec/lib/sql_parser_spec.rb
+++ spec/lib/sql_parser_spec.rb
@@ -3,14 +3,25 @@
 require 'spec_helper'
 
 describe CartoDB::SqlParser do
-  before do
+  before(:all) do
     @user = create_user(username: 'test', email: "client@example.com", password: "clientex")
     @connection = @user.in_database
+    @sql = "select coalesce('tabname', null) from cdb_tablemetadata;select cartodb_id from unexisting_table;selecterror;select 1;select * from spatial_ref_sys"
   end
 
   it "should return the affected tables" do
-    sql = "select coalesce('tabname', null) from cdb_tablemetadata;select cartodb_id from unexisting_table;selecterror;select 1;select * from spatial_ref_sys"
-    CartoDB::SqlParser.new(sql, connection: @connection).affected_tables.should =~ ["cdb_tablemetadata", "spatial_ref_sys"]
+    CartoDB::SqlParser.new(@sql, connection: @connection)
+      .affected_tables.should =~ ["cdb_tablemetadata", "spatial_ref_sys"]
   end
 
+  it "should return the query statements" do
+    CartoDB::SqlParser.new(@sql, connection: @connection)
+      .statements.should =~ [
+        "select coalesce('tabname', null) from cdb_tablemetadata", 
+        "select cartodb_id from unexisting_table", 
+        "selecterror", 
+        "select 1", 
+        "select * from spatial_ref_sys"
+      ]
+  end
 end

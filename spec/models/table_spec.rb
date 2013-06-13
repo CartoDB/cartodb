801bbed5f7dd1ada066750be0c79392f7847eb4d
David Arango <arango@gmail.com>
2013-06-14 00:24:20 +0200

    Fixes table spec

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -142,7 +142,7 @@ describe Table do
       table.map.values.slice(:zoom, :bounding_box_sw, :bounding_box_ne, :provider).should == { zoom: 3, bounding_box_sw: "[0, 0]", bounding_box_ne: "[0, 0]", provider: 'leaflet'}
       table.map.layers.count.should == 2
       table.map.layers.map(&:kind).should == ['tiled', 'carto']
-      table.map.data_layers.first.infowindow["fields"].should == [{"name"=>"cartodb_id", "title"=>true, "position"=>1}, {"name"=>"description", "title"=>true, "position"=>2}, {"name"=>"name", "title"=>true, "position"=>3}]
+      table.map.data_layers.first.infowindow["fields"].should == []
       table.map.data_layers.first.options["table_name"].should == "epaminondas_pantulis"
     end
 

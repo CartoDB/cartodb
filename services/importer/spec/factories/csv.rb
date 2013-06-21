31de429316eaf62e6542f3b361fae3d4738295e6
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-06-21 12:08:18 +0200

    [importer] Accept header and data input in CSV factory

--- services/importer/spec/factories/csv.rb
+++ services/importer/spec/factories/csv.rb
@@ -10,11 +10,15 @@ module CartoDB
           @filepath   = "/var/tmp/#{@name}.csv"
         end #initialize
 
-        def write
+        def write(header=nil, data=nil)
+          header  ||= ["header_1", "header_2"]
+          data    ||= ["cell_#{rand(999)}", "cell_#{rand(999)}"]
+
           ::CSV.open(filepath, "wb") do |csv|
-            csv << ["header_1", "header_2"]
-            10.times { csv << ["cell_#{rand(999)}", "cell_#{rand(999)}"] }
+            csv << header
+            10.times { csv << data }
           end
+
           self
         end #write
         

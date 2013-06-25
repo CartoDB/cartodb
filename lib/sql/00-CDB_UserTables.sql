e03fed627b01c0ffc15ec8f6b576a73f14518c4f
Luis Bosque <luisico@gmail.com>
2013-06-25 17:53:22 +0200

    Changed sql private function variable name

--- lib/sql/00-CDB_UserTables.sql
+++ lib/sql/00-CDB_UserTables.sql
@@ -16,4 +16,4 @@ $$ LANGUAGE 'sql';
 
 -- This is a private function, so only the db owner need privileges
 REVOKE ALL ON FUNCTION CDB_UserTables() FROM PUBLIC;
-GRANT EXECUTE ON FUNCTION CDB_UserTables() TO ##DATABASE_USERNAME##;
+GRANT EXECUTE ON FUNCTION CDB_UserTables() TO :DATABASE_USERNAME;

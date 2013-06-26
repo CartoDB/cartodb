7084e1440f84381248c0727dd412a8b52d7df884
javi <qualopec@gmail.com>
2013-06-26 17:40:31 +0200

    fixed refresh time

--- src/geo/layer_definition.js
+++ src/geo/layer_definition.js
@@ -177,7 +177,7 @@ LayerDefinition.prototype = {
     clearTimeout(this._refreshTimer);
     this._refreshTimer = setTimeout(function() {
       self.invalidate();
-    }, this.options.refreshTime || (10*1000));
+    }, this.options.refreshTime || (60*5*1000));
 
     // check request queue
     if(this._queue.length) {

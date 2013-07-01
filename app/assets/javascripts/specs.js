f7df413c231fb64ec9fb728808a13af3a6cb6e6d
xavijam <xavijam@gmail.com>
2013-07-01 16:04:53 +0200

    new jasmine version added

--- app/assets/javascripts/specs.js
+++ app/assets/javascripts/specs.js
@@ -1,5 +1,5 @@
-//= require ../../../lib/assets/test/lib/jasmine-1.2.0/jasmine
-//= require ../../../lib/assets/test/lib/jasmine-1.2.0/jasmine-html
+//= require ../../../lib/assets/test/lib/jasmine-1.3.1/jasmine
+//= require ../../../lib/assets/test/lib/jasmine-1.3.1/jasmine-html
 //= require ../../../lib/assets/test/lib/jasmine.jquery
 //= require ../../../lib/assets/test/lib/sinon-1.3.4
 //= require ../../../lib/assets/test/spec/SpecHelper

3ccfa2b2221d780966a4b8c6ad64c06e354ea9db
Javier Arce <javierarce@gmail.com>
2013-07-16 16:41:48 +0200

    Adds legends

--- src/geo/ui/legend.js
+++ src/geo/ui/legend.js
@@ -1,4 +1,11 @@
-cdb.geo.ui.LegendItemModel = cdb.core.Model.extend({ });
+cdb.geo.ui.LegendItemModel = cdb.core.Model.extend({
+
+  defaults: {
+    name: "Untitled",
+    value: ""
+  }
+
+});
 
 cdb.geo.ui.LegendItems = Backbone.Collection.extend({
   model: cdb.geo.ui.LegendItemModel
@@ -11,14 +18,134 @@ cdb.geo.ui.LegendItem = cdb.core.View.extend({
   initialize: function() {
 
     _.bindAll(this, "render");
-    this.template = cdb.templates.getTemplate('templates/map/legend/item');
+
+    this.template = this.options.template ? _.template(this.options.template) : cdb.templates.getTemplate('geo/legend');
 
   },
 
   render: function() {
 
     this.$el.html(this.template(this.model.toJSON()));
+
     return this.$el;
+  }
+
+});
+
+
+/*
+ * ChoroplethLegend
+ *
+ * */
+cdb.geo.ui.ChoroplethLegend = cdb.core.View.extend({
+
+  tagName: "ul",
+  initialize: function() {
+
+    this.items = this.options.items;
+    this.template = _.template('<li><%= min %></li><li class="graph"></li><li><%= max %></li>');
+    this.model = new cdb.core.Model();
+
+  },
+
+  _renderGraph: function() {
+
+  },
+
+  render: function() {
+
+    var min = this.items.at(0);
+    var max = this.items.at(this.items.length - 1);
+
+    this.model.set({ min: min.get("name"), max: max.get("name") });
+    this.$el.html(this.template(this.model.toJSON()));
+
+    this._renderGraph();
+
+    return this;
+
+  }
+
+});
+
+
+/*
+ * BubbleLegend
+ *
+ * */
+cdb.geo.ui.BubbleLegend = cdb.core.View.extend({
+
+  tagName: "ul",
+
+  initialize: function() {
+
+    this.items = this.options.items;
+    this.template = _.template('<li><%= min %></li><li class="graph"></li><li><%= max %></li>');
+    this.model = new cdb.core.Model();
+  },
+
+  _renderGraph: function() {
+
+  },
+
+  render: function() {
+
+    var min = this.items.at(0);
+    var max = this.items.at(this.items.length - 1);
+
+    this.model.set({ min: min.get("name"), max: max.get("name") });
+    this.$el.html(this.template(this.model.toJSON()));
+
+    this._renderGraph();
+
+    return this;
+
+  }
+
+});
+
+/*
+ * CustomLegend
+ *
+ * */
+cdb.geo.ui.CustomLegend = cdb.core.View.extend({
+
+  tagName: "ul",
+
+  initialize: function() {
+
+    this.items = this.options.items;
+    this.template = _.template("");
+    this.model = new cdb.core.Model({
+      type: "custom"
+    });
+
+  },
+
+  _renderItems: function() {
+
+    var self = this;
+
+    this.items.each(function(item) {
+
+      view = new cdb.geo.ui.LegendItem({
+        model: item,
+        template: '<div class="bullet" style="background:<%= value %>"></div><%= name %>'
+      });
+
+      self.$el.append(view.render());
+
+    });
+
+  },
+
+  render: function() {
+
+    this.$el.html(this.template(this.model.toJSON()));
+
+    this._renderItems();
+
+    return this;
 
   }
 
@@ -26,27 +153,70 @@ cdb.geo.ui.LegendItem = cdb.core.View.extend({
 
 cdb.geo.ui.Legend = cdb.core.View.extend({
 
-  id: "legend",
+  className: "cartodb-legend",
 
   default_options: {
 
   },
 
+  _updateLegendType: function() {
+    var self = this;
+
+    var legend_name = this._capitalize(this.model.get("type")) + "Legend";
+
+    if (!cdb.geo.ui[legend_name]) return;
+
+    if (this.view) this.view.clean();
+
+      this.view = new cdb.geo.ui[legend_name] ({
+        items: self.items
+      });
+
+    // Sets the type as the element class for styling
+    this.$el.removeClass(this.model.previous("type"));
+    this.$el.addClass(this.model.get("type"));
+
+    this.render();
+
+  },
+
   initialize: function() {
-
-    this.map = this.model;
-
-    this.add_related_model(this.model);
 
     _.bindAll(this, "render", "show", "hide");
 
     _.defaults(this.options, this.default_options);
 
-    if (this.collection) {
-      this.model.collection = this.collection;
-    }
+    this.map = this.options.map;
 
-    this.template = this.options.template ? this.options.template : cdb.templates.getTemplate('geo/legend');
+    this._setupModel();
+    this._setupItems();
+
+    this._updateLegendType();
+
+  },
+
+  _setupModel: function() {
+
+    this.model = new cdb.core.Model({
+      type: this.options.type || "custom"
+    });
+
+    this.add_related_model(this.model);
+
+    this.model.bind("change:type", this._updateLegendType, this);
+
+  },
+
+  _setupItems: function() {
+
+    var self = this;
+
+    this.items = new cdb.geo.ui.LegendItems();
+
+    _.each(this.options.data, function(item) {
+      self.items.add(item);
+    });
+
   },
 
   show: function() {
@@ -57,22 +227,13 @@ cdb.geo.ui.Legend = cdb.core.View.extend({
     this.$el.fadeOut(250);
   },
 
+  _capitalize: function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+
   render: function() {
-    var self = this;
 
-    if (this.model != undefined) {
-      this.$el.html(this.template(this.model.toJSON()));
-    }
-
-    if (this.collection) {
-
-      this.collection.each(function(item) {
-
-        var view = new cdb.geo.ui.LegendItem({ className: item.get("className"), model: item });
-        self.$el.find("ul").append(view.render());
-
-      });
-    }
+    this.$el.append(this.view.render().$el);
 
     return this;
   }
--- test/spec/geo/legend.spec.js
+++ test/spec/geo/legend.spec.js
@@ -1,24 +1,83 @@
-
 describe("common.geo.ui.Legend", function() {
 
-  var legend;
+  var data, legend;
+
+  afterEach(function() {
+    legend.clean();
+  });
 
   beforeEach(function() {
 
+    var map = new cdb.geo.Map();
+
+    data = [
+      { name: "Category 1", value: "#f1f1f1" },
+      { name: "Category 2", value: "red" },
+      { name: "Category 3", value: "#f1f1f1" },
+      { name: "Category 4", value: "#ccc" },
+    ];
+
     legend = new cdb.geo.ui.Legend({
       template: _.template("<ul></ul>"),
-      model: new cdb.core.Model()
+      data: data,
+      map: map
     });
 
     $("body").append(legend.render().$el);
+
   });
 
-  it("should show", function() {
+  it("should have a 'custom' type set by default", function() {
+    expect(legend.model.get("type")).toEqual('custom');
+  });
+
+  it("should allow to change type", function() {
+    legend.model.set({ type: "bubble" });
+    expect(legend.model.get("type")).toEqual('bubble');
+  });
+
+  it("should have a collection", function() {
+    expect(legend.items instanceof cdb.geo.ui.LegendItems).toEqual(true);
+  });
+
+  it("should populate the collection", function() {
+    expect(legend.items.length).toEqual(4);
+
+    for (var i = 0; i < data.length; i++) {
+      expect(legend.items.at(i).get("name")).toEqual(data[i].name);
+    }
+
+  });
+
+  it("should set the type of the legend in the element", function() {
+    legend.model.set({ type: "bubble" });
+    legend.render();
+    expect(legend.$el.hasClass("bubble")).toEqual(true);
+  });
+
+  it("should create the specific legend based on the type", function() {
+    expect(legend.view instanceof cdb.geo.ui.CustomLegend).toEqual(true);
+    expect(legend.$el.hasClass("custom")).toEqual(true);
+
+    legend.model.set({ type: "bubble" });
+    expect(legend.view instanceof cdb.geo.ui.BubbleLegend).toEqual(true);
+    expect(legend.$el.hasClass("bubble")).toEqual(true);
+    expect(legend.$el.hasClass("custom")).toEqual(false);
+  });
+
+  it("shouldn't create the legend if the type is unknown", function() {
+    legend.model.set({ type: "the_legend_of_santana" });
+    expect(legend.view instanceof cdb.geo.ui.CustomLegend).toEqual(true);
+    expect(legend.$el.hasClass("custom")).toEqual(true);
+  });
+
+  it("should show the legend", function() {
     legend.show();
     expect(legend.$el.css('display')).toEqual('block');
   });
 
-  it("should hide", function() {
+  it("should hide the legend", function() {
+
     legend.show();
     legend.hide();
 
@@ -27,6 +86,7 @@ describe("common.geo.ui.Legend", function() {
     runs(function () {
       expect(legend.$el.css('display')).toEqual('none');
     });
+
   });
 
 });

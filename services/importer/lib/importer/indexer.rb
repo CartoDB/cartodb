05d1467fe48cfdceca4022d56743aab7289a39b7
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-07-19 12:46:42 +0200

    [importer] Set schema at initialization time for Indexer

--- services/importer/lib/importer/indexer.rb
+++ services/importer/lib/importer/indexer.rb
@@ -3,28 +3,29 @@
 module CartoDB
   module Importer2
     class Indexer
-      SCHEMA = 'importer'
+      DEFAULT_SCHEMA = 'importer'
 
-      def initialize(db)
-        @db = db
+      def initialize(db, schema=DEFAULT_SCHEMA)
+        @db     = db
+        @schema = schema
       end #initialize
 
       def add(table_name, index_name=nil)
         index_name ||= table_name
         db.run(%Q{
           CREATE INDEX "#{index_name}_the_geom_gist"
-          ON "#{SCHEMA}"."#{table_name}"
+          ON "#{schema}"."#{table_name}"
           USING GIST (the_geom)
         })
       end #add
 
       def drop(index_name)
-        db.run(%Q{DROP INDEX IF EXISTS "importer"."#{index_name}"})
+        db.run(%Q{DROP INDEX IF EXISTS "#{schema}"."#{index_name}"})
       end #drop
 
       private
 
-      attr_reader :db
+      attr_reader :db, :schema
     end # Indexer
   end # Importer2
 end # CartoDB

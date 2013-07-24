2d746ac862268553d5ac77bd48111ac06498d805
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-07-24 18:26:25 +0200

    Do not raise if CartoDB::Metrics can't connect to Mixpanel

--- lib/cartodb/metrics.rb
+++ lib/cartodb/metrics.rb
@@ -3,10 +3,17 @@ require 'mixpanel'
 module CartoDB
   class Metrics
     def self.event(*args)
-      if Cartodb.config[:mixpanel].present?
-        tracker = Mixpanel::Tracker.new(Cartodb.config[:mixpanel]['token'])
-        tracker.send(:track, args)
-      end
+      return self unless Cartodb.config[:mixpanel].present?
+      token = Cartodb.config[:mixpanel]['token']
+      Mixpanel::Tracker.new(token).send(:track, args)
+    rescue => exception
+      Rollbar.report_message(
+        "Failed to send metric to Mixpanel",
+        "error",
+        error_info: args.join('-')
+      )
+      self
     end
   end
 end
+

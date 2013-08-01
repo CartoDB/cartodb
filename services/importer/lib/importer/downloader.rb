830803d483cbe3a8296f9da519a57fd1c27169c9
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-08-01 12:57:21 +0200

    CDB-47 #resolve [importer] Support GitHub GeoJSON URLs

--- services/importer/lib/importer/downloader.rb
+++ services/importer/lib/importer/downloader.rb
@@ -5,6 +5,7 @@ require_relative './source_file'
 require_relative '../../../data-repository/filesystem/local'
 require_relative './url_translator/osm'
 require_relative './url_translator/fusion_tables'
+require_relative './url_translator/github'
 
 module CartoDB
   module Importer2
@@ -13,7 +14,11 @@ module CartoDB
       DEFAULT_FILENAME        = 'importer'
       CONTENT_DISPOSITION_RE  = %r{attachment; filename=\"(.*)\"}
       URL_RE                  = %r{://}
-      URL_TRANSLATORS         = [UrlTranslator::OSM, UrlTranslator::FusionTables]
+      URL_TRANSLATORS         = [
+                                  UrlTranslator::OSM,
+                                  UrlTranslator::FusionTables,
+                                  UrlTranslator::GitHub
+                                ]
 
       def initialize(url, seed=nil, repository=nil)
         self.url          = url
---
+++ services/importer/lib/importer/url_translator/github.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+require 'rack'
+
+module CartoDB
+  module Importer2
+    module UrlTranslator 
+      class GitHub
+        URL_REGEX               = %r{github.com/.*[blob|raw].*}
+        TRANSLATED_URL_REGEX    = %r{raw}
+
+        def translate(url)
+          return url if !supported?(url) || translated?(url) 
+          return url.gsub(%r{/blob/}, '/raw/')
+        end #translate
+
+        def supported?(url)
+          !!(url =~ URL_REGEX)
+        end #supported?
+
+        def translated?(url)
+          !!(url =~ TRANSLATED_URL_REGEX)
+        end #translated?
+      end #GitHub
+    end #UrlTranslator
+  end # Importer2
+end # CartoDB
+

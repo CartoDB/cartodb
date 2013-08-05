7edbb70fca7b890812faa380ec9f10b3c094178b
David Arango <arango@gmail.com>
2013-08-05 15:43:42 +0200

    Prevent errors caused by Sequel dirty attributes plugin

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,7 @@ class User < Sequel::Model
 
   def after_save
     super
-    changes = self.previous_changes.keys
+    changes = (self.previous_changes.present? ? self.previous_changes.keys : [])
     set_statement_timeouts if changes.include?(:user_timeout) || changes.include?(:database_timeout)
   end
 

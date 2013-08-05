0fbed872582fcdc1cfed02d4e7003df1fa5f5811
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-08-05 12:05:19 +0200

    CDB-54 #comment Implement CsvNormalizer#normalize

--- services/importer/lib/importer/csv_normalizer.rb
+++ services/importer/lib/importer/csv_normalizer.rb
@@ -1,29 +1,63 @@
 # encoding: utf-8
 require 'csv'
 require 'charlock_holmes'
+require 'tempfile'
+require 'fileutils'
 
 module CartoDB
   module Importer2
     class CsvNormalizer
-      COMMON_DELIMITERS = ['","',"\"\t\"", '" "']
+      COMMON_DELIMITERS = [',', "\t", ' ']
       DEFAULT_DELIMITER = ','
+      DEFAULT_ENCODING  = "UTF-8"
 
       def initialize(filepath)
         @filepath = filepath
       end #initialize
 
-      def to_utf8
-        
-      end #to_utf8 
+      def normalize
+        return self unless File.exists?(filepath)
+        return self unless filepath =~ /\.csv/ && needs_normalization?
+        temporary_csv = CSV.open(temporary_filepath, 'w', col_sep: ',')
+        csv_options   = { external_encoding: encoding, col_sep: delimiter }
+        stream.rewind
+        ::CSV.new(stream, csv_options).each { |row| temporary_csv << (row) }
 
-      def to_comma_delimiter
-      end #to_comma_delimiter
+        temporary_csv.close
+        release
+        File.rename(temporary_filepath, filepath)
+        FileUtils.rm_rf(temporary_directory)
+        self.temporary_directory = nil
+        self
+      end #normalize
+
+      def temporary_filepath
+        File.join(temporary_directory, File.basename(filepath))
+      end #temporary_path
+
+      def needs_normalization?
+        (encoding != DEFAULT_ENCODING) || (separator != DEFAULT_DELIMITER)
+      end #needs_normalization?
+
+      def temporary_directory
+        generate_temporary_directory unless @temporary_directory
+        @temporary_directory
+      end #temporary_directory
+
+      def generate_temporary_directory
+        tempfile                  = Tempfile.new("")
+        self.temporary_directory  = tempfile.path
+
+        tempfile.close!
+        Dir.mkdir(temporary_directory)
+        self
+      end #generate_temporary_directory
 
       def delimiter
         return DEFAULT_DELIMITER unless first_line
         occurrences = Hash[
           COMMON_DELIMITERS.map { |delimiter| 
-            [delimiter, first_line.count(delimiter)] 
+            [delimiter, first_line.squeeze(delimiter).count(delimiter)] 
           }
         ].sort {|a, b| b.last <=> a.last }
 
@@ -44,6 +78,12 @@ module CartoDB
         stream.first.encode('UTF-8', encoding)
       end #first_line
 
+      def release
+        @stream.close
+        @stream = nil
+        self
+      end #release
+
       def stream
         @stream ||= File.open(filepath, 'rb', external_encoding: encoding)
       end #stream
@@ -53,6 +93,10 @@ module CartoDB
       end #column_count
 
       attr_reader :filepath
+
+      private
+
+      attr_writer :temporary_directory
     end # CsvNormalizer
   end #Importer2
 end # CartoDB
--- services/importer/spec/unit/csv_normalizer_spec.rb
+++ services/importer/spec/unit/csv_normalizer_spec.rb
@@ -7,23 +7,26 @@ require_relative '../../lib/importer/csv_normalizer'
 include CartoDB::Importer2
 
 describe CsvNormalizer do
-  describe '#to_utf8' do
-    it 'encodes the file in UTF-8' do
-      skip
-    end
-  end #to_utf8
-
-  describe '#to_comma_delimiter' do
+  describe '#normalize' do
     it 'transforms the file using a proper comma delimiter' do
-      skip
+      fixture = tab_delimiter_factory
+      csv     = CsvNormalizer.new(fixture)
+      csv.delimiter.must_equal "\t"
+      csv.normalize
+      csv.delimiter.must_equal ','
     end
-  end #to_comma_delimiter
+  end #normalize
+
+  describe '#temporary_directory' do
+    it 'generates a temporary directory' do
+    end 
+  end #temporary_directory
 
   describe '#delimiter' do
     it 'guesses the delimiter' do
       fixture = tab_delimiter_factory
       csv     = CsvNormalizer.new(fixture)
-      csv.delimiter.must_equal "\"\t\""
+      csv.delimiter.must_equal "\t"
 
       FileUtils.rm(fixture)
     end
@@ -42,7 +45,7 @@ describe CsvNormalizer do
   def utf16le_factory
     filepath = "/var/tmp/#{Time.now.to_f}-#{rand(999)}.csv"
 
-    ::CSV.open(filepath, 'wb', encoding: 'ISO-8859-1', col_sep: "\"\t\"") do |csv|
+    ::CSV.open(filepath, 'wb', encoding: 'ISO-8859-1', col_sep: "\t") do |csv|
       csv << (["name", "description", "wadus"].map  { |s| s.encode('ISO-8859-1') })
       csv << (["bogus_1", "bogus_2", "bogus_3"].map { |s| s.encode('ISO-8859-1') })
     end
@@ -53,7 +56,7 @@ describe CsvNormalizer do
   def tab_delimiter_factory
     filepath = "/var/tmp/#{Time.now.to_f}-#{rand(999)}.csv"
 
-    ::CSV.open(filepath, 'w', col_sep: "\"\t\"") do |csv|
+    ::CSV.open(filepath, 'w', col_sep: "\t") do |csv|
       csv << ["name", "description", "wadus"]
       csv << ["bogus_1", "bogus_2", "bogus_3"]
     end

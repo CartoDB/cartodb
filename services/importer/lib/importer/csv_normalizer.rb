d3fff40979f26137801977e4c8d69d718976f4e4
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-08-13 13:01:15 +0200

    [importer] Provide a default encoding if none identified

--- services/importer/lib/importer/csv_normalizer.rb
+++ services/importer/lib/importer/csv_normalizer.rb
@@ -11,6 +11,7 @@ module CartoDB
       LINE_LIMIT        = 1000
       COMMON_DELIMITERS = [',', "\t", ' ', ';']
       DEFAULT_DELIMITER = ','
+      DEFAULT_ENCODING  = 'UTF-8'
       ACCEPTABLE_ENCODINGS = %w{ ISO-8859-1 ISO-8859-2 UTF-8 }
 
       def self.supported?(extension)
@@ -108,7 +109,8 @@ module CartoDB
         sample  = data.gets(LINE_LIMIT)
         data.close
 
-        CharlockHolmes::EncodingDetector.detect(sample).fetch(:encoding)
+        CharlockHolmes::EncodingDetector.detect(sample)
+          .fetch(:encoding, DEFAULT_ENCODING)
       end #encoding
 
       def first_line

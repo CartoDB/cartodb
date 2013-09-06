0db70d56cbfb0e7ed20816028d817ed7fc6cd408
xavijam <xavijam@gmail.com>
2013-09-06 15:15:42 +0200

    fixed problem with data in custom templates - retrocompatibility with custom_infowindow.html

--- src/geo/ui/infowindow.js
+++ src/geo/ui/infowindow.js
@@ -246,7 +246,7 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
         $jscrollpane.data().jsp && $jscrollpane.data().jsp.destroy();
       }
 
-      // Clone fields and template name
+      // Clone fields
       var fields = _.map(this.model.attributes.content.fields, function(field){
         return _.clone(field);
       });
@@ -254,6 +254,7 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
       // If a custom template is not applied, let's sanitized
       // fields for the template rendering
       if (!this.model.get('template')) {
+        // Clone template name
         var template_name = _.clone(this.model.attributes.template_name);
 
         // Sanitized them
@@ -262,16 +263,13 @@ cdb.geo.ui.Infowindow = cdb.core.View.extend({
 
       // Join plan fields values with content to work with
       // custom infowindows and CartoDB infowindows.
-      var values = {};
-      _.each(this.model.get('content').fields, function(pair) {
-        values[pair.title] = pair.value;
-      })
+      var data = this.model.get('content') ? this.model.get('content').data : {}
       var obj = _.extend({
           content: {
-            fields: fields ,
-            data: this.model.get('content').fields
+            fields: fields,
+            data: data
           }
-        },values);
+        },data);
 
       this.$el.html(this.template(obj));
 

564b82752f4dae40b419632800dd53f2c0e6002e
xavijam <xavijam@gmail.com>
2013-09-09 12:38:52 +0200

    first steps with custom autocompletes for codemirror

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,6 +1,7 @@
 //= require codemirror
 //= require show-hint
 //= require anyword-hint
+//= require custom-list-hint
 //= require select2.min
 //= require jquery.faviconNotify
 //= require rgbcolor

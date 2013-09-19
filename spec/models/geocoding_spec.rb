4dce5ee9f8bf9e7f3432f146219ee071f58aa45f
David Arango <arango@gmail.com>
2013-09-19 09:00:38 +0200

    Geocoding model now geocodes tables, yay

--- spec/models/geocoding_spec.rb
+++ spec/models/geocoding_spec.rb
@@ -18,6 +18,10 @@ describe Geocoding do
     it 'links user and geocoding' do
       geocoding.user.should eq @user
     end
+
+    it 'initializes table geocoder' do
+      geocoding.table_geocoder.should be_kind_of(CartoDB::TableGeocoder)
+    end
   end
 
   describe '#save' do
@@ -26,4 +30,31 @@ describe Geocoding do
       expect { geocoding.save }.to change(geocoding, :updated_at)
     end
   end
+
+  describe '#run!' do
+    it 'geocodes tables' do
+      fixture     = "#{Rails.root}/db/fake_data/short_clubbing.csv"
+      data_import = create_import(@user, fixture)
+      table       = data_import.table
+      @user.in_database.fetch("select count(*) from #{table.name} where the_geom is not null").first[:count].should == 0
+      geocoding = Geocoding.create(
+        user: @user,
+        table_name: table.name,
+        formatter: 'direccion, poblacion, provincia, pais'
+      )
+      geocoding.run!
+      @user.in_database.fetch("select count(*) from #{table.name} where the_geom is not null").first[:count].should == 0
+    end
+  end
+
+  def create_import(user, file_name, name=nil)
+    @data_import  = DataImport.create(
+      user_id:      @user.id,
+      data_source:  file_name,
+      table_name:   name
+    )
+    @data_import.send(:new_importer, file_name)
+    @data_import
+  end
+
 end

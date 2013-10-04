dee8533c5f2131790c982b6ecbf11be37f60e750
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-10-04 16:18:01 +0200

    CDB-648 #resolve [importer] Check actual number of tables vs. table quota after import

--- app/models/quota_checker.rb
+++ app/models/quota_checker.rb
@@ -6,9 +6,14 @@ module CartoDB
       @user = user
     end 
 
-    def over_table_quota?(number_of_new_tables)
+    def will_be_over_table_quota?(number_of_new_tables)
       return false unless user.remaining_table_quota
       number_of_new_tables.to_i > user.remaining_table_quota.to_i
+    end
+
+    def over_table_quota?
+      return false unless user.remaining_table_quota
+      user.tables.count > user.table_quota.to_i
     end
 
     private

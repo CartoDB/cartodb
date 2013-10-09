a068ad216d2da0bf781c00029893c79213a9279a
xavijam <xavijam@gmail.com>
2013-10-09 16:22:12 +0200

    sync settings added

--- app/assets/javascripts/common_data.js
+++ app/assets/javascripts/common_data.js
@@ -6,6 +6,7 @@
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_basemap
 //= require ../../../lib/assets/javascripts/cartodb/common/forms/string_field
+//= require ../../../lib/assets/javascripts/cartodb/common/forms/widgets
 //= require ../../../lib/assets/javascripts/cartodb/common/import/import_pane.js
 //= require ../../../lib/assets/javascripts/cartodb/common/import/import_info/import_info.js
 //= require_tree ../../../lib/assets/javascripts/cartodb/common

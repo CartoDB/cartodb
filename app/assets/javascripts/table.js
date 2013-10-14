11d8ae652791eacf4fa58ec127da1ea5cf52746e
Javier Arce <javierarce@gmail.com>
2013-10-14 11:34:08 +0200

    Removes duplicated library

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -31,4 +31,3 @@
 //= require ../../../lib/assets/javascripts/cartodb/table/table
 //= require_tree ../../../lib/assets/javascripts/cartodb/table/views
 //= require_tree ../../../lib/assets/javascripts/cartodb/dashboard/views
-//= require moment

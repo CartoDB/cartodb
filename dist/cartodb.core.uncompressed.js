d1e7afc9f61d3e0f4d876c8121e724d8fc5cb232
Javier Arce <javierarce@gmail.com>
2013-10-15 12:27:38 +0200

    dist files for 3.2.04

--- dist/cartodb.core.uncompressed.js
+++ dist/cartodb.core.uncompressed.js
@@ -1,5 +1,5 @@
-// version: 3.2.03
-// sha: 89af6776aff3e140c258b3807b3b0bec88c8fde3
+// version: 3.2.04
+// sha: 765503bc8f5ed94aee921acf2bb53f45ebab406d
 ;(function() {
   this.cartodb = {};
   var Backbone = {};
@@ -1141,7 +1141,7 @@ var Mustache;
 
     var cdb = root.cdb = {};
 
-    cdb.VERSION = '3.2.03';
+    cdb.VERSION = '3.2.04';
 
     cdb.CARTOCSS_VERSIONS = {
       '2.0.0': '',

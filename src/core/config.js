9221061346a88ec16a28ada88de693f6541a9072
javi <qualopec@gmail.com>
2013-10-31 16:40:08 +0100

    added correct way to generate sql api url

--- src/core/config.js
+++ src/core/config.js
@@ -20,6 +20,7 @@
 
         getSqlApiUrl: function() {
           var url = this.get('sql_api_protocol') + '://' +
+            this.get('user_name') + '.' +
             this.get('sql_api_domain') + ':' +
             this.get('sql_api_port');
           return url;

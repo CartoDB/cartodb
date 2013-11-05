3005112a5057b43d8f74fa25767b8487a9f83038
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-11-05 13:28:55 +0100

    CDB-467 #resolve Raise meaningful error when trying to create a table from query over storage quota

--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -221,6 +221,13 @@ class DataImport < Sequel::Model
     candidates =  current_user.tables.select_map(:name)
     table_name = Table.get_valid_table_name(name, name_candidates: candidates)
     current_user.in_database.run(%Q{CREATE TABLE #{table_name} AS #{query}})
+    if current_user.over_disk_quota?
+      current_user.in_database.run(%Q{DROP TABLE #{table_name}})
+      self.error_code = 8001
+      self.state      = 'failure'
+      save
+      raise CartoDB::QuotaExceeded, "More storage required"
+    end
 
     table_name
   end
@@ -331,3 +338,4 @@ class DataImport < Sequel::Model
     payload
   end
 end
+

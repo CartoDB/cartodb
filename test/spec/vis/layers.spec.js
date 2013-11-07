694c507183a4677a81f940ec32d98f3592007fcf
javi <qualopec@gmail.com>
2013-11-07 19:07:08 +0100

    fixed torque layer to use http by default

--- test/spec/vis/layers.spec.js
+++ test/spec/vis/layers.spec.js
@@ -7,39 +7,28 @@ describe('vis.layers', function() {
 
   describe('https/http', function() {
 
-    it("torque layer should rewrite to http if vis is not forced to https", function() {
+    it("torque layer should not rewrite to http if vis is not forced to https", function() {
       var layer = cdb.vis.Layers.create('torque', vis, {
         type: 'torque',
         sql_api_port: 123,
         sql_api_domain: 'cartodb.com',
         sql_api_protocol: 'https'
       });
-      expect(layer.get('sql_api_protocol')).toEqual('http');
-      expect(layer.get('sql_api_port')).toEqual(80);
-    });
-
-    it("torque layer not should rewrite to http if the domain is not cartodb.com", function() {
-      var layer = cdb.vis.Layers.create('torque', vis, {
-        type: 'torque',
-        sql_api_port: 123,
-        sql_api_domain: 'test.com',
-        sql_api_protocol: 'https'
-      });
       expect(layer.get('sql_api_protocol')).toEqual('https');
       expect(layer.get('sql_api_port')).toEqual(123);
     });
 
-    it("torque layer should not rewrite to http if vis is forced to https", function() {
+    it("torque layer should rewrite to https if the domain is not cartodb.com and is forced", function() {
       vis.https = true;
       var layer = cdb.vis.Layers.create('torque', vis, {
         type: 'torque',
         sql_api_port: 123,
-        sql_api_domain: 'test.com',
-        sql_api_protocol: 'https'
+        sql_api_domain: 'cartodb.com',
+        sql_api_protocol: 'http'
       });
       expect(layer.get('sql_api_protocol')).toEqual('https');
-      expect(layer.get('sql_api_port')).toEqual(123);
+      expect(layer.get('sql_api_port')).toEqual(443);
     });
 
-  })
+  });
 });

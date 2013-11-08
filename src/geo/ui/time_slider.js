485e4934547e760dc536783ddc5971cf89863e8b
javi <qualopec@gmail.com>
2013-11-08 17:54:21 +0100

    pause if time slider is moved

--- src/geo/ui/time_slider.js
+++ src/geo/ui/time_slider.js
@@ -75,6 +75,9 @@ cdb.geo.ui.TimeSlider = cdb.geo.ui.InfoBox.extend({
   _slide: function(e, ui) {
     this.killEvent(e);
     var step = ui.value;
+    if(this.torqueLayer.isRunning()) {
+      this.toggleTime();
+    }
     this.torqueLayer.setStep(step);
   },
    

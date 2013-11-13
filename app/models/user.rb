f80a7346da30c3e3e88aaf910171ddd79c5ea5e6
David Arango <arango@gmail.com>
2013-11-13 09:50:33 +0100

    Critical: do not update user metadata un save

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,6 @@ class User < Sequel::Model
     super
     changes = (self.previous_changes.present? ? self.previous_changes.keys : [])
     set_statement_timeouts if changes.include?(:user_timeout) || changes.include?(:database_timeout)
-    save_metadata
   end
 
   def before_destroy

4f92350d85fa47322e888d960f97c9205754cd9f
Alejandro <yosoyzenitram@gmail.com>
2013-11-14 17:50:10 +0100

    Add ssl_allowed to WMS proxy

--- app/controllers/api/json/wms_controller.rb
+++ app/controllers/api/json/wms_controller.rb
@@ -5,6 +5,7 @@ require_relative '../../../../services/wms/proxy'
 
 class Api::Json::WmsController < Api::ApplicationController
   ssl_required :index
+  ssl_allowed :proxy
 
   def proxy
     proxy = CartoDB::WMS::Proxy.new(params.fetch(:url))

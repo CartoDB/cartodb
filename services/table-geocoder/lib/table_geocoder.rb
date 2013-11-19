bb9f999927c727fb7e7dffcc785c3df138fc8a70
David Arango <arango@gmail.com>
2013-11-19 15:03:15 +0100

    Improve cleaning regex on georeferencer
    Using a double regex here, using postgres 'translate' function does
    not work fine in production.
    
    All specs green

--- services/table-geocoder/lib/table_geocoder.rb
+++ services/table-geocoder/lib/table_geocoder.rb
@@ -48,7 +48,7 @@ module CartoDB
     end
 
     def clean_formatter
-      "translate(concat(#{formatter}), '\n\r\"', '  ')"
+      "regexp_replace(regexp_replace(concat(#{formatter}), E'[\\n\\r]+', ' ', 'g'), E'\"', '', 'g')"
     end
 
     def cancel

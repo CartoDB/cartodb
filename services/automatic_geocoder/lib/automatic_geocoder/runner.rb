03a2271a9e8c3e5767228ae0e8898a3fd442865a
David Arango <arango@gmail.com>
2013-11-21 15:27:54 +0100

    WIP automatic geocoder event machine runner

---
+++ services/automatic_geocoder/lib/automatic_geocoder/runner.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+require 'eventmachine'
+
+module CartoDB
+  module AutomaticGeocoder
+    class Runner
+      TICK_TIME_IN_SECS = 10
+      RUN_FOREVER       = 0
+
+      attr_reader :ticks
+
+      def initialize(options = {})
+        @job_collection     = AutomaticGeocoding.active
+        @max_ticks          = options.fetch(:max_ticks, RUN_FOREVER)
+        @tick_time_in_secs  = options.fetch(:tick_time_in_secs, TICK_TIME_IN_SECS)
+        @ticks              = 0
+      end
+
+      def run
+        EventMachine.run do
+          EventMachine::PeriodicTimer.new(tick_time_in_secs) do
+            stop_if_max_ticks_reached
+            puts 'fetching job_collection'
+            job_collection.all.map &:enqueue
+          end
+        end
+      end
+
+      private
+
+      def stop_if_max_ticks_reached
+        return self if max_ticks == RUN_FOREVER
+
+        self.ticks = ticks + 1
+        EventMachine.stop if ticks >= max_ticks
+      end
+
+      attr_reader :max_ticks, :tick_time_in_secs, :job_collection
+      attr_writer :ticks
+    end # Runner
+  end # AutomaticGeocoder
+end # CartoDB

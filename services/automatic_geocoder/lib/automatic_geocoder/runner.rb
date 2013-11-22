0d6ce29f02ed4de702d4588d8603c1a5f70d8f82
David Arango <arango@gmail.com>
2013-11-22 19:47:56 +0100

    Fixes on automatic geocoder runner

--- services/automatic_geocoder/lib/automatic_geocoder/runner.rb
+++ services/automatic_geocoder/lib/automatic_geocoder/runner.rb
@@ -19,25 +19,13 @@ module CartoDB
 
       def run
         EventMachine.run do
-          EventMachine::PeriodicTimer.new(tick_time_in_secs) do
-            stop_if_max_ticks_reached
+          EventMachine::PeriodicTimer.new(@tick_time_in_secs) do
             puts 'fetching job_collection'
             job_collection.map &:enqueue
           end
         end
       end
 
-      private
-
-      def stop_if_max_ticks_reached
-        return self if max_ticks == RUN_FOREVER
-
-        self.ticks = ticks + 1
-        EventMachine.stop if ticks >= max_ticks
-      end
-
-      attr_reader :max_ticks, :tick_time_in_secs, :job_collection
-      attr_writer :ticks
     end # Runner
   end # AutomaticGeocoder
 end # CartoDB

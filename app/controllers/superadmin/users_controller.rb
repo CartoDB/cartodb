cc13904e9e91e511862302044c42884f6b1cc9c7
David Arango <arango@gmail.com>
2013-12-04 19:28:47 +0100

    Refactor UsersController mass assignement protection

--- app/controllers/superadmin/users_controller.rb
+++ app/controllers/superadmin/users_controller.rb
@@ -27,37 +27,19 @@ class Superadmin::UsersController < Superadmin::SuperadminController
   def create
     # BEWARE. don't get clever. This is all explicit because of mass assignment limitations
     @user = User.new
+    @user.set_only(params[:user], [
+      :username, :email, :admin, :quota_in_bytes, :table_quota, :account_type
+      :private_tables_enabled, :sync_tables_enabled, :map_view_quota, :map_view_block_price
+      :geocoding_quota, :geocoding_block_price, :period_end_date, :max_layers, :user_timeout
+      :database_timeout, :database_host, :upgraded_at, :notification
+    ])
+    @user.enabled = true
 
-    if attributes = params[:user]
-      @user.username                = attributes[:username]
-      @user.email                   = attributes[:email]
-      @user.admin                   = attributes[:admin]
-      @user.enabled                 = true
-      @user.map_enabled             = attributes[:map_enabled]              if attributes.has_key?(:map_enabled)
-      @user.quota_in_bytes          = attributes[:quota_in_bytes]           if attributes.has_key?(:quota_in_bytes)
-      @user.table_quota             = attributes[:table_quota]              if attributes.has_key?(:table_quota)
-      @user.account_type            = attributes[:account_type]             if attributes.has_key?(:account_type)
-      @user.private_tables_enabled  = attributes[:private_tables_enabled]   if attributes.has_key?(:private_tables_enabled)
-      @user.sync_tables_enabled     = attributes[:sync_tables_enabled]      if attributes.has_key?(:sync_tables_enabled)
-      @user.map_view_quota          = attributes[:map_view_quota]           if attributes.has_key?(:map_view_quota)
-      @user.map_view_block_price    = attributes[:map_view_block_price]     if attributes.has_key?(:map_view_block_price)
-      @user.geocoding_quota         = attributes[:geocoding_quota]          if attributes.has_key?(:geocoding_quota)
-      @user.geocoding_block_price   = attributes[:geocoding_block_price]    if attributes.has_key?(:geocoding_block_price)
-      @user.period_end_date         = attributes[:period_end_date]          if attributes.has_key?(:period_end_date)
-      @user.max_layers              = attributes[:max_layers]               if attributes.has_key?(:max_layers)
-      @user.user_timeout            = attributes[:user_timeout]             if attributes.has_key?(:user_timeout)
-      @user.database_timeout        = attributes[:database_timeout]         if attributes.has_key?(:database_timeout)
-      @user.database_host           = attributes[:database_host]            if attributes.has_key?(:database_host)
-      @user.upgraded_at             = attributes[:upgraded_at]              if attributes.has_key?(:upgraded_at)
-      @user.notification            = attributes[:notification]             if attributes.has_key?(:notification)
-
-      if attributes[:password].present?
-        @user.password              = attributes[:password]
-        @user.password_confirmation = attributes[:password]
-      else
-        @user.crypted_password      = attributes[:crypted_password]
-        @user.salt                  = attributes[:salt]
-      end
+    if params[:user][:password].present?
+      @user.password              = attributes[:password]
+    else
+      @user.crypted_password      = attributes[:crypted_password]
+      @user.salt                  = attributes[:salt]
     end
 
     @user.save
@@ -70,37 +52,18 @@ class Superadmin::UsersController < Superadmin::SuperadminController
   end
 
   def update
-    if attributes = params[:user]
-      @user.username                = attributes[:username]                 if attributes.has_key?(:username)
-      @user.email                   = attributes[:email]                    if attributes.has_key?(:email)
-      @user.quota_in_bytes          = attributes[:quota_in_bytes]           if attributes.has_key?(:quota_in_bytes)
-      @user.admin                   = attributes[:admin]                    if attributes.has_key?(:admin)
-      @user.enabled                 = attributes[:enabled]                  if attributes.has_key?(:enabled)
-      @user.map_enabled             = attributes[:map_enabled]              if attributes.has_key?(:map_enabled)
-      @user.quota_in_bytes          = attributes[:quota_in_bytes]           if attributes.has_key?(:quota_in_bytes)
-      @user.table_quota             = attributes[:table_quota]              if attributes.has_key?(:table_quota)
-      @user.account_type            = attributes[:account_type]             if attributes.has_key?(:account_type)
-      @user.private_tables_enabled  = attributes[:private_tables_enabled]   if attributes.has_key?(:private_tables_enabled)
-      @user.sync_tables_enabled     = attributes[:sync_tables_enabled]      if attributes.has_key?(:sync_tables_enabled)
-      @user.map_view_quota          = attributes[:map_view_quota]           if attributes.has_key?(:map_view_quota)
-      @user.map_view_block_price    = attributes[:map_view_block_price]     if attributes.has_key?(:map_view_block_price)
-      @user.geocoding_quota         = attributes[:geocoding_quota]          if attributes.has_key?(:geocoding_quota)
-      @user.geocoding_block_price   = attributes[:geocoding_block_price]    if attributes.has_key?(:geocoding_block_price)
-      @user.period_end_date         = attributes[:period_end_date]          if attributes.has_key?(:period_end_date)
-      @user.max_layers              = attributes[:max_layers]               if attributes.has_key?(:max_layers)
-      @user.user_timeout            = attributes[:user_timeout]             if attributes.has_key?(:user_timeout)
-      @user.database_timeout        = attributes[:database_timeout]         if attributes.has_key?(:database_timeout)
-      @user.database_host           = attributes[:database_host]            if attributes.has_key?(:database_host)
-      @user.upgraded_at             = attributes[:upgraded_at]              if attributes.has_key?(:upgraded_at)
-      @user.notification            = attributes[:notification]             if attributes.has_key?(:notification)
+    @user.set_only(params[:user], [
+      :username, :email, :admin, :quota_in_bytes, :table_quota, :account_type
+      :private_tables_enabled, :sync_tables_enabled, :map_view_quota, :map_view_block_price
+      :geocoding_quota, :geocoding_block_price, :period_end_date, :max_layers, :user_timeout
+      :database_timeout, :database_host, :upgraded_at, :notification
+    ])
 
-      if attributes[:password].present?
-        @user.password = attributes[:password]
-        @user.password_confirmation = attributes[:password]
-      else
-        @user.crypted_password      = attributes[:crypted_password] if attributes.has_key?(:crypted_password)
-        @user.salt                  = attributes[:salt] if attributes.has_key?(:salt)
-      end
+    if params[:user][:password].present?
+      @user.password = attributes[:password]
+    else
+      @user.crypted_password      = params[:user][:crypted_password] if params[:user].has_key?(:crypted_password)
+      @user.salt                  = params[:user][:salt] if attributes.has_key?(:salt)
     end
 
     # check for quota update

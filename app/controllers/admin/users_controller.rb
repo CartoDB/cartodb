50a5a664601c5bf59484999097ff9827ac93a8cf
David Arango <arango@gmail.com>
2013-12-05 01:33:36 +0100

    Implement create and update on organization users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,23 +1,32 @@
 # coding: utf-8
 class Admin::UsersController < ApplicationController
   ssl_required :oauth, :api_key, :regenerate_api_key
-  before_filter :login_required
+
+  before_filter :login_required, :check_permissions
   before_filter :get_user, only: [:show, :update, :destroy]
 
   def show
-    not_authorized unless current_user.organization.present? && current_user.organization_owner
   end
 
   def create
-    not_authorized unless current_user.organization.present? && current_user.organization_owner
+    @user = User.new
+    @user.set_fields(params[:user], [:username, :email, :password])
+    @user.organization = current_user.organization
+    @user.save
+    respond_with @user
   end
 
   def update
-    not_authorized unless current_user.organization.present? && current_user.organization_owner
+    @user.set_fields(params[:user], [:quota_in_bytes, :email])
+    if attributes[:password].present?
+      @user.password              = attributes[:password]
+    end
+
+    @user.save
+    respond_with @user
   end
 
   def destroy
-    not_authorized unless current_user.organization.present? && current_user.organization_owner
     @user.destroy
   end
 
@@ -26,5 +35,9 @@ class Admin::UsersController < ApplicationController
   def get_user
     @user = current_user.organization.users_dataset.where(username: params[:id]).first
     raise RecordNotFound unless @user
+  end
+
+  def check_permissions
+    not_authorized unless current_user.organization.present? && current_user.organization_owner
   end
 end

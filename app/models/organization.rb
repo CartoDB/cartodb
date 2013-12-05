2c9cb04130d796aee7693e25a71409bf7adadf15
David Arango <arango@gmail.com>
2013-12-05 13:42:07 +0100

    Add Organization#assigned_quota

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,12 @@ class Organization < Sequel::Model
   def db_size_in_bytes
     users.map(&:db_size_in_bytes).sum
   end
+
+  def assigned_quota
+    users_dataset.sum(:quota_in_bytes)
+  end
+
+  def unassigned_quota
+    quota_in_bytes - assigned_quota
+  end
 end

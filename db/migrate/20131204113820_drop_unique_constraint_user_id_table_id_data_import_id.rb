85280b7030ffbf793c0b80694bf2707d90a0dc6d
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-12-10 12:16:11 +0100

    Reactivate dropping data_import unicity index migration

--- db/migrate/20131204113820_drop_unique_constraint_user_id_table_id_data_import_id.rb
+++ db/migrate/20131204113820_drop_unique_constraint_user_id_table_id_data_import_id.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
     alter_table :data_imports do
-      #drop_index [:user_id, :table_id], :unique => true
+      drop_index [:user_id, :table_id], :unique => true
     end
   end
   
   down do
     alter_table :data_imports do
-      #add_index [:user_id, :table_id], :unique => true
+      add_index [:user_id, :table_id], :unique => true
     end
   end
 end

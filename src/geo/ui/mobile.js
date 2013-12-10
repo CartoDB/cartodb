cf07032dd103a127ed150c394a3f852e2b0f2e85
Javier Arce <javierarce@gmail.com>
2013-12-10 14:48:52 +0100

    Adds mobile widget

---
+++ src/geo/ui/mobile.js
@@ -0,0 +1,68 @@
+
+cdb.geo.ui.Mobile = cdb.core.View.extend({
+
+  className: "cartodb-mobile",
+
+  events: {
+    'click .toggle': '_toggle'
+  },
+
+  default_options: {
+    timeout: 0,
+    msg: ''
+  },
+
+  initialize: function() {
+    this.map = this.model;
+
+    _.defaults(this.options, this.default_options);
+
+    this.template = this.options.template ? this.options.template : cdb.templates.getTemplate('geo/zoom');
+  },
+
+  _toggle: function(e) {
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (this.isOpen) this.close();
+    else this.open();
+
+  },
+
+  open: function() {
+    var self = this;
+
+    this.$el.addClass("open");
+    this.isOpen = true;
+
+  },
+
+  close: function() {
+
+    var self = this;
+
+    this.$el.removeClass("open");
+    this.isOpen = false;
+
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.options));
+    var width = $(document).width() - 40;
+    this.$el.css( { width: width })
+
+    var slider = new cdb.geo.ui.TimeSlider({type: "time_slider", layer: this.options.torqueLayer, map: this.options.map, pos_margin: 0, position: "none" , width: "auto" });
+    this.$el.find(".slider").append(slider.render().$el);
+
+    var stackedLegend = new cdb.geo.ui.StackedLegend({
+       legends: this.options.legends
+    });
+
+    //this.legends = stackedLegend;
+    this.$el.find(".legends").append(stackedLegend.render().$el);
+
+    return this;
+  }
+
+});

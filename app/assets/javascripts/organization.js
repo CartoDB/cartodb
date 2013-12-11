6e9d2e273c0e2e6af492d2f3a40e3f6302a6a56b
xavijam <xavijam@gmail.com>
2013-12-11 19:02:24 +0100

    added tooltip to user progress

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -2,5 +2,6 @@
 //= require ../../../lib/assets/javascripts/cartodb/common/forms/string_field
 //= require ../../../lib/assets/javascripts/cartodb/common/search_form
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
+//= require ../../../lib/assets/javascripts/cartodb/common/utils
 //= require_tree ../../../lib/assets/javascripts/cartodb/organization
 //= require_tree ../../../lib/assets/javascripts/cartodb/common/views

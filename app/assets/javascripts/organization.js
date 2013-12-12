0f38df7c4b4ac92d8345ced3b1636f9db6a46973
xavijam <xavijam@gmail.com>
2013-12-12 16:37:49 +0100

    first steps with organization form js

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -2,7 +2,11 @@
 //= require ../../../lib/assets/javascripts/cartodb/common/forms/string_field
 //= require ../../../lib/assets/javascripts/cartodb/common/search_form
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
-//= require ../../../lib/assets/javascripts/cartodb/common/utils
-//= require ../../../lib/assets/javascripts/cartodb/common/global_click
+//= require ../../../lib/assets/javascripts/cartodb/common/dropdown_basemap
+//= require ../../../lib/assets/javascripts/cartodb/common/forms/string_field
+//= require ../../../lib/assets/javascripts/cartodb/common/forms/widgets
+//= require ../../../lib/assets/javascripts/cartodb/common/import/import_pane.js
+//= require ../../../lib/assets/javascripts/cartodb/common/import/import_info/import_info.js
+//= require_tree ../../../lib/assets/javascripts/cartodb/common
 //= require_tree ../../../lib/assets/javascripts/cartodb/organization
 //= require_tree ../../../lib/assets/javascripts/cartodb/common/views

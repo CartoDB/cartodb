809d9e1b01669aa1ee5d206d57da7a6ea651d23d
xavijam <xavijam@gmail.com>
2013-12-12 12:07:20 +0100

    new changes for organization index page

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -3,5 +3,6 @@
 //= require ../../../lib/assets/javascripts/cartodb/common/search_form
 //= require ../../../lib/assets/javascripts/cartodb/common/dropdown_menu
 //= require ../../../lib/assets/javascripts/cartodb/common/utils
+//= require ../../../lib/assets/javascripts/cartodb/common/global_click
 //= require_tree ../../../lib/assets/javascripts/cartodb/organization
 //= require_tree ../../../lib/assets/javascripts/cartodb/common/views

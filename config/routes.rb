0b8cc4efa0bdef80e457d08b3acaa1c045c23e06
xavijam <xavijam@gmail.com>
2014-01-09 13:00:43 +0100

    CDB-1296 #resolve - new oauth and api key page

--- config/routes.rb
+++ config/routes.rb
@@ -64,9 +64,9 @@ CartoDB::Application.routes.draw do
     get '/viz/:id/public'           => 'visualizations#public'
     get '/viz/:id/embed_map'        => 'visualizations#embed_map'
 
-    match '/your_apps/oauth'   => 'client_applications#oauth',   :as => :oauth_credentials
-    match '/your_apps/api_key' => 'client_applications#api_key', :as => :api_key_credentials
+    match '/your_apps' => 'client_applications#api_key', :as => :api_key_credentials
     post  '/your_apps/api_key/regenerate' => 'client_applications#regenerate_api_key', :as => :regenerate_api_key
+    delete  '/your_apps/oauth'   => 'client_applications#oauth',   :as => :oauth_credentials
 
   end
 

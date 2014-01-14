779c57ce0cc6d5704c4c1e5773bbe5286424b923
Lorenzo Planas <lorenzo.planas@gmail.com>
2014-01-14 11:00:04 +0100

    CDB-1444 #resolve Clean up old importer code

--- lib/importer/lib/cartodb-importer/constants.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module CartoDB
-  class Importer
-    SUPPORTED_FORMATS     = %W{ .csv .shp .ods .xls .xlsx .tif .tiff .kml .kmz
-    .js .json .tar .gz .tgz .osm .bz2 .geojson .gpx .json .sql }
-  end # Importer
-end # CartoDB
-

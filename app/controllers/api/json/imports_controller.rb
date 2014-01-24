05897e850d2dea963371cbbb431bb25dadc65540
David Arango <arango@gmail.com>
2014-01-24 18:14:42 +0100

    Fix

--- app/controllers/api/json/imports_controller.rb
+++ app/controllers/api/json/imports_controller.rb
@@ -52,24 +52,18 @@ class Api::Json::ImportsController < Api::ApplicationController
 
     random_token = Digest::SHA2.hexdigest("#{Time.now.utc}--#{filename.object_id.to_s}").first(20)
 
-    # Set the public_url attribute
-    remote_url = o.public_url.to_s
-
     s3_config = Cartodb.config[:importer]["s3"]
     if s3_config["access_key_id"] && s3_config["secret_access_key"]
-      Rails.logger.info("Importer AWS configuration: ======== " +
-                        s3_uploader.s3_configuration.to_s)
+      Rails.logger.info("Importer AWS configuration: ======== " + s3_config.inspect)
       AWS.config(access_key_id: Cartodb.config[:importer]["s3"]["access_key_id"], secret_access_key: Cartodb.config[:importer]["s3"]["secret_access_key"])
       s3 = AWS::S3.new
       s3_bucket = s3.buckets[s3_config["bucket_name"]]
 
       path = "#{random_token}/#{File.basename(filename)}"
       o = s3_bucket.objects[path]
-      o.write(filedata, {
-        acl: :public_read
-      })
+      o.write(filedata, { acl: :public_read })
 
-      return o.url_for(:get, expires: url_ttl).to_s
+      return o.url_for(:get, expires: s3_config["url_ttl"]).to_s
     else
       FileUtils.mkdir_p(Rails.root.join('public/uploads').join(random_token))
 

fda886f2829a984ba1e3d43c3fe7595f23e2e80d
javi <qualopec@gmail.com>
2014-02-04 18:43:15 +0100

    fixed torque layers

--- src/geo/leaflet/torque.js
+++ src/geo/leaflet/torque.js
@@ -38,7 +38,8 @@ var LeafLetTorqueLayer = L.TorqueLayer.extend({
       cartodb_logo: layerModel.get('cartodb_logo'),
       attribution: layerModel.get('attribution'),
       cdn_url: layerModel.get('no_cdn') ? null: (layerModel.get('cdn_url') || cdb.CDB_HOST),
-      cartocss: layerModel.get('cartocss') || layerModel.get('tile_style')
+      cartocss: layerModel.get('cartocss') || layerModel.get('tile_style'),
+      named_map: layerModel.get('named_map')
     });
 
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);

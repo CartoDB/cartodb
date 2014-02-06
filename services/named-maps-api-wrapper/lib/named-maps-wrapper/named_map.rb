0367c7e9f66bed6abb527a3828f2aa96948b1cad
Kartones <diego@kartones.net>
2014-02-06 18:07:28 +0100

    CDB-1305 Added tiler option to disable certificate check (for staging)

--- services/named-maps-api-wrapper/lib/named-maps-wrapper/named_map.rb
+++ services/named-maps-api-wrapper/lib/named-maps-wrapper/named_map.rb
@@ -30,7 +30,8 @@ module CartoDB
 
 				response = Typhoeus.post( parent.url + '?api_key=' + parent.api_key, {
 					headers: parent.headers,
-					body: ::JSON.dump( template_data )
+					body: ::JSON.dump( template_data ),
+          ssl_verifypeer: @parent.verify_cert
 					#,verbose: true
 					} )
 
@@ -48,7 +49,8 @@ module CartoDB
 
 				response = Typhoeus.put( url + '?api_key=' + @parent.api_key, {
 					headers: @parent.headers,
-					body: ::JSON.dump( template_data )
+					body: ::JSON.dump( template_data ),
+          ssl_verifypeer: @parent.verify_cert
 				} )
 
 				if response.code == 200
@@ -61,7 +63,7 @@ module CartoDB
 
 			# Delete existing named map
 			def delete
-				response = Typhoeus.delete( url + "?api_key=" + @parent.api_key )
+				response = Typhoeus.delete( url + "?api_key=" + @parent.api_key, { ssl_verifypeer: @parent.verify_cert } )
 				response.code == 204
 			end #delete
 

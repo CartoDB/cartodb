1df4a286919ece363cd44bfbe4c863299bb90056
Kartones <diego@kartones.net>
2014-02-12 16:58:58 +0100

    CDB-1688 More tests + upped Typhoeus version to have make sure we have the option of stubbing only some HTTP methods instead of always all

--- spec/models/named_maps_spec.rb
+++ spec/models/named_maps_spec.rb
@@ -103,6 +103,51 @@ describe CartoDB::NamedMapsWrapper::NamedMaps do
   end #private_table
 
 
+
+
+  describe 'named_map_updated_on_visualization_updated' do
+    it 'checks that when you update a visualization the named map gets updated' do
+      template_data = {
+        template: {
+          version: '0.0.1',
+          name: '@@PLACEHOLDER@@',
+          auth: {
+            method: 'open'
+          },
+          placeholders: {
+            layer0: {
+              type: "number",
+              default: 1
+            }
+          },
+          layergroup: {
+            layers: [
+              {
+                type: "cartodb",
+                options: {
+                  sql: "WITH wrapped_query AS (select * from test1) SELECT * from wrapped_query where <%= layer0 %>=1",
+                  layer_name: "test1",
+                  cartocss: "/** */",
+                  cartocss_version: "2.1.1",
+                  interactivity: "cartodb_id"
+                }
+              }
+            ]
+          }
+        }
+      }
+
+      # Stub for only GET & PUT calls performed inside this method
+      table, derived_vis, template_id = create_private_table_with_public_visualization(template_data)
+
+      derived_vis.name = "renamed_visualization"
+      # This should trigger the PUT call
+      derived_vis.store()
+
+    end
+  end #named_map_updated_on_visualization_updated
+
+
   describe 'only_torque_layer' do
     it 'checks returned viz.json given a named map with only a torque layer' do
 
@@ -143,6 +188,7 @@ describe CartoDB::NamedMapsWrapper::NamedMaps do
           tile_style: '',
         } 
       ) )
+      derived_vis.store()
 
       vizjson = get_vizjson(derived_vis)
 
@@ -294,16 +340,163 @@ describe CartoDB::NamedMapsWrapper::NamedMaps do
           tile_style: '',
         } 
       ) )
+      derived_vis.store()
 
       vizjson = get_vizjson(derived_vis)
 
-      pending
+      vizjson.include?(:id).should eq true
+      vizjson.include?(:version).should eq true
+      vizjson.include?(:title).should eq true
+      vizjson.include?(:description).should eq true
+      vizjson.include?(:layers).should eq true
+
+      vizjson[:layers].size.should eq 3
+      vizjson[:layers][0][:type].should eq 'tiled'
+
+      vizjson[:layers][1][:type].should eq 'namedmap'
+
+      vizjson[:layers][2][:type].should eq 'torque'
+
+      vizjson[:layers][1].include?(:order).should eq true
+      vizjson[:layers][1][:options][:type].should eq 'namedmap'
+      vizjson[:layers][1][:options].include?(:user_name).should eq true
+      vizjson[:layers][1][:options][:tiler_protocol].should eq 'http'
+      vizjson[:layers][1][:options][:tiler_domain].should eq 'localhost.lan'
+      vizjson[:layers][1][:options][:tiler_port].should eq '8181'
+      vizjson[:layers][1][:options].include?(:cdn_url).should eq true
+      vizjson[:layers][1][:options].include?(:named_map).should eq true
+      vizjson[:layers][1][:options][:named_map][:name].should eq template_id
+      vizjson[:layers][1][:options][:named_map][:params].size.should eq 1
+      vizjson[:layers][1][:options][:named_map][:params].include?(:layer0).should eq true
+      vizjson[:layers][1][:options][:named_map][:params][:layer0].should eq 1
+      vizjson[:layers][1][:options][:named_map][:layers].size.should eq 1
+      vizjson[:layers][1][:options][:named_map][:layers][0].deep_symbolize_keys()
+      vizjson[:layers][1][:options][:named_map][:layers][0].include?(:layer_name).should eq true
+      vizjson[:layers][1][:options][:named_map][:layers][0][:interactivity].should eq 'cartodb_id'
+
+      vizjson[:layers][2][:type].should eq 'torque'
+      vizjson[:layers][2].include?(:order).should eq true
+      vizjson[:layers][2][:options][:tiler_protocol].should eq 'http'
+      vizjson[:layers][2][:options][:tiler_domain].should eq 'localhost.lan'
+      vizjson[:layers][2][:options][:tiler_port].should eq '8181'
+      vizjson[:layers][2][:options][:sql_api_protocol].should eq 'http'
+      vizjson[:layers][2][:options][:sql_api_domain].should eq 'localhost.lan'
+      vizjson[:layers][2][:options][:sql_api_endpoint].should eq '/api/v1/sql'
+      vizjson[:layers][2][:options][:sql_api_port].should eq 8080
+      vizjson[:layers][2][:options].include?(:cdn_url).should eq true
+      vizjson[:layers][2][:options].include?(:layer_name).should eq true
+      vizjson[:layers][2][:options].include?(:table_name).should eq true
+      vizjson[:layers][2][:options].include?(:tile_style).should eq true
+      vizjson[:layers][2][:options].include?(:named_map).should eq true
+      vizjson[:layers][2][:options][:named_map][:name].should eq template_id
+
+      vizjson[:layers][2][:options].include?(:sql).should_not eq true
 
     end
   end #torque_and_normal_layers
 
 
+  describe 'two_normal_layers' do
+    it 'checks returned viz.json given a named map with 2 normal ones' do
+      template_data = {
+        template: {
+          version: '0.0.1',
+          name: '@@PLACEHOLDER@@',
+          auth: {
+            method: 'open'
+          },
+          placeholders: {
+            layer0: {
+              type: "number",
+              default: 1
+            },
+            layer1: {
+              type: "number",
+              default: 1
+            }
+          },
+          layergroup: {
+            layers: [
+              {
+                type: "cartodb",
+                options: {
+                  sql: "WITH wrapped_query AS (select * from test1) SELECT * from wrapped_query where <%= layer0 %>=1",
+                  layer_name: "test1",
+                  cartocss: "/** */",
+                  cartocss_version: "2.1.1",
+                  interactivity: "cartodb_id"
+                }
+              },
+              {
+                type: "cartodb",
+                options: {
+                  sql: "WITH wrapped_query AS (select * from test2) SELECT * from wrapped_query where <%= layer1 %>=1",
+                  layer_name: "test2",
+                  cartocss: "/** */",
+                  cartocss_version: "2.1.1",
+                  interactivity: "cartodb_id"
+                }
+              }
+            ]
+          }
+        }
+      }
+
+      table, derived_vis, template_id = create_private_table_with_public_visualization(template_data)
+
+      derived_vis.map.add_layer( Layer.create( 
+        kind: 'carto', 
+        options: { 
+          query: "select * from #{table.name}", 
+          table_name: table.name,
+          style_version: '2.1.1',
+          tile_style: '/** */',
+          interactivity: 'cartodb_id'
+        } 
+      ) )
+      derived_vis.store()
+
+      vizjson = get_vizjson(derived_vis)
+
+      vizjson.include?(:id).should eq true
+      vizjson.include?(:version).should eq true
+      vizjson.include?(:title).should eq true
+      vizjson.include?(:description).should eq true
+      vizjson.include?(:layers).should eq true
+
+
+      vizjson[:layers].size.should eq 2
+      vizjson[:layers][0][:type].should eq 'tiled'
+      vizjson[:layers][1][:type].should eq 'namedmap'
+
+      vizjson[:layers][1].include?(:order).should eq true
+      vizjson[:layers][1][:options][:type].should eq 'namedmap'
+      vizjson[:layers][1][:options].include?(:user_name).should eq true
+      vizjson[:layers][1][:options][:tiler_protocol].should eq 'http'
+      vizjson[:layers][1][:options][:tiler_domain].should eq 'localhost.lan'
+      vizjson[:layers][1][:options][:tiler_port].should eq '8181'
+      vizjson[:layers][1][:options].include?(:cdn_url).should eq true
+      vizjson[:layers][1][:options].include?(:named_map).should eq true
+      vizjson[:layers][1][:options][:named_map][:name].should eq template_id
+      vizjson[:layers][1][:options][:named_map][:params].size.should eq 2
+      vizjson[:layers][1][:options][:named_map][:params].include?(:layer0).should eq true
+      vizjson[:layers][1][:options][:named_map][:params].include?(:layer1).should eq true
+      vizjson[:layers][1][:options][:named_map][:params][:layer0].should eq 1
+      vizjson[:layers][1][:options][:named_map][:params][:layer1].should eq 1
+
+      vizjson[:layers][1][:options][:named_map][:layers].size.should eq 2
+      vizjson[:layers][1][:options][:named_map][:layers][0].deep_symbolize_keys()
+      vizjson[:layers][1][:options][:named_map][:layers][0].include?(:layer_name).should eq true
+      vizjson[:layers][1][:options][:named_map][:layers][0][:interactivity].should eq 'cartodb_id'
+      vizjson[:layers][1][:options][:named_map][:layers][1].deep_symbolize_keys()
+      vizjson[:layers][1][:options][:named_map][:layers][1].include?(:layer_name).should eq true
+      vizjson[:layers][1][:options][:named_map][:layers][1][:interactivity].should eq 'cartodb_id'
+    end
+  end #two_normal_layers
+
+
   private
+
 
   # To ease testing, convert everything to symbols
   def get_vizjson(visualization)
@@ -327,6 +520,7 @@ describe CartoDB::NamedMapsWrapper::NamedMaps do
             .and_return(
               Typhoeus::Response.new(code: 404, body: "")
             )
+    # Stub all petitions, not just GET
     Typhoeus.stub( "http://#{@user.username}.localhost.lan:8181/tiles/template?api_key=#{@user.api_key}" )
             .and_return(
               Typhoeus::Response.new( code: 200, body: JSON::dump( template_id: template_id ) )
@@ -340,10 +534,21 @@ describe CartoDB::NamedMapsWrapper::NamedMaps do
     template_data[:template][:name] = template_id
 
     Typhoeus::Expectation.clear()
-    Typhoeus.stub( "http://#{@user.username}.localhost.lan:8181/tiles/template/#{template_id}?api_key=#{@user.api_key}" )
-                .and_return(
-                  Typhoeus::Response.new( code: 200, body: JSON::dump( template_data ) )
-                )
+    # Retrievals
+    Typhoeus.stub(
+      "http://#{@user.username}.localhost.lan:8181/tiles/template/#{template_id}?api_key=#{@user.api_key}", 
+      { method: :get} )
+            .and_return(
+              Typhoeus::Response.new( code: 200, body: JSON::dump( template_data ) )
+            )
+
+    # Updates
+    Typhoeus.stub(
+        "http://#{@user.username}.localhost.lan:8181/tiles/template/#{template_id}?api_key=#{@user.api_key}", 
+        { method: :put} )
+              .and_return(
+                Typhoeus::Response.new( code: 200, body: JSON::dump( template_data ) )
+              )
 
     return table, derived_vis, template_id
   end #create_map_with_public_visualization

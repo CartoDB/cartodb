352c71722e05b8d3266a6d945b4e45276746c31c
Javier Arce <javierarce@gmail.com>
2014-02-13 11:06:31 +0100

    Fixes bad closed tag

--- src/vis/overlays.js
+++ src/vis/overlays.js
@@ -234,7 +234,7 @@ cdb.vis.Overlay.register('share', function(data, vis) {
   var template = cdb.core.Template.compile(
     data.template || '\
       <div class="mamufas">\
-        <section class="block modal {{modal_type}}">\
+        <div class="block modal {{modal_type}}">\
           <a href="#close" class="close">x</a>\
           <div class="head">\
             <h3>Share this map</h3>\

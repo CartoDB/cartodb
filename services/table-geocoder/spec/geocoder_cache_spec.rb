1b6ea4ec36c09d735e8917ca7baf85c05e39e1e1
David Arango <arango@gmail.com>
2014-02-20 17:10:46 +0100

    CDB-1767 Several geocoder improvements
    * Geocoder cache will run in configurable batches and using the
      same limits of the geocoder
    * Increase geocoder cache batch size to 5k rows
    * Fixes on geocoder row counts

--- services/table-geocoder/spec/geocoder_cache_spec.rb
+++ services/table-geocoder/spec/geocoder_cache_spec.rb
@@ -9,12 +9,42 @@ RSpec.configure do |config|
 end
 
 describe CartoDB::GeocoderCache do
+  before do
+    conn          = CartoDB::Importer2::Factories::PGConnection.new
+    @db           = conn.connection
+    @pg_options   = conn.pg_options
+    @table_name   = "ne_10m_populated_places_simple"
+    load_sql path_to("populated_places_short.sql"), @pg_options
+  end
 
-  describe '#run' do
-    it "cacheses" do
-      cache = CartoDB::GeocoderCache.new
-      cache.resolve.should be_true
+  let(:default_params) { { table_name: @table_name, formatter: "concat(name, sov0name)", connection: @db, sql_api: { table_name: '' } } }
+
+  describe '#get_cache_results' do
+    it "runs the query in batches" do
+      cache = CartoDB::GeocoderCache.new(default_params.merge(batch_size: 5))
+      cache.expects(:run_query).times(8).returns('')
+      cache.get_cache_results
+    end
+
+    it "honors max_rows" do
+      cache = CartoDB::GeocoderCache.new(default_params.merge(
+        batch_size: 5,
+        max_rows: 16
+      ))
+      cache.expects(:run_query).times(4).returns("")
+      cache.get_cache_results
     end
   end #run
+
+  def path_to(filepath = '')
+    File.expand_path(
+      File.join(File.dirname(__FILE__), "../spec/fixtures/#{filepath}")
+    )
+  end #path_to
+
+
+  def load_sql(path, pg_options)
+    `psql -U #{pg_options[:user]} -f #{path} #{pg_options[:database]}`
+  end # create_table
 
 end # CartoDB::GeocoderCache

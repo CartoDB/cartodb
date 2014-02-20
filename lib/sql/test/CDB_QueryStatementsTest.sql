944589d04734e985f7f8ced4af2b0217dfbf5332
Sandro Santilli <strk@keybit.net>
2014-02-20 15:39:39 +0100

    Add test for multiline statements

--- lib/sql/test/CDB_QueryStatementsTest.sql
+++ lib/sql/test/CDB_QueryStatementsTest.sql
@@ -26,3 +26,10 @@ INSER INTO "my''""t" values ('''','""'';;');
 SELECT $qu;oted$ hi $qu;oted$;
 $the_param$) as statement )
 SELECT '5', row_number() over (), statement FROM q;
+
+WITH q AS ( SELECT CDB_QueryStatements($the_param$
+SELECT
+1 ; SELECT
+2
+$the_param$) as statement )
+SELECT '6', row_number() over (), statement FROM q;

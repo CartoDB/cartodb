4efb0823f05b7b3188635835c3b6d131af323bea
andrewxhill <andrewxhill@gmail.com>
2014-03-15 20:23:39 -0400

    extended cdb_latlng to accept (text) and (text, text) inputs. also handles comma as decimal point as a special case e.g. 121,39::text = 121.39::numeric

--- lib/sql/scripts-available/CDB_LatLng.sql
+++ lib/sql/scripts-available/CDB_LatLng.sql
@@ -21,3 +21,37 @@ BEGIN
 END; 
 $$ language plpgsql IMMUTABLE;
 
+
+CREATE OR REPLACE FUNCTION CDB_DecimalDegreeLiteral(deg TEXT) RETURNS numeric as $$ 
+BEGIN 
+	-- Check if comma decimal place
+	-- e.g. '141,00' = 141.00
+	IF array_length(string_to_array(deg, ','), 1) > 1 THEN
+		RETURN replace(deg, ',', '.')::numeric;
+	-- Just try the good old convert to numeric
+	ELSE
+		RETURN deg::numeric;
+	END IF;
+END; 
+$$ language plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION CDB_LatLng(lat TEXT, lng TEXT) RETURNS geometry as $$ 
+BEGIN 
+	-- TODO: Check if other text type. E.g. DDMMSS
+    RETURN CDB_LatLng(
+    	CDB_DecimalDegreeLiteral(lat), 
+    	CDB_DecimalDegreeLiteral(lng)
+    ); 
+END; 
+$$ language plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION CDB_LatLng(coords TEXT) RETURNS geometry as $$ 
+DECLARE
+BEGIN 
+	-- TODO: Check if other spaces. E.g. DD MM SS
+    RETURN CDB_LatLng(
+    	CDB_DecimalDegreeLiteral(split_part(coords, ' ', 1)), 
+    	CDB_DecimalDegreeLiteral(split_part(coords, ' ', 2))
+    ); 
+END; 
+$$ language plpgsql IMMUTABLE;

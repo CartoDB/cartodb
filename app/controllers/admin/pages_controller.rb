21357f351e479906c39e39a4073dc1607e666c22
Javier Arce <javierarce@gmail.com>
2014-03-19 15:05:56 +0100

    Adds public html

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,8 +1,10 @@
 # coding: utf-8
 
 class Admin::PagesController < ApplicationController
-  ssl_required :common_data
+  ssl_required :common_data, :public
 
   before_filter      :login_required
 
+  def public
+  end
 end

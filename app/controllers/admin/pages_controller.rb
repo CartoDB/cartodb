6319e9063d97cbfbd1854b728adceaa8210718a7
Javier Arce <javierarce@gmail.com>
2014-03-21 18:22:42 +0100

    Adds responsive stuff

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -3,8 +3,11 @@
 class Admin::PagesController < ApplicationController
   ssl_required :common_data, :public
 
-  before_filter      :login_required
+  before_filter      :login_required, :except => :public
 
   def public
+    respond_to do |format|
+      format.html { render 'public', layout: 'application_public' }
+    end
   end
 end

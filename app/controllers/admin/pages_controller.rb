2597fa04b5dfc54c3cb2f52506369e18568eb1a4
Javier Arce <javierarce@gmail.com>
2014-03-24 12:22:57 +0100

    IE fixes. Fully responsive

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -3,7 +3,8 @@
 class Admin::PagesController < ApplicationController
   ssl_required :common_data, :public
 
-  before_filter      :login_required, :except => :public
+  before_filter :login_required, :except => :public
+  skip_before_filter :browser_is_html5_compliant?, only: [:public]
 
   def public
     respond_to do |format|

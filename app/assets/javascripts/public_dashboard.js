037e61bef4c93e03d2fb4e9f817a434de0e84d14
Javier Arce <javierarce@gmail.com>
2014-04-03 15:26:41 +0200

    Fixes dashboard/logout URL builder

--- app/assets/javascripts/public_dashboard.js
+++ app/assets/javascripts/public_dashboard.js
@@ -1,12 +1,18 @@
 $(function() {
 
+  var separate = window.location.host.split('.');
+  separate.shift();
+  var host = separate.join('.');
+
   var url = "//" + window.location.host  + "/api/v1/get_authenticated_users";
 
   var onResponse = function(users, status, response) {
 
     if (response.status == 200 && users && users.length > 0) {
 
-      url = "http://" + users[0] + ".cartodb.com/dashboard";
+      var baseURL = "http://" + users[0] + "." +  host;
+
+      url = baseURL + "/dashboard";
 
       var $dashboard = $("li.dashboard");
       var $a         = $dashboard.find("a");
@@ -19,7 +25,7 @@ $(function() {
 
       var $signin = $("a.signin");
 
-      url = "http://" + users[0] + ".cartodb.com/logout";
+      url = baseURL + "/logout";
 
       $signin.text("Logout");
       $signin.attr("href", url);

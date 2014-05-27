fbc408416584897d5a930c9d25eec8a5fec7fd09
Kartones <diego@kartones.net>
2014-05-27 11:17:34 +0200

    CDB-2901 Found the problem, missing import ID on table save causes it to create a duplicate

--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -278,6 +278,7 @@ class DataImport < Sequel::Model
     table.user_id = user_id
     table.name    = new_name
     table.migrate_existing_table = imported_name
+    table.data_import_id = self.id
 
     if table.valid?
       log.append 'Table valid'

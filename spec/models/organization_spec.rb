f175c4441b15f1f7afd22ede67b805003b76a6af
Kartones <diego@kartones.net>
2014-06-13 15:29:00 +0200

    CDB-3100 Organization ensures name does not exist as username either

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 describe Organization do
 
+  before(:all) do
+    @user = create_user(:quota_in_bytes => 524288000, :table_quota => 500)
+  end
+
   describe '#add_user_to_org' do
     it 'Tests adding a user to an organization' do
       org_name = 'wadus'
       org_quota = 1234567890
       org_seats = 5
 
-      user = create_user(:quota_in_bytes => 524288000, :table_quota => 500)
-      username = user.username
+      username = @user.username
 
       organization = Organization.new
 
@@ -20,8 +23,8 @@ describe Organization do
       organization.valid?.should eq true
       organization.errors.should eq Hash.new
 
-      user.organization = organization
-      user.save
+      @user.organization = organization
+      @user.save
 
       user = User.where(username: username).first
       user.should_not be nil
@@ -33,6 +36,38 @@ describe Organization do
       user.organization.name.should eq org_name
       user.organization.quota_in_bytes.should eq org_quota
       user.organization.seats.should eq org_seats
+
+      @user.organization = nil
+      @user.save
+      organization.destroy
+    end
+  end
+
+  describe '#unique_name' do
+    it 'Tests uniqueness of name' do
+      org_name = 'wadus'
+
+      organization = Organization.new
+      organization.name = org_name
+      organization.quota_in_bytes = 123
+      organization.seats = 1
+      organization.errors
+      organization.valid?.should eq true
+
+      # Repeated username
+      organization.name = @user.username
+      organization.valid?.should eq false
+      organization.name = org_name
+      organization.save
+
+      organization2 = Organization.new
+      # Repeated name
+      organization2.name = org_name
+      organization2.quota_in_bytes = 123
+      organization2.seats = 1
+      organization2.valid?.should eq false
+
+      organization.destroy
     end
   end
 

358c02e2361ed4047605053244cbfd195a463f06
Kartones <diego@kartones.net>
2014-06-13 13:08:32 +0200

    CDB-2891 Fixed specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,35 +92,38 @@ describe User do
     end
   end
 
-  describe "organization checks" do
+  describe 'organization checks' do
     it "should not be valid if his organization doesn't have more seats" do
-      organization = FactoryGirl.create(:organization, seats: 1)
+      organization = create_org('test_org', 10.megabytes, 1)
       FactoryGirl.create(:user, organization: organization)
       user = User.new
       user.organization = organization
       user.valid?.should be_false
       user.errors.keys.should include(:organization)
+      organization.destroy
     end
 
-    it "should be valid if his organization has enough seats" do
-      organization = FactoryGirl.create(:organization, seats: 1)
+    it 'should be valid if his organization has enough seats' do
+      organization = create_org('test_org', 10.megabytes, 1)
       user = User.new
       user.organization = organization
       user.valid?
       user.errors.keys.should_not include(:organization)
+      organization.destroy
     end
     
     it "should not be valid if his organization doesn't have enough disk space" do
-      organization = FactoryGirl.create(:organization, quota_in_bytes: 10.megabytes)
+      organization = create_org('test_org', 10.megabytes, 1)
       organization.stubs(:assigned_quota).returns(10.megabytes)
       user = User.new
       user.organization = organization
       user.quota_in_bytes = 1.megabyte
       user.valid?.should be_false
       user.errors.keys.should include(:quota_in_bytes)
+      organization.destroy
     end
 
-    it "should be valid if his organization has enough disk space" do
+    it 'should be valid if his organization has enough disk space' do
       organization = FactoryGirl.create(:organization, quota_in_bytes: 10.megabytes)
       organization.stubs(:assigned_quota).returns(9.megabytes)
       user = User.new
@@ -128,6 +131,7 @@ describe User do
       user.quota_in_bytes = 1.megabyte
       user.valid?
       user.errors.keys.should_not include(:quota_in_bytes)
+      organization.destroy
     end
   end
 
@@ -727,6 +731,15 @@ describe User do
       @user.link_ghost_tables
       @user.tables.count.should_not == 0
     end
+  end
+
+  def create_org(org_name, org_quota, org_seats)
+    organization = Organization.new
+    organization.name = org_name
+    organization.quota_in_bytes = org_quota
+    organization.seats = org_seats
+    organization.save
+    organization
   end
 
 end

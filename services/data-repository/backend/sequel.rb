76e80c9c79f952447a475571f27b67cb68f24802
Kartones <diego@kartones.net>
2014-06-20 10:13:09 +0200

    CDB-2891 Removed connetion_validator from one of the connection points. will require further research

--- services/data-repository/backend/sequel.rb
+++ services/data-repository/backend/sequel.rb
@@ -13,9 +13,7 @@ module DataRepository
         @db         = db
         @relation   = relation.to_sym
         ::Sequel.extension(:pagination)
-        @db.extension(:connection_validator)
-        # TODO: Check how to get config from here
-        @db.pool.connection_validation_timeout = 900
+        ::Sequel.extension(:connection_validator)
         @db.extension :pg_array if postgres?(@db)
       end #initialize
 

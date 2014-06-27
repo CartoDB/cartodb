55ef82fbef1e7aac9fafb357eca767efb0d09149
Jose A. Rilla <jose@rilla.es>
2014-06-27 13:21:46 +0200

    CDB-2891 Added extra fields to Organization
    - Description
    - Discus shortname
    - Display name
    - Website
    - Twitter username

--- app/models/organization/organization_decorator.rb
+++ app/models/organization/organization_decorator.rb
@@ -2,24 +2,29 @@ module CartoDB
   module OrganizationDecorator
     def data(options = {})
       {
-        :id             => self.id,
-        :seats          => self.seats,
-        :quota_in_bytes => self.quota_in_bytes,
-        :created_at     => self.created_at,
-        :updated_at     => self.updated_at,
-        :name           => self.name,
-        :owner          => {
-          :id           => self.owner ? self.owner.id : nil,
-          :username     => self.owner ? self.owner.username : nil,
-          :avatar_url   => self.owner ? self.owner.avatar_url : nil
+        :created_at       => self.created_at,
+        :description      => self.description,
+        :discus_shortname => self.discus_shortname,
+        :display_name     => self.display_name,
+        :id               => self.id,
+        :name             => self.name,
+        :owner            => {
+          :id         => self.owner ? self.owner.id : nil,
+          :username   => self.owner ? self.owner.username : nil,
+          :avatar_url => self.owner ? self.owner.avatar_url : nil
         },
-        :users          => self.users.map { |u|
+        :quota_in_bytes   => self.quota_in_bytes,
+        :seats            => self.seats,
+        :twitter_username => self.twitter_username,
+        :updated_at       => self.updated_at,
+        :users            => self.users.map { |u|
           {
-            :id       => u.id,
-            :username => u.username,
+            :id         => u.id,
+            :username   => u.username,
             :avatar_url => u.avatar_url
           }
-        }
+        },
+        :website          => self.website
       }
     end
   end

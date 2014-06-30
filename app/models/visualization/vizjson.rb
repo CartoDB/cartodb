353ee88d5d60077ea5af6b4dee709e5dfea2d5ef
Javier Arce <javierarce@gmail.com>
2014-06-30 17:40:41 +0200

    Removes hardcoded overlays

--- app/models/visualization/vizjson.rb
+++ app/models/visualization/vizjson.rb
@@ -29,6 +29,7 @@ module CartoDB
           version:        VIZJSON_VERSION,
           title:          visualization.name,
           description:    visualization.description_md,
+          scrollwheel:    map.scrollwheel,
           url:            options.delete(:url),
           map_provider:   map.provider,
           bounds:         bounds_from(map),
@@ -126,26 +127,8 @@ module CartoDB
       end #overlays_for
 
       def ordered_overlays_for(visualization)
-        hardcoded_overlays + visualization.overlays.to_a
+        visualization.overlays.to_a
       end #ordered_overlays_for
-
-      def hardcoded_overlays
-        [zoom_overlay, loader_overlay]
-      end #hardcoded_overlays
-
-      def zoom_overlay
-        OpenStruct.new(
-          type: 'zoom',
-          template: '<a class="zoom_in">+</a><a class="zoom_out">-</a>'
-        )
-      end #zoom_overlay
-
-      def loader_overlay
-        OpenStruct.new(
-          type: 'loader',
-          template: '<div class="loader"></div>'
-        )
-      end #loader_overlay
 
       def default_options
         { full: true, visualization_id: visualization.id }
@@ -153,4 +136,3 @@ module CartoDB
     end # VizJSON
   end # Visualization
 end # CartoDB
-

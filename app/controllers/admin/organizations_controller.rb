f2e02b6b1cd72bddc9b9c68375b48f40cdd4b2bb
Kartones <diego@kartones.net>
2014-07-02 11:49:41 +0200

    CDB-3306 basic view skeleton in place, route and ongoing controller update logic

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,11 +1,28 @@
 # coding: utf-8
 class Admin::OrganizationsController < ApplicationController
   ssl_required :oauth, :api_key, :regenerate_api_key
-  before_filter :login_required
+  before_filter :login_required, :load_organization
 
   def show
-    @organization = current_user.organization
-    raise RecordNotFound unless @organization.present? && current_user.organization_owner?
     @users = current_user.organization.users
   end
+
+  def settings
+    @users = current_user.organization.users
+  end
+
+  def settings_update
+    attributes = params[:organization]
+    redirect_to organization_settings_path(user_domain: params[:user_domain]), flash: { success: "Updated successfully" }
+  rescue Sequel::ValidationFailed => e
+    render action: :settings
+  end
+
+  private
+
+  def load_organization
+    @organization = current_user.organization
+    raise RecordNotFound unless @organization.present? && current_user.organization_owner?
+  end
+
 end

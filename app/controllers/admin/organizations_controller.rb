1235c6c4f3d974eabf2152e63b761a50940bcc83
Kartones <diego@kartones.net>
2014-07-02 11:55:03 +0200

    CDB-3306 saving org data

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -13,6 +13,15 @@ class Admin::OrganizationsController < ApplicationController
 
   def settings_update
     attributes = params[:organization]
+
+    @organization.avatar_url = attributes[:avatar_url] if attributes[:avatar_url].present?
+    @organization.website = attributes[:website] if attributes[:website].present?
+    @organization.description = attributes[:description] if attributes[:description].present?
+    @organization.display_name = attributes[:display_name] if attributes[:display_name].present?
+    @organization.discus_shortname = attributes[:discus_shortname] if attributes[:discus_shortname].present?
+    @organization.twitter_username = attributes[:twitter_username] if attributes[:twitter_username].present?
+    @organization.save(raise_on_failure: true)
+
     redirect_to organization_settings_path(user_domain: params[:user_domain]), flash: { success: "Updated successfully" }
   rescue Sequel::ValidationFailed => e
     render action: :settings

eb6d776568e43525244b69891e3765a6c1a26a70
Kartones <diego@kartones.net>
2014-08-06 18:31:05 +0200

    CDB-3619 ongoing json2csv conversor

--- services/twitter-search/twitter-search.rb
+++ services/twitter-search/twitter-search.rb
@@ -5,3 +5,4 @@ require 'json'
 
 require_relative './lib/twitter-search/exceptions'
 require_relative './lib/twitter-search/search_api'
+require_relative './lib/twitter-search/json_to_csv_converter'

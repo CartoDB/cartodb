b6588752e5e94e136775d376a8e127a6d06f658f
Kartones <diego@kartones.net>
2014-08-28 13:01:05 +0200

    CDB-4017 specs stabilization

--- app/models/visualization/member.rb
+++ app/models/visualization/member.rb
@@ -43,6 +43,8 @@ module CartoDB
       # Upon adding new attributes modify also:
       # app/models/visualization/migrator.rb
       # services/data-repository/spec/unit/backend/sequel_spec.rb -> before do
+      # spec/models/visualization/collection_spec.rb -> random_attributes
+      # spec/models/visualization/member_spec.rb -> random_attributes
       attribute :id,                  String
       attribute :name,                String
       attribute :map_id,              String

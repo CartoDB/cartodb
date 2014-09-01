36f5e6615c6025f6db87be2ee5ef4e67f0e7c217
xavijam <xavijam@gmail.com>
2014-09-01 10:11:02 +0200

    first version of new common data working

--- app/controllers/api/json/common_data_controller.rb
+++ app/controllers/api/json/common_data_controller.rb
@@ -7,7 +7,10 @@ class Api::Json::CommonDataController < Api::ApplicationController
 
   def index
     if (Cartodb.config[:common_data].present? && Cartodb.config[:common_data]['username'] && Cartodb.config[:common_data]['api_key'])
-      url = "https://#{Cartodb.config[:common_data]['username']}.cartodb.com/api/v1/viz?page=1&per_page=500&privacy=public&type=table&api_key=#{Cartodb.config[:common_data]['api_key']}"
+      # Get all tables from the desired account in development
+      # Only public in production
+      privacy = Rails.env.development? ? '' : '&privacy=public'
+      url = "https://#{Cartodb.config[:common_data]['username']}.cartodb.com/api/v1/viz?page=1&per_page=500#{privacy}&type=table&api_key=#{Cartodb.config[:common_data]['api_key']}"
       response = Typhoeus.get(url, followlocation: true)
       raise URI::InvalidURIError unless [200, 201].include?(response.code)
       render_jsonp(response.response_body)

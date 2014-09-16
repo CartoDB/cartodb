b181c0b5fefaaa949f8e862f587bdb0a9673f34e
Kartones <diego@kartones.net>
2014-09-16 12:37:54 +0200

    CDB-3892 ongoing improvement of login to superadmin, redirections and such

--- app/controllers/superadmin/superadmin_controller.rb
+++ app/controllers/superadmin/superadmin_controller.rb
@@ -7,7 +7,6 @@ class Superadmin::SuperadminController < ActionController::Base
   def authenticate
     return true if authenticated?(CartoDB.extract_subdomain(request)) && current_user.admin
     #return true if Rails.env.development? || authenticated?(CartoDB.extract_subdomain(request)) && current_user.admin
-    debugger
     authenticate_or_request_with_http_basic do |username, password|
       username == Cartodb.config[:superadmin]["username"] && password == Cartodb.config[:superadmin]["password"]
     end

f291014266c22422dd3ea6d3304b73c122869e0a
Kartones <diego@kartones.net>
2014-09-16 16:19:31 +0200

    CDB-3844

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -815,7 +815,7 @@ describe User do
       .returns(true)
     CartoDB::Varnish.any_instance.expects(:purge)
       .with(".*#{uuid}:vizjson")
-      .times(2) #only default overlays
+      .times(2 + 5)
       .returns(true)
     Table.any_instance.expects(:delete_tile_style).returns(true)
 

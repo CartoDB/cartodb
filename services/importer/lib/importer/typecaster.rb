5eb08e3a19360820b408a912ad16b8ef41aee1b6
Kartones <diego@kartones.net>
2014-09-22 12:14:11 +0200

    CDB-4182 Started refactoring importer typecaster, on hold for now so just commiting already done stuff

--- services/importer/lib/importer/typecaster.rb
+++ services/importer/lib/importer/typecaster.rb
@@ -13,17 +13,21 @@ module CartoDB
         @table_name   = table_name
         @schema       = schema
         @date_columns = date_columns.map(&:to_sym)
-      end #initialize
+      end
 
-      # For now, only specific columns sent
       def run
-        #(date_columns + candidate_columns).uniq
-        (date_columns).uniq
-          .select { |column_name| castable?(column_name) }
-          .each { |column_name| cast(column_name) }
+        (date_columns + candidate_columns).uniq
+          .select { |column_name|
+            castable?(column_name)
+          }
+          .each { |column_name|
+            cast(column_name)
+          }
       end
 
       def candidate_columns
+        return []
+        # For now, only specific columns sent
         db.schema(table_name, reload: true, schema: schema)
           .map(&:first)
           .select { |column_name|
@@ -33,18 +37,17 @@ module CartoDB
           }
       end
 
-      def cast(column_name) 
-        job.log "Casting '#{column_name}' to timestamp with time zone"
-
-        # TODO: Use CDB_StringToDate() instead whenever generalizing typecasting
-        db.run(%Q(
-          ALTER TABLE "#{schema}"."#{table_name}"
-          ALTER COLUMN #{column_name} TYPE timestamptz
-          USING #{column_name}::timestamp
-        ))
+      def cast(column_name)
+        cast_to_timestamptz(column_name) if castable_to_date?(column_name)
       end
 
       def castable?(column_name)
+        castable_to_date?(column_name)
+      end
+
+      private
+
+      def castable_to_date?(column_name)
         # TODO: CDB_StringToDate() is faulty as by default casts discarding hour/min/secs, but for checks is enough
         !db[table_name.to_sym].with_sql(%Q(
           SELECT CDB_StringToDate(#{column_name})
@@ -55,9 +58,19 @@ module CartoDB
         false
       end
 
-      private
+      def cast_to_timestamptz(column_name)
+        job.log "Casting '#{column_name}' to timestamp with time zone"
+
+        # TODO: Use CDB_StringToDate() instead whenever generalizing typecasting
+        db.run(%Q(
+          ALTER TABLE "#{schema}"."#{table_name}"
+          ALTER COLUMN #{column_name} TYPE timestamptz
+          USING #{column_name}::timestamp
+        ))
+      end
 
       attr_reader :db, :job, :table_name, :schema, :date_columns
+
     end
   end
 end

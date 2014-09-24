0cba3c3fbd28942645768336d85e71dca4980ea6
Raul Ochoa <rochoaf@gmail.com>
2014-09-24 17:31:12 +0200

    Use sorted set for watcher instead of using KEYS

--- app/models/visualization/watcher.rb
+++ app/models/visualization/watcher.rb
@@ -6,43 +6,40 @@ module CartoDB
   module Visualization
     class Watcher
 
-      # watcher:_orgid_:_vis_id_:_user_id_
-      KEY_FORMAT = "watcher:%s:%s:%s"
+      # watcher:_orgid_:_vis_id_
+      KEY_FORMAT = "watcher:%s:%s"
 
       # @params user User
       # @params visualization CartoDB::Visualization::Member
       # @throws CartoDB::Visualization::WatcherError
-      def initialize(user, visualization, notification_ttl = nil)
+      def initialize(user, visualization)
         raise WatcherError.new('User must belong to an organization') if user.organization.nil?
         @user = user
         @visualization = visualization
 
-        default_ttl = Cartodb.config[:watcher].present? ? Cartodb.config[:watcher].try("fetch", 'ttl', 60) : 60
-        @notification_ttl = notification_ttl.nil? ? default_ttl : notification_ttl
-      end #initialize
+        @notification_ttl = Cartodb.config[:watcher].present? ? Cartodb.config[:watcher].try("fetch", 'ttl', 60) : 60
+      end
 
       # Notifies that is editing the visualization
-      # NOTE: Expiration is handled internally by redis
+      # NOTE: Expiration is handled in rake task
       def notify
-        key = KEY_FORMAT % [@user.organization.id, @visualization.id, @user.username]
-        $tables_metadata.multi do
-          $tables_metadata.set(key, @user.username)
-          $tables_metadata.expire(key, @notification_ttl)
-        end
+        $tables_metadata.zadd(key, Time.now.utc.to_i, @user.username)
       end
 
       # Returns a list of usernames currently editing the visualization
       def list
-        key = KEY_FORMAT % [@user.organization.id, @visualization.id, '*']
-        keys = $tables_metadata.keys(key)
-        if keys.empty?
-          []
-        else
-          # Cannot use MGET directly
-          $tables_metadata.multi {
-            keys.each { |k| $tables_metadata.get(k) }
-          }.flatten.compact
-        end
+        now = Time.now.utc.to_i
+        $tables_metadata.zrangebyscore(key, now - @notification_ttl, now)
+      end
+
+      def prune
+        $tables_metadata.zremrangebyscore(key, 0, Time.now.utc.to_i - @notification_ttl)
+      end
+
+      private
+
+      def key
+        KEY_FORMAT % [@user.organization.id, @visualization.id]
       end
 
     end
--- spec/models/visualization/watcher_spec.rb
+++ spec/models/visualization/watcher_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require_relative '../../spec_helper'
 require_relative '../../../app/models/visualization/watcher'
+require 'delorean'
 
 include CartoDB::Visualization
 
@@ -8,75 +9,127 @@ describe TableBlender do
 
   before(:all) do
     $tables_metadata.flushdb
+
+    vis_id = UUIDTools::UUID.timestamp_create.to_s
+    @visualization_mock = mock
+    @visualization_mock.stubs(:id).returns(vis_id)
+
+    # Organization
+    org_id = UUIDTools::UUID.timestamp_create.to_s
+    organization_mock = mock
+    organization_mock.stubs(:id).returns(org_id)
+
+    # Normal org user
+    user1_name = 'wadus1'
+    @user1_mock = mock
+    @user1_mock.stubs(:username).returns(user1_name)
+    @user1_mock.stubs(:organization).returns(organization_mock)
+
+    # Another org user
+    user2_name = 'wadus2'
+    @user2_mock = mock
+    @user2_mock.stubs(:username).returns(user2_name)
+    @user2_mock.stubs(:organization).returns(organization_mock)
+
+
+    @watcher_ttl = Cartodb.config[:watcher].present? ? Cartodb.config[:watcher].try("fetch", 'ttl', 60) : 60
   end
 
   after(:all) do
     $tables_metadata.flushdb
   end
 
+  after(:each) do
+    Delorean.back_to_the_present
+  end
+
   describe '#basic_tests' do
-    it 'tests notify and list methods' do
-      watcher_ttl = 2
-
-      org_id = UUIDTools::UUID.timestamp_create.to_s
-      organization_mock = mock
-      organization_mock.stubs(:id).returns(org_id)
-
-      # Normal user
-      user1_name = 'wadus1'
-      user1_mock = mock
-      user1_mock.stubs(:username).returns(user1_name)
-      user1_mock.stubs(:organization).returns(organization_mock)
-
-      #Another org user
-      user2_name = 'wadus2'
-      user2_mock = mock
-      user2_mock.stubs(:username).returns(user2_name)
-      user2_mock.stubs(:organization).returns(organization_mock)
-
-      vis_id = UUIDTools::UUID.timestamp_create.to_s
-      visualization_mock = mock
-      visualization_mock.stubs(:id).returns(vis_id)
-
+    it 'should fail to initialize a watcher for non organization users' do
       # Try to create a watcher from a non-org user
       expect {
         non_org_user_mock = mock
         non_org_user_mock.stubs(:organization).returns(nil)
-        Watcher.new(non_org_user_mock, visualization_mock)
+        Watcher.new(non_org_user_mock, @visualization_mock)
       }.to raise_exception WatcherError
+    end
 
-      u1_watcher = Watcher.new(user1_mock, visualization_mock, watcher_ttl)
+    it 'should report user and remove after ttl' do
+      u1_watcher = Watcher.new(@user1_mock, @visualization_mock)
       u1_watcher.nil?.should eq false
       u1_watcher.list.should eq []
 
       u1_watcher.notify
-      u1_watcher.list.should eq [user1_mock.username]
+      u1_watcher.list.should eq [@user1_mock.username]
 
-      sleep(watcher_ttl+1)
+      Delorean.jump @watcher_ttl + 1
       u1_watcher.list.should eq []
 
       u1_watcher.notify
-      u1_watcher.list.should eq [user1_mock.username]
+      u1_watcher.list.should eq [@user1_mock.username]
+    end
 
-      u2_watcher = Watcher.new(user2_mock, visualization_mock, watcher_ttl)
-      u2_watcher.nil?.should eq false
-      # All watchers must notify all users
-      u2_watcher.list.should eq [user1_mock.username]
+    it 'should report other user is in visualization' do
+      u1_watcher = Watcher.new(@user1_mock, @visualization_mock)
+      u1_watcher.notify
 
+      u2_watcher = Watcher.new(@user2_mock, @visualization_mock)
+      u2_watcher.list.should eq [@user1_mock.username]
+    end
+
+    it 'should report both users are in visualization from both watchers and removed after the ttl' do
+      u1_watcher = Watcher.new(@user1_mock, @visualization_mock)
+      u1_watcher.notify
+
+      u2_watcher = Watcher.new(@user2_mock, @visualization_mock)
       u2_watcher.notify
-      # Both watchers should be now updated
-      (u2_watcher.list - [user1_mock.username, user2_mock.username]).should eq []
-      (u1_watcher.list - [user1_mock.username, user2_mock.username]).should eq []
 
-      sleep(watcher_ttl+1)
-      # And empty
+      u1_watcher.list.should eq [@user1_mock.username, @user2_mock.username]
+      u2_watcher.list.should eq [@user1_mock.username, @user2_mock.username]
+
+      Delorean.jump @watcher_ttl + 1
       u1_watcher.list.should eq []
       u2_watcher.list.should eq []
 
       u2_watcher.notify
-      sleep(1)
-      u1_watcher.list.should eq [user2_mock.username]
-      u2_watcher.list.should eq [user2_mock.username]
+      Delorean.jump 1
+      u1_watcher.list.should eq [@user2_mock.username]
+      u2_watcher.list.should eq [@user2_mock.username]
+    end
+
+    it 'should have usernames in redis after they have `expired` and removed after pruning' do
+      u1_watcher = Watcher.new(@user1_mock, @visualization_mock)
+      u1_watcher.notify
+      u2_watcher = Watcher.new(@user2_mock, @visualization_mock)
+      u2_watcher.notify
+
+      Delorean.jump @watcher_ttl + 1
+      u1_watcher.list.should eq []
+      u2_watcher.list.should eq []
+
+      redis_key = u1_watcher.send(:key)
+      $tables_metadata.zrange(redis_key, 0, Time.now.utc.to_i).should eq [@user1_mock.username, @user2_mock.username]
+
+      u1_watcher.prune
+
+      $tables_metadata.zrange(redis_key, 0, Time.now.utc.to_i).should eq []
+    end
+
+    it 'should prune only users older than the ttl' do
+      u1_watcher = Watcher.new(@user1_mock, @visualization_mock)
+      u1_watcher.notify
+
+      Delorean.jump @watcher_ttl - 5
+
+      u2_watcher = Watcher.new(@user2_mock, @visualization_mock)
+      u2_watcher.notify
+
+      Delorean.jump @watcher_ttl - 5 + 1
+
+      u1_watcher.prune
+
+      redis_key = u1_watcher.send(:key)
+      $tables_metadata.zrange(redis_key, 0, Time.now.utc.to_i).should eq [@user2_mock.username]
+
     end
   end
 

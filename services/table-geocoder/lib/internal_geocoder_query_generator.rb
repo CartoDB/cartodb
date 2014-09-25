9b3cd72a816606b2d7bf2a47c14558c2d8f1ed20
Rafa de la Torre <rtorre@cartodb.com>
2014-09-25 14:03:17 +0200

    CDB-4232 Add search_terms_query method to InternalGeocoderQueryGenerator

--- services/table-geocoder/lib/internal_geocoder_query_generator.rb
+++ services/table-geocoder/lib/internal_geocoder_query_generator.rb
@@ -3,8 +3,22 @@
 module CartoDB
 
   class InternalGeocoderQueryGenerator
+
+    def initialize(internal_geocoder)
+      @internal_geocoder = internal_geocoder
+    end
+
     def dataservices_query_template
       'WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, {country})).*) SELECT q, null, geom, success FROM geo_function'
+    end
+
+    def search_terms_query(page)
+      %Q{
+            SELECT DISTINCT(quote_nullable(#{@internal_geocoder.column_name})) AS searchtext
+            FROM #{@internal_geocoder.qualified_table_name}
+            WHERE cartodb_georef_status IS NULL
+            LIMIT #{@internal_geocoder.batch_size} OFFSET #{page * @internal_geocoder.batch_size}
+      }
     end
   end
 
--- services/table-geocoder/spec/internal_geocoder_query_generator_spec.rb
+++ services/table-geocoder/spec/internal_geocoder_query_generator_spec.rb
@@ -4,6 +4,11 @@
 require 'rspec'
 
 require_relative '../lib/internal_geocoder_query_generator.rb'
+require_relative '../lib/internal_geocoder.rb'
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+end
 
 =begin
 The class should generate queries to be used by the InternalGeocoder depending on the inputs.
@@ -20,15 +25,32 @@ The class should generate queries to be used by the InternalGeocoder depending o
 
 describe CartoDB::InternalGeocoderQueryGenerator do
 
-  before do
-    @query_gen = CartoDB::InternalGeocoderQueryGenerator.new()
-  end
-
   describe '#dataservices_query_template' do
 
     it 'should return a query template suitable for <namedplace, point, freetext>' do
-      query = @query_gen.dataservices_query_template
+      internal_geocoder = mock
+      query_gen = CartoDB::InternalGeocoderQueryGenerator.new(internal_geocoder)
+      query = query_gen.dataservices_query_template
       query.should == 'WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, {country})).*) SELECT q, null, geom, success FROM geo_function'
+    end
+
+    it 'should replace "world" with null' do
+      pending('TBD')
+    end
+
+  end
+
+  describe '#search_terms_query' do
+    it 'should get the search terms for <namedplace, point, freetext>' do
+      internal_geocoder = mock
+      internal_geocoder.stubs('column_name').once.returns('city')
+      internal_geocoder.stubs('qualified_table_name').once.returns('"public"."untitled_table"')
+      internal_geocoder.stubs('batch_size').returns(5000)
+      query_gen = CartoDB::InternalGeocoderQueryGenerator.new(internal_geocoder)
+
+      query = query_gen.search_terms_query(0)
+      #TODO replace by squish or just copy the template
+      query.strip.gsub(/[\s\n]+/, ' ').should == 'SELECT DISTINCT(quote_nullable(city)) AS searchtext FROM "public"."untitled_table" WHERE cartodb_georef_status IS NULL LIMIT 5000 OFFSET 0'
     end
   end
 

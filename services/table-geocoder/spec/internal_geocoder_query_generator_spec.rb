c44118f3e7f0f5df527ce3b88551fdc64e1f2813
Rafa de la Torre <rtorre@cartodb.com>
2014-09-25 17:00:45 +0200

    CDB-4232 Make the test code more compact with a before(:each)

--- services/table-geocoder/spec/internal_geocoder_query_generator_spec.rb
+++ services/table-geocoder/spec/internal_geocoder_query_generator_spec.rb
@@ -1,8 +1,5 @@
 # encoding: utf-8
 
-#TODO is this needed?
-require 'rspec'
-
 require_relative '../lib/internal_geocoder_query_generator.rb'
 require_relative '../lib/internal_geocoder.rb'
 
@@ -32,24 +29,25 @@ The class should generate queries to be used by the InternalGeocoder depending o
 
 describe CartoDB::InternalGeocoderQueryGenerator do
 
+  before(:each) do
+    @internal_geocoder = mock
+    @query_generator = CartoDB::InternalGeocoderQueryGenerator.new(@internal_geocoder)
+  end
+
   describe '#dataservices_query_template' do
 
     it 'should return a query template suitable for <namedplace, point, freetext>' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('countries').once.returns('any_country')
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new(internal_geocoder)
+      @internal_geocoder.stubs('countries').once.returns('any_country')
 
-      query = query_gen.dataservices_query_template
+      query = @query_generator.dataservices_query_template
 
       query.should == 'WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, {country})).*) SELECT q, null, geom, success FROM geo_function'
     end
 
     it 'should replace "world" with null' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('countries').once.returns("'world'")
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new(internal_geocoder)
+      @internal_geocoder.stubs('countries').once.returns("'world'")
 
-      query = query_gen.dataservices_query_template
+      query = @query_generator.dataservices_query_template
 
       query.should == 'WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, null)).*) SELECT q, null, geom, success FROM geo_function'
     end
@@ -58,13 +56,11 @@ describe CartoDB::InternalGeocoderQueryGenerator do
 
   describe '#search_terms_query' do
     it 'should get the search terms for <namedplace, point, freetext>' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('column_name').once.returns('city')
-      internal_geocoder.stubs('qualified_table_name').once.returns(%Q{"public"."untitled_table"})
-      internal_geocoder.stubs('batch_size').returns(5000)
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new(internal_geocoder)
+      @internal_geocoder.stubs('column_name').once.returns('city')
+      @internal_geocoder.stubs('qualified_table_name').once.returns(%Q{"public"."untitled_table"})
+      @internal_geocoder.stubs('batch_size').returns(5000)
 
-      query = query_gen.search_terms_query(0)
+      query = @query_generator.search_terms_query(0)
 
       query.squish.should == 'SELECT DISTINCT(quote_nullable(city)) AS searchtext FROM "public"."untitled_table" WHERE cartodb_georef_status IS NULL LIMIT 5000 OFFSET 0'
     end
@@ -72,13 +68,11 @@ describe CartoDB::InternalGeocoderQueryGenerator do
 
   describe '#copy_results_to_table_query' do
     it 'should generate a suitable query to update geocoded table with temp table' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('qualified_table_name').once.returns(%Q{"public"."untitled_table"})
-      internal_geocoder.stubs('temp_table_name').once.returns('any_temp_table')
-      internal_geocoder.stubs('column_name').once.returns('any_column_name')
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
+      @internal_geocoder.stubs('qualified_table_name').once.returns(%Q{"public"."untitled_table"})
+      @internal_geocoder.stubs('temp_table_name').once.returns('any_temp_table')
+      @internal_geocoder.stubs('column_name').once.returns('any_column_name')
 
-      query = query_gen.copy_results_to_table_query
+      query = @query_generator.copy_results_to_table_query
 
       query.squish.should == %Q{
         UPDATE "public"."untitled_table" AS dest
@@ -92,13 +86,11 @@ describe CartoDB::InternalGeocoderQueryGenerator do
   describe '#input_type' do
 
     it 'should return an array characterizing the inputs for <namedplace, point, freetext>' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('kind').once.returns(:namedplace)
-      internal_geocoder.stubs('geometry_type').once.returns(:point)
-      internal_geocoder.stubs('country_column').once.returns(nil)
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
+      @internal_geocoder.stubs('kind').once.returns(:namedplace)
+      @internal_geocoder.stubs('geometry_type').once.returns(:point)
+      @internal_geocoder.stubs('country_column').once.returns(nil)
 
-      query_gen.input_type.should == {
+      @query_generator.input_type.should == {
         :kind => :namedplace,
         :geometry_type => :point,
         :country => :freetext
@@ -109,40 +101,27 @@ describe CartoDB::InternalGeocoderQueryGenerator do
 
   describe '#kind' do
     it 'should return the type of the internal geocoding: namedplace' do
-      #TODO move all these mock instantiations to somewhere else
-      internal_geocoder = mock
-      internal_geocoder.stubs('kind').once.returns(:namedplace)
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
-
-      query_gen.kind.should == :namedplace
+      @internal_geocoder.stubs('kind').once.returns(:namedplace)
+      @query_generator.kind.should == :namedplace
     end
   end
 
   describe '#geometry_type' do
     it 'should return the type of the geometry to be geocoded: polygon' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('geometry_type').once.returns(:polygon)
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
-
-      query_gen.geometry_type.should == :polygon
+      @internal_geocoder.stubs('geometry_type').once.returns(:polygon)
+      @query_generator.geometry_type.should == :polygon
     end
   end
 
   describe '#country_input_type' do
     it 'should return column if a column was passed' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('country_column').once.returns('any_column_name')
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
-
-      query_gen.country_input_type.should == :column
+      @internal_geocoder.stubs('country_column').once.returns('any_column_name')
+      @query_generator.country_input_type.should == :column
     end
 
     it 'should return column if no column was passed' do
-      internal_geocoder = mock
-      internal_geocoder.stubs('country_column').once.returns(nil)
-      query_gen = CartoDB::InternalGeocoderQueryGenerator.new internal_geocoder
-
-      query_gen.country_input_type.should == :freetext
+      @internal_geocoder.stubs('country_column').once.returns(nil)
+      @query_generator.country_input_type.should == :freetext
     end
   end
 

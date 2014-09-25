fd84bcacc3c11e67f57604b7e9e840b1ccf866cd
Rafa de la Torre <rtorre@cartodb.com>
2014-09-25 17:43:54 +0200

    CDB-4232 Some modifications to 'generalize' the QueryGenerator. Not fully working yet

--- services/table-geocoder/lib/internal_geocoder.rb
+++ services/table-geocoder/lib/internal_geocoder.rb
@@ -8,7 +8,7 @@ module CartoDB
 
     attr_reader   :connection, :temp_table_name, :sql_api, :geocoding_results,
                   :working_dir, :remote_id, :state, :processed_rows, :country_column,
-                  :qualified_table_name, :batch_size, :countries
+                  :qualified_table_name, :batch_size, :countries, :kind, :geometry_type
 
     attr_accessor :table_schema, :table_name, :column_name
 
--- services/table-geocoder/lib/internal_geocoder_query_generator.rb
+++ services/table-geocoder/lib/internal_geocoder_query_generator.rb
@@ -12,28 +12,58 @@ module CartoDB
       case input_type
         when [:namedplace, :point, :freetext]
           country_clause = @internal_geocoder.countries == "'world'" ? 'null' : '{country}'
-          %Q{WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, #{country_clause})).*) SELECT q, null, geom, success FROM geo_function}
+          "WITH geo_function AS (SELECT (geocode_namedplace(Array[{cities}], null, #{country_clause})).*) SELECT q, null, geom, success FROM geo_function"
+        when [:namedplace, :point, :column]
+          'WITH geo_function AS (SELECT (geocode_namedplace(Array[{search_terms}], null, {country_list})).*) SELECT q, null, geom, success FROM geo_function'
         else
+          #TODO use a custom exception
           raise "Not implemented"
       end
     end
 
     def search_terms_query(page)
-      %Q{
+      case input_type
+        when [:namedplace, :point, :freetext]
+          %Q{
             SELECT DISTINCT(quote_nullable(#{@internal_geocoder.column_name})) AS searchtext
             FROM #{@internal_geocoder.qualified_table_name}
             WHERE cartodb_georef_status IS NULL
             LIMIT #{@internal_geocoder.batch_size} OFFSET #{page * @internal_geocoder.batch_size}
-      }
+          }
+        when [:namedplace, :point, :column]
+          #TODO review this query
+          %Q{
+            SELECT DISTINCT(quote_nullable(#{@internal_geocoder.column_name}), quote_nullable(#{@internal_geocoder.country_column}))
+            AS searchtext, country
+            FROM #{@internal_geocoder.qualified_table_name}
+            WHERE cartodb_georef_status IS NULL
+            LIMIT #{@internal_geocoder.batch_size} OFFSET #{page * @internal_geocoder.batch_size}
+          }
+        else
+          raise "Not implemented"
+      end
     end
 
     def copy_results_to_table_query
-      %Q{
-          UPDATE #{@internal_geocoder.qualified_table_name} AS dest
-          SET the_geom = orig.the_geom, cartodb_georef_status = orig.cartodb_georef_status
-          FROM #{@internal_geocoder.temp_table_name} AS orig
-          WHERE #{@internal_geocoder.column_name}::text = orig.geocode_string AND dest.cartodb_georef_status IS NULL
-      }
+      case input_type
+        when [:namedplace, :point, :freetext]
+          %Q{
+            UPDATE #{@internal_geocoder.qualified_table_name} AS dest
+            SET the_geom = orig.the_geom, cartodb_georef_status = orig.cartodb_georef_status
+            FROM #{@internal_geocoder.temp_table_name} AS orig
+            WHERE #{@internal_geocoder.column_name}::text = orig.geocode_string AND dest.cartodb_georef_status IS NULL
+          }
+        when [:namedplace, :point, :column]
+          #TODO redo this query
+          %Q{
+            UPDATE #{@internal_geocoder.qualified_table_name} AS dest
+            SET the_geom = orig.the_geom, cartodb_georef_status = orig.cartodb_georef_status
+            FROM #{@internal_geocoder.temp_table_name} AS orig
+            WHERE #{@internal_geocoder.column_name}::text = orig.geocode_string AND dest.cartodb_georef_status IS NULL
+          }
+        else
+          raise "Not implemented"
+      end
     end
 
     def input_type

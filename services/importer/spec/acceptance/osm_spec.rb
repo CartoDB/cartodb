bbe91137adfa9888baa4cda195302d735a022411
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-10-16 15:36:05 +0200

    osm spec fixed

--- services/importer/spec/acceptance/osm_spec.rb
+++ services/importer/spec/acceptance/osm_spec.rb
@@ -1,26 +1,24 @@
 # encoding: utf-8
-gem 'minitest'
-require 'minitest/autorun'
 require_relative '../../lib/importer/runner'
 require_relative '../../lib/importer/job'
 require_relative '../../lib/importer/downloader'
 require_relative '../factories/pg_connection'
+require_relative '../doubles/log'
+require_relative 'cdb_importer_context'
 
 include CartoDB::Importer2
 
 describe 'OSM regression tests' do
-  before do
-    @pg_options  = Factories::PGConnection.new.pg_options
-  end
+  include_context 'cdb_importer schema'
 
   it 'imports OSM files' do
     filepath    = path_to('map2.osm')
     downloader  = Downloader.new(filepath)
-    runner      = Runner.new(@pg_options, downloader)
+    runner      = Runner.new(@pg_options, downloader, Doubles::Log.new)
     runner.run
     puts runner.report
 
-    geometry_type_for(runner).wont_be_nil
+    geometry_type_for(runner).should be
   end
 
   def path_to(filepath)

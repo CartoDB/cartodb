5ce42c87aff164a3e73ec7f68323c4b4fce85ea4
Javier Arce <javierarce@gmail.com>
2014-10-16 15:33:05 +0200

    fixes orientation

--- src/geo/ui/annotation.js
+++ src/geo/ui/annotation.js
@@ -17,8 +17,12 @@ cdb.geo.ui.Annotation = cdb.core.View.extend({
     _.defaults(this.options, this.default_options);
 
     this.template = this.options.template;
+    this.mapView  = this.options.mapView;
 
-    this._setupModels();
+    this.extra = this.model.get("extra");
+
+    this.mapView.map.bind('change',      this._place, this);
+    this.mapView.map.bind('change:zoom', this._applyZoomLevelStyle, this);
 
     // zoom level config
     var minZoomLevel     = this.mapView.map.get("minZoom");
@@ -98,7 +102,7 @@ cdb.geo.ui.Annotation = cdb.core.View.extend({
     size            = this.mapView.getSize();
 
     if (textAlign === "right") {
-      left = pos.x - this.$el.width() - lineWidth;
+      left = pos.x - this.extra.width - lineWidth;
     } else {
       left = pos.x  + lineWidth;
     }
@@ -173,9 +177,123 @@ cdb.geo.ui.Annotation = cdb.core.View.extend({
 
   },
 
-  render: function() {
+  _applyStyle: function(save) {
+
+    var style      = this.model.get("style");
+
+    var textAlign  = style["text-align"];
+    var boxColor   = style["box-color"];
+    var boxOpacity = style["box-opacity"];
+    var boxPadding = style["box-padding"];
+
+    var lineWidth  = style["line-width"];
+    var lineColor  = style["line-color"];
+    var fontFamily = style["font-family-name"];
+
+    if (boxOpacity === 0) {
+      this.$el.addClass("border-dark");
+    } else {
+      this.$el.removeClass("border-dark");
+    }
+
+    this.$text.css(style);
+
+    this.$el.find(".content").css("padding", boxPadding);
+    this.$text.css("font-size", style["font-size"] + "px");
+    this.$el.css("z-index", style["z-index"]);
+
+    this.$el.find(".stick").css({ width: lineWidth, left: -lineWidth });
+
+    var fontFamilyClass = "";
+
+    if      (fontFamily  == "Droid Sans") fontFamilyClass = "droid";
+    else if (fontFamily  == "Vollkorn")   fontFamilyClass = "vollkorn";
+    else if (fontFamily  == "Open Sans")  fontFamilyClass = "open_sans";
+    else if (fontFamily  == "Roboto")     fontFamilyClass = "roboto";
+
+    this.$el
+    .removeClass("droid")
+    .removeClass("vollkorn")
+    .removeClass("roboto")
+    .removeClass("open_sans");
+
+    this.$el.addClass(fontFamilyClass);
+
+    if (textAlign === "right") {
+      this.$el.addClass("align-right");
+      this.$el.find(".stick").css({ left: "auto", right: -lineWidth });
+    } else {
+      this.$el.removeClass("align-right");
+    }
 
     this._place();
+
+    this._applyZoomLevelStyle();
+
+    if (save) this.model.save();
+
+    var self = this;
+
+    setTimeout(function() {
+      var width = self.$el.width();
+      self.$el.css("width", self.$el.width() + .4); // ? but it seems to work (TODO: check more browsers)
+    }, 500);
+
+  },
+
+  _applyZoomLevelStyle: function() {
+
+    var style      = this.model.get("style");
+
+    var boxColor   = style["box-color"];
+    var boxOpacity = style["box-opacity"];
+    var lineColor  = style["line-color"];
+
+    var minZoom    = style["zoom-min"];
+    var maxZoom    = style["zoom-max"];
+
+    var currentZoom = this.mapView.map.get("zoom");
+    var textOpacity = 1;
+
+    if (currentZoom >= minZoom && currentZoom <= maxZoom) {
+
+      textOpacity = 1;
+
+      var rgbaLineCol = 'rgba(' + parseInt(lineColor.slice(-6,-4),16)
+      + ',' + parseInt(lineColor.slice(-4,-2),16)
+      + ',' + parseInt(lineColor.slice(-2),16)
+      + ',' + 1 + ' )';
+
+      var rgbaBoxCol = 'rgba(' + parseInt(boxColor.slice(-6,-4),16)
+      + ',' + parseInt(boxColor.slice(-4,-2),16)
+      + ',' + parseInt(boxColor.slice(-2),16)
+      + ',' + boxOpacity + ' )';
+
+    } else {
+
+      textOpacity = .5;
+
+      var rgbaLineCol = 'rgba(' + parseInt(lineColor.slice(-6,-4),16)
+      + ',' + parseInt(lineColor.slice(-4,-2),16)
+      + ',' + parseInt(lineColor.slice(-2),16)
+      + ',' + .2 + ' )';
+
+      var rgbaBoxCol = 'rgba(' + parseInt(boxColor.slice(-6,-4),16)
+      + ',' + parseInt(boxColor.slice(-4,-2),16)
+      + ',' + parseInt(boxColor.slice(-2),16)
+      + ',' + .2 + ' )';
+
+    }
+
+    this.$el.find(".text").animate({ opacity: textOpacity }, 150);
+
+    this.$el.css("background-color", rgbaBoxCol);
+
+    this.$el.find(".stick").css("background-color", rgbaLineCol);
+    this.$el.find(".ball").css("background-color", rgbaLineCol);
+
+  },
+  render: function() {
 
     this.$el.html(this.template(_.extend(this.model.attributes, { text: this.model.attributes.extra.rendered_text })));
 

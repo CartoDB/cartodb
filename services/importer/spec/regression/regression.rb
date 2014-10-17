75478b77cb31f7f9d6b7b202d04af20a16138369
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-10-17 08:37:08 +0200

    regression spec fixed

--- services/importer/spec/regression/regression.rb
+++ services/importer/spec/regression/regression.rb
@@ -1,30 +1,31 @@
 # encoding: utf-8
-gem 'minitest'
-require 'minitest/autorun'
-require 'minitest/ci' if ENV['JOB_NAME']
+#TODO: is this needed for Travis or Jenkins?
+#require 'minitest/ci' if ENV['JOB_NAME']
 
 require_relative '../../lib/importer/runner'
 require_relative '../../lib/importer/job'
 require_relative '../../lib/importer/downloader'
 require_relative '../factories/pg_connection'
+require_relative '../doubles/log'
+require_relative '../../../../services/importer/spec/acceptance/cdb_importer_context'
 
 include CartoDB::Importer2
 
 describe 'Importer regression test' do  
-  before do
-    @pg_options  = Factories::PGConnection.new.pg_options
-  end
+  include_context "cdb_importer schema"
 
   folder = ENV['TEST_FILES'] || File.join(File.dirname(__FILE__), 'failing/')
   Dir[File.join(folder, '/*')].each do |file|
     it "correctly imports file #{file}" do
+      #INFO: this test depends on 'which raster2pgsql'
+
       filepath    = file
       downloader  = Downloader.new(File.expand_path filepath)
-      runner      = Runner.new(@pg_options, downloader)
+      runner      = Runner.new(@pg_options, downloader, Doubles::Log.new)
       runner.run
 
       runner.results.each do |result|
-        result[:success].must_equal true, "---> Importer failed: \n"+runner.report
+        result.success?.should be_true, "error code: #{result.error_code}, trace: #{result.log_trace}"
       end
       runner.db.disconnect
     end

57b82fa8304e0cb37e625ad7d2bc59bd07fcb3d4
Rafa de la Torre <rtorre@cartodb.com>
2014-10-21 16:39:47 +0000

    CDB-4386 Fix presenter tests and add them back to the regression suite

--- spec/models/layer/presenter_spec.rb
+++ spec/models/layer/presenter_spec.rb
@@ -6,38 +6,42 @@ require_relative '../../../app/models/layer/presenter'
 include CartoDB
 
 describe CartoDB::Layer::Presenter do
-  describe '#to_poro' do
+  describe '#to_vizjson_v2' do
     it 'wraps the sql if query_wrapper available' do
       layer = OpenStruct.new(
-        public_values:  {}, 
+        public_values: { 'options' => {'visible' => nil} },
+        tooltip: { 'template' => 'blahblah' },
         kind:           'carto',
         options: {
           'query'         => '',
           'tile_style'    => '',
           'interactivity' => '',
+          'style_version' => '',
           'table_name'    => 'bogus_table',
           'query_wrapper' => 'bogus template <%= sql %>'
         }
       )
 
-      poro = Layer::Presenter.new(layer).to_vizjson_v1
-      poro.fetch(:options).fetch(:query)
+      poro = CartoDB::Layer::Presenter.new(layer).to_vizjson_v2
+      poro.fetch(:options).fetch(:sql)
         .should == 'bogus template select * from bogus_table'
 
       layer = OpenStruct.new(
-        public_values:  {}, 
+        public_values: { 'options' => {'visible' => nil} },
+        tooltip: { 'template' => 'blahblah' },
         kind:           'carto',
         options: {
           'query'         => 'select the_geom from bogus_table',
           'tile_style'    => '',
           'interactivity' => '',
+          'style_version' => '',
           'table_name'    => 'bogus_table',
           'query_wrapper' => 'bogus template <%= sql %>'
         }
       )
 
-      poro = Layer::Presenter.new(layer).to_vizjson_v1
-      poro.fetch(:options).fetch(:query)
+      poro = CartoDB::Layer::Presenter.new(layer).to_vizjson_v2
+      poro.fetch(:options).fetch(:sql)
         .should == 'bogus template select the_geom from bogus_table'
     end
   end

60d18b2f59bab0c7dfe70ed6bb41410a97d40358
javi <qualopec@gmail.com>
2014-10-24 16:29:31 +0200

    updated cartodb.js

--- vendor/assets/javascripts/cartodb.mod.torque.uncompressed.js
+++ vendor/assets/javascripts/cartodb.mod.torque.uncompressed.js
@@ -5878,6 +5878,10 @@ exports.torque.common.TorqueLayer = TorqueLayer;
     return exports.torque.extend({}, a);
   }
 
+  exports.torque.isFunction = function(f) {
+    return typeof f == 'function' || false;
+  }
+
   exports.torque.isArray = function(value) {
       return value && typeof value == 'object' && Object.prototype.toString.call(value) == '[object Array]';
   };
@@ -5892,6 +5896,10 @@ exports.torque.common.TorqueLayer = TorqueLayer;
   exports.torque.isBrowserSupported = function() {
     return !!document.createElement('canvas');
   };
+
+  exports.torque.flags = {
+    sprites_to_images: navigator.userAgent.indexOf('Safari') === -1
+  }
 
 })(typeof exports === "undefined" ? this : exports);
 
@@ -7459,6 +7467,10 @@ exports.Profiler = Profiler;
      // function name
      var fnName = options.callbackName || 'torque_' + Date.now();
 
+     if (torque.isFunction(fnName)) {
+       fnName = fnName();
+     }
+
      function clean() {
        head.removeChild(script);
        clearTimeout(timeoutTimer);
@@ -7549,6 +7561,10 @@ exports.Profiler = Profiler;
   exports.torque = exports.torque || {};
 
   var TAU = Math.PI*2;
+  // min value to render a line. 
+  // it does not make sense to render a line of a width is not even visible
+  var LINEWIDTH_MIN_VALUE = 0.05; 
+
   function renderPoint(ctx, st) {
     ctx.fillStyle = st.fillStyle;
     var pixel_size = st['point-radius'];
@@ -7568,7 +7584,7 @@ exports.Profiler = Profiler;
 
     // stroke
     ctx.globalAlpha = 1.0;
-    if (st.strokeStyle && st.lineWidth) {
+    if (st.strokeStyle && st.lineWidth && st.lineWidth > LINEWIDTH_MIN_VALUE) {
       if (st.strokeOpacity) {
         ctx.globalAlpha = st.strokeOpacity;
       }
@@ -7700,7 +7716,11 @@ exports.Profiler = Profiler;
 
       var pointSize = st['point-radius'];
       if (!pointSize) {
-        throw new Error("marker-width property should be set");
+        return null;
+      }
+
+      if (st.fillOpacity === 0 && !st.strokeOpacity) {
+        return null;
       }
 
       var canvas = document.createElement('canvas');
@@ -7722,9 +7742,12 @@ exports.Profiler = Profiler;
         }
       }
       prof.end(true);
-      var i = new Image();
-      i.src = canvas.toDataURL();
-      return i;
+      if (torque.flags.sprites_to_images) {
+        var i = new Image();
+        i.src = canvas.toDataURL();
+        return i;
+      }
+      return canvas;
     },
 
     //
@@ -7774,12 +7797,14 @@ exports.Profiler = Profiler;
           var c = tile.renderData[pixelIndex + p];
           if(c) {
            var sp = sprites[c];
-           if(!sp) {
+           if(sp === undefined) {
              sp = sprites[c] = this.generateSprite(shader, c, _.extend({ zoom: tile.z, 'frame-offset': frame_offset }, shaderVars));
            }
-           var x = tile.x[posIdx]- (sp.width >> 1);
-           var y = tileMax - tile.y[posIdx]; // flip mercator
-           ctx.drawImage(sp, x, y - (sp.height >> 1));
+           if (sp) {
+             var x = tile.x[posIdx]- (sp.width >> 1);
+             var y = tileMax - tile.y[posIdx]; // flip mercator
+             ctx.drawImage(sp, x, y - (sp.height >> 1));
+           }
           }
         }
       }
@@ -10117,7 +10142,12 @@ var LeafLetTorqueLayer = L.TorqueLayer.extend({
       cartocss: layerModel.get('cartocss') || layerModel.get('tile_style'),
       named_map: layerModel.get('named_map'),
       auth_token: layerModel.get('auth_token'),
-      no_cdn: layerModel.get('no_cdn')
+      no_cdn: layerModel.get('no_cdn'),
+      instanciateCallback: function() {
+        var cartocss = layerModel.get('cartocss') || layerModel.get('tile_style');
+
+        return '_cdbct_' + cartodb.uniqueCallbackName(cartocss + query)
+      }
     });
 
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);

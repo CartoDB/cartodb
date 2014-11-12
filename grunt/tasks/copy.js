364a20ccd377c353b0a0fa75dfb4c70e9969c242
xavijam <xavijam@gmail.com>
2014-11-12 17:15:55 +0100

    Adding examples to CartoDB.js index html

--- grunt/tasks/copy.js
+++ grunt/tasks/copy.js
@@ -5,6 +5,7 @@
  */
 
 var __ = require('underscore');
+var findup = require('findup-sync');
 
 module.exports = {
   task: function(grunt, config) {
@@ -15,10 +16,24 @@ module.exports = {
 
           // Replace string task corrupts images
           if(srcpath.substr(srcpath.length - 3) === '.js' || srcpath.substr(srcpath.length - 5) === '.html') {
+
+            // Get all examples
+            var examples = [];
+
+            grunt.file.expand("examples/*").forEach(function (dir) {
+              var path = dir + '/demo.details';
+              if (grunt.file.exists(path)) {
+                var data = grunt.file.readYAML(path);
+                data.dir = dir.replace('examples/', '');
+                examples.push(data);
+              }
+            });
+
             return __.template(content)({
               last_bugfixing_version: config.version.bugfixing,
               last_minor_version:     config.version.minor,
-              last_major_version:     config.version.major
+              last_major_version:     config.version.major,
+              examples:               examples
             });
           }
 

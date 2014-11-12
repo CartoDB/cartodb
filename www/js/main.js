f4c806ff5662b74e0955a9e917aa5e2168616f32
Carlos Matallín <matallo@cartodb.com>
2014-11-12 15:56:52 +0100

    landing wip

--- www/js/main.js
+++ www/js/main.js
@@ -2,11 +2,39 @@ App.Views.Index = Backbone.View.extend({
   el: document.body,
 
   initialize: function() {
+    this.$header = this.$('.js-Header');
+
+    this._initBindings();
     this._initViews();
   },
 
+  _initBindings: function() {
+    var _this = this;
+
+    $(window)
+      .on('load', function() {
+        _this.$header.css({
+          height: window.innerHeight
+        });
+      });
+
+    $(window)
+      .on('resize', function() {
+        console.log("resize");
+        _this.$header.css({
+          height: window.innerHeight
+        });
+      });
+  },
+
   _initViews: function() {
-    console.log('Hola matata');
+    this._initOffcanvas();
+  },
+
+  _initOffcanvas: function() {
+    this.offcanvas = new CDBUILIB.Views.Offcanvas({
+      el: this.$('.js-Offcanvas')
+    })
   }
 });
 

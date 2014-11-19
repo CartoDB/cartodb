c68f0b3f19c733d97a766c82038794e70c1579a8
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-11-19 19:21:06 +0000

    Fix for nil tables at #1211

--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -168,7 +168,7 @@ class DataImport < Sequel::Model
     #if log.entries =~ /Table (.*) registered/
       self.success  = true
       self.state    = STATE_SUCCESS
-      table_names = results.map { |result| result.name }.sort
+      table_names = results.map { |result| result.name }.select { |name| name != nil}.sort
       self.table_names = table_names.join(' ')
       self.tables_created_count = table_names.size
       log.append "Import finished\n"

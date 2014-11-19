a07262ec4557efe813d766b468df9c0130e3c46c
Nicklas Gummesson <nicklas@cartodb.com>
2014-11-19 18:07:37 +0100

    Implemented support for date+time in TimeSlider
    Rendering date+time if the range is less than a day but still spanning
    over two dates. Add tests for the previous cases and clarified code to
    make it easier to read.

---
+++ test/spec/geo/ui/time_slider.spec.js
@@ -0,0 +1,79 @@
+describe('cdb.geo.ui.TimeSlider', function() {
+  var view;
+  var layer;
+
+  beforeEach(function() {
+    layer = new Backbone.Model();
+    view = new cdb.geo.ui.TimeSlider({
+      layer: layer,
+      width: "auto"
+    });
+  });
+
+  describe(".formatterForRange", function() {
+    var formatter;
+
+    /**
+     * @param {String} str (Optional) e.g. "2014-11-19T13:17:42Z" ATTENTION! This format must be used for Dates to be
+     * created as expected when running the tests on PhantomJS, you can find the report here:
+     * https://code.google.com/p/phantomjs/issues/detail?id=187 until the fix is included in the same version we use
+     * we must use dates this way.
+     * @return {Number} a Unix timestamp
+     */
+    var time = function time(str) {
+      return Date.parse(str)
+    };
+
+    describe("given a range is within the same day", function() {
+      beforeEach(function() {
+        var start = time("2014-11-19T09:13:00Z");
+        var end = time("2014-11-19T18:37:00Z");
+        formatter = view.formatterForRange(start, end)
+      });
+
+      it("should return a formatter function that renders the UTC time of given moment", function() {
+        var moment = new Date("2014-11-19T15:04:00Z");
+        expect(formatter(moment)).toEqual("15:04");
+      });
+    });
+
+    describe("given a range is within the same year", function() {
+      beforeEach(function() {
+        var start = time("2014-11-19T12:00:00Z");
+        var end = Date.parse("Dec 24, 2014 12:00 GMT+01");
+        formatter = view.formatterForRange(start, end)
+      });
+
+      it("should return a formatter function that renders the month/day/year (US format) of given moment", function() {
+        var moment = new Date("2014-11-20T12:00:00Z");
+        expect(formatter(moment)).toEqual("11/20/2014");
+      });
+    });
+
+    describe("given a range is more than a year", function() {
+      beforeEach(function() {
+        var start = time("2014-01-19T12:00:00Z");
+        var end = time("2015-02-01T12:00:00Z");
+        formatter = view.formatterForRange(start, end)
+      });
+
+      it("should return a formatter function that renders the month and year of given moment", function() {
+        var moment = new Date("2014-11-27T12:00:00Z");
+        expect(formatter(moment)).toEqual("11/2014")
+      });
+    });
+
+    describe("given a range is less than a day but spanning two dates", function() {
+      beforeEach(function() {
+        var start = time("2014-11-19T23:33:00Z");
+        var end = time("2014-11-20T09:42:00Z");
+        formatter = view.formatterForRange(start, end)
+      });
+
+      it("should return a formatter function that renders both date and UTC time", function() {
+        var moment = new Date("2014-11-20T01:16:00Z");
+        expect(formatter(moment)).toEqual("11/20/2014 01:16")
+      });
+    });
+  });
+});

7f955c2cb7f6fa66626a65251ba1cc31638b5856
Rafa de la Torre <rtorre@cartodb.com>
2014-11-21 18:40:29 +0100

    913 First version of faster imports with csvkit

--- services/importer/lib/importer/excel2csv.rb
+++ services/importer/lib/importer/excel2csv.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'csv'
-require 'roo'
 require_relative './job'
 require_relative './csv_normalizer'
 
@@ -19,13 +18,13 @@ module CartoDB
 
       def run
         job.log "Converting #{@format.upcase} to CSV"
-        spreadsheet = Roo::Spreadsheet.open(filepath)
+        %x[in2csv #{filepath} > #{converted_filepath}]
 
-        spreadsheet = remove_newlines(spreadsheet)
+        #TODO implement
+        #spreadsheet = remove_newlines(spreadsheet)
 
         # Can be check locally using wc -l ... (converted_filepath)
-        job.log "Orig file: #{filepath}\nTemp destination: #{converted_filepath}"        
-        spreadsheet.to_csv(converted_filepath)
+        job.log "Orig file: #{filepath}\nTemp destination: #{converted_filepath}"
         normalizer = CsvNormalizer.new(converted_filepath, job)
         # Roo gem is not exporting always correctly when source Excel has atypical UTF-8 characters
         normalizer.force_normalize
@@ -40,38 +39,7 @@ module CartoDB
         ) + '.csv'
       end #converted_filepath
 
-      protected
-
-      def remove_newlines(spreadsheet)
-        job.log 'Removing newlines...'
-        spreadsheet.default_sheet = spreadsheet.sheets.first
-        job.log 'Processing first sheet'
-
-        job.log 'Calculating columns (this can take long as will scan the full document)'
-        col_count = spreadsheet.sheet(0).last_column
-        job.log 'Calculating rows'
-        row_count = spreadsheet.sheet(0).last_row
-
-        for row_index in 1..row_count
-          for col_index in 1..col_count
-            if spreadsheet.celltype(row_index, col_index) == :string
-              current_value = spreadsheet.cell(row_index,col_index)
-              # As we are going to export to CSV, remove newlines or will cause problems (even being quoted)
-              if current_value.index("\n") != nil
-                spreadsheet.set(row_index, col_index, current_value.gsub("\n",''))
-              end
-            end
-          end
-        end
-
-        job.log 'Newlines removed'
-        spreadsheet
-      rescue NoMethodError
-        raise XLSXFormatError
-      end #remove_newlines
-
       attr_reader :filepath, :job
     end #Excel2Csv
   end # Importer2
 end # CartoDB
-

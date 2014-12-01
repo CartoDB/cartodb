072d167d837f2cafc8d7aafbcfafee4ef1d16c3b
Kartones <diego@kartones.net>
2014-12-01 12:26:47 +0100

    #1311 Removed error logging if table not found upon calculation of rows and sizes at dashboard

--- app/controllers/api/json/visualizations_controller.rb
+++ app/controllers/api/json/visualizations_controller.rb
@@ -331,11 +331,6 @@ class Api::Json::VisualizationsController < Api::ApplicationController
       table[:name]
       ).first
       if row.nil?
-        CartoDB.notify_error(
-          "Table #{table[:schema]}.#{table[:name]} not found at pg_class for username #{current_user.username}", {
-          user: current_user.username,
-          action: 'Api::Json::VisualizationsController.rows_and_sizes_for()'
-        })
         # don't break whole dashboard
         data[table[:name]] = {
           size: nil,

66595ac9f3a3c78391685e6968345eab94636c3a
Alejandro Martínez <zen@itram.es>
2014-12-02 18:12:37 +0100

    Add resque fork metrics too

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -7,3 +7,4 @@ require 'resque/failure/redis'
 Dir[Rails.root.join("lib/resque/*.rb")].each {|f| require f}
 
 Resque.redis = "#{Cartodb.config[:redis]['host']}:#{Cartodb.config[:redis]['port']}"
+Resque::Metrics.watch_fork

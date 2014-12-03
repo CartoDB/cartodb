c7be0f798c93c62f62a7e9551b7defdb9e1a8f2d
Alejandro Martínez <zen@itram.es>
2014-12-03 14:26:26 +0100

    Use custom logger to extract metrics instead of statsd logger

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -7,4 +7,41 @@ require 'resque/failure/redis'
 Dir[Rails.root.join("lib/resque/*.rb")].each {|f| require f}
 
 Resque.redis = "#{Cartodb.config[:redis]['host']}:#{Cartodb.config[:redis]['port']}"
-Resque::Metrics.watch_fork
+Resque.metric_logger = Logger.new("#{Rails.root}/log/resque_metrics.log")
+
+Resque::Metrics.on_job_complete do |job_class, queue, time|
+  Resque.metric_logger.info(
+    {:event => :job_complete,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_enqueue do |job_class, queue, time|
+  Resque.metric_logger.info(
+    {:event => :job_enqueue,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_fork do |job_class, queue|
+  Resque.metric_logger.info(
+    {:event => :job_fork,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_failure do |job_class, queue|
+  Resque.metric_logger.info(
+    {:event => :job_failure,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+

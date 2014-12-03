edf7140a71d8d6c9d8415a2878ed8c2e2124cd22
Alejandro Martínez <zen@itram.es>
2014-12-03 17:15:22 +0100

    fix logging class

--- lib/resque/metrics_logger.rb
+++ lib/resque/metrics_logger.rb
@@ -11,16 +11,17 @@ end
 Resque::Metrics.on_job_complete do |job_class, queue, time|
   CartoDB::Resque::Metrics.logger.info(
     {:event => :job_complete,
-     :job_class => job_class.class.to_s, 
+     :job_class => job_class.to_s, 
      :queue => queue, 
      :time => time}.to_json
   )
 end
 
 Resque::Metrics.on_job_enqueue do |job_class, queue, time|
+  debugger
   CartoDB::Resque::Metrics.logger.info(
     {:event => :job_enqueue,
-     :job_class => job_class.class.to_s,
+     :job_class => job_class.to_s,
      :queue => queue, 
      :time => time}.to_json
   )
@@ -29,7 +30,7 @@ end
 Resque::Metrics.on_job_fork do |job_class, queue|
   CartoDB::Resque::Metrics.logger.info(
     {:event => :job_fork,
-     :job_class => job_class.class.to_s, 
+     :job_class => job_class.to_s, 
      :queue => queue}.to_json
   )
 end
@@ -37,7 +38,7 @@ end
 Resque::Metrics.on_job_failure do |job_class, queue, time|
   CartoDB::Resque::Metrics.logger.info(
     {:event => :job_failure,
-     :job_class => job_class.class.to_s, 
+     :job_class => job_class.to_s, 
      :queue => queue, 
      :time => time}.to_json
   )

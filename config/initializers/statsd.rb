7c5c63aa17f65e6a6cf9268f6c588e735f488a42
Alejandro Martínez <zen@itram.es>
2014-12-03 15:41:29 +0100

    Move logger to different class

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -3,7 +3,6 @@ require 'statsd'
 begin
   Statsd.host = Cartodb.config[:graphite]['host']
   Statsd.port = Cartodb.config[:graphite]['port']
-  Resque::Metrics.backends.append Resque::Metrics::Backends::Statsd.new(Statsd)
 rescue => e
   Rails.logger.info "Ignoring statsd, because there were a error loading the config"
 end
---
+++ lib/resque/metrics_logger.rb
@@ -0,0 +1,46 @@
+module CartoDB
+  module Resque
+    module Metrics
+      def self.logger
+        @metric_logger ||= ::Logger.new("#{Rails.root}/log/resque_metrics.log")
+      end
+    end
+  end
+end
+
+Resque::Metrics.on_job_complete do |job_class, queue, time|
+  CartoDB::Resque::Metrics.logger.info(
+    {:event => :job_complete,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_enqueue do |job_class, queue, time|
+  CartoDB::Resque::Metrics.logger.info(
+    {:event => :job_enqueue,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_fork do |job_class, queue|
+  CartoDB::Resque::Metrics.logger.info(
+    {:event => :job_fork,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+
+Resque::Metrics.on_job_failure do |job_class, queue|
+  CartoDB::Resque::Metrics.logger.info(
+    {:event => :job_failure,
+     :job_class => job_class, 
+     :queue => queue, 
+     :time => time}.to_json
+  )
+end
+

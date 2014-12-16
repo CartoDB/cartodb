69944684a60f20d22f84f8517ff2bdda5f5999b1
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-12-16 17:18:56 +0100

    Avoid permission owner reload

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -232,11 +232,12 @@ module CartoDB
 
     # @return User|nil
     def owner
-      User.where(id:self.owner_id).first
+      @owner ||= User.where(id:self.owner_id).first
     end
 
     # @param value User
     def owner=(value)
+      @owner = value
       self.owner_id = value.id
       self.owner_username = value.username
     end

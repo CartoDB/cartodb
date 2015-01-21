c76820c1c50f39250e70da1fd8bf047b9bf2e06b
Javier Arce <javierarce@gmail.com>
2015-01-21 19:08:49 +0100

    Brings legends back

--- src/vis/vis.js
+++ src/vis/vis.js
@@ -518,7 +518,7 @@ var Vis = cdb.core.View.extend({
 
     this.mapView = mapView;
 
-    if (options.legends) {
+    if (options.legends === undefined || options.legends) {
       map.layers.bind('reset', this.addLegends, this);
     }
 

b257529446d986625bd4d1114490af4793735251
javi <qualopec@gmail.com>
2015-01-29 20:22:27 +0100

    updated migration to remove organization flags

--- db/migrate/20150125201100_drop_basemap_options.rb
+++ db/migrate/20150125201100_drop_basemap_options.rb
@@ -3,6 +3,9 @@ Sequel.migration do
     drop_column :users, :here_maps_enabled
     drop_column :users, :stamen_maps_enabled
     drop_column :users, :rainbow_maps_enabled
+    drop_column :organizations, :here_maps_enabled
+    drop_column :organizations, :stamen_maps_enabled
+    drop_column :organizations, :rainbow_maps_enabled
   end
   down do
   end

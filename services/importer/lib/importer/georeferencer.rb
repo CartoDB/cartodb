6f2bb7761ce73a9097b82b9f4e6b2d15cd96db8c
Rafa de la Torre <rtorre@cartodb.com>
2015-02-05 16:56:01 +0000

    Prioritize ip over country guessing #2089
    - Tables containing a significant number of IP's are far less frequent
    than tables containing countries.
    
    - IP geocoding gives better precission.
    
    - Points are lighter than polygons.
    
    - The user can geocode by country afterwards if feels so.

--- services/importer/lib/importer/georeferencer.rb
+++ services/importer/lib/importer/georeferencer.rb
@@ -44,8 +44,8 @@ module CartoDB
 
         create_the_geom_from_geometry_column  ||
         create_the_geom_from_latlon           ||
-        create_the_geom_from_country_guessing ||
         create_the_geom_from_ip_guessing      ||
+        create_the_geom_from_country_guessing ||
         create_the_geom_in(table_name)
 
         enable_autovacuum

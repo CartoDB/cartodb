1825d13042eab99a7647331478c485d605809d31
Kartones <kartones@cartodb.com>
2015-02-10 16:56:37 +0100

    #migration-add-max-import-size-to-users

--- db/migrate/20150210154301_add_max_import_file_size_to_users.rb
+++ db/migrate/20150210154301_add_max_import_file_size_to_users.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   up do
-    add_column :users, :max_import_file_size, :integer, null: false, default: 100*1024*1024
+    add_column :users, :max_import_file_size, :integer, null: false, default: 150*1024*1024
   end
   down do
     drop_column :users, :max_import_file_size

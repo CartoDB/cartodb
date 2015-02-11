2f30f94dc69997799af4dc17f07a1ad59bfba412
Nicklas Gummesson <nicklas@cartodb.com>
2015-02-11 17:36:15 +0100

    DRY pages controller
    - extract control flow logic
    - add helper methods for view logic related stuff

---
+++ lib/cartodb/controller_flows/public/content.rb
@@ -0,0 +1,46 @@
+module CartoDB
+  module ControllerFlows
+    module Public
+
+      class Content
+
+        def initialize(request, renderer)
+          @request  = request
+          @renderer = renderer
+        end
+
+        def render
+          username = CartoDB.extract_subdomain(@request).strip.downcase
+          viewed_user = User.where(username: username).first
+
+          if viewed_user.nil?
+            org = Organization.where(name: username).first
+            unless org.nil?
+              if FeatureFlag.allowed?('new_public_dashboard')
+                return @renderer.new_organization_content(org)
+              else
+                return @renderer.old_organization_content(org)
+              end
+            end
+          end
+
+          return @renderer.render_404 if viewed_user.nil?
+
+          if viewed_user.has_organization?
+            if CartoDB.extract_real_subdomain(@request) != viewed_user.organization.name
+              # redirect username.host.ext => org-name.host.ext/u/username
+              @ctrl.redirect_to CartoDB.base_url(viewed_user.organization.name) << @renderer.organization_path and return
+            end
+          end
+
+          if viewed_user.has_feature_flag?('new_public_dashboard')
+            return @renderer.new_user_content(viewed_user)
+          else
+            return @renderer.old_user_content(viewed_user)
+          end
+        end
+      end
+
+    end
+  end
+end

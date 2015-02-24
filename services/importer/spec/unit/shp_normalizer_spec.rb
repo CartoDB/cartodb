5ceeadce58ea774da2939d61abbf0c996ca91ece
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-02-24 17:50:40 +0100

    Only accept pg-supported encodings

--- services/importer/spec/unit/shp_normalizer_spec.rb
+++ services/importer/spec/unit/shp_normalizer_spec.rb
@@ -20,6 +20,15 @@ describe CartoDB::Importer2::ShpNormalizer do
       shp_normalizer.shape_encoding.should eq 'UTF-8'
     end
   
+    it 'guesses LATIN1 encoding for a "greek", unsupported encoding' do
+      job = CartoDB::Importer2::Doubles::Job.new
+      job.stubs(:table_name).returns('greek')
+      path = File.expand_path(File.join(File.dirname(__FILE__), "../fixtures/greek/greek.shp"))
+      shp_normalizer = CartoDB::Importer2::ShpNormalizer.new(path, job)
+
+      shp_normalizer.shape_encoding.should eq 'LATIN1'
+    end
+
   end
 
 end

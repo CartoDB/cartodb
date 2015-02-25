6fbb56fed07acf0200659cad3d8557d2439f46aa
Rafa de la Torre <rtorre@cartodb.com>
2015-02-25 19:53:27 +0000

    Fix for invalidation test (yeah, we know sometimes we invalidate more than we should) #2407

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -590,7 +590,7 @@ describe Table do
 
     id = table.table_visualization.id
     CartoDB::Varnish.any_instance.expects(:purge)
-      .times(2)
+      .times(3)
       .with(".*#{id}:vizjson")
       .returns(true)
 

cd17f935f7c6e4ebac206e2f9c1b4ae87641d92b
Rafa de la Torre <rtorre@cartodb.com>
2015-02-25 16:54:45 +0000

    Re-enable the vizjson caching in redis #2407

--- app/models/visualization/member.rb
+++ app/models/visualization/member.rb
@@ -321,9 +321,9 @@ module CartoDB
       end
 
       def to_vizjson
-        calculate_vizjson
-        #redis_cached(redis_vizjson_key) do
-        #end
+        redis_cached(redis_vizjson_key) do
+          calculate_vizjson
+        end
       end
 
       def is_owner?(user)

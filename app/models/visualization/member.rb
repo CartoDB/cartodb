33611b68a519955d91876e58d4db4028a2e8cdd2
Rafa de la Torre <rtorre@cartodb.com>
2015-03-13 18:47:55 +0100

    Some fixes for member #2660

--- app/models/visualization/member.rb
+++ app/models/visualization/member.rb
@@ -774,9 +774,9 @@ module CartoDB
 
       # @param table Table
       def propagate_name_to(table)
-        table.name = self.name
+        table.user_table.name = self.name
         table.register_table_only = self.register_table_only
-        table.update(name: self.name)
+        table.user_table.update(name: self.name)
         if name_changed
           support_tables.rename(old_name, name, recreate_constraints=true, seek_parent_name=old_name)
         end

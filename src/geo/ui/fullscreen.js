d9cee7d1018393857485ada6b54b157d3b74b7a4
Xatpy <jaime.chapinal@gmail.com>
2015-03-25 13:44:36 +0100

    Avoid error messages fullscreen button

--- src/geo/ui/fullscreen.js
+++ src/geo/ui/fullscreen.js
@@ -76,7 +76,11 @@ cdb.ui.common.FullScreen = cdb.core.View.extend({
         // Reference: Ehttp://stackoverflow.com/questions/8427413/webkitrequestfullscreen-fails-when-passing-element-allow-keyboard-input-in-safar
         requestFullScreen.call(docEl, undefined);
       } else {
-        requestFullScreen.call(docEl);
+        // CartoDB.js #412 : 
+        // Nowadays (2015/03/25), fullscreen is not supported in iOS Mobile. Reference: http://caniuse.com/#feat=fullscreen
+        if (requestFullScreen !== undefined) {
+          requestFullScreen.call(docEl);
+        }
       }
 
       if (mapView) {

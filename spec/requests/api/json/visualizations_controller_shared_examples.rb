114b4ed9dee9cbcb0f38b413080a63a0a2137919
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-03-26 16:28:14 +0100

    Working check for old visualization body response

--- spec/requests/api/json/visualizations_controller_shared_examples.rb
+++ spec/requests/api/json/visualizations_controller_shared_examples.rb
@@ -5,7 +5,7 @@ shared_examples_for "visualization controllers" do
 
   describe 'index' do
 
-    it 'returns success response for empty user' do
+    it 'returns success, empty response for empty user' do
       login_as(@user1, scope: @user1.subdomain)
       get base_url, nil, @headers
       last_response.status.should == 200
@@ -13,10 +13,19 @@ shared_examples_for "visualization controllers" do
       body.should == { 'visualizations' => [], 'total_entries' => 0, 'total_user_entries' => 0, 'total_likes' => 0, 'total_shared' => 0}
     end
 
-    xit 'returns right information for a user with one table' do
+    it 'returns right information for a user with one table' do
       login_as(@user1, scope: @user1.subdomain)
-      body = JSON.parse(get(base_url, nil, @headers))
-      body.should == { 'visualizations' => [], 'total_entries' => 0, 'total_user_entries' => 0, 'total_likes' => 0, 'total_shared' => 0}
+      table1 = create_table(@user1)
+      get base_url, nil, @headers
+      body = JSON.parse(last_response.body)
+      expected_visualization = JSON.parse(table1.table_visualization.to_hash(
+        related: false,
+        table_data: true,
+        user: @user1,
+        table: table1,
+        synchronization: nil
+      ).to_json)
+      body.should == { 'visualizations' => [expected_visualization], 'total_entries' => 1, 'total_user_entries' => 1, 'total_likes' => 0, 'total_shared' => 0}
     end
 
   end

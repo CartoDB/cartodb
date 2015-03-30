d89fa1e4462c0bd0e1926f3c38a3f55225add246
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-03-30 13:53:28 +0200

    Getting connection from Sequel instead of from model fixes migration failing

--- db/migrate/20130220145819_add_default_to_layers_updated_at.rb
+++ db/migrate/20130220145819_add_default_to_layers_updated_at.rb
@@ -1,6 +1,6 @@
 class AddDefaultToLayersUpdatedAt < Sequel::Migration
   def up
-    Layer.db.run("ALTER TABLE layers ALTER COLUMN updated_at SET DEFAULT NOW()")
+    Rails::Sequel.connection.run("ALTER TABLE layers ALTER COLUMN updated_at SET DEFAULT NOW()")
   end
 
   def down
--- db/migrate/20130220153907_update_old_layers_updated_at.rb
+++ db/migrate/20130220153907_update_old_layers_updated_at.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   up do
-    Layer.db.run("UPDATE layers SET updated_at = NOW() WHERE updated_at IS NULL")
+    Rails::Sequel.connection.run("UPDATE layers SET updated_at = NOW() WHERE updated_at IS NULL")
   end
   
   down do

b749838add4d751be08eb4a559b61e627002aa74
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-04-01 19:55:35 +0200

    Like endpoint test moved to common

--- spec/requests/api/json/visualizations_controller_shared_examples.rb
+++ spec/requests/api/json/visualizations_controller_shared_examples.rb
@@ -356,6 +356,81 @@ shared_examples_for "visualization controllers" do
       body['visualizations'][0]['id'].should eq u1_t_1_id
     end
 
+    describe 'tests visualization likes endpoints' do
+      it 'tests like endpoints' do
+        CartoDB::Visualization::Member.any_instance.stubs(:has_named_map?).returns(false)
+        CartoDB::NamedMapsWrapper::NamedMaps.any_instance.stubs(:get).returns(nil)
+
+        user_2 = create_user(
+          username: 'test2',
+          email:    'client2@example.com',
+          password: 'clientex'
+        )
+
+        post api_v1_visualizations_create_url(user_domain: @user.username, api_key: @api_key),
+          factory(@user).to_json, @headers
+        vis_1_id = JSON.parse(last_response.body).fetch('id')
+
+        get api_v1_visualizations_likes_count_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 0
+
+        get api_v1_visualizations_likes_list_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('likes').should eq []
+
+        get api_v1_visualizations_is_liked_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+
+        post api_v1_visualizations_add_like_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 1
+
+        get api_v1_visualizations_is_liked_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('liked').should eq true
+
+        get api_v1_visualizations_likes_count_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 1
+
+        get api_v1_visualizations_likes_list_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('likes').should eq [{'actor_id' => @user.id}]
+
+        post api_v1_visualizations_add_like_url(user_domain: user_2.username, id: vis_1_id, api_key: user_2.api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 2
+
+        get api_v1_visualizations_likes_list_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        # Careful with order of array items
+        (JSON.parse(last_response.body).fetch('likes') - [
+                                                           {'actor_id' => @user.id},
+                                                           {'actor_id' => user_2.id}
+                                                         ]).should eq []
+
+        delete api_v1_visualizations_remove_like_url(user_domain: user_2.username, id: vis_1_id, api_key: user_2.api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 1
+
+        # No effect expected
+        delete api_v1_visualizations_remove_like_url(user_domain: user_2.username, id: vis_1_id, api_key: user_2.api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 1
+
+        post api_v1_visualizations_add_like_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        last_response.status.should == 400
+        last_response.body.should eq "You've already liked this visualization"
+
+        delete api_v1_visualizations_remove_like_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 0
+
+        post api_v1_visualizations_add_like_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        last_response.status.should == 200
+        JSON.parse(last_response.body).fetch('likes').to_i.should eq 1
+
+        get api_v1_visualizations_likes_list_url(user_domain: @user.username, id: vis_1_id, api_key: @api_key)
+        JSON.parse(last_response.body).fetch('likes').should eq [{'actor_id' => @user.id}]
+
+        user_2.destroy
+      end
+  end
+
   end
 
 end

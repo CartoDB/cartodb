842738c366f8f4bd18a8baecb1d2109751f23193
Rafa de la Torre <rtorre@cartodb.com>
2015-04-10 12:31:29 +0200

    Fix missing env var RAILS_ENV in some cases (unicorn)

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -2,10 +2,9 @@ require 'active_record'
 
 ::Sequel::DATABASES.each{|d| d.sql_log_level = :debug }
 
-@environment = ENV['RAILS_ENV'] || 'development'
 @dbconfig = YAML.load(File.read('config/database.yml'))
 # INFO: our current database.yml sets Sequel PostgreSQL adapter, which is called 'postgres'. Rails' is 'postgresql'
-@dbconfig[@environment]['adapter'] = 'postgresql'
-ActiveRecord::Base.establish_connection @dbconfig[@environment]
+@dbconfig[Rails.env]['adapter'] = 'postgresql'
+ActiveRecord::Base.establish_connection @dbconfig[Rails.env]
 # TODO: console debugging purposes
 ActiveRecord::Base.logger = Logger.new(STDOUT)

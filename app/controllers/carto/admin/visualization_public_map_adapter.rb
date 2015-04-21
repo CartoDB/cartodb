3c3baf046be1267bd194b7baf07399ce35e62bfb
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-04-21 13:01:53 +0200

    related_visualizations_simple_geometry_types and related_tables_simple_geometry_types

--- app/controllers/carto/admin/visualization_public_map_adapter.rb
+++ app/controllers/carto/admin/visualization_public_map_adapter.rb
@@ -4,6 +4,14 @@ require_relative '../api/vizjson_presenter'
 class Carto::Admin::VisualizationPublicMapAdapter
   extend Forwardable
   include Carto::HtmlSafe
+      GEOMETRY_MAPPING = {
+        'st_multipolygon'    => 'polygon',
+        'st_polygon'         => 'polygon',
+        'st_multilinestring' => 'line',
+        'st_linestring'      => 'line',
+        'st_multipoint'      => 'point',
+        'st_point'           => 'point'
+      }
 
   delegate [ :type_slide?, :has_permission?, :derived?, :organization, :organization?, :id, :likes, :password_protected?, :varnish_key, :related_tables, :is_password_valid?, :get_auth_tokens, :table, :name, :overlays, :created_at, :description, :tags, :mapviews, :geometry_types, :privacy, :surrogate_key ] => :visualization
 
@@ -72,6 +80,20 @@ class Carto::Admin::VisualizationPublicMapAdapter
 
   def related_tables_geometry_types
     related_tables.collect(&:geometry_types).flatten.uniq
+  end
+
+  def related_visualizations_simple_geometry_types
+    simplify_geometry_types(related_visualizations_geometry_types)
+  end
+
+  def related_tables_simple_geometry_types
+    simplify_geometry_types(related_tables_geometry_types)
+  end
+
+  private
+
+  def simplify_geometry_types(geometry_types)
+    geometry_types.map { |gt| GEOMETRY_MAPPING.fetch(gt.downcase, "unknown: #{gt}") }
   end
 
 end

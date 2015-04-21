04ece2c8a97d6d6c9865018c1cee762713a996ba
Rafa de la Torre <rtorre@cartodb.com>
2015-04-21 18:27:43 +0200

    First attempt with working code #3280
    This needs a bit more work to avoid usage of old CartoDB::Visualization stuff.

--- app/controllers/carto/api/tables_controller.rb
+++ app/controllers/carto/api/tables_controller.rb
@@ -4,8 +4,18 @@ module Carto
 
     class TablesController < ::Api::ApplicationController
 
+      ssl_required :show
+
       def show
-        #TODO: implement
+        return head(404) if table == nil
+        return head(403) unless table.table_visualization.has_permission?(current_user, CartoDB::Visualization::Member::PERMISSION_READONLY)
+        render_jsonp(table.public_values({request:request}, current_user).merge(schema: table.schema(reload: true)))
+      end
+
+      private
+
+      def table
+        @table ||= ::Table.get_by_id_or_name(params.fetch('id'), current_user)
       end
 
     end

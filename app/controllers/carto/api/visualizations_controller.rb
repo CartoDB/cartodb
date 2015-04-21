b13f752099678be01c5fd3d2830c6270a04e1eab
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-04-21 10:21:28 +0200

    Better types and total_types settings fixes #3291

--- app/controllers/carto/api/visualizations_controller.rb
+++ app/controllers/carto/api/visualizations_controller.rb
@@ -173,11 +173,19 @@ module Carto
       private
 
       def get_types_parameters
-        type = params[:type].present? ? params[:type] : nil
+        # INFO: this fits types and type into types, so only types is used for search.
+        # types defaults to type if empty.
+        # types defaults to derived if type is also empty.
+        # total_types are the types used for total counts.
+        types = params.fetch(:types, "").split(',')
+
+        type = params[:type].present? ? params[:type] : (types.empty? ? nil : types[0])
         # TODO: add this assumption to a test or remove it (this is coupled to the UI)
         total_types = [(type == Carto::Visualization::TYPE_REMOTE ? Carto::Visualization::TYPE_CANONICAL : type)].compact
-        types = params.fetch(:types, "#{type}").split(',')
-        types = nil if types.empty?
+
+        types = [type].compact if types.empty?
+        types = [Carto::Visualization::TYPE_DERIVED] if types.empty?
+
         return types, total_types
       end
 

f87ab45c379d87ddc0c2e9ccd2b9417e8a34bec3
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-05-07 11:44:01 +0200

    fails with password protected files

--- spec/requests/api/json/imports_controller_shared_examples.rb
+++ spec/requests/api/json/imports_controller_shared_examples.rb
@@ -88,4 +88,17 @@ shared_examples_for "imports controllers" do
     import['state'].should be == 'complete'
   end
 
+  it 'fails with password protected files' do
+    post api_v1_imports_create_url,
+      params.merge(:filename => upload_file('spec/support/data/alldata-pass.zip', 'application/octet-stream'))
+
+    item_queue_id = JSON.parse(response.body)['item_queue_id']
+
+    get api_v1_imports_show_url(:id => item_queue_id), params
+
+    response.code.should be == '200'
+    import = JSON.parse(response.body)
+    import['state'].should be == 'failure'
+  end
+
 end

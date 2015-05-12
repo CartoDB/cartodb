4c417c81affda4d7c1ab80155933bcb0fd7b0fd9
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-05-12 16:46:17 +0200

    Load ActiveRecord in the end of initialization

--- config/initializers/00_01_active_record.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record'
-
-@dbconfig = YAML.load(File.read(File.join(Rails.root, 'config/database.yml')))
-# INFO: our current database.yml sets Sequel PostgreSQL adapter, which is called 'postgres'. Rails' is 'postgresql'
-@dbconfig[Rails.env]['adapter'] = 'postgresql'
-ActiveRecord::Base.establish_connection @dbconfig[Rails.env]
-# INFO: console debugging purposes
-ActiveRecord::Base.logger = Logger.new(STDOUT) if Rails.env == 'development'
-

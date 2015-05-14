cbbb3d79dbf2a9ba66cba3762d3a12c4a3b187ff
Kartones <kartones@cartodb.com>
2015-05-14 18:17:47 +0200

    #3587 Layer model's set_default_order relies on nulls, so cannot set default value

--- db/migrate/20150514141720_add_missing_default_values.rb
+++ db/migrate/20150514141720_add_missing_default_values.rb
@@ -2,7 +2,6 @@ Sequel.migration do
   up do
     alter_table :layers do
       set_column_default :options, '{}'
-      set_column_default "order", 0
     end
     alter_table :overlays do
       set_column_default :options, '{}'
@@ -13,7 +12,6 @@ Sequel.migration do
   down do
     alter_table :layers do
       set_column_default :options, nil
-      set_column_default "order", nil
     end
     alter_table :overlays do
       set_column_default :options, nil

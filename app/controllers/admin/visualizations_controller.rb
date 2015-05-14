80cfc60a7588c9f69b80f60ec8873bef04fc43cc
Javier Álvarez Medina <xavijam@gmail.com>
2015-05-14 10:27:06 +0200

    export format to SHP

--- app/controllers/admin/visualizations_controller.rb
+++ app/controllers/admin/visualizations_controller.rb
@@ -178,7 +178,7 @@ class Admin::VisualizationsController < ApplicationController
     }.count
 
     # Public export API SQL url
-    @export_sql_api_url = "#{ sql_api_url("SELECT * FROM #{ @table.owner.sql_safe_database_schema }.#{ @table.name }", @user) }&format=csv"
+    @export_sql_api_url = "#{ sql_api_url("SELECT * FROM #{ @table.owner.sql_safe_database_schema }.#{ @table.name }", @user) }&format=shp"
 
     respond_to do |format|
       format.html { render 'public_table', layout: 'application_table_public' }

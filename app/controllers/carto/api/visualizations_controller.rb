82df87b48d4311b59bf86636adcdf18021cf96f1
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-05-19 16:15:59 +0200

    is_liked endpoint doesn't force authentication

--- app/controllers/carto/api/visualizations_controller.rb
+++ app/controllers/carto/api/visualizations_controller.rb
@@ -7,8 +7,8 @@ module Carto
     class VisualizationsController < ::Api::ApplicationController
 
       # TODO: compare with older, there seems to be more optional authentication endpoints
-      skip_before_filter :api_authorization_required, only: [:index, :vizjson2]
-      before_filter :optional_api_authorization, only: [:index, :vizjson2]
+      skip_before_filter :api_authorization_required, only: [:index, :vizjson2, :is_liked]
+      before_filter :optional_api_authorization, only: [:index, :vizjson2, :is_liked]
 
       before_filter :id_and_schema_from_params
       before_filter :load_table, only: [:vizjson2]

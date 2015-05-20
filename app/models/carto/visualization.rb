fb66ea89f196b42a9c27b004f361ffcbb886cb32
Kartones <kartones@cartodb.com>
2015-05-20 11:40:47 +0200

    #3701 backend support for size on all visualization types

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -44,7 +44,9 @@ class Carto::Visualization < ActiveRecord::Base
   belongs_to :map
 
   def size
-    table.size
+    # Only canonical visualizations (Datasets) have a related table and then count against disk quota,
+    # but we want to not break and even allow ordering by size multiple types
+    table ? table.size : 0
   end
 
   def tags

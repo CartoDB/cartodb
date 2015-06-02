6f5d8dba8c7404e5e1c5adedfe20960eb9ac3c8c
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-06-02 17:53:19 +0200

    Support for non-string download objects

--- services/importer/lib/importer/downloader.rb
+++ services/importer/lib/importer/downloader.rb
@@ -55,7 +55,8 @@ module CartoDB
           @translated_url = translator.translate(url)
           @custom_filename = translator.respond_to?(:rename_destination) ? translator.rename_destination(url) : nil
         end
-        @translated_url = URI.escape(@translated_url) unless @translated_url.nil?
+        # INFO: runner_spec.rb uses File instead of String, so we chose to support both
+        @translated_url = URI.escape(@translated_url) if !@translated_url.nil? && @translated_url.kind_of?(String)
       end
 
       def provides_stream?

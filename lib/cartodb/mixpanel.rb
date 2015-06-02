3da84d8218afcc4743b27b9a7d959f5e960ab4a5
Carlos Matallín <matallo@cartodb.com>
2015-06-02 21:28:29 +0200

    mixpanel: resolve conflict

--- lib/cartodb/mixpanel.rb
+++ lib/cartodb/mixpanel.rb
@@ -50,7 +50,7 @@ module CartoDB
     def mixpanel_event(*args)
       return self unless Cartodb.config[:mixpanel].present?
       token = Cartodb.config[:mixpanel]['token']
-      Mixpanel::Tracker.new(token).send(:track, *args)
+      ::Mixpanel::Tracker.new(token).send(:track, *args)
     rescue => exception
       Rollbar.report_message(
         "Failed to send metric to Mixpanel",

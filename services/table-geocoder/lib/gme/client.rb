dafa3b7e36361f6bb4e5dd6863fc1051222aaa32
Rafa de la Torre <rtorre@cartodb.com>
2015-06-08 13:00:53 +0200

    Add timeouts to gme client

--- services/table-geocoder/lib/gme/client.rb
+++ services/table-geocoder/lib/gme/client.rb
@@ -3,6 +3,7 @@
 require 'typhoeus'
 require 'addressable/uri'
 require_relative '../../../../lib/url_signer'
+require_relative '../../../../lib/carto/http/client'
 
 module Carto
   module Gme
@@ -13,15 +14,27 @@ module Carto
 
       BASE_URL = 'https://maps.googleapis.com'
 
-      def initialize(client_id, private_key)
+      DEFAULT_CONNECT_TIMEOUT = 45
+      DEFAULT_READ_TIMEOUT = 60
+      DEFAULT_RETRY_TIMEOUT = 60
+
+      HTTP_CLIENT_TAG = 'gme_client'
+
+      # Performs requests to Google Maps API web services
+      # Based on https://github.com/googlemaps/google-maps-services-python/blob/master/googlemaps/client.py
+      def initialize(client_id, private_key, options = {})
         @client_id = client_id
         @url_signer = UrlSigner.new(private_key)
+        @connect_timeout = options[:connect_timeout] || DEFAULT_CONNECT_TIMEOUT
+        @read_timeout = options[:read_timeout] || DEFAULT_READ_TIMEOUT
+        @retry_timeout = options[:retry_timeout] || DEFAULT_RETRY_TIMEOUT
+        @http_client = Carto::Http::Client.get(HTTP_CLIENT_TAG)
       end
 
       def get(endpoint, params)
         # TODO: error mgmt, retries, throttling
         url = generate_auth_url(BASE_URL+endpoint, params)
-        resp = Typhoeus.get(url)
+        resp = @http_client.get(url, timeout: @read_timeout, connecttimeout: @connect_timeout)
         resp.body
       end
 

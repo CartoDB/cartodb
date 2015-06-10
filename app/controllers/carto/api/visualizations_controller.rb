bc213e45ee77e527157bd9265f51f271107f0512
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-06-10 16:33:14 +0200

    Organization shared vizs should not count or display owned

--- app/controllers/carto/api/visualizations_controller.rb
+++ app/controllers/carto/api/visualizations_controller.rb
@@ -80,7 +80,7 @@ module Carto
           response.merge!({
             total_user_entries: VisualizationQueryBuilder.new.with_types(total_types).with_user_id(current_user.id).build.count,
             total_likes: VisualizationQueryBuilder.new.with_types(total_types).with_liked_by_user_id(current_user.id).build.count,
-            total_shared: VisualizationQueryBuilder.new.with_types(total_types).with_shared_with_user_id(current_user.id).build.count
+            total_shared: VisualizationQueryBuilder.new.with_types(total_types).with_shared_with_user_id(current_user.id).with_user_id_not(current_user.id).build.count
           })
         end
         render_jsonp(response)

25f5d9c1d57e80a422aae2cbab6e8171226c2d0a
Rafa de la Torre <rtorre@cartodb.com>
2015-06-12 12:52:46 +0200

    More tests for the Gme::TableGeocoder

--- services/table-geocoder/spec/lib/gme/table_geocoder_spec.rb
+++ services/table-geocoder/spec/lib/gme/table_geocoder_spec.rb
@@ -6,19 +6,20 @@ RSpec.configure do |config|
   config.mock_with :mocha
 end
 
-# TODO: add it to Makefile
 
 describe Carto::Gme::TableGeocoder do
 
+  mandatory_args = {
+      connection: nil,
+      original_formatter: '{mock}',
+      client_id: 'my_client_id',
+      private_key: 'my_private_key'
+  }
+
   describe '#initialize' do
+
     it 'returns an object that responds to AbstractTableGeocoder interface' do
-      arguments = {
-        connection: nil,
-        original_formatter: '{mock}',
-        client_id: 'mock',
-        private_key: 'mock'
-      }
-      table_geocoder = Carto::Gme::TableGeocoder.new(arguments)
+      table_geocoder = Carto::Gme::TableGeocoder.new(mandatory_args)
       interface_methods = [:add_georef_status_column,
                            :cancel,
                            :run,
@@ -36,14 +37,8 @@ describe Carto::Gme::TableGeocoder do
         Carto::Gme::TableGeocoder.new
       }.to raise_error(ArgumentError)
 
-      mandatory_arguments = {
-        connection: nil,
-        original_formatter: '{mock}',
-        client_id: 'mock',
-        private_key: 'mock'
-      }
-      mandatory_arguments.each do |arg|
-        args_missing_one = mandatory_arguments.dup
+      mandatory_args.each do |arg|
+        args_missing_one = mandatory_args.dup
         args_missing_one.delete(arg[0])
         lambda {
           Carto::Gme::TableGeocoder.new(args_missing_one)
@@ -51,12 +46,32 @@ describe Carto::Gme::TableGeocoder do
       end
     end
 
-    xit 'creates and keeps an instance of Gme::Client with the provided credentials' do
+    it 'creates a client with the provided credentials' do
+      gme_client_mock = mock
+      Carto::Gme::Client.expects(:new).with('my_client_id', 'my_private_key').once.returns(gme_client_mock)
+      Carto::Gme::GeocoderClient.expects(:new).with(gme_client_mock).once
+
+      table_geocoder = Carto::Gme::TableGeocoder.new(mandatory_args)
     end
   end
 
+
   describe '#run' do
-    xit 'does something' do
+    pending "set's the state to 'processing' when it starts" do
+      # Actually as a requirement this does not make much sense
     end
+
+    xit "set's the state to 'completed' when it ends" do
+      
+    end
+
+    xit "if there's an uncontrolled exception, sets the state to 'failed' and raises it" do
+      
+    end
+
+    xit "processes 1 block at a time, keeping track of processed rows in each block" do
+      
+    end
+
   end
 end

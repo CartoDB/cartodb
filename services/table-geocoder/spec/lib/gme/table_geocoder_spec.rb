d232cf70510a133f4eded223d986e6ecd01c1638
Rafa de la Torre <rtorre@cartodb.com>
2015-06-15 12:55:56 +0200

    Add more tests for gme-geocoder

--- services/table-geocoder/spec/lib/gme/table_geocoder_spec.rb
+++ services/table-geocoder/spec/lib/gme/table_geocoder_spec.rb
@@ -57,20 +57,51 @@ describe Carto::Gme::TableGeocoder do
 
 
   describe '#run' do
-    pending "set's the state to 'processing' when it starts" do
-      # Actually as a requirement this does not make much sense
+
+    before(:all) do
+      gme_client_mock = mock
+      Carto::Gme::Client.expects(:new).with('my_client_id', 'my_private_key').once.returns(gme_client_mock)
+      Carto::Gme::GeocoderClient.expects(:new).with(gme_client_mock).once
+
+      @table_geocoder = Carto::Gme::TableGeocoder.new(mandatory_args)
     end
 
-    xit "set's the state to 'completed' when it ends" do
-      
+    it "set's the state to 'processing' when it starts" do
+      pending 'actually as a requirement this does not make much sense'
     end
 
-    xit "if there's an uncontrolled exception, sets the state to 'failed' and raises it" do
-      
+    it "set's the state to 'completed' when it ends" do
+      @table_geocoder.stubs(:add_georef_status_column)
+      @table_geocoder.stubs(:data_input_blocks).returns([])
+
+      @table_geocoder.run
+      @table_geocoder.state.should == 'completed'
     end
 
-    xit "processes 1 block at a time, keeping track of processed rows in each block" do
-      
+    it "if there's an uncontrolled exception, sets the state to 'failed' and raises it" do
+      @table_geocoder.stubs(:add_georef_status_column)
+      @table_geocoder.stubs(:data_input_blocks).returns([{cartodb_id: 1, searchtext: 'dummy text'}])
+      @table_geocoder.stubs(:geocode).raises(StandardError, 'unexpected exception')
+
+      expect {
+        @table_geocoder.run
+      }.to raise_error('unexpected exception')
+      @table_geocoder.state.should == 'failed'
+    end
+
+    it "processes 1 block at a time, keeping track of processed rows in each block" do
+      @table_geocoder.stubs(:add_georef_status_column)
+      mocked_input = Enumerator.new do |enum|
+        # 2 blocks of 2 rows each as input
+        enum.yield [{cartodb_id: 1, searchtext: 'dummy text'}, {cartodb_id: 2, searchtext: 'dummy text'}]
+        enum.yield [{cartodb_id: 3, searchtext: 'dummy text'}, {cartodb_id: 4, searchtext: 'dummy text'}]
+      end
+      @table_geocoder.stubs(:data_input_blocks).returns(mocked_input)
+      @table_geocoder.expects(:geocode).twice
+      @table_geocoder.expects(:update_table).twice
+
+      @table_geocoder.run
+      @table_geocoder.processed_rows.should == 4
     end
 
   end

9d2c30d9294b0b3806542320b167465d7e2cc408
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-06-23 09:37:42 +0200

    Improved spec checking and central stubbing

--- spec/models/carto/user_creation_spec.rb
+++ spec/models/carto/user_creation_spec.rb
@@ -8,6 +8,7 @@ describe Carto::UserCreation do
 
     it 'assigns an enable_account_token if user has not signed up with Google' do
       User.any_instance.stubs(:create_in_central).returns(true)
+      User.any_instance.stubs(:enable_remote_db_user).returns(true)
       user_data = FactoryGirl.build(:valid_user)
       user_data.organization = @organization
       user_data.google_sign_in = false
@@ -21,14 +22,17 @@ describe Carto::UserCreation do
 
     it 'does not assign an enable_account_token if user has signed up with Google' do
       User.any_instance.stubs(:create_in_central).returns(true)
+      User.any_instance.stubs(:enable_remote_db_user).returns(true)
       user_data = FactoryGirl.build(:valid_user)
       user_data.organization = @organization
       user_data.google_sign_in = true
+      user_data.save
 
       user_creation = Carto::UserCreation.new_user_signup(user_data)
       user_creation.next_creation_step until user_creation.finished?
 
       saved_user = Carto::User.order("created_at desc").limit(1).first
+      saved_user.username.should == user_data.username
       saved_user.enable_account_token.should be_nil
     end
   end
@@ -39,6 +43,7 @@ describe Carto::UserCreation do
     # INFO : this mail contains validation link
     it 'triggers a ::Resque::UserJobs::Mail::NewOrganizationUser' do
       User.any_instance.stubs(:create_in_central).returns(true)
+      User.any_instance.stubs(:enable_remote_db_user).returns(true)
       ::Resque.expects(:enqueue).with(Resque::UserJobs::Mail::NewOrganizationUser, instance_of(String)).once
 
       user_data = FactoryGirl.build(:valid_user)

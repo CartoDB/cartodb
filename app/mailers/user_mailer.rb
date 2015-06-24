2661ec2576d848419c6daeeabc4f3df27f997877
Carlos Matallín <matallo@cartodb.com>
2015-06-24 16:29:10 +0200

    mailer: org user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,9 @@ class UserMailer < ActionMailer::Base
     @organization = @user.organization
     @owner = @organization.owner
     @link = "#{CartoDB.base_url(@organization.name, @user.username)}"
+    @subject = "You have been invited to CartoDB organization '#{@organization.name}'"
     mail :to => @user.email, 
-         :subject => "You have been invited to CartoDB organization '#{@organization.name}'"
+         :subject => @subject
   end
 
   def share_table(table, user)
@@ -16,8 +17,9 @@ class UserMailer < ActionMailer::Base
     @user = user
     organization = @table_visualization.user.organization
     @link = "#{CartoDB.base_url(organization.name, @user.username)}#{CartoDB.path(self, 'public_tables_show_bis', {id: "#{@table_visualization.user.username}.#{@table_visualization.name}"})}"
+    @subject = "#{@table_visualization.user.username} has shared a CartoDB table with you"
     mail :to => @user.email, 
-         :subject => "#{@table_visualization.user.username} has shared a CartoDB table with you"
+         :subject => @subject
   end
   
   def share_visualization(visualization, user)
@@ -25,24 +27,27 @@ class UserMailer < ActionMailer::Base
     @user = user
     organization = @visualization.user.organization
     @link = "#{CartoDB.base_url(organization.name, @visualization.user.username)}#{CartoDB.path(self, 'public_visualizations_show_map', {id: @visualization.id})}"
+    @subject = "#{@visualization.user.username} has shared a CartoDB visualization with you"
     mail :to => @user.email,
-         :subject => "#{@visualization.user.username} has shared a CartoDB visualization with you"
+         :subject => @subject
   end
 
   def unshare_table(table_visualization_name, table_visualization_owner_name, user)
     @table_visualization_name = table_visualization_name
     @table_visualization_owner_name = table_visualization_owner_name
     @user = user
+    @subject = "#{@table_visualization_owner_name} has stopped sharing a CartoDB table with you"
     mail :to => @user.email,
-         :subject => "#{@table_visualization_owner_name} has stopped sharing a CartoDB table with you"
+         :subject => @subject
   end
   
   def unshare_visualization(visualization_name, visualization_owner_name, user)
     @visualization_name = visualization_name
     @visualization_owner_name = visualization_owner_name
     @user = user
+    @subject = "#{@visualization_owner_name} has stopped sharing a CartoDB visualization with you"
     mail :to => @user.email,
-         :subject => "#{@visualization_owner_name} has stopped sharing a CartoDB visualization with you"
+         :subject => @subject
   end
 
 end

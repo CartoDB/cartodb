7f7ecf0793a8a26b4b32e347f47f6b78a7f48ec7
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-06-29 14:22:56 +0200

    cartodb_hosted logic moved inside upgrade_url

--- vendor/assets/javascripts/cartodb.mod.odyssey.uncompressed.js
+++ vendor/assets/javascripts/cartodb.mod.odyssey.uncompressed.js
@@ -1,5 +1,5 @@
-// version: 3.15.0
-// sha: 172320bd56167754a7e36c24a2367d3cd34ec673
+// version: 3.14.0
+// sha: a89883013ddbe27335df75b08815533cd42112f9
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.O=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 

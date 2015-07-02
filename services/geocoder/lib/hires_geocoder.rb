61ab02da77fc4f68956edf8a740281e4423d912a
Rafa de la Torre <rtorre@cartodb.com>
2015-07-02 13:03:51 +0200

    Sanitize the non-batched geocoder
    - Remove a lot of unused stuff from there
    - Rename `run_non_batched` to `run`

--- services/geocoder/lib/hires_geocoder.rb
+++ services/geocoder/lib/hires_geocoder.rb
@@ -36,53 +36,9 @@ module CartoDB
       @mailto             = arguments.fetch(:mailto)
       @force_batch        = arguments[:force_batch] || false
       @dir                = arguments[:dir] || Dir.mktmpdir
-      @used_batch_request = false
-      begin
-        @batch_api_disabled = Cartodb.config[:geocoder]['batch_api_disabled'] == true
-      rescue
-        @batch_api_disabled = false
-      end
-    end # initialize
-
-    def upload
-      run_non_batched
     end
 
-    def used_batch_request?
-      @used_batch_request
-    end
-
-    def cancel
-      # TODO implement. This needs peeking into model state
-    end
-
-    def update_status
-      # TODO remove
-    end
-
-    def result
-      return @result unless @result.nil?
-      results_filename = File.join(dir, "#{request_id}.zip")
-      # TODO: check for status
-      stdout, stderr, status  = Open3.capture3('wget', '-nv', '-E', '-O', results_filename, api_url({}, 'result'))
-      @result = Dir[File.join(dir, '*')][0]
-    end # results
-
-
-    private
-
-    def http_client
-      @http_client ||= Carto::Http::Client.get('hires_geocoder', log_requests: true)
-    end
-
-    def input_rows
-      stdout, stderr, status  = Open3.capture3('wc', '-l', input_file)
-      stdout.to_i
-    rescue => e
-      0
-    end
-
-    def run_non_batched
+    def run
       @result = File.join(dir, 'generated_csv_out.txt')
       @status = 'running'
       @total_rows = input_rows
@@ -99,6 +55,36 @@ module CartoDB
       # TODO: is this really needed? seems to be used for temp directory creation
       # TODO: split non-batched and batched geocodes
       @request_id = UUIDTools::UUID.timestamp_create.to_s.gsub('-', '')
+    end
+
+    def used_batch_request?
+      false
+    end
+
+    def cancel
+      # TODO implement. This needs peeking into model state
+    end
+
+    def update_status
+      # TODO remove
+    end
+
+    def result
+      @result
+    end
+
+
+    private
+
+    def http_client
+      @http_client ||= Carto::Http::Client.get('hires_geocoder', log_requests: true)
+    end
+
+    def input_rows
+      stdout, stderr, status  = Open3.capture3('wc', '-l', input_file)
+      stdout.to_i
+    rescue => e
+      0
     end
 
     def geocode_text(text)
@@ -126,16 +112,6 @@ module CartoDB
       components << '?' + URI.encode_www_form(arguments)
       components.join('/')
     end # api_url
-
-    def extract_response_field(response, query = '//Response/MetaInfo/RequestId')
-      Nokogiri::XML(response).xpath("#{query}").first.content
-    rescue NoMethodError => e
-      nil
-    end # extract_response_field
-
-    def handle_api_error(response)
-      raise "Geocoding API communication failure: #{extract_response_field(response.body, '//Details')}" if response.code != 200
-    end # handle_api_errpr
 
   end # Geocoder
 end # CartoDB

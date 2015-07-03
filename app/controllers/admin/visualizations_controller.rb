318e6c453d971b77b21d008a1613813b11ce2c77
Kartones <kartones@cartodb.com>
2015-07-03 14:24:13 +0200

    3932-warden-improvements

--- app/controllers/admin/visualizations_controller.rb
+++ app/controllers/admin/visualizations_controller.rb
@@ -4,7 +4,7 @@ require_dependency 'resque/user_jobs'
 require_relative '../carto/admin/user_table_public_map_adapter'
 require_relative '../carto/admin/visualization_public_map_adapter'
 require_relative '../../helpers/embed_redis_cache'
-require_dependency 'lib/static_maps_url_helper'
+require_dependency 'static_maps_url_helper'
 
 class Admin::VisualizationsController < ApplicationController
 

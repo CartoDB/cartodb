acdd2d001a2cbf0dd55e0313797eedeb209c8368
Francisco Dans <fdansv@gmail.com>
2015-07-09 13:53:34 +0200

    adds more properties to query

--- src/api/sql.js
+++ src/api/sql.js
@@ -448,9 +448,16 @@
 
   SQL.prototype.describeBoolean = function(sql, column, options, callback){
     var s = [
-      'null_ratio as (SELECT sum(case when {{column}} is null or "" then 1 else 0 end)::numeric / count(*)::numeric as null_ratio FROM ({{sql}}) _wrap), ',
-      'true_ratio as (SELECT sum(case when {{column}} is true then 1 else 0 end)::numeric / count(*)::numeric as true_ratio FROM ({{sql}}) _wrap), ',
-      'SELECT * FROM true_ratio, null_ratio'
+      'with stats as (',
+            'select count(distinct({{column}})) as uniq,',
+                   'count(*) as cnt',
+              'from ({{sql}}) _wrap ',
+        '),',
+      'null_ratio as (',
+        'SELECT sum(case when {{column}} is null then 1 else 0 end)::numeric / count(*)::numeric as null_ratio FROM ({{sql}}) _wrap), ',
+      'true_ratio as (',
+        'SELECT sum(case when {{column}} is true then 1 else 0 end)::numeric / count(*)::numeric as true_ratio FROM ({{sql}}) _wrap) ',
+      'SELECT * FROM true_ratio, null_ratio, stats'
     ];
     var query = Mustache.render(s.join('\n'), {
       column: column,
@@ -463,7 +470,9 @@
       callback({
         type: 'boolean',
         null_ratio: row.null_ratio,
-        true_ratio: row.true_ratio
+        true_ratio: row.true_ratio,
+        distinct: row.uniq,
+        count: cnt
       });
     });
   }

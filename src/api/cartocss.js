c3891fd96e7cd1694a4563d7ed982a23f38041be
Francisco Dans <fdansv@gmail.com>
2015-07-15 17:31:01 +0200

    Revert "alters behaviour of ramps"
    This reverts commit cf59fed70752e3e2338271b7ba397a1441a1ad5f.

--- src/api/cartocss.js
+++ src/api/cartocss.js
@@ -238,13 +238,11 @@ function getWeightFromShape(dist_type){
 
 function getMethodProperties(stats) {
 
-  var method;
-  var ramp = ramps[_.shuffle(["green", "blue", "pink", "black", "red"])[0]]
+  var method, ramp;
+
   if (['A','U'].indexOf(stats.dist_type) != -1) { // apply divergent scheme
     method = stats.jenks;
-    if (stats.min < 0 && stats.max > 0){
-      ramp = ramps.divergent;
-    }
+    ramp = ramps.divergent;
   } else if (stats.dist_type === 'F') {
     method = stats.equalint;
     ramp = ramps.red;

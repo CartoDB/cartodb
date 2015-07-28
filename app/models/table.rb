cb2524726143b186b17135116777f3138748694f
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-07-28 19:12:19 +0200

    Fixes PG::ERROR rising when renaming a table to anything starting with an underscore

--- app/models/table.rb
+++ app/models/table.rb
@@ -1258,7 +1258,16 @@ class Table
 
       unless register_table_only
         begin
-          owner.in_database.rename_table(@name_changed_from, name)
+          if name.starts_with?('_')
+            # PG gem uses an intermediate file for renaming
+            # and uses '_original_name' as it's name. This
+            # hack avoids a PG::ERROR raising
+            owner.in_database.rename_table(@name_changed_from, "aux-#{name}")
+            owner.in_database.rename_table("aux-#{name}", name)
+          else
+            owner.in_database.rename_table(@name_changed_from, name)
+          end
+          
         rescue StandardError => exception
           exception_to_raise = CartoDB::BaseCartoDBError.new(
               "Table update_name_changes(): '#{@name_changed_from}' doesn't exist", exception)

d1e8810b7da8926a65165a4f528101c058e25a2c
Kartones <kartones@cartodb.com>
2015-07-31 12:15:56 +0200

    #4805 DB model ready

---
+++ db/migrate/20150731082648_create_templates.rb
@@ -0,0 +1,42 @@
+Sequel.migration do
+
+  up do
+    Rails::Sequel::connection.run 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
+
+    create_table :templates do
+      Uuid        :id,                      primary_key: true, null: false, unique: false, default: 'uuid_generate_v4()'.lit
+      Uuid        :source_visualization_id, null: false
+      String      :title,                   null: false
+      String      :description,             null: false, default: ''
+      String      :min_supported_version,   null: false
+      String      :max_supported_version,   null: false
+      String      :code,                    null: false, default: ''
+      Uuid        :organization_id,         null: false
+    end
+
+    Rails::Sequel.connection.run(%Q{
+      ALTER TABLE "templates"
+      ADD CONSTRAINT  source_visualization_id_fkey FOREIGN KEY (source_visualization_id) REFERENCES visualizations(id)
+      })
+
+    Rails::Sequel.connection.run(%Q{
+      ALTER TABLE "templates"
+      ADD CONSTRAINT  organization_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations(id)
+      })
+
+    Rails::Sequel.connection.run(%Q{
+      ALTER TABLE "templates"
+      ADD COLUMN required_tables uuid[] NOT NULL DEFAULT '{}'
+    })
+
+    Rails::Sequel.connection.run(%Q{
+      CREATE INDEX organization_id_idx ON templates(organization_id)
+    })
+
+  end
+
+  down do
+    drop_table :templates
+  end
+
+end

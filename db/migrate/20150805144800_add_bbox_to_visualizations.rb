be2517a93535dfe49b46853876265e806f89016d
Mario de Frutos <mario.defrutos@cartodb.com>
2015-08-05 17:33:50 +0200

    Migration to add postgis support and bbox field to visualizations table

---
+++ db/migrate/20150805144800_add_bbox_to_visualizations.rb
@@ -0,0 +1,29 @@
+Sequel.migration do
+  up do
+    Rails::Sequel.connection.run(%Q{
+      CREATE EXTENSION IF NOT EXISTS "postgis"
+    })
+
+    Rails::Sequel.connection.run(%Q{
+      ALTER TABLE visualizations ADD COLUMN bbox geometry;
+    })
+
+    Rails::Sequel.connection.run(%Q{
+      CREATE INDEX bbox_visualizations_gix ON visualizations USING GIST (bbox);
+    })
+  end
+
+  down do
+    Rails::Sequel.connection.run(%Q{
+      DROP INDEX IF EXISTS bbox_visualizations_gix
+    })
+
+    Rails::Sequel.connection.run(%Q{
+      ALTER TABLE visualizations DROP COLUMN IF EXISTS bbox
+    })
+
+    Rails::Sequel.connection.run(%Q{
+      DROP EXTENSION IF EXISTS "postgis" CASCADE
+    })
+  end
+end

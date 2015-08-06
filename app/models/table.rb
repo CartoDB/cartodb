0e62aea934d6c5afb11e7acd0883a41add60e967
Mario de Frutos <mario.defrutos@cartodb.com>
2015-08-06 20:01:57 +0200

    Add bounding box to all import datasets

--- app/models/table.rb
+++ app/models/table.rb
@@ -16,9 +16,11 @@ require_relative './overlay/presenter'
 require_relative '../../services/importer/lib/importer/query_batcher'
 require_relative '../../services/datasources/lib/datasources/decorators/factory'
 require_relative '../../services/table-geocoder/lib/internal-geocoder/latitude_longitude'
+require_relative '../helpers/bounding_box_helper'
 
 class Table
   extend Forwardable
+  include BoundingBoxHelper
 
   SYSTEM_TABLE_NAMES = %w( spatial_ref_sys geography_columns geometry_columns raster_columns raster_overviews cdb_tablemetadata geometry raster )
 
@@ -56,7 +58,6 @@ class Table
   DEFAULT_THE_GEOM_TYPE = 'geometry'
 
   VALID_GEOMETRY_TYPES = %W{ geometry multipolygon point multilinestring }
-
 
   def_delegators :relator, *CartoDB::TableRelator::INTERFACE
   def_delegators :@user_table, *::UserTable::INTERFACE
@@ -548,6 +549,7 @@ class Table
     member.store
 
     member.map.recalculate_bounds!
+    add_bounding_box_info
 
     CartoDB::Visualization::Overlays.new(member).create_default_overlays
   end
@@ -1389,6 +1391,12 @@ class Table
     rescue
       []
     end
+  end
+
+  def add_bounding_box_info
+    db = table_visualization.user.in_database
+    bounds = calculate_bounding_box(db, qualified_table_name)
+    table_visualization.save_bounding_box(bounds)
   end
 
   def cache
--- db/migrate/20150805144800_add_bbox_to_visualizations.rb
+++ db/migrate/20150805144800_add_bbox_to_visualizations.rb
@@ -5,21 +5,21 @@ Sequel.migration do
     })
 
     Rails::Sequel.connection.run(%Q{
-      ALTER TABLE visualizations ADD COLUMN bbox geometry;
+      ALTER TABLE visualizations ADD COLUMN bounding_box geometry;
     })
 
     Rails::Sequel.connection.run(%Q{
-      CREATE INDEX bbox_visualizations_gix ON visualizations USING GIST (bbox);
+      CREATE INDEX bounding_box_visualizations_gix ON visualizations USING GIST (bounding_box);
     })
   end
 
   down do
     Rails::Sequel.connection.run(%Q{
-      DROP INDEX IF EXISTS bbox_visualizations_gix
+      DROP INDEX IF EXISTS bounding_box_visualizations_gix
     })
 
     Rails::Sequel.connection.run(%Q{
-      ALTER TABLE visualizations DROP COLUMN IF EXISTS bbox
+      ALTER TABLE visualizations DROP COLUMN IF EXISTS bounding_box
     })
 
     Rails::Sequel.connection.run(%Q{

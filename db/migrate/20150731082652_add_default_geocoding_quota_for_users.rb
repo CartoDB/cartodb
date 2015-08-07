0bd0979c5de0314c489cc767dd4f0b1be44979dd
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-08-07 09:59:56 +0200

    Changes migration timestamp to avoid collisions with master after merging

---
+++ db/migrate/20150731082652_add_default_geocoding_quota_for_users.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  up do
+    Rails::Sequel.connection.run(%Q{
+      UPDATE users SET geocoding_quota = 0 WHERE geocoding_quota IS NULL
+    })
+    alter_table :users do
+        set_column_not_null(:geocoding_quota)
+        set_column_default(:geocoding_quota, 0)
+    end
+  end
+  
+  down do
+    alter_table :users do
+        set_column_allow_null(:geocoding_quota)
+        set_column_default(:geocoding_quota, nil)
+    end
+  end
+end

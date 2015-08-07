271d507a75ed0fe8ef5a591c92f4749de9c33112
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-08-07 18:16:56 +0200

    Fix cartodb_com_hosted check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -37,7 +37,7 @@ class HomeController < ApplicationController
   end
 
   def app_diagnosis
-    return head(400) if Cartodb.config[:cartodb_com_hosted] == true
+    return head(400) if Cartodb.config[:cartodb_com_hosted] == false
 
     @diagnosis = [
       diagnosis_output('Operating System') { single_line_command_version_diagnosis(OS_VERSION, 'lsb_release -a', 1) },

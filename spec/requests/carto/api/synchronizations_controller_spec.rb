121f64aa8cc52e26fd94b9b4daa55efd8c6e1752
Kartones <kartones@cartodb.com>
2015-08-11 10:02:19 +0200

    #4935 tests moved

--- spec/requests/carto/api/synchronizations_controller_spec.rb
+++ spec/requests/carto/api/synchronizations_controller_spec.rb
@@ -1,34 +1,94 @@
 # encoding: utf-8
 
+require 'sequel'
+require 'rack/test'
 require_relative '../../../spec_helper'
 require_relative '../../api/json/synchronizations_controller_shared_examples'
 require_relative '../../../../app/controllers/carto/api/synchronizations_controller'
 
 describe Carto::Api::SynchronizationsController do
-
-  before(:all) do
-    # Spec the routes so that it uses the new controller. Needed for alternative routes testing
-    Rails.application.routes.draw do
-
-      # new controller
-      scope :module => 'carto/api', :format => :json do
-        get    '(/user/:user_domain)(/u/:user_domain)/api/v1/synchronizations'              => 'synchronizations#index',    as: :api_v1_synchronizations_index
-        get    '(/user/:user_domain)(/u/:user_domain)/api/v1/synchronizations/:id'          => 'synchronizations#show',     as: :api_v1_synchronizations_show
-        get    '(/user/:user_domain)(/u/:user_domain)/api/v1/synchronizations/:id/sync_now' => 'synchronizations#syncing?', as: :api_v1_synchronizations_syncing
-      end
-
-      # old controller
-      scope :module => 'api/json', :format => :json do
-        post   '(/user/:user_domain)(/u/:user_domain)/api/v1/synchronizations'              => 'synchronizations#create',   as: :api_v1_synchronizations_create
-      end
-    end
-  end
-
-  after(:all) do
-    Rails.application.reload_routes!
-  end
+  include Rack::Test::Methods
+  include Warden::Test::Helpers
+  include CacheHelper
 
   it_behaves_like 'synchronization controllers' do
   end
+
+  describe 'main behaviour' do
+    # INFO: this tests come from spec/requests/api/json/synchronizations_controller_spec.rb
+
+    before(:all) do
+      @user = create_user(
+        username: 'test',
+        email:    'client@example.com',
+        password: 'clientex',
+        sync_tables_enabled: true
+      )
+      @api_key = @user.api_key
+    end
+
+    before(:each) do
+      @db = Rails::Sequel.connection
+      Sequel.extension(:pagination)
+
+      CartoDB::Synchronization.repository  = DataRepository::Backend::Sequel.new(@db, :synchronizations)
+
+      delete_user_data @user
+      @headers = {
+        'CONTENT_TYPE'  => 'application/json',
+      }
+      host! 'test.localhost.lan'
+    end
+
+    after(:all) do
+      @user.destroy
+    end
+
+    describe 'GET /api/v1/synchronizations/:id' do
+      it 'returns a synchronization record' do
+        payload = {
+          table_name: 'table_1',
+          interval:   3600,
+          url:        'http://www.foo.com'
+        }
+
+        post "/api/v1/synchronizations?api_key=#{@api_key}", payload.to_json, @headers
+        id = JSON.parse(last_response.body).fetch('id')
+
+        get "/api/v1/synchronizations/#{id}?api_key=#{@api_key}", nil, @headers
+        last_response.status.should == 200
+
+        response = JSON.parse(last_response.body)
+        response.fetch('id').should == id
+        response.fetch('url').should == payload.fetch(:url)
+      end
+
+      it 'returns 404 for unknown synchronizations' do
+        get "/api/v1/synchronizations/56b40691-541b-4ef3-96da-f2be29563566?api_key=#{@api_key}", nil, @headers
+        last_response.status.should == 404
+      end
+    end
+
+    describe 'GET /api/v1/synchronizations/:id/sync_now' do
+      it 'returns sync status' do
+        payload = {
+          table_name: 'table_1',
+          interval:   3600,
+          url:        'http://www.foo.com'
+        }
+
+        post "/api/v1/synchronizations?api_key=#{@api_key}", payload.to_json, @headers
+        id = JSON.parse(last_response.body).fetch('id')
+
+        get "/api/v1/synchronizations/#{id}/sync_now?api_key=#{@api_key}", nil, @headers
+        last_response.status.should == 200
+
+        response = JSON.parse(last_response.body)
+        response.fetch('state').should == 'created'
+      end
+    end
+
+  end
+
 end
 

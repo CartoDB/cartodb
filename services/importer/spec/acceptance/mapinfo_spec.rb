eb7aea5fdaa1e5705d8655f2977cd91e79ecf1ae
Kartones <kartones@cartodb.com>
2015-08-24 13:28:45 +0200

    #5085 more spec stabilization due to config error handling

--- services/importer/spec/acceptance/mapinfo_spec.rb
+++ services/importer/spec/acceptance/mapinfo_spec.rb
@@ -8,12 +8,14 @@ require_relative '../doubles/log'
 require_relative '../doubles/user'
 require_relative 'cdb_importer_context'
 require_relative 'acceptance_helpers'
+require_relative 'no_stats_context'
 
 include CartoDB::Importer2
 
 describe 'Mapinfo regression tests' do
   include AcceptanceHelpers
   include_context "cdb_importer schema"
+  include_context "no stats"
 
   it 'imports Mapinfo files' do
     # Rails.root not loaded yet. This is a workaround
@@ -31,5 +33,5 @@ describe 'Mapinfo regression tests' do
   end
 
 end # Mapinfo regression tests
- 
+
 

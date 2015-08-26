4d45509024059aaf1fecc8c4a721fde6a37bc196
Nicklas Gummesson <nicklas@cartodb.com>
2015-08-26 12:53:23 +0200

    Setup boilerplate for new groups entry point

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ CartoDB::Application.routes.draw do
     get    '(/user/:user_domain)(/u/:user_domain)/organization/settings'        => 'organizations#settings',        as: :organization_settings
     put    '(/user/:user_domain)(/u/:user_domain)/organization/settings'        => 'organizations#settings_update', as: :organization_settings_update
     post '(/user/:user_domain)(/u/:user_domain)/organization/regenerate_api_keys'       => 'organizations#regenerate_all_api_keys', as: :regenerate_organization_users_api_key
+    get    '(/user/:user_domain)(/u/:user_domain)/organization/groups'          => 'organizations#groups',          as: :organization_settings
     # Organization users management
     get    '(/user/:user_domain)(/u/:user_domain)/organization/users/:id/edit'  => 'organization_users#edit',    as: :edit_organization_user,   constraints: { id: /[0-z\.\-]+/ }
     put    '(/user/:user_domain)(/u/:user_domain)/organization/users/:id'       => 'organization_users#update',  as: :update_organization_user, constraints: { id: /[0-z\.\-]+/ }

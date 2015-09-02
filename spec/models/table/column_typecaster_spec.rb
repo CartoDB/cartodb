a832c9d99fe9008b5cfbde0a6d03be1e4adf0778
Rafa de la Torre <rtorre@cartodb.com>
2015-09-02 19:11:16 +0200

    Add test for column cast #2202

--- spec/models/table/column_typecaster_spec.rb
+++ spec/models/table/column_typecaster_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative '../../rspec_configuration'
 require_relative '../../../app/models/table/column_typecaster'
+require_relative '../../spec_helper'
 
 describe CartoDB::ColumnTypecaster do
 
@@ -46,5 +47,34 @@ describe CartoDB::ColumnTypecaster do
         .should == %Q{replace(replace("my_column", ',', ''), '.', '.')}
     end
   end
-end
 
+  describe '#date_to_number' do
+    it 'casts a date column to its unix timestamp' do
+      stub_named_maps_calls
+      table = new_table(:user_id => $user_1.id)
+      table.force_schema = "mydate timestamp with time zone"
+      table.save
+
+      birthday = '1980-05-31'.to_time
+      table.insert_row!(mydate: birthday)
+      table.insert_row!(mydate: nil)
+
+      column_typecaster = CartoDB::ColumnTypecaster.new({
+          user_database: $user_1.in_database,
+          schema: $user_1.database_schema,
+          table_name: table.name,
+          column_name: 'mydate',
+          new_type: 'number'
+        })
+      column_typecaster.run
+
+      table.schema(reload: true).should include([:mydate, "number"])
+      rows = table.run_query("select mydate from #{table.name} order by cartodb_id")[:rows]
+      rows.first[:mydate].to_i.should == birthday.to_i
+      rows.last[:mydate].should == nil
+
+      table.destroy
+    end
+  end
+
+end

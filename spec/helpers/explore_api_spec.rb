2c0b9bf42bbeb0086e3ae9536d28342bfe4a7601
Mario de Frutos <mario.defrutos@cartodb.com>
2015-09-03 19:52:34 +0200

    Check for visualizations without associated maps

--- spec/helpers/explore_api_spec.rb
+++ spec/helpers/explore_api_spec.rb
@@ -62,6 +62,15 @@ describe 'Helpers' do
       geometry_data.should eq expected_data
     end
 
+    it 'should return empty if there is no map associated to the visualization' do
+      user = FactoryGirl.build(:user)
+      visualization = FactoryGirl.build(:derived_visualization, :user_id => user.id)
+      visualization.stubs(:map).returns(nil)
+      geometry_data = @explore_api.get_geometry_data(visualization)
+      expected_data = {}
+      geometry_data.should eq expected_data
+    end
+
     it 'should return the visualization insertion values in order with the table columns' do
       user_options = {
         id: 'dcb16d4e-e74a-4764-b509-1b5f682238b5',
@@ -97,7 +106,36 @@ describe 'Helpers' do
       visualization.stubs(:layers).with(:carto_and_torque).returns([layer_1])
       bbox_values = {visualization.id => '0103000000010000000500000069B6E1BBD0FD69C17200D8285370474169B6E1BBD0FD69C1E6DDC1415E6B574120FFD8C8C1365FC1E6DDC1415E6B574120FFD8C8C1365FC17200D8285370474169B6E1BBD0FD69C17200D82853704741'}
       insert_values = @explore_api.get_visualizations_values_for_insert([visualization],bbox_values)
-      insert_values[0].should eq "('ffb16d4e-e74a-4764-b509-1b5f682238b5', 'vis_name', 'vis_description','derived','false', '{\\\"user_name_1\\\".table_1}', '{lala,lele}', ST_AsText('0103000000010000000500000069B6E1BBD0FD69C17200D8285370474169B6E1BBD0FD69C1E6DDC1415E6B574120FFD8C8C1365FC1E6DDC1415E6B574120FFD8C8C1365FC17200D8285370474169B6E1BBD0FD69C17200D82853704741'), ST_Transform(ST_Envelope('SRID=4326;POLYGON((-179.0 -85.0511, -179.0 85.0511, 179.0 85.0511, 179.0 -85.0511, -179.0 -85.0511))'::geometry), 3857),ST_GeomFromText('POINT(0 30)',3857), 3,'2015-08-13 06:13:15 +0000', '2015-09-03 06:12:15 +0000', '','vis_title',1,0, 'dcb16d4e-e74a-4764-b509-1b5f682238b5', 'user_test', NULL, 'twitter_user_test','website_test', 'avatar_url_test', 'true')"
+      insert_values[0].should eq "('ffb16d4e-e74a-4764-b509-1b5f682238b5','vis_name','vis_description','derived','false','{\\\"user_name_1\\\".table_1}','{lala,lele}',ST_AsText('0103000000010000000500000069B6E1BBD0FD69C17200D8285370474169B6E1BBD0FD69C1E6DDC1415E6B574120FFD8C8C1365FC1E6DDC1415E6B574120FFD8C8C1365FC17200D8285370474169B6E1BBD0FD69C17200D82853704741'),ST_Transform(ST_Envelope('SRID=4326;POLYGON((-179.0 -85.0511, -179.0 85.0511, 179.0 85.0511, 179.0 -85.0511, -179.0 -85.0511))'::geometry), 3857),ST_GeomFromText('POINT(0 30)',3857),3,'2015-08-13 06:13:15 +0000','2015-09-03 06:12:15 +0000','','vis_title',1,0,'dcb16d4e-e74a-4764-b509-1b5f682238b5','user_test',NULL,'twitter_user_test','website_test','avatar_url_test','true')"
+    end
+
+    it 'should return the visualization insertion values in order with the table columns without geomery data' do
+      user_options = {
+        id: 'dcb16d4e-e74a-4764-b509-1b5f682238b5',
+        username: 'user_test',
+        twitter_username: 'twitter_user_test',
+        website: 'website_test',
+        avatar_url: 'avatar_url_test',
+        available_for_hire: true
+      }
+      user = FactoryGirl.build(:user, user_options)
+      visualization_options = {
+        :id => 'ffb16d4e-e74a-4764-b509-1b5f682238b5',
+        :user_id => user.id,
+        :name => 'vis_name',
+        :description => 'vis_description',
+        :created_at => '2015-08-13 06:13:15+00',
+        :updated_at => '2015-09-03 06:12:15+00',
+        :title => 'vis_title',
+        :tags => ['lala', 'lele']
+      }
+      visualization = FactoryGirl.build(:derived_visualization, visualization_options)
+      visualization.stubs(:user).returns(user)
+      visualization.stubs(:synchronization).returns(Hash.new)
+      visualization.stubs(:likes_count).returns(1)
+      visualization.stubs(:map_views).returns(10)
+      insert_values = @explore_api.get_visualizations_values_for_insert([visualization],{})
+      insert_values[0].should eq "('ffb16d4e-e74a-4764-b509-1b5f682238b5','vis_name','vis_description','derived','false','{}','{lala,lele}',NULL,NULL,NULL,NULL,'2015-08-13 06:13:15 +0000','2015-09-03 06:12:15 +0000','','vis_title',1,0,'dcb16d4e-e74a-4764-b509-1b5f682238b5','user_test',NULL,'twitter_user_test','website_test','avatar_url_test','true')"
     end
   end
 

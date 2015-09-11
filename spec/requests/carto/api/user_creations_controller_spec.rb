f808ee63666cc33dfae4c2eb446673579f2c3749
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-09-11 13:05:27 +0200

    Google auto authentication test

--- spec/requests/carto/api/user_creations_controller_spec.rb
+++ spec/requests/carto/api/user_creations_controller_spec.rb
@@ -35,9 +35,26 @@ describe Carto::Api::UserCreationsController do
         response.body[:requires_validation_email].should == user_creation.requires_validation_email?
         response.body[:state].should == user_creation.state
       end
-
     end
 
+    it 'triggers user_creation authentication for google users' do
+      User.any_instance.stubs(:create_in_central).returns(true)
+      User.any_instance.stubs(:enable_remote_db_user).returns(true)
+      user_data = FactoryGirl.build(:valid_user)
+      user_data.organization = @organization
+      user_data.google_sign_in = true
+
+      Carto::Api::UserCreationsController.any_instance.expects(:authenticate!).with(:user_creation, scope: user_data.organization.name).once
+
+      user_creation = Carto::UserCreation.new_user_signup(user_data)
+      user_creation.next_creation_step until user_creation.finished?
+
+      get_json api_v1_user_creations_show_url(id: user_creation.id), @headers do |response|
+        response.body[:state].should == 'success'
+        response.body[:google_sign_in].should == user_creation.google_sign_in
+        response.body[:enable_account_token].should be_nil
+      end
+    end
   end
 
 end

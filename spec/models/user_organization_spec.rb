047ccd9ef8c0fd2083e6b95d31c1a9b0b7a81e7e
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-09-15 19:18:13 +0200

    Code style

--- spec/models/user_organization_spec.rb
+++ spec/models/user_organization_spec.rb
@@ -39,7 +39,7 @@ describe UserOrganization do
       User.any_instance.stubs(:update_in_central).returns(true)
       @organization = Organization.new(quota_in_bytes: 1234567890, name: 'wadus', seats: 5).save
 
-      @owner = create_user(:quota_in_bytes => 524288000, :table_quota => 500)
+      @owner = create_user(quota_in_bytes: 524288000, table_quota: 500)
       @owner.in_database.run('create table no_cartodbfied_table (test integer)')
 
       @owner.real_tables.count.should == 1
@@ -50,7 +50,5 @@ describe UserOrganization do
 
       @owner.real_tables.count.should == 1
     end
-
   end
-
 end

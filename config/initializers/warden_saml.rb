ee0e28314baa5fb324920f2057cf42f55b428278
cartodb <datavis@bloomberg.net>
2015-09-15 09:40:30 -0400

    Create an user based on the user_information table

--- config/initializers/warden_saml.rb
+++ config/initializers/warden_saml.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 
 Warden::Strategies.add(:saml_header) do
 
@@ -10,66 +11,40 @@ Warden::Strategies.add(:saml_header) do
   end
 
   def authenticate!
-    user = User.where(username: SamlAuthenticator.username_from_saml(params)).first
+    user_info = SamlAuthenticator.get_user_info(params)
+    user = SamlAuthenticator.check_user(user_info)
     user ? success!(user) : fail!
   end
 end 
  
 class SamlAuthenticator
 
-  # Modify the signature and test the failure
-  def self.testSignature(params)
-     xmlstr =  Base64.decode64(params[:SAMLResponse])
-        
-      puts Base64.decode64(params[:SAMLResponse])
+  def self.get_user_info(params)
+   
+     response = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+   
+     response.settings = get_saml_settings
+   
+     if response.is_valid?
+   
+       user_data = UserInfo.where(uuid: response.nameid).first
+       return user_data
+   
+     else
+   
+       puts "Response Invalid. Errors: #{response.errors}"
 
-     signStart = xmlstr =~/<ds:SignatureValue>/
-     signEnd = xmlstr =~/<\/ds:SignatureValue>/
-     sign = xmlstr[signStart+"<ds:SignatureValue>".length, signEnd - signStart]
-     puts "found the signature as  #{sign}"
-     #xmlstr[signStart+"<ds:SignatureValue>".length, 2]= 'xx'
-     sign = xmlstr[signStart+"<ds:SignatureValue>".length, signEnd - signStart]
-     puts "changed signature is #{sign}"
-     puts "after modifying"
-     puts xmlstr
-     xmlstr
+     end
+
+     return nil
+
   end
 
-  def self.username_from_saml(params)
-   
-   puts Base64.decode64(params[:SAMLResponse])
-   
-   # Test the signature
-   xmlstr =  testSignature(params) 
-   
-   response = OneLogin::RubySaml::Response.new(xmlstr)
-   
-   response.settings = get_saml_settings
-   
-   puts "The name id is #{response.nameid}"
-   if response.is_valid?
-      puts "It's a valid response"
-      response.nameid
-   else
-      puts "Invalid response"
-      puts "Response Invalid. Errors: #{response.errors}"
-      response.errors
-      nil
-   end
-  end
+  def self.get_saml_settings
+    settings = OneLogin::RubySaml::Settings.new
 
-   def self.get_saml_settings
-       settings = OneLogin::RubySaml::Settings.new
-
-       settings.soft = true
-      # settings.assertion_consumer_service_url = "/bloomberg_sso/scs"
-      # settings.assertion_consumer_logout_service_url = "/bloomberg_sso/logout"
-       #settings.issuer                         = "/bloomberg_sso/metadata"
-
-       #settings.idp_entity_id                  = "https://app.onelogin.com/saml/metadata/<onelogin-app-id>"
-       #settings.idp_sso_target_url             = "https://app.onelogin.com/trust/saml2/http-post/sso/<onelogin-app-id>"
-       #settings.idp_slo_target_url             = "https://app.onelogin.com/trust/saml2/http-redirect/slo/<onelogin-app-id>"
-       settings.idp_cert                       = "-----BEGIN CERTIFICATE-----
+    settings.soft = true
+    settings.idp_cert                       = "-----BEGIN CERTIFICATE-----
 MIIDNDCCAhygAwIBAgIIRVI0ZQSoAHEwDQYJKoZIhvcNAQEEBQAwgZUxCzAJBgNV
 BAYTAlVTMREwDwYDVQQIEwhOZXcgWW9yazEWMBQGA1UEBxMNTmV3IFlvcmsgQ2l0
 eTEVMBMGA1UEChQMQmxvb21iZXJnIExQMRkwFwYDVQQLExBJbnRlcm5hbCBTeXN0
@@ -89,22 +64,36 @@ Qt9ym/tbZCrsJZNEc/TYIhRB6wqeRR94ItV5QjYpspdiw6MAsgZIoZvxuzePq5ov
 BxU+B/zVosVn9V8JA1p5CfKMpfD27It6KUE0nJp5ODAFJZqFBakeaLtNRkN7lR6f
 YGD+vIsxIbs=
 -----END CERTIFICATE-----"
-    #settings.idp_cert_fingerprint           = "3B:05:BE:0A:EC:84:CC:D4:75:97:B3:A2:22:AC:56:21:44:EF:59:E6"
-    #settings.idp_cert_fingerprint_algorithm = XMLSecurity::Document::SHA1
-
-    #settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-
-                
-    #settings.security[:authn_requests_signed] = false
-    #settings.security[:logout_requests_signed] = false
-    #settings.security[:logout_responses_signed] = false
-    #settings.security[:metadata_signed] = false
-    #settings.security[:digest_method] = XMLSecurity::Document::SHA1
-    #settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
 
     settings
-end
+  end
 
+
+  def self.check_user(user_data)
+    
+    existing_user = User.where("email = '#{user_data.email}' OR username = '#{user_data.username}'").first
+
+    if (existing_user != nil) 
+        return existing_user
+    end
+
+    user = User.new
+    set_user_attributes(user, user_data)
+
+    user.save(raise_on_failure: true)
+    user.create_in_central
+
+    user
+  end
+
+
+  def self.set_user_attributes(user, data)
+    user.username = data.username
+    user.email = data.email
+    user.password = "123456"
+    user.password_confirmation = user.password
+  end
 
 end 
+
 

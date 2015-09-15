a7ce8b54e04c7ec9b985f2d6521e0d873c752dbd
Mario de Frutos <mario.defrutos@cartodb.com>
2015-09-15 17:52:23 +0200

    Added unsubscribe link and check for subscription to finished geocoding mail

--- app/mailers/geocoder_mailer.rb
+++ app/mailers/geocoder_mailer.rb
@@ -11,20 +11,27 @@ class GeocoderMailer < ActionMailer::Base
     @geocoded_rows = geocoded_rows
     @all_rows_geocoded = (processable_rows - geocoded_rows == 0)
     @link = "#{user.public_url}#{CartoDB.path(self, 'public_tables_show', { id: @table_name })}"
+    @unsubscribe_link = generate_unsubscribe_link(user, Carto::Notification::GEOCODING_NOTIFICATION)
 
-    mail ({:to => user.email, :subject => @subject})
+    mail :to => user.email,
+         :subject => @subject
   end
 
   private
 
-    def set_subject(state)
-      if @geocoding_failed
-        subject = "Your CartoDB dataset geocoding failed"
-      else
-        subject = "Your CartoDB dataset geocoding just finished"
-      end
-
-      subject
+  def set_subject(state)
+    if @geocoding_failed
+      subject = "Your CartoDB dataset geocoding failed"
+    else
+      subject = "Your CartoDB dataset geocoding just finished"
     end
+
+    subject
+  end
+
+  def generate_unsubscribe_link(user, notification_type)
+    hash = Carto::UserNotification.generate_unsubscribe_hash(user, notification_type)
+    return "#{user.public_url}#{CartoDB.path(self, 'notifications_unsubscribe', { notification_hash: hash })}"
+  end
 
 end

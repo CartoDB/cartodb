4378864f76414c2d076e8ca128764f6fe8f70472
Mario de Frutos <mario.defrutos@cartodb.com>
2015-09-16 13:45:55 +0200

    Don't show the explore link if the user don't have the flag activated

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,9 +3,9 @@
 class ExploreController < ApplicationController
   layout 'explore'
 
+  before_filter :get_viewed_user
+
   def index
-    username = CartoDB.extract_subdomain(request).strip.downcase
-    @viewed_user = User.where(username: username).first
     render_404 and return if @viewed_user.nil? || !@viewed_user.has_feature_flag?("explore_site")
     @default_fallback_basemap = @viewed_user.default_basemap
 
@@ -15,12 +15,17 @@ class ExploreController < ApplicationController
   end
 
   def search
-    username = CartoDB.extract_subdomain(request).strip.downcase
-    @viewed_user = User.where(username: username).first
     render_404 and return if @viewed_user.nil? || !@viewed_user.has_feature_flag?("explore_site")
     @query_param = params[:q]
     respond_to do |format|
       format.html { render 'search' }
     end
+  end
+
+  private
+
+  def get_viewed_user
+    username = CartoDB.extract_subdomain(request).strip.downcase
+    @viewed_user = User.where(username: username).first
   end
 end

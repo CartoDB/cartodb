93813fe29e13019af3f52fe192d261fe4ac0885a
Mario de Frutos <mario.defrutos@cartodb.com>
2015-09-16 12:39:27 +0200

    Use viewed user instead of current user to disable explore access

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -4,9 +4,9 @@ class ExploreController < ApplicationController
   layout 'explore'
 
   def index
-    render_404 and return if current_user.nil? || !current_user.has_feature_flag?("explore_site")
     username = CartoDB.extract_subdomain(request).strip.downcase
     @viewed_user = User.where(username: username).first
+    render_404 and return if @viewed_user.nil? || !@viewed_user.has_feature_flag?("explore_site")
     @default_fallback_basemap = @viewed_user.default_basemap
 
     respond_to do |format|
@@ -15,7 +15,9 @@ class ExploreController < ApplicationController
   end
 
   def search
-    render_404 and return if current_user.nil? || !current_user.has_feature_flag?("explore_site")
+    username = CartoDB.extract_subdomain(request).strip.downcase
+    @viewed_user = User.where(username: username).first
+    render_404 and return if @viewed_user.nil? || !@viewed_user.has_feature_flag?("explore_site")
     @query_param = params[:q]
     respond_to do |format|
       format.html { render 'search' }

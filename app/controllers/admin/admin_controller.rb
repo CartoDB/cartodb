c28feb05fbecb78cc662a0e6d1175b60275eb163
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-09-17 11:57:43 +0200

    Adds new AdminController that disallows framing by default

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,8 @@
+class Admin::AdminController < ApplicationController
+  before_filter :x_frame_options_deny
+
+  protected
+  def x_frame_options_deny
+    response.headers['X-Frame-Options'] = 'DENY'
+  end
+end

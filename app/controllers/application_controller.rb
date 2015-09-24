c3e41f7c4281237237c83e86f44ffb5f1d3a33ca
Mario de Frutos <mario.defrutos@cartodb.com>
2015-09-24 18:23:10 +0200

    Change max age and refactor of headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,23 +85,25 @@ class ApplicationController < ActionController::Base
 
   def cors_preflight_check
     if request.method == :options && check_cors_headers_for_whitelisted_referer
-      response.headers['Access-Control-Allow-Origin'] = request.headers['origin']
-      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE'
-      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-      response.headers['Access-Control-Max-Age'] = '172800'
+      common_cors_headers
+      response.headers['Access-Control-Max-Age'] = '3600'
     end
   end
 
   def allow_cross_domain_access
     if !request.headers['origin'].blank? && check_cors_headers_for_whitelisted_referer
-      response.headers['Access-Control-Allow-Origin'] = request.headers['origin']
-      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE'
+      common_cors_headers
       response.headers['Access-Control-Allow-Credentials'] = 'true'
-      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     elsif !Rails.env.production? && !Rails.env.staging?
       response.headers['Access-Control-Allow-Origin'] = '*'
       response.headers['Access-Control-Allow-Methods'] = '*'
     end
+  end
+
+  def common_cors_headers
+    response.headers['Access-Control-Allow-Origin'] = request.headers['origin']
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
 
   def check_cors_headers_for_whitelisted_referer

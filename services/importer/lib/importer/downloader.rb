13f9b1abb8d0784992bf20e66fe1a1db03f6d894
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-09-29 12:40:27 +0200

    Refactoring constants 5704

--- services/importer/lib/importer/downloader.rb
+++ services/importer/lib/importer/downloader.rb
@@ -24,8 +24,9 @@ module CartoDB
       DEFAULT_HTTP_REQUEST_TIMEOUT = 600
       URL_ESCAPED_CHARACTERS = 'áéíóúÁÉÍÓÚñÑçÇàèìòùÀÈÌÒÙ'
 
-      EXTS = CartoDB::Importer2::Unp::SUPPORTED_FORMATS.concat(CartoDB::Importer2::Unp::COMPRESSED_EXTENSIONS).join('|')
-      URL_FILENAME_REGEX = Regexp.new("\\w+(#{EXTS})+", true)
+      SUPPORTED_EXTENSIONS = CartoDB::Importer2::Unp::SUPPORTED_FORMATS
+                            .concat(CartoDB::Importer2::Unp::COMPRESSED_EXTENSIONS)
+      URL_FILENAME_REGEX = Regexp.new("\\w+(#{SUPPORTED_EXTENSIONS.join('|')})+", true)
       
       DEFAULT_FILENAME        = 'importer'
       CONTENT_DISPOSITION_RE  = %r{;\s*filename=(.*;|.*)}

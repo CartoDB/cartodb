d6d406010b616ac5b33f0a2a2f5b5bd778381e5d
Jesús <jesusvzpg@gmail.com>
2015-09-30 11:46:39 +0200

    Added parallel env to the prepare-test-db script. BTW this scripts are not going to belong to the cartodb repo. They are temporal files till we perform the right solution

--- prepare-db.sh
+++ prepare-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-MOCHA_OPTIONS=skip_integration RAILS_ENV=test RAILS_DATABASE_FILE=$1 bundle exec rake cartodb:test:prepare
+MOCHA_OPTIONS=skip_integration PARALLEL=true RAILS_ENV=test RAILS_DATABASE_FILE=$1 bundle exec rake cartodb:test:prepare
 

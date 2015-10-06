db57ecc87f304a91263452c0e3a7c8972c26442c
Kartones <kartones@cartodb.com>
2015-10-06 15:45:21 +0200

    #3385 avoiding AR cache errors

--- app/models/carto/shared_entity.rb
+++ app/models/carto/shared_entity.rb
@@ -5,11 +5,15 @@ module Carto
   class SharedEntity < ActiveRecord::Base
     ENTITY_TYPE_VISUALIZATION = CartoDB::SharedEntity::ENTITY_TYPE_VISUALIZATION
 
-    scope :shared_visualizations, where(:entity_type => ENTITY_TYPE_VISUALIZATION)
+    scope :shared_visualizations, where(entity_type: ENTITY_TYPE_VISUALIZATION)
 
     belongs_to :visualization, foreign_key: :entity_id, inverse_of: :shared_entities
 
     attr_accessible :entity_id, :recipient_id
+
+    def self.columns
+      super.reject { |c| c.name == "id" }
+    end
 
   end
 end

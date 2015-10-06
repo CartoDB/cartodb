76114c8ea972cc775a024a2d8e496ef188f6f3cb
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-10-06 10:59:20 +0200

    Adds nullity check in presenter 5778

--- app/controllers/carto/api/data_import_presenter.rb
+++ app/controllers/carto/api/data_import_presenter.rb
@@ -106,7 +106,7 @@ module Carto
         values.merge!(original_url: @data_import.original_url)
         values.merge!(data_type: @data_import.data_type)
 
-        if !@data_import.http_response_code.start_with?('2')
+        if !@data_import.http_response_code.nil? && !@data_import.http_response_code.start_with?('2')
           values.merge!(http_response_code: @data_import.http_response_code)
           values.merge!(http_response_code_message: get_http_response_code_message(@data_import.http_response_code))
         end

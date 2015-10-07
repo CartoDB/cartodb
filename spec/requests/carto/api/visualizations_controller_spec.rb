4fc6763f48e7f7bb4cf3f9a79c5b8bea51ad4340
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-10-07 17:31:58 +0200

    WIP

--- spec/requests/carto/api/visualizations_controller_spec.rb
+++ spec/requests/carto/api/visualizations_controller_spec.rb
@@ -1826,6 +1826,14 @@ describe Carto::Api::VisualizationsController do
         end
       end
 
+      xit 'returns 200 with valid shared user (current_viewer) user_domain, with current_user being the owner' do
+        ApplicationController.any_instance.stubs(:current_user).returns(@vis_owner)
+        ApplicationController.any_instance.stubs(:current_viewer).returns(@shared_user)
+        get_json url(@shared_user.username, @shared_vis.id, @shared_user.api_key), {}, @headers do |response|
+          response.status.should == 200
+        end
+      end
+
       it 'returns 404 if visualization does not exist' do
         random_uuid = UUIDTools::UUID.timestamp_create.to_s
         get_json url(@vis_owner.username, random_uuid, @vis_owner.api_key), {}, @headers do |response|
@@ -1856,9 +1864,32 @@ describe Carto::Api::VisualizationsController do
         end
       end
 
+      xit 'returns 404 if user at url is empty, current_user is the owner and current_viewer has permission' do
+        ApplicationController.any_instance.stubs(:current_user).returns(@vis_owner)
+        ApplicationController.any_instance.stubs(:current_viewer).returns(@shared_user)
+        login_as(@shared_user, scope: @shared_user.organization.name)
+        get_json url(nil, @shared_vis.id, @shared_user.api_key), {}, @headers do |response|
+          response.status.should == 404
+          response.body[:errors].should == 'Visualization of that user does not exist'
+        end
+      end
+
       it 'returns 404 if user at url does not match visualization owner' do
         app = ApplicationController.any_instance
         app.stubs(:current_user).returns(@shared_user)
+        app.stubs(:current_viewer).returns(@shared_user)
+        app.stubs(:api_authorization_required).returns(true)
+
+        login_as(@shared_user, scope: @shared_user.organization.name)
+        get_json url(@not_shared_user.username, @shared_vis.id, @shared_user.api_key), {}, @headers do |response|
+          response.status.should == 404
+          response.body[:errors].should == 'Visualization of that user does not exist'
+        end
+      end
+
+      xit 'returns 404 if user at url does not match visualization owner with current_user being the owner and current_viewer the shared to' do
+        app = ApplicationController.any_instance
+        app.stubs(:current_user).returns(@vis_owner)
         app.stubs(:current_viewer).returns(@shared_user)
         app.stubs(:api_authorization_required).returns(true)
 

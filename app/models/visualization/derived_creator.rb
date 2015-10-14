f477252d3b7b43341c6205adfbdfa1ac3517f852
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-10-14 17:15:41 +0200

    Code refactor 5880

--- app/models/visualization/derived_creator.rb
+++ app/models/visualization/derived_creator.rb
@@ -24,12 +24,12 @@ module CartoDB
         map = blender.blend
 
         vis = CartoDB::Visualization::Member.new({
-            name: beautify_name,
-            map_id: map.id,
-            type: CartoDB::Visualization::Member::TYPE_DERIVED,
-            privacy: blender.blended_privacy,
-            user_id: user.id
-            })
+          name: beautify_name,
+          map_id: map.id,
+          type: CartoDB::Visualization::Member::TYPE_DERIVED,
+          privacy: blender.blended_privacy,
+          user_id: user.id
+          })
 
         CartoDB::Visualization::Overlays.new(vis).create_default_overlays
         vis.store

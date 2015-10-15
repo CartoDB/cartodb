7c678fc215124ca6c2cb28a69e90d3b371519925
Jesús <jesusvzpg@gmail.com>
2015-10-15 12:27:06 +0200

    Cleaning branch. Should remove scripts from repo too

--- prepare-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Jesus Vazquez
-# prepare-db.sh: This scripts drops and then create the database specified by param.
-MOCHA_OPTIONS=skip_integration PARALLEL=true RAILS_ENV=test RAILS_DATABASE_FILE=$1 bundle exec rake cartodb:test:prepare >> database-creation.log 2>&1;
-

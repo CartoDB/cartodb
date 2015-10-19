2731ad90ec518af74872ef1d073f7dfe6fe6430a
Alejandro Martínez <zen@itram.es>
2015-10-19 16:03:45 +0200

    Style fixes

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,20 +1,18 @@
 # encoding: utf-8
 
 FactoryGirl.define do
-
-  factory :carto_group, :class => Carto::Group do
+  factory :carto_group, class: Carto::Group do
     name 'A_Group'
     display_name 'A Group'
     database_role 'database_role'
   end
 
-  factory :new_random_group, :class => Carto::Group do |g|
+  factory :new_random_group, class: Carto::Group do |_g|
     name { "g_#{String.random(4)}" }
-    display_name  { "Group #{name}" }
+    display_name { "Group #{name}" }
 
     factory :random_group do
       database_role { "database_role_#{name}" }
     end
   end
-
 end

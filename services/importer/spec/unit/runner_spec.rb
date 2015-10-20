a96ce44c6714e8322463f3fa9c08cdb1a8b6fa8b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-10-20 21:28:20 +0200

    spec_helper must be loaded before doubles load in order to avoid TypeError

--- services/importer/spec/unit/runner_spec.rb
+++ services/importer/spec/unit/runner_spec.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+require_relative '../../../../spec/spec_helper'
+require_relative '../../../../spec/rspec_configuration'
 require_relative '../../lib/importer/runner'
 require_relative '../../lib/importer/job'
 require_relative '../../lib/importer/downloader'
@@ -12,8 +14,6 @@ require_relative '../doubles/loader'
 require_relative '../doubles/user'
 require_relative '../doubles/input_file_size_limit'
 require_relative '../doubles/table_row_count_limit'
-require_relative '../../../../spec/rspec_configuration'
-require_relative '../../../../spec/spec_helper'
 
 include CartoDB::Importer2
 

8959f63c9cc1031e185dc0c3834b6ed8b1a35834
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2015-10-22 12:43:50 +0200

    Nobody is using this now

--- src/geo/ui/widgets/widget_model.js
+++ src/geo/ui/widgets/widget_model.js
@@ -1,13 +1,4 @@
 
 cdb.geo.ui.Widget.Model = cdb.core.Model.extend({
 
-  bindDashboardInstance: function(dashboardInstance) {
-    dashboardInstance.bind('change:layergroupid', function(dashboardInstance) {
-      this.set({
-        dashboardBaseURL: dashboardInstance.getBaseURL(),
-        urls: dashboardInstance.getTiles()
-      });
-    }.bind(this));
-  }
-  
 });

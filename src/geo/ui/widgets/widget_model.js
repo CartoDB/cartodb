1214721c253cbfa6ba74a041c41be4b54edd2038
xavijam <xavijam@gmail.com>
2015-10-26 11:11:18 +0100

    Added first simple tests for widget view

--- src/geo/ui/widgets/widget_model.js
+++ src/geo/ui/widgets/widget_model.js
@@ -1,4 +1,12 @@
 
 cdb.geo.ui.Widget.Model = cdb.core.Model.extend({
 
+  defaults: {
+    data: []
+  },
+
+  getData: function() {
+    return this.get('data');
+  }
+
 });
--- test/spec/geo/ui/widget.spec.js
+++ test/spec/geo/ui/widget.spec.js
@@ -1,26 +1,31 @@
 describe('widget', function() {
 
   beforeEach(function() {
-    this.datasource = jasmine.createSpyObj('Datasource', ['addWidgetModel']);
-    this.datasource.addWidgetModel.and.returnValue(new cdb.core.Model());
-    this.view = new cdb.geo.ui.Widget.View({
-      datasource: this.datasource
+    // this.datasource = jasmine.createSpyObj('Datasource', ['addWidgetModel']);
+    // this.datasource.addWidgetModel.and.returnValue(new cdb.core.Model());
+    this.model = new cdb.geo.ui.Widget.Model({
+      id: 'widget_1',
+      options: {
+        title: 'Hello widget',
+        columns: ['cartodb_id', 'description']
+      }
     });
-    // spyOn(this.view.dataModel, 'bind');
-    // spyOn(this.view.dataModel, 'unbind');
-    // spyOn(this.view.viewModel, 'bind');
-    // spyOn(this.view.viewModel, 'unbind');
+    this.view = new cdb.geo.ui.Widget.View({
+      model: this.model
+    });
+    this.view.render();
+  });
+
+  it('should have 3 subviews, content, loader and error panes', function() {
+    expect(_.size(this.view._subviews)).toBe(3);
+  });
+
+  it('should have defined "_createContentView" method', function() {
+    expect(this.view._createContentView).toBeDefined();
   });
 
   it('should not have bindings when view is cleaned', function() {
     this.view.clean();
-    // debugger;
-    // console.log(this.view.dataModel.bind);
-    // console.log(this.view.viewModel.bind.calls);
-  });
-
-  afterEach(function() {
-
   });
 
 });

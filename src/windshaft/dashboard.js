decc89f2e121ca76792285ec9c75b647e59b212f
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2015-11-03 16:19:54 +0100

    Client uses success and error callbacks

--- src/windshaft/dashboard.js
+++ src/windshaft/dashboard.js
@@ -1,3 +1,6 @@
+// TODO:
+//  - Instead of using this.layers, we should only consider visible layers for LayerGroups
+//  - createInstance callback
 cdb.windshaft.Dashboard = function(options) {
   BOUNDING_BOX_FILTER_WAIT = 500;
 
@@ -12,49 +15,13 @@ cdb.windshaft.Dashboard = function(options) {
 
   this.instance = new cdb.windshaft.DashboardInstance();
 
-  this.boundingBoxFilter = new cdb.windshaft.filters.BoundingBoxFilter(this.map.getViewBounds());
-
   // Bindings
   this.layerGroup.bindDashboardInstance(this.instance);
   this.layers.bind('change', this.createInstance, this);
   this.filters.bind('change', this.createInstance, this);
-
-  // When the instance has changed, we need to update some models (eg: widgets) in this class
-  // with the information that the instance contains.
-  var self = this;
-  this.instance.bind('change:layergroupid', function(dashboardInstance) {
-
-    // TODO: Set the URL of the attributes service once it's available
-    this.layerGroup.set({
-      urls: dashboardInstance.getTiles()
-    });
-
-    this._updateWidgetURLs();
-  }.bind(this));
-
   this.map.bind('change:center change:zoom', _.debounce(this._boundingBoxChanged, BOUNDING_BOX_FILTER_WAIT), this);
-};
 
-cdb.windshaft.Dashboard.prototype._boundingBoxChanged = function() {
-  if (this.instance.isLoaded()) {
-    this.boundingBoxFilter.setBounds(this.map.getViewBounds());
-
-    this._updateWidgetURLs();
-  }
-};
-
-cdb.windshaft.Dashboard.prototype._updateWidgetURLs = function() {
-  var self = this;
-  this.instance.forEachWidget(function(widgetId, widgetMetadata) {
-    var widgetModel = self._getWidgetById(widgetId);
-
-    // TODO: Could be https
-    var url = widgetMetadata.url.http;
-    widgetModel.set({
-      'url': url,
-      'boundingBox': self.boundingBoxFilter.toString()
-    });
-  });
+  this.createInstance();
 };
 
 cdb.windshaft.Dashboard.prototype.createInstance = function() {
@@ -64,14 +31,47 @@ cdb.windshaft.Dashboard.prototype.createInstance = function() {
   });
   console.log(dashboardConfig);
 
-  var instance = this.client.instantiateMap(dashboardConfig, this.filters.toJSON());
-  instance.bind('change:layergroupid', function() {
-    this.instance.set(instance.toJSON());
-  }.bind(this));
+  this.client.instantiateMap({
+    mapDefinition: dashboardConfig,
+    filters: this.filters.toJSON(),
+    success: function(dashboardInstance) {
+
+      // Update the dashboard instance with the attributes of the new one
+      this.instance.set(dashboardInstance.toJSON());
+
+      // TODO: Set the URL of the attributes service once it's available
+      this.layerGroup.set({
+        urls: dashboardInstance.getTiles()
+      });
+
+      this._updateWidgetURLs();
+    }.bind(this),
+    error: function(error) {
+      console.log('Error creating dashboard instance: ' + error);
+    }
+  });
 
   return this.instance;
 };
 
-cdb.windshaft.Dashboard.prototype._getWidgetById = function(widgetId) {
-  return this.widgets.find({ id: widgetId });
+cdb.windshaft.Dashboard.prototype._boundingBoxChanged = function() {
+  if (this.instance) {
+    this._updateWidgetURLs();
+  }
 };
+
+cdb.windshaft.Dashboard.prototype._updateWidgetURLs = function() {
+  var self = this;
+  var boundingBoxFilter = new cdb.windshaft.filters.BoundingBoxFilter(this.map.getViewBounds());
+  this.instance.forEachWidget(function(widgetId, widgetMetadata) {
+    var widgetModel = self.widgets.find({ id: widgetId });
+
+    // TODO: Could be https
+    var url = widgetMetadata.url.http;
+    widgetModel.set({
+      'url': url,
+      'boundingBox': boundingBoxFilter.toString()
+    });
+  });
+};
+

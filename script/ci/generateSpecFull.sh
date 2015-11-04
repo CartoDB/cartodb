a61e536d0f347dbcd8eb7761cc68c3d288173d5b
jesusvazquez <jesusvzpg@gmail.com>
2015-11-04 11:02:16 +0100

    Removing Makefile.parallel dependency

--- script/ci/generateSpecFull.sh
+++ script/ci/generateSpecFull.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Jesus Vazquez
 truncate -s 0 specfull.txt
-cat Makefile.parallel| grep -v '# S' | grep -v '# Pending' | grep 'rb' | sed -e 's/^\s*//' -e '/^$/d' | \
-  sed '/^#/ d' | sed 's/\\//' | sed 's/\s.*$//' > temp.txt
+cat Makefile.parallel| grep -v 'spec/lib/varnish_spec.rb'| \
+grep -v 'spec/integrations/common_data_integration.rb'| \
+grep -v 'spec/models/visualization/table_blender.rb'| \
+grep -v 'services/user-mover/spec/user_mover_spec.rb'| \
+grep -v 'spec/requests/carto/api/layers_controller_spec.rb'| \
+grep 'rb'| sed -e 's/^\s*//' -e '/^$/d' | sed '/^#/ d' | sed 's/\\//' | sed 's/\s.*$//' > temp.txt
 
 i=6001;
 while read -r line

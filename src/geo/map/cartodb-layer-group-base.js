a771ed4e6de440d35e6f805878a5089e93411556
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2015-11-11 12:18:56 +0100

    Removed comment and simplified method

--- src/geo/map/cartodb-layer-group-base.js
+++ src/geo/map/cartodb-layer-group-base.js
@@ -25,33 +25,22 @@ var CartoDBLayerGroupBase = MapLayer.extend({
   },
 
   getTileJSONFromTiles: function(layerIndex) {
-    if (!this.get('urls')) {
+    var urls = this.get('urls');
+    if (!urls) {
       throw 'tileJSON for the layer cannot be calculated until urls are set';
     }
 
-    // Layergroup
-    var urls = this.get('urls');
-
-
-    var index = this._getIndexOfVisibleLayer(layerIndex);
-
-    // TODO: layerIndex should take into account the hidden layers.
-    // For example, for a layergroup, if the layerIndex is 1 but layer 0 is hidden, this method should
-    // use urls.grids[0]
     return {
       tilejson: '2.0.0',
       scheme: 'xyz',
-      grids: urls.grids[index],
+      grids: urls.grids[this._getIndexOfVisibleLayer(layerIndex)],
       tiles: urls.tiles,
       formatter: function(options, data) { return data; }
     };
   },
 
-  // Returns the position of a visible layer in relation to all layers when the map is 
-  // an "Anonymous Map". For example, if there are two CartoDB layers and layer #0 is
-  // hidden, this method would return -1 for #0 and 0 for layer #1.
   _getIndexOfVisibleLayer: function(layerIndex) {
-    throw "_getIndexOfVisibleLayer must be implemented"
+    throw "_getIndexOfVisibleLayer must be implemented";
   },
 
   fetchAttributes: function(layer, featureID, callback) {

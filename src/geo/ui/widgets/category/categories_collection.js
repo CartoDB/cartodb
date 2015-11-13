1b1dd7cd0bcfb93fcbf43ae1ad1bd6b057beb203
Raul Ochoa <rochoaf@gmail.com>
2015-11-13 18:55:34 +0100

    Comparator for 'Other' value

--- src/geo/ui/widgets/category/categories_collection.js
+++ src/geo/ui/widgets/category/categories_collection.js
@@ -7,7 +7,11 @@ var Backbone = require('backbone');
 module.exports = Backbone.Collection.extend({
 
   comparator: function(a,b) {
-    if (a.get('value') === b.get('value')) {
+    if (a.get('name') === 'Other') {
+      return 1;
+    } else if (b.get('name') === 'Other') {
+      return -1;
+    } else if (a.get('value') === b.get('value')) {
       return (a.get('selected') < b.get('selected')) ? 1 : -1;
     } else {
       return (a.get('value') < b.get('value')) ? 1 : -1;

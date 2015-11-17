f094263c4a6e94be3b2b2e6745e8cd6bd01e8f57
Nicklas Gummesson <nicklas@cartodb.com>
2015-11-17 14:17:45 +0100

    Remove unnecessary placeholder

--- src/cdb.js
+++ src/cdb.js
@@ -24,7 +24,6 @@ cdb.geo.ui.Widget.List = {};
 cdb.geo.ui.Widget.Category = {};
 cdb.geo.ui.Widget.Histogram = {};
 cdb.geo.ui.Widget.Formula = {};
-cdb.geo.ui.Widget.Time = {};
 
 cdb.ui = {};
 cdb.ui.common = {};

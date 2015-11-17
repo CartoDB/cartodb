9132d0a2f8115b6e5e83f82a2f98d8bb3f5630f6
Carla Iriberri <iriberrig@gmail.com>
2015-11-17 18:10:08 +0100

    Trying a normalization

--- app/controllers/carto/api/geocodings_controller.rb
+++ app/controllers/carto/api/geocodings_controller.rb
@@ -66,7 +66,7 @@ module Carto
         return head(400) if input.nil? && params[:table_name].present?
         render(json: []) and return if input.nil? || input.empty?
 
-        list = input.map{ |v| "'#{ v }'" }.join(",")
+        list = input.map{ |v| "'#{ v.downcase.gsub(/[^\p{Alnum}]/) }'" }.join(",")
 
         services = CartoDB::SQLApi.new({ username: 'geocoding', timeout: GEOCODING_SQLAPI_CALLS_TIMEOUT}).fetch("SELECT (admin0_available_services(Array[#{list}])).*")
 

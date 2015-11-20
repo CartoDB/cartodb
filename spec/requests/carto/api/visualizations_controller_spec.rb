f45692df7a54929bf530048ae9947c6ecf0d66bd
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-11-20 12:29:06 +0100

    Fixes failing tests

--- spec/requests/carto/api/visualizations_controller_spec.rb
+++ spec/requests/carto/api/visualizations_controller_spec.rb
@@ -778,7 +778,8 @@ describe Carto::Api::VisualizationsController do
 
         put api_v1_visualizations_update_url(user_domain: $user_2.username, api_key: $user_2.api_key, id: pub_vis_id),
              {
-               privacy: CartoDB::Visualization::Member::PRIVACY_PUBLIC
+                id: pub_vis_id,
+                privacy: CartoDB::Visualization::Member::PRIVACY_PUBLIC
              }.to_json, @headers
         last_response.status.should == 200
 
@@ -789,7 +790,8 @@ describe Carto::Api::VisualizationsController do
 
         put api_v1_visualizations_update_url(user_domain: $user_2.username, api_key: $user_2.api_key, id: priv_vis_id),
              {
-               privacy: CartoDB::Visualization::Member::PRIVACY_PRIVATE
+                id: priv_vis_id,
+                privacy: CartoDB::Visualization::Member::PRIVACY_PRIVATE
              }.to_json, @headers
         last_response.status.should == 200
 
@@ -841,6 +843,7 @@ describe Carto::Api::VisualizationsController do
                                                                                api_key: user_2.api_key,
                                                                                id: pub_vis_id)}",
             {
+              id: pub_vis_id,
               privacy: CartoDB::Visualization::Member::PRIVACY_PUBLIC
             }.to_json, @headers
         last_response.status.should == 200
@@ -855,6 +858,7 @@ describe Carto::Api::VisualizationsController do
                                                                                api_key: user_2.api_key,
                                                                                id: priv_vis_id)}",
             {
+              id: priv_vis_id,
               privacy: CartoDB::Visualization::Member::PRIVACY_PRIVATE
             }.to_json, @headers
         last_response.status.should == 200
@@ -922,7 +926,10 @@ describe Carto::Api::VisualizationsController do
         u1_vis_1_perm_id = body.fetch('permission').fetch('id')
         # By default derived vis from private tables are WITH_LINK, so setprivate
         put api_v1_visualizations_update_url(user_domain: user_1.username, id: u1_vis_1_id, api_key: user_1.api_key),
-            { privacy: CartoDB::Visualization::Member::PRIVACY_PRIVATE }.to_json, @headers
+            {
+              id: u1_vis_1_id,
+              privacy: CartoDB::Visualization::Member::PRIVACY_PRIVATE
+            }.to_json, @headers
         last_response.status.should == 200
 
         # Share vis with user_2 in readonly (vis can never be shared in readwrite)
@@ -962,7 +969,10 @@ describe Carto::Api::VisualizationsController do
         # Now with link
         # -------------
         put api_v1_visualizations_update_url(user_domain: user_1.username, id: u1_vis_1_id, api_key: user_1.api_key),
-            { privacy: CartoDB::Visualization::Member::PRIVACY_LINK }.to_json, @headers
+            {
+              id: u1_vis_1_id,
+              privacy: CartoDB::Visualization::Member::PRIVACY_LINK
+            }.to_json, @headers
         last_response.status.should == 200
 
         # Owner authenticated
@@ -986,7 +996,10 @@ describe Carto::Api::VisualizationsController do
         # Now public
         # ----------
         put api_v1_visualizations_update_url(user_domain: user_1.username, id: u1_vis_1_id, api_key: user_1.api_key),
-            { privacy: CartoDB::Visualization::Member::PRIVACY_LINK }.to_json, @headers
+            {
+              id: u1_vis_1_id,
+              privacy: CartoDB::Visualization::Member::PRIVACY_LINK
+            }.to_json, @headers
         last_response.status.should == 200
 
         get api_v2_visualizations_vizjson_url(user_domain: user_1.username, id: u1_vis_1_id, api_key: user_1.api_key)
@@ -1107,7 +1120,7 @@ describe Carto::Api::VisualizationsController do
         collection.should_not be_empty
 
         delete api_v1_visualizations_destroy_url(id: id, api_key: @api_key),
-          {}, @headers
+          {id: id}.to_json, @headers
         get api_v1_visualizations_index_url(api_key: @api_key),
           {}, @headers
 
@@ -1288,18 +1301,19 @@ describe Carto::Api::VisualizationsController do
         viz_id = JSON.parse(last_response.body).fetch('id')
 
         put api_v1_visualizations_show_url(user_domain: $user_1.username, id: viz_id, api_key: @api_key),
-            { privacy: 'PUBLIC' }.to_json, @headers
+            { privacy: 'PUBLIC', id: viz_id }.to_json, @headers
 
         get api_v2_visualizations_vizjson_url(user_domain: $user_1.username, id: viz_id, api_key: @api_key),
-            {}, @headers
+            { id: viz_id }, @headers
 
         last_response.status.should == 200
         last_response.headers.should have_key('Surrogate-Key')
         last_response['Surrogate-Key'].should include(CartoDB::SURROGATE_NAMESPACE_VIZJSON)
         last_response['Surrogate-Key'].should include(get_surrogate_key(CartoDB::SURROGATE_NAMESPACE_VISUALIZATION, viz_id))
 
-        delete api_v1_visualizations_show_url(user_domain: $user_1.username, id: viz_id, api_key: @api_key),
-               { }, @headers
+        delete api_v1_visualizations_destroy_url(user_domain: $user_1.username, id: viz_id, api_key: @api_key),
+               { id: viz_id }.to_json, @headers
+
       end
 
       it 'joins the attributions of the layers in a layergroup in the viz.json' do
@@ -1419,14 +1433,14 @@ describe Carto::Api::VisualizationsController do
         table2_visualization.update_attribute(:attributions, 'attribution 2')
 
         # Call to cache the vizjson after generating it
-        get api_v2_visualizations_vizjson_url(id: visualization.fetch('id'), api_key: @api_key),{}, @headers
+        get api_v2_visualizations_vizjson_url(id: visualization.fetch('id'), api_key: @api_key), { id: visualization.fetch('id') }, @headers
 
         # Now force a change
         put api_v1_visualizations_update_url(api_key: @api_key, id: table2_visualization.id),
-          { attributions: modified_table_2_attribution }.to_json, @headers
+          { attributions: modified_table_2_attribution, id: table2_visualization.id }.to_json, @headers
         last_response.status.should == 200
 
-        get api_v2_visualizations_vizjson_url(id: visualization.fetch('id'), api_key: @api_key),{}, @headers
+        get api_v2_visualizations_vizjson_url(id: visualization.fetch('id'), api_key: @api_key),{ id: visualization.fetch('id') }, @headers
         visualization = JSON.parse(last_response.body)
 
         layer_group_layer = visualization["layers"][1]

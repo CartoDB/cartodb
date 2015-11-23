40c806097dea39026421d34a867044430db61e0e
acanimal <asantiagop@gmail.com>
2015-11-23 17:58:12 +0100

    Attribution issue fixed

--- src/cdb.config.js
+++ src/cdb.config.js
@@ -2,7 +2,7 @@ var Config = require('./core/config');
 
 var config = new Config();
 config.set({
-  cartodb_attributions: "CartoDB <a href='http://cartodb.com/attributions' target='_blank'>attribution</a>",
+  cartodb_attributions: "CartoDB <a href=\"http://cartodb.com/attributions\" target=\"_blank\">attribution</a>",
   cartodb_logo_link: "http://www.cartodb.com"
 });
 

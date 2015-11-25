348c8f1f879225396d08cda36f514d2ff2fbff02
xavijam <xavijam@gmail.com>
2015-11-25 12:07:38 +0100

    Added first tests for items view

---
+++ test/spec/geo/ui/widgets/category/items_view.spec.js
@@ -0,0 +1,64 @@
+var _ = require('underscore');
+var CategoryModel = require('cdb/geo/ui/widgets/category/model.js');
+var ViewModel = require('cdb/geo/ui/widgets/category/models/view_model.js');
+var ItemsView = require('cdb/geo/ui/widgets/category/list/items_view.js');
+var WindshaftFiltersCategory = require('cdb/windshaft/filters/category');
+var $ = require('jQuery');
+
+describe('widgets/category/items_view', function() {
+
+  beforeEach(function() {
+    this.model = new CategoryModel(null, {
+      filter: new WindshaftFiltersCategory()
+    });
+    this.viewModel = new ViewModel();
+    this.view = new ItemsView({
+      viewModel: this.viewModel,
+      dataModel: this.model
+    });
+  });
+
+  describe('render', function() {
+
+    it('should render placeholder when data is empty', function() {
+      spyOn(this.view, '_renderPlaceholder').and.callThrough();
+      this.model.setCategories([]);
+      this.view.render();
+      expect(this.view._renderPlaceholder).toHaveBeenCalled();
+      expect(this.view.$el.hasClass('Widget-list--withBorders')).toBeTruthy();
+      expect(this.view.$el.hasClass('Widget-list--wrapped')).toBeFalsy();
+    });
+
+    it('should render properly a list of categories', function() {
+      this.model.setCategories([{ name: 'Hey' }, { name: 'test' }]);
+      this.view.render();
+      expect(this.view.$('.Widget-listGroup').length).toBe(1);
+      expect(this.view.$('.Widget-listItem').length).toBe(2);
+      expect(this.view.$el.hasClass('Widget-list--withBorders')).toBeFalsy();
+    });
+
+  });
+
+  describe('bind', function() {
+
+    beforeEach(function() {
+      spyOn(this.model, 'bind');
+      spyOn(this.viewModel, 'bind');
+      this.view._initBinds();
+    });
+
+    it('should render when any data has changed (origin or search data)', function() {
+      var bind = this.model.bind.calls.argsFor(0);
+      expect(bind[0]).toEqual('change:data change:searchData');
+      expect(bind[1]).toEqual(this.view.render);
+    });
+
+    it('should toggle view when search is enabled/disabled', function() {
+      var bind = this.viewModel.bind.calls.argsFor(0);
+      expect(bind[0]).toEqual('change:search');
+      expect(bind[1]).toEqual(this.view.toggle);
+    });
+
+  });
+
+});

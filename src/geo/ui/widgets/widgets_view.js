0fbc238e2c6c86131d6ad673f1c5774c03bd1e4f
xavijam <xavijam@gmail.com>
2015-11-26 18:28:07 +0100

    Added shadows to widget canvas element

--- src/geo/ui/widgets/widgets_view.js
+++ src/geo/ui/widgets/widgets_view.js
@@ -1,4 +1,5 @@
 var View = require('cdb/core/view');
+var _ = require('underscore');
 var LayerWidgetsView = require('cdb/geo/ui/widgets/layer_widgets_view');
 require('simplebar');
 
@@ -14,9 +15,11 @@ module.exports = View.extend({
   },
 
   render: function() {
+    this._cleanScrollEvent();
     this.clearSubViews();
     this.layers.each(this._renderLayerWidgetsView, this);
-    this.$el.simplebar();
+    this._renderScroll();
+    this._renderShadows();
     return this;
   },
 
@@ -27,5 +30,39 @@ module.exports = View.extend({
     });
     this.$el.append(layerWidgetsView.render().el);
     this.addView(layerWidgetsView);
+  },
+
+  _renderScroll: function() {
+    this.$el.simplebar().on('scroll', _.bind(this._checkShadows, this));
+  },
+
+  _renderShadows: function() {
+    var self = this;
+    this.$shadowTop = $('<div>').addClass("Widget-canvasShadow Widget-canvasShadow--top");
+    this.$shadowBottom = $('<div>').addClass("Widget-canvasShadow Widget-canvasShadow--bottom is-visible");
+    this.$el.append(this.$shadowTop);
+    this.$el.append(this.$shadowBottom);
+    setTimeout(function() {
+      self._checkShadows();
+    },0);
+  },
+
+  _checkShadows: function() {
+    var currentPos = this.$el.simplebar('getScrollElement').scrollTop();
+    var max = this.$el.simplebar('getContentElement').get(0).scrollHeight;
+    var height = this.$el.simplebar('getScrollElement').outerHeight();
+    var maxPos = max - height;
+    this.$shadowTop.toggleClass('is-visible', currentPos > 0);
+    this.$shadowBottom.toggleClass('is-visible', currentPos < maxPos);
+  },
+
+  _cleanScrollEvent: function() {
+    this.$el.off('scroll');
+  },
+
+  clean: function() {
+    this._cleanScrollEvent();
+    View.prototype.clean.call(this);
   }
+
 });

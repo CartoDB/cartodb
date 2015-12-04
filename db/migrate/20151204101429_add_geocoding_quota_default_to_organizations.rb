0f5f1772ca858d2a8bd4b7920daf1f39090cd6b5
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-12-04 12:28:12 +0100

    Converts all organisations geocoding_quota to 0 if null before applying migration

--- db/migrate/20151204101429_add_geocoding_quota_default_to_organizations.rb
+++ db/migrate/20151204101429_add_geocoding_quota_default_to_organizations.rb
@@ -1,5 +1,7 @@
 Sequel.migration do
   up do
+    execute "UPDATE organizations SET geocoding_quota = 0 WHERE geocoding_quota IS NULL"
+
     alter_table(:organizations) do
       set_column_default :geocoding_quota, 0
       set_column_not_null :geocoding_quota

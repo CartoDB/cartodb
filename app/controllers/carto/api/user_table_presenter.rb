224d05f8bc78157e77b64b30762316e43977e527
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-12-11 19:06:10 +0100

    Adds param to user table presenter call

--- app/controllers/carto/api/user_table_presenter.rb
+++ app/controllers/carto/api/user_table_presenter.rb
@@ -32,7 +32,7 @@ module Carto
         {
           id: @user_table.id,
           name: @user_table.name_for_user(@current_viewer),
-          permission: Carto::Api::PermissionPresenter.new(@permission).with_presenter_cache(@presenter_cache).to_poro,
+          permission: Carto::Api::PermissionPresenter.new(@permission, current_viewer: @current_viewer).with_presenter_cache(@presenter_cache).to_poro,
           geometry_types: @user_table.geometry_types,
           privacy: privacy_text(@user_table.privacy).upcase,
           updated_at: @user_table.updated_at,

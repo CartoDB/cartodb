ae6abbbdd672ef7c08a88ce3d5e7c4f2aa218ac9
Nicklas Gummesson <nicklas@cartodb.com>
2015-12-14 09:34:47 +0100

    Auto-format file
    ./node_modules/.bin/standard --format src/offending-file-test.js

--- src/offending-file-test.js
+++ src/offending-file-test.js
@@ -1,5 +1,5 @@
-module.exports = function() {
+module.exports = function () {
   // this should not be valid
-  var foobar = "abc123";
-  return foobar;
-};
+  var foobar = 'abc123'
+  return foobar
+}

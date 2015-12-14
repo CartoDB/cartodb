7c5e3e7b2c9b82326a97e4bdcbe0f9b385264f39
Nicklas Gummesson <nicklas@cartodb.com>
2015-12-14 10:57:17 +0100

    Post-merge fix linter formatting

--- src/widgets/time-series/torque-cumulative-render-info-view.js
+++ src/widgets/time-series/torque-cumulative-render-info-view.js
@@ -1,28 +1,27 @@
-var View = cdb.core.View;
-var d3 = cdb.d3;
-var template = require('./torque-cumulative-render-info.tpl');
+var View = cdb.core.View
+var d3 = cdb.d3
+var template = require('./torque-cumulative-render-info.tpl')
 
 /**
  * View for to display info about the cumulative render data, e.g. the time range that's being displayed
  * this.model is expected to be a torqueLayer model
  */
 module.exports = View.extend({
+  initialize: function () {
+    this._torqueLayerModel = this.options.torqueLayerModel
 
-  initialize: function() {
-    this._torqueLayerModel = this.options.torqueLayerModel;
-
-    var data = this.model.get('data');
+    var data = this.model.get('data')
     this._scale = d3.time.scale()
       .domain([data[0].start * 1000, data[data.length - 1].end * 1000])
       .nice()
-      .range([0, this.model.get('bins')]);
+      .range([0, this.model.get('bins')])
 
     // for format rules see https://github.com/mbostock/d3/wiki/Time-Formatting
-    this._timeFormatter = d3.time.format('%H:%M');
-    this._dateFormatter = d3.time.format('%x');
+    this._timeFormatter = d3.time.format('%H:%M')
+    this._dateFormatter = d3.time.format('%x')
   },
 
-  render: function() {
+  render: function () {
     var cumulativeRender = this._torqueLayerModel.get('cumulativeRender')
 
     this.$el.html(template({
@@ -30,8 +29,8 @@ module.exports = View.extend({
       dateFormatter: this._dateFormatter,
       startDate: new Date(this._scale.invert(cumulativeRender.start)),
       endDate: new Date(this._scale.invert(cumulativeRender.end))
-    }));
+    }))
 
-    return this;
+    return this
   }
-});
+})
--- src/widgets/time-series/torque-reset-cumulative-render-view.js
+++ src/widgets/time-series/torque-reset-cumulative-render-view.js
@@ -1,23 +1,22 @@
-var View = cdb.core.View;
-var template = require('./torque-reset-cumulative-render.tpl');
+var View = cdb.core.View
+var template = require('./torque-reset-cumulative-render.tpl')
 
 /**
  * View to reset cumulative render.
  * this.model is expected to be a torqueLayer model
  */
 module.exports = View.extend({
-
   className: 'CDB-Widget-filterButtons',
   events: {
     'click .js-clear': '_onClick'
   },
 
-  render: function() {
-    this.$el.html(template());
-    return this;
+  render: function () {
+    this.$el.html(template())
+    return this
   },
 
-  _onClick: function() {
-    this.model.resetCumulativeRender();
+  _onClick: function () {
+    this.model.resetCumulativeRender()
   }
-});
+})

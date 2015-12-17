64c6c0ceae0c5301d61ea583df79675c10ec0358
Raul Ochoa <rochoaf@gmail.com>
2015-12-17 14:44:12 +0100

    Start connect on same port watch is watching

--- grunt-tasks/connect.js
+++ grunt-tasks/connect.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     options: {
       port: 9002,
-      livereload: 35731,
+      livereload: 35732,
       open: true,
       hostname: '0.0.0.0', // to be able to access the server not only from localhost
       base: {

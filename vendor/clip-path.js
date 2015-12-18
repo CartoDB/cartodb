32a24b467e0883f4ac6dc3cdbaf2b1e9fc1c00df
Javier Arce <javierarce@gmail.com>
2015-12-18 10:23:25 +0100

    Renames class

--- vendor/clip-path.js
+++ vendor/clip-path.js
@@ -5,7 +5,7 @@
       var path = $(this).attr('data-clipPath');
       var src = $(this).attr('src');
 
-      var tpl = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="svgMask" width="<%- w %>" height="<%- h %>" viewBox="0 0 <%- w %> <%- h %>">';
+      var tpl = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="CDB-infowindow-mask" width="<%- w %>" height="<%- h %>" viewBox="0 0 <%- w %> <%- h %>">';
       tpl += '<defs><clipPath id="maskID<%-id %>"><path d="<%-path %>"/></clipPath></defs>';
       tpl += '<image clip-path="url(#maskID<%-id %>)" x="<%- x %>" y="<%- y %>" width="<%-w %>" height="<%-h %>" xlink:href="<%-src %>" />';
       tpl += '</svg>';

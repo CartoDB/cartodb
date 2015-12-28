bed9ed5f4a9a84cd1052ddd7e5cd0c981365e637
Javier Arce <javierarce@gmail.com>
2015-12-28 15:23:43 +0100

    Improves legibility

--- src/vis/vis.js
+++ src/vis/vis.js
@@ -476,10 +476,21 @@ var Vis = View.extend({
 
       var opt = data.options;
 
-      if (type == 'share' && options['shareable'] || type == 'share' && overlay.model.get('display') && options['shareable'] == undefined) overlay.show();
-      if (type == 'layer_selector' && options[type] || type == 'layer_selector' && overlay.model.get('display') && options[type] == undefined) overlay.show();
-      if (type == 'fullscreen' && options[type] || type == 'fullscreen' && opt.display && options[type] == undefined) overlay.show();
-      if (type == 'search' && options[type] || type == 'search' && opt.display && options[type] == undefined) overlay.show();
+      if (type == 'share' && options['shareable'] || type == 'share' && overlay.model.get('display') && options['shareable'] == undefined) {
+        overlay.show();
+      }
+
+      if (type == 'layer_selector' && options[type] || type == 'layer_selector' && overlay.model.get('display') && options[type] == undefined) {
+        overlay.show();
+      }
+
+      if (type == 'fullscreen' && options[type] || type == 'fullscreen' && opt.display && options[type] == undefined) {
+        overlay.show();
+      }
+
+      if (type == 'search' && options[type] || type == 'search' && opt.display && options[type] == undefined) {
+        overlay.show();
+      }
 
       if (type === 'header') {
         var m = overlay.model;

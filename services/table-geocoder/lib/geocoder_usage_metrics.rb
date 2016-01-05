05f8227a41630d9d506faa4751a20c6fec779671
Rafa de la Torre <rtorre@cartodb.com>
2016-01-05 15:29:42 +0100

    Add some other methods to track geocoded rows

--- services/table-geocoder/lib/geocoder_usage_metrics.rb
+++ services/table-geocoder/lib/geocoder_usage_metrics.rb
@@ -4,6 +4,7 @@ module CartoDB
   # The purpose of this class is to encapsulate storage of usage metrics.
   # This shall be used for billing, quota checking and metrics.
   class GeocoderUsageMetrics
+    # TODO generalize metric tracking function
     def incr_total_requests(n=1)
       # TODO implement
       CartoDB.notify_debug "#{__method__}(n=#{n})"
@@ -23,5 +24,26 @@ module CartoDB
       # TODO implement
       CartoDB.notify_debug "#{__method__}(n=#{n})"
     end
+
+    def incr_processable_rows(n=1)
+      # TODO implement
+      CartoDB.notify_debug "#{__method__}(n=#{n})"
+    end
+
+    def incr_success_rows(n=1)
+      # TODO implement
+      CartoDB.notify_debug "#{__method__}(n=#{n})"
+    end
+
+    def incr_empty_rows(n=1)
+      # TODO implement
+      CartoDB.notify_debug "#{__method__}(n=#{n})"
+    end
+
+    def incr_failed_rows(n=1)
+      # TODO implement
+      CartoDB.notify_debug "#{__method__}(n=#{n})"
+    end
+
   end
 end

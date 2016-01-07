8141a2b570e7028426ce794e92324adb7a8bc844
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-01-07 12:33:35 +0100

    Adds no-cache headers for browser

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,16 @@
 class Admin::AdminController < ApplicationController
   before_filter :x_frame_options_deny
+  before_filter :invalidate_browser_cache
 
   protected
 
   def x_frame_options_deny
     response.headers['X-Frame-Options'] = 'DENY'
+  end
+
+  def invalidate_browser_cache
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Mon, 01 Jan 1990 00:00:00 GMT'
   end
 end

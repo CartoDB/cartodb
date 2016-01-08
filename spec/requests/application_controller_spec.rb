2974ee34337736190bc364b867968c8e92984240
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-01-08 10:39:02 +0100

    Spec organization

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -20,35 +20,40 @@ describe ApplicationController do
       Admin::VisualizationsController.any_instance.stubs(:load_common_data).returns(true)
     end
 
-    it 'is triggered if http_header_authentication is configured and header is sent' do
-      stub_http_header_authentication_configuration
-      ApplicationController.any_instance.expects(:http_header_authentication)
-      get dashboard_url, {}, authenticated_email_header
+    describe 'triggering' do
+      it 'enabled if http_header_authentication is configured and header is sent' do
+        stub_http_header_authentication_configuration
+        ApplicationController.any_instance.expects(:http_header_authentication)
+        get dashboard_url, {}, authenticated_email_header
+      end
+
+      it 'disabled if http_header_authentication is configured and header is not set' do
+        stub_http_header_authentication_configuration
+        ApplicationController.any_instance.expects(:http_header_authentication).never
+        get dashboard_url, {}, {}
+      end
+
+      it 'disabled if http_header_authentication is not configured' do
+        ApplicationController.any_instance.expects(:http_header_authentication).never
+        get dashboard_url, {}, {}
+        get dashboard_url, {}, authenticated_email_header
+      end
     end
 
-    it 'is not triggered if http_header_authentication is configured and header is not set' do
-      stub_http_header_authentication_configuration
-      ApplicationController.any_instance.expects(:http_header_authentication).never
-      get dashboard_url, {}, {}
+    describe 'email autentication' do
+      it 'loads the dashboard for a known user email' do
+        stub_load_common_data
+        stub_http_header_authentication_configuration
+        get dashboard_url, {}, authenticated_email_header
+        response.status.should == 200
+        response.body.should_not include("Login to Carto")
+      end
+
+      it 'does not load the dashboard for an unknown user email' do
+        stub_http_header_authentication_configuration
+        get dashboard_url, {}, wrong_authenticated_email_header
+      end
     end
 
-    it 'is not triggered if http_header_authentication is not configured' do
-      ApplicationController.any_instance.expects(:http_header_authentication).never
-      get dashboard_url, {}, {}
-      get dashboard_url, {}, authenticated_email_header
-    end
-
-    it 'loads the dashboard for a known user email' do
-      stub_load_common_data
-      stub_http_header_authentication_configuration
-      get dashboard_url, {}, authenticated_email_header
-      response.status.should == 200
-      response.body.should_not include("Login to Carto")
-    end
-
-    it 'does not load the dashboard for an unknown user email' do
-      stub_http_header_authentication_configuration
-      get dashboard_url, {}, wrong_authenticated_email_header
-    end
   end
 end

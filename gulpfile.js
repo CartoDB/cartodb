d36c046386e1e7bd2645f1b2a6480de252bca416
Carlos Matallín <matallo@cartodb.com>
2016-01-13 11:27:32 +0100

    iconfont

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
-var gulp = require('gulp');
-var iconfont = require('gulp-iconfont');
-var iconfontCss = require('gulp-iconfont-css');
+var gulp = require('gulp')
+var iconfont = require('gulp-iconfont')
+var iconfontCss = require('gulp-iconfont-css')
 
-
-gulp.task('default', function(){
+gulp.task('default', function () {
   gulp.src(['./app/assets/fonts/icon_font/svgs/*.svg'])
-    
     .pipe(iconfontCss({
       fontName: 'cartoIcon',
       path: './app/assets/fonts/icon_font/templates/_icons.scss',
       targetPath: '../stylesheets/common/icon-font.css.scss',
       fontPath: '../../fonts/'
     }))
-
     .pipe(iconfont({
       fontName: 'cartoIcon', // required
       appendCodepoints: true // recommended option
     }))
-
-    .pipe(gulp.dest('app/assets/fonts/'));
-});
+    .pipe(gulp.dest('app/assets/fonts/'))
+})

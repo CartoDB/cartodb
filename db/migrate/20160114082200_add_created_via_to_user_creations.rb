b6ce3c7a5e540cb05f04d272678cf28d156b1aec
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-01-15 08:51:49 +0100

    #6367 (#6397 step 1) migration

---
+++ db/migrate/20160114082200_add_created_via_to_user_creations.rb
@@ -0,0 +1,10 @@
+Sequel.migration do
+  up do
+    add_column :user_creations, :created_via, :text, null: false, default: 'org_signup'
+    Rails::Sequel.connection.run("update user_creations set created_via = 'api' where created_via_api = 'true'")
+  end
+
+  down do
+    drop_column :user_creations, :created_via
+  end
+end

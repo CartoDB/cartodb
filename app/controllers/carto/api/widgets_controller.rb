65eb42b5b36c69be9a970820c05a7db5f389736d
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-01-20 19:48:59 +0100

    Removed whitespaces

--- app/controllers/carto/api/widgets_controller.rb
+++ app/controllers/carto/api/widgets_controller.rb
@@ -21,6 +21,7 @@ module Carto
         @map_id = params[:map_id]
         @layer_id = params[:layer_id]
         @widget_id = params[:widget_id]
+
         true
       end
 
@@ -28,9 +29,7 @@ module Carto
         @widget = Carto::Widget.where(layer_id: @layer_id, id: @widget_id).first
 
         raise Carto::LoadError.new("Widget not found: #{@widget_id}") unless @widget
-
         raise Carto::LoadError.new("Widget not found: #{@widget_id} for that map (#{@map_id})") unless @widget.belongs_to_map?(@map_id)
-
         raise Carto::UnauthorizedError.new("Not authorized for widget #{@widget_id}") unless @widget.writable_by_user?(current_user)
 
         true

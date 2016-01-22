c40e081799dd55bbc3f0cfc8f7f9449c450e4f00
Nicklas Gummesson <nicklas@cartodb.com>
2016-01-22 17:13:57 +0100

    Use separate layout for editor3

--- app/controllers/carto/editor/visualizations_controller.rb
+++ app/controllers/carto/editor/visualizations_controller.rb
@@ -9,6 +9,8 @@ module Carto
 
       after_filter :update_user_last_activity, only: [:show]
 
+      layout 'application_editor3'
+
       def show
         @vizjson = Carto::Api::VizJSONPresenter.new(@visualization, $tables_metadata)
                                                .to_vizjson(https_request: is_https?)

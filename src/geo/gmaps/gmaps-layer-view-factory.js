7ba68d5533ebfea9f4c483356a5fa7c311173384
Pablo Alonso Garcia <pablo@cartodb.com>
2016-01-25 15:42:08 +0100

    Fixed google maps API dependency

--- src/geo/gmaps/gmaps-layer-view-factory.js
+++ src/geo/gmaps/gmaps-layer-view-factory.js
@@ -1,29 +1,33 @@
 var cdb = require('cdb');
 var log = require('cdb.log');
-var GMapsBaseLayerView = require('./gmaps-base-layer-view');
-var GMapsTiledLayerView = require('./gmaps-tiled-layer-view');
-var LeafletWMSLayerView = require('../leaflet/leaflet-wms-layer-view');
-var GMapsPlainLayerView = require('./gmaps-plain-layer-view');
-var GMapsCartoDBLayerGroupView = require('./gmaps-cartodb-layer-group-view');
 
 var GMapsLayerViewFactory = function () {};
 
-GMapsLayerViewFactory.prototype._constructors = {
-  'tiled': GMapsTiledLayerView,
-  'wms': LeafletWMSLayerView,
-  'plain': GMapsPlainLayerView,
-  'gmapsbase': GMapsBaseLayerView,
-  'layergroup': GMapsCartoDBLayerGroupView,
-  'namedmap': GMapsCartoDBLayerGroupView,
-  'torque': function (layer, map) {
-    // TODO for now adding this error to be thrown if object is not present, since it's dependency
-    // is not included in the standard bundle
-    if (!cdb.geo.GMapsTorqueLayerView) {
-      throw new Error('torque library must have been loaded for a torque layer to work');
+// Only "register" the layer view mappings if Google Maps library is present
+if (typeof (google) !== 'undefined' && typeof (google.maps) !== 'undefined') {
+  var GMapsBaseLayerView = require('./gmaps-base-layer-view');
+  var GMapsTiledLayerView = require('./gmaps-tiled-layer-view');
+  var LeafletWMSLayerView = require('../leaflet/leaflet-wms-layer-view');
+  var GMapsPlainLayerView = require('./gmaps-plain-layer-view');
+  var GMapsCartoDBLayerGroupView = require('./gmaps-cartodb-layer-group-view');
+
+  GMapsLayerViewFactory.prototype._constructors = {
+    'tiled': GMapsTiledLayerView,
+    'wms': LeafletWMSLayerView,
+    'plain': GMapsPlainLayerView,
+    'gmapsbase': GMapsBaseLayerView,
+    'layergroup': GMapsCartoDBLayerGroupView,
+    'namedmap': GMapsCartoDBLayerGroupView,
+    'torque': function (layer, map) {
+      // TODO for now adding this error to be thrown if object is not present, since it's dependency
+      // is not included in the standard bundle
+      if (!cdb.geo.GMapsTorqueLayerView) {
+        throw new Error('torque library must have been loaded for a torque layer to work');
+      }
+      return new cdb.geo.GMapsTorqueLayerView(layer, map);
     }
-    return new cdb.geo.GMapsTorqueLayerView(layer, map);
-  }
-};
+  };
+}
 
 GMapsLayerViewFactory.prototype.createLayerView = function (layerModel, mapModel) {
   var layerType = layerModel.get('type').toLowerCase();

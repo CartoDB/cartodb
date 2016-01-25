c15dd52daa0e1bd0ef3c328463eea8f8d65e33aa
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-01-25 09:48:25 +0100

    Checking current_user fixes #6452

--- app/controllers/carto/editor/editor_controller.rb
+++ app/controllers/carto/editor/editor_controller.rb
@@ -7,7 +7,7 @@ module Carto
       private
 
       def editor_users_only
-        render_404 unless editor_user?(current_user)
+        render_404 unless current_user && editor_user?(current_user)
       end
 
       def editor_user?(user)

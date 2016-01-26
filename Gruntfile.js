6dae52401eabc1a7aa800566066c391d832fd463
Nicklas Gummesson <nicklas@cartodb.com>
2016-01-26 13:00:36 +0100

    Add semistandard linting for new files
    Resolves #6456

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,11 +161,29 @@
       grunt.task.run('connect:jasmine:keepalive');
     });
 
+    grunt.registerTask('lint', 'lint source files', function () {
+      var done = this.async();
+      require('child_process').exec('PATH=$(npm bin):$PATH semistandard', function (error, stdout, stderr) {
+        if (error) {
+          grunt.log.fail(error);
+
+          // Filter out lines that are ignored,
+          // e.g. "src/foobar.js:0:0: File ignored because of your .eslintignore file. Use --no-ignore to override."
+          grunt.log.fail(stdout.replace(/.+--no-ignore.+(\r?\n|\r)/g, ''));
+          grunt.fail.warn('try `node_modules/.bin/semistandard --format src/filename.js` to auto-format code (you might still need to fix some things manually).');
+        } else {
+          grunt.log.ok('All linted files OK!');
+          grunt.log.writeln('Note that files listed in .eslintignore are not linted');
+        }
+        done();
+      });
+    });
+
     // Order in terms of task dependencies
     grunt.registerTask('js',          ['cdb', 'browserify', 'concat:js', 'jst']);
     grunt.registerTask('pre_default', ['clean', 'config', 'js']);
     grunt.registerTask('test', '(CI env) Re-build JS files and run all tests. ' +
-    'For manual testing use `grunt jasmine` directly', ['pre_default', 'jasmine']);
+    'For manual testing use `grunt jasmine` directly', ['lint', 'pre_default', 'jasmine']);
     grunt.registerTask('css',         ['copy:vendor', 'copy:app', 'compass', 'concat:css']);
     grunt.registerTask('default',     ['pre_default', 'css', 'manifest']);
     grunt.registerTask('minimize',    ['default', 'copy:js', 'exorcise', 'uglify']);

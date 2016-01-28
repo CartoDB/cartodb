d60cc70d8e2e604e0e5cc69c1ab775f4a4976ccf
xavijam <xavijam@gmail.com>
2016-01-28 18:05:43 +0100

    Adding jasmine cartodb3 generation within dev environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,7 +176,7 @@
     grunt.registerTask('minimize',    ['default', 'copy:js', 'exorcise', 'uglify']);
     grunt.registerTask('release',     ['check_release', 'minimize', 's3', 'invalidate']);
     grunt.registerTask('dev',         'Typical task for frontend development (watch JS/CSS changes)',
-      ['setConfig:env.browserify_watch:true', 'browserify', 'connect', 'watch']);
+      ['setConfig:env.browserify_watch:true', 'browserify', 'jasmine:cartodb3', 'connect', 'watch']);
     grunt.registerTask('sourcemaps', 'generate sourcemaps, to be used w/ trackjs.com for bughunting',
       ['setConfig:assets_dir:./tmp/sourcemaps', 'config', 'js', 'copy:js', 'exorcise', 'uglify']);
   };

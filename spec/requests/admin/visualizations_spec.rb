99b72ecf8d9d15a809b13bd8f103532b7061d800
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-02-02 17:25:39 +0100

    Stubbing current methods

--- spec/requests/admin/visualizations_spec.rb
+++ spec/requests/admin/visualizations_spec.rb
@@ -452,7 +452,7 @@ describe Admin::VisualizationsController do
       )
       CartoDB::Visualization.repository = DataRepository::Backend::Sequel.new(db, :visualizations)
 
-      CartoDB::UserOrganization.any_instance.stubs(:move_user_tables_to_schema).returns(nil)
+      CartoDB::UserModule::DBService.any_instance.stubs(:move_to_own_schema).returns(nil)
       CartoDB::TablePrivacyManager.any_instance.stubs(
         set_from_table_privacy: nil,
         propagate_to_varnish: nil

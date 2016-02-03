9aa30ab7bfb91c5a6b018febce63a6e0d88bc224
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-02-03 15:58:23 +0100

    Refactor sql_parser_spec.rb

--- spec/lib/sql_parser_spec.rb
+++ spec/lib/sql_parser_spec.rb
@@ -3,10 +3,13 @@
 require 'spec_helper'
 
 describe CartoDB::SqlParser do
-  it "should return the affected tables" do
-    sql = "select coalesce('tabname', null) from cdb_tablemetadata;select 1;select * from spatial_ref_sys"
-    CartoDB::SqlParser.new(sql, connection: $user_1.in_database)
-      .affected_tables.should =~ ["cartodb.cdb_tablemetadata", "public.spatial_ref_sys"]
+  before(:each) do
+    @user_1 = FactoryGirl.create(:valid_user)
   end
 
+  it "should return the affected tables" do
+    sql = "select coalesce('tabname', null) from cdb_tablemetadata;select 1;select * from spatial_ref_sys"
+    CartoDB::SqlParser.new(sql, connection: @user_1.in_database)
+      .affected_tables.should =~ ["cartodb.cdb_tablemetadata", "public.spatial_ref_sys"]
+  end
 end

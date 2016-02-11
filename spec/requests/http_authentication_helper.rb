c2936b5d04c4b2d793f32a50f5569f153070cadd
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-02-11 17:00:43 +0100

    fixes spec/requests/http_authentication_helper.rb

--- spec/requests/http_authentication_helper.rb
+++ spec/requests/http_authentication_helper.rb
@@ -4,16 +4,16 @@ module HttpAuthenticationHelper
   end
 
   def authentication_headers(value)
-    { authenticated_header => value }
+    { "#{authenticated_header}" => value }
   end
 
   def stub_http_header_authentication_configuration(field: 'email', autocreation: false, enabled: true)
     Cartodb.stubs(:get_config)
 
     config = {
-      header: authenticated_header,
-      field: field,
-      autocreation: autocreation
+      'header' => authenticated_header,
+      'field' => field,
+      'autocreation' => autocreation
     }
 
     config.each do |f, v|

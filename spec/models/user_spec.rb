873a6edf12d5c9fa3127cfd42e4961545536dc4c
Alejandro Martínez <zen@itram.es>
2016-02-11 15:01:01 +0100

    Fix Hound barks on spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1026,13 +1026,16 @@ describe User do
     table_id  = data_import.table_id
     uuid      = UserTable.where(id: table_id).first.table_visualization.id
 
-    CartoDB::Varnish.any_instance.expects(:purge)
-                    .with("#{doomed_user.database_name}.*")
-                    .returns(true)
-    CartoDB::Varnish.any_instance.expects(:purge)
+    CartoDB::Varnish
+      .any_instance.expects(:purge)
+      .with("#{doomed_user.database_name}.*")
+      .returns(true)
+    CartoDB::Varnish
+      .any_instance.expects(:purge)
       .with("(^|;;)#{doomed_user.database_name}:((?:(?!;;).)*public(\\\\\")?\\.clubbing.*)|(table)$")
       .returns(true)
-    CartoDB::Varnish.any_instance.expects(:purge)
+    CartoDB::Varnish
+      .any_instance.expects(:purge)
       .with(".*#{uuid}:vizjson")
       .times(2 + 5)
       .returns(true)

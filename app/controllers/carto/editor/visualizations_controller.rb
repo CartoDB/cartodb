39f89b355579d0617ed421589bdd268686225c6d
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-02-12 12:36:51 +0100

    Refactor editor/visualizations_controller.rb

--- app/controllers/carto/editor/visualizations_controller.rb
+++ app/controllers/carto/editor/visualizations_controller.rb
@@ -8,28 +8,14 @@ module Carto
       ssl_required :show
 
       before_filter :load_visualization, only: [:show]
+      before_filter :load_visualization_data, only: [:show]
+      before_filter :load_vizjson, only: [:show]
 
       after_filter :update_user_last_activity, only: [:show]
 
       layout 'application_editor3'
 
       def show
-        @visualization_data = Carto::Api::VisualizationPresenter.new(@visualization, current_viewer, self).to_poro
-        @vizjson = Carto::Api::VizJSON3Presenter.new(@visualization, $tables_metadata)
-                                               .to_vizjson(https_request: is_https?)
-      end
-
-      private
-
-      def load_visualization
-        @visualization = get_priority_visualization(params[:id], current_user.id)
-
-        render_404 && return if @visualization.nil?
-        render_403 && return unless allowed?(@visualization)
-      end
-
-      def allowed?(visualization)
-        !(visualization.type_slide? || visualization.kind_raster? || !visualization.is_writable_by_user(current_user))
       end
     end
   end
--- app/controllers/visualizations_controller_helper.rb
+++ app/controllers/visualizations_controller_helper.rb
@@ -11,4 +11,23 @@ module VisualizationsControllerHelper
                                     end
                                     .first
   end
+
+  def load_visualization
+    @visualization = get_priority_visualization(params[:id], current_user.id)
+
+    render_404 && return if @visualization.nil?
+    render_403 && return unless allowed?(@visualization)
+  end
+
+  def allowed?(visualization)
+    !(visualization.type_slide? || visualization.kind_raster? || !visualization.is_writable_by_user(current_user))
+  end
+
+  def load_visualization_data
+    @visualization_data = Carto::Api::VisualizationPresenter.new(@visualization, current_viewer, self).to_poro
+  end
+
+  def load_vizjson
+    @vizjson = Carto::Api::VizJSON3Presenter.new(@visualization, $tables_metadata).to_vizjson(https_request: is_https?)
+  end
 end

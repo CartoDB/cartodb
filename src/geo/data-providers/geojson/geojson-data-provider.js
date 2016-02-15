e8bdc10b5b632b0cf2d5ec24bb405c11e6dcfe28
Francisco Dans <fdansv@gmail.com>
2016-02-15 12:41:19 +0100

    lint

--- src/geo/data-providers/geojson/geojson-data-provider.js
+++ src/geo/data-providers/geojson/geojson-data-provider.js
@@ -123,18 +123,18 @@ GeoJSONDataProvider.prototype._dataGeneratorsForDataviews = {
   formula: function (features, options) {
     var operation = options.operation;
     var columnName = options.column;
-    var nulls = features.reduce(function(p, c) { return p + (c.properties[columnName] === null ? 1 : 0) }, 0);
+    var nulls = features.reduce(function (p, c) { return p + (c.properties[columnName] === null ? 1 : 0); }, 0);
     var result;
     if (operation === 'count') {
       result = features.length;
     } else if (operation === 'avg') {
-      result = features.reduce(function (p,c) { return p+c.properties[columnName] }, 0) / features.length;
+      result = features.reduce(function (p, c) { return p + c.properties[columnName]; }, 0) / features.length;
     } else if (operation === 'sum') {
-      result = features.reduce(function(p,c){return p+c.properties[columnName]},0);
+      result = features.reduce(function (p, c) { return p + c.properties[columnName]; }, 0);
     } else if (operation === 'min') {
-      result = features.reduce(function(p,c){return Math.min(p,c.properties[columnName])}, Infinity);
+      result = features.reduce(function (p, c) { return Math.min(p, c.properties[columnName]); }, Infinity);
     } else if (operation === 'max') {
-      result = features.reduce(function(p,c){return Math.max(p,c.properties[columnName])}, 0);
+      result = features.reduce(function (p, c) { return Math.max(p, c.properties[columnName]); }, 0);
     } else {
       throw new Error("Coudn't generate data for formula dataview and '" + operation + "' operation.");
     }

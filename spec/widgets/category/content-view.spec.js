b15302108d03909edd4ce71e7e26cbec63f975c1
xavijam <xavijam@gmail.com>
2016-02-17 16:26:02 +0100

    Toggling category stats depending of show_stats attribute

--- spec/widgets/category/content-view.spec.js
+++ spec/widgets/category/content-view.spec.js
@@ -20,8 +20,16 @@ describe('widgets/category/content-view', function () {
     this.renderResult = this.view.render();
   });
 
-  it('should render ifine', function () {
+  it('should render fine', function () {
     expect(this.renderResult).toBe(this.view);
+  });
+
+  it('should render category stats if show_stats is enabled', function () {
+    expect(_.size(this.view._subviews)).toBe(6);
+    this.model.set('show_stats', true);
+    this.view.render();
+    expect(_.size(this.view._subviews)).toBe(7);
+    expect(this.view.$('.CDB-Widget-info').length).toBe(1);
   });
 
   afterEach(function () {

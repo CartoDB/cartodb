acdaa3d464a4947ba74d3c0c134118564d10c0d0
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-02-24 19:14:43 +0100

    Removed leaky gems and a couple of suspect leaks

--- lib/cartodb/analyzer.rb
+++ lib/cartodb/analyzer.rb
@@ -3,7 +3,8 @@ require 'json'
 # Parses rbtrace memory dumps
 # Taken from https://samsaffron.com/archive/2015/03/31/debugging-memory-leaks-in-ruby
 
-class Analyzer1
+require 'json'
+class Analyzer
   def initialize(filename)
     @filename = filename
   end
@@ -24,24 +25,25 @@ class Analyzer1
   end
 end
 
+#Analyzer.new(ARGV[0]).analyze
+
+require 'json'
 class Analyzer2
   def initialize(filename)
     @filename = filename
   end
 
-  def analyze_all
-    for g in 0..20
-      puts "\n\n#{g}\n"
-      analyze(g)
-    end
-  end
-
-  def analyze(generation)
+  def analyze
+for i in 1..10
+bottom = 1 + (i-1)*10
+top = i*10
+puts "-------- #{bottom}-#{top}"
     data = []
     File.open(@filename) do |f|
         f.each_line do |line|
           parsed=JSON.parse(line)
-          data << parsed if parsed["generation"] == generation
+range = bottom..top
+          data << parsed if range.include?(parsed["generation"].to_i)
         end
     end
     data.group_by{|row| "#{row["file"]}:#{row["line"]}"}
@@ -49,5 +51,8 @@ class Analyzer2
         .each do |k,v|
           puts "#{k} * #{v.count}"
         end
+end
   end
 end
+
+Analyzer2.new(ARGV[0]).analyze

341e96b0141ffd60e9c49fbd84eef9c351180d4c
Javier Torres <jtorres@cartodb.com>
2016-02-24 10:36:44 +0100

    Add logging to visualization renaming

--- app/models/table.rb
+++ app/models/table.rb
@@ -1212,9 +1212,9 @@ class Table
         end
       end
 
-      propagate_namechange_to_table_vis
-
-
+      begin
+        propagate_namechange_to_table_vis
+        raise nil
         if @user_table.layers.blank?
           exception_to_raise = CartoDB::TableError.new("Attempt to rename table without layers #{qualified_table_name}")
           CartoDB::notify_exception(exception_to_raise, user: owner)
@@ -1223,7 +1223,10 @@ class Table
             layer.rename_table(@name_changed_from, name).save
           end
         end
-
+      rescue Exception => exception
+        CartoDB::report_exception(exception, "Failed while renaming visualization #{@name_changed_from} to #{name}", user: owner)
+        raise exception
+      end
     end
     @name_changed_from = nil
   end

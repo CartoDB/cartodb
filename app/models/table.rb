2010c253ef2d9324b9641650e2f3be4ede3ccf79
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-02-24 11:53:51 +0100

    only update metadata if table is not new

--- app/models/table.rb
+++ app/models/table.rb
@@ -656,7 +656,7 @@ class Table
 
     @user_table[:name] = new_name
 
-    update_cdb_tablemetadata
+    update_cdb_tablemetadata unless new? # No need to update if table doesn't exist in metadata yet
   end
 
   def set_default_table_privacy

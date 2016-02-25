05bc7c33023353978d6a0b5964f4f4fa8b4aee72
Pablo Alonso Garcia <pablo@cartodb.com>
2016-02-25 15:48:14 +0100

    Fixed lint errors

--- src/dataviews/dataview-model-base.js
+++ src/dataviews/dataview-model-base.js
@@ -192,7 +192,7 @@ module.exports = Model.extend({
   fetch: function (opts) {
     opts = opts || {};
     if (this._dataProvider) {
-      this.set(this.parse(this._dataProvider.getData()));      
+      this.set(this.parse(this._dataProvider.getData()));
     } else {
       this.trigger('loading', this);
 

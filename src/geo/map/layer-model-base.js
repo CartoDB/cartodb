0ce032a19492a7eb043a33115e3c8417ded54222
Pablo Alonso Garcia <pablo@cartodb.com>
2016-03-04 19:11:14 +0100

    Fixed linter errors

--- src/geo/map/layer-model-base.js
+++ src/geo/map/layer-model-base.js
@@ -15,8 +15,8 @@ var MapLayer = Model.extend({
     type: 'Tiled'
   },
 
-  initialize: function() {
-    this.bind('change:type', function() {
+  initialize: function () {
+    this.bind('change:type', function () {
       log.error('changing layer type is not allowed, remove it and add a new one instead');
     });
   },
@@ -47,54 +47,51 @@ var MapLayer = Model.extend({
 
   // INTERNAL CartoDB.js METHODS
 
-  /***
-  * Compare the layer with the received one
-  * @method isEqual
-  * @param layer {Layer}
-  */
-  isEqual: function(layer) {
-    var me          = this.toJSON()
-      , other       = layer.toJSON()
-      // Select params generated when layer is added to the map
-      , map_params  = ['id', 'order'];
+  /*
+   * Compare the layer with the received one
+   * @method isEqual
+   * @param layer {Layer}
+   */
+  isEqual: function (layer) {
+    var me = this.toJSON();
+    var other = layer.toJSON();
+    // Select params generated when layer is added to the map
+    var map_params = ['id', 'order'];
 
     // Delete from the layers copy
-    _.each(map_params, function(param){
+    _.each(map_params, function (param) {
       delete me[param];
       delete other[param];
-      if (me.options)     delete me.options[param];
-      if (other.options)  delete other.options[param];
+      if (me.options) delete me.options[param];
+      if (other.options) delete other.options[param];
     });
 
-    var myType  = me.type? me.type : me.options.type
-      , itsType = other.type? other.type : other.options.type;
-
-    if(myType && (myType === itsType)) {
-
-      if(myType === 'Tiled') {
-        var myTemplate  = me.urlTemplate? me.urlTemplate : me.options.urlTemplate;
-        var itsTemplate = other.urlTemplate? other.urlTemplate : other.options.urlTemplate;
-        var myName = me.name? me.name : me.options.name;
-        var itsName = other.name? other.name : other.options.name;
+    var myType = me.type ? me.type : me.options.type;
+    var itsType = other.type ? other.type : other.options.type;
+    var myTemplate;
+    var itsTemplate;
+    if (myType && (myType === itsType)) {
+      if (myType === 'Tiled') {
+        myTemplate = me.urlTemplate ? me.urlTemplate : me.options.urlTemplate;
+        itsTemplate = other.urlTemplate ? other.urlTemplate : other.options.urlTemplate;
+        var myName = me.name ? me.name : me.options.name;
+        var itsName = other.name ? other.name : other.options.name;
 
         return myTemplate === itsTemplate && myName === itsName;
-      } else if(myType === 'WMS') {
-        var myTemplate  = me.urlTemplate? me.urlTemplate : me.options.urlTemplate;
-        var itsTemplate = other.urlTemplate? other.urlTemplate : other.options.urlTemplate;
-        var myLayer  = me.layers? me.layers : me.options.layers;
-        var itsLayer = other.layers? other.layers : other.options.layers;
+      } else if (myType === 'WMS') {
+        myTemplate = me.urlTemplate ? me.urlTemplate : me.options.urlTemplate;
+        itsTemplate = other.urlTemplate ? other.urlTemplate : other.options.urlTemplate;
+        var myLayer = me.layers ? me.layers : me.options.layers;
+        var itsLayer = other.layers ? other.layers : other.options.layers;
         return myTemplate === itsTemplate && myLayer === itsLayer;
-      }
-      else if (myType === 'torque') {
+      } else if (myType === 'torque') {
         return cdb.geo.TorqueLayer.prototype.isEqual.call(this, layer);
-      }
-      else if (myType === 'named_map') {
+      } else if (myType === 'named_map') {
         return cdb.geo.CartoDBNamedMapLayer.prototype.isEqual.call(this, layer);
       } else { // same type but not tiled
-        var myBaseType = me.base_type? me.base_type : me.options.base_type;
-        var itsBaseType = other.base_type? other.base_type : other.options.base_type;
-        if(myBaseType) {
-          if(_.isEqual(me,other)) {
+        var myBaseType = me.base_type ? me.base_type : me.options.base_type;
+        if (myBaseType) {
+          if (_.isEqual(me, other)) {
             return true;
           } else {
             return false;

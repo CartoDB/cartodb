8f9c22e8cd0f5e5297756e0f1e459ab07536385a
Francisco Dans <fdansv@gmail.com>
2016-03-04 18:06:05 +0100

    makes sure that setUrl is not called more than once

--- src/geo/leaflet/leaflet-cartodb-vector-layer-group-view.js
+++ src/geo/leaflet/leaflet-cartodb-vector-layer-group-view.js
@@ -31,7 +31,9 @@ var LeafletCartoDBVectorLayerGroupView = CartoDBd3Layer.extend({
       var index
       child.collection.models.filter(function (c, i) { if(child.cid === c.cid) index = i })
       this.options.styles[index - 1] = meta.cartocss
-      this.setUrl(this.model.get('urls').tiles[0])
+      if (this.options.styles.filter(function (p) { return typeof p === 'undefined' }).length === 0) {
+        this.setUrl(this.model.get('urls').tiles[0])
+      }
     }, this);
 
     layerModel.layers.each(function (layer) {

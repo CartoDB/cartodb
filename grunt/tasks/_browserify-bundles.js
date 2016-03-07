bd4ac737c91a01a39f1a829dfecbb595f7ccb079
Pablo Alonso Garcia <pablo@cartodb.com>
2016-03-07 15:06:38 +0100

    Removed sharedModules since there's only one module now

--- grunt/tasks/_browserify-bundles.js
+++ grunt/tasks/_browserify-bundles.js
@@ -1,16 +1,3 @@
-// modules that will be exporte from default cartodb bundle, to be used in e.g. cartodb.mod.torque bundle
-var sharedModules = [
-  'backbone',
-  'cdb',
-  'cdb.config',
-  'cdb.core.util',
-  'cdb.log',
-  'jquery',
-  'leaflet',
-  'underscore',
-  'torque.js'
-];
-
 module.exports = {
   'src-specs': {
     src: [
@@ -33,19 +20,13 @@ module.exports = {
 
   cartodb: {
     src: 'src/cartodb.js',
-    dest: '<%= config.dist %>/cartodb.uncompressed.js',
-    options: {
-      require: sharedModules
-    }
+    dest: '<%= config.dist %>/cartodb.uncompressed.js'
   },
   'cartodb-specs': {
     src: [
       'test/fail-tests-if-have-errors-in-src.js',
       'test/spec/cartodb.spec.js'
     ],
-    dest: '<%= config.tmp %>/cartodb-specs.js',
-    options: {
-      require: sharedModules
-    }
+    dest: '<%= config.tmp %>/cartodb-specs.js'
   }
 };

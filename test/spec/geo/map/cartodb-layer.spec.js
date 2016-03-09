e5c5be15cf560ec3a026016a4fde2ec9be004d48
xavijam <xavijam@gmail.com>
2016-03-09 12:46:51 +0100

    Do not add cartodb_id column in interactivity column if there is no infowindow or tooltip field names required

--- test/spec/geo/map/cartodb-layer.spec.js
+++ test/spec/geo/map/cartodb-layer.spec.js
@@ -8,4 +8,30 @@ describe('geo/map/cartodb-layer', function () {
     var layer = new CartoDBLayer();
     expect(layer.get('type')).toEqual('CartoDB');
   });
+
+  describe('.getInteractiveColumnNames', function() {
+    beforeEach(function () {
+      this.layer = new CartoDBLayer();
+      spyOn(this.layer, 'getInfowindowFieldNames');
+      spyOn(this.layer, 'getTooltipFieldNames');
+    });
+
+    it('should include cartodb_id if there is any other field required from the infowindow or tooltip', function () {
+      this.layer.getInfowindowFieldNames.and.returnValue(['column_a']);
+      this.layer.getTooltipFieldNames.and.returnValue([]);
+      expect(_.contains(this.layer.getInteractiveColumnNames(), 'cartodb_id')).toBeTruthy();
+
+      this.layer.getInfowindowFieldNames.and.returnValue([]);
+      this.layer.getTooltipFieldNames.and.returnValue(['column_b']);
+      expect(_.contains(this.layer.getInteractiveColumnNames(), 'cartodb_id')).toBeTruthy();
+    });
+
+    it('should not include cartodb_id if there isn\'t any field required', function () {
+      this.layer.getInfowindowFieldNames.and.returnValue([]);
+      this.layer.getTooltipFieldNames.and.returnValue([]);
+      expect(_.contains(this.layer.getInteractiveColumnNames(), 'cartodb_id')).toBeFalsy();
+    });
+
+  });
+
 });

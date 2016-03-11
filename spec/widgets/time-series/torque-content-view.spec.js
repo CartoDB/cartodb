5f7b5743ff642c6f702951cb6fd68d53cffef586
xavijam <xavijam@gmail.com>
2016-03-11 12:14:02 +0100

    Fixed tests related with torque content view

--- spec/widgets/time-series/torque-content-view.spec.js
+++ spec/widgets/time-series/torque-content-view.spec.js
@@ -1,6 +1,7 @@
 var specHelper = require('../../spec-helper');
 var TorqueTimesSeriesContentView = require('../../../src/widgets/time-series/torque-content-view');
 var WidgetModel = require('../../../src/widgets/widget-model');
+var cdb = require('cartodb.js');
 
 describe('widgets/time-series/torque-content-view', function () {
   beforeEach(function () {
@@ -8,22 +9,30 @@ describe('widgets/time-series/torque-content-view', function () {
     this.dataviewModel = vis.dataviews.createHistogramModel(vis.map.layers.first(), {
       column: 'col'
     });
+    spyOn(this.dataviewModel, 'fetch').and.callThrough();
+    this.originalData = this.dataviewModel.getUnfilteredDataModel();
+    this.originalData.set({
+      data: [{ bin: 10 }, { bin: 3 }],
+      start: 0,
+      end: 256,
+      bins: 2
+    });
     this.dataviewModel.sync = function (method, model, options) {
       this.options = options;
     }.bind(this);
-    this.torqueLayerModel = new cdb.geo.TorqueLayer({
-    });
+    this.torqueLayerModel = new cdb.geo.TorqueLayer();
     var widgetModel = new WidgetModel({}, {
       dataviewModel: this.dataviewModel
     });
     this.view = new TorqueTimesSeriesContentView({
       model: widgetModel
     });
-    this.renderResult = this.view.render();
   });
 
-  it('should render loading state', function () {
-    expect(this.renderResult).toBe(this.view);
+  it('should not fetch new data until unfilteredData is loaded', function () {
+    expect(this.dataviewModel.fetch).not.toHaveBeenCalled();
+    this.originalData.trigger('change:data', this.originalData);
+    expect(this.dataviewModel.fetch).toHaveBeenCalled();
   });
 
   describe('when data is provided', function () {

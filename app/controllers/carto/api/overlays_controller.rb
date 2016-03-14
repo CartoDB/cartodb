100f0c5d611bed318c2b201a46fa49552e88a3a1
Javier Torres <jtorres@cartodb.com>
2016-03-14 16:24:25 +0100

    Move edition to the new CartoApi controller

--- app/controllers/carto/api/overlays_controller.rb
+++ app/controllers/carto/api/overlays_controller.rb
@@ -8,23 +8,79 @@ module Carto
       include Carto::UUIDHelper
 
       ssl_required :index, :show
-      before_filter :check_current_user_has_permissions_on_vis, only: [ :index ]
-      before_filter :check_current_user_owns_vis, only: [ :show ]
+      before_filter :check_current_user_has_permissions_on_vis, only: [:index, :create]
+      before_filter :check_current_user_owns_vis, only: [:show, :update, :destroy]
 
       def index
-        collection = Carto::Overlay.where(visualization_id: params.fetch('visualization_id')).map { |overlay|
+        collection = Carto::Overlay.where(visualization_id: params.fetch('visualization_id')).map do |overlay|
           Carto::Api::OverlayPresenter.new(overlay).to_poro
-        }
+        end
         render_jsonp(collection)
       rescue KeyError
         head :not_found
       end
 
       def show
-        member = Carto::Overlay.where(id: params.fetch('id')).first
-        render_jsonp(member.attributes)
+        overlay = Carto::Overlay.find(params.fetch('id'))
+        render_jsonp(Carto::Api::OverlayPresenter.new(overlay).to_poro)
       rescue KeyError
         head :not_found
+      end
+
+      def create
+        @stats_aggregator.timing('overlays.create') do
+          begin
+            overlay = Carto::Overlay.new(type:             params[:type],
+                                         options:          params[:options],
+                                         template:         params[:template],
+                                         order:            params[:order],
+                                         visualization_id: params[:visualization_id])
+
+            saved = false
+            @stats_aggregator.timing('save') do
+              saved = overlay.save
+            end
+            if saved
+              render_jsonp(Carto::Api::OverlayPresenter.new(overlay).to_poro)
+            else
+              render_jsonp({ errors: overlay.errors }, 400)
+            end
+          end
+        end
+      end
+
+      def update
+        @stats_aggregator.timing('overlays.update') do
+          begin
+            overlay = Carto::Overlay.find(params.fetch('id'))
+            overlay.type =     params[:type]     if params[:type]
+            overlay.options =  params[:options]  if params[:options]
+            overlay.template = params[:template] if params[:template]
+            overlay.order =    params[:order]    if params[:order]
+
+            saved = false
+            @stats_aggregator.timing('save') do
+              saved = overlay.save
+            end
+            if saved
+              render_jsonp(Carto::Api::OverlayPresenter.new(overlay).to_poro)
+            else
+              render_jsonp({ errors: overlay.errors }, 400)
+            end
+          rescue KeyError
+            head :not_found
+          end
+        end
+      end
+
+      def destroy
+        @stats_aggregator.timing('overlays.destroy') do
+          overlay = Carto::Overlay.find(params.fetch('id'))
+          @stats_aggregator.timing('delete') do
+            overlay.destroy
+          end
+          head 204
+        end
       end
 
       protected

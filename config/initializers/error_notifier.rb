c0091bce7680d2eac35ec343a414eefe47d344a6
Javier Torres <jtorres@cartodb.com>
2016-03-15 16:06:24 +0100

    Little refactor after merge

--- config/initializers/error_notifier.rb
+++ config/initializers/error_notifier.rb
@@ -43,18 +43,9 @@ module CartoDB
 
   def self.log(level, exception: nil, message: nil, user: nil, **additional_data)
     if Rails.env.development?
-      error_msg = "#{level}: #{message}\n"
-      unless exception.nil?
-        error_msg += exception.inspect + "\n"
-        error_msg += exception.backtrace.inspect + "\n"
-      end
-      unless user.nil?
-        error_msg += user.inspect + "\n"
-      end
-      error_msg += additional_data.inspect + "\n"
-
-      ::Logger.new(STDOUT).error(error_msg)
+      report_error_to_console(level, exception: exception, message: message, user: user, **additional_data)
     end
+
     rollbar_scope(user).log(level, exception, message, additional_data)
   end
 
@@ -69,4 +60,19 @@ module CartoDB
     Rollbar.scope(scope)
   end
   private_class_method :rollbar_scope
+
+  def self.report_error_to_console(level, exception: nil, message: nil, user: nil, **additional_data)
+    error_msg = "#{level}: #{message}\n"
+    unless exception.nil?
+      error_msg += exception.inspect + "\n"
+      error_msg += exception.backtrace.inspect + "\n"
+    end
+    unless user.nil?
+      error_msg += user.inspect + "\n"
+    end
+    error_msg += additional_data.inspect + "\n"
+
+    ::Logger.new(STDOUT).error(error_msg)
+  end
+  private_class_method :report_error_to_console
 end

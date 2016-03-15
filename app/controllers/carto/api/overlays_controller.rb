3d44610c7ca343a95a9af121cdc0bf267302a07e
Javier Torres <jtorres@cartodb.com>
2016-03-15 11:49:23 +0100

    Avoid double-loading models in overlays_controller

--- app/controllers/carto/api/overlays_controller.rb
+++ app/controllers/carto/api/overlays_controller.rb
@@ -9,10 +9,10 @@ module Carto
 
       ssl_required :index, :show
       before_filter :check_current_user_has_permissions_on_vis, only: [:index, :create]
-      before_filter :check_current_user_owns_vis, only: [:show, :update, :destroy]
+      before_filter :check_current_user_owns_overlay, only: [:show, :update, :destroy]
 
       def index
-        collection = Carto::Overlay.where(visualization_id: params.fetch('visualization_id')).map do |overlay|
+        collection = Carto::Overlay.where(visualization_id: @visualization.id).map do |overlay|
           Carto::Api::OverlayPresenter.new(overlay).to_poro
         end
         render_jsonp(collection)
@@ -21,8 +21,7 @@ module Carto
       end
 
       def show
-        overlay = Carto::Overlay.find(params.fetch('id'))
-        render_jsonp(Carto::Api::OverlayPresenter.new(overlay).to_poro)
+        render_jsonp(Carto::Api::OverlayPresenter.new(@overlay).to_poro)
       rescue KeyError
         head :not_found
       end
@@ -34,7 +33,7 @@ module Carto
                                          options:          params[:options],
                                          template:         params[:template],
                                          order:            params[:order],
-                                         visualization_id: params[:visualization_id])
+                                         visualization_id: @visualization.id)
 
             saved = false
             @stats_aggregator.timing('save') do
@@ -52,20 +51,19 @@ module Carto
       def update
         @stats_aggregator.timing('overlays.update') do
           begin
-            overlay = Carto::Overlay.find(params.fetch('id'))
-            overlay.type =     params[:type]     if params[:type]
-            overlay.options =  params[:options]  if params[:options]
-            overlay.template = params[:template] if params[:template]
-            overlay.order =    params[:order]    if params[:order]
+            @overlay.type =     params[:type]     if params[:type]
+            @overlay.options =  params[:options]  if params[:options]
+            @overlay.template = params[:template] if params[:template]
+            @overlay.order =    params[:order]    if params[:order]
 
             saved = false
             @stats_aggregator.timing('save') do
-              saved = overlay.save
+              saved = @overlay.save
             end
             if saved
-              render_jsonp(Carto::Api::OverlayPresenter.new(overlay).to_poro)
+              render_jsonp(Carto::Api::OverlayPresenter.new(@overlay).to_poro)
             else
-              render_jsonp({ errors: overlay.errors }, 400)
+              render_jsonp({ errors: @overlay.errors }, 400)
             end
           rescue KeyError
             head :not_found
@@ -75,9 +73,8 @@ module Carto
 
       def destroy
         @stats_aggregator.timing('overlays.destroy') do
-          overlay = Carto::Overlay.find(params.fetch('id'))
           @stats_aggregator.timing('delete') do
-            overlay.destroy
+            @overlay.destroy
           end
           head 204
         end
@@ -85,14 +82,14 @@ module Carto
 
       protected
 
-      def check_current_user_owns_vis
+      def check_current_user_owns_overlay
         head 401 and return if current_user.nil?
         head 401 and return unless is_uuid?(params.fetch('id'))
 
-        overlay = Carto::Overlay.where(id: params.fetch('id')).first
-        head 401 and return if overlay.nil?
+        @overlay = Carto::Overlay.where(id: params.fetch('id')).first
+        head 401 and return if @overlay.nil?
 
-        vis = Carto::Visualization.where(id: overlay.visualization_id).first
+        vis = Carto::Visualization.where(id: @overlay.visualization_id).first
         head 403 and return if vis.user_id != current_user.id
       end
 
@@ -100,10 +97,10 @@ module Carto
         head 401 and return if current_user.nil?
         head 401 and return unless is_uuid?(params.fetch('visualization_id'))
 
-        vis = Carto::Visualization.where(id: params.fetch('visualization_id')).first
-        head 401 and return if vis.nil?
+        @visualization = Carto::Visualization.where(id: params.fetch('visualization_id')).first
+        head 401 and return if @visualization.nil?
 
-        head 403 and return if !vis.is_writable_by_user(current_user)
+        head 403 and return if !@visualization.is_writable_by_user(current_user)
       end
 
     end

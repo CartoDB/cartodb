49e299964718681743425c4edb1a3db73bb8429d
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-03-15 16:52:40 +0100

    fixed tests

--- spec/helpers/carto/ghost_tables_manager_spec.rb
+++ spec/helpers/carto/ghost_tables_manager_spec.rb
@@ -19,25 +19,25 @@ module Carto
     end
 
     it 'should be inconsistent when renamed/dropped tables' do
-      @ghost_tables_manager.stubs(:stale_tables).returns [id: 3, name: 'manolo']
+      @ghost_tables_manager.stubs(:stale_tables).returns [id: 3, name: 'ManoloEscobar']
 
       @ghost_tables_manager.consistent?.should be false
     end
 
     it 'should be inconsistent when new tables' do
-      @ghost_tables_manager.stubs(:non_linked_tables).returns [id: 5, name: 'manolo']
+      @ghost_tables_manager.stubs(:non_linked_tables).returns [id: 5, name: 'ManoloEscobar']
 
       @ghost_tables_manager.consistent?.should be false
     end
 
     it 'should relink renamed tables' do
-      @ghost_tables_manager.stubs(:all_tables).returns [{ id: 1, name: 'manolo' }]
-      @ghost_tables_manager.stubs(:non_linked_tables).returns [{ id: 1, name: 'manolo' }]
+      @ghost_tables_manager.stubs(:all_tables).returns [{ id: 1, name: 'ManoloEscobar' }]
+      @ghost_tables_manager.stubs(:non_linked_tables).returns [{ id: 1, name: 'ManoloEscobar' }]
 
       table = Table.new
 
       vis = ::Visualization::Member.new(privacy: 'private',
-                                        name: 'escobar',
+                                        name: '_escobar',
                                         user_id: UUIDTools::UUID.timestamp_create.to_s,
                                         type: 'carto')
 
@@ -45,24 +45,25 @@ module Carto
       @ghost_tables_manager.stubs(:fetch_table_for_user_table).returns table
       table.stubs(:table_visualization).returns vis
 
-      vis.expects(:name=).with('manolo').once
+      vis.expects(:name=).with('ManoloEscobar').once
 
       @ghost_tables_manager.link
     end
 
     it 'should link new tables' do
-      @ghost_tables_manager.stubs(:all_tables).returns [{ id: 1, name: 'manolo' }, { id: 2, name: 'escobar' }]
-      @ghost_tables_manager.stubs(:non_linked_tables).returns [{ id: 1, name: 'manolo' }, { id: 2, name: 'escobar' }]
+      @ghost_tables_manager.stubs(:all_tables).returns [{ id: 1, name: 'ManoloEscobar' }, { id: 2, name: '_escobar' }]
+      @ghost_tables_manager.stubs(:non_linked_tables)
+                           .returns [{ id: 1, name: 'ManoloEscobar' }, { id: 2, name: '_escobar' }]
 
-      Table.any_instance.expects(:name=).with('manolo').once
-      Table.any_instance.expects(:name=).with('escobar').once
+      Table.any_instance.expects(:name=).with('ManoloEscobar').once
+      Table.any_instance.expects(:name=).with('_escobar').once
       ::Visualization::Member.any_instance.expects(:name=).never
 
       @ghost_tables_manager.link
     end
 
     it 'should unlink dropped tables' do
-      @ghost_tables_manager.stubs(:stale_tables).returns [{ id: 1, name: 'manolo' }, { id: 2, name: 'escobar' }]
+      @ghost_tables_manager.stubs(:stale_tables).returns [{ id: 1, name: 'ManoloEscobar' }, { id: 2, name: '_escobar' }]
       @ghost_tables_manager.stubs(:fetch_table_for_user_table).with(1).returns Table.new
       @ghost_tables_manager.stubs(:fetch_table_for_user_table).with(2).returns Table.new
 

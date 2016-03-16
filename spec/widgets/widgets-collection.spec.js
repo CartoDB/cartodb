dba8758d5a1f3d4d3a5c6a8a814a68e437c68029
xavijam <xavijam@gmail.com>
2016-03-16 13:03:03 +0100

    Adding test for widgets-collection order behaviour

--- spec/widgets/widgets-collection.spec.js
+++ spec/widgets/widgets-collection.spec.js
@@ -1,12 +1,20 @@
 var WidgetsCollection = require('../../src/widgets/widgets-collection');
+var _ = require('underscore');
 
 describe('widgets/widgets-collection', function () {
   beforeEach(function () {
+    // "Removed" debounce for not conflict with tests
+    spyOn(_, 'debounce').and.callFake(function (func) {
+      return function () {
+        func.apply(this, arguments);
+      };
+    });
     this.collection = new WidgetsCollection();
+    spyOn(this.collection, 'sort');
     this.collection.reset([
-      {isColorsApplied: false},
-      {},
-      {isColorsApplied: true}
+      {order: 0, isColorsApplied: false},
+      {order: 1},
+      {order: 2, isColorsApplied: true}
     ]);
   });
 
@@ -24,4 +32,15 @@ describe('widgets/widgets-collection', function () {
     expect(m2.get('isColorsApplied')).toBeUndefined();
     expect(m3.get('isColorsApplied')).toBeFalsy();
   });
+
+  it('should sort the collection and trigger orderChanged event when any widget order changes', function () {
+    var orderChangedSpy = jasmine.createSpy('orderChanged');
+    this.collection.on('orderChanged', orderChangedSpy);
+    this.collection.at(0).set('order', 3);
+    this.collection.at(1).set('order', 2);
+    this.collection.at(2).set('order', 1);
+    expect(this.collection.sort).toHaveBeenCalled();
+    expect(orderChangedSpy).toHaveBeenCalled();
+  });
+
 });

a505cf8f1a3540f1443425fe75c1e6a521213c66
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-03-17 12:20:50 +0100

    Adds new tests for Carto::StrongPasswordValidator

--- spec/helpers/carto/strong_password_spec.rb
+++ spec/helpers/carto/strong_password_spec.rb
@@ -20,11 +20,30 @@ module Carto
       validator.message.should == "must be at most #{PASSWORD.length - 1} characters long"
     end
 
-    it 'should be invalid when password not enough numbers or symbols' do
+    it 'should be invalid when password does not have enough letters' do
+      validator = Carto::StrongPasswordValidator.new(PASSWORD, min_letters: 9)
+
+      validator.valid?.should be_false
+      validator.message.should == "must contain at least 9 letters"
+    end
+
+    it 'should be invalid when password does not have enough numbers or symbols' do
       validator = Carto::StrongPasswordValidator.new(PASSWORD, min_symbols: 3, min_numbers: 2)
 
       validator.valid?.should be_false
       validator.message.should == "must contain at least 3 symbols or 2 numbers"
+    end
+
+    it 'should be valid when password has enough numbers but not enough symbols' do
+      validator = Carto::StrongPasswordValidator.new(PASSWORD, min_symbols: 3, min_numbers: 1)
+
+      validator.valid?.should be_true
+    end
+
+    it 'should be valid when password has enough symbols but not enough numbers' do
+      validator = Carto::StrongPasswordValidator.new(PASSWORD, min_symbols: 2, min_numbers: 3)
+
+      validator.valid?.should be_true
     end
 
     it 'should be invalidate a nil password' do

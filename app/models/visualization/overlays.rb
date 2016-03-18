427ef0efbc2baf6d465916e7f67b736fa59158f3
Javier Torres <jtorres@cartodb.com>
2016-03-18 10:43:29 +0100

    Removed some dead code

--- app/models/visualization/overlays.rb
+++ app/models/visualization/overlays.rb
@@ -1,8 +1,3 @@
-
-# encoding: utf-8
-require 'sequel'
-require_relative '../../../services/data-repository/backend/sequel'
-
 # encoding: utf-8
 module CartoDB
   module Visualization
@@ -10,11 +5,6 @@ module CartoDB
 
       def initialize(visualization)
         @visualization = visualization
-      end
-
-      def create_legacy_overlays
-        create_zoom_overlay(@visualization, 6)
-        create_loader_overlay(@visualization, 8)
       end
 
       def create_default_overlays
@@ -29,74 +19,7 @@ module CartoDB
         end
       end
 
-      def get_overlay_by_type(t)
-        @visualization.overlays.find { |o| o.type == t }
-      end
-
-      def create_overlays_from_url_options(url_options)
-        create_default_overlays
-        map_updated = false
-
-        return if not url_options
-
-        # parse from url options
-        opts = CGI::parse(url_options).deep_symbolize_keys
-        opts.each_pair do |opt, value|
-          bool_value = value[0] == 'true'
-          overlay = nil
-          case opt
-          when :title, :description
-            overlay = get_overlay_by_type('header')
-            # if not present, cartodb.js will create the title and description, so don't add them here
-            if overlay
-              overlay.options['extra']['show_title'] = opts[:title] == 'true'
-              overlay.options['extra']['show_description'] = opts[:description] == 'true'
-              bool_value = opts[:title] == 'true' || opts[:description] == 'true'
-              overlay.show.save if overlay && bool_value
-            end
-          when :shareable
-            overlay = get_overlay_by_type('share')
-            overlay.hide.save if overlay && !bool_value
-          when :search
-            overlay = get_overlay_by_type('search')
-            overlay.hide.save if overlay && !bool_value
-          when :layer_selector
-            overlay = get_overlay_by_type('layer_selector')
-            overlay.show.save if overlay && bool_value
-          when :fullscreen
-            overlay = get_overlay_by_type('fullscreen')
-            overlay.show.save if overlay && bool_value
-          when :cartodb_logo
-            overlay = get_overlay_by_type('logo')
-            overlay.hide.save if overlay && !bool_value
-
-          # map stuff, already in the viz
-          when :legends, :scrollwheel
-            if not map_updated
-              map = @visualization.map
-              map_needs_save = false
-              legends = opts[:legends] == 'true'
-              if map.legends != legends
-                map_needs_save = true
-              end
-              scrollwheel = opts[:scrollwheel] == 'true'
-              if map.scrollwheel != scrollwheel
-                map_needs_save = true
-              end
-              if map_needs_save
-                # query directly the database because it's a simple update and
-                # doing though the ORM raises requests to the tiler (not needed in
-                # this case)
-                # https://mondaybynoon.com/wp-content/uploads/xVyoSl.jpg
-                Sequel::Model(:maps).fetch(%{
-                  UPDATE maps SET scrollwheel= ?, legends = ? WHERE id = ?
-                }, scrollwheel, legends, map.id).first
-              end
-              map_updated = true
-            end
-          end
-        end
-      end
+      private
 
       def create_logo_overlay(member, order)
         options = { display: true, x: 10, y: 40 }
@@ -144,12 +67,6 @@ module CartoDB
         ).save
       end
 
-      def create_fullscreen_overlay(member, order)
-        options = { display: false, x: 20, y: 172 }
-
-        generate_overlay(member.id, options, "fullscreen", order).save
-      end
-
       def create_share_overlay(member, order)
         options = { display: true, x: 20, y: 20 }
 
@@ -160,25 +77,6 @@ module CartoDB
         options = { display: true, x: 60, y: 20 }
 
         generate_overlay(member.id, options, "search", order).save
-      end
-
-      def create_layer_selector_overlay(member, order)
-        options = { display: false, x: 212, y: 20 }
-
-        generate_overlay(member.id, options, "layer_selector", order).save
-      end
-
-      def create_header_overlay(member, order, show_title = false, show_description = false)
-        options = {
-          display: false,
-          extra: { title:            member.name,
-                   description:      member.description,
-                   show_title:       show_title,
-                   show_description: show_description
-                }
-        }
-
-        generate_overlay(member.id, options, "header", order).save
       end
     end
   end
--- spec/models/visualization/overlays_spec.rb
+++ spec/models/visualization/overlays_spec.rb
@@ -50,44 +50,4 @@ describe Visualization::Overlays do
       @visualization.overlays.count.should eq 4
     end
   end
-
-  describe 'from_url' do
-    it 'should create overlays from urls all true' do
-      url_options = "title=true&description=true&search=true&shareable=true&cartodb_logo=true&layer_selector=true&legends=true&scrollwheel=true&fullscreen=true&sublayer_options=1%7C1%7C1&sql=&zoom=3&center_lat=15.961329081596686&center_lon=44.736328125"
-      Visualization::Overlays.new(@visualization).create_overlays_from_url_options(url_options)
-
-      @visualization.overlays.count.should eq 5
-      @visualization.overlays.select { |o| o.options['display'] }.count.should eq 5
-    end
-
-    it 'should create overlays from urls header' do
-      url_options = "title=false&description=true&search=true&shareable=true&cartodb_logo=true&layer_selector=true&legends=true&scrollwheel=true&fullscreen=true&sublayer_options=1%7C1%7C1&sql=&zoom=3&center_lat=15.961329081596686&center_lon=44.736328125"
-      Visualization::Overlays.new(@visualization).create_overlays_from_url_options(url_options)
-
-      @visualization.overlays.count.should eq 5
-      @visualization.overlays.select { |o| o.options['display'] }.count.should eq 5
-
-    end
-
-    it 'should create overlays from urls header' do
-      url_options = "title=false&description=true&search=true&shareable=false&cartodb_logo=false&layer_selector=true&legends=true&scrollwheel=true&fullscreen=true&sublayer_options=1%7C1%7C1&sql=&zoom=3&center_lat=15.961329081596686&center_lon=44.736328125"
-      Visualization::Overlays.new(@visualization).create_overlays_from_url_options(url_options)
-
-      @visualization.overlays.count.should eq 5
-      @visualization.overlays.count { |o| o.options['display'] }.should eq 3
-
-    end
-
-    it 'should create default overlays for nil ' do
-      Visualization::Overlays.new(@visualization).create_overlays_from_url_options(nil)
-      @visualization.overlays.count.should eq 5
-    end
-
-    it 'should create default overlays for empty' do
-      Visualization::Overlays.new(@visualization).create_overlays_from_url_options('')
-      @visualization.overlays.count.should eq 5
-    end
-
-
-  end
 end

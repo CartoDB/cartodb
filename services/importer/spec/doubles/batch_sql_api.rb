9d761c3a59cd0f64481c86178f5fe00c8f8d7419
Carla Iriberri <iriberrig@gmail.com>
2016-03-21 17:17:51 +0100

    Fix status check

--- services/importer/spec/doubles/batch_sql_api.rb
+++ services/importer/spec/doubles/batch_sql_api.rb
@@ -21,11 +21,15 @@ module CartoDB
           fake_response(query, 'completed')
         end
 
+        def check_status(id)
+          fake_response(query, 'completed')
+        end
+
         private
 
         def fake_response(query, status='running')
           escaped_query = query.gsub(/\"/, '\\"')
-          body = %Q[{"job_id": "93a7f9b9-2660-4542-a5a6-1f2931eb9549", "user": "#{@user.username}", "status": "#{status}", "query": "\n            #{escaped_query}::REGCLASS;\n          ", "created_at": "2016-03-19T17:38:12.434Z", "updated_at": "2016-03-19T17:38:12.451Z"}]
+          body = %Q[{"job_id": "93a7f9b9-2660-4542-a5a6-1f2931eb9549", "user": "#{@user.username}", "status": "#{status}", "query": "#{escaped_query}", "created_at": "2016-03-19T17:38:12.434Z", "updated_at": "2016-03-19T17:38:12.451Z"}]
           ::JSON.parse(body)
         end
       end

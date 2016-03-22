dabd6e626e7193487d7a5a0405e29953b9166e01
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-03-22 19:01:54 +0100

    move replace to proper migration

--- db/migrate/20160322165836_add_not_null_constraint_to_layers_kind.rb
+++ db/migrate/20160322165836_add_not_null_constraint_to_layers_kind.rb
@@ -1,5 +1,7 @@
 Sequel.migration do
   up do
+    Rails::Sequel.connection.run(%{ UPDATE layers SET kind = 'carto' WHERE kind IS NULL; })
+
     alter_table :layers do
       set_column_not_null(:kind)
     end

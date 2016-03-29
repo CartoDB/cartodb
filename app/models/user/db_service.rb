65656b042abf137803e21081e33c208abaa97339
Javier Goizueta <jgoizueta@gmail.com>
2016-03-29 14:05:35 +0200

    Upgrade SQL extension version to 0.14.4
    The upgraded versions is a bugfix to support Overviews

--- app/models/user/db_service.rb
+++ app/models/user/db_service.rb
@@ -437,7 +437,7 @@ module CartoDB
       # Upgrade the cartodb postgresql extension
       def upgrade_cartodb_postgres_extension(statement_timeout = nil, cdb_extension_target_version = nil)
         if cdb_extension_target_version.nil?
-          cdb_extension_target_version = '0.14.3'
+          cdb_extension_target_version = '0.14.4'
         end
 
         @user.in_database(as: :superuser, no_cartodb_in_schema: true) do |db|

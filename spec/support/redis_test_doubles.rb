5e20d40cbb8624ab2ff3618ac95e04c6dd3e58ba
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-03-29 17:32:48 +0200

    Fixed test for user vizjson purge

--- spec/support/redis_test_doubles.rb
+++ spec/support/redis_test_doubles.rb
@@ -4,10 +4,17 @@ module RedisDoubles
 
     def initialize
       @deleted = []
+      @invokes = Hash.new([])
     end
 
     def del(keys)
       @deleted.concat(keys)
+      del_invokes = @invokes[:del] << keys
     end
+
+    def invokes(method_name)
+      @invokes[method_name]
+    end
+
   end
 end

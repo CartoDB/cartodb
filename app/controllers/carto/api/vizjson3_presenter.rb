9789271109f667b2efbe36fd03f583ce6923d711
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-03-30 16:42:11 +0200

    LayerGroup::Presenter dependency removed #6953

--- app/controllers/carto/api/vizjson3_presenter.rb
+++ app/controllers/carto/api/vizjson3_presenter.rb
@@ -157,15 +157,11 @@ module Carto
       end
 
       def layer_group_for(visualization, options)
-        LayerGroup::Presenter.new(
-          visualization.data_layers.map { |l| Carto::Api::LayerVizJSONAdapter.new(l) },
-          options, configuration).to_poro
+        VizJSON3LayerGroupPresenter.new(visualization.data_layers, options, configuration).to_poro
       end
 
       def named_map_layer_group_for(visualization, options)
-        LayerGroup::Presenter.new(
-          visualization.named_map_layers.map { |l| Carto::Api::LayerVizJSONAdapter.new(l) },
-          options, configuration).to_poro
+        VizJSON3LayerGroupPresenter.new(visualization.named_map_layers, options, configuration).to_poro
       end
 
       def other_layers_for(visualization, options, named_maps_presenter = nil)
@@ -493,6 +489,58 @@ module Carto
           end
         end
         @loaded = true
+      end
+    end
+
+    class VizJSON3LayerGroupPresenter
+      LAYER_GROUP_VERSION = '3.0.0'.freeze
+      DEFAULT_TILER_FILTER = 'mapnik'.freeze
+
+      def initialize(layers, options, configuration)
+        @layers         = layers
+        @options        = options
+        @configuration  = configuration
+      end
+
+      def to_poro
+        return nil if cartodb_layers.empty?
+
+        api_templates_type = options.fetch(:https_request, false) ? 'private' : 'public'
+
+        {
+          type:               'layergroup',
+          options:            {
+            user_name:          options.fetch(:user_name),
+            maps_api_template:  ApplicationHelper.maps_api_template(api_templates_type),
+            sql_api_template:   ApplicationHelper.sql_api_template(api_templates_type),
+            # tiler_* and sql_api_* are kept for backwards compatibility
+            tiler_protocol:     (configuration[:tiler]["public"]["protocol"] rescue nil),
+            tiler_domain:       (configuration[:tiler]["public"]["domain"] rescue nil),
+            tiler_port:         (configuration[:tiler]["public"]["port"] rescue nil),
+            sql_api_protocol:   (configuration[:sql_api]["public"]["protocol"] rescue nil),
+            sql_api_domain:     (configuration[:sql_api]["public"]["domain"] rescue nil),
+            sql_api_endpoint:   (configuration[:sql_api]["public"]["endpoint"] rescue nil),
+            sql_api_port:       (configuration[:sql_api]["public"]["port"] rescue nil),
+            filter:             @configuration[:tiler].fetch('filter', DEFAULT_TILER_FILTER),
+            layer_definition:   {
+              stat_tag:           options.fetch(:visualization_id),
+              version:            LAYER_GROUP_VERSION,
+              layers:             cartodb_layers
+            },
+            attribution: options.fetch(:attributions).join(', ')
+          }
+        }
+      end
+
+      private
+
+      attr_reader :layers, :configuration, :options
+
+      def cartodb_layers
+        @cartodb_layers ||= layers.map do |layer|
+          CartoDB::LayerModule::Presenter.new(
+            Carto::Api::LayerVizJSONAdapter.new(layer), options, configuration).to_vizjson_v2
+        end
       end
     end
   end

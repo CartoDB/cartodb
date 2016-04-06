5d2fc0bf3f34620a57146de41b0c280c76d5518a
javi <qualopec@gmail.com>
2016-04-06 08:51:00 +0200

    changed api methods

--- src/api.js
+++ src/api.js
@@ -4,21 +4,84 @@ var cdb = require('cartodb.js');
 
 function Dashboard (dashboard) {
   this.dashboard = dashboard;
-
-  // bind widget related methods
-  var ws = dashboard.widgets;
-  this.createCategory = ws.createCategoryModel.bind(ws);
-  this.createHistogram = ws.createHistogramModel.bind(ws);
-  this.createFormula = ws.createFormulaModel.bind(ws);
-  this.createTimeSeries = ws.createTimeSeriesModel.bind(ws);
-  this.getWidget = ws.get.bind(ws);
-  this.getWidgets = ws.getList.bind(ws);
 }
 
 Dashboard.prototype = {
+
+  /**
+   * @return {Map} the map used in the dashboard
+   */
   getMap: function () {
     return this.dashboard.vis;
+  },
+
+  /**
+   * @return {Array} of widgets in the dashboard
+   */
+  getWidgets: function () {
+    return this._dashboard.widgets.getList.call(this);
+  },
+
+  /**
+   * @param {Integer} id - widget id
+   * @return a widget object
+   */
+  getWidget: function (id) {
+    return this._dashboard.widgets.getWidget.call(this, id);
+  },
+
+  /**
+   * Create a category widget.
+   * @param {Object} widgetAttrs - attributes for the new widget
+   * @param {string} widgetAttrs.id - id (required)
+   * @param {string} widgetAttrs.title - title (required)
+   * @param {number} widgetAttrs.order - index of the widget (optional)
+   * @param ...
+   * @return {CategoryWidget} The new widget
+   */
+  createCategoryWidget: function (widgetAttrs) {
+    this._dashboard.widgets.createCategoryModel.call(this, widgetAttrs);
+  },
+
+  /**
+   * Create a histogram widget
+   * @param {Object} widgetAttrs - attributes for the new widget
+   * @param {string} widgetAttrs.id - id (required)
+   * @param {string} widgetAttrs.title - title (required)
+   * @param {number} widgetAttrs.order - index of the widget (optional)
+   * @param ...
+   * @return {HistogramWidget} The new widget
+   */
+  createHistogramWidget: function (widgetAttrs) {
+    this._dashboard.widgets.createHistogramModel.call(this, widgetAttrs);
+  },
+
+  /**
+   * Create a formula widget
+   * @param {Object} widgetAttrs - attributes for the new widget
+   * @param {string} widgetAttrs.id - id (required)
+   * @param {string} widgetAttrs.title - title (required)
+   * @param {number} widgetAttrs.order - index of the widget (optional)
+   * @param ...
+   * @return {FormulaWidget} The new widget
+   */
+  createFormulaWidget: function (widgetAttrs) {
+    this._dashboard.widgets.createFormulaModel.call(this, widgetAttrs);
+  },
+
+  /**
+   * Create a timesier es widget
+   * @param {Object} widgetAttrs - attributes for the new widget
+   * @param {string} widgetAttrs.id - id (required)
+   * @param {string} widgetAttrs.title - title (required)
+   * @param {number} widgetAttrs.order - index of the widget (optional)
+   * @param ...
+   * @return {TimeSeriesWidget} The new widget
+   */
+  createTimeSeriesWidget: function (widgetAttrs) {
+    this._dashboard.widgets.createTimeSeriesModel.call(this, widgetAttrs);
   }
+
 };
 
 module.exports = function (selector, vizJSON, opts, callback) {
@@ -44,6 +107,8 @@ module.exports = function (selector, vizJSON, opts, callback) {
         callback(new Error('error fetching viz.json file'));
       }
     });
+  } else {
+     _load(vizJSON, opts);
   }
 };
 

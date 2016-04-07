d7d3dea0a22ad871528cc316c2eaf64118a66946
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-04-07 08:07:59 +0200

    If source is present sql won't #6846 #6847 #7061

--- spec/requests/carto/api/vizjson3_presenter_spec.rb
+++ spec/requests/carto/api/vizjson3_presenter_spec.rb
@@ -96,8 +96,8 @@ describe Carto::Api::VizJSON3Presenter do
       v2_vizjson = Carto::Api::VizJSONPresenter.new(@visualization, $tables_metadata).send :calculate_vizjson
       v3_vizjson = Carto::Api::VizJSON3Presenter.new(@visualization, viewer_user).send :calculate_vizjson
 
-      v2_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:sql].should eq query
-      v2_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:source].should be_nil
+      v2_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:sql].should be_nil
+      v2_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:source].should eq source
       v3_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:sql].should be_nil
       v3_vizjson[:layers][1][:options][:layer_definition][:layers][0][:options][:source].should eq source
     end

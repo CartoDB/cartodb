bc91ff8f35ec3d3933780c2d8fb526aebe1bba16
Mario de Frutos <mario.defrutos@cartodb.com>
2016-04-12 13:30:44 +0200

    Fixes error when try to finish the sync of datasources

--- app/models/synchronization/member.rb
+++ app/models/synchronization/member.rb
@@ -360,7 +360,10 @@ module CartoDB
         self.run_at         = Time.now + interval
         self.modified_at    = importer.last_modified
         geocode_table
-      rescue
+      rescue => exception
+        CartoDB::Logger.error(exception: exception,
+                                message: 'Error updating state for sync table',
+                                sync_id: self.id)
         self
       end
 
@@ -511,7 +514,6 @@ module CartoDB
         false
       end
 
-      # @return mixed|nil
       def get_datasource(datasource_name)
         begin
           datasource = DatasourcesFactory.get_datasource(datasource_name, user, {

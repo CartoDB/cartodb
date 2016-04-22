d5fab540e9636b125e9577ace0fbc129c3674cb3
Francisco Dans <fdansv@gmail.com>
2016-04-22 14:57:13 +0200

    refactors auto style with factory

---
+++ src/widgets/auto-style/auto-styler.js
@@ -0,0 +1,49 @@
+var cdb = require('cartodb.js');
+var CategoryColors = require('./category-colors');
+var AutoStyler = cdb.core.Model.extend({
+  initialize: function (dataviewModel) {
+    this.dataviewModel = dataviewModel;
+    this.colors = new CategoryColors();
+    this.layer = this.dataviewModel.layer;
+  },
+
+  _getLayerHeader: function (symbol) {
+    return '#' + this.dataviewModel.layer.get('layer_name') + '[mapnik-geometry-type=' + AutoStyler.MAPNIK_MAPPING[symbol] + ']{';
+  }
+
+});
+
+AutoStyler.STYLE_TEMPLATE = {
+  polygon: ['{{layername}}',
+            '  polygon-fill: {{defaultColor}};',
+            '  polygon-opacity: 0.6;  ',
+            '  line-color: #FFF;',
+            '  line-width: 0.3;',
+            '  line-opacity: 0.3;',
+            '  {{ramp}}',
+            '}'].join('\n'),
+  marker: ['{{layername}}',
+         '  marker-width: {{markerWidth}};',
+         '  marker-fill-opacity: 0.8;  ',
+         '  marker-fill: {{defaultColor}};  ',
+         '  marker-line-color: #fff;',
+         '  marker-allow-overlap: true;',
+         '  marker-line-width: 0.3;',
+         '  marker-line-opacity: 0.8;',
+         '  {{ramp}}',
+         '}'].join('\n'),
+  line: ['{{layername}}',
+          '  line-color: {{defaultColor}};',
+          '  line-width: 0.3;',
+          '  line-opacity: 0.3;',
+          '  {{ramp}}',
+          '}'].join('\n')
+};
+
+AutoStyler.MAPNIK_MAPPING = {
+  polygon: 'polygon',
+  marker: 'point',
+  line: 'linestring'
+};
+
+module.exports = AutoStyler;
---
+++ src/widgets/auto-style/category-colors.js
@@ -0,0 +1,66 @@
+var _ = require('underscore');
+var colorScales = [['#2CA095', '#E5811B', '#4A4DBA', '#AD2BAD', '#559030', '#E1C221'],
+                   ['rgb(166,206,227)', 'rgb(31,120,180)', 'rgb(178,223,138)', 'rgb(51,160,44)', 'rgb(251,154,153)'],
+                   ['rgb(27,158,119)', 'rgb(217,95,2)', 'rgb(117,112,179)', 'rgb(231,41,138)', 'rgb(102,166,30)', 'rgb(230,171,2)'],
+                   ['rgb(127,201,127)', 'rgb(190,174,212)', 'rgb(253,192,134)', 'rgb(255,255,153)', 'rgb(56,108,176)', 'rgb(240,2,127)'],
+                   ['rgb(141,211,199)', 'rgb(255,255,179)', 'rgb(190,186,218)', 'rgb(251,128,114)', 'rgb(128,177,211)', 'rgb(253,180,98)']
+                  ]; // Demo colors
+var defaultColor = '#CCC';
+
+/**
+ *  Class to set categories to each color
+ *  - Right now, there is a relation 1 color to 1 category.
+ *  - If that category is not available in the new data, that
+ *    color will be freed.
+ *
+ */
+
+function CategoryColors () {
+  this.colors = {};
+  _.each(colorScales[0], function (c) {
+    this.colors[c] = null;
+  }, this);
+}
+
+CategoryColors.prototype.updateData = function (d) {
+  // Remove categories from colors where they are not present anymore
+  // _.each(this.colors, function (value, key) {
+  //   if (!_.contains(d, value)) {
+  //     this.colors[key] = null;
+  //   } else {
+  //     d = _.without(d, value);
+  //   }
+  // }, this);
+
+  // Set colors by new categories
+  _.each(d, function (category) {
+    var nextFreeColor = this.getNextAvailableColor();
+    if (nextFreeColor) {
+      this.colors[nextFreeColor] = category;
+    }
+  }, this);
+};
+
+CategoryColors.prototype.getNextAvailableColor = function () {
+  for (var i in this.colors) {
+    if (this.colors[i] === null) {
+      return i;
+    }
+  }
+  return null;
+};
+
+CategoryColors.prototype.getColorByCategory = function (category) {
+  for (var i in this.colors) {
+    if (this.colors[i] === category) {
+      return i;
+    }
+  }
+  return defaultColor;
+};
+
+CategoryColors.prototype.getCategoryByColor = function (color) {
+  return this.colors[color];
+};
+
+module.exports = CategoryColors;

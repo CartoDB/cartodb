90a965205bdf30755a8f059add75a9788b98cd5e
Francisco Dans <fdansv@gmail.com>
2016-04-22 12:02:01 +0200

    adds auto style tests

---
+++ spec/widgets/auto-style.spec.js
@@ -0,0 +1,27 @@
+var specHelper = require('../spec-helper');
+var CategoryWidgetModel = require('../../src/widgets/category/category-widget-model');
+describe('auto-style', function () {
+  beforeEach(function () {
+    var vis = specHelper.createDefaultVis();
+    var layer = vis.map.layers.first();
+    layer.restoreCartoCSS = jasmine.createSpy('restore');
+    this.dataviewModel = vis.dataviews.createCategoryModel(layer, {
+      column: 'col'
+    });
+    this.widgetModel = new CategoryWidgetModel({}, {
+      dataviewModel: this.dataviewModel
+    });
+    this.autoStyler = this.widgetModel.autoStyler;
+  });
+
+  it('should generate some cartocss', function () {
+    expect(typeof this.autoStyler.getStyle()).toEqual('string');
+  });
+
+  it('should generate category ramps', function () {
+    this.dataviewModel.set('allCategoryNames', ['manolo', 'jacinto', 'eustaquio']);
+    expect(this.autoStyler._generateCategoryRamp('marker').indexOf('marker-fill')).not.toBeLessThan(0);
+    expect(this.autoStyler._generateCategoryRamp('polygon').indexOf('polygon-fill')).not.toBeLessThan(0);
+    expect(this.autoStyler._generateCategoryRamp('line').indexOf('line-color')).not.toBeLessThan(0);
+  });
+});

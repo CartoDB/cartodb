302a5b73ef488fa50079205cb9d447d6de666eb5
Javier Torres <jtorres@cartodb.com>
2016-04-26 16:07:10 +0200

    Fix gdrive_spec

--- services/datasources/spec/acceptance/gdrive_spec.rb
+++ services/datasources/spec/acceptance/gdrive_spec.rb
@@ -14,13 +14,12 @@ describe Url::GDrive do
 
   describe '#manual_test' do
     it 'with user interaction, tests the full oauth flow and lists files of an account' do
+      config = get_config
       if !config.include?(:refresh_token)
         pending('If config unset, this test requires manual running. Check its source code to see what to do')
       end
 
       user_mock = CartoDB::Datasources::Doubles::User.new
-
-      config = get_config
       gdrive_datasource = Url::GDrive.get_new(config, user_mock)
 
       if config.include?(:refresh_token)

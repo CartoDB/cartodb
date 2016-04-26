4a36fabd8699bfa8ba5c6e1fac2ed34011d29d79
Francisco Dans <fdansv@gmail.com>
2016-04-26 17:15:33 +0200

    lint

--- spec/widgets/auto-style/category.spec.js
+++ spec/widgets/auto-style/category.spec.js
@@ -5,8 +5,8 @@ describe('src/widgets/auto-style/category', function () {
   beforeEach(function () {
     this.dataview = new Backbone.Model({
       allCategoryNames: ['a', 'b', 'c'],
-      column: 'something' 
-    })
+      column: 'something'
+    });
 
     this.layer = this.dataview.layer = jasmine.createSpyObj('layer', ['getGeometryType']);
     this.categoryAutoStyler = new CategoryAutoStyler(this.dataview);
@@ -15,17 +15,17 @@ describe('src/widgets/auto-style/category', function () {
   describe('.getStyle', function () {
     it('should generate the right styles when layer has polygons', function () {
       this.layer.getGeometryType.and.returnValue('polygon');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, "")).toEqual('#layer{polygon-fill:#CCC;polygon-opacity:0.6;line-color:#FFF;line-width:0.3;line-opacity:0.3;[something=\'a\']{polygon-fill:#CCC;}[something=\'b\']{polygon-fill:#CCC;}[something=\'c\']{polygon-fill:#CCC;}}');
-    })
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{polygon-fill:#CCC;polygon-opacity:0.6;line-color:#FFF;line-width:0.3;line-opacity:0.3;[something=\'a\']{polygon-fill:#CCC;}[something=\'b\']{polygon-fill:#CCC;}[something=\'c\']{polygon-fill:#CCC;}}');
+    });
 
     it('should generate the right styles when layer has points', function () {
       this.layer.getGeometryType.and.returnValue('marker');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, "")).toEqual('#layer{marker-width:10;marker-fill-opacity:0.8;marker-fill:#CCC;marker-line-color:#fff;marker-allow-overlap:true;marker-line-width:0.3;marker-line-opacity:0.8;[something=\'a\']{marker-fill:#CCC;}[something=\'b\']{marker-fill:#CCC;}[something=\'c\']{marker-fill:#CCC;}}');
-    })
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{marker-width:10;marker-fill-opacity:0.8;marker-fill:#CCC;marker-line-color:#fff;marker-allow-overlap:true;marker-line-width:0.3;marker-line-opacity:0.8;[something=\'a\']{marker-fill:#CCC;}[something=\'b\']{marker-fill:#CCC;}[something=\'c\']{marker-fill:#CCC;}}');
+    });
 
     it('should generate the right styles when layer has lines', function () {
       this.layer.getGeometryType.and.returnValue('line');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, "")).toEqual('#layer{line-color:#CCC;line-width:0.3;line-opacity:0.3;[something=\'a\']{line-color:#CCC;}[something=\'b\']{line-color:#CCC;}[something=\'c\']{line-color:#CCC;}}');
-    })
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{line-color:#CCC;line-width:0.3;line-opacity:0.3;[something=\'a\']{line-color:#CCC;}[something=\'b\']{line-color:#CCC;}[something=\'c\']{line-color:#CCC;}}');
+    });
   });
 });
--- spec/widgets/auto-style/histogram.js
+++ spec/widgets/auto-style/histogram.js
@@ -4,8 +4,8 @@ var Backbone = require('backbone');
 describe('src/widgets/auto-style/histogram', function () {
   beforeEach(function () {
     this.dataview = new Backbone.Model({
-      column: 'something' 
-    })
+      column: 'something'
+    });
 
     this.layer = this.dataview.layer = jasmine.createSpyObj('layer', ['getGeometryType']);
     this.histogramAutoStyler = new HistogramAutoStyler(this.dataview);
@@ -14,17 +14,17 @@ describe('src/widgets/auto-style/histogram', function () {
   describe('.getStyle', function () {
     it('should generate the right styles when layer has polygons', function () {
       this.layer.getGeometryType.and.returnValue('polygon');
-      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, "").indexOf('#layer{polygon-fill:ramp([something]')).not.toBeLessThan(0);
-    })
+      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, '').indexOf('#layer{polygon-fill:ramp([something]')).not.toBeLessThan(0);
+    });
 
     it('should generate the right styles when layer has points', function () {
       this.layer.getGeometryType.and.returnValue('marker');
-      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, "").indexOf('#layer{marker-width:ramp([something]')).not.toBeLessThan(0);
-    })
+      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, '').indexOf('#layer{marker-width:ramp([something]')).not.toBeLessThan(0);
+    });
 
     it('should generate the right styles when layer has lines', function () {
       this.layer.getGeometryType.and.returnValue('line');
-      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, "")).toEqual('#layer{line-color:#000;line-width:0.3;line-opacity:0.3;}');
-    })
+      expect(this.histogramAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{line-color:#000;line-width:0.3;line-opacity:0.3;}');
+    });
   });
 });

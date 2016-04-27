de20baa87c7ff1c716c1003f660a0061af3fa6d7
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-04-27 14:20:45 +0200

    Remove commented code

--- app/models/table_registrar.rb
+++ app/models/table_registrar.rb
@@ -25,11 +25,6 @@ module CartoDB
     end
 
     def get_valid_table_name(table_name)
-      # table_klass.get_valid_table_name(table_name, {
-      #     connection: user.in_database,
-      #     database_schema: user.database_schema
-      #   })
-
       table_klass.new_get_valid_table_name(table_name, user)
     end
 

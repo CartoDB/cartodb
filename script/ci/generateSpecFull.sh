836df328e22037ed8679af0b08b228568d795d88
Javier Torres <jtorres@cartodb.com>
2016-04-27 15:24:22 +0200

    Re-disable Varnish test

--- script/ci/generateSpecFull.sh
+++ script/ci/generateSpecFull.sh
@@ -24,7 +24,7 @@ do
   first=0
 done
 
-cat Makefile | \
+cat Makefile | grep -v 'spec/lib/varnish_spec.rb' | \
 grep -v $DISABLED_TEST_REGEX | \
 grep -v 'require ./spec/rspec_configuration.rb'| \
 grep 'rb'| sed -e 's/^\s*//' -e '/^$/d' | sed '/^#/ d' | sed 's/\\//' | sed 's/\s.*$//' > temp.txt

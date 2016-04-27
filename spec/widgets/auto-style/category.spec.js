2bd743179ac71e6f6b8c58358895ed6177f2c832
Francisco Dans <fdansv@gmail.com>
2016-04-27 17:14:55 +0200

    corrects tests

--- spec/widgets/auto-style/category.spec.js
+++ spec/widgets/auto-style/category.spec.js
@@ -15,17 +15,17 @@ describe('src/widgets/auto-style/category', function () {
   describe('.getStyle', function () {
     it('should generate the right styles when layer has polygons', function () {
       this.layer.getGeometryType.and.returnValue('polygon');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{polygon-fill:#CCC;polygon-opacity:0.6;line-color:#FFF;line-width:0.3;line-opacity:0.3;[something=\'a\']{polygon-fill:#CCC;}[something=\'b\']{polygon-fill:#CCC;}[something=\'c\']{polygon-fill:#CCC;}}');
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{polygon-fill:#E1C221;polygon-opacity:0.6;line-color:#FFF;line-width:0.3;line-opacity:0.3;[something=\'a\']{polygon-fill:#E1C221;}[something=\'b\']{polygon-fill:#E1C221;}[something=\'c\']{polygon-fill:#E1C221;}}');
     });
 
     it('should generate the right styles when layer has points', function () {
       this.layer.getGeometryType.and.returnValue('marker');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{marker-width:10;marker-fill-opacity:0.8;marker-fill:#CCC;marker-line-color:#fff;marker-allow-overlap:true;marker-line-width:0.3;marker-line-opacity:0.8;[something=\'a\']{marker-fill:#CCC;}[something=\'b\']{marker-fill:#CCC;}[something=\'c\']{marker-fill:#CCC;}}');
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{marker-width:10;marker-fill-opacity:0.8;marker-fill:#E1C221;marker-line-color:#fff;marker-allow-overlap:true;marker-line-width:0.3;marker-line-opacity:0.8;[something=\'a\']{marker-fill:#E1C221;}[something=\'b\']{marker-fill:#E1C221;}[something=\'c\']{marker-fill:#E1C221;}}');
     });
 
     it('should generate the right styles when layer has lines', function () {
       this.layer.getGeometryType.and.returnValue('line');
-      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{line-color:#CCC;line-width:0.3;line-opacity:0.3;[something=\'a\']{line-color:#CCC;}[something=\'b\']{line-color:#CCC;}[something=\'c\']{line-color:#CCC;}}');
+      expect(this.categoryAutoStyler.getStyle().replace(/\s/g, '')).toEqual('#layer{line-color:#E1C221;line-width:0.3;line-opacity:0.3;[something=\'a\']{line-color:#E1C221;}[something=\'b\']{line-color:#E1C221;}[something=\'c\']{line-color:#E1C221;}}');
     });
   });
 });

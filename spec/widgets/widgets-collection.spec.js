2bf84bea8cb8de4e4ee264ae18e085ce32805a0c
Francisco Dans <fdansv@gmail.com>
2016-04-27 12:09:30 +0200

    adds corrected test

--- spec/widgets/widgets-collection.spec.js
+++ spec/widgets/widgets-collection.spec.js
@@ -1,5 +1,6 @@
 var WidgetsCollection = require('../../src/widgets/widgets-collection');
 var _ = require('underscore');
+var Backbone = require('backbone');
 
 describe('widgets/widgets-collection', function () {
   beforeEach(function () {
@@ -16,6 +17,39 @@ describe('widgets/widgets-collection', function () {
       {order: 1},
       {order: 2, autoStyle: true}
     ]);
+  });
+
+  it('should only allow autoStyle in one layer at a time', function () {
+    var m1 = this.collection.at(0);
+    var m2 = this.collection.at(1);
+    var m3 = this.collection.at(2);
+
+    m1.dataviewModel = { layer: new Backbone.Model({
+        layer_name: 'l1'
+      })
+    }
+    m2.dataviewModel = { layer: new Backbone.Model({
+        layer_name: 'l2'
+      })
+    }
+    m3.dataviewModel = { layer: new Backbone.Model({
+        layer_name: 'l1'
+      })
+    }
+
+    expect(m1.get('autoStyle')).toBeFalsy();
+    expect(m2.get('autoStyle')).toBeUndefined();
+    expect(m3.get('autoStyle')).toBeTruthy();
+
+    m1.set('autoStyle', true);
+    expect(m1.get('autoStyle')).toBeTruthy();
+    expect(m2.get('autoStyle')).toBeUndefined();
+    expect(m3.get('autoStyle')).toBeFalsy();
+
+    m2.set('autoStyle', true);
+    expect(m1.get('autoStyle')).toBeTruthy();
+    expect(m2.get('autoStyle')).toBeTruthy();
+    expect(m3.get('autoStyle')).toBeFalsy();
   });
 
   it('should sort the collection and trigger orderChanged event when any widget order changes', function () {

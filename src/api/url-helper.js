758e5354c427889551bbe8485f461376cc206781
Francisco Dans <fdansv@gmail.com>
2016-04-29 13:06:37 +0200

    lint

--- src/api/url-helper.js
+++ src/api/url-helper.js
@@ -3,13 +3,13 @@ var _ = require('underscore');
 
 module.exports = {
   getURLFromState: function (state) {
-      var thisURL = new URI(this.getLocalURL());
-      thisURL.removeQuery('state');
-      if (!_.isEmpty(state)) {
-        var statesString = encodeURIComponent(JSON.stringify(state));
-        thisURL.setQuery('state', statesString);
-      }
-      return thisURL.toString();
+    var thisURL = new URI(this.getLocalURL());
+    thisURL.removeQuery('state');
+    if (!_.isEmpty(state)) {
+      var statesString = encodeURIComponent(JSON.stringify(state));
+      thisURL.setQuery('state', statesString);
+    }
+    return thisURL.toString();
   },
   getStateFromCurrentURL: function () {
     var currentURL = this.getLocalURL();
@@ -19,7 +19,7 @@ module.exports = {
     var uri = new URI(url);
     if (uri.hasQuery('state')) {
       return JSON.parse(decodeURIComponent(uri.query(true)['state']));
-    } 
+    }
     return {};
   },
   getLocalURL: _.constant(window.location.href)

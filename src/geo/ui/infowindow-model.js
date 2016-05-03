dc35713169ed2730c6e264b698fe6dfbd55a3e79
Pablo Alonso Garcia <pablo@cartodb.com>
2016-05-03 11:36:05 +0200

    Removed unused methods

--- src/geo/ui/infowindow-model.js
+++ src/geo/ui/infowindow-model.js
@@ -1,5 +1,3 @@
-var _ = require('underscore');
-var $ = require('jquery');
 var Backbone = require('backbone');
 
 /**
@@ -45,141 +43,6 @@ var InfowindowModel = Backbone.Model.extend({
 
   hasInfowindowTemplate: function (infowindowTemplateModel) {
     return this._infowindowTemplateModel && this._infowindowTemplateModel === infowindowTemplateModel;
-  },
-
-  // THE FOLLOWING METHODS ARE NOT USED INTERNALLY:
-
-  clearFields: function () {
-    this.set({fields: []});
-  },
-
-  saveFields: function (where) {
-    where = where || 'old_fields';
-    this.set(where, _.clone(this.get('fields')));
-  },
-
-  fieldCount: function () {
-    var fields = this.get('fields');
-    if (!fields) return 0;
-    return fields.length;
-  },
-
-  restoreFields: function (whiteList, from) {
-    from = from || 'old_fields';
-    var fields = this.get(from);
-    if (whiteList) {
-      fields = fields.filter(function (f) {
-        return _.contains(whiteList, f.name);
-      });
-    }
-    if (fields && fields.length) {
-      this._setFields(fields);
-    }
-    this.unset(from);
-  },
-
-  _cloneFields: function () {
-    return _(this.get('fields')).map(function (v) {
-      return _.clone(v);
-    });
-  },
-
-  _setFields: function (f) {
-    f.sort(function (a, b) { return a.position - b.position; });
-    this.set({'fields': f});
-  },
-
-  sortFields: function () {
-    this.get('fields').sort(function (a, b) { return a.position - b.position; });
-  },
-
-  _addField: function (fieldName, at) {
-    var dfd = $.Deferred();
-    if (!this.containsField(fieldName)) {
-      var fields = this.get('fields');
-      if (fields) {
-        at = at === undefined ? fields.length : at;
-        fields.push({ name: fieldName, title: true, position: at });
-      } else {
-        at = at === undefined ? 0 : at;
-        this.set('fields', [{ name: fieldName, title: true, position: at }], { silent: true });
-      }
-    }
-    dfd.resolve();
-    return dfd.promise();
-  },
-
-  addField: function (fieldName, at) {
-    var self = this;
-    $.when(this._addField(fieldName, at)).then(function () {
-      self.sortFields();
-      self.trigger('change:fields');
-      self.trigger('add:fields');
-    });
-    return this;
-  },
-
-  getFieldProperty: function (fieldName, k) {
-    if (this.containsField(fieldName)) {
-      var fields = this.get('fields') || [];
-      var idx = _.indexOf(_(fields).pluck('name'), fieldName);
-      return fields[idx][k];
-    }
-    return null;
-  },
-
-  setFieldProperty: function (fieldName, k, v) {
-    if (this.containsField(fieldName)) {
-      var fields = this._cloneFields() || [];
-      var idx = _.indexOf(_(fields).pluck('name'), fieldName);
-      fields[idx][k] = v;
-      this._setFields(fields);
-    }
-    return this;
-  },
-
-  setAlternativeName: function (fieldName, alternativeName) {
-    var alternativeNames = this.get('alternative_names') || [];
-    alternativeNames[fieldName] = alternativeName;
-    this.set({ 'alternative_names': alternativeNames });
-    this.trigger('change:alternative_names');
-  },
-
-  getFieldPos: function (fieldName) {
-    var p = this.getFieldProperty(fieldName, 'position');
-    if (p === undefined) {
-      return Number.MAX_VALUE;
-    }
-    return p;
-  },
-
-  containsAlternativeName: function (fieldName) {
-    var names = this.get('alternative_names') || [];
-    return names[fieldName];
-  },
-
-  containsField: function (fieldName) {
-    var fields = this.get('fields') || [];
-    return _.contains(_(fields).pluck('name'), fieldName);
-  },
-
-  removeField: function (fieldName) {
-    if (this.containsField(fieldName)) {
-      var fields = this._cloneFields() || [];
-      var idx = _.indexOf(_(fields).pluck('name'), fieldName);
-      if (idx >= 0) {
-        fields.splice(idx, 1);
-      }
-      this._setFields(fields);
-      this.trigger('remove:fields');
-    }
-    return this;
-  },
-
-  closeInfowindow: function () {
-    if (this.get('visibility')) {
-      this.set('visibility', false);
-    }
   }
 }, {
   contentForFields: function (attributes, fields, options) {
@@ -192,7 +55,7 @@ var InfowindowModel = Backbone.Model.extend({
       if (options.empty_fields || (value !== undefined && value !== null)) {
         render_fields.push({
           title: field.title ? field.name : null,
-          value: value ? value : '&nbsp;',
+          value: value || '&nbsp;',
           index: j
         });
       }
--- test/spec/geo/ui/infowindow-model.spec.js
+++ test/spec/geo/ui/infowindow-model.spec.js
@@ -1,110 +1,41 @@
-var $ = require('jquery');
 var InfowindowModel = require('../../../../src/geo/ui/infowindow-model');
 
-describe('geo/ui/infowindow-model', function() {
-  var model;
-
-  beforeEach(function() {
-    model = new InfowindowModel();
-  });
-
-  it("should allow adding an alternative name", function() {
-    model.addField('the_name');
-    model.addField('the_description');
-
-    model.setAlternativeName('the_name', 'nombre');
-    model.setAlternativeName('the_description', 'descripción');
-
-    n = model.getAlternativeName('the_name');
-    d = model.getAlternativeName('the_description');
-
-    expect(n).toEqual("nombre");
-    expect(d).toEqual("descripción");
-
-  });
-
-  it("should add a field", function() {
-    expect(model.containsField('test')).toEqual(false);
-    model.addField('test');
-    model.addField('test2');
-    expect(model.containsField('test')).toEqual(true);
-    model.removeField('test');
-    expect(model.containsField('test')).toEqual(false);
-    expect(model.containsField('test2')).toEqual(true);
-    model.clearFields();
-    expect(model.containsField('test2')).toEqual(false);
-  });
-
-  it("should add a field in order", function() {
-    model.addField('test', 1);
-    model.addField('test2', 0);
-    expect(model.get('fields')[0].name).toEqual('test2');
-    expect(model.get('fields')[1].name).toEqual('test');
-  });
-
-  it("should allow modify field properties", function() {
-    var spy = jasmine.createSpy('change:fields');
-    model.addField('test');
-    var t = model.getFieldProperty('test', 'title');
-    expect(t).toEqual(true);
-    model.bind('change:fields', spy);
-    expect(spy).not.toHaveBeenCalled();
-    model.setFieldProperty('test', 'title', false);
-    t = model.getFieldProperty('test', 'title');
-    expect(t).toEqual(false);
-    expect(spy).toHaveBeenCalled();
-  });
-
-  it("should save and restore fields", function() {
-    model.addField('test', 1);
-    model.addField('test2', 0);
-    model.addField('test3', 3);
-    model.saveFields();
-    expect(model.get('old_fields')).toEqual(model.get('fields'));
-    model.clearFields();
-    model.restoreFields();
-    expect(model.get('old_fields')).toEqual(undefined);
-    expect(model.get('fields')[0].name).toEqual('test2');
-    expect(model.get('fields')[1].name).toEqual('test');
-    expect(model.get('fields')[2].name).toEqual('test3');
-  });
-
-  describe(".contentForFields", function() {
-
-    it('should return the title and value of each field', function() {
+describe('geo/ui/infowindow-model', function () {
+  describe('.contentForFields', function () {
+    it('should return the title and value of each field', function () {
       var attributes = { field1: 'value1' };
       var fields = [{ name: 'field1', title: true }];
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.fields.length).toEqual(1);
       expect(content.fields[0].title).toEqual('field1');
       expect(content.fields[0].value).toEqual('value1');
     });
 
-    it('should not return the title if not specified', function() {
+    it('should not return the title if not specified', function () {
       var attributes = { field1: 'value1' };
       var fields = [{ name: 'field1' }]; // Field doesn't have a title attribute
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.fields.length).toEqual(1);
       expect(content.fields[0].title).toEqual(null);
     });
 
-    it('should return the index of each field', function() {
+    it('should return the index of each field', function () {
       var attributes = { field1: 'value1', field2: 'value2' };
       var fields = [{ name: 'field1' }, { name: 'field2' }];
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.fields.length).toEqual(2);
       expect(content.fields[0].index).toEqual(0);
       expect(content.fields[1].index).toEqual(1);
     });
 
-    it('should return empty fields', function() {
+    it('should return empty fields', function () {
       var attributes = { field1: 'value1' };
       var fields = [{ name: 'field1' }, { name: 'field2' }];
       var options = { empty_fields: true };
-      var content = InfowindowModel.contentForFields(attributes, fields, options)
+      var content = InfowindowModel.contentForFields(attributes, fields, options);
 
       expect(content.fields.length).toEqual(2);
       expect(content.fields[0]).toEqual({
@@ -119,11 +50,11 @@ describe('geo/ui/infowindow-model', function() {
       });
     });
 
-    it('should not return empty fields', function() {
+    it('should not return empty fields', function () {
       var attributes = { field1: 'value1' };
       var fields = [{ name: 'field1' }, { name: 'field2' }];
       var options = { empty_fields: false };
-      var content = InfowindowModel.contentForFields(attributes, fields, options)
+      var content = InfowindowModel.contentForFields(attributes, fields, options);
 
       expect(content.fields.length).toEqual(1);
       expect(content.fields[0]).toEqual({
@@ -133,10 +64,10 @@ describe('geo/ui/infowindow-model', function() {
       });
     });
 
-    it('should not return fields with a null value', function() {
+    it('should not return fields with a null value', function () {
       var attributes = { field1: 'wadus', field2: null };
       var fields = [{ name: 'field1' }, { name: 'field2' }];
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.fields.length).toEqual(1);
       expect(content.fields[0]).toEqual({
@@ -146,18 +77,18 @@ describe('geo/ui/infowindow-model', function() {
       });
     });
 
-    it('should return the attributes as data', function() {
+    it('should return the attributes as data', function () {
       var attributes = { field1: 'value1' };
       var fields = [{ name: 'field1' }];
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.data).toEqual(attributes);
     });
 
-    it('should return an empty field when no data is available', function() {
+    it('should return an empty field when no data is available', function () {
       var attributes = {};
       var fields = [{ name: 'field1' }, { name: 'field2' }];
-      var content = InfowindowModel.contentForFields(attributes, fields, {})
+      var content = InfowindowModel.contentForFields(attributes, fields, {});
 
       expect(content.fields.length).toEqual(1);
       expect(content.fields[0]).toEqual({

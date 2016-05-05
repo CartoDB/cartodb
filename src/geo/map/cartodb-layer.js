59f4e21012738cbcba0def3fdad8574c4b8bc12a
Pablo Alonso Garcia <pablo@cartodb.com>
2016-05-05 18:40:00 +0200

    No need to set attributes silently... nobody is listening yet!

--- src/geo/map/cartodb-layer.js
+++ src/geo/map/cartodb-layer.js
@@ -22,9 +22,9 @@ var CartoDBLayer = LayerModelBase.extend({
     }
 
     this.infowindow = new InfowindowTemplate(attrs.infowindow);
-    this.unset('infowindow', { silent: true });
+    this.unset('infowindow');
     this.tooltip = new TooltipTemplate(attrs.tooltip);
-    this.unset('tooltip', { silent: true });
+    this.unset('tooltip');
 
     this.bind('change:visible change:sql change:source', this._reloadMap, this);
     this.bind('change:cartocss', this._onCartoCSSChanged);

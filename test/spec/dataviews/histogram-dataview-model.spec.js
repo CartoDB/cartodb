b8faad736bd6273ac5fb58f634f06a3b1c1434e7
Francisco Dans <fdansv@gmail.com>
2016-05-09 15:26:36 +0200

    skips test instead of commenting our

--- test/spec/dataviews/histogram-dataview-model.spec.js
+++ test/spec/dataviews/histogram-dataview-model.spec.js
@@ -89,17 +89,17 @@ describe('dataviews/histogram-dataview-model', function () {
       ]);
       expect(this.model.getHistogramShape()).toEqual('L');
     });
-    // it ('when it is clustered', function () {
-    //   this.model.set('data', [
-    //     {bin: 0, freq: 20},
-    //     {bin: 1, freq: 18},
-    //     {bin: 2, freq: 5},
-    //     {bin: 3, freq: 0},
-    //     {bin: 4, freq: 32},
-    //     {bin: 5, freq: 16}
-    //   ]);
-    //   expect(this.model.getHistogramShape()).toEqual('C');
-    // });
+   xit ('when it is clustered', function () {
+     this.model.set('data', [
+       {bin: 0, freq: 20},
+       {bin: 1, freq: 18},
+       {bin: 2, freq: 5},
+       {bin: 3, freq: 0},
+       {bin: 4, freq: 32},
+       {bin: 5, freq: 16}
+     ]);
+     expect(this.model.getHistogramShape()).toEqual('C');
+   });
   });
 
   describe('on unfiltered data model fetch', function () {

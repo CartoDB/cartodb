9f255ccfa93e7b04ae7764464be67e7fd4fcc252
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-10 14:04:58 +0200

    refactored *_limit? methods

--- app/models/carto/user.rb
+++ app/models/carto/user.rb
@@ -335,40 +335,47 @@ class Carto::User < ActiveRecord::Base
   def soft_geocoding_limit?
     Carto::AccountType.new.soft_geocoding_limit?(self)
   end
+
   alias_method :soft_geocoding_limit, :soft_geocoding_limit?
 
   def hard_geocoding_limit?
-    !self.soft_geocoding_limit?
+    !soft_geocoding_limit?
   end
+
   alias_method :hard_geocoding_limit, :hard_geocoding_limit?
 
   def soft_here_isolines_limit?
     Carto::AccountType.new.soft_here_isolines_limit?(self)
   end
+
   alias_method :soft_here_isolines_limit, :soft_here_isolines_limit?
 
   def hard_here_isolines_limit?
-    !self.soft_here_isolines_limit?
+    !soft_here_isolines_limit?
   end
+
   alias_method :hard_here_isolines_limit, :hard_here_isolines_limit?
 
   def soft_obs_snapshot_limit?
     Carto::AccountType.new.soft_obs_snapshot_limit?(self)
   end
+
   alias_method :soft_obs_snapshot_limit, :soft_obs_snapshot_limit?
 
   def hard_obs_snapshot_limit?
-    !self.soft_obs_snapshot_limit?
+    !soft_obs_snapshot_limit?
   end
+
   alias_method :hard_obs_snapshot_limit, :hard_obs_snapshot_limit?
 
   def soft_twitter_datasource_limit?
-    self.soft_twitter_datasource_limit  == true
+    !!soft_twitter_datasource_limit
   end
 
   def hard_twitter_datasource_limit?
-    !self.soft_twitter_datasource_limit?
+    !soft_twitter_datasource_limit?
   end
+
   alias_method :hard_twitter_datasource_limit, :hard_twitter_datasource_limit?
 
   def trial_ends_at

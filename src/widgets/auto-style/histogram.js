b98c6f74fb3dac47bcb0cb9be7e0f3e646195cfb
makella <makella@cartodb.com>
2016-05-12 13:51:05 -0600

    changed default marker width and marker ramp for C and U

--- src/widgets/auto-style/histogram.js
+++ src/widgets/auto-style/histogram.js
@@ -42,16 +42,16 @@ var HistogramAutoStyler = AutoStyler.extend({
     } else if (geometryType === 'marker') {
       if (shape === 'F') {
         style = style.replace('{{defaultColor}}', 'ramp([{{column}}], cartocolor(RedOr1, {{bins}})), quantiles')
-                     .replace('{{markerWidth}}', '6');
+                     .replace('{{markerWidth}}', '7');
       } else if (shape === 'L' || shape === 'J') {
         style = style.replace('{{defaultColor}}', 'ramp([{{column}}], cartocolor(Sunset2, {{bins}}), headstails)')
-                     .replace('{{markerWidth}}', '6');
+                     .replace('{{markerWidth}}', '7');
       } else if (shape === 'A') {
         style = style.replace('{{defaultColor}}', 'ramp([{{column}}], cartocolor(Geyser, {{bins}})), quantiles')
-                     .replace('{{markerWidth}}', '6');
+                     .replace('{{markerWidth}}', '7');
       } else if (shape === 'C' || shape === 'U') {
-        style = style.replace('{{defaultColor}}', 'ramp([{{column}}], cartocolor(Emrld1, {{bins}}), jenks)')
-                     .replace('{{markerWidth}}', '6');
+        style = style.replace('{{defaultColor}}', 'ramp([{{column}}], cartocolor(BluYl1, {{bins}}), jenks)')
+                     .replace('{{markerWidth}}', '7');
       } else {
         style = style.replace('{{markerWidth}}', 'ramp([{{column}}], {{min}}, {{max}}, {{bins}})');
       }

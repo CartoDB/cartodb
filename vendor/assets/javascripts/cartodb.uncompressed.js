cc45d3f0272884602e66cb33869609db7762de6f
CloudNiner <andrew@cloudniner.co>
2016-05-12 14:19:50 -0400

    Update cartodb.js source auto-updated by grunt

--- vendor/assets/javascripts/cartodb.uncompressed.js
+++ vendor/assets/javascripts/cartodb.uncompressed.js
@@ -1,6 +1,6 @@
 // cartodb.js version: 3.15.9
 // uncompressed version: cartodb.uncompressed.js
-// sha: ea0290f51d64a2772c69af6ef67dfa7e9af7a9d0
+// sha: 5bee7c8c6c6b2a62ec29c598f9c4331a9cb831fb
 (function() {
   var define;  // Undefine define (require.js), see https://github.com/CartoDB/cartodb.js/issues/543
   var root = this;
@@ -28605,6 +28605,30 @@ cdb.geo.ui.ZoomInfo = cdb.core.View.extend({
     return this;
   }
 });
+// HELPERS
+
+cdb.geo.utils = cdb.geo.utils || {};
+
+cdb.geo.utils.cssStringToObject = function (str) {
+  if (!str) { return {}; }
+  var css = {};
+  var properties = str.split(';');
+  _.each(properties, function (p) {
+    var keyValue = p.trim().split(':');
+    if (keyValue.length === 2) {
+      css[keyValue[0].trim()] = keyValue[1].trim();
+    }
+  });
+  return css;
+}
+
+cdb.geo.utils.objectToCssString = function (obj) {
+  if (!(_.isObject(obj) && _.size(obj))) { return null; }
+
+  return _.map(obj, function (v, k) {
+    return k + ': ' + v;
+  }).join('; ');
+}
 
 // MODELS & COLLECTIONS
 
@@ -28724,18 +28748,6 @@ cdb.geo.ui.Legend = cdb.core.View.extend({
 
   className: "cartodb-legend",
 
-  events: {
-    "dragstart":            "_stopPropagation",
-    "mousedown":            "_stopPropagation",
-    "touchstart":           "_stopPropagation",
-    "MSPointerDown":        "_stopPropagation",
-    "dblclick":             "_stopPropagation",
-    "mousewheel":           "_stopPropagation",
-    "DOMMouseScroll":       "_stopPropagation",
-    "dbclick":              "_stopPropagation",
-    "click":                "_stopPropagation"
-  },
-
   initialize: function() {
     _.bindAll(this, "render", "show", "hide");
 
@@ -28747,10 +28759,6 @@ cdb.geo.ui.Legend = cdb.core.View.extend({
     this._setupItems();
 
     this._updateLegendType();
-  },
-
-  _stopPropagation: function(ev) {
-    ev.stopPropagation();
   },
 
   _setupModel: function() {
@@ -29442,26 +29450,42 @@ cdb.geo.ui.Legend.Color = cdb.geo.ui.Legend.Category.extend({ });
  * */
 cdb.geo.ui.StackedLegend = cdb.core.View.extend({
 
+  className: "cartodb-legend-stack",
+
   events: {
-    "dragstart":            "_stopPropagation",
-    "mousedown":            "_stopPropagation",
-    "touchstart":           "_stopPropagation",
-    "MSPointerDown":        "_stopPropagation",
-    "dblclick":             "_stopPropagation",
-    "mousewheel":           "_stopPropagation",
-    "DOMMouseScroll":       "_stopPropagation",
-    "dbclick":              "_stopPropagation",
-    "click":                "_stopPropagation"
+    'click .reset': 'resetStyle'
   },
 
-  className: "cartodb-legend-stack",
+  template: _.template('<div class="reset">&bigotimes;</div>'),
 
   initialize: function() {
     _.each(this.options.legends, this._setupBinding, this);
+
+    this.vis = this.options.vis;
+
+    this._setupModels();
   },
 
-  _stopPropagation: function(ev) {
-    ev.stopPropagation();
+  _setupModels: function () {
+    this.model = new cdb.core.Model({});
+
+    if (this.vis) {
+        var style = this.vis.get('legend_style');
+        var css = cdb.geo.utils.cssStringToObject(style);
+        if (style) {
+            this.model.set('style', css);
+        }
+        this.model.bind('change:style', this._setStyle, this);
+    }
+    this.model.bind('change', this.render, this);
+
+    this.add_related_model(this.model);
+  },
+
+  _setStyle: function () {
+    var style = this.model.get('style') || {};
+    this.vis.set("legend_style", cdb.geo.utils.objectToCssString(style));
+    this.vis.save()
   },
 
   getLegendByIndex: function(index) {
@@ -29481,9 +29505,26 @@ cdb.geo.ui.StackedLegend = cdb.core.View.extend({
     this.add_related_model(legend.model);
   },
 
+  resetStyle: function (event) {
+    if (this.vis) {
+      this.model.set('style', null);
+      if (event && event.stopPropagation) {
+        event.stopPropagation();
+      }
+    }
+  },
+
   render: function() {
+
+    if (this.vis) {
+      this.$el.html(this.template());
+    }
+
     this._renderItems();
     this._checkVisibility();
+
+    var style = this.model.get('style');
+    this._renderLegendStyles(style);
 
     return this;
   },
@@ -29513,6 +29554,30 @@ cdb.geo.ui.StackedLegend = cdb.core.View.extend({
         item.show();
       }
     }, this);
+  },
+
+  _renderLegendStyles: function (style) {
+    if (!style) {
+        this.$el.attr('style', '');
+        return;
+    }
+    var formattedStyle = getFormattedStyle(style);
+    var textStyle = _.pick(formattedStyle, ['color', 'font-size', 'font-family']);
+    this.$el.css(formattedStyle);
+    this.$el.find('li').css(textStyle);
+    this.$el.find('.legend-title').css(textStyle);
+
+    function getFormattedStyle(style) {
+        var formattedStyle = _.extend({}, style);
+        // font-size already has px applied in the style model
+        var needsPx = ['font-size', 'width', 'height', 'border-radius', 'border-width'];
+        _.forEach(formattedStyle, function (v, k) {
+            if (needsPx.indexOf(k) !== -1) {
+                formattedStyle[k] = parseInt(v, 10) + 'px';
+            }
+        });
+        return formattedStyle;
+    }
   },
 
   show: function() {
@@ -34562,12 +34627,13 @@ cdb.geo.LeafLetPlainLayerView = LeafLetPlainLayerView;
 
 (function() {
 
-if(typeof(L) == "undefined") 
+if(typeof(L) == "undefined")
   return;
 
 var LeafLetTiledLayerView = L.TileLayer.extend({
   initialize: function(layerModel, leafletMap) {
-    L.TileLayer.prototype.initialize.call(this, layerModel.get('urlTemplate'), {
+
+    var tmpLayer = {
       tms:          layerModel.get('tms'),
       attribution:  layerModel.get('attribution'),
       minZoom:      layerModel.get('minZoom'),
@@ -34575,7 +34641,17 @@ var LeafLetTiledLayerView = L.TileLayer.extend({
       subdomains:   layerModel.get('subdomains') || 'abc',
       errorTileUrl: layerModel.get('errorTileUrl'),
       opacity:      layerModel.get('opacity')
-    });
+    };
+
+    if ( layerModel.get('tileSize') ) {
+      tmpLayer.tileSize = layerModel.get('tileSize');
+    }
+
+    if ( layerModel.get('zoomOffset') ) {
+      tmpLayer.zoomOffset = layerModel.get('zoomOffset');
+    }
+
+    L.TileLayer.prototype.initialize.call(this, layerModel.get('urlTemplate'), tmpLayer);
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);
   }
 
@@ -34670,7 +34746,7 @@ if(typeof(L) == "undefined")
 var LeafLetWMSLayerView = L.TileLayer.WMS.extend({
   initialize: function(layerModel, leafletMap) {
 
-    L.TileLayer.WMS.prototype.initialize.call(this, layerModel.get('urlTemplate'), {
+    var tmpLayer = {
       attribution:  layerModel.get('attribution'),
       layers:       layerModel.get('layers'),
       format:       layerModel.get('format'),
@@ -34680,7 +34756,17 @@ var LeafLetWMSLayerView = L.TileLayer.WMS.extend({
       subdomains:   layerModel.get('subdomains') || 'abc',
       errorTileUrl: layerModel.get('errorTileUrl'),
       opacity:      layerModel.get('opacity')
-    });
+    };
+
+    if ( layerModel.get('tileSize') ) {
+      tmpLayer.tileSize = layerModel.get('tileSize');
+    }
+
+    if ( layerModel.get('zoomOffset') ) {
+      tmpLayer.zoomOffset = layerModel.get('zoomOffset');
+    }
+
+    L.TileLayer.WMS.prototype.initialize.call(this, layerModel.get('urlTemplate'), tmpLayer);
 
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);
   }

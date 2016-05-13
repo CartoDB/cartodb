e22ebe7ad9012a266e8a63c04a8aeb350ed48829
Pablo Alonso Garcia <pablo@cartodb.com>
2016-05-13 12:20:45 +0200

    Sort collection of layers after re-assigning new orders

--- src/geo/map/layers.js
+++ src/geo/map/layers.js
@@ -9,7 +9,7 @@ var Layers = Backbone.Collection.extend({
 
   model: LayerModelBase,
 
-  initialize: function() {
+  initialize: function () {
     this.comparator = function (m) {
       return parseInt(m.get('order'), 10);
     };
@@ -21,15 +21,15 @@ var Layers = Backbone.Collection.extend({
    * each time a layer is added or removed
    * the index should be recalculated
    */
-  _assignIndexes: function(model, col, options) {
+  _assignIndexes: function (model, col, options) {
     if (this.size() > 0) {
-
       // Assign an order of 0 to the first layer
       this.at(0).set({ order: 0 });
 
       if (this.size() > 1) {
         var layersByType = {};
-        for (var i = 1; i < this.size(); ++i) {
+        var i;
+        for (i = 1; i < this.size(); ++i) {
           var layer = this.at(i);
           var layerType = layer.get('type');
           layersByType[layerType] = layersByType[layerType] || [];
@@ -38,11 +38,11 @@ var Layers = Backbone.Collection.extend({
 
         var lastOrder = 0;
         var sortedTypes = [CARTODB_LAYER_TYPE, TORQUE_LAYER_TYPE, TILED_LAYER_TYPE];
-        for (var i = 0; i < sortedTypes.length; ++i) {
+        for (i = 0; i < sortedTypes.length; ++i) {
           var type = sortedTypes[i];
           var layers = layersByType[type] || [];
           for (var j = 0; j < layers.length; ++j) {
-            var layer = layers[j];
+            layer = layers[j];
             layer.set({
               order: ++lastOrder
             });
@@ -50,6 +50,8 @@ var Layers = Backbone.Collection.extend({
         }
       }
     }
+
+    this.sort();
   }
 });
 
--- test/spec/geo/map/layers.spec.js
+++ test/spec/geo/map/layers.spec.js
@@ -7,7 +7,7 @@ var CartoDBLayer = require('../../../../src/geo/map/cartodb-layer');
 describe('geo/map/layers', function() {
   var layers;
 
-  beforeEach(function() {
+  beforeEach(function () {
     layers = new Layers();
   });
 
@@ -24,20 +24,20 @@ describe('geo/map/layers', function() {
     layers.add(layer3);
 
     expect(layer3.isEqual(layer4)).toBeTruthy();
-  })
+  });
 
-  it("should compare TileLayers", function() {
+  it('should compare TileLayers', function () {
     var layer1 = new TileLayer({ urlTemplate: 'urlTemplate', name: 'layer1', other: 'something' });
     var layer2 = new TileLayer({ urlTemplate: 'urlTemplate', name: 'layer2', other: 'else' });
 
     expect(layer1.isEqual(layer2)).toBeFalsy();
 
-    layer2.set({ name: 'layer1' }, { silent: true});
+    layer2.set({ name: 'layer1' }, { silent: true });
 
     expect(layer1.isEqual(layer2)).toBeTruthy();
-  })
+  });
 
-  it("should re-assign order when new layers are added to the collection", function() {
+  it('should re-assign order when new layers are added to the collection', function () {
     var baseLayer = new TileLayer();
     var layer1 = new CartoDBLayer();
     var layer2 = new CartoDBLayer();
@@ -55,14 +55,16 @@ describe('geo/map/layers', function() {
     expect(baseLayer.get('order')).toEqual(0);
     expect(layer1.get('order')).toEqual(1);
     expect(layer2.get('order')).toEqual(2);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2 ]);
 
     // Sets the order to 1 and re-orders the rest of the layers
-    layers.add(layer3, { at: 1});
+    layers.add(layer3, { at: 1 });
 
     expect(baseLayer.get('order')).toEqual(0);
     expect(layer1.get('order')).toEqual(2);
     expect(layer2.get('order')).toEqual(3);
     expect(layer3.get('order')).toEqual(1);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3 ]);
 
     var torqueLayer = new TorqueLayer({});
 
@@ -74,6 +76,7 @@ describe('geo/map/layers', function() {
     expect(layer2.get('order')).toEqual(3);
     expect(layer3.get('order')).toEqual(1);
     expect(torqueLayer.get('order')).toEqual(4);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3, 4 ]);
 
     var tiledLayer = new TileLayer({});
 
@@ -86,6 +89,7 @@ describe('geo/map/layers', function() {
     expect(layer3.get('order')).toEqual(1);
     expect(torqueLayer.get('order')).toEqual(4);
     expect(tiledLayer.get('order')).toEqual(5);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3, 4, 5 ]);
 
     var layer4 = new CartoDBLayer({});
     layers.add(layer4);
@@ -97,9 +101,10 @@ describe('geo/map/layers', function() {
     expect(layer4.get('order')).toEqual(4);
     expect(torqueLayer.get('order')).toEqual(5);
     expect(tiledLayer.get('order')).toEqual(6);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3, 4, 5, 6 ]);
   });
 
-  it("should re-assign order when new layers are removed from the collection", function() {
+  it('should re-assign order when new layers are removed from the collection', function () {
     var baseLayer = new TileLayer();
     var layer1 = new CartoDBLayer();
     var layer2 = new CartoDBLayer();
@@ -118,6 +123,7 @@ describe('geo/map/layers', function() {
     expect(layer2.get('order')).toEqual(2);
     expect(torqueLayer.get('order')).toEqual(3);
     expect(labelsLayer.get('order')).toEqual(4);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3, 4 ]);
 
     layers.remove(layer1);
 
@@ -125,16 +131,19 @@ describe('geo/map/layers', function() {
     expect(layer2.get('order')).toEqual(1);
     expect(torqueLayer.get('order')).toEqual(2);
     expect(labelsLayer.get('order')).toEqual(3);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2, 3 ]);
 
     layers.remove(torqueLayer);
 
     expect(baseLayer.get('order')).toEqual(0);
     expect(layer2.get('order')).toEqual(1);
     expect(labelsLayer.get('order')).toEqual(2);
+    expect(layers.pluck('order')).toEqual([ 0, 1, 2 ]);
 
     layers.remove(labelsLayer);
 
     expect(baseLayer.get('order')).toEqual(0);
     expect(layer2.get('order')).toEqual(1);
+    expect(layers.pluck('order')).toEqual([ 0, 1 ]);
   });
 });

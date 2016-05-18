ed1f29159b3c1a5bf13e4c30dd95b999fa114d4c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-18 15:25:52 +0200

    Fix typo

--- lib/carto/file_system/sanitize.rb
+++ lib/carto/file_system/sanitize.rb
@@ -2,13 +2,13 @@ module Carto
   module FileSystem
     module Sanitize
       # Sanitize: NUL, slash, backslash, colon, asterisk, question, quote, less, greater, pipe
-      DISALLOWED_CHARACATERS = /[\x00\/\\:\*\?\"<>\|]/
+      DISALLOWED_CHARACTERS = /[\x00\/\\:\*\?\"<>\|]/
 
       def self.sanitize_identifier(identifier, replacement_character: '_')
-        if replacement_character =~ DISALLOWED_CHARACATERS
+        if replacement_character =~ DISALLOWED_CHARACTERS
           raise "Unsafe replacement character '#{replacement_character}'"
         else
-          identifier.gsub(DISALLOWED_CHARACATERS, replacement_character)
+          identifier.gsub(DISALLOWED_CHARACTERS, replacement_character)
         end
       end
     end

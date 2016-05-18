e09ff1d86f51e92a096d256892025af1669622d5
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-05-18 08:35:39 +0200

    Comment migration down #7315

--- db/migrate/20160516103200_drop_visualization_exports_user_id_null_constraint.rb
+++ db/migrate/20160516103200_drop_visualization_exports_user_id_null_constraint.rb
@@ -6,8 +6,11 @@ Sequel.migration do
   end
 
   down do
-    Rails::Sequel.connection.run(%{
-      ALTER TABLE visualization_exports alter column user_id set not null;
-    })
+    # This is the symmetric action, but it's not safe (there could be null `user_id`s)
+    # and it's not needed for `up` to be safe (`up` code is idempotent).
+    #
+    # Rails::Sequel.connection.run(%{
+    #   ALTER TABLE visualization_exports alter column user_id set not null;
+    # })
   end
 end

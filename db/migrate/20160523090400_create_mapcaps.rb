6d47242b4e0d9a13bd9fa98d0c310e5c4bd6d4c6
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-23 14:33:48 +0200

    change map for viz in mapcap migration

--- db/migrate/20160523090400_create_mapcaps.rb
+++ db/migrate/20160523090400_create_mapcaps.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   up do
     create_table :mapcaps do
       Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
-      Uuid :map_id, type: 'uuid', null: false
+      Uuid :vis_id, type: 'uuid', null: false
       Uuid :user_id, type: 'uuid', null: false
       String :channel, null: false
       String :export_vizjson, text: true, null: false

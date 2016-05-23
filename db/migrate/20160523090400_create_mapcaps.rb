be8683593386c732aa4b55343797c56ea1519a6b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-23 12:11:14 +0200

    migration for mapcaps

---
+++ db/migrate/20160523090400_create_mapcaps.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  up do
+    create_table :mapcaps do
+      Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+      Uuid :map_id, type: 'uuid', null: false
+      Uuid :user_id, type: 'uuid', null: false
+      String :channel, null: false
+      String :export_vizjson, text: true, null: false
+
+      DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+
+  down do
+    drop_table :mapcaps
+  end
+end

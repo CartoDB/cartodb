4a01ffd34f547280b333e6e51dc48e468344271c
María Checa <mariajecheca@gmail.com>
2016-05-23 11:26:32 +0200

    Mobile API keys frontend: after-review improvements

--- app/controllers/carto/api/mobile_app_presenter.rb
+++ app/controllers/carto/api/mobile_app_presenter.rb
@@ -28,31 +28,31 @@ module Carto
             "android": {
               text: "Android",
               available: platform_available?('android', true),
-              selected: platform_selected?('android'),
+              selected: platform_enabled?('android'),
               legend: "Use package from AndroidManifest.xml. E.g: com.example.mycartoapp."
             },
             "ios": {
               text: "iOS",
               available: platform_available?('ios', true),
-              selected: platform_selected?('ios'),
+              selected: platform_enabled?('ios'),
               legend: "Use Bundle identifier. You can find it in the project properties. E.g: com.example.mycartoapp."
             },
             "xamarin-android": {
               text: "Xamarin Android",
               available: platform_available?('xamarin-android', @current_user.mobile_xamarin),
-              selected: platform_selected?('xamarin-android'),
+              selected: platform_enabled?('xamarin-android'),
               legend: "Use package from AndroidManifest.xml. E.g: com.example.mycartoapp."
             },
             "xamarin-ios": {
               text: "Xamarin iOS",
               available: platform_available?('xamarin-ios', @current_user.mobile_xamarin),
-              selected: platform_selected?('xamarin-ios'),
+              selected: platform_enabled?('xamarin-ios'),
               legend: "Use Bundle identifier. You can find it in the project properties. E.g: com.example.mycartoapp."
             },
             "windows-phone": {
               text: "Windows Phone",
               available: platform_available?('windows-phone', @current_user.mobile_xamarin),
-              selected: platform_selected?('windows-phone'),
+              selected: platform_enabled?('windows-phone'),
               legend: "Use the Package name from Package.appmanifest. E.g: c882d38a-5c09-4994-87f0-89875cdee539."
             }
           }
@@ -63,17 +63,17 @@ module Carto
             "open": {
               text: "Limits based on your CartoDB plan. <a href='#'>Learn more</a>.",
               available: app_type_available?('open', @current_user.open_apps_enabled?),
-              selected: app_type_selected?('open')
+              selected: app_type_enabled?('open')
             },
             "dev": {
               text: "Limited to 5 users, unlimited feature-wise. <a href='#'>Learn more</a>.",
               available: app_type_available?('dev', true),
-              selected: app_type_selected?('dev')
+              selected: app_type_enabled?('dev')
             },
             "private": {
               text: "Only for enterprise. <a href='#'>Learn more</a>.",
               available: app_type_available?('private', @current_user.private_apps_enabled?),
-              selected: app_type_selected?('private')
+              selected: app_type_enabled?('private')
             }
           }
         end
@@ -83,20 +83,20 @@ module Carto
 
       private
 
-      def platform_available?(platform, selectable)
-        ((@mobile_app.persisted? && @mobile_app.platform != platform) || selectable == false) ? false : true
+      def platform_available?(platform, enabled)
+        !platform_enabled?(platform) || enabled == false
       end
 
-      def platform_selected?(platform)
-        (@mobile_app.persisted? && @mobile_app.platform == platform) ? true : false
+      def platform_enabled?(platform)
+        @mobile_app.persisted? && @mobile_app.platform == platform
       end
 
-      def app_type_available?(app_type, selectable)
-        ((@mobile_app.persisted? && @mobile_app.app_type != app_type) || selectable == false) ? false : true
+      def app_type_available?(app_type, enabled)
+        !app_type_enabled?(app_type) || enabled == false
       end
 
-      def app_type_selected?(app_type)
-        (@mobile_app.persisted? && @mobile_app.app_type == app_type) ? true : false
+      def app_type_enabled?(app_type)
+        @mobile_app.persisted? && @mobile_app.app_type == app_type
       end
     end
   end
--- app/helpers/mobile_apps_helper.rb
+++ app/helpers/mobile_apps_helper.rb
@@ -10,4 +10,24 @@ module MobileAppsHelper
       ''
     end
   end
+
+  def progres_bar_width
+    if @mobile_app.app_type == 'dev'
+      (@mobile_app.monthly_users*100) / @max_dev_users
+    elsif @mobile_app.app_type == 'open'
+      (@mobile_app.monthly_users*100) / progress_bar_max_users
+    elsif @mobile_app.app_type == 'private'
+      (@mobile_app.monthly_users*100) / progress_bar_max_users
+    end
+  end
+
+  def progress_bar_max_users
+    if @mobile_app.app_type == 'dev'
+      @max_dev_users
+    elsif @mobile_app.app_type == 'open'
+      current_user.mobile_max_open_users
+    elsif @mobile_app.app_type == 'private'
+      current_user.mobile_max_private_users
+    end
+  end
 end

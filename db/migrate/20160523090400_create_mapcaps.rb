e280a5f4b81b148e718b9d4002ef49a64cca9482
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-24 14:11:02 +0200

    remove channel from migration

--- db/migrate/20160523090400_create_mapcaps.rb
+++ db/migrate/20160523090400_create_mapcaps.rb
@@ -3,7 +3,6 @@ Sequel.migration do
     create_table :mapcaps do
       Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
       Uuid :vis_id, type: 'uuid', null: false
-      String :channel, null: false
       String :export_vizjson, text: true, null: false
 
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP

8aa09a57d4a574e4bfbc7e49d8147b3c43f4a39f
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-05-24 14:10:17 +0200

    Add mapcap load to embeds

--- app/controllers/carto/editor/public/embeds_controller.rb
+++ app/controllers/carto/editor/public/embeds_controller.rb
@@ -8,7 +8,7 @@ module Carto
 
         ssl_required :show, :show_protected
 
-        before_filter :load_visualization, only: [:show, :show_protected]
+        before_filter :load_visualization, :load_mapcap_vizjson, only: [:show, :show_protected]
         before_filter :ensure_viewable, only: [:show]
 
         skip_before_filter :editor_users_only # This is supposed to be public even in beta
@@ -17,7 +17,7 @@ module Carto
 
         def show
           @visualization_data = Carto::Api::VisualizationPresenter.new(@visualization, current_viewer, self).to_poro
-          @vizjson = generate_named_map_vizjson3(@visualization, params)
+          @vizjson = generate_named_map_vizjson3(@mapcap ? @mapcap.regenerate_visualization : @visualization, params)
 
           render 'show'
         end
@@ -34,6 +34,10 @@ module Carto
         def load_visualization
           @visualization = load_visualization_from_id_or_name(params[:visualization_id])
           render_404 unless @visualization
+        end
+
+        def load_mapcap_vizjson
+          @mapcap = Carto::Mapcap.where(vis_id: @visualization.id).first
         end
 
         def ensure_viewable

7a1fad45e1112ee5a97060fb2e51ce4eafd03b2c
Pablo Alonso Garcia <pablo@cartodb.com>
2016-05-31 18:52:43 +0200

    Reverted change in vendor/assets/javascripts/cartodb.uncompressed.js

--- vendor/assets/javascripts/cartodb.uncompressed.js
+++ vendor/assets/javascripts/cartodb.uncompressed.js
@@ -1,6 +1,6 @@
 // cartodb.js version: 3.15.9
 // uncompressed version: cartodb.uncompressed.js
-// sha: 2702898e1990e09c12435c3496092f7946821b1e
+// sha: ea0290f51d64a2772c69af6ef67dfa7e9af7a9d0
 (function() {
   var define;  // Undefine define (require.js), see https://github.com/CartoDB/cartodb.js/issues/543
   var root = this;
@@ -26885,8 +26885,8 @@ cdb.geo.geocoder.YAHOO = {
           }
 
           for(var i in res) {
-            var r = res[i],
-            position;
+            var r = res[i]
+              , position;
 
             position = {
               lat: r.latitude,
@@ -26904,55 +26904,8 @@ cdb.geo.geocoder.YAHOO = {
         callback(coordinates);
       });
   }
-};
+}
 
-cdb.geo.geocoder.MAPZEN = {
-  keys:{
-    app_id:  "search-DH1Lkhw"
-  },
-
-  geocode: function(address, callback){
-    address = address.toLowerCase()
-      .replace(/é/g,'e')
-      .replace(/á/g,'a')
-      .replace(/í/g,'i')
-      .replace(/ó/g,'o')
-      .replace(/ú/g,'u');
-
-    var protocol = '';
-    if(location.protocol.indexOf('http') === -1) {
-      protocol = 'http:';
-    }
-
-    $.getJSON(protocol + '//search.mapzen.com/v1/search?text=' + encodeURIComponent(address) + '&api_key=' + this.keys.app_id, function(data) {
-  
-    var coordinates = [];
-    if (data && data.features && data.features.length > 0) {
-      var res = data.features;
-      for (var i in res){
-        var r = res[i],
-        position;
-        position = {
-          lat: r.geometry.coordinates[1],
-          lon: r.geometry.coordinates[0]
-        };
-        if(r.properties.layer){
-          position.type = r.properties.layer;
-        }  
-        
-        if(r.properties.label){
-          position.title = r.properties.label;
-        } 
-
-        coordinates.push(position);
-      }
-    }
-    if (callback) {
-      callback.call(this, coordinates);
-    }
-  });
-  }
-};
 
 
 cdb.geo.geocoder.NOKIA = {
@@ -26983,8 +26936,8 @@ cdb.geo.geocoder.NOKIA = {
           var res = data.results.items;
 
           for(var i in res) {
-            var r = res[i],
-            position;
+            var r = res[i]
+              , position;
 
             position = {
               lat: r.position[0],
@@ -26997,7 +26950,7 @@ cdb.geo.geocoder.NOKIA = {
                 south: r.bbox[1],
                 east: r.bbox[2],
                 west: r.bbox[0]
-              };
+              }
             }
             if (r.category) {
               position.type = r.category.id;
@@ -27014,7 +26967,7 @@ cdb.geo.geocoder.NOKIA = {
         }
       });
   }
-};
+}
 
 
 /**
@@ -31035,14 +30988,6 @@ cdb.geo.ui.Search = cdb.core.View.extend({
   _ZOOM_BY_CATEGORY: {
     'building': 18,
     'postal-area': 15,
-    'venue':18,
-    'region':8,
-    'address':18,
-    'country':5,
-    'county':8,
-    'locality':12,
-    'localadmin':11,
-    'neighbourhood':15,
     'default': 12
   },
 
@@ -31114,7 +31059,7 @@ cdb.geo.ui.Search = cdb.core.View.extend({
     this._showLoader();
     // Remove previous pin
     this._destroySearchPin();
-    cdb.geo.geocoder.MAPZEN.geocode(address, function(places) {
+    cdb.geo.geocoder.NOKIA.geocode(address, function(places) {
       self._onResult(places);
       // Hide loader
       self._hideLoader();
@@ -31187,7 +31132,7 @@ cdb.geo.ui.Search = cdb.core.View.extend({
   _destroySearchPin: function() {
     this._unbindEvents();
     this._destroyPin();
-    this._destroyInfowindow();
+    this._destroyInfowindow()
   },
 
   _createInfowindow: function(position, address) {
@@ -34617,13 +34562,12 @@ cdb.geo.LeafLetPlainLayerView = LeafLetPlainLayerView;
 
 (function() {
 
-if(typeof(L) == "undefined")
+if(typeof(L) == "undefined") 
   return;
 
 var LeafLetTiledLayerView = L.TileLayer.extend({
   initialize: function(layerModel, leafletMap) {
-
-    var tmpLayer = {
+    L.TileLayer.prototype.initialize.call(this, layerModel.get('urlTemplate'), {
       tms:          layerModel.get('tms'),
       attribution:  layerModel.get('attribution'),
       minZoom:      layerModel.get('minZoom'),
@@ -34631,17 +34575,7 @@ var LeafLetTiledLayerView = L.TileLayer.extend({
       subdomains:   layerModel.get('subdomains') || 'abc',
       errorTileUrl: layerModel.get('errorTileUrl'),
       opacity:      layerModel.get('opacity')
-    };
-
-    if ( layerModel.get('tileSize') ) {
-      tmpLayer.tileSize = layerModel.get('tileSize');
-    }
-
-    if ( layerModel.get('zoomOffset') ) {
-      tmpLayer.zoomOffset = layerModel.get('zoomOffset');
-    }
-
-    L.TileLayer.prototype.initialize.call(this, layerModel.get('urlTemplate'), tmpLayer);
+    });
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);
   }
 
@@ -34736,7 +34670,7 @@ if(typeof(L) == "undefined")
 var LeafLetWMSLayerView = L.TileLayer.WMS.extend({
   initialize: function(layerModel, leafletMap) {
 
-    var tmpLayer = {
+    L.TileLayer.WMS.prototype.initialize.call(this, layerModel.get('urlTemplate'), {
       attribution:  layerModel.get('attribution'),
       layers:       layerModel.get('layers'),
       format:       layerModel.get('format'),
@@ -34746,17 +34680,7 @@ var LeafLetWMSLayerView = L.TileLayer.WMS.extend({
       subdomains:   layerModel.get('subdomains') || 'abc',
       errorTileUrl: layerModel.get('errorTileUrl'),
       opacity:      layerModel.get('opacity')
-    };
-
-    if ( layerModel.get('tileSize') ) {
-      tmpLayer.tileSize = layerModel.get('tileSize');
-    }
-
-    if ( layerModel.get('zoomOffset') ) {
-      tmpLayer.zoomOffset = layerModel.get('zoomOffset');
-    }
-
-    L.TileLayer.WMS.prototype.initialize.call(this, layerModel.get('urlTemplate'), tmpLayer);
+    });
 
     cdb.geo.LeafLetLayerView.call(this, layerModel, this, leafletMap);
   }

fc8d84383cb7f4ca27bd24aa18d795d4c093fade
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-03 15:28:36 +0200

    added tests for template auth

---
+++ spec/lib/carto/named_maps/template_spec.rb
@@ -0,0 +1,54 @@
+# encoding utf-8
+
+require_relative '../../../spec_helper_min.rb'
+require_relative '../../../../lib/carto/named_maps/template'
+
+module Carto
+  module NamedMaps
+    describe Template do
+      include Carto::Factories::Visualizations
+
+      before(:all) do
+        bypass_named_maps
+        @user = FactoryGirl.create(:carto_user, private_tables_enabled: true)
+
+        _, _, _, @visualization = create_full_visualization(@user)
+      end
+
+      describe '#auth' do
+        it 'should generate open auth for public, link and private visualizations' do
+          @visualization.privacy = Carto::Visualization::PRIVACY_PUBLIC
+
+          template = Carto::NamedMaps::Template.new(@visualization)
+          template.to_hash[:auth][:method].should eq Carto::NamedMaps::Template::AUTH_TYPE_OPEN
+
+          @visualization.privacy = Carto::Visualization::PRIVACY_LINK
+
+          template = Carto::NamedMaps::Template.new(@visualization)
+          template.to_hash[:auth][:method].should eq Carto::NamedMaps::Template::AUTH_TYPE_OPEN
+
+          @visualization.privacy = Carto::Visualization::PRIVACY_PRIVATE
+
+          template = Carto::NamedMaps::Template.new(@visualization)
+          template.to_hash[:auth][:method].should eq Carto::NamedMaps::Template::AUTH_TYPE_OPEN
+        end
+
+        it 'should use signed auth for password protected visualizations' do
+          @visualization.privacy = Carto::Visualization::PRIVACY_PROTECTED
+
+          template = Carto::NamedMaps::Template.new(@visualization)
+          template.to_hash[:auth][:method].should eq Carto::NamedMaps::Template::AUTH_TYPE_SIGNED
+        end
+
+        it 'should use signed auth for organization private visualizations' do
+          @visualization.privacy = Carto::Visualization::PRIVACY_PRIVATE
+
+          @visualization.stubs(:organization?).returns(true)
+
+          template = Carto::NamedMaps::Template.new(@visualization)
+          template.to_hash[:auth][:method].should eq Carto::NamedMaps::Template::AUTH_TYPE_SIGNED
+        end
+      end
+    end
+  end
+end

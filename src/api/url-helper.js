4b710215b6f33547c6c50efe8c0a0e5ec53ee7e4
francisco <francisco@MacBook-Pro-de-cartodb.local>
2016-06-06 17:26:38 +0200

    only encodes once

--- src/api/url-helper.js
+++ src/api/url-helper.js
@@ -6,7 +6,7 @@ module.exports = {
     var thisURL = new URI(this.getLocalURL());
     thisURL.removeQuery('state');
     if (!_.isEmpty(state)) {
-      var statesString = encodeURIComponent(JSON.stringify(state));
+      var statesString = JSON.stringify(state);
       thisURL.setQuery('state', statesString);
     }
     return thisURL.toString();

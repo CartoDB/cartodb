f10ef06c8f72653f5a781912f2d3cea8e1c24b1b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-09 14:46:15 +0200

    fix stubs

--- spec/helpers/named_maps_helper.rb
+++ spec/helpers/named_maps_helper.rb
@@ -1,6 +1,6 @@
 module NamedMapsHelper
   def bypass_named_maps
-    CartoDB::Visualization::Member.any_instance.stubs(:has_named_map?).returns(false)
+    CartoDB::Visualization::Member.any_instance.stubs(save_named_map: true, create_named_map: true)
     Carto::NamedMaps::Api.any_instance.stubs(show: nil, create: true, update: true, destroy: true)
   end
 end

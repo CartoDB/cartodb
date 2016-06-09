848bd6738b0ace9c6ec8d075fb7477a29986c315
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-09 14:49:52 +0200

    simplify stubs for nm

--- spec/helpers/named_maps_helper.rb
+++ spec/helpers/named_maps_helper.rb
@@ -1,6 +1,5 @@
 module NamedMapsHelper
   def bypass_named_maps
-    CartoDB::Visualization::Member.any_instance.stubs(save_named_map: true, create_named_map: true)
     Carto::NamedMaps::Api.any_instance.stubs(show: nil, create: true, update: true, destroy: true)
   end
 end
--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -193,7 +193,6 @@ describe SessionsController do
   private
 
   def bypass_named_maps
-    CartoDB::Visualization::Member.any_instance.stubs(save_named_map: true, create_named_map: true)
     Carto::NamedMaps::Api.any_instance.stubs(show: nil, create: true, update: true, destroy: true)
   end
 end

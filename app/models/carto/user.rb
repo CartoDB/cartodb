50205346f60fd3e9b75fa63010037e1d331ec48c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-13 14:42:44 +0200

    fix auth_token generation for Carto::User

--- app/models/carto/user.rb
+++ app/models/carto/user.rb
@@ -453,9 +453,14 @@ class Carto::User < ActiveRecord::Base
     tokens = [get_auth_token]
 
     tokens << organization.get_auth_token if has_organization?
+
+    tokens
   end
 
   def get_auth_token
+    # Circumvent DEFAULT_SELECT, didn't add auth_token there for sercurity (presenters, etc)
+    auth_token = Carto::User.select(auth_token).find(id).auth_token
+
     auth_token.present? ? auth_token : generate_auth_token
   end
 

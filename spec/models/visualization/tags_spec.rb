7f522107baf20901d59711b633a02ee2864d0c30
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-06-15 16:40:33 +0200

    Revert style change to avoid future conflict

--- spec/models/visualization/tags_spec.rb
+++ spec/models/visualization/tags_spec.rb
@@ -17,7 +17,7 @@ describe Visualization::Tags do
     @db = Rails::Sequel.connection
     Visualization.repository = DataRepository::Backend::Sequel.new(@db, :visualizations)
 
-    CartoDB::NamedMapsWrapper::NamedMaps.any_instance.stubs(get: nil, create: true, update: true)
+    CartoDB::NamedMapsWrapper::NamedMaps.any_instance.stubs(:get => nil, :create => true, :update => true)
 
     # For relator->permission
     user_id = UUIDTools::UUID.timestamp_create.to_s

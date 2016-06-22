865c80c2512b88ae43ece8f369d1d7166b9a101b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-22 12:16:30 +0200

    fix mapcaps controller

--- app/controllers/carto/api/mapcaps_controller.rb
+++ app/controllers/carto/api/mapcaps_controller.rb
@@ -14,23 +14,31 @@ module Carto
                     :owners_only
       before_filter :load_mapcap, only: [:show, :destroy]
 
-      after_filter :render_mapcap
-
       rescue_from StandardError, with: :rescue_from_standard_error
       rescue_from Carto::LoadError,
                   Carto::UnauthorizedError,
                   Carto::UnprocesableEntityError,
                   Carto::UnauthorizedError, with: :rescue_from_carto_error
 
+      def index
+        render_jsonp(@visualization.mapcaps.map { |mapcap| Carto::Api::MapcapPresenter.new(mapcap).to_poro }, 201)
+      end
+
       def create
         @mapcap = Carto::Mapcap.create!(visualization_id: @visualization.id)
+
+        render_jsonp(Carto::Api::MapcapPresenter.new(@mapcap).to_poro, 201)
+      end
+
+      def show
+        render_jsonp(Carto::Api::MapcapPresenter.new(@mapcap).to_poro)
       end
 
       def destroy
         @mapcap.destroy
-      end
 
-      def show; end
+        render_jsonp(Carto::Api::MapcapPresenter.new(@mapcap).to_poro)
+      end
 
       private
 
@@ -43,7 +51,7 @@ module Carto
       end
 
       def owners_only
-        raise Carto::UnauthorizedError.new unless @visualization.owner?(current_user)
+        raise Carto::UnauthorizedError.new unless @visualization.is_writable_by_user(current_user)
       end
 
       def load_mapcap
@@ -52,10 +60,6 @@ module Carto
         @mapcap = Carto::Mapcap.find(mapcap_id)
       rescue ActiveRecord::RecordNotFound
         raise Carto::LoadError.new("Mapcap not found: #{mapcap_id}")
-      end
-
-      def render_mapcap
-        render_jsonp(Carto::MapcapPresenter.new(@mapcap).to_poro, 200)
       end
     end
   end

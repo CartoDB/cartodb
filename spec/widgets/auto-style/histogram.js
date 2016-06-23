b4042f341c7760acc7a78acc5a1f6298bd554c2e
Francisco Dans <francisco@cartodb.com>
2016-06-23 18:48:16 +0200

    adds tets

--- spec/widgets/auto-style/histogram.js
+++ spec/widgets/auto-style/histogram.js
@@ -8,7 +8,7 @@ describe('src/widgets/auto-style/histogram', function () {
     });
 
     this.dataview.getDistributionType = jasmine.createSpy('disttype').and.returnValue('F');
-    this.layer = this.dataview.layer = jasmine.createSpyObj('layer', ['getGeometryType']);
+    this.layer = this.dataview.layer = jasmine.createSpyObj('layer', ['getGeometryType', 'get']);
     this.histogramAutoStyler = new HistogramAutoStyler(this.dataview);
   });
 
@@ -21,6 +21,12 @@ describe('src/widgets/auto-style/histogram', function () {
     it('should generate the right styles when layer has points', function () {
       this.layer.getGeometryType.and.returnValue('marker');
       expect(this.histogramAutoStyler.getStyle().replace(/\s/g, '').indexOf('{{')).toBeLessThan(0);
+    });
+
+    it('should preserve the previous marker widths if they were formed with turbocarto', function () {
+      this.layer.getGeometryType.and.returnValue('marker');
+      this.layer.get.and.returnValue('#layer {  marker-line-width: 0.5;  marker-line-color: #1e1e1e;  marker-line-opacity: 1;  marker-width: 3;  marker-fill: #b7f2de;  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
+      expect(this.histogramAutoStyler.getStyle().indexOf('ramp([something], cartocolor(')).not.toBeLessThan(0);
     });
 
     it('should generate the right styles when layer has lines', function () {

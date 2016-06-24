8610c84fefda99977d71f5e0f05e852fd1ab0e3e
Javier Torres <jtorres@cartodb.com>
2016-06-24 17:21:35 +0200

    Use a boolean column for builder_enabled

--- db/migrate/20160624101145_add_builder_enabled_to_users.rb
+++ db/migrate/20160624101145_add_builder_enabled_to_users.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table :users do
-      add_column :builder_enabled, :text
+      add_column :builder_enabled, :boolean, null: true, default: true
     end
   end
 end

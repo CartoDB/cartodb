c04c47f77d9e00696e9d562be39d211fd91fecd0
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-06-27 14:01:27 +0200

    WIP

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -181,6 +181,38 @@ describe CartoDB::Permission do
       user2.destroy
     end
 
+    it 'fails granting write permission for viewer users' do
+        user2_mock = mock
+        user2_mock.stubs(:id).returns(UUIDTools::UUID.timestamp_create.to_s)
+        user2_mock.stubs(:username).returns('user2')
+        user2_mock.stubs(:organization).returns(nil)
+        user2_mock.stubs(:viewer).returns(true)
+
+        # Don't check/handle DB permissions
+        Permission.any_instance.stubs(:revoke_previous_permissions).returns(nil)
+        Permission.any_instance.stubs(:grant_db_permission).returns(nil)
+        # No need to check for real DB visualizations
+        prepare_vis_mock_in_permission
+
+        permission = Permission.new(
+          owner_id:       @user.id,
+          owner_username: @user.username
+        ).save
+        permission.acl = [
+          {
+            type: Permission::TYPE_USER,
+            entity: {
+              id: user2_mock.id,
+              username: user2_mock.username
+            },
+            access: Permission::ACCESS_READWRITE
+          }
+        ]
+
+        permission.save.should eq false
+        permission.errors[:user].should eq "Viewers can't get write permission"
+    end
+
     it 'supports groups' do
       entity_id = UUIDTools::UUID.timestamp_create.to_s
       entity_type = Permission::ENTITY_TYPE_VISUALIZATION

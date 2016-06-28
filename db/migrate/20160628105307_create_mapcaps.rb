267e4e2de3d4a1f10de6a3e32265c224f52c5725
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-28 19:37:22 +0200

    add foreign key to mapcaps

--- db/migrate/20160628105307_create_mapcaps.rb
+++ db/migrate/20160628105307_create_mapcaps.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   up do
     create_table :mapcaps do
-      foreign_key :visualization_id, :visualization, type: 'uuid', null: false
+      foreign_key :visualization_id, :visualizations, type: 'uuid', null: false
 
       Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
 

1e63a8ad0eaa122dd2eb017a7f924783d6dd29a7
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-06-28 12:56:44 +0200

    use json type in mapcap migrations

--- db/migrate/20160628105307_create_mapcaps.rb
+++ db/migrate/20160628105307_create_mapcaps.rb
@@ -4,8 +4,8 @@ Sequel.migration do
       Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
       Uuid :visualization_id, type: 'uuid', null: false
 
-      String :export_json, text: true, null: false
-      String :ids_json, text: true, null: false
+      String :export_json, null: false, type: 'json'
+      String :ids_json, null: false, type: 'json'
 
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
     end

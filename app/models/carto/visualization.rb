1e9a044fe2a4c60d8152db380b3b0fd2f557677c
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-07-01 09:13:29 +0200

    Deny write permission for viewers #7713

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -402,7 +402,7 @@ class Carto::Visualization < ActiveRecord::Base
   end
 
   def has_write_permission?(user)
-    user && (owner?(user) || (permission && permission.user_has_write_permission?(user)))
+    user && !user.viewer? && (owner?(user) || (permission && permission.user_has_write_permission?(user)))
   end
 
   def owner?(user)

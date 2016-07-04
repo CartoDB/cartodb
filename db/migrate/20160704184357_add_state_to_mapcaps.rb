fb37e5b34f95f20f5e3e1a52757c9b662072270a
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-04 20:56:34 +0200

    rename state to state_json

--- db/migrate/20160704184357_add_state_to_mapcaps.rb
+++ db/migrate/20160704184357_add_state_to_mapcaps.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
     alter_table :mapcaps do
-      add_column :state, :string, type: 'json', null: false, default: '{}'
+      add_column :state_json, :string, type: 'json', null: false, default: '{}'
     end
   end
 
   down do
     alter_table :mapcaps do
-      drop_column :state
+      drop_column :state_json
     end
   end
 end

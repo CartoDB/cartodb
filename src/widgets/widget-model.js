198b30d4d55551ad7d193c0048a9f38ad9b3974c
Francisco Dans <francisco@cartodb.com>
2016-07-05 20:39:38 +0200

    performs epsilon operation instead of rounding

--- src/widgets/widget-model.js
+++ src/widgets/widget-model.js
@@ -53,16 +53,11 @@ module.exports = cdb.core.Model.extend({
     var state = {};
     for (var key in this.defaultState) {
       var attribute = this.get(key);
-      if (_.isNumber(attribute)) {
-        attribute = Math.round(attribute);
-      }
       var defaultValue = this.defaultState[key];
       if (typeof defaultValue === 'function') {
         defaultValue = defaultValue.call(this);
       }
-      if (_.isNumber(defaultValue)) {
-        defaultValue = Math.round(defaultValue);
-      }
+      if (_.isNumber(attribute) && _.isNumber(defaultValue) && Math.abs(attribute - defaultValue) / attribute < 0.10) continue;
       if (typeof defaultValue !== 'undefined' && typeof attribute !== 'undefined' && !_.isEqual(attribute, defaultValue)) {
         state[key] = attribute;
       }

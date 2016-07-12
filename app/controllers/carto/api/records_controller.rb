412fbb39cfccbd60921d1c4f7f3df8af237d8b4b
Javier Torres <javier@javiertorres.eu>
2016-07-12 17:55:05 +0200

    Rewrite table/record/column controller to use Carto models

--- app/controllers/carto/api/records_controller.rb
+++ app/controllers/carto/api/records_controller.rb
@@ -9,34 +9,25 @@ module Carto
       ssl_required :show
 
       before_filter :set_start_time
+      before_filter :load_user_table, only: [:show]
       before_filter :read_privileges?, only: [:show]
 
       # This endpoint is not used by the editor but by users. Do not remove
       def show
-        render_jsonp(get_record(params[:id]))
-      rescue => e
-        render_jsonp({ :errors => ["Record #{params[:id]} not found"] }, 404)
+        render_jsonp(@user_table.service.record(params[:id]))
+      rescue
+        render_jsonp({ errors: ["Record #{params[:id]} not found"] }, 404)
       end
 
       protected
 
-      def get_record(id)
-        table.record(id)
-      end
-
-      def table
-        if @table.nil?
-          @table = Carto::Helpers::TableLocator.new.get_by_id_or_name(params[:table_id], current_user)
-          raise RecordNotFound if @table.nil?
-        end
-        @table
+      def load_user_table
+        @user_table = Carto::Helpers::TableLocator.new.get_by_id_or_name(params[:table_id], current_user)
+        raise RecordNotFound unless @user_table
       end
 
       def read_privileges?
-        head(401) unless current_user && table.table_visualization.has_permission?(
-          current_user,
-          CartoDB::Permission::ACCESS_READONLY
-        )
+        head(401) unless current_user && @user_table.visualization.is_viewable_by_user?(current_user)
       end
     end
   end

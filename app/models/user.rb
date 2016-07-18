17b6d2ed2bac73a8235508c55999ee90bbd9b7c6
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-18 10:44:39 +0200

    Hound refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1515,16 +1515,33 @@ class User < Sequel::Model
   end
 
   def copy_account_features(to)
-    to.set_fields(self, [
-      :private_tables_enabled, :sync_tables_enabled, :user_timeout,
-      :database_timeout, :geocoding_quota, :map_view_quota, :table_quota, :database_host,
-      :period_end_date, :map_view_block_price, :geocoding_block_price, :account_type,
-      :twitter_datasource_enabled, :soft_twitter_datasource_limit, :twitter_datasource_quota,
-      :twitter_datasource_block_price, :twitter_datasource_block_size, :here_isolines_quota,
-      :here_isolines_block_price, :soft_here_isolines_limit, :obs_snapshot_quota,
-      :obs_snapshot_block_price, :soft_obs_snapshot_limit, :obs_general_quota,
-      :obs_general_block_price, :soft_obs_general_limit
-    ])
+    to.set_fields(self, [:private_tables_enabled,
+                         :sync_tables_enabled,
+                         :user_timeout,
+                         :database_timeout,
+                         :geocoding_quota,
+                         :map_view_quota,
+                         :table_quota,
+                         :database_host,
+                         :period_end_date,
+                         :map_view_block_price,
+                         :geocoding_block_price,
+                         :account_type,
+                         :twitter_datasource_enabled,
+                         :soft_twitter_datasource_limit,
+                         :twitter_datasource_quota,
+                         :twitter_datasource_block_price,
+                         :twitter_datasource_block_size,
+                         :here_isolines_quota,
+                         :here_isolines_block_price,
+                         :soft_here_isolines_limit,
+                         :obs_snapshot_quota,
+                         :obs_snapshot_block_price,
+                         :soft_obs_snapshot_limit,
+                         :obs_general_quota,
+                         :obs_general_block_price,
+                         :soft_obs_general_limit])
+
     to.invite_token = ::User.make_token
   end
 

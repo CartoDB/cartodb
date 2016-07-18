c1a34f35c8f4baf1196c72b8c16c3d59253f8f34
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-18 19:18:22 +0200

    add CreatedDataset event

--- lib/carto/tracking/events.rb
+++ lib/carto/tracking/events.rb
@@ -6,13 +6,13 @@ module Carto
       class MapExported
         EVENT_NAME = 'Exported map'.freeze
 
-        def initialize(user_id, visualization_id)
-          @user = Carto::User.find(user_id)
-          @visualization = Carto::Visualization.find(visualization_id)
+        def initialize(user, visualization)
+          @user = user
+          @visualization = visualization
         end
 
         def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(@user, properties)
+          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
         end
 
         private
@@ -21,6 +21,30 @@ module Carto
           { privacy: @visualization.privacy,
             type: @visualization.type,
             id: @visualization.id }
+        end
+      end
+
+      class CreatedDataset
+        EVENT_NAME = 'Created dataset'.freeze
+
+        def initialize(user, table)
+          @user = user
+          @table = table
+        end
+
+        def report
+          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
+        end
+
+        private
+
+        def properties
+          {
+            privacy: @table.table_visualization.privacy,
+            type: @table.table_visualization.type,
+            vis_id: @table.table_visualization.id,
+            origin: 'blank'
+          }
         end
       end
     end

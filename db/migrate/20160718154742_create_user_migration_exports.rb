2898f26936f72c52542ed66d0b5c830731bd5b1c
Javier Torres <jtorres@carto.com>
2016-07-18 18:04:23 +0200

    Migration to create user_mover_exports

---
+++ db/migrate/20160718154742_create_user_migration_exports.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  change do
+    create_table :user_migration_exports do
+      Uuid   :id,             primary_key: true, default: 'uuid_generate_v4()'.lit
+      String :state,          null: false
+      String :exported_file,  null: true, text: true
+      DateTime :created_at,   default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+
+      foreign_key :user_id,         :users,         type: :uuid, null: true, index: true
+      foreign_key :organization_id, :organizations, type: :uuid, null: true, index: true
+      foreign_key :log_id,          :logs,          type: :uuid, null: false
+    end
+  end
+end

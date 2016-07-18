88b8b4c73af50dce05c30d9de387e1f28f6ad55a
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-18 17:23:45 +0200

    refactor

--- lib/resque/event_delivery_jobs.rb
+++ lib/resque/event_delivery_jobs.rb
@@ -1,19 +1,16 @@
-require_relative  '../cartodb/segment_tracker'
+requiere_dependency 'carto/segment_tracker'
 
 module Resque
   module EventDeliveryJobs
-
     module TrackEvent
       @queue = :tracker
 
       def self.perform(user_id, event_name, properties)
-        tracker = Cartodb::SegmentTracker.new
-        tracker.track_event(user_id, 
-                            event_name, 
-                            properties)
+        tracker = Carto::SegmentTracker.new
+
+        tracker.track_event(user_id, event_name, properties)
         tracker.flush
       end
     end
-
   end
 end

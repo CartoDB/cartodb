8b064296de496f839321bc72af4cbc2b11d9a561
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-18 12:05:57 +0200

    change default max layers for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,6 +73,7 @@ class User < Sequel::Model
     :naked => true # avoid adding json_class to result
   }
 
+  DEFAULT_MAX_LAYERS = 8
 
   MIN_PASSWORD_LENGTH = 6
   MAX_PASSWORD_LENGTH = 64
@@ -204,6 +205,7 @@ class User < Sequel::Model
         self.google_maps_key = self.organization.google_maps_key
         self.google_maps_private_key = self.organization.google_maps_private_key
       end
+      self.max_layers ||= DEFAULT_MAX_LAYERS
       self.private_tables_enabled ||= true
       self.private_maps_enabled ||= true
       self.sync_tables_enabled ||= true
@@ -1515,33 +1517,16 @@ class User < Sequel::Model
   end
 
   def copy_account_features(to)
-    to.set_fields(self, [:private_tables_enabled,
-                         :sync_tables_enabled,
-                         :user_timeout,
-                         :database_timeout,
-                         :geocoding_quota,
-                         :map_view_quota,
-                         :table_quota,
-                         :database_host,
-                         :period_end_date,
-                         :map_view_block_price,
-                         :geocoding_block_price,
-                         :account_type,
-                         :twitter_datasource_enabled,
-                         :soft_twitter_datasource_limit,
-                         :twitter_datasource_quota,
-                         :twitter_datasource_block_price,
-                         :twitter_datasource_block_size,
-                         :here_isolines_quota,
-                         :here_isolines_block_price,
-                         :soft_here_isolines_limit,
-                         :obs_snapshot_quota,
-                         :obs_snapshot_block_price,
-                         :soft_obs_snapshot_limit,
-                         :obs_general_quota,
-                         :obs_general_block_price,
-                         :soft_obs_general_limit])
-
+    to.set_fields(self, [
+      :private_tables_enabled, :sync_tables_enabled, :max_layers, :user_timeout,
+      :database_timeout, :geocoding_quota, :map_view_quota, :table_quota, :database_host,
+      :period_end_date, :map_view_block_price, :geocoding_block_price, :account_type,
+      :twitter_datasource_enabled, :soft_twitter_datasource_limit, :twitter_datasource_quota,
+      :twitter_datasource_block_price, :twitter_datasource_block_size, :here_isolines_quota,
+      :here_isolines_block_price, :soft_here_isolines_limit, :obs_snapshot_quota,
+      :obs_snapshot_block_price, :soft_obs_snapshot_limit, :obs_general_quota,
+      :obs_general_block_price, :soft_obs_general_limit
+    ])
     to.invite_token = ::User.make_token
   end
 

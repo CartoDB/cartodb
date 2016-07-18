9ea786acb50accee40997ec02983168a4035fbee
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-18 19:09:39 +0200

    add MapExported event

--- lib/carto/tracking/events.rb
+++ lib/carto/tracking/events.rb
@@ -1,6 +1,28 @@
+require_dependency 'carto/tracking/segment_wrapper'
+
 module Carto
   module Tracking
-    class Events
+    module Events
+      class MapExported
+        EVENT_NAME = 'Exported map'.freeze
+
+        def initialize(user_id, visualization_id)
+          @user = Carto::User.find(user_id)
+          @visualization = Carto::Visualization.find(visualization_id)
+        end
+
+        def report
+          Carto::Tracking::SegmentWrapper.new(@user).send_event(@user, properties)
+        end
+
+        private
+
+        def properties
+          { privacy: @visualization.privacy,
+            type: @visualization.type,
+            id: @visualization.id }
+        end
+      end
     end
   end
 end

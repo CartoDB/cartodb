f27bc770f2fe67f8fb641603dcfc2ba4becb0925
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-19 12:02:16 +0200

    use inheritance to clena up stuff

--- lib/carto/tracking/events.rb
+++ lib/carto/tracking/events.rb
@@ -1,204 +1,85 @@
 require_dependency 'carto/tracking/segment_wrapper'
+require_dependency 'carto/tracking/properties_helper'
 
 module Carto
   module Tracking
     module Events
-      class ExportedMap
-        EVENT_NAME = 'Exported map'.freeze
+      include Carto::Tracking::PropertiesHelper
 
+      class TrackingEvent
+        def initialize(user, name, properties)
+          @user = user
+          @name = name
+          @properties = properties
+        end
+
+        def report
+          Carto::Tracking::SegmentWrapper.new(@user).send_event(@name, @properties)
+        rescue => exception
+          CartoDB::Logger.error(message: 'Carto::Tracking: Event could\'t be reported',
+                                exception: exception,
+                                properties: @properties,
+                                user: @user)
+        end
+      end
+
+      class ExportedMap < TrackingEvent
         def initialize(user, visualization)
-          @user = user
-          @visualization = visualization
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {
-            privacy: @visualization.privacy,
-            type: @visualization.type,
-            id: @visualization.id
-          }
+          super(user, 'Exported map', visualization_properties(visualization))
         end
       end
 
-      class VisitedDashboard
-        EVENT_NAME = 'Visited dashboard'.freeze
-
-        def initialize(user)
-          @user = user
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {}
-        end
-      end
-
-      class VisitedBuilder
-        EVENT_NAME = 'Visited builder'.freeze
-
-        def initialize(user)
-          @user = user
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {}
-        end
-      end
-
-      class VisitedDataset
-        EVENT_NAME = 'Visited dataset'.freeze
-
-        def initialize(user)
-          @user = user
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {}
-        end
-      end
-
-      class CreatedMap
-        EVENT_NAME = 'Created map'.freeze
-
+      class CreatedMap < TrackingEvent
         def initialize(user, visualization)
-          @user = user
-          @visualization = visualization
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {
-            privacy: @visualization.privacy,
-            type: @visualization.type,
-            id: @visualization.id
-          }
+          super(user, 'Created map', visualization_properties(visualization))
         end
       end
 
-      class CreatedDataset
-        EVENT_NAME = 'Created dataset'.freeze
+      class VisitedDashboard < TrackingEvent
+        def initialize(user)
+          super(user, 'Visited dashboard', visit_properties(user))
+        end
+      end
 
+      class VisitedBuilder < TrackingEvent
+        def initialize(user)
+          super(user, 'Visited builder', visit_properties(user))
+        end
+      end
+
+      class VisitedDataset < TrackingEvent
+        def initialize(user)
+          super(user, 'Visited dataset', visit_properties(user))
+        end
+      end
+
+      class CreatedDataset < TrackingEvent
         def initialize(user, table_visualization, origin: 'blank')
-          @user = user
-          @table_visualization = table_visualization
-          @origin = origin
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {
-            privacy: @table_visualization.privacy,
-            type: @table_visualization.type,
-            vis_id: @table_visualization.id,
-            origin: @origin
-          }
+          super(user, 'Created dataset', dataset_properties(table_visualization, origin: origin))
         end
       end
 
-      class DeletedDataset
-        EVENT_NAME = 'Deleted dataset'.freeze
-
+      class DeletedDataset < TrackingEvent
         def initialize(user, table_visualization)
-          @user = user
-          @table_visualization = table_visualization
-        end
-
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
-        end
-
-        private
-
-        def properties
-          {
-            privacy: @table_visualization.privacy,
-            type: @table_visualization.type,
-            vis_id: @table_visualization.id
-          }
+          super(user, 'Deleted dataset', dataset_properties(table_visualization))
         end
       end
 
-      class LikedMap
-        def initialize(user, visualization)
-          @user = user
-          @visualization = visualization
-        end
-
-        def report
-          Carto::Tracking::Events::MapLiking.new(@user, @visualization, 'like').report
-        end
-      end
-
-      class DislikedMap
-        def initialize(user, visualization)
-          @user = user
-          @visualization = visualization
-        end
-
-        def report
-          Carto::Tracking::Events::MapLiking.new(@user, @visualization, 'remove').report
-        end
-      end
-
-      class MapLiking
-        EVENT_NAME = 'Liked map'.freeze
-
+      class MapLiking < TrackingEvent
         def initialize(user, visualization, action)
-          @user = user
-          @visualization = visualization
-          @action = action
+          super(user, 'Liked map', like_properties(visualization, action))
         end
+      end
 
-        def report
-          Carto::Tracking::SegmentWrapper.new(@user).send_event(EVENT_NAME, properties)
+      class LikedMap < MapLiking
+        def initialize(user, visualization)
+          super(user, visualization, 'like')
         end
+      end
 
-        private
-
-        def properties
-          visualization_user = @visualization.user
-          {
-            action: @action,
-            vis_id: @visualization.id,
-            vis_name: @visualization.name,
-            vis_type: @visualization.type == 'derived' ? 'map' : 'dataset',
-            vis_author: visualization_user.username,
-            vis_author_email: visualization_user.email,
-            vis_author_id: visualization_user.id
-          }
+      class DislikedMap < MapLiking
+        def initialize(user, visualization)
+          super(user, visualization, 'remove')
         end
       end
     end

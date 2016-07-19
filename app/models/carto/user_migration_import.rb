6d2d480f18a2916250922a495e807a091aaafc60
Javier Torres <jtorres@carto.com>
2016-07-19 14:05:29 +0200

    UserMigrationImport downloads and imports users

--- app/models/carto/user_migration_import.rb
+++ app/models/carto/user_migration_import.rb
@@ -2,11 +2,12 @@
 
 require 'active_record'
 require 'fileutils'
-require_dependency 'services/user-mover/export_user'
-require_dependency 'file_upload'
+require_dependency 'services/user-mover/import_user'
 
 module Carto
   class UserMigrationImport < ::ActiveRecord::Base
+    belongs_to :organization, class_name: Carto::Organization
+    belongs_to :user, class_name: Carto::User
     belongs_to :log, class_name: Carto::Log
 
     STATE_PENDING = 'pending'.freeze
@@ -24,10 +25,52 @@ module Carto
     validates :json_file, presence: true
 
     def run_import
+      update_attributes(state: STATE_DOWNLOADING)
+      work_dir = create_work_directory
+      package_file = download_package(work_dir)
+      unzip_package(work_dir, package_file)
 
+      update_attributes(state: STATE_IMPORTING)
+      log.append("Importing user data")
+      CartoDB::DataMover::ImportJob.new(import_job_arguments(work_dir)).run!
+
+      log.append("Data import finished")
+      update_attributes(state: STATE_COMPLETE)
+    rescue => e
+      log.append_exception('Importing', exception: e)
+      CartoDB::Logger.error(exception: e, message: 'Error importing user data', job: inspect)
+      update_attributes(state: STATE_FAILURE)
+      false
     end
 
     private
+
+    def unzip_package(work_dir, package)
+      log.append("Unzipping #{package}")
+      `cd #{work_dir}; unzip #{package}; cd -`
+    end
+
+    def create_work_directory
+      log.append("Creating work directory")
+      work_dir = "#{import_dir}/#{id}/"
+      FileUtils.mkdir_p(work_dir)
+      work_dir
+    end
+
+    def download_package(work_dir)
+      destination = "#{work_dir}/export.zip"
+      log.append("Downloading #{exported_file} to #{destination}")
+      if exported_file.starts_with?('http')
+        http_client.get_file(exported_file, destination)
+      else
+        FileUtils.cp(exported_file, destination)
+        destination
+      end
+    end
+
+    def http_client
+      Carto::Http::Client.get('user_imports')
+    end
 
     def import_dir
       Cartodb.get_config(:user_migrator, 'user_imports_folder')
@@ -42,21 +85,23 @@ module Carto
       organization if !org_import?
     end
 
-    def import_job_arguments
+    def import_job_arguments(work_dir)
       {
         job_uuid: id,
-        file: "#{import_dir}/#{json_file}",
+        file: "#{work_dir}/#{json_file}",
         data: true,
         metadata: !import_only_data?,
         host: database_host,
         rollback: false,
         into_org_name: import_into.try(:name),
-        mode: :import
+        mode: :import,
+        logger: log.logger,
+        import_job_logger: log.logger
       }
     end
 
     def set_defaults
-      self.log = Carto::Log.create(type: 'user_migration_export') unless log
+      self.log = Carto::Log.create(type: 'user_migration_import') unless log
       self.state = STATE_PENDING unless state
       save
     end

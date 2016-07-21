bea02f663135a84268d1283030461fc8f99e592a
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-07-21 10:14:06 +0200

    fix namespace

--- services/importer/lib/importer/runner.rb
+++ services/importer/lib/importer/runner.rb
@@ -446,7 +446,7 @@ module CartoDB
         over_quota_amount = (QUOTA_MAGIC_NUMBER * file_size) - available_quota
 
         if over_quota_amount > 0
-          Carto::Events::ExceededQuota.new(user, quota_overage: over_quota_amount).report
+          Carto::Tracking::Events::ExceededQuota.new(user, quota_overage: over_quota_amount).report
           raise StorageQuotaExceededError
         end
         self

339d58b2d3c4b012da67c24ff1c7bd9bab61828e
Javier Torres <jtorres@carto.com>
2016-07-26 15:13:41 +0200

    Completely remove query from Torque layers. First removed in #7034

--- app/controllers/carto/api/vizjson3_presenter.rb
+++ app/controllers/carto/api/vizjson3_presenter.rb
@@ -350,11 +350,7 @@ module Carto
       end
 
       def as_torque
-        layer_options = decorate_with_data(
-          # Make torque always have a SQL query too (as vizjson v2)
-          @layer.options.deep_symbolize_keys.merge(query: wrap(sql_from(@layer.options), @layer.options)),
-          @decoration_data
-        )
+        layer_options = @layer.options.deep_symbolize_keys
 
         torque = {
           id:         @layer.id,

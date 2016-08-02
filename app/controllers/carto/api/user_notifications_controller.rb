f5aa40e7bbc9a957baa4bd2255363441b6cb8b27
Javier Torres <jtorres@carto.com>
2016-08-02 14:00:13 +0200

    User notification category setter

--- app/controllers/carto/api/user_notifications_controller.rb
+++ app/controllers/carto/api/user_notifications_controller.rb
@@ -7,24 +7,22 @@ module Carto
       include Carto::ControllerHelper
 
       ssl_required :update
-      before_filter :load_notifications, only: [:update]
+      before_filter :load_user_notifications, only: [:update]
 
       rescue_from StandardError, with: :rescue_from_standard_error
       rescue_from Carto::LoadError, with: :rescue_from_carto_error
-      rescue_from Carto::UnauthorizedError, with: :rescue_from_carto_error
-      rescue_from Carto::UnprocesableEntityError, with: :rescue_from_carto_error
 
       def update
-        @category_notifications.replace(params[:notifications])
-        @user.notifications.save!
-        render_jsonp({ notifications: @category_notifications }, 200)
+        category = params[:category].to_sym
+        @notifications.set_notifications_for_category(category, params[:notifications])
+        @notifications.save
+        render_jsonp({ notifications: @notifications.notifications_for_category(category) }, 200)
       end
 
       private
 
-      def load_notifications
-        @user = Carto::User.find(current_user.id)
-        @category_notifications = @user.notifications.notifications_for(:builder)
+      def load_user_notifications
+        @notifications = Carto::User.find(current_user.id).notifications
       end
     end
   end

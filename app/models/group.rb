831f0db75394f7911379634ddaaf8a94605076ee
Javier Torres <jtorres@carto.com>
2016-08-03 15:12:24 +0200

    Add auth_token to old group model for compatibility

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,15 @@ class Group < Sequel::Model
 
   many_to_one :organization
 
+  def get_auth_token
+    auth_token || generate_auth_token
+  end
+
+  private
+
+  def generate_auth_token
+    update(auth_token: SecureRandom.urlsafe_base64(nil, false))
+    auth_token
+  end
+
 end

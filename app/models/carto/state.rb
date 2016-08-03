f5a90575712582762e2da7bbb0ae85baf97d0f5d
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-03 17:20:43 +0200

    add basic validations

--- app/models/carto/state.rb
+++ app/models/carto/state.rb
@@ -4,10 +4,13 @@ require 'json'
 require_relative './carto_json_serializer'
 
 class Carto::State < ActiveRecord::Base
-  serialize :json, ::Carto::CartoJsonSymbolizerSerializer
-  validates :json, carto_json_symbolizer: true
-
   belongs_to :visualization, class_name: Carto::Visualization
   belongs_to :user, class_name: Carto::User
 
+  STATE_CHANNELS = ['public', 'private'].freeze
+
+  serialize :json, ::Carto::CartoJsonSymbolizerSerializer
+
+  validates :json, carto_json_symbolizer: true
+  validates :channel, in: STATE_CHANNELS
 end

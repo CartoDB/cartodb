4561320ea7e84e0acb253042443cc110597bc9a0
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-03 18:26:00 +0200

    add more validation

--- app/models/carto/state.rb
+++ app/models/carto/state.rb
@@ -7,10 +7,22 @@ class Carto::State < ActiveRecord::Base
   belongs_to :visualization, class_name: Carto::Visualization
   belongs_to :user, class_name: Carto::User
 
+  default_scope order('created_at DESC')
+
   STATE_CHANNELS = ['public', 'private'].freeze
 
   serialize :json, ::Carto::CartoJsonSymbolizerSerializer
-
   validates :json, carto_json_symbolizer: true
-  validates :channel, in: STATE_CHANNELS
+
+  validates :channel, inclusion: { in: STATE_CHANNELS }
+
+  validates :visualization, :user, presence: true
+
+  before_save :ensure_json
+
+  private
+
+  def ensure_json
+    self.json ||= Hash.new
+  end
 end

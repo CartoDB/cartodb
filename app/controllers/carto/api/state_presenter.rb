1ac767d10d263257b12e2deefe494d802ed39b5c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-05 16:10:29 +0200

    add state presenter

---
+++ app/controllers/carto/api/state_presenter.rb
@@ -0,0 +1,20 @@
+module Carto
+  module Api
+    class StatePresenter
+      def initialize(state)
+        @state = state
+      end
+
+      def to_hash
+        {
+          id: @state.id,
+          created_at: @state.created_at,
+          updated_at: @state.updated_at,
+          json: @user.json,
+          channel: @user.channel,
+          user: Carto::Api::UserPresenter.new(@state.user).to_public_poro
+        }
+      end
+    end
+  end
+end

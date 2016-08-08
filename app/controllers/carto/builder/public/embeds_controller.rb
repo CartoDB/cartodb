79a3b3b64ccc4bd95517812963d9fdc7cc3b9715
Javier Torres <jtorres@carto.com>
2016-08-08 12:37:03 +0200

    Add auth tokens tu builde embed

--- app/controllers/carto/builder/public/embeds_controller.rb
+++ app/controllers/carto/builder/public/embeds_controller.rb
@@ -32,6 +32,11 @@ module Carto
         def load_visualization
           @visualization = load_visualization_from_id_or_name(params[:visualization_id])
           render_404 unless @visualization
+          @auth_tokens = if @visualization.password_protected?
+                           @visualization.get_auth_tokens
+                         else
+                           current_viewer.get_auth_tokens
+                         end
         end
 
         def load_vizjson

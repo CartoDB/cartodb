5b37a2a6fb6c9b0e401bc54c3ba4a3cf07881b09
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-08 12:03:29 +0200

    add state route

--- config/routes.rb
+++ config/routes.rb
@@ -493,8 +493,7 @@ CartoDB::Application.routes.draw do
       scope '/viz/:visualization_id', constraints: { id: /[^\/]+/ } do
         resources :analyses, only: [:show, :create, :update, :destroy], constraints: { id: /[^\/]+/ }
         resources :mapcaps, only: [:index, :show, :create, :destroy], constraints: { id: /[^\/]+/ }
-        # resources :snapshots, only: [:index, :show, :create, :destroy], constraints: { id: /[^\/]+/ }
-        resource :state, only: :update
+        resource :state, only: [:update]
       end
 
       resources :visualization_exports, only: [:create, :show], constraints: { id: /[^\/]+/ } do

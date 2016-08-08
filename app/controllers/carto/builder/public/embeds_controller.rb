93da427d695e2c50e3a8b0346f208264ebec6a07
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-08 18:29:05 +0200

    pass state for embeds

--- app/controllers/carto/builder/public/embeds_controller.rb
+++ app/controllers/carto/builder/public/embeds_controller.rb
@@ -9,7 +9,9 @@ module Carto
         ssl_required :show, :show_protected
 
         before_filter :load_visualization, only: [:show, :show_protected]
-        before_filter :load_vizjson, only: [:show, :show_protected]
+        before_filter :load_visualization_for_vizjson_and_state,
+                      :load_vizjson,
+                      :load_state, only: [:show, :show_protected]
         before_filter :ensure_viewable, only: [:show]
 
         skip_before_filter :builder_users_only # This is supposed to be public even in beta
@@ -34,14 +36,20 @@ module Carto
           render_404 unless @visualization
         end
 
-        def load_vizjson
-          visualization_for_vizjson = if @visualization.mapcapped?
-                                        @visualization.latest_mapcap.regenerate_visualization
-                                      else
-                                        @visualization
-                                      end
+        def load_visualization_for_vizjson_and_state
+          @visualization_for_vizjson_and_state = if @visualization.mapcapped?
+                                                   @visualization.latest_mapcap.regenerate_visualization
+                                                 else
+                                                   @visualization
+                                                 end
+        end
 
-          @vizjson = generate_named_map_vizjson3(visualization_for_vizjson, params)
+        def load_vizjson
+          @vizjson = generate_named_map_vizjson3(@visualization_for_vizjson_and_state, params)
+        end
+
+        def load_state
+          @state = @visualization_for_vizjson_and_state.state
         end
 
         def ensure_viewable

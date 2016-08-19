b1b502894483e8b1872d2decc103cfc2c522b722
Javier Torres <jtorres@carto.com>
2016-08-19 10:12:22 +0200

    Edit github configuration to work with or without central

--- lib/oauth/github/config.rb
+++ lib/oauth/github/config.rb
@@ -3,29 +3,34 @@ module Carto
     class Config
       attr_reader :client_id, :client_secret, :state
 
-      def self.instance(state, base_callback_url, plan: nil, after: nil)
-        Github::Config.new(state, base_callback_url, plan, after) if CartoDB.get_config(:oauth, 'github').present?
+      def self.instance(state, after: nil)
+        Github::Config.new(state, after) if CartoDB.get_config(:oauth, 'github').present?
       end
 
-      def initialize(state, base_callback_url, plan, after)
+      def initialize(state, after)
         @client_id = CartoDB.get_config(:oauth, 'github', 'client_id')
         @client_secret = CartoDB.get_config(:oauth, 'github', 'client_secret')
         @state = state
-        @plan = plan
         @after = after
-        @base_callback_url = base_callback_url
       end
 
-      def github_url
+      def github_url(controller)
         url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&state=#{Rack::Utils.escape(state)}&scope=user"
 
         params = {}
-        params[:plan] = @plan if @plan
         params[:after] = @after if @after
-        redirect_uri = "#{@base_callback_url}?#{URI.encode_www_form(params)}" unless params.empty?
+        redirect_uri = "#{base_callback_url(controller)}?#{URI.encode_www_form(params)}" unless params.empty?
         url += "&redirect_uri=#{CGI.escape(redirect_uri)}" if redirect_uri
 
         url
+      end
+
+      def base_callback_url(controller)
+        if Cartodb::Central.sync_data_with_cartodb_central?
+          Cartodb::Central.new.host + '/github'
+        else
+          CartoDB.url(controller, 'github')
+        end
       end
     end
   end

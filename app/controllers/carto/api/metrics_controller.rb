a917b37935ac4a3916a671ee7b58008fb9d24530
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-19 13:52:32 +0200

    metrics controller shell

---
+++ app/controllers/carto/api/metrics_controller.rb
@@ -0,0 +1,12 @@
+# enconding utf-8
+
+module Carto
+  module Api
+    class MetricsController < ::Api::ApplicationController
+      ssl_required only: :create
+
+      def create
+      end
+    end
+  end
+end

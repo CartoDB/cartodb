a19219f5b93bdaf677abc57c3e5c629b96f010db
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-22 12:15:28 +0200

    reorder stuff

---
+++ lib/carto/tracking/carto_api.rb
@@ -0,0 +1,35 @@
+# encoding utf-8
+
+module Carto
+  module Tracking
+    module Formats
+      class CartoApi
+        def initialize(hash)
+          @hash = hash ? hash.with_indifferent_access : Hash.new
+        end
+
+        def fetch_record(symbol)
+          symbol_string = symbol.to_s
+          record_class_name = "Carto::#{symbol_string.camelize}".freeze
+          record_id = "#{symbol_string}_id".freeze
+
+          record_class_name.constantize.find(@hash[record_id])
+        rescue
+          nil
+        end
+
+        def to_hash
+          @hash
+        end
+
+        def to_segment_properties
+          user = fetch_record(:user)
+          visualization = fetch_record(:visualization)
+
+          Carto::Tracking::Formats::Segment.new(user: user, visualization: visualization)
+                                           .properties
+        end
+      end
+    end
+  end
+end

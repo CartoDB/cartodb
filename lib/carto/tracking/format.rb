49f17855264b4860b13fd920eb30165586914f0f
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-22 11:51:52 +0200

    use generic fetcher

--- lib/carto/tracking/format.rb
+++ lib/carto/tracking/format.rb
@@ -4,18 +4,16 @@ module Carto
   module Tracking
     class Format
       def initialize(hash)
-        @hash = hash
+        @hash = hash ? hash.with_indifferent_access : Hash.new
       end
 
-      def user
-        Carto::User.find(@hash[:user_id])
-      rescue ActiveRecord::RecordNotFound
-        nil
-      end
+      def fetch(object_symbol)
+        snake_case = object_symbol.to_s
+        class_name = "Carto::#{snake_case.camelize}".freeze
+        object_id = "#{snake_case}_id".freeze
 
-      def visualization
-        Carto::Visualization.find(@hash[:visualization_id])
-      rescue ActiveRecord::RecordNotFound
+        class_name.constantize.find(@hash[object_id])
+      rescue
         nil
       end
     end

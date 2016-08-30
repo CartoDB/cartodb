56505c0bd675766d7919c7681c76a44b8068dc77
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-30 17:19:05 +0200

    fixes test for no CompletedConnection

--- spec/lib/carto/tracking/events_spec.rb
+++ spec/lib/carto/tracking/events_spec.rb
@@ -418,19 +418,11 @@ module Carto
 
             it 'requires a user_id' do
               @event = @event_class.new(@user.id,
-                                        visualization_id: @visualization.id,
                                         connection: connection)
-            end
-
-            it 'requires a visualization_id' do
-              @event = @event_class.new(@user.id,
-                                        connection: connection,
-                                        user_id: @user.id)
             end
 
             it 'requires a connection' do
               @event = @event_class.new(@user.id,
-                                        visualization_id: @visualization.id,
                                         user_id: @user.id)
             end
           end
@@ -444,19 +436,9 @@ module Carto
               @event = nil
             end
 
-            it 'must have write access to visualization' do
-              @event = @event_class.new(@intruder.id,
-                                        visualization_id: @visualization.id,
-                                        user_id: @intruder.id,
-                                        connection: connection)
-
-              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
-            end
-
             it 'must be reported by user' do
               @event = @event_class.new(@intruder.id,
-                                        visualization_id: @visualization.id,
-                                        user_id: @intruder.id,
+                                        user_id: @user.id,
                                         connection: connection)
 
               expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
@@ -465,20 +447,8 @@ module Carto
 
           it 'reports' do
             event = @event_class.new(@user.id,
-                                     visualization_id: @visualization.id,
                                      user_id: @user.id,
                                      connection: connection)
-
-            expect { event.report! }.to_not raise_error
-          end
-
-          it 'reports by user with access' do
-            event = @event_class.new(@intruder.id,
-                                     visualization_id: @visualization.id,
-                                     user_id: @intruder.id,
-                                     connection: connection)
-
-            Carto::Visualization.any_instance.stubs(:writable_by?).with(@intruder).returns(true)
 
             expect { event.report! }.to_not raise_error
           end
@@ -497,7 +467,6 @@ module Carto
                                        :creation_time]
 
             format = @event_class.new(@user.id,
-                                      visualization_id: @visualization.id,
                                       user_id: @user.id,
                                       connection: connection)
                                  .instance_eval { @format }
@@ -568,7 +537,7 @@ module Carto
             it 'must be reported by user' do
               @event = @event_class.new(@intruder.id,
                                         visualization_id: @visualization.id,
-                                        user_id: @intruder.id,
+                                        user_id: @user.id,
                                         connection: connection)
 
               expect { @event.report! }.to raise_error(Carto::UnauthorizedError)

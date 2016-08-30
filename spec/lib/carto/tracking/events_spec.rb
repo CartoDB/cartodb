9e153d3efd1b278fd49716627768f952d55bd65f
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-30 11:27:53 +0200

    add tests for three events

--- spec/lib/carto/tracking/events_spec.rb
+++ spec/lib/carto/tracking/events_spec.rb
@@ -28,7 +28,17 @@ module Carto
           time_object.to_f / 60 / 60 / 24
         end
 
+        def check_hash_has_keys(hash, keys)
+          keys.each do |key|
+            puts "checking #{key} is not nil"
+            hash[key].should_not be_nil
+          end
+        end
+
         describe ExportedMap do
+          before (:all) { @event_class = self.class.description.constantize }
+          after  (:all) { @event_class = nil }
+
           describe '#properties validation' do
             after(:each) do
               expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
@@ -39,13 +49,11 @@ module Carto
             end
 
             it 'requires a user_id' do
-              @event = Carto::Tracking::Events::ExportedMap.new(@user.id,
-                                                                visualization_id: @visualization.id)
+              @event = @event_class.new(@user.id, visualization_id: @visualization.id)
             end
 
             it 'requires a visualization_id' do
-              @event = Carto::Tracking::Events::ExportedMap.new(@user.id,
-                                                                user_id: @user.id)
+              @event = @event_class.new(@user.id, user_id: @user.id)
             end
           end
 
@@ -59,72 +67,241 @@ module Carto
             end
 
             it 'must have access read access to visualization' do
-              @event = Carto::Tracking::Events::ExportedMap.new(@intruder.id,
-                                                                visualization_id: @visualization.id,
-                                                                user_id: @intruder.id)
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @intruder.id)
 
               expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
             end
 
             it 'must be reported by user' do
-              @event = Carto::Tracking::Events::ExportedMap.new(@intruder.id,
-                                                                visualization_id: @visualization.id,
-                                                                user_id: @user.id)
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @user.id)
 
               expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
             end
           end
 
           it 'reports' do
-            event = Carto::Tracking::Events::ExportedMap.new(@user.id,
-                                                             visualization_id: @visualization.id,
-                                                             user_id: @user.id)
+            event = @event_class.new(@user.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @user.id)
 
             expect { event.report! }.to_not raise_error
           end
 
           it 'reports by user with access' do
-            event = Carto::Tracking::Events::ExportedMap.new(@intruder.id,
-                                                             visualization_id: @visualization.id,
-                                                             user_id: @intruder.id)
+            event = @event_class.new(@intruder.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @intruder.id)
 
-            Carto::Visualization.any_instance
-                                .stubs(:is_accesible_by_user?)
-                                .with(@intruder)
-                                .returns(true)
+            Carto::Visualization.any_instance.stubs(:is_accesible_by_user?).with(@intruder).returns(true)
 
             expect { event.report! }.to_not raise_error
           end
 
           it 'matches current prod properites' do
-            let(:current_prod_properties) do
-              [:vis_id,
-               :privacy,
-               :type,
-               :object_created_at,
-               :lifetime,
-               :username,
-               :email,
-               :plan,
-               :user_active_for,
-               :user_created_at,
-               :organization,
-               :event_origin,
-               :creation_time]
-            end
+            current_prod_properties = [:vis_id,
+                                       :privacy,
+                                       :type,
+                                       :object_created_at,
+                                       :lifetime,
+                                       :username,
+                                       :email,
+                                       :plan,
+                                       :user_active_for,
+                                       :user_created_at,
+                                       :event_origin,
+                                       :creation_time]
 
-            event = Carto::Tracking::Events::ExportedMap.new(@user.id,
-                                                             visualization_id: @visualization.id,
-                                                             user_id: @user.id)
+            format = @event_class.new(@user.id,
+                                      visualization_id: @visualization.id,
+                                      user_id: @user.id)
+                                 .instance_eval { @format }
+
+            check_hash_has_keys(format.to_segment, current_prod_properties)
           end
         end
 
         describe CreatedMap do
+          before (:all) { @event_class = self.class.description.constantize }
+          after  (:all) { @event_class = nil }
 
+          describe '#properties validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'requires a user_id' do
+              @event = @event_class.new(@user.id, visualization_id: @visualization.id)
+            end
+
+            it 'requires a visualization_id' do
+              @event = @event_class.new(@user.id, user_id: @user.id)
+            end
+          end
+
+          describe '#security validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'must have access write access to visualization' do
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @intruder.id)
+
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+
+            it 'must be reported by user' do
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @user.id)
+
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+          end
+
+          it 'reports' do
+            event = @event_class.new(@user.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @user.id)
+
+            expect { event.report! }.to_not raise_error
+          end
+
+          it 'reports by user with access' do
+            event = @event_class.new(@intruder.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @intruder.id)
+
+            Carto::Visualization.any_instance.stubs(:writable_by?).with(@intruder).returns(true)
+
+            expect { event.report! }.to_not raise_error
+          end
+
+          it 'matches current prod properites' do
+            current_prod_properties = [:vis_id,
+                                       :privacy,
+                                       :type,
+                                       :object_created_at,
+                                       :lifetime,
+                                       :origin,
+                                       :username,
+                                       :email,
+                                       :plan,
+                                       :user_active_for,
+                                       :user_created_at,
+                                       :event_origin,
+                                       :creation_time]
+
+            format = @event_class.new(@user.id,
+                                      visualization_id: @visualization.id,
+                                      user_id: @user.id,
+                                      origin: 'bananas')
+                                 .instance_eval { @format }
+
+            check_hash_has_keys(format.to_segment, current_prod_properties)
+          end
         end
 
         describe DeletedMap do
+          before (:all) { @event_class = self.class.description.constantize }
+          after  (:all) { @event_class = nil }
 
+          describe '#properties validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'requires a user_id' do
+              @event = @event_class.new(@user.id, visualization_id: @visualization.id)
+            end
+
+            it 'requires a visualization_id' do
+              @event = @event_class.new(@user.id, user_id: @user.id)
+            end
+          end
+
+          describe '#security validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'must have access write access to visualization' do
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @intruder.id)
+
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+
+            it 'must be reported by user' do
+              @event = @event_class.new(@intruder.id,
+                                        visualization_id: @visualization.id,
+                                        user_id: @user.id)
+
+              expect { @event.report! }.to raise_error(Carto::UnauthorizedError)
+            end
+          end
+
+          it 'reports' do
+            event = @event_class.new(@user.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @user.id)
+
+            expect { event.report! }.to_not raise_error
+          end
+
+          it 'reports by user with access' do
+            event = @event_class.new(@intruder.id,
+                                     visualization_id: @visualization.id,
+                                     user_id: @intruder.id)
+
+            Carto::Visualization.any_instance.stubs(:writable_by?).with(@intruder).returns(true)
+
+            expect { event.report! }.to_not raise_error
+          end
+
+          it 'matches current prod properites' do
+            current_prod_properties = [:vis_id,
+                                       :privacy,
+                                       :type,
+                                       :object_created_at,
+                                       :lifetime,
+                                       :username,
+                                       :email,
+                                       :plan,
+                                       :user_active_for,
+                                       :user_created_at,
+                                       :event_origin,
+                                       :creation_time]
+
+            format = @event_class.new(@user.id,
+                                      visualization_id: @visualization.id,
+                                      user_id: @user.id)
+                                 .instance_eval { @format }
+
+            check_hash_has_keys(format.to_segment, current_prod_properties)
+          end
         end
 
         describe PublishedMap do

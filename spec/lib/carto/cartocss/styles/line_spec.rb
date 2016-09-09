47d5cab4fbd9bae7e7c430f4fb24fb7a8982ecad
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-09 15:27:36 +0200

    added specs for styles

---
+++ spec/lib/carto/cartocss/styles/line_spec.rb
@@ -0,0 +1,27 @@
+# encoding utf-8
+
+require 'spec_helper_min'
+
+module Carto
+  module CartoCSS
+    module Styles
+      describe Line do
+        describe '#default' do
+          let(:production_default_line_cartocss) do
+            "#layer {\n"\
+            "  line-width: 1.5;\n"\
+            "  line-color: #3EBCAE;\n"\
+            "  line-opacity: 1;\n"\
+            "}"
+          end
+
+          it 'has stayed the same' do
+            current_default_line_cartocss = Carto::CartoCSS::Styles::Line.new.to_cartocss
+
+            current_default_line_cartocss.should eq production_default_line_cartocss
+          end
+        end
+      end
+    end
+  end
+end

a9806b029aac59326ca9cd343c2e4de9b7d3d7fa
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-09 18:40:34 +0200

    pass cartocss_array

--- lib/carto/cartocss/presenter.rb
+++ lib/carto/cartocss/presenter.rb
@@ -5,37 +5,29 @@ module Carto
     class Presenter
       CARTOCSS_PROPERTY_INDENTATION = '  '.freeze
 
-      def initialize(class_name: 'layer', cartocss_blocks: [])
+      def initialize(class_name: 'layer', cartocss_array: [])
         @class_name = class_name
-        @cartocss_blocks = cartocss_blocks
+        @cartocss_array = cartocss_array
       end
 
       def to_s
         "##{@class_name} {\n"\
-        "#{formatted_cartocss_blocks}\n"\
+        "#{formatted_cartocss_body}\n"\
         "}"
       end
 
       private
 
-      def formatted_cartocss_blocks
-        formatted_cartocss_blocks = @cartocss_blocks.map do |cartocss_block|
-          format_cartocss_block(cartocss_block)
+      def formatted_cartocss_body
+        formatted_cartocss_properties = @cartocss_array.map do |cartocss_property|
+          format_cartocss_property(cartocss_property)
         end
 
-        formatted_cartocss_blocks.join("\n")
+        formatted_cartocss_properties.join("\n")
       end
 
-      def format_cartocss_block(block)
-        formatted_properties = block.lines.map do |property|
-          format_property(property)
-        end
-
-        formatted_properties.join
-      end
-
-      def format_property(line)
-        "#{CARTOCSS_PROPERTY_INDENTATION}#{line}"
+      def format_cartocss_property(cartocss_property)
+        "#{CARTOCSS_PROPERTY_INDENTATION}#{cartocss_property}"
       end
     end
   end

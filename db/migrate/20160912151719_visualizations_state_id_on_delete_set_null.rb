ade1fdc04d5894468a6dbb866fcb595f85e95562
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-12 17:58:43 +0200

    1 one operation

--- db/migrate/20160912151719_visualizations_state_id_on_delete_set_null.rb
+++ db/migrate/20160912151719_visualizations_state_id_on_delete_set_null.rb
@@ -2,9 +2,7 @@ Sequel.migration do
   up do
     Rails::Sequel.connection.run(%{
         ALTER TABLE visualizations
-          DROP CONSTRAINT visualizations_state_id_fkey;
-
-        ALTER TABLE visualizations
+          DROP CONSTRAINT visualizations_state_id_fkey,
           ADD CONSTRAINT visualizations_state_id_fkey
             FOREIGN KEY (state_id)
             REFERENCES states(id)
@@ -14,9 +12,7 @@ Sequel.migration do
   down do
     Rails::Sequel.connection.run(%{
         ALTER TABLE visualizations
-          DROP CONSTRAINT visualizations_state_id_fkey;
-
-        ALTER TABLE visualizations
+          DROP CONSTRAINT visualizations_state_id_fkey,
           ADD CONSTRAINT visualizations_state_id_fkey
             FOREIGN KEY (state_id)
             REFERENCES states(id); })

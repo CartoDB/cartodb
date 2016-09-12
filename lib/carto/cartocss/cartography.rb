506911d366bea420544706ead760a8a94fc33eef
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-12 09:19:09 +0200

    make cartography singleton

--- lib/carto/cartocss/cartography.rb
+++ lib/carto/cartocss/cartography.rb
@@ -1,21 +1,25 @@
 # encoding utf-8
 
+require 'singleton'
+
 module Carto
   module CartoCSS
     class Cartography
+      include Singleton
+
       CARTOGRAPHY_DEFAULT_FILE_PATH =
         "#{Rails.root}/lib/assets/javascripts/cartodb3/data/default-cartography.json".freeze
 
-      def initialize(file_path: CARTOGRAPHY_DEFAULT_FILE_PATH)
-        @file_path = file_path
+      def initialize
+        @cartographies = {}
       end
 
-      def to_hash
-        return @cartography if @cartography
+      def to_hash(file_path: CARTOGRAPHY_DEFAULT_FILE_PATH)
+        return @cartographies[file_path] if @cartographies[file_path]
 
-        cartography_file = File.read(@file_path)
+        cartography_file = File.read(file_path)
 
-        @cartography = JSON.parse(cartography_file).with_indifferent_access
+        @cartographies[file_path] = JSON.parse(cartography_file).with_indifferent_access
       end
     end
   end

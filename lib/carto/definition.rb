42549935ce380aeeb1cc07e32856e0ab27eac889
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-13 16:47:15 +0200

    fix naming

--- lib/carto/definition.rb
+++ lib/carto/definition.rb
@@ -13,9 +13,9 @@ module Carto
     def load_from_file(file_path)
       return @definitions[file_path] if @definitions[file_path]
 
-      cartography_file = File.read(file_path)
+      definition_file = File.read(file_path)
 
-      @definitions[file_path] = JSON.parse(cartography_file).with_indifferent_access
+      @definitions[file_path] = JSON.parse(definition_file).with_indifferent_access
     rescue Errno::ENOENT
       message = 'Carto::Definition: Couldn\'t read from file'
       CartoDB::Logger.error(message: message, file_path: file_path)

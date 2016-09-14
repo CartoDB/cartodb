984a5475aa9f0693eea9fa65c232b3167edeae20
Javier Torres <jtorres@carto.com>
2016-09-14 10:34:54 +0200

    Test: do not rewrite queries for own user datasets

--- spec/requests/api/json/visualizations_controller_spec.rb
+++ spec/requests/api/json/visualizations_controller_spec.rb
@@ -170,7 +170,28 @@ describe Api::Json::VisualizationsController do
           response.status.should eq 200
           vid = response.body[:id]
           v = CartoDB::Visualization::Member.new(id: vid).fetch
-          v.map.data_layers.first.options['query'].should eq "SELECT * FROM #{@org_user_1.username}.#{table1.name} LIMIT 1"
+          layer = v.map.data_layers.first
+          layer.options['query'].should eq "SELECT * FROM #{@org_user_1.username}.#{table1.name} LIMIT 1"
+        end
+      end
+
+      it 'does not rewrite queries for same user datasets' do
+        table1 = create_table(user_id: @org_user_1.id)
+        layer = table1.map.data_layers.first
+        layer.options['query'] = "SELECT * FROM #{table1.name} LIMIT 1"
+        layer.save
+        share_table_with_user(table1, @org_user_1)
+        payload = {
+          type: 'derived',
+          tables: ["#{@org_user_1.username}.#{table1.name}"]
+        }
+        post_json(api_v1_visualizations_create_url(user_domain: @org_user_2.username, api_key: @org_user_2.api_key),
+                  payload) do |response|
+          response.status.should eq 200
+          vid = response.body[:id]
+          v = CartoDB::Visualization::Member.new(id: vid).fetch
+          new_layer = v.map.data_layers.first
+          new_layer.options['query'].should eq layer.options['query']
         end
       end
     end

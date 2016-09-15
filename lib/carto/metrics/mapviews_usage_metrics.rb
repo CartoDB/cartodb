8eb49d23df437e2bcf9ae46cf5c811a3481adec5
Javier Torres <jtorres@carto.com>
2016-09-15 14:49:34 +0200

    Hound

--- lib/carto/metrics/mapviews_usage_metrics.rb
+++ lib/carto/metrics/mapviews_usage_metrics.rb
@@ -17,7 +17,8 @@ module Carto::Metrics
       if @organization
         @organization.users.map { |user|
           CartoDB::Stats::APICalls.new.get_api_calls_from_redis_source(
-            user.username, 'mapviews', from: date, to: date).values[0]
+            user.username, 'mapviews', from: date, to: date
+          ).values[0]
         }.sum
       else
         CartoDB::Stats::APICalls.new.get_api_calls_from_redis_source(@user, 'mapviews', from: date, to: date).values[0]

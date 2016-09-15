adf727157e5fa5944f0a550aa3b6b2ba588b019c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-15 17:35:33 +0200

    remove unnecesary array

--- app/models/carto/legend.rb
+++ app/models/carto/legend.rb
@@ -5,7 +5,6 @@ require_relative './carto_json_serializer'
 module Carto
   class Legend < ActiveRecord::Base
     VALID_LEGEND_TYPES = %(html category bubble choropleth custom).freeze
-    VALID_DEFINITION_KEYS = [].freeze # Note: Should be overriden by child classes
 
     serialize :definition, ::Carto::CartoJsonSerializer
 

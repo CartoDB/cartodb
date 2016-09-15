056ca2d8ba9457ecfa64c68b45e0d3d0557102ab
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-15 14:56:02 +0200

    back to basics

--- app/models/carto/legend.rb
+++ app/models/carto/legend.rb
@@ -12,44 +12,5 @@ module Carto
     validates :definition, carto_json_symbolizer: true
     validates :type, inclusion: { in: VALID_LEGEND_TYPES }
     validates :prehtml, :posthtml, presence: true
-    validate :validate_definition
-
-    def html?
-      type == 'html'
-    end
-
-    def category?
-      type == 'category'
-    end
-
-    def bubble?
-      type == 'bubble'
-    end
-
-    def choropleth?
-      type == 'choropleth'
-    end
-
-    def custom?
-      type == 'custom'
-    end
-
-    private
-
-    # Note: may be overriden or extendend by child classes
-    def validate_definition
-      missing_keys = VALID_DEFINITION_KEYS - definition.keys
-      exceeding_keys = definition.keys - VALID_DEFINITION_KEYS
-
-      if missing_keys.empty? && exceeding_keys.emtpy?
-        true
-      elsif missing_keys.any?
-        errors.add(:definition, "Missing keys: #{missing.join(', ')}")
-      elsif exceeding_keys.any?
-        errors.add(:definition, "Exceeding keys: #{exceeding_keys.join(', ')}")
-      end
-
-      false
-    end
   end
 end

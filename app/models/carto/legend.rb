4d9ee1242330748f45369d3cb060591950ef2868
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-16 16:01:29 +0200

    better validations

--- app/models/carto/legend.rb
+++ app/models/carto/legend.rb
@@ -1,29 +1,40 @@
 # encoding utf-8
 
 require_relative './carto_json_serializer'
+require_relative '../../controllers/carto/api/legend_presenter'
 require_dependency 'carto/lengend_definition_validator'
 
 module Carto
   class Legend < ActiveRecord::Base
     self.inheritance_column = :_type
 
+    belongs_to :layer, class_name: Carto::Layer
+
     VALID_LEGEND_TYPES = %(html category bubble choropleth custom).freeze
 
     serialize :definition, ::Carto::CartoJsonSerializer
 
     validates :definition, carto_json_symbolizer: true
-    validates :type, inclusion: { in: VALID_LEGEND_TYPES }
-    validates :prehtml, :posthtml, :definition, presence: true
+    validates :prehtml, :posthtml, :definition, :type, presence: true
+    validates :type, inclusion: { in: VALID_LEGEND_TYPES }, allow_nil: true
 
     validate :validate_definition_schema
 
+    before_validation :ensure_definition
+
     def to_hash
-      Carto::Api::LegendPresenter(self).to_hash
+      Carto::Api::LegendPresenter.to_hash(self)
     end
 
     private
 
+    def ensure_definition
+      self.definition ||= Hash.new
+    end
+
     def validate_definition_schema
+      return unless type && definiton
+
       definition_errors = Carto::LegendDefinitionValidator.errors(type, definition)
 
       errors.add(:definition, definition_errors.join(', ')) if definition_errors.any?

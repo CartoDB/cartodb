5c7b423b675e3c2994fa4fc0167f17bf5029cd1e
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-16 16:01:01 +0200

    make presenter use class methods

--- app/controllers/carto/api/legend_presenter.rb
+++ app/controllers/carto/api/legend_presenter.rb
@@ -3,21 +3,17 @@
 module Carto
   module Api
     class LegendPresenter
-      def initialize(legend)
-        @legend = legend
-      end
-
-      def to_hash
+      def self.to_hash(layer)
         {
-          created_at: @layer.created_at,
-          definition: @layer.definition,
-          id: @layer.id,
-          layer_id: @layer.layer_id,
-          posthtml: @layer.posthtml,
-          prehtml: @layer.prehtml,
-          title: @layer.title,
-          type: @layer.type,
-          updated_at: @layer.updated_at
+          created_at: layer.created_at,
+          definition: layer.definition,
+          id: layer.id,
+          layer_id: layer.layer_id,
+          posthtml: layer.posthtml,
+          prehtml: layer.prehtml,
+          title: layer.title,
+          type: layer.type,
+          updated_at: layer.updated_at
         }
       end
     end

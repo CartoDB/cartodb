2802359d8918ed57d55193bbb4978eea00020736
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-19 14:55:04 +0200

    fix nil check

--- lib/carto/lengend_definition_validator.rb
+++ lib/carto/lengend_definition_validator.rb
@@ -15,9 +15,10 @@ module Carto
     }.with_indifferent_access.freeze
 
     def self.errors(type, definition)
-      schema_location = "#{Rails.root}/#{VALIDATION_FORMAT_LOCATIONS_MAP[type]}"
-      return ['could not be validated'] unless definition && schema_location
+      raw_schema_location = VALIDATION_FORMAT_LOCATIONS_MAP[type]
+      return ['could not be validated'] unless definition && raw_schema_location
 
+      schema_location = "#{Rails.root}/#{raw_schema_location}"
       schema = Carto::Definition.instance.load_from_file(schema_location)
 
       JSON::Validator.fully_validate(schema, definition.with_indifferent_access)

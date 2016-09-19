148041439594efb700cdba392b8a7620930cdec2
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-19 13:48:44 +0200

    fix tests

--- spec/requests/carto/api/legends_controller_spec.rb
+++ spec/requests/carto/api/legends_controller_spec.rb
@@ -11,8 +11,8 @@ module Carto
 
       let(:html_legend_payload) do
         {
-          prehtml: "<h3>Es acaso</h3>",
-          posthtml: "<h3>el mejor artista del mundo?</h3>",
+          pre_html: "<h3>Es acaso</h3>",
+          post_html: "<h3>el mejor artista del mundo?</h3>",
           title: "La verdad",
           type: "html",
           definition: {
@@ -23,8 +23,8 @@ module Carto
 
       let(:category_legend_payload) do
         {
-          prehtml: "<h3>Es acaso</h3>",
-          posthtml: "<h3>el mejor artista del mundo?</h3>",
+          pre_html: "<h3>Es acaso</h3>",
+          post_html: "<h3>el mejor artista del mundo?</h3>",
           title: "La verdad",
           type: "category",
           definition: {}
@@ -33,8 +33,8 @@ module Carto
 
       let(:custom_legend_payload) do
         {
-          prehtml: "<h3>Es acaso</h3>",
-          posthtml: "<h3>el mejor artista del mundo?</h3>",
+          pre_html: "<h3>Es acaso</h3>",
+          post_html: "<h3>el mejor artista del mundo?</h3>",
           title: "La verdad",
           type: "custom",
           definition: {
@@ -48,20 +48,20 @@ module Carto
 
       let(:bubble_legend_payload) do
         {
-          prehtml: "<h3>Es acaso</h3>",
-          posthtml: "<h3>el mejor artista del mundo?</h3>",
+          pre_html: "<h3>Es acaso</h3>",
+          post_html: "<h3>el mejor artista del mundo?</h3>",
           title: "La verdad",
           type: "bubble",
           definition: {
-            fill_color: '#fff'
+            color: '#fff'
           }
         }
       end
 
       let(:choropleth_legend_payload) do
         {
-          prehtml: "<h3>Es acaso</h3>",
-          posthtml: "<h3>el mejor artista del mundo?</h3>",
+          pre_html: "<h3>Es acaso</h3>",
+          post_html: "<h3>el mejor artista del mundo?</h3>",
           title: "La verdad",
           type: "choropleth",
           definition: {
@@ -86,14 +86,15 @@ module Carto
       end
 
       def legend_is_correct(legend)
-        persisted_legend = Carto::Legend.find(legend[:id])
+        symbolized_legend = legend.deep_symbolize_keys
+        persisted_legend = Carto::Legend.find(symbolized_legend[:id])
         persisted_legend_hash = persisted_legend.to_hash
                                                 .except(:created_at, :updated_at, :definition)
 
-        legend.except(:created_at, :updated_at, :definition)
-              .should eq persisted_legend_hash
+        symbolized_legend.except(:created_at, :updated_at, :definition)
+                         .should eq persisted_legend_hash
 
-        legend[:definition].with_indifferent_access.should eq persisted_legend.definition
+        symbolized_legend[:definition].with_indifferent_access.should eq persisted_legend.definition
       end
 
       describe '#create' do
@@ -137,43 +138,72 @@ module Carto
           end
         end
 
-        it 'should create a valid html legend' do
-          post_json create_lengend_url, html_legend_payload do |response|
-            response.status.should eq 201
+        describe 'with valid definitions' do
+          after(:each) do
+            post_json create_lengend_url, @payload do |response|
+              response.status.should eq 201
 
-            legend_is_correct(response.body)
+              legend_is_correct(response.body)
+            end
+          end
+
+          after(:all) do
+            @payload = nil
+          end
+
+          it 'for html' do
+            @payload = html_legend_payload
+          end
+
+          it 'for category' do
+            @payload = category_legend_payload
+          end
+
+          it 'for custom' do
+            @payload = custom_legend_payload
+          end
+
+          it 'for bubble' do
+            @payload = bubble_legend_payload
+          end
+
+          it 'for choropleth' do
+            @payload = choropleth_legend_payload
           end
         end
 
-        it 'should create a valid category legend' do
-          post_json create_lengend_url, category_legend_payload do |response|
-            response.status.should eq 201
+        describe 'with spammy definitions' do
+          after(:each) do
+            spammy_definition = @payload[:definition].merge(spam: 'hell')
+            @payload[:definition] = spammy_definition
 
-            legend_is_correct(response.body)
+            post_json create_lengend_url, @payload do |response|
+              response.status.should eq 422
+            end
           end
-        end
 
-        it 'should create a valid custom legend' do
-          post_json create_lengend_url, custom_legend_payload do |response|
-            response.status.should eq 201
-
-            legend_is_correct(response.body)
+          after(:all) do
+            @payload = nil
           end
-        end
 
-        it 'should create a valid bubble legend' do
-          post_json create_lengend_url, bubble_legend_payload do |response|
-            response.status.should eq 201
-
-            legend_is_correct(response.body)
+          it 'banned for html' do
+            @payload = html_legend_payload
           end
-        end
 
-        it 'should create a valid choropleth legend' do
-          post_json create_lengend_url, choropleth_legend_payload do |response|
-            response.status.should eq 201
+          it 'banned for category' do
+            @payload = category_legend_payload
+          end
 
-            legend_is_correct(response.body)
+          it 'banned for custom' do
+            @payload = custom_legend_payload
+          end
+
+          it 'banned for bubble' do
+            @payload = bubble_legend_payload
+          end
+
+          it 'banned for choropleth' do
+            @payload = choropleth_legend_payload
           end
         end
       end
@@ -182,11 +212,12 @@ module Carto
         before (:all) { @legend = Carto::Legend.create!(html_legend_payload.merge(layer_id: @layer.id)) }
         after  (:all) { @legend.destroy }
 
-        def show_lengend_url(user: @user, visualization: @visualization, layer: @layer)
-          legends_url(user_domain: user.subdomain,
-                      visualization_id: visualization.id,
-                      layer_id: layer.id,
-                      api_key: user.api_key)
+        def show_lengend_url(user: @user, visualization: @visualization, layer: @layer, legend: @legend)
+          legend_url(user_domain: user.subdomain,
+                     visualization_id: visualization.id,
+                     layer_id: layer.id,
+                     id: legend.id,
+                     api_key: user.api_key)
         end
 
         it 'should show a legend' do
@@ -200,14 +231,40 @@ module Carto
         it 'should not show a legend to others' do
           get_json show_lengend_url(user: @intruder), {} do |response|
             response.status.should eq 403
+          end
+        end
+      end
 
-            response.body.should be_empty
+      describe '#update' do
+        before (:all) { @legend = Carto::Legend.create!(html_legend_payload.merge(layer_id: @layer.id)) }
+        after  (:all) { @legend.destroy }
+
+        def update_lengend_url(user: @user, visualization: @visualization, layer: @layer, legend: @legend)
+          legend_url(user_domain: user.subdomain,
+                     visualization_id: visualization.id,
+                     layer_id: layer.id,
+                     id: legend.id,
+                     api_key: user.api_key)
+        end
+
+        it 'update a legend' do
+          html_legend_payload[:definition][:html] = '<p>modified</p>'
+          put_json update_lengend_url, html_legend_payload do |response|
+            response.status.should eq 200
+
+            legend_is_correct(response.body)
+          end
+        end
+
+        it 'should let others update a legend' do
+          put_json update_lengend_url(user: @intruder), html_legend_payload do |response|
+            response.status.should eq 403
           end
         end
       end
 
       describe '#index' do
-        before (:all) { @legends = 5.times { Carto::Legend.create!(html_legend_payload.merge(layer_id: @layer.id)) } }
+        before (:all) { @legends = 2.times { Carto::Legend.create!(html_legend_payload.merge(layer_id: @layer.id)) } }
         after  (:all) { @legends.map(&:destroy) }
 
         def index_lengend_url(user: @user, visualization: @visualization, layer: @layer)
@@ -221,15 +278,15 @@ module Carto
           get_json index_lengend_url, {} do |response|
             response.status.should eq 200
 
-            legend_is_correct(response.body)
+            response.body.each do |legend|
+              legend_is_correct(legend)
+            end
           end
         end
 
-        it 'should not show a legend to others' do
+        it 'should not index legends to others' do
           get_json index_lengend_url(user: @intruder), {} do |response|
             response.status.should eq 403
-
-            response.body.should be_empty
           end
         end
       end

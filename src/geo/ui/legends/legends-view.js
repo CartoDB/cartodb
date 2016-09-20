a0250468040224a4d0bd38c8ebe1ea507b94f901
Pablo Alonso Garcia <pablo@cartodb.com>
2016-09-20 13:00:52 +0200

    Render bottom and top shadows when while scrolling

--- src/geo/ui/legends/legends-view.js
+++ src/geo/ui/legends/legends-view.js
@@ -1,12 +1,13 @@
+var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
 var Ps = require('perfect-scrollbar');
-
+var template = require('./legends-view.tpl');
 var LayerLegendsView = require('./layer-legends-view');
 
 var LegendsView = Backbone.View.extend({
 
-  className: 'CDB-Legends',
+  className: 'CDB-Legends-canvas',
 
   initialize: function (deps) {
     if (!deps.layersCollection) throw new Error('layersCollection is required');
@@ -21,16 +22,19 @@ var LegendsView = Backbone.View.extend({
   },
 
   render: function () {
+    this.$el.html(template());
     var layerModelsWithLegends = this._layersCollection.getLayersWithLegends();
     _.each(layerModelsWithLegends.reverse(), this._renderLayerLegends, this);
     this._isRendered = true;
     this._renderScroll();
+    this._renderShadows();
+    this._bindScroll();
 
     return this;
   },
 
   _renderScroll: function () {
-    Ps.initialize(this.el, {
+    Ps.initialize(this._container(), {
       wheelSpeed: 1,
       wheelPropagation: false,
       swipePropagation: true,
@@ -39,9 +43,46 @@ var LegendsView = Backbone.View.extend({
     });
   },
 
+  _renderShadows: function () {
+    this.$shadowTop = $('<div>').addClass('CDB-Legends-canvasShadow CDB-Legends-canvasShadow--top');
+    this.$shadowBottom = $('<div>').addClass('CDB-Legends-canvasShadow CDB-Legends-canvasShadow--bottom');
+    this.$el.append(this.$shadowTop);
+    this.$el.append(this.$shadowBottom);
+  },
+
+  _bindScroll: function () {
+    this.$(this._container())
+      .on('ps-y-reach-start', _.bind(this._onScrollTop, this))
+      .on('ps-y-reach-end', _.bind(this._onScrollBottom, this))
+      .on('ps-scroll-y', _.bind(this._onScroll, this));
+  },
+
+  _onScrollTop: function () {
+    this.$shadowTop.removeClass('is-visible');
+  },
+
+  _onScroll: function () {
+    var $el = $(this._container());
+    var currentPos = $el.scrollTop();
+    var max = $el.get(0).scrollHeight;
+    var height = $el.outerHeight();
+    var maxPos = max - height;
+
+    this.$shadowTop.toggleClass('is-visible', currentPos > 0);
+    this.$shadowBottom.toggleClass('is-visible', currentPos < maxPos);
+  },
+
+  _onScrollBottom: function () {
+    this.$shadowBottom.removeClass('is-visible');
+  },
+
+  _container: function () {
+    return this.el.querySelector('.js-container');
+  },
+
   _renderLayerLegends: function (layerModel) {
     var layerLegendsView = new LayerLegendsView({ model: layerModel });
-    this.$el.append(layerLegendsView.render().$el);
+    this.$(this._container()).append(layerLegendsView.render().$el);
   },
 
   _onLayerAddedOrRemoved: function (layerModel) {
---
+++ src/geo/ui/legends/legends-view.tpl
@@ -0,0 +1,1 @@
+<div class="CDB-Legends-canvasInner js-container"></div>

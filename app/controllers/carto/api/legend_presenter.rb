df80e46a9c2cba6b04adf6860f7a3a96cb9c1f69
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-20 10:03:32 +0200

    do away with class methods

--- app/controllers/carto/api/legend_presenter.rb
+++ app/controllers/carto/api/legend_presenter.rb
@@ -3,17 +3,21 @@
 module Carto
   module Api
     class LegendPresenter
-      def self.to_hash(layer)
+      def initialize(layer)
+        @layer = layer
+      end
+
+      def to_hash
         {
-          created_at: layer.created_at,
-          definition: layer.definition,
-          id: layer.id,
-          layer_id: layer.layer_id,
-          post_html: layer.post_html,
-          pre_html: layer.pre_html,
-          title: layer.title,
-          type: layer.type,
-          updated_at: layer.updated_at
+          created_at: @layer.created_at,
+          definition: @layer.definition,
+          id: @layer.id,
+          layer_id: @layer.layer_id,
+          post_html: @layer.post_html,
+          pre_html: @layer.pre_html,
+          title: @layer.title,
+          type: @layer.type,
+          updated_at: @layer.updated_at
         }
       end
     end

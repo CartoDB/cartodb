ceaca934fc41ee576e6e68aaa65090c4bd054475
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-20 10:22:37 +0200

    validate max legends per layer in model

--- app/models/carto/legend.rb
+++ app/models/carto/legend.rb
@@ -18,7 +18,9 @@ module Carto
     validates :pre_html, :post_html, :type, :layer_id, presence: true
     validates :type, inclusion: { in: VALID_LEGEND_TYPES }, allow_nil: true
 
-    validate :on_data_layer, :validate_definition_schema
+    validate :on_data_layer,
+             :under_max_legends_per_layer,
+             :validate_definition_schema
 
     before_validation :ensure_definition
 
@@ -28,18 +30,26 @@ module Carto
       self.definition ||= Hash.new
     end
 
+    def on_data_layer
+      unless layer.data_layer?
+        errors.add(:layer_id, "'#{layer.kind}' layers can't have legends")
+      end
+    end
+
+    MAX_LEGENDS_PER_LAYER = 2
+
+    def under_max_legends_per_layer
+      unless layer.legends.count < MAX_LEGENDS_PER_LAYER
+        errors.add(:layer_id, 'Maximum number of legends per layer reached')
+      end
+    end
+
     def validate_definition_schema
       return unless type && definition
 
       definition_errors = Carto::LegendDefinitionValidator.errors(type, definition)
 
       errors.add(:definition, definition_errors.join(', ')) if definition_errors.any?
-    end
-
-    def on_data_layer
-      unless layer.data_layer?
-        errors.add(:layer_id, "'#{layer.kind}' layers can't have legends")
-      end
     end
   end
 end

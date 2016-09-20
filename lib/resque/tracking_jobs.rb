db06d84edb84f0c35a896f6c78a738d6c073a484
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-20 15:43:04 +0200

    fix some segment stuff (off topic)

--- lib/resque/tracking_jobs.rb
+++ lib/resque/tracking_jobs.rb
@@ -1,12 +1,16 @@
 # encoding utf-8
 
+require 'carto/tracking/services/segment'
+
 module Resque
   module TrackingJobs
     module SendSegmentEvent
+      include Carto::Tracking::Services::Segment
+
       @queue = :tracker
 
       def self.perform(user_id, name, properties)
-        return unless key = Cartodb.get_config(:segment, 'api_key')
+        return unless key = api_key
 
         segment = Segment::Analytics.new(write_key: key)
         segment.track(user_id: user_id, event: name, properties: properties)

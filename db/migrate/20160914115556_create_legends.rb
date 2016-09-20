a4fb03cbed4ed57bb145c335467aa04fc5c95904
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-20 17:10:51 +0200

    concurrent index WIP

--- db/migrate/20160914115556_create_legends.rb
+++ db/migrate/20160914115556_create_legends.rb
@@ -1,17 +1,29 @@
 Sequel.migration do
-  change do
+  up do
     create_table :legends do
-      foreign_key :layer_id, :layers, type: 'uuid', null: false, on_delete: :cascade
-
       Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
       DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP
 
+      Uuid :layer_id
       String :title
       String :pre_html
       String :post_html
       String :type, null: false
       String :definition, null: false, type: 'json'
     end
+
+    Rails::Sequel.connection.run %{
+      CREATE INDEX CONCURRENTLY "legends_layer_id_index"
+        ON "legends" ("layer_id");
+
+      ALTER TABLE legends
+        ADD CONSTRAINT "legends_layer_id_fkey" FOREIGN KEY (layer_id)
+        USING INDEX legends_layer_id_index
+        REFERENCES layers (id); }
+  end
+
+  down do
+    drop_table :legends
   end
 end

814eecfff5f2a409dfab62fbabc2f40b828c9157
Luis Bosque <luisico@gmail.com>
2016-09-20 12:27:59 +0200

    Remove trailing spaces so hound can be happy

--- db/migrate/20160920104742_drop_log_id_fk_from_migration_exports.rb
+++ db/migrate/20160920104742_drop_log_id_fk_from_migration_exports.rb
@@ -2,19 +2,18 @@ Sequel.migration do
   up do
     alter_table :user_migration_exports do
       drop_constraint :user_migration_exports_log_id_fkey
-    end 
+    end
     alter_table :user_migration_imports do
       drop_constraint :user_migration_imports_log_id_fkey
-    end 
-  end 
+    end
+  end
 
   down do
     alter_table :user_migration_exports do
       add_foreign_key [:log_id], :logs, null: false
-    end 
+    end
     alter_table :user_migration_imports do
       add_foreign_key [:log_id], :logs, null: false
-    end 
-  end 
+    end
+  end
 end
-

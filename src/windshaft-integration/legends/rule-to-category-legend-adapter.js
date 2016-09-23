7385265e7efeff5fa95e4c2357a59fbf96cc130f
Pablo Alonso Garcia <pablo@cartodb.com>
2016-09-23 16:07:40 +0200

    Better rule matching for category and choropleth adapters

--- src/windshaft-integration/legends/rule-to-category-legend-adapter.js
+++ src/windshaft-integration/legends/rule-to-category-legend-adapter.js
@@ -1,6 +1,20 @@
+var _ = require('underscore');
+
+var VALID_PROPS = ['line-color', 'marker-fill', 'polygon-fill'];
+var VALID_MAPPINGS = ['='];
+
 module.exports = {
   canAdapt: function (rule) {
-    return rule.mapping === '=';
+    return this._ruleHasValidProperty(rule) &&
+      this._ruleHasValidMapping(rule);
+  },
+
+  _ruleHasValidProperty: function (rule) {
+    return _.contains(VALID_PROPS, rule.prop);
+  },
+
+  _ruleHasValidMapping: function (rule) {
+    return _.contains(VALID_MAPPINGS, rule.mapping);
   },
 
   adapt: function (rule) {

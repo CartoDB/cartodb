ab9f69031eaa43e87a801d44592525f410a9e319
Mario de Frutos <mario.defrutos@cartodb.com>
2016-09-28 17:33:45 +0200

    Added autobuild parameters based on the connection information

--- app/controllers/carto/api/connectors_controller.rb
+++ app/controllers/carto/api/connectors_controller.rb
@@ -26,10 +26,12 @@ module Carto
 
       def connect
         provider_id = params[:provider_id]
-        parameters = build_connection_parameters(params)
+        parameters = build_connection_parameters(provider_id, params)
         begin
           connector = Carto::Connector.new(parameters, user: current_user, logger: nil)
           render_jsonp({"connected": connector.check_connection})
+        rescue Carto::Connector::InvalidParametersError => e
+          render_jsonp({ errors: e.message }, 422)
         rescue
           render_jsonp({ errors: "Error connecting to provider #{provider_id}, check connection parameters" }, 400)
         end
@@ -37,10 +39,16 @@ module Carto
 
       def tables
         provider_id = params[:provider_id]
-        parameters = build_connection_parameters(params)
+        parameters = build_connection_parameters(provider_id, params)
         if Carto::Connector.list_tables?(provider_id)
-          connector = Carto::Connector.new(parameters, user: current_user, logger: nil)
-          render_jsonp(connector.list_tables(MAX_LISTED_TABLES))
+          begin
+            connector = Carto::Connector.new(parameters, user: current_user, logger: nil)
+            render_jsonp(connector.list_tables(MAX_LISTED_TABLES))
+          rescue Carto::Connector::InvalidParametersError => e
+            render_jsonp({ errors: e.message }, 422)
+          rescue
+            render_jsonp({ errors: "Error connecting to provider #{provider_id}, check connection parameters" }, 400)
+          end
         else
           render_jsonp({ errors: "Provider #{provider_id} doesn't support list tables" }, 422)
         end
@@ -48,18 +56,15 @@ module Carto
 
       private
 
-      def build_connection_parameters(request_params)
+      def build_connection_parameters(provider_id, request_params)
         parameters = {}
         parameters[:provider] = request_params[:provider_id]
         parameters[:connection] = {}
-        parameters[:connection][:server] = request_params[:server]
-        parameters[:connection][:port] = request_params[:port]
-        parameters[:connection][:database] = request_params[:database]
-        parameters[:connection][:username] = request_params[:username]
-        if request_params[:password].blank?
-          parameters[:connection][:password] = ''
-        else
-          parameters[:connection][:password] = request_params[:password]
+        provider_information = Carto::Connector.information(provider_id)
+        provider_information[:parameters]["connection"].each do |key, _value|
+          if request_params[key.to_sym].present?
+            parameters[:connection][key.to_sym] = request_params[key.to_sym]
+          end
         end
         parameters
       end

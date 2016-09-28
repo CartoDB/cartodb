4cf96c5d45222f1a596331a264d6843a782ff17f
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-28 16:45:44 +0200

    add tests for update

--- spec/requests/carto/api/maps_controller_spec.rb
+++ spec/requests/carto/api/maps_controller_spec.rb
@@ -11,25 +11,92 @@ describe Carto::Api::MapsController do
   it_behaves_like 'maps controllers' do
   end
 
-  before(:all) do
-    @user = FactoryGirl.create(:carto_user)
-    @user2 = FactoryGirl.create(:carto_user)
-    @map, @table, @table_visualization, @visualization = create_full_visualization(@user)
-  end
-
-  after(:all) do
-    destroy_full_visualization(@map, @table, @table_visualization, @visualization)
-    # This avoids connection leaking.
-    ::User[@user.id].destroy
-    ::User[@user2.id].destroy
-  end
-
   def create_show_map_url(user: @user, map: @map)
     map_url(user_domain: user.subdomain, api_key: user.api_key, id: map.id)
   end
 
+  describe '#update' do
+    before(:all) do
+      @user = FactoryGirl.create(:carto_user)
+      @user2 = FactoryGirl.create(:carto_user)
+      @map, @table, @table_visualization, @visualization = create_full_visualization(@user)
+    end
+
+    after(:all) do
+      destroy_full_visualization(@map, @table, @table_visualization, @visualization)
+      # This avoids connection leaking.
+      ::User[@user.id].destroy
+      ::User[@user2.id].destroy
+    end
+
+    let(:payload) do
+      {
+        bounding_box_ne: "[85.0511, 179]",
+        bounding_box_sw: "[-85.0511, -179]",
+        center: "[28.483177058570757, -82.825927734375]",
+        legends: true,
+        provider: "leaflet",
+        scrollwheel: true,
+        view_bounds_ne: "[32.30570601389429, -76.32202148437499]",
+        view_bounds_sw: "[24.51713945052515, -89.329833984375]",
+        zoom: 7,
+        embed_options: {
+          show_menu: false
+        }
+      }
+    end
+
+    it 'updates an existing map' do
+      put_json create_show_map_url, payload do |response|
+        response.status.should be_success
+      end
+    end
+
+    it 'validates on update' do
+      payload[:embed_options] = { spammy: 'hell', show_menu: true }
+      put_json create_show_map_url, payload do |response|
+        response.status.should eq 422
+      end
+    end
+
+    it 'returns 401 for unathorized user' do
+      put_json map_url(user_domain: @user2.subdomain,
+                       api_key: 'wadus',
+                       id: @map.id) do |response|
+        response.status.should eq 401
+      end
+    end
+
+    it 'returns 404 for maps not owned by the user' do
+      put_json create_show_map_url(user: @user2) do |response|
+        response.status.should eq 404
+      end
+    end
+
+    it 'returns 404 for unexisting map' do
+      put_json map_url(user_domain: @user.subdomain,
+                       api_key: @user.api_key,
+                       id: random_uuid) do |response|
+        response.status.should eq 404
+      end
+    end
+  end
+
   describe '#show' do
-    it 'returns existing map by id' do
+    before(:all) do
+      @user = FactoryGirl.create(:carto_user)
+      @user2 = FactoryGirl.create(:carto_user)
+      @map, @table, @table_visualization, @visualization = create_full_visualization(@user)
+    end
+
+    after(:all) do
+      destroy_full_visualization(@map, @table, @table_visualization, @visualization)
+      # This avoids connection leaking.
+      ::User[@user.id].destroy
+      ::User[@user2.id].destroy
+    end
+
+    it 'returns an existing map' do
       get_json create_show_map_url do |response|
         response.status.should be_success
         response.body[:id].should eq @map.id

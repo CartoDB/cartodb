a0bffd856728c2619ec06ce00636684e1c75a5b6
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-04 16:15:14 +0200

    Migration for 9926

---
+++ db/migrate/20160928095559_add_embed_options_to_map.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    alter_table :maps do
+      add_column :embed_options,
+                 :string,
+                 type: 'json'
+    end
+  end
+
+  down do
+    alter_table :maps do
+      drop_column :embed_options
+    end
+  end
+end

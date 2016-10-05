a8cca02209220fdc0db6c3125072e783794b4eda
xavijam <xavijam@gmail.com>
2016-10-05 13:29:07 +0200

    Applying CR suggestions, :kiss: @javierarce

--- src/widgets/auto-style/category.js
+++ src/widgets/auto-style/category.js
@@ -15,25 +15,18 @@ module.exports = AutoStyler.extend({
     var cats = this.dataviewModel.get('data');
     var column = this.dataviewModel.get('column');
     var ramp = ['ramp([' + column + ']'];
+    var catListColors = cats.map(function (cat, i) {
+      return this.colors.getColorByCategory(cat.name);
+    }.bind(this)).join(', ');
+    var catListValues = _.reduce(cats, function (memo, cat, i) {
+      if (!cat.agg) {
+        memo.push('\'' + cat.name.replace(/'/g, '\\\'') + '\'');
+      }
+      return memo;
+    }, []).join(', ');
 
-    ramp.push(
-      '(' +
-      cats.map(function (cat, i) {
-        return this.colors.getColorByCategory(cat.name);
-      }.bind(this)).join(', ') +
-      ')'
-    );
-
-    ramp.push(
-      '(' +
-      _.reduce(cats, function (memo, cat, i) {
-        if (!cat.agg) {
-          memo.push('\'' + cat.name.replace(/'/g, '\\\'') + '\'');
-        }
-        return memo;
-      }, []).join(', ') +
-      ')'
-    );
+    ramp.push('(' + catListColors + ')');
+    ramp.push('(' + catListValues + ')');
 
     return sym + ': ' + ramp.join(', ') + ');';
   }

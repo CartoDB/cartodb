84bfe7e0f6d1d51baed7382775956c453f5066cb
xavijam <xavijam@gmail.com>
2016-10-05 12:05:07 +0200

    Using proper colors instead of the default one

--- spec/widgets/auto-style/category.spec.js
+++ spec/widgets/auto-style/category.spec.js
@@ -1,4 +1,5 @@
 var specHelper = require('../../spec-helper');
+var _ = require('underscore');
 var CategoryAutoStyler = require('../../../src/widgets/auto-style/category.js');
 var Backbone = require('backbone');
 
@@ -16,24 +17,32 @@ describe('src/widgets/auto-style/category', function () {
       ],
       column: 'something'
     });
+
     this.layer = this.dataview.layer = layer;
     this.categoryAutoStyler = new CategoryAutoStyler(this.dataview);
+
+    // Trick to set colors properly in this test
+    this.updateColorsByData = function () {
+      this.categoryAutoStyler.colors.updateData(_.pluck(this.dataview.get('data'), 'name'));
+    }.bind(this);
+
+    this.updateColorsByData();
   });
 
   describe('.getStyle', function () {
     it('should generate the right styles when layer has polygons', function () {
       this.dataview.layer.set('initialStyle', '#layer {  polygon-line-width: 0.5;  polygon-line-color: #fcfafa;  polygon-line-opacity: 1;  polygon-fill: #e49115;  polygon-fill-opacity: 0.9; }');
-      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  polygon-line-width: 0.5;  polygon-line-color: #fcfafa;  polygon-line-opacity: 1; polygon-fill: ramp([something], (#A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  polygon-fill-opacity: 0.9; }');
+      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  polygon-line-width: 0.5;  polygon-line-color: #fcfafa;  polygon-line-opacity: 1; polygon-fill: ramp([something], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  polygon-fill-opacity: 0.9; }');
     });
 
     it('should generate the right styles when layer has points', function () {
       this.layer.set('initialStyle', '#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077;  marker-fill: #e49115;  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
-      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
+      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
     });
 
     it('should generate the right styles when layer has lines', function () {
       this.dataview.layer.set('initialStyle', '#layer {  line-width: 0.5;  line-color: #fcfafa;  line-opacity: 1; }');
-      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  line-width: 0.5; line-color: ramp([something], (#A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  line-opacity: 1; }');
+      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  line-width: 0.5; line-color: ramp([something], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  line-opacity: 1; }');
     });
 
     it('should escape single quotes correctly', function () {
@@ -41,7 +50,8 @@ describe('src/widgets/auto-style/category', function () {
       data.push({ name: "Oh'Yeah" }); // eslint-disable-line 
       this.dataview.set('data', data);
       this.layer.set('initialStyle', '#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077;  marker-fill: #e49115;  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
-      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\', \'Oh\\\'Yeah\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
+      this.updateColorsByData();
+      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\', \'Oh\\\'Yeah\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
     });
 
     it('should generate proper CartoCSS when Others is included', function () {
@@ -49,7 +59,8 @@ describe('src/widgets/auto-style/category', function () {
       data.push({ name: 'Others', agg: true });
       this.dataview.set('data', data);
       this.layer.set('initialStyle', '#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077;  marker-fill: #e49115;  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
-      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
+      this.updateColorsByData();
+      expect(this.categoryAutoStyler.getStyle()).toBe('#layer {  marker-line-width: 0.5;  marker-line-color: #fcfafa;  marker-line-opacity: 1;  marker-width: 6.076923076923077; marker-fill: ramp([something], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74, #A5AA99), (\'soccer\', \'basketball\', \'baseball\', \'handball\', \'hockey\'));  marker-fill-opacity: 0.9;  marker-allow-overlap: true;}');
     });
   });
 });

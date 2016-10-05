7d4308405fb256a497a4ee08879763eb729f5284
Javier Torres <jtorres@carto.com>
2016-10-05 10:21:55 +0200

    Hound

--- app/models/user/user_decorator.rb
+++ app/models/user/user_decorator.rb
@@ -4,50 +4,50 @@ module CartoDB
     # - show_api_calls: load api calls. Default: true.
     # - extended: load real_table_count and last_active_time. Default: false.
     def data(options = {})
-      calls = options.fetch(:show_api_calls, true) ? self.get_api_calls(from: self.last_billing_cycle, to: Date.today) : []
+      calls = options.fetch(:show_api_calls, true) ? get_api_calls(from: last_billing_cycle, to: Date.today) : []
       calls.fill(0, calls.size..29)
 
       db_size_in_bytes = self.db_size_in_bytes
 
       data = {
-        id: self.id,
-        email: self.email,
-        name: self.name,
-        username: self.username,
-        account_type: self.account_type,
-        table_quota: self.table_quota,
-        table_count: self.table_count,
-        public_visualization_count: self.public_visualization_count,
-        all_visualization_count: self.all_visualization_count,
-        visualization_count: self.visualization_count,
-        owned_visualization_count: self.owned_visualizations_count,
-        failed_import_count: self.failed_import_count,
-        success_import_count: self.success_import_count,
-        import_count: self.import_count,
-        last_visualization_created_at: self.last_visualization_created_at,
-        quota_in_bytes: self.quota_in_bytes,
+        id: id,
+        email: email,
+        name: name,
+        username: username,
+        account_type: account_type,
+        table_quota: table_quota,
+        table_count: table_count,
+        public_visualization_count: public_visualization_count,
+        all_visualization_count: all_visualization_count,
+        visualization_count: visualization_count,
+        owned_visualization_count: owned_visualizations_count,
+        failed_import_count: failed_import_count,
+        success_import_count: success_import_count,
+        import_count: import_count,
+        last_visualization_created_at: last_visualization_created_at,
+        quota_in_bytes: quota_in_bytes,
         db_size_in_bytes: db_size_in_bytes,
         db_size_in_megabytes: db_size_in_bytes.present? ? (db_size_in_bytes / (1024.0 * 1024.0)).round(2) : nil,
-        remaining_table_quota: self.remaining_table_quota,
-        remaining_byte_quota: self.remaining_quota(false, db_size_in_bytes).to_f,
+        remaining_table_quota: remaining_table_quota,
+        remaining_byte_quota: remaining_quota(false, db_size_in_bytes).to_f,
         api_calls: calls,
-        api_calls_quota: self.organization_user? ? self.organization.map_view_quota : self.map_view_quota,
-        api_calls_block_price: self.organization_user? ? self.organization.map_view_block_price : self.map_view_block_price,
+        api_calls_quota: organization_user? ? organization.map_view_quota : map_view_quota,
+        api_calls_block_price: organization_user? ? organization.map_view_block_price : map_view_block_price,
         geocoding: {
-          quota:       self.organization_user? ? self.organization.geocoding_quota : self.geocoding_quota,
-          block_price: self.organization_user? ? self.organization.geocoding_block_price : self.geocoding_block_price,
-          monthly_use: self.organization_user? ? self.organization.get_geocoding_calls : self.get_geocoding_calls,
-          hard_limit:  self.hard_geocoding_limit?
+          quota:       organization_user? ? organization.geocoding_quota : geocoding_quota,
+          block_price: organization_user? ? organization.geocoding_block_price : geocoding_block_price,
+          monthly_use: organization_user? ? organization.get_geocoding_calls : get_geocoding_calls,
+          hard_limit:  hard_geocoding_limit?
         },
         here_isolines: {
-          quota:       self.organization_user? ? self.organization.here_isolines_quota : self.here_isolines_quota,
-          block_price: self.organization_user? ? self.organization.here_isolines_block_price : self.here_isolines_block_price,
-          monthly_use: self.organization_user? ? self.organization.get_here_isolines_calls : self.get_here_isolines_calls,
-          hard_limit:  self.hard_here_isolines_limit?
+          quota:       organization_user? ? organization.here_isolines_quota : here_isolines_quota,
+          block_price: organization_user? ? organization.here_isolines_block_price : here_isolines_block_price,
+          monthly_use: organization_user? ? organization.get_here_isolines_calls : get_here_isolines_calls,
+          hard_limit:  hard_here_isolines_limit?
         },
-        geocoder_provider: self.geocoder_provider,
-        isolines_provider: self.isolines_provider,
-        routing_provider: self.routing_provider,
+        geocoder_provider: geocoder_provider,
+        isolines_provider: isolines_provider,
+        routing_provider: routing_provider,
         obs_snapshot: {
           quota:       organization_user? ? organization.obs_snapshot_quota : obs_snapshot_quota,
           block_price: organization_user? ? organization.obs_snapshot_block_price : obs_snapshot_block_price,
@@ -61,61 +61,61 @@ module CartoDB
           hard_limit:  hard_obs_general_limit?
         },
         twitter: {
-          enabled:     self.organization_user? ? self.organization.twitter_datasource_enabled         : self.twitter_datasource_enabled,
-          quota:       self.organization_user? ? self.organization.twitter_datasource_quota           :  self.twitter_datasource_quota,
-          block_price: self.organization_user? ? self.organization.twitter_datasource_block_price     : self.twitter_datasource_block_price,
-          block_size:  self.organization_user? ? self.organization.twitter_datasource_block_size      : self.twitter_datasource_block_size,
-          monthly_use: self.organization_user? ? self.organization.get_twitter_imports_count          : self.get_twitter_imports_count,
-          hard_limit:  self.hard_twitter_datasource_limit
+          enabled:     organization_user? ? organization.twitter_datasource_enabled         : twitter_datasource_enabled,
+          quota:       organization_user? ? organization.twitter_datasource_quota           :  twitter_datasource_quota,
+          block_price: organization_user? ? organization.twitter_datasource_block_price     : twitter_datasource_block_price,
+          block_size:  organization_user? ? organization.twitter_datasource_block_size      : twitter_datasource_block_size,
+          monthly_use: organization_user? ? organization.get_twitter_imports_count          : get_twitter_imports_count,
+          hard_limit:  hard_twitter_datasource_limit
         },
         salesforce: {
           enabled: organization_user? ? organization.salesforce_datasource_enabled : salesforce_datasource_enabled
         },
-        billing_period: self.last_billing_cycle,
-        api_key: self.api_key,
-        layers: self.layers.map(&:public_values),
-        trial_ends_at: self.trial_ends_at,
-        upgraded_at: self.upgraded_at,
-        show_trial_reminder: self.trial_ends_at.present?,
-        show_upgraded_message: (self.account_type.downcase != 'free' && self.upgraded_at && self.upgraded_at + 15.days > Date.today ? true : false),
+        billing_period: last_billing_cycle,
+        api_key: api_key,
+        layers: layers.map(&:public_values),
+        trial_ends_at: trial_ends_at,
+        upgraded_at: upgraded_at,
+        show_trial_reminder: trial_ends_at.present?,
+        show_upgraded_message: (account_type.downcase != 'free' && upgraded_at && upgraded_at + 15.days > Date.today ? true : false),
         actions: {
-          private_tables: self.private_tables_enabled,
-          private_maps: self.private_maps_enabled?,
-          dedicated_support: self.dedicated_support?,
-          remove_logo: self.remove_logo?,
-          sync_tables: self.sync_tables_enabled,
-          google_maps_geocoder_enabled: self.google_maps_geocoder_enabled?,
-          google_maps_enabled: self.google_maps_enabled?
+          private_tables: private_tables_enabled,
+          private_maps: private_maps_enabled?,
+          dedicated_support: dedicated_support?,
+          remove_logo: remove_logo?,
+          sync_tables: sync_tables_enabled,
+          google_maps_geocoder_enabled: google_maps_geocoder_enabled?,
+          google_maps_enabled: google_maps_enabled?
         },
         limits: {
           concurrent_syncs: CartoDB::PlatformLimits::Importer::UserConcurrentSyncsAmount::MAX_SYNCS_PER_USER,
-          concurrent_imports: self.max_concurrent_import_count,
-          import_file_size: self.max_import_file_size,
-          import_table_rows: self.max_import_table_row_count,
-          max_layers: self.max_layers
+          concurrent_imports: max_concurrent_import_count,
+          import_file_size: max_import_file_size,
+          import_table_rows: max_import_table_row_count,
+          max_layers: max_layers
         },
-        notification: self.notification,
-        avatar_url: self.avatar,
-        feature_flags: self.feature_flags,
-        base_url: self.public_url,
-        needs_password_confirmation: self.needs_password_confirmation?,
+        notification: notification,
+        avatar_url: avatar,
+        feature_flags: feature_flags,
+        base_url: public_url,
+        needs_password_confirmation: needs_password_confirmation?,
         viewer: viewer
       }
 
-      if self.organization.present?
-        data[:organization] = self.organization.to_poro
-        data[:organization][:available_quota_for_user] = self.organization.unassigned_quota + self.quota_in_bytes
+      if organization.present?
+        data[:organization] = organization.to_poro
+        data[:organization][:available_quota_for_user] = organization.unassigned_quota + quota_in_bytes
       end
 
       if !groups.nil?
-        data[:groups] = self.groups.map { |g| Carto::Api::GroupPresenter.new(g).to_poro }
+        data[:groups] = groups.map { |g| Carto::Api::GroupPresenter.new(g).to_poro }
       end
 
       if options[:extended]
-        data.merge({
-          :real_table_count => self.real_tables.size,
-          :last_active_time => self.get_last_active_time
-        })
+        data.merge(
+          real_table_count: real_tables.size,
+          last_active_time: get_last_active_time
+        )
       else
         data
       end

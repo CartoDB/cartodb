69d042fc353a9d8756a9c6bc7207e831a8b07b6a
Javier Torres <jtorres@carto.com>
2016-10-11 09:52:11 +0200

    Add comment

--- db/migrate/20161011074820_add_index_to_database_hosts.rb
+++ db/migrate/20161011074820_add_index_to_database_hosts.rb
@@ -1,6 +1,7 @@
 Sequel.migration do
   no_transaction # indexes need to be created outside of the transaction
 
+  # This index is used for metrics gathering per database host. See #10115
   up do
     Rails::Sequel.connection.run %{
       CREATE INDEX CONCURRENTLY "users_database_host_index" ON "users" ("database_host");

dcac0f83e2e4cbf49d7be07c8506c052b090bc74
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-13 15:10:58 +0200

    make NAMES_CLASSES_MAP a constant

--- lib/carto/styles/geometry.rb
+++ lib/carto/styles/geometry.rb
@@ -12,15 +12,15 @@ module Carto::Styles
       ['geometry', 'st_geometry']
     end
 
-    def to_cartocss
-      names_classes_map = {
-        "layer['mapnik::geometry_type' = 1]" => Carto::Styles::Point,
-        "layer['mapnik::geometry_type' = 2]" => Carto::Styles::Line,
-        "layer['mapnik::geometry_type' = 3]" => Carto::Styles::Polygon
-      }
+    NAMES_CLASSES_MAP = {
+      "layer['mapnik::geometry_type' = 1]" => Carto::Styles::Point,
+      "layer['mapnik::geometry_type' = 2]" => Carto::Styles::Line,
+      "layer['mapnik::geometry_type' = 3]" => Carto::Styles::Polygon
+    }.freeze
 
-      cartocss_classes = names_classes_map.keys.map do |class_name|
-        cartocss_array = names_classes_map[class_name].new.to_cartocss_array
+    def to_cartocss
+      cartocss_classes = NAMES_CLASSES_MAP.keys.map do |class_name|
+        cartocss_array = NAMES_CLASSES_MAP[class_name].new.to_cartocss_array
         Carto::Styles::Presenters::CartoCSS.new(cartocss_array: cartocss_array,
                                                 class_name: class_name)
                                            .to_s + "\n"

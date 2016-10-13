0092ed03f980c42d323749e4450eefbeb132a254
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-13 18:14:43 +0200

    update specs

--- spec/lib/carto/styles/geometry_spec.rb
+++ spec/lib/carto/styles/geometry_spec.rb
@@ -11,24 +11,24 @@ module Carto
           "  marker-width: 7;\n"\
           "  marker-fill: #FFB927;\n"\
           "  marker-fill-opacity: 0.9;\n"\
-          "  marker-line-width: 1;\n"\
           "  marker-line-color: #FFF;\n"\
+          "  marker-line-width: 1;\n"\
           "  marker-line-opacity: 1;\n"\
           "  marker-placement: point;\n"\
           "  marker-type: ellipse;\n"\
           "  marker-allow-overlap: true;\n"\
           "}\n"\
           "#layer['mapnik::geometry_type'=2] {\n"\
-          "  line-width: 1.5;\n"\
           "  line-color: #3EBCAE;\n"\
+          "  line-width: 1.5;\n"\
           "  line-opacity: 1;\n"\
           "}\n"\
           "#layer['mapnik::geometry_type'=3] {\n"\
           "  polygon-fill: #374C70;\n"\
           "  polygon-opacity: 0.9;\n"\
           "  polygon-gamma: 0.5;\n"\
-          "  line-width: 1;\n"\
           "  line-color: #FFF;\n"\
+          "  line-width: 1;\n"\
           "  line-opacity: 0.5;\n"\
           "  line-comp-op: soft-light;\n"\
           "}"

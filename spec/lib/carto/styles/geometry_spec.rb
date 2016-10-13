d101628d8b6fa8d287f756c2156b065c8b103a38
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-13 16:49:05 +0200

    update tests

--- spec/lib/carto/styles/geometry_spec.rb
+++ spec/lib/carto/styles/geometry_spec.rb
@@ -7,7 +7,7 @@ module Carto
     describe Geometry do
       describe '#default' do
         let(:prod_default_geometry_cartocss) do
-          "#layer['mapnik::geometry_type' = 1] {\n"\
+          "#layer['mapnik::geometry_type'=1] {\n"\
           "  marker-width: 7;\n"\
           "  marker-fill: #FFB927;\n"\
           "  marker-fill-opacity: 0.9;\n"\
@@ -18,12 +18,12 @@ module Carto
           "  marker-type: ellipse;\n"\
           "  marker-allow-overlap: true;\n"\
           "}\n"\
-          "#layer['mapnik::geometry_type' = 2] {\n"\
+          "#layer['mapnik::geometry_type'=2] {\n"\
           "  line-width: 1.5;\n"\
           "  line-color: #3EBCAE;\n"\
           "  line-opacity: 1;\n"\
           "}\n"\
-          "#layer['mapnik::geometry_type' = 3] {\n"\
+          "#layer['mapnik::geometry_type'=3] {\n"\
           "  polygon-fill: #374C70;\n"\
           "  polygon-opacity: 0.9;\n"\
           "  polygon-gamma: 0.5;\n"\

e72c52ac46f0b250243caee980751d1218f24b8f
Javier Torres <jtorres@carto.com>
2016-10-14 11:04:52 +0200

    Migration to add builder_enabled to organizations

---
+++ db/migrate/20161014085122_add_builder_enabled_to_organizations.rb
@@ -0,0 +1,24 @@
+Sequel.migration do
+  up do
+    alter_table :organizations do
+      add_column :builder_enabled, :boolean, null: false, default: false
+    end
+
+    alter_table :users do
+      Rails::Sequel.connection.run('UPDATE users SET builder_enabled = true WHERE builder_enabled IS NULL')
+      set_column_default :builder_enabled, false
+      set_column_not_null :builder_enabled
+    end
+  end
+
+  down do
+    alter_table :organizations do
+      drop_column :builder_enabled
+    end
+
+    alter_table :users do
+      set_column_allow_null :builder_enabled
+      set_column_default :builder_enabled, true
+    end
+  end
+end

203472068907e30611190255cf19b9045eaa7582
Rafa de la Torre <rtorre@cartodb.com>
2016-10-14 10:40:08 +0200

    Remove default value and NOT NULL constraints #10110

--- db/migrate/20161011162345_mapzen_routing_quota.rb
+++ db/migrate/20161011162345_mapzen_routing_quota.rb
@@ -1,9 +1,9 @@
 Sequel.migration do
   up do
-    add_column :users, :mapzen_routing_quota, :integer, null: false, default: 0
+    add_column :users, :mapzen_routing_quota, :integer
     add_column :users, :mapzen_routing_block_price, :integer
     add_column :users, :soft_mapzen_routing_limit, :boolean
-    add_column :organizations, :mapzen_routing_quota, :integer, null: false, default: 0
+    add_column :organizations, :mapzen_routing_quota, :integer
     add_column :organizations, :mapzen_routing_block_price, :integer
     add_column :user_creations, :soft_mapzen_routing_limit, :boolean
   end

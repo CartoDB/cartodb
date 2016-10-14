819077f8f6536687e4f47662b504a3ac0fcff55c
Javier Torres <jtorres@carto.com>
2016-10-14 18:48:00 +0200

    Fix redirection tests

--- spec/requests/carto/builder/datasets_controller_spec.rb
+++ spec/requests/carto/builder/datasets_controller_spec.rb
@@ -14,7 +14,6 @@ describe Carto::Builder::DatasetsController do
     end
 
     before(:each) do
-      @user.stubs(:has_feature_flag?).with('editor-3').returns(true)
       @user.stubs(:has_feature_flag?).with('new_geocoder_quota').returns(true)
       login(@user)
     end
@@ -28,21 +27,12 @@ describe Carto::Builder::DatasetsController do
     end
 
     it 'redirects to public view non-builder users requests' do
-      @user.stubs(:has_feature_flag?).with('editor-3').returns(false)
-
-      get builder_dataset_url(id: @visualization.id)
-
-      response.status.should eq 302
-      response.location.should end_with public_table_map_path(id: @visualization.id)
-    end
-
-    it 'redirects to editor if disabled' do
       @user.stubs(:builder_enabled).returns(false)
 
       get builder_dataset_url(id: @visualization.id)
 
       response.status.should eq 302
-      response.location.should include '/tables/' + @visualization.id
+      response.location.should end_with public_table_map_path(id: @visualization.id)
     end
 
     it 'returns 404 for non-existent visualizations' do

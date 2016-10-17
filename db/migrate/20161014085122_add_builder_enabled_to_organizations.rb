977ab27a3f9d3236d1eb821c27f8b0a6a26c0864
Javier Torres <jtorres@carto.com>
2016-10-17 12:02:34 +0200

    Change migration, allow users nulls (inherit from org)

--- db/migrate/20161014085122_add_builder_enabled_to_organizations.rb
+++ db/migrate/20161014085122_add_builder_enabled_to_organizations.rb
@@ -4,10 +4,8 @@ Sequel.migration do
       add_column :builder_enabled, :boolean, null: false, default: false
     end
 
-    Rails::Sequel.connection.run('UPDATE users SET builder_enabled = true WHERE builder_enabled IS NULL')
     alter_table :users do
       set_column_default :builder_enabled, false
-      set_column_not_null :builder_enabled
     end
   end
 
@@ -17,7 +15,6 @@ Sequel.migration do
     end
 
     alter_table :users do
-      set_column_allow_null :builder_enabled
       set_column_default :builder_enabled, true
     end
   end

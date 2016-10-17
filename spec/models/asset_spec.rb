74e021f7449414cbb190495acf75784ffdc8500b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-10-17 09:02:33 +0200

    Fixes assets test paths #10136

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -59,6 +59,11 @@ describe Asset do
     end
   end
 
+  def local_path(asset)
+    local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}\/uploads/,'')
+    Carto::Conf.new.public_uploaded_assets_path + local_url
+  end
+
   describe '#create' do
     describe 'on local filesystem' do
       before(:each) do
@@ -67,9 +72,7 @@ describe Asset do
 
       it 'should save the file when passing a full path as an argument' do
         asset = Asset.create user_id: @user.id, asset_file: (Rails.root + 'spec/support/data/cartofante_blue.png').to_s
-        local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}/,'')
-        path = Carto::Conf.new.public_uploads_path + local_url
-        File.exists?(path).should be_true
+        File.exists?(local_path(asset)).should be_true
         asset.public_url.should =~ /.*test\/#{@user.username}\/assets\/\d+cartofante_blue\.png.*/
       end
 
@@ -79,9 +82,8 @@ describe Asset do
         asset = Asset.create(
           user_id: @user.id,
           asset_file: Rack::Test::UploadedFile.new(file_path, 'image/png'))
-        local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}/, '')
-        path = Carto::Conf.new.public_uploads_path + local_url
-        File.exists?(path).should be_true
+        local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}\/uploads/, '')
+        File.exists?(local_path(asset)).should be_true
         asset.public_url.should =~ /.*test\/#{@user.username}\/assets\/\d+cartofante_blue.png.*/
       end
 
@@ -97,9 +99,7 @@ describe Asset do
         file = Rails.root.join('spec/support/data/cartofante_blue.png')
         serve_file file do |url|
           asset = Asset.create(user_id: @user.id, url: url)
-          local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}/,'')
-          path = Carto::Conf.new.public_uploads_path + local_url
-          File.exists?(path).should be_true
+          File.exists?(local_path(asset)).should be_true
           asset.public_url.should =~ /\/test\/test\/assets\/\d+cartofante_blue\.png/
         end
       end
@@ -111,8 +111,7 @@ describe Asset do
     it 'removes the file from storage if needed' do
       Asset.any_instance.stubs("use_s3?").returns(false)
       asset = Asset.create user_id: @user.id, asset_file: (Rails.root + 'spec/support/data/cartofante_blue.png').to_s
-      local_url = asset.public_url.gsub(/http:\/\/#{CartoDB.account_host}/,'')
-      path = Carto::Conf.new.public_uploads_path + local_url
+      path = local_path(asset)
       File.exists?(path).should be_true
       asset.destroy
       File.exists?(path).should be_false

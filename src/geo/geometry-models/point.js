9e1965bf3884abf0f1526ceb994e795b529678c7
Pablo Alonso Garcia <pablo@cartodb.com>
2016-10-18 18:45:16 +0200

    Use base64 encoded image url for marker points

--- src/geo/geometry-models/point.js
+++ src/geo/geometry-models/point.js
@@ -4,7 +4,7 @@ var Point = GeometryBase.extend({
   defaults: {
     type: 'point',
     editable: false,
-    iconUrl: '/themes/img/default-marker-icon.png',
+    iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWBAMAAAA2mnEIAAAAKlBMVEUAAAD////////////////////////////////e6fPO3+5zo85Cg705fbrPhX3RAAAADXRSTlMAADVBRn6fqLO9weP7Sr4MvgAAAHRJREFUGNNjEEQABhQ2AwNLeEepAwOYzZTR0dHRpgBmW3SAQDOIzdTRuefu6RkdikA2W8eau3fvnupIBLI9Os8C2XdmtADZEd13QWBHK5Bd0QNmn2gHsjt6wewbHWhsZDXIepHNRLYL2Q3IbkNxM5JfsPsXACkGXYWXfeLsAAAAAElFTkSuQmCC',
     iconAnchor: [ 11, 11 ]
   },
 

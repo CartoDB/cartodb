2aa702c76007fbc2f730732a89a69e0c3ab0b36c
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-10-18 09:12:19 +0200

    require_dependency breaks migrations #10201

--- lib/carto/styles/point.rb
+++ lib/carto/styles/point.rb
@@ -1,7 +1,7 @@
 # encoding utf-8
 
-require_dependency 'carto/styles/style.rb'
-require_dependency 'carto/definition.rb'
+require_relative './style.rb'
+require_relative '../definition.rb'
 
 module Carto::Styles
   class Point < Style

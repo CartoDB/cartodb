dc4cc41ba12a34499a0cfc6179443aa5c8d6518e
nobuti <buti@nobuti.com>
2016-10-25 17:00:59 +0200

    Comment cleanned.

--- src/dataviews/dataviews-collection.js
+++ src/dataviews/dataviews-collection.js
@@ -1,26 +1,7 @@
 var _ = require('underscore');
 var Backbone = require('backbone');
 
-// var REQUIRED_OPTS = [
-//   'vis'
-// ];
-
 var DataviewsCollection = Backbone.Collection.extend({
-  // initialize: function (models, options) {
-  //   _.each(REQUIRED_OPTS, function (item) {
-  //     if (options[item] === undefined) throw new Error(item + ' is required');
-  //     this['_' + item] = options[item];
-  //   }, this);
-
-  //   this._tracking = false;
-  //   this._initBinds();
-  // },
-
-  // track: function () {
-  //   this._tracking = true;
-  //   this._check();
-  // },
-
   isAnalysisLinkedToDataview: function (analysisModel) {
     return this.any(function (dataviewModel) {
       var sourceId = dataviewModel.getSourceId();
@@ -45,22 +26,6 @@ var DataviewsCollection = Backbone.Collection.extend({
       return filters;
     }, {});
   }
-
-  // _initBinds: function () {
-  //   this.on('change:status', this._check, this);
-  // },
-
-  // _check: function () {
-  //   if (this._tracking === false) return;
-
-  //   var allFetched = this.every(function (dataviewModel) {
-  //     return dataviewModel.isUnavailable() || dataviewModel.isFetched();
-  //   });
-
-  //   if (allFetched) {
-  //     this._vis.trigger('dataviewsFetched');
-  //   }
-  // }
 });
 
 module.exports = DataviewsCollection;

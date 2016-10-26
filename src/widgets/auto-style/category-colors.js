bba670f799a69d3da1d97651928457526e6d5e8f
Francisco López <donflopez@gmail.com>
2016-10-26 11:10:23 +0200

    Check styles var #470

--- src/widgets/auto-style/category-colors.js
+++ src/widgets/auto-style/category-colors.js
@@ -8,7 +8,7 @@ var colorScales = [
 ]; // Demo colors
 
 function getColorRange (styles) {
-  styles = styles.auto_style || styles;
+  styles = styles ? styles.auto_style : styles;
 
   var colorRange = styles && styles.definition &&
       styles.definition.color &&

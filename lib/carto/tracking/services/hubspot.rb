3d4abef17a963444dcc93df73b6441362b4b00cc
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-26 15:38:10 +0200

    add enqueing logic to hubspot service

--- lib/carto/tracking/services/hubspot.rb
+++ lib/carto/tracking/services/hubspot.rb
@@ -1,8 +1,16 @@
+require 'hubspot/events_api'
+
 module Carto
   module Tracking
     module Services
       module Hubspot
         def report_to_hubspot
+          return unless ::Hubspot::EventsAPI.instance.enabled?
+
+          hubspot_job = Resque::TrackingJobs::SendHubspotEvent
+          supplied_properties = @format.to_hubspot
+
+          Resque.enqueue(hubspot_job, id, supplied_properties)
         end
       end
     end

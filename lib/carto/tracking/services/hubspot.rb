257b615b8c7fe0b7dc06fd22a060676f9ecdb4e4
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-26 16:52:55 +0200

    add event mapping to app_config.yml

--- lib/carto/tracking/services/hubspot.rb
+++ lib/carto/tracking/services/hubspot.rb
@@ -9,8 +9,15 @@ module Carto
 
           hubspot_job = Resque::TrackingJobs::SendHubspotEvent
           supplied_properties = @format.to_hubspot
+          event_name = name.downcase.tr(' ', '_')
+          id = Cartodb.get_config(:metrics, 'hubspot', 'event_ids')[event_name]
 
-          Resque.enqueue(hubspot_job, id, supplied_properties)
+          if id.present?
+            Resque.enqueue(hubspot_job, id, supplied_properties)
+          else
+            message = 'Carto::Tracking: Hubspot event id not configured'
+            CartoDB::Logger.error(message: message, event_name: event_name)
+          end
         end
       end
     end

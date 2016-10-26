2b25d106629dc81b35165ee68e8bf980e8930380
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-26 12:45:44 +0200

    add Hubspot format Skelleton

---
+++ lib/carto/tracking/formats/hubspot.rb
@@ -0,0 +1,16 @@
+# encoding utf-8
+
+module Carto
+  module Tracking
+    module Formats
+      class Hubspot
+        def initialize
+        end
+
+        def to_hash
+          {}
+        end
+      end
+    end
+  end
+end

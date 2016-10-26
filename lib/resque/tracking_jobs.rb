23c3bfca4f559f5b74fcce06721b5229fd55212b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-10-26 15:37:00 +0200

    avoid assigments in conditional

--- lib/resque/tracking_jobs.rb
+++ lib/resque/tracking_jobs.rb
@@ -8,7 +8,8 @@ module Resque
       @queue = :tracker
 
       def self.perform(user_id, name, properties)
-        return unless segment_api_key = Cartodb.get_config(:segment, 'api_key')
+        segment_api_key = Cartodb.get_config(:segment, 'api_key')
+        return unless segment_api_key
 
         segment = Segment::Analytics.new(write_key: segment_api_key)
         segment.track(user_id: user_id, event: name, properties: properties)

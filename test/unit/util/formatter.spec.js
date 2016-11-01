212b9039807eaf95d35dfda4d102201f936e214f
nobuti <buti@nobuti.com>
2016-11-01 15:31:17 +0100

    Fix tests.

--- test/unit/util/formatter.spec.js
+++ test/unit/util/formatter.spec.js
@@ -3,7 +3,6 @@ var formatter = require('../../../src/util/formatter');
 describe('src/util/formatter', function () {
   it('should format numbers', function () {
     expect(formatter.formatNumber(0)).toBe(0);
-    expect(formatter.formatNumber(0.001)).toBe('0');
     expect(formatter.formatNumber(0.071)).toBe('0.07');
     expect(formatter.formatNumber(0.71)).toBe('0.71');
     expect(formatter.formatNumber(-0.71)).toBe('-0.71');
@@ -16,12 +15,10 @@ describe('src/util/formatter', function () {
     expect(formatter.formatNumber(96.7)).toBe('96.7');
     expect(formatter.formatNumber(500)).toBe('500');
     expect(formatter.formatNumber(1234)).toBe('1.2k');
-    expect(formatter.formatNumber(0.0071)).toBe('0.01');
   });
 
   it('should format strings', function () {
     expect(formatter.formatNumber('0')).toBe('0');
-    expect(formatter.formatNumber('0.001')).toBe('0');
     expect(formatter.formatNumber('0.071')).toBe('0.07');
     expect(formatter.formatNumber('0.71')).toBe('0.71');
     expect(formatter.formatNumber('-0.71')).toBe('-0.71');
@@ -34,11 +31,12 @@ describe('src/util/formatter', function () {
     expect(formatter.formatNumber('96.7')).toBe('96.7');
     expect(formatter.formatNumber('500')).toBe('500');
     expect(formatter.formatNumber('1234')).toBe('1.2k');
-    expect(formatter.formatNumber('0.0071')).toBe('0.01');
   });
 
   it('should format exponentials', function () {
     expect(formatter.formatNumber(0.0001)).toBe('1.0<span class="Legend-exponential">x</span>10<sup class="Legend-exponential">-4</sup>');
+    expect(formatter.formatNumber(0.001)).toBe('1.0<span class="Legend-exponential">x</span>10<sup class="Legend-exponential">-3</sup>');
+    expect(formatter.formatNumber(0.0071)).toBe('7.1<span class="Legend-exponential">x</span>10<sup class="Legend-exponential">-3</sup>');
     expect(formatter.formatNumber(7.1e-5)).toBe('7.1<span class="Legend-exponential">x</span>10<sup class="Legend-exponential">-5</sup>');
   });
 

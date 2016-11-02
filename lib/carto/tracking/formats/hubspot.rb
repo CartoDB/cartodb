c3b8366b9a6f1cfe5fead54bcdb6fa4edf737bbe
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-02 16:34:25 +0100

    only send email

--- lib/carto/tracking/formats/hubspot.rb
+++ lib/carto/tracking/formats/hubspot.rb
@@ -4,43 +4,12 @@ module Carto
   module Tracking
     module Formats
       class Hubspot
-        def initialize(user: nil, visualization: nil, hash: {})
-          @user = user
-          @visualization = visualization
-          @hash = hash
+        def initialize(email: nil)
+          @email = email
         end
 
         def to_hash
-          properties_hash = @hash
-
-          properties_hash.merge(user_properties)
-          properties_hash.merge(visualization_properties)
-
-          properties_hash
-        end
-
-        private
-
-        def user_properties
-          return {} unless @user
-
-          user_organization = @user.organization ? @user.organization.name : ''
-
-          {
-            user_username: @user.username,
-            user_email: @user.email,
-            user_organization: user_organization,
-            user_account_type: @user.account_type
-          }
-        end
-
-        def visualization_properties
-          return {} unless @visualization
-
-          {
-            visualization_id: @visualization.id,
-            visualization_created_at: @visualization.created_at
-          }
+          @email ? { email: @email } : {}
         end
       end
     end

6b6f586479f338f9c30280eb8b6c93445a92d7d9
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-02 17:26:04 +0100

    remove unused method

--- lib/hubspot/events_api.rb
+++ lib/hubspot/events_api.rb
@@ -24,14 +24,6 @@ module Hubspot
 
     private
 
-    def serialize_params(params)
-      parse_params_array = params.keys.map do |key|
-        "#{key}=#{params[key]}"
-      end
-
-      parse_params_array.join('&')
-    end
-
     def base_url
       @base_url ||= Cartodb.get_config(:metrics, 'hubspot', 'events_host')
     end

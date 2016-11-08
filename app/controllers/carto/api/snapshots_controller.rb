206f7b49900a1aa4699224edc86b3604813ad7bd
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-08 17:24:28 +0100

    add load visualization code to controller

--- app/controllers/carto/api/snapshots_controller.rb
+++ app/controllers/carto/api/snapshots_controller.rb
@@ -2,5 +2,16 @@
 
 module Carto
   class SnapshotsController < ::Api::ApplicationController
+    before_filter :load_visualization
+
+    rescue_from Carto::LoadError, with: :rescue_from_carto_error
+
+    private
+
+    def load_visualization
+      @visualization = Carto::visualization.find(params[:visualization_id])
+    rescue ActiveRecord::RecordNotFound
+      raise Carto::LoadError.new('Visualization not found')
+    end
   end
 end

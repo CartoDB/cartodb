af9a2dfb341e4e08ce82ee104bd6345b92a5d36e
nobuti <buti@nobuti.com>
2016-11-11 10:28:18 +0100

    Insert a superfluous css rule.

--- src/geo/ui/legends/html/legend-view.js
+++ src/geo/ui/legends/html/legend-view.js
@@ -2,7 +2,14 @@ var LegendViewBase = require('../base/legend-view-base');
 
 var HTMLLegendView = LegendViewBase.extend({
   _getCompiledTemplate: function () {
-    return this.model.get('html');
+    console.log(this._patchStylesForIE(this.model.get('html')));
+    return this._patchStylesForIE(this.model.get('html'));
+  },
+
+  _patchStylesForIE: function (str) {
+    var find = /\bstyle=(['"])/g;
+    var replace = 'style="opacity:1; ';
+    return str.replace(find, replace);
   }
 });
 

1abfa6b6f7a5d6e9f4f7632c2e506797f1a486e9
Francisco López <donflopez@gmail.com>
2016-11-24 17:18:42 +0100

    Fix lint errors

--- src/geo/leaflet/leaflet-cartodb-webgl-layer-group-view.js
+++ src/geo/leaflet/leaflet-cartodb-webgl-layer-group-view.js
@@ -1,12 +1,7 @@
-// require('d3.cartodb');// TODO: The 'd3.cartodb' module doens't currently export L.CartoDBd3Layer
-// and it's currently relying on window.L so weed to do the following trick.
-// Check out: https://github.com/CartoDB/d3.cartodb/issues/93 for more info
-// var CartoDBd3Layer = window.L.CartoDBd3Layer;
 var TC = require('tangram.cartodb');
 var LeafletLayerView = require('./leaflet-layer-view');
-var GeoJSONDataProvider = require('../data-providers/geojson/data-provider');
 
-var LeafletCartoDBVectorLayerGroupView = L.TileLayer.extend({
+var LeafletCartoDBVectorLayerGroupView = window.L.TileLayer.extend({
   includes: [
     LeafletLayerView.prototype
   ],

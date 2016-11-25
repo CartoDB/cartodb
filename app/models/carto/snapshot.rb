3d91c162fbe85c230708a5cd69cd8e3de72c8672
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 18:59:08 +0100

    add relations to Snapshot model

--- app/models/carto/snapshot.rb
+++ app/models/carto/snapshot.rb
@@ -5,9 +5,13 @@ require_relative './carto_json_serializer'
 
 module Carto
   class Snapshot
+    belongs_to :visualization, class_name: Carto::Visualization
+    belongs_to :user, class_name: Carto::User
+
     default_scope order('created_at DESC')
 
     serialize :state, ::Carto::CartoJsonSymbolizerSerializer
     validates :state, carto_json_symbolizer: true
+    validates :visualization, :user, presence: true
   end
 end

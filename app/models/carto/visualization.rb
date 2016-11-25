6fd74dee71ad093ffcc2c462249c460fc0b39932
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 19:11:19 +0100

    update reflection for new Snapshot model

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -65,10 +65,7 @@ class Carto::Visualization < ActiveRecord::Base
   belongs_to :state, class_name: Carto::State
   after_save :save_state_if_needed
 
-  has_many :snapshots,
-           conditions: proc { "id != '#{state_id}'" if state_id },
-           class_name: Carto::State,
-           dependent: :destroy
+  has_many :snapshots, class_name: Carto::Snapshot, dependent: :destroy
 
   validates :version, presence: true
 

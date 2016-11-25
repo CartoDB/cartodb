648d0c97b02d394d55521d44bc284940f23272d7
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 15:01:41 +0100

    add tests for update

--- spec/requests/carto/api/snapshots_controller_specs.rb
+++ spec/requests/carto/api/snapshots_controller_specs.rb
@@ -200,4 +200,81 @@ describe Carto::Api::SnapshotsController do
       end
     end
   end
+
+  describe('#update') do
+    def snapshots_update_url(user_domain: @user.subdomain,
+                             visualization_id: @visualization.id,
+                             snapshot_id: @snapshot.id,
+                             api_key: @user.api_key)
+      snapshot_url(user_domain: user_domain,
+                   visualization_id: visualization_id,
+                   id: snapshot_id,
+                   api_key: api_key)
+    end
+
+    before(:all) do
+      @snapshot = Carto::State.create!(user_id: @user.id,
+                                       visualization_id: @visualization.id,
+                                       json: fake_json)
+    end
+
+    after(:all) do
+      @snapshot.destroy
+    end
+
+    it 'rejects unauthenticated access' do
+      Carto::Visualization.any_instance
+                          .stubs(:is_publically_accesible?)
+                          .returns(false)
+
+      put_json(snapshots_update_url(api_key: nil), Hash.new) do |response|
+        response.status.should eq 401
+      end
+    end
+
+    it 'rejects users with no read access' do
+      Carto::Visualization.any_instance
+                          .stubs(:is_viewable_by_user?)
+                          .returns(false)
+
+      intruder_url = snapshots_update_url(user_domain: @intruder.subdomain,
+                                          api_key: @intruder.api_key)
+      put_json(intruder_url, Hash.new) do |response|
+        response.status.should eq 403
+      end
+    end
+
+    it 'returns 404 for non existent visualizations' do
+      not_found_url = snapshots_update_url(visualization_id: random_uuid)
+      put_json(not_found_url, Hash.new) do |response|
+        response.status.should eq 404
+      end
+    end
+
+    it 'returns 404 for inexistent snapshots' do
+      not_found_url = snapshots_update_url(snapshot_id: random_uuid)
+
+      put_json(not_found_url, Hash.new) do |response|
+        response.status.should eq 404
+      end
+    end
+
+    it 'only accepts owners of snapshots' do
+      intruder_url = snapshots_update_url(user_domain: @intruder.subdomain,
+                                          api_key: @intruder.api_key)
+      put_json(intruder_url, Hash.new) do |response|
+        response.status.should eq 403
+      end
+    end
+
+    it 'updates a snapshot' do
+      new_json = { minili: 'iscibir' }
+
+      put_json(snapshots_update_url, json: new_json) do |response|
+        response.status.should eq 200
+      end
+
+      @snapshot.reload.json.should eq new_json
+    end
+  end
 end

afd5e702cb48d74092d34e3967599a6e967f237e
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 15:47:56 +0100

    fetch snapshots throught visualization

--- app/controllers/carto/api/snapshots_controller.rb
+++ app/controllers/carto/api/snapshots_controller.rb
@@ -15,12 +15,8 @@ module Carto
                   Carto::UnprocesableEntityError, with: :rescue_from_carto_error
 
       def index
-        snapshots = State.where('id != ? AND ' \
-                                'visualization_id = ? AND ' \
-                                'user_id = ?',
-                                @visualization.state.id,
-                                @visualization.id,
-                                current_viewer.try(:id))
+        snapshots = @visualization.snapshots
+                                  .where(user_id: current_viewer.try(:id))
 
         render json: StatePresenter.collection_to_hash(snapshots)
       end

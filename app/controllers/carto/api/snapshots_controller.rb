dff272fb1ff9b325f1dc6590ff1d55c8e6dc0c82
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 19:22:46 +0100

    rename :json -> :state

--- app/controllers/carto/api/snapshots_controller.rb
+++ app/controllers/carto/api/snapshots_controller.rb
@@ -36,7 +36,7 @@ module Carto
       end
 
       def update
-        @snapshot.update_attributes!(state: params[:json])
+        @snapshot.update_attributes!(state: params[:state])
 
         render json: SnapshotPresenter.new(@snapshot.reload).to_hash
       rescue ActiveRecord::RecordInvalid => exception

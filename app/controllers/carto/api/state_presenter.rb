f85a4eb3a3955cdc9dbd5980fd606abf58fa04bd
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-25 19:04:34 +0100

    remove user from state presentation

--- app/controllers/carto/api/state_presenter.rb
+++ app/controllers/carto/api/state_presenter.rb
@@ -5,19 +5,12 @@ module Carto
         @state = state
       end
 
-      def self.collection_to_hash(states)
-        states.map do |state|
-          StatePresenter.new(state).to_hash
-        end
-      end
-
       def to_hash
         {
           id: @state.id,
           created_at: @state.created_at,
           updated_at: @state.updated_at,
-          json: @state.json,
-          user: Carto::Api::UserPresenter.new(@state.user).to_public_poro
+          json: @state.json
         }
       end
     end

c6f3ebb51b031ab4b83c7503166ec2dc2a793540
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-28 14:47:37 +0100

    change AssetsPresenter -> AssetPresenter

--- app/controllers/carto/api/assets_controller.rb
+++ app/controllers/carto/api/assets_controller.rb
@@ -4,7 +4,7 @@ class Carto::Api::AssetsController < ::Api::ApplicationController
   ssl_required :index
 
   def index
-    assets = AssetsPresenter.collection_to_hash(current_viewer.assets)
+    assets = AssetPresenter.collection_to_hash(current_viewer.assets)
 
     render json: { total_entries: assets.size, assets: assets_presentation }
   end

86e0de82ede7182be7bd585af8350e0943963bac
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-28 14:10:30 +0100

    remove unnecesary uri_user method

--- app/controllers/carto/api/assets_controller.rb
+++ app/controllers/carto/api/assets_controller.rb
@@ -4,20 +4,13 @@ class Carto::Api::AssetsController < ::Api::ApplicationController
   ssl_required :index
 
   def index
-    assets = uri_user.assets
+    assets = current_viewer.assets
+
     render_jsonp({ 
         total_entries: assets.size, 
         assets: assets.map { |asset| 
             Carto::Api::AssetsPresenter.new(asset).public_values
           }
       })
-  end
-
-  private
-
-  # TODO: this should be moved upwards in the controller hierarchy, and make it a replacement for current_user
-  # URI present-user if has valid session, or nil
-  def uri_user
-    @uri_user ||= (current_user.nil? ? nil : Carto::User.where(id: current_user.id).first)
   end
 end

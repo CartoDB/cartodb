19f5169aae397e2f3f947fb566f93f7da8e1f443
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-28 14:46:36 +0100

    avoid uneeded indentation

--- app/controllers/carto/api/asset_presenter.rb
+++ app/controllers/carto/api/asset_presenter.rb
@@ -1,26 +1,22 @@
 # encoding utf-8
 
-module Carto
-  module Api
-    class AssetPresenter
-      def initialize(asset)
-        @asset = asset
-      end
+class Carto::Api::AssetPresenter
+  def initialize(asset)
+    @asset = asset
+  end
 
-      def self.collection_to_hash(assets)
-        assets.map do |asset|
-          AssetPresenter.new(asset).public_values
-        end
-      end
-
-      def to_hash
-        {
-          id: @asset.id,
-          public_url: @asset.public_url,
-          user_id: @asset.user_id,
-          kind: @asset.kind
-        }
-      end
+  def self.collection_to_hash(assets)
+    assets.map do |asset|
+      AssetPresenter.new(asset).public_values
     end
+  end
+
+  def to_hash
+    {
+      id: @asset.id,
+      public_url: @asset.public_url,
+      user_id: @asset.user_id,
+      kind: @asset.kind
+    }
   end
 end

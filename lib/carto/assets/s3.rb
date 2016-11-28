6f6f39dc0315c33e62a11862c3e7b07401c95b00
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-28 16:42:56 +0100

    add skeleton for s3

--- lib/carto/assets/s3.rb
+++ lib/carto/assets/s3.rb
@@ -1,5 +1,28 @@
 # encoding utf-8
 
 class Carto::Storage::S3
+  def self.instance_if_enabled
+    s3 = Carto::Storage::S3.new
 
+    s3 if s3.enabled?
+  end
+
+  def enabled?
+    config.present? && bucket_name.present?
+  end
+
+  private
+
+  def bucket
+    AWS.config(config)
+    AWS::S3.new.buckets[bucket_name]
+  end
+
+  def config
+    Cartodb.config.fetch(:aws, 's3')
+  end
+
+  def bucket_name
+    Cartodb.config(:assets, 's3_bucket_name')
+  end
 end

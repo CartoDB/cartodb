eb4fd4c087e22c10bd8f418c988fc8d6f9ba8b0c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-28 14:37:44 +0100

    add loader method load_organization

--- app/controllers/carto/api/organization_assets_controller.rb
+++ app/controllers/carto/api/organization_assets_controller.rb
@@ -1,8 +1,20 @@
 # encoding: utf-8
 
 class Carto::Api::OrganizationAssetsController < ::Api::ApplicationController
+  include Carto::ControllerHelper
+
   ssl_required :index
 
+  rescue_from LoadError, with: :rescue_from_carto_error
+
   def index
+  end
+
+  private
+
+  def load_organization
+    @organization = Organization.find(params[:organization_id])
+  rescue ActiveRecord::RecordNotFound
+    raise LoadError.new
   end
 end

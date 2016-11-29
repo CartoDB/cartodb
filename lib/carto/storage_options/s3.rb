f5b69b33407261dc955831ae515d6f9e1f85768f
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-29 17:52:52 +0100

    inline namespace

--- lib/carto/storage_options/s3.rb
+++ lib/carto/storage_options/s3.rb
@@ -1,45 +1,41 @@
 # encoding utf-8
 
-module Carto
-  module StorageOptions
-    class S3
-      def self.instance_if_enabled
-        s3 = Carto::StorageOptions::S3.new
-        s3 if s3.config.present? && s3.bucket_name.present?
-      end
+class Carto::StorageOptions::S3
+  def self.instance_if_enabled
+    s3 = Carto::StorageOptions::S3.new
+    s3 if s3.config.present? && s3.bucket_name.present?
+  end
 
-      def initailize
-        AWS::config(config) if config.present
-      end
+  def initailize
+    AWS::config(config) if config.present
+  end
 
-      def config
-        @config ||= Cartodb.config.fetch(:aws, 's3')
-      end
+  def config
+    @config ||= Cartodb.config.fetch(:aws, 's3')
+  end
 
-      def bucket_name
-        @bucket_name ||= Cartodb.config.fetch(:assets, 's3_bucket_name')
-      end
+  def bucket_name
+    @bucket_name ||= Cartodb.config.fetch(:assets, 's3_bucket_name')
+  end
 
-      def upload(namespaced_name, file_path)
-        asset = bucket.objects[namespaced_name]
-        asset.write(file: file_path)
+  def upload(namespaced_name, file_path)
+    asset = bucket.objects[namespaced_name]
+    asset.write(file: file_path)
 
-        asset.url_for(:read)
-      end
+    asset.url_for(:read)
+  end
 
-      def remove(namespaced_name)
-        bucket.delete(namespaced_name)
-      end
+  def remove(namespaced_name)
+    bucket.delete(namespaced_name)
+  end
 
-      private
+  private
 
-      def bucket
-        @bucket ||= s3.new.buckets[bucket_name]
-      end
+  def bucket
+    @bucket ||= s3.new.buckets[bucket_name]
+  end
 
-      def s3
-        @s3 ||= AWS::S3.new
-      end
-    end
+  def s3
+    @s3 ||= AWS::S3.new
   end
 end

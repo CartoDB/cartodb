cf36ffd1bdba53e2f1cde1b6dc565fb491aa2430
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-29 17:37:53 +0100

    added remove method

--- lib/carto/assets/local.rb
+++ lib/carto/assets/local.rb
@@ -4,4 +4,8 @@ class Carto::Storage::Local
   def upload(namespaced_name, file)
     FileUtils.mv(file.path, namespaced_name)
   end
+
+  def remove(namespaced_name)
+    File.delet(namespaced_name) if File.exist?(namespaced_name)
+  end
 end

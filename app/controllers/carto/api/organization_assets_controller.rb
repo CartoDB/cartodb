13ac955a58b2e4d2bf189dde7d99f0b7cc17b485
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-29 15:18:25 +0100

    introduced stepped namespacing to help load

--- app/controllers/carto/api/organization_assets_controller.rb
+++ app/controllers/carto/api/organization_assets_controller.rb
@@ -1,53 +1,57 @@
 # encoding: utf-8
 
-class Carto::Api::OrganizationAssetsController < ::Api::ApplicationController
-  include Carto::ControllerHelper
+module Carto
+  module Api
+    class OrganizationAssetsController < ::Api::ApplicationController
+      include Carto::ControllerHelper
 
-  ssl_required :index, :show, :create, :destroy
+      ssl_required :index, :show, :create, :destroy
 
-  before_filter :load_organization,
-                :organization_owners_only
-  before_filter :load_asset, only: [:show, :destroy]
+      before_filter :load_organization,
+                    :organization_owners_only
+      before_filter :load_asset, only: [:show, :destroy]
 
-  rescue_from LoadError,
-              UnprocesableEntityError, with: :rescue_from_carto_error
+      rescue_from LoadError,
+                  UnprocesableEntityError, with: :rescue_from_carto_error
 
-  def index
-    presentation = AssetPresenter.collection_to_hash(@organization.assets)
+      def index
+        presentation = AssetPresenter.collection_to_hash(@organization.assets)
 
-    render json: presentation
-  end
+        render json: presentation
+      end
 
-  def show
-    render json: AssetPresenter.new(@asset).to_hash
-  end
+      def show
+        render json: AssetPresenter.new(@asset).to_hash
+      end
 
-  def create
-    Asset.create!(kind: params[:kind], organization_id: @organization.id)
-  rescue ActiveRecord::RecordInvalid => exception
-    message = exception.record.errors.full_messages.join(', ')
-    raise UnprocesableEntityError.new(message)
-  end
+      def create
+        Asset.create!(kind: params[:kind], organization_id: @organization.id)
+      rescue ActiveRecord::RecordInvalid => exception
+        message = exception.record.errors.full_messages.join(', ')
+        raise UnprocesableEntityError.new(message)
+      end
 
-  def destroy
-    @asset.destroy
-  end
+      def destroy
+        @asset.destroy
+      end
 
-  private
+      private
 
-  def load_organization
-    @organization = Organization.find(params[:organization_id])
-  rescue ActiveRecord::RecordNotFound
-    raise LoadError.new('Organization not found')
-  end
+      def load_organization
+        @organization = Organization.find(params[:organization_id])
+      rescue ActiveRecord::RecordNotFound
+        raise LoadError.new('Organization not found')
+      end
 
-  def organization_owners_only
-    raise UnauthorizedError.new unless @organization.owner?(current_viewer)
-  end
+      def organization_owners_only
+        raise UnauthorizedError.new unless @organization.owner?(current_viewer)
+      end
 
-  def load_asset
-    @asset = Organization.find(params[:asset_id])
-  rescue ActiveRecord::RecordNotFound
-    raise LoadError.new('Asset not found')
+      def load_asset
+        @asset = Organization.find(params[:asset_id])
+      rescue ActiveRecord::RecordNotFound
+        raise LoadError.new('Asset not found')
+      end
+    end
   end
 end

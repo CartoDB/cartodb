0c752eeb2e5193675a81f15888414067c9c0fac0
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-11-29 12:13:16 +0100

    add hooks

--- spec/models/carto/snapshot_spec.rb
+++ spec/models/carto/snapshot_spec.rb
@@ -5,4 +5,14 @@ require 'factories/carto_visualizations'
 
 describe Carto::Snapshot do
   include Carto::Factories::Visualizations
+
+  before(:all) do
+    @user = FactoryGirl.create(:carto_user)
+    @_m, @_t, @_tv, @visualization = create_full_visualization(@user)
+  end
+
+  after(:all) do
+    destroy_full_visualization(@_m, @_t, @_tv, @visualization)
+    @user.destroy
+  end
 end

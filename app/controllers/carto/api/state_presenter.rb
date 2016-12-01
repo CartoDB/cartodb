dfb7869a518088d8ddf1260164a5380b5702e3b3
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-01 13:49:54 +0100

    add visualization_id to state presenter

--- app/controllers/carto/api/state_presenter.rb
+++ app/controllers/carto/api/state_presenter.rb
@@ -8,6 +8,7 @@ module Carto
       def to_hash
         {
           id: @state.id,
+          visualization_id: @state.visualization_id,
           created_at: @state.created_at,
           updated_at: @state.updated_at,
           json: @state.json

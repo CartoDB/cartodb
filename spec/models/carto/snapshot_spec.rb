82a4eadcf992d92022000797a8a8376bb22802d7
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-01 16:02:44 +0100

    use factory girl

--- spec/models/carto/snapshot_spec.rb
+++ spec/models/carto/snapshot_spec.rb
@@ -1,18 +1,15 @@
 # encoding: utf-8
 
 require 'spec_helper_min'
-require 'factories/carto_visualizations'
 
 describe Carto::Snapshot do
-  include Carto::Factories::Visualizations
-
   before(:all) do
     @user = FactoryGirl.create(:carto_user)
-    @_m, @_t, @_tv, @visualization = create_full_visualization(@user)
+    @visualization = FactoryGirl.create(:carto_visualization, user: @user)
   end
 
   after(:all) do
-    destroy_full_visualization(@_m, @_t, @_tv, @visualization)
+    @visualization.destroy
     @user.destroy
   end
 

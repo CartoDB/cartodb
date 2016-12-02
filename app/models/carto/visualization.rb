91249c29ec1a0d1c2320112b06c0553f1bb1ba1b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-02 18:12:22 +0100

    remove user from state build in visulization

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -495,7 +495,7 @@ class Carto::Visualization < ActiveRecord::Base
   end
 
   def build_state
-    self.state = Carto::State.new(user: user, visualization: self)
+    self.state = Carto::State.new(visualization: self)
   end
 
   def save_state_if_needed

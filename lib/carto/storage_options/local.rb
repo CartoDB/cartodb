c4470a44a9727a052e41ea54f70b6cf111b5bba5
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-05 11:20:23 +0100

    parametrizable protocol

--- lib/carto/storage_options/local.rb
+++ lib/carto/storage_options/local.rb
@@ -3,14 +3,14 @@
 class Carto::StorageOptions::Local
   UPLOADS_DIRECTORY = "public/uploads".freeze
 
-  def upload(namespaced_name, file)
+  def upload(namespaced_name, file, protocol: 'https')
     FileUtils.mkdir_p(UPLOADS_DIRECTORY)
 
     target_full_path = File.join(UPLOADS_DIRECTORY, namespaced_name)
     FileUtils.mkdir_p(target_full_path)
     FileUtils.mv(file.path, target_full_path)
 
-    "http://#{CartoDB.account_host}/#{target_full_path}"
+    "#{protocol}://#{CartoDB.account_host}/#{target_full_path}"
   end
 
   def remove(namespaced_name)

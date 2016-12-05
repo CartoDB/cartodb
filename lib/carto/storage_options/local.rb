781bc8d523d20e5b3a00f2167f220351f8859d0b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-05 14:27:35 +0100

    working version for local upload

--- lib/carto/storage_options/local.rb
+++ lib/carto/storage_options/local.rb
@@ -4,12 +4,13 @@ class Carto::StorageOptions::Local
   include Carto::Configuration
 
   def upload(namespaced_name, file, protocol: 'https')
-    target_full_path = public_uploads_path(subfolder: namespaced_name)
+    filename = Pathname.new(file.path).basename
+    target_full_path = File.join(public_uploads_path, namespaced_name)
     FileUtils.mkdir_p(target_full_path)
 
     FileUtils.mv(file.path, target_full_path)
 
-    url_path = target_full_path.gsub('public/', '')
+    url_path = File.join(target_full_path.gsub('public/', ''), filename)
     "#{protocol}://#{CartoDB.account_host}/#{url_path}"
   end
 

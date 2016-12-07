7d3ee134e2b01da5617d1127957d0dd31038a3d2
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-07 18:57:07 +0100

    add skeleton for index specs

--- spec/requests/carto/api/organization_assets_controller_spec.rb
+++ spec/requests/carto/api/organization_assets_controller_spec.rb
@@ -1,9 +1,24 @@
 # encoding: utf-8
 
+require 'spec_helper_min'
+
 describe Carto::Api::OrganizationAssetsController do
+  include_context 'organization with users helper'
+
   before(:all) do
+    @intruder = FactoryGirl.create(:carto_user)
   end
 
   after(:all) do
+    @intruder.destroy
+  end
+
+  describe('#index') do
+    before(:all) do
+
+    end
+
+    after(:all) do
+    end
   end
 end

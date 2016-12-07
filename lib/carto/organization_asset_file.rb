568d0c039a6db55a57d61568088dd0766222b004
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-07 12:47:37 +0100

    add method to fetch max_size_in_bytes

--- lib/carto/organization_asset_file.rb
+++ lib/carto/organization_asset_file.rb
@@ -16,5 +16,11 @@ module Carto
     def namespace
       @namespace ||= CartoDB.config.fetch(:assets, 'organizations', 'namespace')
     end
+
+    def max_size_in_bytes
+      @max_size_in_bytes ||= CartoDB.config.fetch(:assets,
+                                                  'organizations',
+                                                  'max_size_in_bytes')
+    end
   end
 end

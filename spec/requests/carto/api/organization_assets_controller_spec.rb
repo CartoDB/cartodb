e47397446dd21b876454ee63880bf1784bc359f2
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-09 18:57:00 +0100

    WIP

--- spec/requests/carto/api/organization_assets_controller_spec.rb
+++ spec/requests/carto/api/organization_assets_controller_spec.rb
@@ -15,10 +15,14 @@ describe Carto::Api::OrganizationAssetsController do
 
   describe('#index') do
     before(:all) do
-
+      5.times do
+        Carto::Asset.create!(organization_id: @carto_organization.id,
+                             public_url: 'manolo')
+      end
     end
 
     after(:all) do
+      Carto::Asset.all.map(&:destroy)
     end
   end
 end

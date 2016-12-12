0e036dec4be28297459cd645dab7e4c0867d6b71
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-12 15:06:40 +0100

    add storage_type, path and location to assets

--- db/migrate/20161212145833_alter_assets.rb
+++ db/migrate/20161212145833_alter_assets.rb
@@ -7,10 +7,19 @@ Sequel.migration do
                       on_delete: :cascade
 
       add_index :organization_id
+
+      add_column :storage_type, :text
+      add_column :path, :text
+      add_column :location, :text
     end
   end
 
   down do
-    drop_column :assets, :organization_id
+    alter_table :assets do
+      drop_column :organization_id
+      drop_column :storage_type
+      drop_column :path
+      drop_column :location
+    end
   end
 end

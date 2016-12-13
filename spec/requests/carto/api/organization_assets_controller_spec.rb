760b24ef38c8f09c458da62710fcd709c87e334b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-13 17:49:39 +0100

    stubs WIP

--- spec/requests/carto/api/organization_assets_controller_spec.rb
+++ spec/requests/carto/api/organization_assets_controller_spec.rb
@@ -1,16 +1,27 @@
 # encoding: utf-8
 
 require 'spec_helper_min'
+require 'support/helpers'
 
 describe Carto::Api::OrganizationAssetsController do
+  include HelperMethods
   include_context 'organization with users helper'
 
   before(:all) do
+    @owner = @carto_organization.owner
     @intruder = FactoryGirl.create(:carto_user)
+
+    Carto::Storage.instance.stubs(:s3_enabled?).returns(false)
+    Carto::StorageOptions::Local.any_instance.stubs(:upload).returns do
+      ['pepito/menganito/fulanito.png',
+       'https://manolo.escobar.es/pepito/menganito/fulanito.png']
+    end
+    Carto::StorageOptions::Local.any_instance.stubs(:remove)
   end
 
   after(:all) do
     @intruder.destroy
+    @owner = nil
   end
 
   describe('#index') do

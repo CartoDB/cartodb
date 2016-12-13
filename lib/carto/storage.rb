c36bf181b0ec0fca85a29813df3335adc45fa068
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-13 12:38:29 +0100

    refactor storage

--- lib/carto/storage.rb
+++ lib/carto/storage.rb
@@ -12,11 +12,8 @@ module Carto
       @storages = Hash.new
     end
 
-    def for(location, preferred_storage_type: nil)
-      case preferred_storage_type.to_s
-      when 's3'
-        Carto::StorageOptions::S3.new_if_available(location)
-      when 'local'
+    def for(location, preferred_type: nil)
+      if preferred_type == 'local'
         Carto::StorageOptions::Local.new(location)
       else
         get_or_set_location(location)
@@ -28,13 +25,18 @@ module Carto
       if existing_location
         existing_location
       else
-        @storages[location] = available_storage_option(location)
+        @storages[location] = available_storage_option.new(location)
       end
     end
 
-    def available_storage_option(location)
-      Carto::StorageOptions::S3.new_if_available(location) ||
-        Carto::StorageOptions::Local.new(location)
+    def available_storage_option
+      s3_enabled? ? Carto::StorageOptions::S3 : Carto::StorageOptions::Local
+    end
+
+    private
+
+    def s3_enabled?
+      @s3_enabled ||= Carto::StorageOptions::S3.conf.try(:any?) ? true : false
     end
   end
 end

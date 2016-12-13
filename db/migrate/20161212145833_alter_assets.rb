552f702cd0de5bc5581233465683a1eb34564415
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-13 13:16:42 +0100

    path -> identifier

--- db/migrate/20161212145833_alter_assets.rb
+++ db/migrate/20161212145833_alter_assets.rb
@@ -9,7 +9,7 @@ Sequel.migration do
       add_index :organization_id
 
       add_column :storage_type, :text
-      add_column :path, :text
+      add_column :identifier, :text
       add_column :location, :text
     end
   end
@@ -18,7 +18,7 @@ Sequel.migration do
     alter_table :assets do
       drop_column :organization_id
       drop_column :storage_type
-      drop_column :path
+      drop_column :identifier
       drop_column :location
     end
   end

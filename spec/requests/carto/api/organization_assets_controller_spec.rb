24c104e3db50f5609575575ee09d4751e2fbfa06
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-14 12:36:19 +0100

    index test suite

--- spec/requests/carto/api/organization_assets_controller_spec.rb
+++ spec/requests/carto/api/organization_assets_controller_spec.rb
@@ -11,12 +11,14 @@ describe Carto::Api::OrganizationAssetsController do
 
   before(:all) do
     @owner = @carto_organization.owner
+    @sub = @carto_org_user_1
     @intruder = FactoryGirl.create(:carto_user)
   end
 
   after(:all) do
     @intruder.destroy
     @owner = nil
+    @sub = nil
   end
 
   describe('#index') do
@@ -31,17 +33,37 @@ describe Carto::Api::OrganizationAssetsController do
       Carto::Asset.all.map(&:destroy)
     end
 
-    def index_url(subdomain: @owner.username,
-                  organization_id: @carto_organization.id,
-                  api_key: @owner.api_key)
-      assets_url(user_domain: subdomain,
-                 organization_id: organization_id,
-                 api_key: api_key)
+    def index_url(user: @owner, organization: @carto_organization)
+      assets_url(user_domain: user.username,
+                 organization_id: organization.id,
+                 api_key: user.api_key)
+    end
+
+    it 'works for organization owners' do
+      get_json index_url, {} do |response|
+        response.status.should eq 200
+        response.should_not be_empty
+      end
     end
 
     it 'works for organization users' do
-      get_json index_url, {} do |response|
+      get_json index_url(user: @sub), {} do |response|
         response.status.should eq 200
+        response.should_not be_empty
+      end
+    end
+
+    it 'fails for intruders' do
+      get_json index_url(user: @intruder), {} do |response|
+        response.status.should eq 403
+      end
+    end
+
+    it 'fails for unauthenticated' do
+      unauthenticated_url = assets_url(organization_id: @carto_organization.id,
+                                       user_domain: @owner.username)
+      get_json unauthenticated_url, {} do |response|
+        response.status.should eq 401
       end
     end
   end

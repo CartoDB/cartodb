37ea79cf786e6b8b1937fd4053610367a34b5e6e
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-14 14:18:44 +0100

    fix refactor

--- app/controllers/carto/api/assets_controller.rb
+++ app/controllers/carto/api/assets_controller.rb
@@ -4,8 +4,8 @@ class Carto::Api::AssetsController < ::Api::ApplicationController
   ssl_required :index
 
   def index
-    assets = AssetPresenter.collection_to_hash(current_viewer.assets)
+    assets = Carto::Api::AssetPresenter.collection_to_hash(current_viewer.assets)
 
-    render json: { total_entries: assets.size, assets: assets_presentation }
+    render json: { total_entries: assets.size, assets: assets }
   end
 end

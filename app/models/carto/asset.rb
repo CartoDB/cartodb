4552abc5fa0913e39a5478a7a16c82d8315f3fdb
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-14 14:52:59 +0100

    add json storage_info

--- app/models/carto/asset.rb
+++ app/models/carto/asset.rb
@@ -7,12 +7,46 @@ module Carto
     belongs_to :user, class_name: Carto::User
     belongs_to :organization, class_name: Carto::Organization
 
+    serialize :storage_info, ::Carto::CartoJsonSymbolizerSerializer
+    validates :storage_info, carto_json_symbolizer: true
+
+    before_validation :ensure_storage_info
+
+    validate :validate_storage_info
+
     def destroy
       if organization_id
         Storage.instance.for(location, preferred_type: storage_type).remove(identifier)
       end
 
       super
+    end
+
+    private
+
+    def validate_storage_info
+      if organization_id
+        if storage_info
+          schema = Definition.instance.load_from_file('lib/formats/asset/storage_info.json')
+          indifferent_storage_info = storage_info.with_indifferent_access
+          errs = JSON::Validator::fully_validate(schema, indifferent_storage_info)
+          errors.add(:conf, errs.join(', ')) if errs_any
+        else
+          errors.add(:storage_info, 'can\'t be blank')
+        end
+      end
+    end
+
+    def validate_conf_schema
+      schema = Carto::Definition.instance
+                                .load_from_file('lib/formats/legends/conf.json')
+
+      parsed_conf = conf.try(:is_a?, Hash) ? conf.with_indifferent_access : conf
+      conf_errors = JSON::Validator.fully_validate(schema, parsed_conf)
+
+      if conf_errors.any?
+        errors.add(:conf, conf_errors.join(', '))
+      end
     end
   end
 end

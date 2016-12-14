e33a8285588cba8fe93c2740d93de5d489c54a1a
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-14 15:12:46 +0100

    use new storage_info and vaidate it only if org_id

--- app/models/carto/asset.rb
+++ app/models/carto/asset.rb
@@ -10,12 +10,14 @@ module Carto
     serialize :storage_info, ::Carto::CartoJsonSymbolizerSerializer
     validates :storage_info, carto_json_symbolizer: true
 
-    before_validation :ensure_storage_info
-
     validate :validate_storage_info
 
     def destroy
       if organization_id
+        location = storage_info[:location]
+        storage_type = storage_info[:type]
+        identifier = storage_info[:identifier]
+
         Storage.instance.for(location, preferred_type: storage_type).remove(identifier)
       end
 
@@ -29,8 +31,9 @@ module Carto
         if storage_info
           schema = Definition.instance.load_from_file('lib/formats/asset/storage_info.json')
           indifferent_storage_info = storage_info.with_indifferent_access
+
           errs = JSON::Validator::fully_validate(schema, indifferent_storage_info)
-          errors.add(:conf, errs.join(', ')) if errs_any
+          errors.add(:conf, errs.join(', ')) if errs.any?
         else
           errors.add(:storage_info, 'can\'t be blank')
         end

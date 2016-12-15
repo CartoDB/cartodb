9124654ae061d8f88b11885c76fcb4bcb77af933
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-15 11:08:23 +0100

    use explicit construction

--- app/controllers/carto/api/assets_controller.rb
+++ app/controllers/carto/api/assets_controller.rb
@@ -4,7 +4,9 @@ class Carto::Api::AssetsController < ::Api::ApplicationController
   ssl_required :index
 
   def index
-    assets = Carto::Api::AssetPresenter.collection_to_hash(current_viewer.assets)
+    assets = current_viewer.assets.each do |asset|
+      Carto::Api::AssetPresenter.new(asset).to_hash
+    end
 
     render json: { total_entries: assets.size, assets: assets }
   end

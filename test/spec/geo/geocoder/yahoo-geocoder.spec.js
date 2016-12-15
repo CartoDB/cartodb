8647c571e7924095688b4add87f2c6392f77175d
nobuti <buti@nobuti.com>
2016-12-15 12:07:54 +0100

    Revert "Increase default timeout for yahoo geocoder."
    This reverts commit 5a6b778f82ee02b25d400fb9eca9f129cc4f46a6.

--- test/spec/geo/geocoder/yahoo-geocoder.spec.js
+++ test/spec/geo/geocoder/yahoo-geocoder.spec.js
@@ -1,16 +1,6 @@
 var YAHOO = require('../../../../src/geo/geocoder/yahoo-geocoder');
 
 describe('geo/geocoder/yahoo-geocoder', function () {
-  var originalTimeout;
-  beforeEach(function () {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  });
-
-  afterEach(function () {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-  });
-
   it("we shouldn't get a direction that doesn't exist", function (done) {
     YAHOO.geocode('68461092610314965639', function (d) {
       expect(d.length).toEqual(0);

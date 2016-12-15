d0bb1d442174b345a51cf652ab55e0cb2edd26f0
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-15 16:29:58 +0100

    convert to singleton

--- lib/carto/organization_asset_service.rb
+++ lib/carto/organization_asset_service.rb
@@ -1,36 +1,20 @@
 # encoding: utf-8
 
 require 'carto/storage'
+require 'singleton'
 
 module Carto
   class OrganizationAssetService
-    DEFAULT_LOCATION = 'organization_assets'.freeze
+    include Singleton
 
-    def self.location
-      Cartodb.get_config_if_present(:assets, 'organization', 'bucket') ||
-        Cartodb.get_config_if_present(:assets, 'organization', 'location') ||
-        DEFAULT_LOCATION
-    end
-
-    DEFAULT_MAX_SIZE_IN_BYTES = 1_048_576 # 1 MB
-
-    def self.max_size_in_bytes
-      Cartodb.get_config_if_present(:assets, 'organizations', 'max_size_in_bytes') ||
-        DEFAULT_MAX_SIZE_IN_BYTES
-    end
-
-    def initialize(organization)
-      @organization = organization
-    end
-
-    def upload(resource)
+    def upload(organization_id, resource)
       file = fetch_file(resource)
-      storage = Storage.instance.for(self.class.location)
-      identifier, url = storage.upload(@organization.id, file)
+      storage = Storage.instance.for(location)
+      identifier, url = storage.upload(organization_id, file)
 
       storage_info = {
         type: storage.class.name.demodulize.downcase,
-        location: self.class.location,
+        location: location,
         identifier: identifier
       }
 
@@ -43,11 +27,8 @@ module Carto
              .remove(storage_info[:location])
     end
 
-    private
-
     def fetch_file(resource)
       temp_file = Tempfile.new("org_asset_download_#{Time.now.utc.to_i}")
-      max_size_in_bytes = self.class.max_size_in_bytes
 
       begin
         read = IO.copy_stream(open(resource), temp_file, max_size_in_bytes + 1)
@@ -61,6 +42,26 @@ module Carto
       end
 
       temp_file
+    end
+
+    DEFAULT_LOCATION = 'organization_assets'.freeze
+
+    def location
+      @location ||= Cartodb.get_config_if_present(:assets, 'organization', 'bucket') ||
+                    Cartodb.get_config_if_present(:assets, 'organization', 'location') ||
+                    DEFAULT_LOCATION
+    end
+
+    DEFAULT_MAX_SIZE_IN_BYTES = 1_048_576 # 1 MB
+
+    def max_size_in_bytes
+      return @max_size_in_bytes if @max_size_in_bytes
+
+      configured = Cartodb.get_config_if_present(:assets,
+                                                 'organizations',
+                                                 'max_size_in_bytes')
+
+      @max_size_in_bytes = configured || DEFAULT_MAX_SIZE_IN_BYTES
     end
   end
 end

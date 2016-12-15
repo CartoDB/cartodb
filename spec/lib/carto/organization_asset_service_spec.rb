47bbe74afc48b012542ca9f5375b470bece0a05b
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-15 17:25:18 +0100

    add specs for max_size_in_bytes

--- spec/lib/carto/organization_asset_service_spec.rb
+++ spec/lib/carto/organization_asset_service_spec.rb
@@ -47,4 +47,26 @@ describe Carto::OrganizationAssetService do
       Carto::OrganizationAssetService.instance.location.should eq default_location
     end
   end
+
+  describe('#max_size_in_bytes') do
+    after(:each) do
+      Carto::OrganizationAssetService.instance.instance_variable_set(:@max_size_in_bytes, nil)
+    end
+
+    it 'uses conf when available' do
+      Cartodb.stubs(:get_config_if_present)
+             .with(:assets, 'organization', 'max_size_in_bytes')
+             .returns(123456789)
+
+      Carto::OrganizationAssetService.instance.max_size_in_bytes.should eq 123456789
+    end
+
+    it 'uses default neither bucket nor location are available' do
+      Cartodb.stubs(:get_config_if_present)
+             .with(:assets, 'organization', 'max_size_in_bytes')
+
+      default_max_size = Carto::OrganizationAssetService::DEFAULT_MAX_SIZE_IN_BYTES
+      Carto::OrganizationAssetService.instance.max_size_in_bytes.should eq default_max_size
+    end
+  end
 end

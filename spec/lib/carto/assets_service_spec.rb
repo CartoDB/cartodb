fb856f64b52b8fb784c0a7034e07e6107c30f9cb
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-16 12:01:15 +0100

    use plural

--- spec/lib/carto/assets_service_spec.rb
+++ spec/lib/carto/assets_service_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper_min'
 
-describe Carto::AssetService do
+describe Carto::AssetsService do
   describe('#fetch_file') do
     it 'reject files that are too big' do
-      max_size = Carto::AssetService.instance.max_size_in_bytes
+      max_size = Carto::AssetsService.instance.max_size_in_bytes
       IO.stubs(:copy_stream).returns(max_size + 1)
 
       expect {
-        Carto::AssetService.instance.fetch_file(Tempfile.new('manolo'))
+        Carto::AssetsService.instance.fetch_file(Tempfile.new('manolo'))
       }.to raise_error(Carto::UnprocesableEntityError)
     end
   end

d5b31bb04c9d3931b7c696905f34c2a4b3aa5429
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-16 14:35:12 +0100

    remove singleton notation

--- app/models/carto/asset.rb
+++ app/models/carto/asset.rb
@@ -40,7 +40,7 @@ module Carto
     end
 
     def remove_asset_from_storage
-      Carto::AssetsService.instance.remove(storage_info)
+      Carto::AssetsService.remove(storage_info)
     end
   end
 end

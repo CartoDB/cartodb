1ec5abc1d53c87aea76b39a3b6e0ff58e0e302ab
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-16 12:41:36 +0100

    use get_config

--- lib/carto/organization_assets_service.rb
+++ lib/carto/organization_assets_service.rb
@@ -13,17 +13,15 @@ module Carto
     DEFAULT_LOCATION = 'organization_assets'.freeze
 
     def location
-      @location ||= Cartodb.get_config_if_present(:assets, 'organization', 'bucket') ||
-                    Cartodb.get_config_if_present(:assets, 'organization', 'location') ||
+      @location ||= Cartodb.get_config(:assets, 'organization', 'bucket') ||
+                    Cartodb.get_config(:assets, 'organization', 'location') ||
                     DEFAULT_LOCATION
     end
 
     def max_size_in_bytes
       return @max_size_in_bytes if @max_size_in_bytes
 
-      configured = Cartodb.get_config_if_present(:assets,
-                                                 'organization',
-                                                 'max_size_in_bytes')
+      configured = Cartodb.get_config(:assets, 'organization', 'max_size_in_bytes')
 
       @max_size_in_bytes = configured || super
     end

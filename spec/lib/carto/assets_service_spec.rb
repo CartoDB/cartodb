ab913670803807582724369fd36a5618d73e4be1
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-16 14:36:46 +0100

    drop singleton notation

--- spec/lib/carto/assets_service_spec.rb
+++ spec/lib/carto/assets_service_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper_min'
 describe Carto::AssetsService do
   describe('#fetch_file') do
     it 'reject files that are too big' do
-      max_size = Carto::AssetsService.instance.max_size_in_bytes
+      max_size = Carto::AssetsService.new.max_size_in_bytes
       IO.stubs(:copy_stream).returns(max_size + 1)
 
       expect {
-        Carto::AssetsService.instance.fetch_file(Tempfile.new('manolo'))
+        Carto::AssetsService.new.fetch_file(Tempfile.new('manolo'))
       }.to raise_error(Carto::UnprocesableEntityError)
     end
   end

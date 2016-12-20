bc42b433bf2a4b0e9675b95c949967159b1a4f22
Javier Torres <jtorres@carto.com>
2016-12-20 15:13:20 +0100

    Clean up carto layers controller

--- app/controllers/carto/api/layers_controller.rb
+++ app/controllers/carto/api/layers_controller.rb
@@ -4,16 +4,16 @@ require_dependency 'carto/uuidhelper'
 module Carto
   module Api
     class LayersController < ::Api::ApplicationController
-      include Carto::UUIDHelper
+      include Carto::ControllerHelper
 
       ssl_required :show, :layers_by_map, :custom_layers_by_user
 
-      before_filter :owner_from_params, only: [ :custom_layers_by_user, :show ]
-      before_filter :map_from_params, only: [ :layers_by_map, :show ]
+      before_filter :ensure_current_user, only: [:custom_layers_by_user, :show_for_user]
+      before_filter :load_map, only: [:layers_by_map, :show_for_map]
+      before_filter :load_user_layer, only: [:show_for_user]
+      before_filter :load_map_layer, only: [:show_for_map]
 
       def layers_by_map
-        raise RecordNotFound if @parent.nil?
-
         layers = @parent.layers(@parent).map do |layer|
           Carto::Api::LayerPresenter.new(layer, viewer_user: current_user, user: owner_user(layer)).to_poro
         end
@@ -22,56 +22,62 @@ module Carto
       end
 
       def custom_layers_by_user
-        raise RecordNotFound if @owner_user.nil?
-
         layers = Carto::Layer.joins(:layers_user).where(layers_users: { user_id: current_user.id })
-        layers = layers.map { |layer|
-            Carto::Api::LayerPresenter.new(layer, { viewer_user: current_user, user: @owner_user }).to_poro
-          }
+        layers = layers.map do |layer|
+          Carto::Api::LayerPresenter.new(layer, viewer_user: @user, user: @user).to_poro
+        end
         render_jsonp layers: layers, total_entries: layers.size
       end
 
-      def show
-        raise RecordNotFound unless is_uuid?(params[:id])
-        raise RecordNotFound if (@owner_user.nil? && @parent.nil?)
-
-        if @parent
-          parent = @parent
-          owner = @parent.user
-        else
-          parent = @owner_user
-          owner = @owner_user
-        end
-
-        layer = parent.layers.where(id: params[:id]).first
-        raise RecordNotFound if layer.nil?
-        render_jsonp Carto::Api::LayerPresenter.new(layer, { viewer_user: current_user, user: owner}).to_json
+      def show_for_user
+        show(current_user)
       end
 
-      protected
-
-      def owner_from_params
-         @owner_user = current_user if (params[:user_id] && is_uuid?(params[:user_id]))
-      end
-
-      def map_from_params
-        return if (!params[:map_id] || !is_uuid?(params[:map_id]))
-
-        # User must be owner or have permissions for the map's visualization
-        vis = Carto::Visualization.where({
-            map_id: params[:map_id]
-          }).first
-        raise RecordNotFound if vis.nil? || !vis.is_viewable_by_user?(current_user)
-
-        @parent = Carto::Map.where(id: params[:map_id]).first
+      def show_for_map
+        show(@map.user)
       end
 
       private
 
+      def show(owner)
+        render_jsonp Carto::Api::LayerPresenter.new(layer, viewer_user: current_user, user: owner).to_json
+      end
+
+      def ensure_current_user
+        user_id = uuid_parameter(:user_id)
+        raise UnauthorizedError unless current_user.id == user_id
+        @user = Carto::User.find(user_id)
+      end
+
+      def load_user_layer
+        layer_id = uuid_parameter(:id)
+        @user.layers.find(layer_id)
+      rescue ActiveRecord::RecordNotFound
+        raise LoadError.new('Layer not found')
+      end
+
+      def load_map
+        map_id = uuid_parameter(:map_id)
+
+        # User must be owner or have permissions for the map's visualization
+        @map = Carto::Map.find(map_id)
+        vis = @map.visualization
+        raise LoadError.new('Map not found') unless vis && vis.is_viewable_by_user?(current_user)
+      rescue ActiveRecord::RecordNotFound
+        raise LoadError.new('Map not found')
+      end
+
+      def load_map_layer
+        layer_id = uuid_parameter(:id)
+        @map.layers.find(layer_id)
+      rescue ActiveRecord::RecordNotFound
+        raise LoadError.new('Layer not found')
+      end
+
       def owner_user(layer)
-        if current_user.nil? || @parent.user.id != current_user.id
+        if current_user.nil? || @map.user.id != current_user.id
           # This keeps backwards compatibility with map user assignment. See #8974
-          @parent.user
+          @map.user
         elsif layer.options && layer.options['user_name'].present?
           ::User.where(username: layer.options['user_name']).first
         else

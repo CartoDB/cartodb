1f9bd0a3d6c5a9951789a1c21c0fca18eb6f414c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-22 18:58:31 +0100

    remove unused factories

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -22,33 +22,5 @@ FactoryGirl.define do
       whitelisted_email_domains ['carto.com']
       auth_username_password_enabled true
     end
-
-    factory :organization_with_users do
-      after(:create) do |org|
-        owner = FactoryGirl.create(:user)
-        uo = CartoDB::UserOrganization.new(org.id, owner.id)
-        uo.promote_user_to_admin
-        org.reload
-        user = FactoryGirl.build(:user)
-        user.organization_id = org.id
-        user.enabled = true
-        user.save
-        org.reload
-      end
-    end
-
-    factory :saml_organization do
-      auth_saml_configuration do
-        {
-          issuer: 'localhost.lan',
-          idp_sso_target_url: 'https://example.com/saml/signon/',
-          idp_slo_target_url: 'https://example.com/saml/signon/',
-          idp_cert_fingerprint: '',
-          assertion_consumer_service_url: 'https://localhost.lan/saml/finalize',
-          name_identifier_format: '',
-          email_attribute: 'username'
-        }
-      end
-    end
   end
 end

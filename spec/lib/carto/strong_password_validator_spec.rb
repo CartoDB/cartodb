b132a3cc303456cf409e157e8917cc5bed572d32
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-12-23 17:05:50 +0100

    sepparate per method

--- spec/lib/carto/strong_password_validator_spec.rb
+++ spec/lib/carto/strong_password_validator_spec.rb
@@ -3,96 +3,98 @@
 require 'spec_helper_min'
 
 describe Carto::StrongPasswordValidator do
-  let(:password) { '2{Patrañas}' }
+  describe('#validate') do
+    let(:password) { '2{Patrañas}' }
 
-  it 'should be invalid when password too short' do
-    validator = Carto::StrongPasswordValidator.new(min_length: password.length + 1)
+    it 'should be invalid when password too short' do
+      validator = Carto::StrongPasswordValidator.new(min_length: password.length + 1)
 
-    errors = validator.validate(password)
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == "must be at least #{password.length + 1} characters long"
-  end
-
-  it 'should be invalid when password too long' do
-    validator = Carto::StrongPasswordValidator.new(max_length: password.length - 1)
-
-    errors = validator.validate(password)
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == "must be at most #{password.length - 1} characters long"
-  end
-
-  it 'should be invalid when password does not have enough letters' do
-    validator = Carto::StrongPasswordValidator.new(min_letters: 9)
-
-    errors = validator.validate(password)
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == "must contain at least 9 letters"
-  end
-
-  it 'should be invalid when password does not have enough numbers or symbols' do
-    validator = Carto::StrongPasswordValidator.new(min_symbols: 3, min_numbers: 2)
-
-    errors = validator.validate(password)
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == "must contain at least 3 symbols or 2 numbers"
-  end
-
-  it 'should be valid when password has enough numbers but not enough symbols' do
-    validator = Carto::StrongPasswordValidator.new(min_symbols: 3, min_numbers: 1)
-
-    errors = validator.validate(password)
-    errors.empty?.should be_true
-    validator.formatted_error_message(errors).should be_nil
-  end
-
-  it 'should be valid when password has enough symbols but not enough numbers' do
-    validator = Carto::StrongPasswordValidator.new(min_symbols: 2, min_numbers: 3)
-
-    errors = validator.validate(password)
-    errors.empty?.should be_true
-    validator.formatted_error_message(errors).should be_nil
-  end
-
-  it 'should invalidate a nil password' do
-    validator = Carto::StrongPasswordValidator.new
-
-    errors = validator.validate(nil)
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == 'must be at least 8 characters long, must contain at ' +
-                                                        'least 1 letter and must contain at least 1 symbol or ' +
-                                                        '1 number'
-  end
-
-  it 'should invalidate an empty password' do
-    validator = Carto::StrongPasswordValidator.new
-
-    errors = validator.validate('')
-    errors.empty?.should be_false
-    validator.formatted_error_message(errors).should == 'must be at least 8 characters long, must contain at ' +
-                                                        'least 1 letter and must contain at least 1 symbol or ' +
-                                                        '1 number'
-  end
-
-  it 'should return an error array' do
-    validator = Carto::StrongPasswordValidator.new
-
-    errors = validator.validate(nil)
-    errors.empty?.should be_false
-
-    message = validator.formatted_error_message(errors)
-    message.should == 'must be at least 8 characters long, must contain at least 1 letter and must ' +
-                      'contain at least 1 symbol or 1 number'
-
-    errors.each do |error|
-      message.should include(error)
+      errors = validator.validate(password)
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == "must be at least #{password.length + 1} characters long"
     end
-  end
 
-  it 'should validate a good password' do
-    validator = Carto::StrongPasswordValidator.new
+    it 'should be invalid when password too long' do
+      validator = Carto::StrongPasswordValidator.new(max_length: password.length - 1)
 
-    errors = validator.validate(password)
-    errors.empty?.should be_true
-    validator.formatted_error_message(errors).should be_nil
+      errors = validator.validate(password)
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == "must be at most #{password.length - 1} characters long"
+    end
+
+    it 'should be invalid when password does not have enough letters' do
+      validator = Carto::StrongPasswordValidator.new(min_letters: 9)
+
+      errors = validator.validate(password)
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == "must contain at least 9 letters"
+    end
+
+    it 'should be invalid when password does not have enough numbers or symbols' do
+      validator = Carto::StrongPasswordValidator.new(min_symbols: 3, min_numbers: 2)
+
+      errors = validator.validate(password)
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == "must contain at least 3 symbols or 2 numbers"
+    end
+
+    it 'should be valid when password has enough numbers but not enough symbols' do
+      validator = Carto::StrongPasswordValidator.new(min_symbols: 3, min_numbers: 1)
+
+      errors = validator.validate(password)
+      errors.empty?.should be_true
+      validator.formatted_error_message(errors).should be_nil
+    end
+
+    it 'should be valid when password has enough symbols but not enough numbers' do
+      validator = Carto::StrongPasswordValidator.new(min_symbols: 2, min_numbers: 3)
+
+      errors = validator.validate(password)
+      errors.empty?.should be_true
+      validator.formatted_error_message(errors).should be_nil
+    end
+
+    it 'should invalidate a nil password' do
+      validator = Carto::StrongPasswordValidator.new
+
+      errors = validator.validate(nil)
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == 'must be at least 8 characters long, must contain at ' +
+                                                          'least 1 letter and must contain at least 1 symbol or ' +
+                                                          '1 number'
+    end
+
+    it 'should invalidate an empty password' do
+      validator = Carto::StrongPasswordValidator.new
+
+      errors = validator.validate('')
+      errors.empty?.should be_false
+      validator.formatted_error_message(errors).should == 'must be at least 8 characters long, must contain at ' +
+                                                          'least 1 letter and must contain at least 1 symbol or ' +
+                                                          '1 number'
+    end
+
+    it 'should return an error array' do
+      validator = Carto::StrongPasswordValidator.new
+
+      errors = validator.validate(nil)
+      errors.empty?.should be_false
+
+      message = validator.formatted_error_message(errors)
+      message.should == 'must be at least 8 characters long, must contain at least 1 letter and must ' +
+                        'contain at least 1 symbol or 1 number'
+
+      errors.each do |error|
+        message.should include(error)
+      end
+    end
+
+    it 'should validate a good password' do
+      validator = Carto::StrongPasswordValidator.new
+
+      errors = validator.validate(password)
+      errors.empty?.should be_true
+      validator.formatted_error_message(errors).should be_nil
+    end
   end
 end

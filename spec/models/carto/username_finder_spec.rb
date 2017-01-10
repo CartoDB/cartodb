af906d31848b72edc380274bbe85953c5da7965b
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-01-10 15:31:37 +0100

    new class name

--- spec/models/carto/username_finder_spec.rb
+++ spec/models/carto/username_finder_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper_min'
 
 module Carto
-  describe UsernameFinder do
+  describe UsernameProposer do
     describe '#find_unique' do
       before(:all) do
         @users = Array.new
@@ -16,29 +16,29 @@ module Carto
       end
 
       it 'returns same if available' do
-        UsernameFinder.find_unique('escobar').should eq 'escobar'
+        UsernameProposer.find_unique('escobar').should eq 'escobar'
       end
 
       it 'returns unique if not available' do
-        UsernameFinder.find_unique('manolo').should eq 'manolo-2'
+        UsernameProposer.find_unique('manolo').should eq 'manolo-2'
       end
 
       it 'fills in the gaps' do
         @users << FactoryGirl.create(:carto_user, username: 'manolo-3')
 
-        UsernameFinder.find_unique('manolo').should eq 'manolo-2'
+        UsernameProposer.find_unique('manolo').should eq 'manolo-2'
       end
 
       it 'returns same if max_retries is 0' do
-        UsernameFinder.find_unique('manolo', max_retries: 0).should eq 'manolo'
+        UsernameProposer.find_unique('manolo', max_retries: 0).should eq 'manolo'
       end
 
       it 'starts at given offset' do
-        UsernameFinder.find_unique('manolo', offset: 8).should eq 'manolo-8'
+        UsernameProposer.find_unique('manolo', offset: 8).should eq 'manolo-8'
       end
 
       it 'gives up if max_retries is reached' do
-        UsernameFinder.find_unique('manolo', max_retries: 1).should eq 'manolo-1'
+        UsernameProposer.find_unique('manolo', max_retries: 1).should eq 'manolo-1'
       end
     end
   end

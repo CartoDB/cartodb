30c785f38a33ebc1a3015d81f2f9ecc9eee36633
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-01-10 13:12:40 +0100

    type <= -> <

--- lib/carto/username_finder.rb
+++ lib/carto/username_finder.rb
@@ -6,7 +6,7 @@ module Carto
       suffix = "-#{offset}" if offset > 0
       candidate_username_with_suffix = "#{candidate_username}#{suffix}"
 
-      if offset <= max_retries && Carto::User.exists?(username: candidate_username_with_suffix)
+      if offset < max_retries && Carto::User.exists?(username: candidate_username_with_suffix)
         find_unique(candidate_username, offset: offset + 1, max_retries: max_retries)
       else
         candidate_username_with_suffix

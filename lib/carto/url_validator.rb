3fb346510efb7e6b8b6cec18ab32cffaaabdd680
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-01-16 17:25:03 +0100

    add support for https and ftp

--- lib/carto/url_validator.rb
+++ lib/carto/url_validator.rb
@@ -20,7 +20,9 @@ module Carto
 
     def valid_url?(str, valid_ports)
       uri = URI.parse(str)
-      return uri.is_a?(URI::HTTP) && valid_ports.include?(uri.port) && !blacklisted_ip?(uri)
+      (uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS) || uri.is_a?(URI::FTP)) &&
+        valid_ports.include?(uri.port) &&
+        !blacklisted_ip?(uri)
     rescue URI::InvalidURIError
       return false
     end

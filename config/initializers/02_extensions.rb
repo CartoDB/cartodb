be6cc3151b79b5b8a6a40877814cc5b732018598
Javier Torres <jtorres@carto.com>
2017-02-03 18:24:13 +0100

    Monkey patches for ruby 2.4

--- config/initializers/02_extensions.rb
+++ config/initializers/02_extensions.rb
@@ -1,3 +1,27 @@
 # Disable XML parameter parsing, see:
 # http://www.insinuator.net/2013/01/rails-yaml/
-ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML) 
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
+# Patches for Ruby 2.4
+class ActiveSupport::Duration
+  def coerce(other)
+    [other, to_i]
+  end
+end
+
+require 'arel'
+module Arel
+  module Visitors
+    class Dot
+      alias :visit_Integer :visit_String
+    end
+
+    class ToSql
+      alias :visit_Integer :literal
+    end
+
+    class DepthFirst
+      alias :visit_Integer :terminal
+    end
+  end
+end

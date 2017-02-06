82003f5b337830b1104798176e7536d73e3f60c6
Javier Torres <jtorres@carto.com>
2017-02-06 11:47:59 +0100

    Fix test after renaming

--- spec/models/table/relator_spec.rb
+++ spec/models/table/relator_spec.rb
@@ -72,7 +72,7 @@ describe CartoDB::TableRelator do
       before :each do
         bypass_named_maps
 
-        CartoDB::Visualization::Member.any_instance.stubs(:dependent?).returns(true, false, true)
+        CartoDB::Visualization::Member.any_instance.stubs(:fully_dependent?).returns(true, false, true)
         @dependents = @table_relator.serialize_dependent_visualizations
       end
 
@@ -128,7 +128,7 @@ describe CartoDB::TableRelator do
       before :each do
         bypass_named_maps
 
-        CartoDB::Visualization::Member.any_instance.stubs(:non_dependent?).returns(true, false, false)
+        CartoDB::Visualization::Member.any_instance.stubs(:partially_dependent?).returns(true, false, false)
         @non_dependents = @table_relator.serialize_non_dependent_visualizations
       end
 

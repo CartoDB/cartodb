afbd01fbb14d90f9cd66825c202ea955b57c163d
Javier Torres <javier@javiertorres.eu>
2017-02-10 00:10:48 +0100

    Fix rspec-rails loading order
    Hacky backport of https://github.com/rspec/rspec-rails/pull/1372
    to avoid having to update to rspec 3+, which we are not compatible with.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'helpers/unique_names_helper'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+ActiveRecord.send(:remove_const, :TestFixtures)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,

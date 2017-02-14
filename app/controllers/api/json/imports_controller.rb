2254973e125545813d6cd818728c6ae19a9f069c
Javier Torres <jtorres@carto.com>
2017-02-14 14:17:20 +0100

    Also close file tempfile (for API imports)

--- app/controllers/api/json/imports_controller.rb
+++ app/controllers/api/json/imports_controller.rb
@@ -58,8 +58,10 @@ class Api::Json::ImportsController < Api::ApplicationController
             # In Rack < 1.6 / Rails < 4, tempfiles are not inmediately cleaned (https://github.com/rack/rack/pull/671).
             # Instead they stay around until a GC cycle which can take a while in instances with low traffic.
             # This forces the tempfile to be removed right away, just after we have saved it to our storage.
-            file = params[:filename]
-            file.tempfile.close! if file
+            [:filename, :file].each do |param_name|
+              file = params[param_name]
+              file.tempfile.close! if file
+            end
 
             # Not queued import is set by skipping pending state and setting directly as already enqueued
             options.merge({

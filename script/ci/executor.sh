85e3e6ea002b8fe4c9679d9024abdd5b7b7634f2
Javier Torres <jtorres@carto.com>
2017-02-24 16:50:30 +0100

    Sock, not seq

--- script/ci/executor.sh
+++ script/ci/executor.sh
@@ -3,7 +3,7 @@
 main() {
     port=$((6000 + $2))
     # Run the rspec
-    ZEUSSOCK=".zeus$port.seq" bundle exec zeus rspec $1 >> $port.log 2>&1;
+    ZEUSSOCK=".zeus$port.sock" bundle exec zeus rspec $1 >> $port.log 2>&1;
     #RAILS_ENV=test PARALLEL=true RAILS_DATABASE_FILE=database_${2}.yml REDIS_PORT=$port bundle exec rspec --require ./spec/rspec_configuration.rb $1 >> $port.log 2>&1;
 
     # Give some feedback

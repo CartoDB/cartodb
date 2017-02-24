a0fde7e8de3993da221ffb3babf65ce7c4827f88
Javier Torres <jtorres@carto.com>
2017-02-24 15:32:40 +0100

    Use Zeus in parallel testing

--- script/ci/executor.sh
+++ script/ci/executor.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
-# Jesus Vazquez
-# executor.sh: This scripts executes the rspec recieved by param and stores it in specsuccess.log or specfailed.log
-# depending on the execution result.
-
-lock() {
-  touch config/$1.lock;
-}
-
-unlock() {
-  rm config/$1 >> executor.log 2>&1;
-}
-
-# Return first database.yml free
-redis_file() {
-  for redisfile in $(ls config -1| grep -v 'lock' | grep redis)
-  do
-    if [ ! -f config/$redisfile.lock ]; then
- #     echo $redisfile;
-      break
-    fi
-  done
-}
 
 main() {
-    # (hack) Choose a redis port for the execution
-    redis_file
-    # Lock the redis file
-    lock $redisfile;
-
-    # Choose redis-server port
-    port=$(cat config/$redisfile)
-
+    port="600$2"
     # Run the rspec
-    RAILS_ENV=test PARALLEL=true RAILS_DATABASE_FILE=database_${2}.yml REDIS_PORT=$port bundle exec rspec --require ./spec/rspec_configuration.rb $1 >> $port.log 2>&1;
+    ZEUSSOCK=".zeus$port.seq" bundle exec zeus rspec $1 >> $port.log 2>&1;
+    #RAILS_ENV=test PARALLEL=true RAILS_DATABASE_FILE=database_${2}.yml REDIS_PORT=$port bundle exec rspec --require ./spec/rspec_configuration.rb $1 >> $port.log 2>&1;
 
     # Give some feedback
     if [ $? -eq 0 ]; then
@@ -42,10 +14,8 @@ main() {
       echo "Finished (FAILED): $1 Port: $port";
       echo "$1" >> specfailed.log;
     fi
-    # Unlock file
-    unlock $redisfile.lock;
 }
 
 # Init
-main $1 $2];
+main $1 $2;
 exit 0;
--- script/ci/wrapper.sh
+++ script/ci/wrapper.sh
@@ -1,78 +1,25 @@
 #!/bin/sh
-# Jesus Vazquez
-# wrapper.sh: This script creates as much database.yml configuration files as we specify by param. It also
-# starts redis server on database.yml specified port.
 rm config/database_*
-rm config/redis_*
-#sudo killall redis-server
-
-# Start redis servers
-port=6001
-for i in $(seq 1 $1);
-do
-        # Start redis server
-        if [ ! -d "/tmp/redis-$port" ]; then
-                mkdir /tmp/redis-$port
-        fi
-        touch redis.conf.test
-        truncate -s 0 redis.conf.test
-        echo "bind 127.0.0.1" >> redis.conf.test
-        echo "port $port" >> redis.conf.test
-        echo "daemonize yes" >> redis.conf.test
-        echo "pidfile /tmp/redis-test-$port.tmp" >> redis.conf.test
-        echo "timeout 300" >> redis.conf.test
-        echo "dbfilename redis_test.rdb" >> redis.conf.test
-        echo "dir /tmp/redis-$port" >> redis.conf.test
-        echo "loglevel debug" >> redis.conf.test
-        echo "logfile /tmp/redis-$port/stdout" >> redis.conf.test
-        cat redis.conf.test | redis-server  - 2>&1
-        rm redis.conf.test
-        touch config/redis_$port;
-        echo $port >> config/redis_$port;
-        sleep 0.5; # Let redis server start
-
-        # Increase port
-        port=$((port+1))
-done;
-if [ ! -d "/tmp/redis-6335" ]; then
-        mkdir /tmp/redis-6335
-fi
-
-# Start default redis on port 6335
-touch redis.conf.test
-truncate -s 0 redis.conf.test
-echo "bind 127.0.0.1" >> redis.conf.test
-echo "port 6335" >> redis.conf.test
-echo "daemonize yes" >> redis.conf.test
-echo "pidfile /tmp/redis-test-6335.tmp" >> redis.conf.test
-echo "timeout 300" >> redis.conf.test
-echo "dbfilename redis_test.rdb" >> redis.conf.test
-echo "dir /tmp/redis-6335" >> redis.conf.test
-echo "loglevel debug" >> redis.conf.test
-echo "logfile /tmp/redis-6335/stdout" >> redis.conf.test
-cat redis.conf.test | redis-server  - 2>&1
-rm redis.conf.test
-# ps -eaf | grep -v "grep" | grep redis-server >> wrapper.log 2>&1
-
-# Create databases
-
-# Read database name
+threads=$1
 databaseName="carto_db_test"
 dbAdmin="postgres"
-specCount=$(cat specfull.txt | wc -l)
-specCount=$((specCount+6000))
+lastPort=$((threads + 6000))
 startPort=6001
 # Iterate and create one database per spec
-for j in $(seq $startPort $specCount)
+for i in $(seq $startPort $lastPort)
 do
     # Get database owner
     owner=$(psql -U $dbAdmin -t -c "select r.rolname from pg_database d, pg_roles r where d.datname='carto_db_test' and d.datdba = r.oid")
-    newDatabase="${databaseName}_${j}";
+    newDatabase="${databaseName}_${i}";
     # Create the database with specific owner and template
+    dropdb -U $dbAdmin $newDatabase
     $(psql -U $dbAdmin -t -c "create database $newDatabase with owner $owner template $databaseName;") >> wrapper.log 2>&1
     # Create the database.yml file
-    echo "# Creating database_$j.yml file" >> wrapper.log 2>&1
-    sed -e s/carto_db_test/carto_db_test_$j/g config/database.yml.sample > config/database_$j.yml
+    echo "# Creating database_$i.yml file" >> wrapper.log 2>&1
+    sed -e s/carto_db_test/carto_db_test_$i/g config/database.yml.sample > config/database_$i.yml
+
+    # Start Zeus server
+    TURBO=1 ZEUSSOCK=".zeus$i.seq" RAILS_ENV=test PARALLEL=true RAILS_DATABASE_FILE=database_$i.yml REDIS_PORT=$i bundle exec zeus start >/dev/null 2>/dev/null &
 done
 
 touch specfailed.log

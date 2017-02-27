a5caade9ca94084120cefb667ffcb049035ea843
Javier Torres <jtorres@carto.com>
2017-02-27 13:11:01 +0100

    Add error exit if intermediate commands fail in parallel CI

--- script/ci/runParallelTests.sh
+++ script/ci/runParallelTests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # BACKEND PARALLEL
-sh generateSpecFull.sh
+sh generateSpecFull.sh || exit 1
 
 # CLEANER
-sh cleaner.sh
+sh cleaner.sh || exit 1
 
 # WRAPPER
-sh wrapper.sh $1
+sh wrapper.sh $1 || exit 1
 
 # TESTS
-time parallel -j $1 --delay 0.1 -a specfull.txt './executor.sh'
+time parallel -j $1 --delay 0.1 -a specfull.txt './executor.sh' || exit 1
 
 # SECOND TRY
-sh secondTry.sh
+sh secondTry.sh || exit 1
 
 # REPORTER
-sh reporter.sh
+sh reporter.sh || exit 1

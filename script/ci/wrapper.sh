cb612c8a7a6f16660f430bee7b512b28e76a9c23
Javier Torres <jtorres@carto.com>
2017-03-02 11:17:06 +0100

    Wait for all Zeus masters to start

--- script/ci/wrapper.sh
+++ script/ci/wrapper.sh
@@ -18,12 +18,15 @@ do
     sed -e s/carto_db_test/carto_db_test_$i/g config/database.yml.sample > config/database_$i.yml
 done
 
-sleep 5
 for i in $(seq $startPort $lastPort)
 do
   # Start Zeus server
   TURBO=1 ZEUSSOCK=".zeus$i.sock" RAILS_DATABASE_FILE=database_$i.yml REDIS_PORT=$i bundle exec zeus start >/dev/null 2>/dev/null &
 done
+
+# Wait for a few seconds for Zeus servers to startup.
+# A better way to do it would be to wait for all `.zeus*.sock` files to be created.
+sleep 5
 
 touch parallel_tests/specfailed.log
 touch parallel_tests/specsuccess.log

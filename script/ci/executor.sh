6ff451974da97a4f6046c4f09977e57db8e243f0
Javier Torres <jtorres@carto.com>
2017-03-02 09:50:29 +0100

    Correctly check exit code

--- script/ci/executor.sh
+++ script/ci/executor.sh
@@ -5,11 +5,12 @@ main() {
     # Run the rspec
     start=$SECONDS
     ZEUSSOCK=".zeus$port.sock" bundle exec zeus rspec -J#$3 $1 >> parallel_tests/$port.log 2>&1;
+    exitCode=$?
     taken=$(($SECONDS - $start))
     formatted_time=$(date -u -d @$taken +'%-0M:%-0S')
 
     # Give some feedback
-    if [ $? -eq 0 ]; then
+    if [ $exitCode -eq 0 ]; then
       echo "[$formatted_time] Finished: $1 Port: $port";
       echo "$1" >> parallel_tests/specsuccess.log;
     else

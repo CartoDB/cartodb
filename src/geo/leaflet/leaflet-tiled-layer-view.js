460e8551cfe7054188da703d81ca230c13dfed9d
Francisco López <donflopez@gmail.com>
2017-03-08 15:51:01 +0100

    Remove wms inheritance

--- src/geo/leaflet/leaflet-tiled-layer-view.js
+++ src/geo/leaflet/leaflet-tiled-layer-view.js
@@ -2,6 +2,18 @@ var _ = require('underscore');
 var L = require('leaflet');
 var LeafletLayerView = require('./leaflet-layer-view');
 
+var generateLeafletLayerOptions = function (layerModel) {
+  return {
+    tms: !!layerModel.get('tms'),
+    attribution: layerModel.get('attribution'),
+    minZoom: layerModel.get('minZoom'),
+    maxZoom: layerModel.get('maxZoom'),
+    subdomains: layerModel.get('subdomains') || 'abc',
+    errorTileUrl: layerModel.get('errorTileUrl'),
+    opacity: layerModel.get('opacity')
+  };
+};
+
 var LeafletTiledLayerView = function (layerModel, leafletMap) {
   var self = this;
   LeafletLayerView.apply(this, [layerModel, this._createLeafletLayer(layerModel), leafletMap]);
@@ -29,15 +41,7 @@ LeafletTiledLayerView.prototype = _.extend(
     },
 
     _createLeafletLayer: function (layerModel) {
-      return new L.TileLayer(layerModel.get('urlTemplate'), {
-        tms: !!layerModel.get('tms'),
-        attribution: layerModel.get('attribution'),
-        minZoom: layerModel.get('minZoom'),
-        maxZoom: layerModel.get('maxZoom'),
-        subdomains: layerModel.get('subdomains') || 'abc',
-        errorTileUrl: layerModel.get('errorTileUrl'),
-        opacity: layerModel.get('opacity')
-      });
+      return new L.TileLayer(layerModel.get('urlTemplate'), generateLeafletLayerOptions(layerModel));
     },
 
     _onAdd: function () {
@@ -49,18 +53,8 @@ LeafletTiledLayerView.prototype = _.extend(
     },
 
     _modelUpdated: function () {
-      var model = this.model;
-      var options = {
-        subdomains: model.get('subdomains') || 'abc',
-        attribution: model.get('attribution'),
-        maxZoom: model.get('maxZoom'),
-        minZoom: model.get('minZoom'),
-        tms: !!model.get('tms')
-      };
-
-      L.Util.setOptions(this.leafletLayer, options);
-
-      this.leafletLayer.setUrl(model.get('urlTemplate'));
+      L.Util.setOptions(this.leafletLayer, generateLeafletLayerOptions(this.model));
+      this.leafletLayer.setUrl(this.model.get('urlTemplate'));
     }
   }
 );

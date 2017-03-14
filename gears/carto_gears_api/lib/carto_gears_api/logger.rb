50b9675ec499ff33b3329b895fc50ac0385542a3
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-03-14 12:38:40 +0100

    Log gear name

--- gears/carto_gears_api/lib/carto_gears_api/logger.rb
+++ gears/carto_gears_api/lib/carto_gears_api/logger.rb
@@ -43,7 +43,8 @@ module CartoGearsApi
 
     def log(level, exception: nil, message: nil, user: nil, **additional_data)
       gear_message = "{#{@gear}}: #{message}"
-      CartoDB::Logger.log(level, exception: exception, message: gear_message, user: user, **additional_data)
+      additional = additional_data.merge(gear: @gear)
+      CartoDB::Logger.log(level, exception: exception, message: gear_message, user: user, **additional)
     end
   end
 end

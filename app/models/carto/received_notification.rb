56f42bbd5b2ab6c1389f0d3307ccbf8f04a073bf
Javier Torres <jtorres@carto.com>
2017-03-17 10:03:01 +0100

    Convert unread notifications association to scope

--- app/models/carto/received_notification.rb
+++ app/models/carto/received_notification.rb
@@ -4,6 +4,7 @@ module Carto
   class ReceivedNotification < ActiveRecord::Base
     belongs_to :user, inverse_of: :received_notifications
     belongs_to :notification, inverse_of: :received_notifications
+    scope :unread, -> { where(read_at: nil).order('received_at DESC') }
 
     delegate :icon, :body, to: :notification
   end

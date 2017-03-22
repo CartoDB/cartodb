ffb6dcf10c735b91b69d006d8c4ba49454b842df
Rafa de la Torre <rtorre@cartodb.com>
2017-03-22 12:33:53 +0100

    ServiceUsageMetrics test refactor
    - create dummy service class
    - move creation of dummy service to top level

--- services/dataservices-metrics/spec/unit/service_usage_metrics_spec.rb
+++ services/dataservices-metrics/spec/unit/service_usage_metrics_spec.rb
@@ -5,36 +5,40 @@ require 'mock_redis'
 require_relative '../../../../spec/rspec_configuration'
 
 describe CartoDB::ServiceUsageMetrics do
+
+  class DummyServiceUsageMetrics < CartoDB::ServiceUsageMetrics
+    VALID_METRICS = [ :dummy_metric ]
+    VALID_SERVICES = [ :dummy_service ]
+
+    def check_valid_data(service, metric)
+      raise ArgumentError.new('Invalid service') unless VALID_SERVICES.include?(service)
+      raise ArgumentError.new('Invalid metric') unless VALID_METRICS.include?(metric)
+    end
+  end
+
+  before(:each) do
+    @redis_mock = MockRedis.new
+    @usage_metrics = DummyServiceUsageMetrics.new('rtorre', 'team', @redis_mock)
+  end
+
   describe 'Read quota info from redis with zero padding' do
 
-    before(:each) do
-      @redis_mock = MockRedis.new
-      @usage_metrics = CartoDB::ServiceUsageMetrics.new('rtorre', 'team', @redis_mock)
-      @usage_metrics.stubs(:check_valid_data)
-    end
-
     it 'reads standard zero padded keys' do
-      @redis_mock.zincrby('org:team:here_isolines:isolines_generated:201606', 1543, '01')
-      @usage_metrics.get('here_isolines', 'isolines_generated', Date.new(2016, 6, 1)).should eq 1543
+      @redis_mock.zincrby('org:team:dummy_service:dummy_metric:201606', 1543, '01')
+      @usage_metrics.get(:dummy_service, :dummy_metric, Date.new(2016, 6, 1)).should eq 1543
     end
 
     it "does not request redis twice when there's no need" do
-      @redis_mock.expects(:zscore).once.with('org:team:here_isolines:isolines_generated:201606', '20').returns(3141592)
-      @usage_metrics.get('here_isolines', 'isolines_generated', Date.new(2016, 6, 20)).should eq 3141592
+      @redis_mock.expects(:zscore).once.with('org:team:dummy_service:dummy_metric:201606', '20').returns(3141592)
+      @usage_metrics.get(:dummy_service, :dummy_metric, Date.new(2016, 6, 20)).should eq 3141592
     end
 
     it "returns zero when there's no consumption" do
-      @usage_metrics.get('here_isolines', 'isolines_generated', Date.new(2016, 6, 20)).should eq 0
+      @usage_metrics.get(:dummy_service, :dummy_metric, Date.new(2016, 6, 20)).should eq 0
     end
   end
 
   describe :assert_valid_amount do
-    before(:each) do
-      @redis_mock = MockRedis.new
-      @usage_metrics = CartoDB::ServiceUsageMetrics.new('rtorre', 'team', @redis_mock)
-      @usage_metrics.stubs(:check_valid_data)
-    end
-
     it 'passes when fed with a positive integer' do
       @usage_metrics.send(:assert_valid_amount, 42).should eq nil
     end
@@ -57,12 +61,6 @@ describe CartoDB::ServiceUsageMetrics do
   end
 
   describe :incr do
-    before(:each) do
-      @redis_mock = MockRedis.new
-      @usage_metrics = CartoDB::ServiceUsageMetrics.new('rtorre', 'team', @redis_mock)
-      @usage_metrics.stubs(:check_valid_data)
-    end
-
     it 'validates that the amount passed can actually be zero' do
       @usage_metrics.incr(:dummy_service, :dummy_metric, _amount = 0)
       @usage_metrics.get(:dummy_service, :dummy_metric).should eq 0
@@ -70,19 +68,13 @@ describe CartoDB::ServiceUsageMetrics do
   end
 
   describe :get_date_range do
-    before(:each) do
-      @redis_mock = MockRedis.new
-      @usage_metrics = CartoDB::ServiceUsageMetrics.new('rtorre', 'team', @redis_mock)
-      @usage_metrics.stubs(:check_valid_data)
-    end
-
     it 'gets a sum of the zscores stored in a given date range' do
       @redis_mock.zincrby('org:team:dummy_service:dummy_metric:201703', _amount=1, _day='20')
       @redis_mock.zincrby('org:team:dummy_service:dummy_metric:201703', _amount=2, _day='21')
       @redis_mock.zincrby('org:team:dummy_service:dummy_metric:201703', _amount=3, _day='22')
 
-      @usage_metrics.get_date_range('dummy_service',
-                                    'dummy_metric',
+      @usage_metrics.get_date_range(:dummy_service,
+                                    :dummy_metric,
                                     Date.new(2017, 03, 20),
                                     Date.new(2017, 03, 22)).should eq 6
     end

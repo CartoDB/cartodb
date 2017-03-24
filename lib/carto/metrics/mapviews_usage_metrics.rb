3b89cb8c93229c5ba897819c5dbca34edc8bf797
Rafa de la Torre <rtorre@cartodb.com>
2017-03-24 12:38:36 +0100

    Add method MapviewsUsageMetrics#get_date_range

--- lib/carto/metrics/mapviews_usage_metrics.rb
+++ lib/carto/metrics/mapviews_usage_metrics.rb
@@ -27,5 +27,22 @@ module Carto::Metrics
         end
       end
     end
+
+    def get_date_range(_service, _metric, date_from, date_to)
+      stats = CartoDB::Stats::APICalls.new
+      ret = {}
+      (@organization ? @organization.users.map(&:username) : [@username]).each do |username|
+        MAPVIEWS_REDIS_KEYS.each do |redis_key|
+          user_map_views = stats.get_api_calls_from_redis_source(
+            username,
+            redis_key,
+            from: date_from,
+            to: date_to
+          )
+          ret.merge!(user_map_views) { |date, accum, value| accum + value }
+        end
+      end
+      ret.reduce({}) { |h, (key, val)| h[Date.parse(key)] = val; h}
+    end
   end
 end

92cc1d06805db48c6a98c4c25a69bc97bdc9f195
Javier Torres <javier@javiertorres.eu>
2017-03-27 09:48:37 +0200

    Return user from `make_builder/viewer`

--- gears/carto_gears_api/lib/carto_gears_api/users/users_service.rb
+++ gears/carto_gears_api/lib/carto_gears_api/users/users_service.rb
@@ -10,13 +10,15 @@ module CartoGearsApi
       # @param request [ActionDispatch::Request] CARTO request, as received in any controller.
       # @return [User] the user.
       def logged_user(request)
-        user(request.env['warden'].user(CartoDB.extract_subdomain(request)))
+        gears_user(request.env['warden'].user(CartoDB.extract_subdomain(request)))
       end
 
       # Converts an user to a viewer, without editing rights.
       # It also sets all quotas to 0
       #
       # @param user_id [UUID] the user id
+      # @return [User] the updated user
+      #
       # @raise [Errors::RecordNotFound] if the user could not be found in the database
       # @raise [Errors::ValidationFailed] if the validation failed
       def make_viewer(user_id)
@@ -25,12 +27,16 @@ module CartoGearsApi
         user.viewer = true
         raise CartoGearsApi::Errors::ValidationFailed.new(user.errors) unless user.save
         user.update_in_central
+
+        gears_user(user)
       end
 
       # Converts an user to a builder, with full editing rights.
       #
       # @param user_id [UUID] the user id
       # @param quota_in_bytes [Integer] quota for the user. It defaults to the organization default quota
+      # @return [User] the updated user
+      #
       # @raise [Errors::RecordNotFound] if the user could not be found in the database
       # @raise [Errors::ValidationFailed] if the validation failed
       def make_builder(user_id, quota_in_bytes: nil)
@@ -47,16 +53,18 @@ module CartoGearsApi
 
         raise CartoGearsApi::Errors::ValidationFailed.new(user.errors) unless user.save
         user.update_in_central
+
+        gears_user(user)
       end
 
       private
 
-      def user(user)
+      def gears_user(user)
         CartoGearsApi::Users::User.with(
           id: user.id,
           username: user.username,
           email: user.email,
-          organization: user.organization ? organization(user.organization) : nil,
+          organization: user.organization ? gears_organization(user.organization) : nil,
           feature_flags: user.feature_flags,
           can_change_email: user.can_change_email?,
           quota_in_bytes: user.quota_in_bytes,
@@ -64,7 +72,7 @@ module CartoGearsApi
         )
       end
 
-      def organization(organization)
+      def gears_organization(organization)
         CartoGearsApi::Organizations::Organization.with(name: organization.name)
       end
 

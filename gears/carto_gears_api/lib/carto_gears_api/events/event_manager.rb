c629b418da8636c5f51114b2f29da0bd3b81dff2
Javier Torres <jtorres@carto.com>
2017-03-30 11:48:19 +0200

    Allow unsubscribing from events

--- gears/carto_gears_api/lib/carto_gears_api/events/event_manager.rb
+++ gears/carto_gears_api/lib/carto_gears_api/events/event_manager.rb
@@ -10,12 +10,23 @@ module CartoGearsApi
       end
 
       # Add a subscriber to an event type
-      # @param [Class] event_type class of the event to subscribe to
+      # @param [Class] event_type class of the event to subscribe to. Subclass of {BaseEvent}
       # @yield [EventType] block to run when the event is triggered. Will receive an object of +event_type+ class
+      # @return a descriptor that can be used to {#unsubscribe} from the event
+      # @example
+      #   event_manager = CartoGearsApi::Events::EventManager.instance.
+      #   event_manager.subscribe(CartoGearsApi::Events::UserCreationEvent) { |e| puts e.user.username }
       def subscribe(event_type, &block)
         raise ArgumentError.new('block is mandatory') unless block
         raise ArgumentError.new('event_type must be a subclass of BaseEvent') unless event_type.superclass == BaseEvent
         listeners_for(event_type).append(block)
+        [event_type, block]
+      end
+
+      # Unsubscribes from an event
+      # @param descriptor A descritor returned from {#subscribe}
+      def unsubscribe(descriptor)
+        listeners_for(descriptor[0]).delete(descriptor[1])
       end
 
       # @api private

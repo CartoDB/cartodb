0cf6a9bc185955bb40c3ebaf896c3412f99c8357
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-04-05 11:50:51 +0200

    Better explanation about valid parameters #11692

--- gears/carto_gears_api/lib/carto_gears_api/queue/jobs_service.rb
+++ gears/carto_gears_api/lib/carto_gears_api/queue/jobs_service.rb
@@ -5,7 +5,9 @@ module CartoGearsApi
       #     `CartoGearsApi::Queue::JobsService.new.send_job('MyModule::MyClass', :class_method, 'param1', 2)`
       # @param class_name [String] Name of the class that will be run in the queue.
       # @param class_method [String] Name of the class method (not instance method) to call.
-      # @param args Arguments to send to the class method.
+      # @param args Arguments to be sent send to the class method. This will be be serialized and passed
+      #     over to the job at the queue, so don't use models here. Instead, use simple types and perform searches in
+      #     the job if needed.
       def send_job(class_name, method, *args)
         ::Resque.enqueue(CartoGearsApi::Queue::GenericJob, class_name, method, *args)
       end

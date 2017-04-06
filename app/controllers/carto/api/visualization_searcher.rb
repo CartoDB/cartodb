4f9c508beb39ba87fad999d51f3480cf607e7c7d
Vishal Jadhav <vjadhav@bloomberg.net>
2017-04-06 13:42:59 -0400

    fix scrollbar, description, ol zoom

--- app/controllers/carto/api/visualization_searcher.rb
+++ app/controllers/carto/api/visualization_searcher.rb
@@ -58,8 +58,8 @@ module Carto
             vqb.with_owned_by_or_shared_with_user_id(current_user.id)
           when FILTER_SHARED_NO
             if samples
-              if Cartodb.config[:map_samples] && Cartodb.config[:map_samples]["user_id"]
-                vqb.with_user_id(Cartodb.config[:map_samples]["user_id"]) 
+              if Cartodb.config[:map_samples] && Cartodb.config[:map_samples]["username"]
+                vqb.with_user_id(Carto::User.where(username: Cartodb.config[:map_samples]["username"]).first.id) 
               else
                 raise "The sample user is not setup in app_config"
               end

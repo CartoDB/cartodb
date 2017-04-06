a25d972b88065fab7304d4e00f331fc1c5bcfef2
Javier Torres <jtorres@carto.com>
2017-04-06 16:38:36 +0200

    Destroy layers on Carto::Map destriction

--- app/models/carto/layers_map.rb
+++ app/models/carto/layers_map.rb
@@ -4,8 +4,8 @@ require 'active_record'
 
 module Carto
   class LayersMap < ActiveRecord::Base
-
-    belongs_to :layer, class_name: Carto::Layer
+    # Destroy the actual layer when removing it from the map (as layers cannot be in multiple maps)
+    belongs_to :layer, class_name: Carto::Layer, dependent: :destroy
     belongs_to :map, class_name: Carto::Map
 
   end

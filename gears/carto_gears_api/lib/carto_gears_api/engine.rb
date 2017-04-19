640493d77d0072bca8fa4d29c91188959150d756
Javier Torres <jtorres@carto.com>
2017-04-19 13:11:59 +0200

    Also eager load CartoGearsApi so it is loaded in production/tests

--- gears/carto_gears_api/lib/carto_gears_api/engine.rb
+++ gears/carto_gears_api/lib/carto_gears_api/engine.rb
@@ -2,6 +2,7 @@ module CartoGearsApi
   class Engine < ::Rails::Engine
     isolate_namespace CartoGearsApi
 
-    config.autoload_paths << config.root.join('lib')
+    config.autoload_paths << config.root.join('lib').to_s
+    config.eager_load_paths << config.root.join('lib').to_s
   end
 end

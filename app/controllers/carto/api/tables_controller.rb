9f1262c1dc632fbd326e317a900470251eda8aa6
Javier Torres <javier@javiertorres.eu>
2017-04-24 13:53:54 +0200

    Complete migrationg tables controller update to AR

--- app/controllers/carto/api/tables_controller.rb
+++ app/controllers/carto/api/tables_controller.rb
@@ -24,26 +24,26 @@ module Carto
       def create
         @stats_aggregator.timing('tables.create') do
           begin
-            @table = ::Table.new
-            @table.user_id = current_user.id
+            table = ::Table.new
+            table.user_id = current_user.id
 
             tables = Carto::Db::UserSchema.new(current_user).table_names
-            @table.name = Carto::ValidTableNameProposer.new.propose_valid_table_name(params[:name], taken_names: tables)
+            table.name = Carto::ValidTableNameProposer.new.propose_valid_table_name(params[:name], taken_names: tables)
 
-            @table.description    = params[:description]   if params[:description]
-            @table.the_geom_type  = params[:the_geom_type] if params[:the_geom_type]
-            @table.force_schema   = params[:schema]        if params[:schema]
-            @table.tags           = params[:tags]          if params[:tags]
-            @table.import_from_query = params[:from_query] if params[:from_query]
+            table.description    = params[:description]   if params[:description]
+            table.the_geom_type  = params[:the_geom_type] if params[:the_geom_type]
+            table.force_schema   = params[:schema]        if params[:schema]
+            table.tags           = params[:tags]          if params[:tags]
+            table.import_from_query = params[:from_query] if params[:from_query]
 
             save_status = @stats_aggregator.timing('save') do
-              @table.valid? && @table.save
+              table.valid? && table.save
             end
 
             if save_status
-              render_jsonp(@table.public_values(request: request), 200, location: "/tables/#{@table.id}")
+              render_jsonp(table.public_values(request: request), 200, location: "/tables/#{table.id}")
 
-              table_visualization = @table.table_visualization
+              table_visualization = table.table_visualization
               if table_visualization
                 current_viewer_id = current_viewer.id
                 Carto::Tracking::Events::CreatedDataset.new(current_viewer_id,
@@ -52,8 +52,8 @@ module Carto
                                                             origin: 'blank').report
               end
             else
-              CartoDB::StdoutLogger.info 'Error on tables#create', @table.errors.full_messages
-              render_jsonp({ description: @table.errors.full_messages, stack: @table.errors.full_messages }, 400)
+              CartoDB::StdoutLogger.info 'Error on tables#create', table.errors.full_messages
+              render_jsonp({ description: table.errors.full_messages, stack: table.errors.full_messages }, 400)
             end
           rescue CartoDB::QuotaExceeded
             current_viewer_id = current_viewer.id
@@ -66,8 +66,8 @@ module Carto
       end
 
       def update
+        table = @user_table.service
         @stats_aggregator.timing('tables.update') do
-
           begin
             warnings = []
 
@@ -76,41 +76,42 @@ module Carto
             # TODO consider removing this code. The entry point is only used to set lat/long columns
             unless params[:name].nil?
               new_name = params[:name].downcase
-              if new_name != @table.name
+              if new_name != table.name
                 # TODO reverse this logic: make explicit if this needs to start with a letter
                 if params[:name] =~ /\A[0-9_]/
                   raise "Table names can't start with numbers or dashes."
                 elsif current_user.tables.filter(:name.like(/\A#{params[:name]}/)).select_map(:name).include?(new_name)
                   raise "Table '#{new_name}' already exists."
                 else
-                  @table.set_all(name: new_name)
+                  table.set_all(name: new_name)
                   @stats_aggregator.timing('save-name') do
-                    @table.save(:name)
+                    table.save(:name)
                   end
                 end
               end
             end
 
-            @table.set_except(params, :name) # TODO: this is bad, passing all params blindly to the table object
+            # TODO: this is bad, passing all params blindly to the table object
+            @user_table.assign_attributes(params.symbolize_keys.reject { |k, _| k == :name })
             if params.keys.include?('latitude_column') && params.keys.include?('longitude_column')
               latitude_column  = params[:latitude_column]  == 'nil' ? nil : params[:latitude_column].try(:to_sym)
               longitude_column = params[:longitude_column] == 'nil' ? nil : params[:longitude_column].try(:to_sym)
               @stats_aggregator.timing('georeference') do
-                @table.georeference_from!(latitude_column: latitude_column, longitude_column: longitude_column)
+                table.georeference_from!(latitude_column: latitude_column, longitude_column: longitude_column)
               end
-              @table.update_bounding_box
-              render_jsonp(@table.public_values(request: request).merge(warnings: warnings))
+              table.update_bounding_box
+              render_jsonp(table.public_values(request: request).merge(warnings: warnings))
               return
             end
 
             update_status = @stats_aggregator.timing('save') do
-              @table.update(@table.values.delete_if { |k, _| k == :tags_names })
+              @user_table.save
             end
 
             if update_status != false
-              render_jsonp(@table.public_values(request: request).merge(warnings: warnings))
+              render_jsonp(table.public_values(request: request).merge(warnings: warnings))
             else
-              render_jsonp({ errors: @table.errors.full_messages }, 400)
+              render_jsonp({ errors: table.errors.full_messages }, 400)
             end
           rescue => e
             CartoDB::StdoutLogger.info e.class.name, e.message

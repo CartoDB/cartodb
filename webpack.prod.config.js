6b05750128c40a9f2ac31ffb020df8a3d0b3a3f8
nobuti <buti@nobuti.com>
2017-04-25 16:31:01 +0200

    Split webpack configuration.
    We will need to refactor to DRY.

---
+++ webpack.prod.config.js
@@ -0,0 +1,115 @@
+const webpack = require('webpack');
+const {resolve} = require('path');
+const PACKAGE = require('./package.json');
+const version = PACKAGE.version;
+
+const isVendor = (module, count) => {
+  const userRequest = module.userRequest;
+  return userRequest && userRequest.indexOf('node_modules') >= 0;
+};
+
+const entryPoints = {
+  embed: './lib/assets/core/javascripts/cartodb3/public_editor.js',
+  dataset: './lib/assets/core/javascripts/cartodb3/dataset.js',
+  builder: './lib/assets/core/javascripts/cartodb3/editor.js'
+};
+
+module.exports = env => {
+  return {
+    entry: entryPoints,
+    output: {
+      filename: `${version}/javascripts/[name].js`,
+      path: resolve(__dirname, 'public/assets')
+    },
+    devtool: 'source-map',
+    plugins: Object.keys(entryPoints)
+        .map(entry => new webpack.optimize.CommonsChunkPlugin({
+          name: `${entry}_vendor`,
+          chunks: [entry],
+          minChunks: isVendor
+        }))
+    .concat([
+      // Extract common chuncks from the 3 vendor files
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'common',
+        chunks: Object.keys(entryPoints).map(n => `${n}_vendor`),
+        minChunks: (module, count) => {
+          return count >= Object.keys(entryPoints).length && isVendor(module);
+        }
+      }),
+
+      // Extract common chuncks from the 3 entry points
+      new webpack.optimize.CommonsChunkPlugin({
+        children: true,
+        minChunks: Object.keys(entryPoints).length
+      }),
+
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        ['window.jQuery']: 'jquery'
+      }),
+
+      // Minify
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      })
+    ]),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'shim-loader',
+          include: [
+            resolve(__dirname, 'node_modules/cartodb.js')
+          ],
+          options: {
+            shim: {
+              'wax.cartodb.js': {
+                exports: 'wax'
+              },
+              'html-css-sanitizer': {
+                exports: 'html'
+              }
+            }
+          }
+        },
+        {
+          test: /\.tpl$/,
+          use: 'tpl-loader',
+          include: [
+            resolve(__dirname, 'lib/assets/core/javascripts/cartodb3'),
+            resolve(__dirname, 'node_modules/cartodb.js'),
+            resolve(__dirname, 'node_modules/cartodb-deep-insights.js')
+          ]
+        },
+        {
+          test: /\.mustache$/,
+          use: 'raw-loader',
+          include: [
+            resolve(__dirname, 'lib/assets/core/javascripts/cartodb3'),
+            resolve(__dirname, 'node_modules/cartodb.js'),
+            resolve(__dirname, 'node_modules/cartodb-deep-insights.js')
+          ]
+        }
+      ]
+    },
+
+    node: {
+      fs: 'empty' // This fixes the error Module not found: Error: Can't resolve 'fs'
+    },
+
+    stats: {
+      warnings: false
+    }
+  };
+};

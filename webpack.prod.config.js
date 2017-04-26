34641358b19e925c785906b6ea058270efe0232b
Carlos Matallín <matallo@carto.com>
2017-04-26 17:15:52 +0200

    test remove minify

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -48,20 +48,20 @@ module.exports = env => {
         $: 'jquery',
         jQuery: 'jquery',
         ['window.jQuery']: 'jquery'
-      }),
+      // }),
 
-      // Minify
-      new webpack.optimize.UglifyJsPlugin({
-        sourceMap: true,
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true
-        },
-        compress: {
-          screw_ie8: true
-        },
-        comments: false
+      // // Minify
+      // new webpack.optimize.UglifyJsPlugin({
+      //   sourceMap: true,
+      //   beautify: false,
+      //   mangle: {
+      //     screw_ie8: true,
+      //     keep_fnames: true
+      //   },
+      //   compress: {
+      //     screw_ie8: true
+      //   },
+      //   comments: false
       })
     ]),
     module: {

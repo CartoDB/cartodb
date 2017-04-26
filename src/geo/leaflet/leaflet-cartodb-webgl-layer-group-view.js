f45af5964afb792d559f6d244864b50e8a657cfc
Francisco López <donflopez@gmail.com>
2017-04-26 15:11:05 +0200

    Add tooltip and infowindow behaviour to vector

--- src/geo/leaflet/leaflet-cartodb-webgl-layer-group-view.js
+++ src/geo/leaflet/leaflet-cartodb-webgl-layer-group-view.js
@@ -7,6 +7,7 @@ var LeafletCartoDBWebglLayerGroupView = function (layerGroupModel, leafletMap) {
   LeafletLayerView.apply(this, arguments);
 
   this.tangram = new TC(leafletMap, this.initConfig.bind(this, layerGroupModel));
+  this.layerGroupModel = layerGroupModel;
 };
 
 LeafletCartoDBWebglLayerGroupView.prototype = _.extend(
@@ -20,7 +21,44 @@ LeafletCartoDBWebglLayerGroupView.prototype = _.extend(
 
       layerGroupModel.forEachGroupedLayer(this._onLayerAdded, this);
       layerGroupModel.onLayerAdded(this._onLayerAdded.bind(this));
+
+      this._addInteractiveEvents();
     },
+
+    _addInteractiveEvents: function () {
+      var hovered = false;
+      var self = this;
+      this.tangram.layer.setSelectionEvents({
+        hover: function (e) {
+          if (e.feature) {
+            hovered = true;
+            self.trigger('featureOver', self._getFeatureObject(e));
+          }
+          else if (hovered) {
+            hovered = false;
+            self.trigger('featureOut', self._getFeatureObject(e));
+          }
+        },
+        click: function (e) {
+          if (e.feature) {
+            self.trigger('featureClick', self._getFeatureObject(e));
+          }
+        }
+      });
+    },
+    _getFeatureObject: function (e) {
+      var index = 0;
+      var layer = this.layerGroupModel.getLayerInLayerGroupAt(index);
+
+      return {
+        layer: layer,
+        layerIndex: index,
+        latlng: e.leaflet_event.latlng,
+        position: e.pixel,
+        feature: e.feature
+      };
+    },
+
     _createLeafletLayer: function () {
       var leafletLayer = new L.Layer();
       leafletLayer.onAdd = function () {};

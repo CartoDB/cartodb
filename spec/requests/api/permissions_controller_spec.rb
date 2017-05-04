1e03d0a1ca45321fa6e3df5b88ce56d5f17a97dc
Javier Torres <jtorres@carto.com>
2017-05-04 18:07:43 +0200

    Fix failing test due to user presenter changes (in permission)

--- spec/requests/api/permissions_controller_spec.rb
+++ spec/requests/api/permissions_controller_spec.rb
@@ -72,6 +72,7 @@ describe Api::Json::PermissionsController do
                   username:   @user2.username,
                   avatar_url: @user2.avatar_url,
                   viewer:     false,
+                  org_admin:  false,
                   base_url:   @user2.public_url,
                   groups:     []
               },

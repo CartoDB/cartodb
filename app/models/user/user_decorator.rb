b965e70faaa77a0e6f2c124c1441b0afc45dd3fa
Javier Álvarez Medina <xavijam@gmail.com>
2017-05-29 15:12:34 +0200

    Adding same change in user_decorator

--- app/models/user/user_decorator.rb
+++ app/models/user/user_decorator.rb
@@ -111,7 +111,7 @@ module CartoDB
           import_table_rows: max_import_table_row_count,
           max_layers: max_layers
         },
-        google_maps_key: google_maps_key,
+        google_maps_api_key: google_maps_api_key,
         notification: notification,
         avatar_url: avatar,
         feature_flags: feature_flags,

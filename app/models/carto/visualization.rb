68a703f9ce1a6b1d7d42a4d734d497a7b05c58fb
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-05-29 17:23:12 +0200

    Removed transition_options Odyssey attributes

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -172,14 +172,6 @@ class Carto::Visualization < ActiveRecord::Base
     @stats ||= CartoDB::Visualization::Stats.new(self).to_poro
   end
 
-  def transition_options
-    @transition_options ||= (slide_transition_options.nil? ? {} : JSON.parse(slide_transition_options).symbolize_keys)
-  end
-
-  def transition_options=(value)
-    self.slide_transition_options = ::JSON.dump(value.nil? ? DEFAULT_OPTIONS_VALUE : value)
-  end
-
   def children
     ordered = []
     children_vis = self.unordered_children

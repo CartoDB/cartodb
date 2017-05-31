75ae03326b85a8b981bfcad618ab6af643405e71
Francisco López <donflopez@gmail.com>
2017-05-31 09:29:05 +0200

    Fix multiple set view

--- src/geo/leaflet/leaflet-map-view.js
+++ src/geo/leaflet/leaflet-map-view.js
@@ -170,7 +170,9 @@ var LeafletMapView = MapView.extend({
   },
 
   _setView: function () {
-    this._leafletMap.flyTo(this.map.get('center'), this.map.get('zoom') || 0);
+    if (this.map.hasChanged('zoom') || this.map.hasChanged('center')) {
+      this._leafletMap.flyTo(this.map.get('center'), this.map.get('zoom') || 0);
+    }
   },
 
   _getNativeMap: function () {

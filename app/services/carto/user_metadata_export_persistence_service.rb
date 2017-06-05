b6916d05a106ee84fd63d6e97dd132f742670646
Javier Torres <javier@javiertorres.eu>
2017-06-05 17:13:51 +0200

    Correctly persist user visualizations

--- app/services/carto/user_metadata_export_persistence_service.rb
+++ app/services/carto/user_metadata_export_persistence_service.rb
@@ -6,20 +6,16 @@ module Carto
   # Export/import is versioned, but importing should generate a model tree that can be persisted by this class
   class UserMetadataExportPersistenceService
     def save_import(user)
-      user = ActiveRecord::Base.transaction do
-        visualizations = user.visualizations
-        user.visualizations = []
+      visualizations = user.visualizations.dup
+      user.visualizations = []
 
-        user.save!
-
-        vis_persister = VisualizationsExportPersistenceService.new
-        visualizations.each do |vis|
-          vis_persister.save_import(user, vis)
-        end
-
-        user
-      end
+      user.save!
       ::User.find(id: user.id).after_save
+
+      vis_persister = VisualizationsExportPersistenceService.new
+      visualizations.each do |vis|
+        vis_persister.save_import(user, vis)
+      end
 
       user
     end

c1cce803083a9e743ad9da8c7af5dc6d3fd8f177
Javier Torres <jtorres@carto.com>
2017-06-09 11:55:22 +0200

    Support api key signing in google maps

--- lib/carto/google_maps_api_signer.rb
+++ lib/carto/google_maps_api_signer.rb
@@ -3,18 +3,30 @@ require 'openssl'
 
 module Carto
   class GoogleMapsApiSigner
-    def initialize(signature_key)
-      @key = signature_key
+    def initialize(user)
+      @user = user
     end
 
     def sign(url)
+      raise 'User does not have Google configured' unless @user.google_maps_query_string.present?
+      url_with_qs = "#{url}&#{@user.google_maps_query_string}"
+      if @user.google_maps_client_id.present? && @user.google_maps_private_key.present?
+        # Add client=xxx + signature
+        cryptographically_sign_url(url_with_qs)
+      else
+        # Just add key=xxx
+        url_with_qs
+      end
+    end
+
+    private
+
+    def cryptographically_sign_url(url)
       binary_signature = hmac(uri_path_and_query(url))
       signature = Base64.urlsafe_encode64(binary_signature)
 
       "#{url}&signature=#{signature}"
     end
-
-    private
 
     def uri_path_and_query(url)
       uri = URI.parse(url)
@@ -26,7 +38,7 @@ module Carto
     end
 
     def binary_key
-      Base64.urlsafe_decode64(@key)
+      Base64.urlsafe_decode64(@user.google_maps_private_key)
     end
   end
 end
--- spec/lib/carto/google_maps_api_signer_spec.rb
+++ spec/lib/carto/google_maps_api_signer_spec.rb
@@ -5,9 +5,29 @@ require 'carto/google_maps_api_signer'
 
 module Carto
   describe GoogleMapsApiSigner do
-    it 'signs google maps urls' do
-      gmas = GoogleMapsApiSigner.new('MjM0MzJk-3N_czQzJmFkc2Rhc2Q=')
-      expect(gmas.sign('https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c-73.998&zoom=12&size=400x400&key=wadus')).to eq 'https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c-73.998&zoom=12&size=400x400&key=wadus&signature=Z7NEIja_cCwV7xtNpbXo7l_wrv0='
+    let(:url) { 'https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c-73.998&zoom=12&size=400x400' }
+
+    describe 'with client_id' do
+      # You can generate test signature with https://developers.google.com/maps/documentation/static-maps/get-api-key
+      let(:user) do
+        FactoryGirl.build(:user, google_maps_key: 'client=wds', google_maps_private_key: 'MjM0MzJk-3N_czQzJmFkc2Rhc2Q=')
+      end
+
+      it 'signs google maps urls' do
+        gmas = GoogleMapsApiSigner.new(user)
+        expect(gmas.sign(url)).to eq "#{url}&client=wds&signature=fK5mHV9MLROw4xZnBPcrCOUdjio="
+      end
+    end
+
+    describe 'with api_key' do
+      let(:user) do
+        FactoryGirl.build(:user, google_maps_key: 'key=xxx')
+      end
+
+      it 'signs google maps urls' do
+        gmas = GoogleMapsApiSigner.new(user)
+        expect(gmas.sign(url)).to eq "#{url}&key=xxx"
+      end
     end
   end
 end

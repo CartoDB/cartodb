7275b77465009e0ae34788fc77d566ea3ee4d5eb
Javier Torres <javier@javiertorres.eu>
2017-06-13 13:07:15 +0200

    Fix conflict with log keyword in rake

--- spec/factories/synchronizations.rb
+++ spec/factories/synchronizations.rb
@@ -5,9 +5,12 @@ FactoryGirl.define do
     interval 2592000
     url 'https://common-data.cartodb.com/api/v2/sql?q=select+*+from+%22histcounties_1979_present%22&format=shp&filename=histcounties_1979_present'
     state 'success'
-    log FactoryGirl.build(:carto_log, type: 'sync')
     type_guessing true
     quoted_fields_guessing true
     content_guessing true
+
+    after(:build) do |sync|
+      sync.log = FactoryGirl.build(:carto_log, type: 'sync')
+    end
   end
 end

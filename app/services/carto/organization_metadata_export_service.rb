bd07940b62c824a6ce2fed593e29ac7f9d291978
Javier Torres <jtorres@carto.com>
2017-06-14 13:42:45 +0200

    Fix whitelist domain inserting in AR organization

--- app/services/carto/organization_metadata_export_service.rb
+++ app/services/carto/organization_metadata_export_service.rb
@@ -7,10 +7,10 @@ module Carto
     CURRENT_VERSION = '1.0.0'.freeze
     EXPORTED_ORGANIZATION_ATTRIBUTES = [
       :id, :seats, :quota_in_bytes, :created_at, :updated_at, :name, :avatar_url, :owner_id, :website, :description,
-      :display_name, :discus_shortname, :twitter_organizationname, :geocoding_quota, :map_view_quota, :auth_token,
+      :display_name, :discus_shortname, :twitter_username, :geocoding_quota, :map_view_quota, :auth_token,
       :geocoding_block_price, :map_view_block_price, :twitter_datasource_enabled, :twitter_datasource_block_price,
       :twitter_datasource_block_size, :twitter_datasource_quota, :google_maps_key, :google_maps_private_key, :color,
-      :default_quota_in_bytes, :whitelisted_email_domains, :admin_email, :auth_organizationname_password_enabled,
+      :default_quota_in_bytes, :whitelisted_email_domains, :admin_email, :auth_username_password_enabled,
       :auth_google_enabled, :location, :here_isolines_quota, :here_isolines_block_price, :strong_passwords_enabled,
       :obs_snapshot_quota, :obs_snapshot_block_price, :obs_general_quota, :obs_general_block_price,
       :salesforce_datasource_enabled, :viewer_seats, :geocoder_provider, :isolines_provider, :routing_provider,

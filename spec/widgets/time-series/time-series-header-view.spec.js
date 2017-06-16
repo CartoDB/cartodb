c49329fe7c6302ca312e15f01c9466713667b7ff
Ivan Malagon <hacheka@gmail.com>
2017-06-16 16:50:55 +0200

    Add two missing specs

--- spec/widgets/time-series/time-series-header-view.spec.js
+++ spec/widgets/time-series/time-series-header-view.spec.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone');
 var TimeSeriesHeaderView = require('../../../src/widgets/time-series/time-series-header-view');
+var AnimateValues = require('../../../src/widgets/animate-values.js');
 
 describe('widgets/time-series/time-series-header-view', function () {
   var isFilterEmpty = true;
@@ -38,6 +39,14 @@ describe('widgets/time-series/time-series-header-view', function () {
 
       expect(this.view.$el.html().indexOf('Selected from')).toBe(-1);
     });
+
+    it('should call to _animateValue', function () {
+      spyOn(this.view, '_animateValue');
+
+      this.view.render();
+
+      expect(this.view._animateValue).toHaveBeenCalled();
+    });
   });
 
   describe('._initBinds', function () {
@@ -55,6 +64,35 @@ describe('widgets/time-series/time-series-header-view', function () {
       this.view._rangeFilter.set('min', 5);
 
       expect(this.view.render).toHaveBeenCalled();
+    });
+  });
+
+  describe('._animateValue', function () {
+    it('should animate from _selectedAmount to totalAmount if no filter is present', function () {
+      spyOn(AnimateValues.prototype, 'animateFromValues');
+      this.view._selectedAmount = 49;
+      this.view._dataviewModel.set('totalAmount', 69, { silent: true });
+
+      this.view._animateValue();
+
+      var args = AnimateValues.prototype.animateFromValues.calls.mostRecent().args;
+      expect(args[0]).toBe(49);
+      expect(args[1]).toBe(69);
+    });
+
+    it('should animate from _selectedAmount to filteredAmount if filter is present', function () {
+      spyOn(AnimateValues.prototype, 'animateFromValues');
+      spyOn(this.view._rangeFilter, 'isEmpty').and.returnValue(false);
+
+      this.view._selectedAmount = 49;
+      this.view._dataviewModel.set('totalAmount', 69, { silent: true });
+      this.view._dataviewModel.set('filteredAmount', 89, { silent: true });
+
+      this.view._animateValue();
+
+      var args = AnimateValues.prototype.animateFromValues.calls.mostRecent().args;
+      expect(args[0]).toBe(49);
+      expect(args[1]).toBe(89);
     });
   });
 });

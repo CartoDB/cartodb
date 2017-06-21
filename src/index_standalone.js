0fa41235abe85d849f8e9de27a1125bb1cf7d801
Carlos Matallín <matallo@carto.com>
2017-06-21 14:28:13 +0200

    i18n

--- src/index_standalone.js
+++ src/index_standalone.js
@@ -1,6 +1,16 @@
+var ACTIVE_LOCALE = 'en';
+var Locale = require('./locale/index');
+var Polyglot = require('node-polyglot');
+var polyglot = new Polyglot({
+  locale: ACTIVE_LOCALE, // Needed for pluralize behaviour
+  phrases: Locale[ACTIVE_LOCALE]
+});
+window._t = polyglot.t.bind(polyglot);
+
 var cdb = require('cartodb.js');
 cdb.deepInsights = {
   VERSION: require('../package.json').version,
   createDashboard: require('./api/create-dashboard')
 };
 module.exports = cdb;
+

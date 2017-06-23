825c674cb5e6503cadb3ab70c7f8fd5521b24a83
Carlos Matallín <matallo@carto.com>
2017-06-23 18:58:57 +0200

    torquehistogram tests

---
+++ spec/widgets/time-series/torque-histogram-view.spec.js
@@ -0,0 +1,58 @@
+var Backbone = require('backbone');
+var TorqueHistogramView = require('../../../src/widgets/time-series/torque-histogram-view');
+var specHelper = require('../../spec-helper');
+var HistogramChartView = require('../../../src/widgets/histogram/chart');
+var HistogramView = require('../../../src/widgets/time-series/histogram-view');
+
+describe('widgets/time-series/torque-histogram-view', function () {
+  var isFilterEmpty = true;
+
+  beforeEach(function () {
+    var vis = specHelper.createDefaultVis();
+
+    this.timeSeriesModel = new Backbone.Model();
+    this.timeSeriesModel.getWidgetColor = function () {};
+
+    this.dataviewModel = vis.dataviews.createHistogramModel(vis.map.layers.first(), {
+      column: 'dates',
+      bins: 256
+    });
+    this.dataviewModel.set({
+      start: 0,
+      end: 1
+    });
+
+    this.torqueLayerModel = new cdb.geo.TorqueLayer({
+      isRunning: false,
+      step: 0,
+      steps: 256,
+      start: 0,
+      end: 1000
+    }, {
+      vis: vis
+    });
+
+    spyOn(HistogramChartView.prototype, '_setupFillColor').and.returnValue('red');
+
+    this.view = new TorqueHistogramView({
+      timeSeriesModel: this.timeSeriesModel,
+      model: this.dataviewModel,
+      rangeFilter: this.dataviewModel.filter,
+      torqueLayerModel: this.torqueLayerModel,
+      displayShadowBars: false,
+      normalized: true
+    });
+    this.view.render();
+  });
+
+  describe('._onBrushClick', function () {
+    it('should set torque layer step', function () {
+      spyOn(HistogramView.prototype, 'resetFilter');
+
+      this.view._onBrushClick(12);
+
+      expect(HistogramView.prototype.resetFilter).toHaveBeenCalled();
+      expect(this.torqueLayerModel.get('step')).toBe(12);
+    });
+  });
+});

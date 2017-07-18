fef319b5daff86dcb783dc00dc20033022e9edfa
Javier Álvarez Medina <xavijam@gmail.com>
2017-07-18 13:13:16 +0200

    Taking the correct sql_wrap attribute when it is present instead of query_wrapper

--- src/geo/torque-layer-view-base.js
+++ src/geo/torque-layer-view-base.js
@@ -179,7 +179,7 @@ var TorqueLayerViewBase = {
 
   _getQuery: function (layerModel) {
     var query = layerModel.get('query');
-    var qw = layerModel.get('query_wrapper');
+    var qw = layerModel.get('sql_wrap');
     if (qw) {
       query = _.template(qw)({ sql: query || ('select * from ' + layerModel.get('table_name')) });
     }

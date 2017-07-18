5c12427b80cca5bff0977f8b2093feb195f35cb1
Carlos Matallín <matallo@carto.com>
2017-07-18 17:43:18 +0200

    fix tests

--- spec/formatter.spec.js
+++ spec/formatter.spec.js
@@ -1,6 +1,8 @@
 var formatter = require('../src/formatter');
 
 describe('formatter', function () {
+  var timestamp = 1494066976; // 2017-05-06 10:36:16
+
   it('should format numbers', function () {
     expect(formatter.formatNumber(0)).toBe(0);
     expect(formatter.formatNumber(0.001)).toBe('0.00100');
@@ -23,13 +25,20 @@ describe('formatter', function () {
     expect(formatter.formatNumber('I am not a number')).toBe('I am not a number');
   });
 
-  it('should format timestamps correctly', function () {
-    var timestamp = 1494066976; // 2017-05-06 10:36:16
-    expect(formatter.timestampFactory('year')(timestamp)).toEqual('2017');
-    expect(formatter.timestampFactory('quarter')(timestamp)).toEqual('Q2 2017');
-    expect(formatter.timestampFactory('month')(timestamp)).toEqual('May 2017');
-    expect(formatter.timestampFactory('week')(timestamp)).toEqual('6th May 2017');
-    expect(formatter.timestampFactory('day')(timestamp)).toEqual('6th May 2017');
-    expect(formatter.timestampFactory('minute')(timestamp)).toEqual('10:36 05/06/2017');
+  describe('without offset', function () {
+    it('should format timestamps correctly', function () {
+      expect(formatter.timestampFactory('year', 0)(timestamp)).toEqual('2017');
+      expect(formatter.timestampFactory('quarter', 0)(timestamp)).toEqual('Q2 2017');
+      expect(formatter.timestampFactory('month', 0)(timestamp)).toEqual('May 2017');
+      expect(formatter.timestampFactory('week', 0)(timestamp)).toEqual('6th May 2017');
+      expect(formatter.timestampFactory('day', 0)(timestamp)).toEqual('6th May 2017');
+      expect(formatter.timestampFactory('minute', 0)(timestamp)).toEqual('10:36 05/06/2017');
+    });
+  });
+
+  describe('with offset', function () {
+    it('should format timestamps correctly', function () {
+      expect(formatter.timestampFactory('minute', 3600)(timestamp)).toEqual('11:36 05/06/2017');
+    });
   });
 });

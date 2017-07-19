254831955a48304ff0393baecaf74136b3aa2aa9
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-07-19 18:32:11 +0200

    Hound

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -60,14 +60,14 @@ class Admin::PagesController < Admin::AdminController
         redirect_to CartoDB.base_url(@viewed_user.organization.name) << CartoDB.path(self, 'public_sitemap') and return
       end
 
-      visualizations = Carto::VisualizationQueryBuilder.
-        new.
-        with_user_id(@viewed_user.id).
-        with_privacy(Carto::Visualization::PRIVACY_PUBLIC).
-        with_order('visualizations.updated_at', :desc).
-        without_raster.
-        with_prefetch_user(true).
-        build
+      visualizations = Carto::VisualizationQueryBuilder
+        .new
+        .with_user_id(@viewed_user.id)
+        .with_privacy(Carto::Visualization::PRIVACY_PUBLIC)
+        .with_order('visualizations.updated_at', :desc)
+        .without_raster
+        .with_prefetch_user(true)
+        .build
     end
 
     @urls = visualizations.map { |vis|

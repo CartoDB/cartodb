cce99fc738192785a7d5232a509bf97dcb583b1e
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-07-21 09:34:28 +0200

    Workaround for wrong change detection on tag change

--- app/models/carto/external_source.rb
+++ app/models/carto/external_source.rb
@@ -12,7 +12,8 @@ class Carto::ExternalSource < ActiveRecord::Base
 
   def update_data(import_url, geometry_types, rows_counted, size, username = nil)
     self.import_url = import_url
-    self.geometry_types = geometry_types
+    # This check avoids a "false change" if the data is actually equal (AR bug on arrays)
+    self.geometry_types = geometry_types unless equal_pg_array?(self.geometry_types, geometry_types)
     self.rows_counted = rows_counted
     self.size = size
     self.username = username
@@ -21,5 +22,15 @@ class Carto::ExternalSource < ActiveRecord::Base
 
   def importable_by?(user)
     user.present? && visualization.user_id == user.id
+  end
+
+  private
+
+  # True if array (`['ST_MultiPolygon']`) contains the same elements than string, in PG notation (`{ST_MultiPolygon}`)
+  def equal_pg_array?(array, string)
+    array = [] if array.nil?
+    string = '{}' if string.nil?
+
+    "{#{array.join(',')}}" == string
   end
 end

a7a0a97958d60aab78e373ddd22a834aaa7dd889
Javier Álvarez Medina <xavijam@gmail.com>
2017-07-24 16:46:06 +0200

    Changed torque layer animation duration method

--- src/geo/map/torque-layer.js
+++ src/geo/map/torque-layer.js
@@ -2,6 +2,7 @@ var _ = require('underscore');
 var LayerModelBase = require('./layer-model-base');
 var carto = require('carto');
 var Legends = require('./legends/legends');
+var postcss = require('postcss');
 var ATTRIBUTES_THAT_TRIGGER_VIS_RELOAD = ['sql', 'sql_wrap', 'source', 'cartocss'];
 var TORQUE_LAYER_CARTOCSS_PROPS = [
   '-torque-frame-count',
@@ -12,6 +13,7 @@ var TORQUE_LAYER_CARTOCSS_PROPS = [
 ];
 var LAYER_NAME_IN_CARTO_CSS = 'Map';
 var DEFAULT_ANIMATION_DURATION = 30;
+var TORQUE_DURATION_ATTRIBUTE = '-torque-animation-duration';
 
 /**
  * Model for a Torque Layer
@@ -31,18 +33,18 @@ var TorqueLayer = LayerModelBase.extend({
     time: undefined // should be a Date instance
   },
 
-  _animationDurationRegex: /-torque-animation-duration: ([0-9]+)/,
-
   // Helper method to be used from a few places, it parses torque cartocss to get
   // the animation duration or a default duration
   getAnimationDuration: function (cartocss) {
-    var match = cartocss.match(this._animationDurationRegex);
+    var cssTree = postcss().process(cartocss);
+    var root = cssTree.result.root;
+    var torqueDuration = DEFAULT_ANIMATION_DURATION;
 
-    if (match === null) {
-      return DEFAULT_ANIMATION_DURATION;
-    }
+    root.walkDecls(TORQUE_DURATION_ATTRIBUTE, function (decl) {
+      torqueDuration = parseInt(decl.value, 10);
+    });
 
-    return parseInt(match[1], 10);
+    return torqueDuration;
   },
 
   initialize: function (attrs, options) {

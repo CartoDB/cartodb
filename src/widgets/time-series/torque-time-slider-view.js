697886ef62c01e333ca098a3d0d99b88d0b6746c
Román Jiménez <roman@cartodb.com>
2017-07-31 14:52:13 +0200

    Listen to changes in dataviewmodel range

--- src/widgets/time-series/torque-time-slider-view.js
+++ src/widgets/time-series/torque-time-slider-view.js
@@ -59,6 +59,8 @@ module.exports = cdb.core.View.extend({
 
     this.listenTo(this._dataviewModel, 'change:bins', this._updateChartandTimeslider);
     this.listenTo(this._dataviewModel.filter, 'change:min change:max', this._onFilterMinMaxChange);
+
+    this.listenTo(this._dataviewModel, 'change:start change:end', this._updateChartandTimeslider);
   },
 
   clean: function () {

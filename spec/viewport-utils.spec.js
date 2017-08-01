1737b7ee999a0e8dc05b2177d4a608805de6ffaa
Carlos Matallín <matallo@carto.com>
2017-08-01 16:01:22 +0200

    viewport utils specs

---
+++ spec/viewport-utils.spec.js
@@ -0,0 +1,36 @@
+var $ = require('jquery');
+var viewportUtils = require('../src/viewport-utils');
+
+describe('viewport-utils', function () {
+  beforeEach(function () {
+    spyOn($.prototype, 'width').and.returnValue(100);
+  });
+
+
+  describe('._isViewport', function () {
+    it('should return true if window is lower than provided viewport', function () {
+      expect(viewportUtils._isViewport(101)).toBe(true);
+      expect(viewportUtils._isViewport(99)).toBe(false);
+    });
+  });
+
+  describe('.isMobileViewport', function () {
+    it('should return true if window is mobile', function () {
+      spyOn(viewportUtils, '_isViewport');
+
+      viewportUtils.isMobileViewport();
+
+      expect(viewportUtils._isViewport).toHaveBeenCalledWith(480);
+    });
+  });
+
+  describe('.isTabletViewport', function () {
+    it('should return true if window is tablet', function () {
+      spyOn(viewportUtils, '_isViewport');
+
+      viewportUtils.isTabletViewport();
+
+      expect(viewportUtils._isViewport).toHaveBeenCalledWith(759);
+    });
+  });
+});

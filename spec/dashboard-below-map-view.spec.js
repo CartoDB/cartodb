e16a6630cbb0dbe331e738f9f4bdf7e3d4cf7806
nobuti <buti@nobuti.com>
2017-08-10 18:22:22 +0200

    Create torque view if layer has animation style.

--- spec/dashboard-below-map-view.spec.js
+++ spec/dashboard-below-map-view.spec.js
@@ -1,5 +1,49 @@
+var _ = require('underscore');
 var Backbone = require('backbone');
 var DashboardBelowMapView = require('../src/dashboard-below-map-view');
+var TimeSeriesContentView = require('../src/widgets/time-series/content-view');
+var TorqueTimeSeriesContentView = require('../src/widgets/time-series/torque-content-view');
+
+var createFakeTimeSeriesWidgetModel = function (attrs) {
+  attrs = _.extend({}, attrs, {
+    type: 'time-series'
+  });
+
+  return new Backbone.Model(attrs);
+}
+
+var createFakeDataviewModel = function (attrs) {
+  attrs = _.extend({}, {
+    source: {
+      id: 'a0'
+    }
+  }, attrs);
+
+  var dataview = new Backbone.Model(attrs);
+
+  dataview.getUnfilteredDataModel = function () {
+    return new Backbone.Model();
+  };
+  dataview.getUnfilteredData = function () {
+    return {};
+  };
+  dataview.getSourceType = function () {
+    return '';
+  };
+  dataview.getLayerName = function () {
+    return '';
+  };
+
+  return dataview;
+}
+
+var createFakeDataviewLayer = function (attrs) {
+  attrs = _.extend({}, {
+    hasAnimationStyle: false
+  }, attrs);
+
+  return new Backbone.Model(attrs);
+}
 
 describe('dashboard-below-map-view', function () {
   beforeEach(function () {
@@ -16,32 +60,53 @@ describe('dashboard-below-map-view', function () {
 
   describe('when a time-series widget is added', function () {
     beforeEach(function () {
-      var timeSeriesWidgetModelFake = new Backbone.Model({
-        type: 'time-series'
-      });
-      timeSeriesWidgetModelFake.dataviewModel = new Backbone.Model({
-        source: {
-          id: 'a0'
-        }
-      });
-      timeSeriesWidgetModelFake.dataviewModel.getUnfilteredDataModel = function () {
-        return new Backbone.Model();
-      };
-      timeSeriesWidgetModelFake.dataviewModel.getUnfilteredData = function () {
-        return {};
-      };
-      timeSeriesWidgetModelFake.dataviewModel.getSourceType = function () {
-        return '';
-      };
-      timeSeriesWidgetModelFake.dataviewModel.getLayerName = function () {
-        return '';
-      };
-      timeSeriesWidgetModelFake.dataviewModel.layer = new Backbone.Model();
+      var timeSeriesWidgetModelFake = createFakeTimeSeriesWidgetModel();
+      timeSeriesWidgetModelFake.dataviewModel = createFakeDataviewModel();
+      timeSeriesWidgetModelFake.dataviewModel.layer = createFakeDataviewLayer();
       this.widgetsCollection.add(timeSeriesWidgetModelFake);
     });
 
     it('should render view', function () {
       expect(this.view.$el.attr('style')).not.toContain('none');
+    });
+  });
+
+  describe('widgetFactory', function () {
+    var getDefinitions = function (view, model) {
+      return _.find(view._widgetViewFactory.defs, function (def) {
+        return def.match(model);
+      });
+    };
+
+    beforeEach(function () {
+      spyOn(TimeSeriesContentView.prototype, 'initialize');
+      spyOn(TorqueTimeSeriesContentView.prototype, 'initialize');
+    });
+
+    it('should create time series widget if not has animated styles', function () {
+      this.model = createFakeTimeSeriesWidgetModel();
+      this.model.dataviewModel = createFakeDataviewModel();
+      this.model.dataviewModel.layer = createFakeDataviewLayer();
+
+      var def = getDefinitions(this.view, this.model);
+      def.createContentView(this.model);
+
+      expect(TimeSeriesContentView.prototype.initialize).toHaveBeenCalled();
+      expect(TorqueTimeSeriesContentView.prototype.initialize).not.toHaveBeenCalled();
+    });
+
+    it('should create torque time series widget if has animated styles', function () {
+      this.model = createFakeTimeSeriesWidgetModel();
+      this.model.dataviewModel = createFakeDataviewModel();
+      this.model.dataviewModel.layer = createFakeDataviewLayer({
+        hasAnimationStyle: true
+      });
+
+      var def = getDefinitions(this.view, this.model);
+      def.createContentView(this.model);
+
+      expect(TimeSeriesContentView.prototype.initialize).not.toHaveBeenCalled();
+      expect(TorqueTimeSeriesContentView.prototype.initialize).toHaveBeenCalled();
     });
   });
 });

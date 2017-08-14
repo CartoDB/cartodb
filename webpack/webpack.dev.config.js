8493b4f777a22206ac336202820db053293ab808
Carlos Matallín <matallo@carto.com>
2017-08-14 15:06:14 +0200

    revert webpack on top of browserify

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -6,11 +6,11 @@ const PACKAGE = require('./../package.json');
 const version = PACKAGE.version;
 
 const entryPoints = {
-  dashboard_static: [
+  static: [
     // './tmp/assets/javascripts/cdb_static.js',
     // './tmp/assets/javascripts/models_static.js',
     // './tmp/assets/javascripts/dashboard_deps_static.js',
-    '../lib/assets/javascripts/cartodb/dashboard/static.js'
+    './lib/assets/javascripts/cartodb/dashboard/static.js'
   ]
 };
 
@@ -22,34 +22,6 @@ module.exports = {
     publicPath: '/assets/'
   },
   devtool: 'cheap-module-source-map',
-  resolve: {
-    alias: {
-      'jst': path.resolve(__dirname, '../tmp/assets/javascripts/dashboard_templates_static.js')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'shim-loader',
-        include: [path.resolve(__dirname, '../tmp/assets/javascripts')],
-        query: {
-          shim: {
-            [path.resolve(__dirname, '../tmp/assets/javascripts/cdb_static.js')]: {
-              exports: 'cdb',
-              deps: [
-                'underscore:_',
-                'jst'
-              ]
-            },
-            [path.resolve(__dirname, '../tmp/assets/javascripts/dashboard_templates_static.js')]: {
-              exports: 'jst'
-            }
-          }
-        }
-      }
-    ]
-  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, '../public/static/dashboard/index.html'),

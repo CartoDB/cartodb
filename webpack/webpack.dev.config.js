e9567cd6a8b063f4acdf18ef507bb92b00e95984
Carlos Matallín <matallo@carto.com>
2017-08-14 12:20:42 +0200

    move static html to template

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,5 +1,7 @@
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+
 const PACKAGE = require('./../package.json');
 const version = PACKAGE.version;
 
@@ -16,7 +18,8 @@ module.exports = {
   entry: entryPoints,
   output: {
     filename: `${version}/javascripts/[name].js`,
-    path: path.resolve(__dirname, '../public/assets')
+    path: path.resolve(__dirname, '../public/assets'),
+    publicPath: '/assets/'
   },
   devtool: 'cheap-module-source-map',
   resolve: {
@@ -46,5 +49,12 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../public/static/dashboard/index.html'),
+      template: path.resolve(__dirname, '../lib/assets/javascripts/cartodb/dashboard/views/index.ejs'),
+      version: version
+    })
+  ]
 };

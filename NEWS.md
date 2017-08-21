c8e1adfe660ae83d15b238f96ab776e159da9fac
Carlos Matallín <matallo@carto.com>
2017-08-21 13:06:31 +0200

    static_frontend feature flag, update NEWS

--- NEWS.md
+++ NEWS.md
@@ -2,9 +2,8 @@ Development
 -----------
 
 ### Features
-* Add /api/v3/me endpoint (#12599)
-
-* Change select "attribute" placeholders (#12498)
+* Split frontend for Dashboard view from Rails backend (#12607)
+* Add /api/v3/me endpoint (#12599)* Change select "attribute" placeholders (#12498)
 * Add pointer cursor to the sliders (#12499)
 * Fixed a bug that would break the bubble legend on IE11 (#support/891)
 * Support for SAML signed logout requests (#12355)
--- app/controllers/admin/visualizations_controller.rb
+++ app/controllers/admin/visualizations_controller.rb
@@ -58,7 +58,9 @@ class Admin::VisualizationsController < Admin::AdminController
   skip_before_filter :verify_authenticity_token, only: [:show_protected_public_map, :show_protected_embed_map]
 
   def index
-    @first_time    = !current_user.dashboard_viewed?
+    return render(file: "public/static/dashboard/index.html", layout: false) unless current_user.has_feature_flag?('static_frontend')
+
+    @first_time = !current_user.dashboard_viewed?
     @just_logged_in = !!flash['logged']
     @google_maps_query_string = current_user.google_maps_query_string
 

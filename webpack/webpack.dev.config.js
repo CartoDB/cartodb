c067fe7391ab39e2cf22c6258f947291283359dc
Carlos Matallín <matallo@carto.com>
2017-08-21 19:00:02 +0200

    prepare entrypoints

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,31 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+var webpack_files = require('../lib/build/files//webpack_files');
 
 const PACKAGE = require('./../package.json');
 const version = PACKAGE.version;
 
-const entryPoints = {
-  static: [
-    './lib/assets/javascripts/cartodb/static.js'
-  ]
-};
-
 module.exports = {
-  entry: entryPoints,
+  entry: './lib/assets/javascripts/cartodb/static.js',
   output: {
     filename: `${version}/javascripts/[name].js`,
     path: path.resolve(__dirname, '../public/assets'),
     publicPath: '/assets/'
   },
   devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
+  plugins: Object.keys(webpack_files).map(function(entryName) {
+    return new HtmlWebpackPlugin({
       inject: false,
       cache: false,
-      filename: path.resolve(__dirname, '../public/static/dashboard/index.html'),
-      template: path.resolve(__dirname, '../lib/assets/javascripts/cartodb/dashboard/views/index.jst.ejs')
+      filename: path.resolve(__dirname, '../public/static/' + entryName + '/index.html'),
+      template: path.resolve(__dirname, '../lib/assets/javascripts/cartodb/dashboard/views/index.jst.ejs'),
+      config: webpack_files[entryName]
     })
-  ],
+  }),
   module: {
     rules: [
       {

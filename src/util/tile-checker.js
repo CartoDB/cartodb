5fa8efbcd0a4219ae2ca1f04808e622e0fc1f39a
rubenmoya <ruben@carto.com>
2017-08-23 14:26:33 +0200

    Fix linter errors

--- src/util/tile-checker.js
+++ src/util/tile-checker.js
@@ -3,26 +3,26 @@ var getValue = require('./get-object-value');
 var limitsSVG = require('../../themes/img/timeout.svg');
 
 var buildErrorTile = function (svg) {
-  return 'data:image/svg+xml;base64,' + btoa(svg);
-}
+  return 'data:image/svg+xml;base64,' + window.btoa(svg);
+};
 
 var TILE_ERRORS = {
   limit: buildErrorTile(limitsSVG),
   default: buildErrorTile(limitsSVG)
-}
+};
 
 module.exports = {
-  check: function(url, callback) {
+  check: function (url, callback) {
     $.ajax({
       url: url,
       complete: function (jqXHR) {
         var errors = getValue(jqXHR, 'responseJSON.errors_with_context', []);
         var error = errors[0] || {};
         var errorType = error.type || 'default';
-        var errorTile = TILE_ERRORS[errorType] || TILE_ERRORS['default']
+        var errorTile = TILE_ERRORS[errorType] || TILE_ERRORS['default'];
 
         return callback(errorType, errorTile, error.message);
       }
     });
   }
-}
+};

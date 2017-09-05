8de8a337ede968bb31ef8ca602135bd09e6e2228
IagoLast <iago.lastra@gmail.com>
2017-09-05 23:27:49 +0200

    Fix lint errors

--- test/spec/api/createVis/create-vis-new.spec.js
+++ test/spec/api/createVis/create-vis-new.spec.js
@@ -3,21 +3,21 @@ var createVis = require('../../../../src/api/create-vis');
 var scenarios = require('./scenarios');
 
 describe('create-vis-new', function () {
-    beforeEach(function () {
-        this.container = $('<div id="map">').css('height', '200px');
-        this.containerId = this.container[0].id;
-        $('body').append(this.container);
-    });
+  beforeEach(function () {
+    this.container = $('<div id="map">').css('height', '200px');
+    this.containerId = this.container[0].id;
+    $('body').append(this.container);
+  });
 
-    afterEach(function () {
-        this.container.remove();
-    });
+  afterEach(function () {
+    this.container.remove();
+  });
 
-    describe('CreateVis', function () {
-        it('should set the right map center', function () {
-            var visJson = scenarios.load(0);
-            var vis = createVis(this.containerId, visJson);
-            expect(vis.map.get('center')).toEqual(visJson.center);
-        });
-    })
+  describe('CreateVis', function () {
+    it('should set the right map center', function () {
+      var visJson = scenarios.load(0);
+      var vis = createVis(this.containerId, visJson);
+      expect(vis.map.get('center')).toEqual(visJson.center);
+    });
+  });
 });
--- test/spec/api/createVis/scenarios/index.js
+++ test/spec/api/createVis/scenarios/index.js
@@ -2,14 +2,14 @@
  * Load a vis.json from the scenarios folder
  * It returns a copy from the object to easy reusing.
  */
-function load(index) {
-    // We use a switch because our current build system doesn't support variables in the require.
-    switch (index) {
-        case 0:
-            return Object.assign({}, require('./vis_0.js'));
-    }
+function load (index) {
+  // We use a switch because our current build system doesn't support variables in the require.
+  switch (index) {
+    case 0:
+      return Object.assign({}, require('./vis_0.js'));
+  }
 }
 
 module.exports = {
-    load: load,
-}
+  load: load
+};

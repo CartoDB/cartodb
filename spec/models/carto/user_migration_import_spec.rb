501f9068cb69b9d107d0fb1d7656e2503cb328d7
Alex Martín <amartin@carto.com>
2017-09-15 10:42:02 +0200

    Minor test changes

--- spec/models/carto/user_migration_import_spec.rb
+++ spec/models/carto/user_migration_import_spec.rb
@@ -45,8 +45,8 @@ describe Carto::UserMigrationImport do
       Carto::UserMigrationPackage.stubs(:for_import).returns @user_migration_package_mock
       @user_migration_package_mock.stubs(:download).with(:irrelevant_file)
       @user_migration_package_mock.stubs(:meta_dir).returns(:irrelevant_meta_dir)
-      @user_mock = Carto::User.new
       @user_migration_package_mock.stubs(:data_dir).returns :irrelevant_data_dir
+      @user_mock = Carto::User.new
       @export_job_mock = Object.new
       @export_job_mock.expects(:run!).once
       CartoDB::DataMover::ImportJob.stubs(:new).returns @export_job_mock
@@ -73,24 +73,23 @@ describe Carto::UserMigrationImport do
 
     def should_import_metadata_for_user(user)
       @user_migration_package_mock.stubs(:meta_dir).returns :irrelevant_meta_dir
-      Carto::UserMetadataExportService.any_instance.stubs(:import_from_directory).with(:irrelevant_meta_dir)
+      Carto::UserMetadataExportService.any_instance.expects(:import_from_directory).with(:irrelevant_meta_dir)
                                       .returns user
-      Carto::UserMetadataExportService.any_instance.stubs(:import_metadata_from_directory)
+      Carto::UserMetadataExportService.any_instance.expects(:import_metadata_from_directory)
                                       .with(user, :irrelevant_meta_dir)
     end
 
     def should_import_metadata_for_organization(organization)
       @user_migration_package_mock.stubs(:meta_dir).returns :irrelevant_meta_dir
       Carto::OrganizationMetadataExportService.any_instance.stubs(:import_from_directory).with(:irrelevant_meta_dir)
-                                              .returns organization
+                                              .once.returns organization
       Carto::OrganizationMetadataExportService.any_instance.stubs(:import_metadata_from_directory)
-                                              .with(organization, :irrelevant_meta_dir)
+                                              .once.with(organization, :irrelevant_meta_dir)
     end
 
     def create_and_add_users_to_organizaton
       user2 = Carto::User.new
       user3 = Carto::User.new
-      2
       @organization_mock.users << user2
       @organization_mock.users << user3
     end

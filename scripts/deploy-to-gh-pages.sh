dc4e45aae1b1607a7cf48681b3c25c887561a01d
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-09-20 16:12:19 +0200

    Setup deploy script

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
-if [[ "$TRAVIS_BRANCH" != "jsdoc-ghpages" ]]; then
+
+# Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "jsdoc-ghpages" ]; then
   exit 0
 fi
 
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-DIST_DIRECTORY="out/"
+SOURCE_DIR="out/v4"
+TARGET_DIR="v4"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
 
-cp .gitignore $DIST_DIRECTORY || exit 1
+echo "Fetching gh-pages branch"
+git fetch origin gh-pages:refs/remotes/origin/gh-pages || exit 1
 
 echo "Checking out gh-pages branch"
-git checkout -B gh-pages || exit 1
+git checkout -b gh-pages origin/gh-pages || exit 1
 
-echo "Removing old static content"
-git rm -rf . || exit 1
-
-echo "Copying dist content to root"
-cp -r $DIST_DIRECTORY/* . || exit 1
-cp $DIST_DIRECTORY/.gitignore . || exit 1
+echo "Copying source content to root"
+rm -rf $TARGET_DIR || exit 1
+cp -r $SOURCE_DIR $TARGET_DIR || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
-git config user.name "Carto CI" || exit 1
+git config user.name "CartoBot" || exit 1
 git config user.email "frontend@carto.com" || exit 1
 
-git add -A . || exit 1
-git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+git add $TARGET_DIR || exit 1
+git commit --allow-empty -m "Update docs for $CURRENT_COMMIT" || exit 1
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
 
 echo "Cleaning up temp files"
-rm -Rf $DIST_DIRECTORY
+rm -rf $TARGET_DIR
 
 echo "Deployed successfully."
 exit 0

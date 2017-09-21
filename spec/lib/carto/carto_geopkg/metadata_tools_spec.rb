804ca0c2afb9ba130236757267585d54fe0aa697
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-09-21 16:04:05 +0200

    add tests for correct generation

--- spec/lib/carto/carto_geopkg/metadata_tools_spec.rb
+++ spec/lib/carto/carto_geopkg/metadata_tools_spec.rb
@@ -27,14 +27,14 @@ describe Carto::CartoGeoPKG::MetadataTools do
 
     describe('#source_for_visualization') do
       it 'should return nil when no sync is present' do
-        @generator.source_for_visualization(@visualization).should be_nil
+        @generator.source_for_visualization(@table_visualization).should be_nil
       end
 
       it 'should return a sync type source for synchronizations' do
         sync = FactoryGirl.build(:carto_synchronization)
-        @visualization.synchronization = sync
+        @table_visualization.synchronization = sync
 
-        source_json = @generator.source_for_visualization(@visualization)
+        source_json = @generator.source_for_visualization(@table_visualization)
         source_json.should_not be_empty
         source_json[:type].should eq 'sync'
 
@@ -43,7 +43,7 @@ describe Carto::CartoGeoPKG::MetadataTools do
         sync_json_config[:refresh_interval_in_seconds].should eq sync.interval
         sync_json_config[:url].should eq sync.url
 
-        @visualization.synchronization = nil
+        @table_visualization.synchronization = nil
       end
     end
 
@@ -69,6 +69,53 @@ describe Carto::CartoGeoPKG::MetadataTools do
       it 'should return a valid schema hash' do
         @generator.schema_for_table_schema(table_schema).should eq expected_schema
       end
+    end
+  end
+
+  describe('#visualization_to_json') do
+    let(:tags) { ['amazing', 'new', 'table'] }
+    let(:description) { 'Every thing about this table is cool.' }
+    let(:schema) do
+      {
+        cartodb_id: { type: "integer" },
+        the_geom: { type: "geometry" },
+        description: { type: "text" },
+        name: { type: "text" }
+      }
+    end
+
+    it 'should return a valid hash' do
+      @table_visualization.description = description
+      @table_visualization.tags = tags
+
+      json = visualization_to_json(@table_visualization)
+
+      json.should_not be_empty
+
+      json_data = json[:data]
+      json_data.should_not be_empty
+      json_data[:source].should be_nil
+
+      json_information = json[:information]
+      json_information.should_not be_empty
+      json_information[:vendor].should eq 'carto'
+      json_information[:version].should eq '0.0.1'
+      json_information[:name].should_not be_nil
+      json_information[:description].should eq description
+      json_information[:created_at].should_not be_nil
+
+      json_classification = json_information[:classification]
+      json_classification.should_not be_empty
+      json_classification[:tags].should eq tags
+
+      json_publishing = json[:publishing]
+      json_publishing.should_not be_empty
+      json_publishing[:privacy].should eq @table_visualization.privacy
+
+      json[:schema].should eq schema
+
+      @table_visualization.description = nil
+      @table_visualization.tags = nil
     end
   end
 end

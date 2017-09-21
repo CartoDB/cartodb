1d00cc031f1b2c754fa29708c87c75ee6126fc7f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-09-21 13:12:10 +0200

    Autogenerate all (internal) docs

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "jsdoc-ghpages" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "jsdoc-api" ]; then
+  echo "Current branch does not generate docs."
+  echo "Skip deploy."
   exit 0
 fi
 
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-SOURCE_DIR="out/v4"
-TARGET_DIR="v4"
+TARGET_DIR="docs"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
 
 echo "Generating documentation"
 npm run docs
+npm run docs:all
 
 echo "Fetching gh-pages branch"
 git fetch origin gh-pages:refs/remotes/origin/gh-pages || exit 1
@@ -25,7 +27,7 @@ git checkout -b gh-pages origin/gh-pages || exit 1
 
 echo "Copying source content to root"
 rm -rf $TARGET_DIR || exit 1
-cp -r $SOURCE_DIR $TARGET_DIR || exit 1
+mv out $TARGET_DIR || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "CartoBot" || exit 1

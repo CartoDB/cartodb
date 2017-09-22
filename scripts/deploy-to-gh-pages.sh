b9d366a866d4ae23968629749b321d97265b2415
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-09-22 10:31:37 +0200

    Generate jsdoc in `docs`

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -10,8 +10,8 @@ fi
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-SOURCE_DIR="out"
-TARGET_DIR="docs"
+DOCS_DIR="docs"
+TMP_DOCS_DIR="tmp_docs"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
@@ -19,7 +19,10 @@ ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
 echo "Generating documentation"
 npm run docs
 npm run docs:all
-cp doc/index.html out/index.html || exit 1
+cp config/jsdoc/index.html $DOCS_DIR/index.html || exit 1
+
+echo "Move to tmp file"
+mv $DOCS_DIR $TMP_DOCS_DIR
 
 echo "Fetching gh-pages branch"
 git fetch origin gh-pages:refs/remotes/origin/gh-pages || exit 1
@@ -28,21 +31,21 @@ echo "Checking out gh-pages branch"
 git checkout -b gh-pages origin/gh-pages || exit 1
 
 echo "Copying source content to root"
-rm -rf $TARGET_DIR || exit 1
-mv $SOURCE_DIR $TARGET_DIR || exit 1
-mv $TARGET_DIR/index.html index.html || exit 1
+rm -rf $DOCS_DIR || exit 1
+mv $TMP_DOCS_DIR $DOCS_DIR || exit 1
+mv $DOCS_DIR/index.html index.html || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Cartofante" || exit 1
 git config user.email "systems@cartodb.com" || exit 1
 
 git add index.html || exit 1
-git add $TARGET_DIR || exit 1
+git add $DOCS_DIR || exit 1
 git commit --allow-empty -m "Update docs for $CURRENT_COMMIT" || exit 1
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
 
 echo "Cleaning up temp files"
-rm -rf $TARGET_DIR
+rm -rf $DOCS_DIR
 
 echo "Deployed successfully."
 exit 0

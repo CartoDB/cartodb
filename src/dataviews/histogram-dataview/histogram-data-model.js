638a89eaa1c0283838c45174b1251fbbb287d602
rubenmoya <ruben@carto.com>
2017-09-25 11:51:50 +0200

    Handle errors in histogram data model

--- src/dataviews/histogram-dataview/histogram-data-model.js
+++ src/dataviews/histogram-dataview/histogram-data-model.js
@@ -94,6 +94,15 @@ module.exports = Model.extend({
       this.set('aggregation', 'auto', { silent: true });
       this._resetStartEndCache();
     });
+
+    this.on('error', function (model, response, options) {
+      if (response.status === 429) {
+        var error = response.responseJSON.errors_with_context[0];
+        setTimeout(function () {
+          this.trigger('error:' + error.type, error, model);
+        }.bind(this), 0);
+      }
+    });
   },
 
   setUrl: function (url) {
@@ -153,6 +162,10 @@ module.exports = Model.extend({
     parsedData.bins = numberOfBins;
 
     return parsedData;
+  },
+
+  refresh: function () {
+    this.fetch();
   },
 
   _getCurrentOffset: function () {

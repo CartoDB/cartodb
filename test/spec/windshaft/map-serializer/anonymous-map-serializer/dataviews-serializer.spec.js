f31168488cde23f96847d91b440b8d6fdd1f6dbc
IagoLast <iago.lastra@gmail.com>
2017-09-25 16:05:30 +0200

    Remove fdescribe

--- test/spec/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer.spec.js
+++ test/spec/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer.spec.js
@@ -2,7 +2,7 @@
 // var DataviewsSerializer = require('../../../../../src/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer');
 // var HistogramDataviewModel = require('../../../../../src/dataviews/histogram-dataview-model');
 
-fdescribe('dataviews-serializer', function () {
+describe('dataviews-serializer', function () {
   it('.serialize', function () {
     pending('TODO: create dataviews using the new v4 api and check the serialization');
   });

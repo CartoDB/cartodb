440d1c9fd5833bca5c746b9f6787eaa9993643dc
IagoLast <iago.lastra@gmail.com>
2017-09-25 15:58:00 +0200

    Add pending tests for dataviews-serializer

---
+++ test/spec/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer.spec.js
@@ -0,0 +1,9 @@
+// var Backbone = require('backbone');
+// var DataviewsSerializer = require('../../../../../src/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer');
+// var HistogramDataviewModel = require('../../../../../src/dataviews/histogram-dataview-model');
+
+fdescribe('dataviews-serializer', function () {
+  it('.serialize', function () {
+    pending('TODO: create dataviews using the new v4 api and check the serialization');
+  });
+});

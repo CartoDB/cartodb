9f091802f7f4a09c4f5a6580d03cba44f1f00eb0
Pablo Alonso Garcia <pablo@cartodb.com>
2017-09-26 13:57:50 +0200

    Added more tests

--- test/spec/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer.spec.js
+++ test/spec/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer.spec.js
@@ -1,9 +1,121 @@
-// var Backbone = require('backbone');
-// var DataviewsSerializer = require('../../../../../src/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer');
-// var HistogramDataviewModel = require('../../../../../src/dataviews/histogram-dataview-model');
+var Backbone = require('backbone');
+var DataviewsSerializer = require('../../../../../src/windshaft/map-serializer/anonymous-map-serializer/dataviews-serializer');
+var CategoryDataviewModel = require('../../../../../src/dataviews/category-dataview-model');
+var HistogramDataviewModel = require('../../../../../src/dataviews/histogram-dataview-model');
+var FormulaDataviewModel = require('../../../../../src/dataviews/formula-dataview-model');
 
 describe('dataviews-serializer', function () {
-  it('.serialize', function () {
-    pending('TODO: create dataviews using the new v4 api and check the serialization');
+  describe('.serialize', function () {
+    var vis;
+    var map;
+    var layer;
+
+    beforeEach(function () {
+      vis = new Backbone.Model();
+      map = new Backbone.Model();
+      layer = new Backbone.Model();
+    });
+
+    it('serialises histogram dataviews', function () {
+      var histogramDataview = new HistogramDataviewModel({
+        id: 'dataview1',
+        column: 'column1',
+        column_type: 'date',
+        aggregation: 'week',
+        source: {
+          id: 'a0'
+        }
+      }, {
+        map: map,
+        vis: vis,
+        layer: layer,
+        analysisCollection: new Backbone.Collection()
+      });
+
+      var dataviewsCollection = new Backbone.Collection([
+        histogramDataview
+      ]);
+
+      expect(DataviewsSerializer.serialize(dataviewsCollection)).toEqual({
+        dataview1: {
+          type: 'histogram',
+          source: {
+            id: 'a0'
+          },
+          options: {
+            column: 'column1',
+            aggregation: 'week'
+          }
+        }
+      });
+    });
+
+    it('serialises category dataviews', function () {
+      var categoryDataview = new CategoryDataviewModel({
+        id: 'dataview2',
+        column: 'column2',
+        aggregation: 'aggregation2',
+        aggregation_column: 'aggregation_column2',
+        source: {
+          id: 'a1'
+        }
+      }, {
+        map: map,
+        vis: vis,
+        layer: layer,
+        analysisCollection: new Backbone.Collection()
+      });
+
+      var dataviewsCollection = new Backbone.Collection([
+        categoryDataview
+      ]);
+
+      expect(DataviewsSerializer.serialize(dataviewsCollection)).toEqual({
+        dataview2: {
+          type: 'aggregation',
+          source: {
+            id: 'a1'
+          },
+          options: {
+            column: 'column2',
+            aggregation: 'aggregation2',
+            aggregationColumn: 'aggregation_column2'
+          }
+        }
+      });
+    });
+
+    it('serialises formula dataviews', function () {
+      var formulaDataview = new FormulaDataviewModel({
+        id: 'dataview3',
+        column: 'column3',
+        operation: 'sum',
+        source: {
+          id: 'a2'
+        }
+      }, {
+        map: map,
+        vis: vis,
+        layer: layer,
+        analysisCollection: new Backbone.Collection()
+      });
+
+      var dataviewsCollection = new Backbone.Collection([
+        formulaDataview
+      ]);
+
+      expect(DataviewsSerializer.serialize(dataviewsCollection)).toEqual({
+        dataview3: {
+          type: 'formula',
+          source: {
+            id: 'a2'
+          },
+          options: {
+            column: 'column3',
+            operation: 'sum'
+          }
+        }
+      });
+    });
   });
 });

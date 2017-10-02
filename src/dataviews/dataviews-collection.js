c310a748336393894c70a56dfb9c15554b09f060
Pablo Alonso Garcia <pablo@cartodb.com>
2017-10-02 16:37:47 +0200

    Moved responsibility to collection

--- src/dataviews/dataviews-collection.js
+++ src/dataviews/dataviews-collection.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 
 var DataviewsCollection = Backbone.Collection.extend({
-  isAnalysisLinkedToDataview: function (analysisModel) {
+  isAnalysisLinkedToAnyDataview: function (analysisModel) {
     return this.any(function (dataviewModel) {
       var sourceId = dataviewModel.getSourceId();
       return analysisModel.get('id') === sourceId;

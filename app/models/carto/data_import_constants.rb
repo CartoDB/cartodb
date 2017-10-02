c5ca8c18d15337766cec1a939a9fb5892b20172c
Alex Martín <amartin@carto.com>
2017-10-02 14:48:37 +0200

    Add collision strategy

--- app/models/carto/data_import_constants.rb
+++ app/models/carto/data_import_constants.rb
@@ -1,10 +1,18 @@
 module Carto
   module DataImportConstants
     COLLISION_STRATEGY_SKIP = 'skip'.freeze
+    COLLISION_STRATEGY_OVERWRITE = 'overwrite'.freeze
 
     def validate_collision_strategy
-      valid = collision_strategy.nil? || collision_strategy == COLLISION_STRATEGY_SKIP
+      valid = collision_strategy.nil? || valid_collision_strategies.include?(collision_strategy)
       errors.add(:collision_strategy, "Invalid collision strategy") unless valid
+    end
+
+    def valid_collision_strategies
+      [
+        COLLISION_STRATEGY_SKIP,
+        COLLISION_STRATEGY_OVERWRITE
+      ]
     end
   end
 end

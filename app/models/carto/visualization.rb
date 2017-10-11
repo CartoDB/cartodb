697bc12fb595be5ce65ad927118e9a75da967862
Alex Martín <amartin@carto.com>
2017-10-11 17:19:26 +0200

    Open Gmaps in builder

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -531,7 +531,7 @@ class Carto::Visualization < ActiveRecord::Base
   end
 
   def open_in_editor?
-    !builder? && (uses_vizjson2? || layers.any?(&:gmapsbase?))
+    !builder? && uses_vizjson2?
   end
 
   def can_be_automatically_migrated_to_v3?

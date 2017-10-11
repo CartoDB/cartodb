c74a115bfc703fca53a5561d857b1e0deeec125c
IagoLast <iago.lastra@gmail.com>
2017-10-11 17:52:56 +0200

    [1818] Add basic reload functionality

--- src/engine.js
+++ src/engine.js
@@ -1,9 +1,14 @@
 var _ = require('underscore');
 var AnalysisPoller = require('./analysis/analysis-poller');
+var AnonymousMapSerializer = require('./windshaft/map-serializer/anonymous-map-serializer/anonymous-map-serializer');
 var Backbone = require('backbone');
 var CartoDBLayerGroup = require('./geo/cartodb-layer-group');
 var DataviewsCollection = require('./dataviews/dataviews-collection');
 var LayersCollection = require('./geo/map/layers');
+var ModelUpdater = require('./windshaft-integration/model-updater');
+var NamedMapSerializer = require('./windshaft/map-serializer/named-map-serializer/named-map-serializer');
+var Request = require('./windshaft/request');
+var WindshaftClient = require('./windshaft/client');
 
 /**
  *
@@ -17,10 +22,11 @@ var LayersCollection = require('./geo/map/layers');
  *  - Notify errors or successful operations.
  * 
  * @param {Object} params - The parameters to initialize the engine.
- * @param {string} employee.apiKey - Api key used to be autenticate in the windshaft server.
- * @param {string} employee.authToken - Token used to be autenticate in the windshaft server.
- * @param {string} employee.username - Name of the user registered in the windshaft server.
- * @param {string} employee.serverUrl - Url of the windshaft server.
+ * @param {string} params.apiKey - Api key used to be autenticate in the windshaft server.
+ * @param {string} params.authToken - Token used to be autenticate in the windshaft server.
+ * @param {string} params.username - Name of the user registered in the windshaft server.
+ * @param {string} params.serverUrl - Url of the windshaft server.
+ * @param {boolean} params.templateName - While we dont remove named maps we must explicitly say when the map is named. Defaults to false.
  * @class
  */
 function Engine (params) {
@@ -29,6 +35,21 @@ function Engine (params) {
   this._authToken = params.authToken;
   this._username = params.username;
   this._serverUrl = params.serverUrl;
+  this._templateName = params.templateName;
+  this._isNamedMap = params.templateName !== undefined;
+  this._stat_tag;
+
+  // Create the client
+  var windshaftSettings = {
+    urlTemplate: this._serverUrl,
+    userName: this._username,
+    statTag: this._stat_tag,
+    apiKey: this._apiKey,
+    authToken: this._authToken,
+    templateName: this._templateName
+  };
+
+  this._windshaftClient = new WindshaftClient(windshaftSettings);
 
   // This object will be responsible of triggering the engine events.
   this._eventEmmitter = _.extend({}, Backbone.Events);
@@ -40,6 +61,15 @@ function Engine (params) {
     { apiKey: this._apiKey, authToken: this._authToken },
     { layersCollection: this._layersCollection }
   );
+
+  // TODO: this is not working.
+  this._modelUpdater = new ModelUpdater({
+    dataviewsCollection: this._dataviewsCollection,
+    layerGroupModel: this.layerGroupModel,
+    layersCollection: this._layersCollection,
+    mapModel: {},
+    visModel: { setOk: function () { }, setErrors: function () { } }
+  });
 }
 
 /**
@@ -68,6 +98,69 @@ Engine.prototype.on = function on (event, callback) {
  */
 Engine.prototype.off = function off (event, callback) {
   this._eventEmmitter.off(event, callback);
+};
+
+/**
+ * This is the most important function of the engine.
+ * Generate a payload from the current state, send it to the windshaft server
+ * and update the internal models with the server response.
+ * 
+ * Once the response has arrived trigger a 'reload-succes' or 'reload-error' event.
+ */
+Engine.prototype.reload = function reload () {
+  var params; // TODO: 
+  var payload = this._getSerializer().serialize(this._layersCollection, this._dataviewsCollection);
+  var options = { success: this._onSuccess.bind(this), error: this._onError.bind(this) };
+  var request = new Request(payload, params, options);
+  this._windshaftClient.instantiateMap(request);
+  // TODO: update options, use promises or explicit callbacks function (error, params).
+};
+
+/**
+ * @api
+ * @public
+ * 
+ * Add a layer to the engine layersCollection
+ * 
+ * @param {layer} layer - A new layer to be added to the engine.
+ */
+Engine.prototype.addLayer = function addLayer (layer) {
+  this._layersCollection.add(layer);
+};
+
+/**
+ * @private
+ * Callback executed when the windhsaft client returns a successful response.
+ * Update internal models and trigger a reload_sucess event.
+ */
+Engine.prototype._onSuccess = function onSuccess (serverResponse) {
+  this._modelUpdater.updateModels(serverResponse, this.layersCollection, this.dataviewsCollection, this.cartoDBLayerGroup);
+  this._eventEmmitter.trigger(Engine.Events.RELOAD_SUCCESS);
+};
+
+/**
+ * @private
+ * Callback executed when the windhsaft client returns a failed response.
+ * Update internal models setting errores and trigger a reload_error event.
+ */
+Engine.prototype._onError = function onError (serverResponse) {
+  var errors = serverResponse;
+  this._modelUpdater.setErrors(errors);
+  this._eventEmmitter.trigger(Engine.Events.RELOAD_ERROR);
+};
+
+/**
+ * @private
+ * Get the instance of the serializer service depending on is an anonymous or a named map.
+ */
+Engine.prototype._getSerializer = function _getSerializer () {
+  return this._isNamedMap ? NamedMapSerializer : AnonymousMapSerializer;
+};
+
+// Public "enum" with the events generated by the engine
+Engine.Events = {
+  RELOAD_SUCCESS: 'reload-success',
+  RELOAD_ERROR: 'reload-error'
 };
 
 module.exports = Engine;
--- test/spec/engine.spec.js
+++ test/spec/engine.spec.js
@@ -1,6 +1,11 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
 var Engine = require('../../src/engine');
+var FakeFactory = require('../helpers/fakeFactory');
+var CartoDBLayer = require('../../src/geo/map/cartodb-layer');
 
 fdescribe('Engine', function () {
+  var fakeVis = new Backbone.Model();
   describe('Constructor', function () {
     it('Should throw a descriptive error when called with no parameters', function () {
       expect(function () {
@@ -13,7 +18,7 @@ fdescribe('Engine', function () {
     var engine;
     var spy;
     beforeEach(function () {
-      engine = new Engine({});
+      engine = new Engine({ serverUrl: 'http://example.com', username: 'fake-username' });
       spy = jasmine.createSpy('spy');
     });
     describe('on', function () {
@@ -34,6 +39,80 @@ fdescribe('Engine', function () {
         engine._eventEmmitter.trigger('fake-event');
         expect(spy.calls.count()).toBe(1);
       });
+    });
+  });
+
+  describe('.addLayer', function () {
+    it('Should add a new layer', function () {
+      var engine = new Engine({ serverUrl: 'http://example.com', username: 'fake-username' });
+      var style = '#layer { marker-color: red; }';
+      var source = FakeFactory.createAnalysisModel({ id: 'a1', type: 'source', query: 'SELECT * FROM table' });
+      var layer = new CartoDBLayer({ source: source, style: style }, { vis: fakeVis });
+      expect(engine._layersCollection.length).toEqual(0);
+      engine.addLayer(layer);
+      expect(engine._layersCollection.length).toEqual(1);
+      expect(engine._layersCollection.at(0)).toEqual(layer);
+    });
+  });
+
+  describe('.reload', function () {
+    var layer;
+    var engine;
+    var fakeVis = new Backbone.Model();
+
+    beforeEach(function () {
+      engine = new Engine({ serverUrl: 'http://example.com', username: 'fake-username' });
+      var style = '#layer { marker-color: red; }';
+      var source = FakeFactory.createAnalysisModel({ id: 'a1', type: 'source', query: 'SELECT * FROM table' });
+      layer = new CartoDBLayer({ source: source, style: style }, { vis: fakeVis });
+    });
+
+    it('Should perform a request with the state encoded in a payload (no layers, no dataviews) ', function (done) {
+      spyOn($, 'ajax').and.callFake(function (params) {
+        var actual = params.url;
+        var expected = 'http://example.com/api/v1/map?config=%7B%22buffersize%22%3A%7B%22mvt%22%3A0%7D%2C%22layers%22%3A%5B%5D%2C%22dataviews%22%3A%7B%7D%2C%22analyses%22%3A%5B%5D%7D';
+        expect(actual).toEqual(expected);
+        done();
+      });
+      engine.reload();
+    });
+
+    it('Should perform a request with the state encoded in a payload (single layer) ', function (done) {
+      spyOn($, 'ajax').and.callFake(function (params) {
+        var actual = params.url;
+        var expected = 'http://example.com/api/v1/map?config=%7B%22buffersize%22%3A%7B%22mvt%22%3A0%7D%2C%22layers%22%3A%5B%7B%22type%22%3A%22mapnik%22%2C%22options%22%3A%7B%22cartocss_version%22%3A%222.1.0%22%2C%22source%22%3A%7B%22id%22%3A%22a1%22%7D%2C%22interactivity%22%3A%5B%22cartodb_id%22%5D%7D%7D%5D%2C%22dataviews%22%3A%7B%7D%2C%22analyses%22%3A%5B%7B%22id%22%3A%22a1%22%2C%22type%22%3A%22source%22%2C%22params%22%3A%7B%22query%22%3A%22SELECT%20*%20FROM%20table%22%7D%7D%5D%7D';
+        expect(actual).toEqual(expected);
+        done();
+      });
+
+      engine.addLayer(layer);
+      engine.reload();
+    });
+
+    it('Should trigger a RELOAD_SUCCESS event when the server returns a successful response ', function () {
+      // Successfull server response
+      spyOn($, 'ajax').and.callFake(function (params) { params.success({}); });
+      // Fake model updater.
+      spyOn(engine._modelUpdater, 'updateModels').and.callFake(function () {});
+      // Attach the success event to a spy.
+      var spy = jasmine.createSpy('successCallback');
+
+      engine.on(Engine.Events.RELOAD_SUCCESS, spy);
+      engine.reload();
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('Should trigger a RELOAD_ERROR event when the server returns an error response ', function () {
+      // Error server response
+      spyOn($, 'ajax').and.callFake(function (params) { params.error({}); });
+      // Fake model updater.
+      spyOn(engine._modelUpdater, 'setErrors').and.callFake(function () {});
+      // Attach the error event to a spy.
+      var spy = jasmine.createSpy('errorCallback');
+
+      engine.on(Engine.Events.RELOAD_ERROR, spy);
+      engine.reload();
+      expect(spy).toHaveBeenCalled();
     });
   });
 });

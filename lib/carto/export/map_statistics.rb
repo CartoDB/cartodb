d67f0693383fac4fc8f536fe21690950b0347dde
Alex Martín <amartin@carto.com>
2017-10-16 13:39:15 +0200

    Simplify and use CSV

--- lib/carto/export/map_statistics.rb
+++ lib/carto/export/map_statistics.rb
@@ -1,61 +1,42 @@
+require 'csv'
+
 module Carto
   module Export
     class MapStatistics
-      FILE_MODE = :file
-      STDOUT_MODE = :stdout
-      MEMORY_MODE = :memory
-      VALID_MODES = [FILE_MODE, STDOUT_MODE, MEMORY_MODE].freeze
 
-      def initialize(mode: :file, path: nil, types: ['derived'])
-        assert_valid_mode(mode)
-        @mode = mode
-        @path = path || "/tmp/map_statistics_#{SecureRandom.urlsafe_base64}.csv"
+      attr_reader :filepath
+
+      def initialize(types: ['derived'])
         @types = types
+        @headers_written = false
+        @filepath = "/tmp/map_statistics_#{SecureRandom.urlsafe_base64}.csv"
       end
 
       def run!
-        @out = @mode == :file ? File.new(@path, 'w+') : STDOUT
-        @headers_sent = false
-        @result = []
-
-        Carto::Visualization.where(type: @types).find_each { |vis| process_row(statistics_for_visualization(vis)) }
-
-        @result
-      ensure
-        @out.close if @mode == :file
-      end
-
-      def filepath
-        @mode == FILE_MODE ? @out.path : nil
+        csv = CSV.open(filepath, 'wb', headers: true)
+        Carto::Visualization.where(type: @types).find_each { |vis| process_row(statistics_for_visualization(vis), csv) }
+        csv.close()
       end
 
       private
 
-      def process_row(row)
-        if !@headers_sent && !memory_mode?
-          @headers_sent = true
-          @out.write(row.keys.join(', ') + "\n")
+      def process_row(row, csv)
+        if !@headers_written
+          csv << row.keys
+          @headers_written = true
         end
-        memory_mode? ? @result << row : @out.write(row.values.join(', ') + "\n")
-      end
-
-      def memory_mode?
-        @mode == MEMORY_MODE
+        csv << row.values
       end
 
       def statistics_for_visualization(visualization)
         {
-          name: visualization.name,
+          id: visualization.id,
           total_data_layers: visualization.data_layers.count,
           total_analyses: visualization.analyses.count,
           user_account_type: visualization.user.account_type,
           creation_date: visualization.created_at,
           type: visualization.builder? ? 'builder' : 'editor'
         }
-      end
-
-      def assert_valid_mode(mode)
-        raise "Invalid mode: #{mode}" unless VALID_MODES.include?(mode)
       end
     end
   end

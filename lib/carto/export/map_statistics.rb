b5c28f8d9ac5fc397edaff2f9cedd63caf007f8e
Alex Martín <amartin@carto.com>
2017-10-16 16:29:52 +0200

    Fix analysis node count

--- lib/carto/export/map_statistics.rb
+++ lib/carto/export/map_statistics.rb
@@ -29,10 +29,12 @@ module Carto
       end
 
       def statistics_for_visualization(visualization)
+        analysis_node_count = visualization.analyses.flat_map { |a| a.analysis_node.descendants.map(&:id) }.uniq.count
         {
           id: visualization.id,
           total_data_layers: visualization.data_layers.count,
-          total_analyses: visualization.analyses.count,
+          total_analysis_nodes: analysis_node_count,
+          analysis_nodes_per_layer: (analysis_node_count.to_f / visualization.layers.count.to_f).round(2),
           user_account_type: visualization.user.account_type,
           creation_date: visualization.created_at,
           type: visualization.builder? ? 'builder' : 'editor'

b062ced6d87b08f05d3a225220591587c31b7fcd
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-10-17 16:51:00 +0200

    DataFactory persisting changes
    Now, the DataFactory class won't persist. It will return a
    visualization object in memory with all reflections for maps and
    layers in place.

--- lib/carto/dataset_factory.rb
+++ lib/carto/dataset_factory.rb
@@ -9,22 +9,34 @@ module Carto
   # 'Dataset' outside of the product scope, which may very well disappear or
   # change in the future
   class DatasetFactory
-    def initialize(user:, metadata:)
+    def initialize(user:, metadata:, kind:, geometry_type:)
       @user = user
       @metadata = metadata
+      @kind = kind
+      @geometry_type = geometry_type
     end
 
     def dataset_visualization
-      @visualization ||= Carto::Visualization.new(
+      provider = Carto::Map.provider_for_baselayer_kind(layers.first.kind)
+      map = Carto::Map.new(user: @user, provider: provider)
+
+      dataset_visualization = Carto::Visualization.new(
         type: 'table',
         user: @user,
+        map: map,
         name: name,
         description: description,
         attributions: attributions,
         source: source,
         privacy: privacy,
-        tags: tags
+        tags: tags,
+        kind: @kind,
+        overlays: Carto::OverlayFactory.build_default_overlays(@user)
       )
+
+      dataset_visualization.map.layers = layers
+
+      dataset_visualization
     end
 
     private
@@ -63,6 +75,20 @@ module Carto
 
     def privacy
       @privacy ||= publishing && publishing[:privacy]
+    end
+
+    def layers
+      return @layers if @layers
+
+      base_layer = Carto::LayerFactory.build_default_base_layer(@user)
+      data_layer = Carto::LayerFactory.build_data_layer(@user, name, @geometry_type)
+      layers = [base_layer, data_layer]
+
+      if base_layer.supports_labels_layer?
+        layers << Carto::LayerFactory.build_default_labels_layer(base_layer)
+      end
+
+      @layers = layers
     end
   end
 end

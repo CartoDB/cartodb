006bbfd5cd3ab0099edb7fa84c36cdf2ed4c956c
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-10-17 11:27:14 +0200

    DatasetFactory class
    This class provides a factory pattern to create Datasets. A 'Dataset'
    is only a product scope thing, and is not directly represented
    through a model in our ORM. Instead of that, this factory wiill
    return a table type viusalization.

---
+++ lib/carto/dataset_factory.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+module Carto
+  # This class is in lib/carto because there's no 'Dataset' model in CARTO. The
+  # idea here is to provide an abstract interface that allows the programmer to
+  # take distance with the underlying 'table type visualization' and focus on
+  # the product concept of 'Dataset'. This should NOT be an entry point to
+  # solving problems with the visualization model. There is no such thing as a
+  # 'Dataset' outside of the product scope, which may very well disappear or
+  # change in the future
+  class DatasetFactory
+    def with_name(name)
+      visualization.name = name
+
+      self
+    end
+
+    def with_description(description)
+      visualization.description = description
+
+      self
+    end
+
+    def with_attributions(attributions)
+      visualization.attributions = attributions
+
+      self
+    end
+
+    def with_tags(tags)
+      visualization.tags = tags
+
+      self
+    end
+
+    def with_privacy(privacy)
+      visualization.privacy = privacy
+
+      self
+    end
+
+    def visualization
+      @visualization ||= Carto::Visualization.new(type: 'table')
+    end
+  end
+end

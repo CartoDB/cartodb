2a599e4d9743fba4d29379f6343cd071fb2d3e4e
IagoLast <iago.lastra@gmail.com>
2017-10-18 12:14:51 +0200

    Refactor engine tests

--- test/spec/engine.spec.js
+++ test/spec/engine.spec.js
@@ -8,7 +8,7 @@ var CartoDBLayer = require('../../src/geo/map/cartodb-layer');
 describe('Engine', function () {
   var fakeVis = new Backbone.Model();
   describe('Constructor', function () {
-    it('Should throw a descriptive error when called with no parameters', function () {
+    it('should throw a descriptive error when called with no parameters', function () {
       expect(function () {
         new Engine(); // eslint-disable-line
       }).toThrowError('new Engine() called with no paramters');
@@ -23,7 +23,7 @@ describe('Engine', function () {
       spy = jasmine.createSpy('spy');
     });
     describe('on', function () {
-      it('Should register a callback thats called for "fake-event"', function () {
+      it('should register a callback thats called for "fake-event"', function () {
         engine.on('fake-event', spy);
         expect(spy).not.toHaveBeenCalled(); // Ensure the spy not has been called previosuly
         engine._eventEmmitter.trigger('fake-event');
@@ -31,7 +31,7 @@ describe('Engine', function () {
       });
     });
     describe('off', function () {
-      it('Should unregister a callback', function () {
+      it('should unregister a callback', function () {
         engine.on('fake-event', spy);
         expect(spy).not.toHaveBeenCalled(); // Ensure the spy not has been called previosuly
         engine._eventEmmitter.trigger('fake-event');
@@ -44,7 +44,7 @@ describe('Engine', function () {
   });
 
   describe('.addLayer', function () {
-    it('Should add a new layer', function () {
+    it('should add a new layer', function () {
       var engine = new Engine({ serverUrl: 'http://example.com', username: 'fake-username' });
       var style = '#layer { marker-color: red; }';
       var source = FakeFactory.createAnalysisModel({ id: 'a1', type: 'source', query: 'SELECT * FROM table' });
@@ -56,7 +56,7 @@ describe('Engine', function () {
     });
   });
 
-  describe('.reload', function () {
+  fdescribe('.reload', function () {
     var layer;
     var engine;
     var fakeVis = new Backbone.Model();
@@ -68,7 +68,7 @@ describe('Engine', function () {
       layer = new CartoDBLayer({ source: source, style: style }, { vis: fakeVis });
     });
 
-    it('Should perform a request with the state encoded in a payload (no layers, no dataviews) ', function (done) {
+    it('should perform a request with the state encoded in a payload (no layers, no dataviews) ', function (done) {
       spyOn($, 'ajax').and.callFake(function (params) {
         var actual = params.url;
         var expected = 'http://example.com/api/v1/map?config=%7B%22buffersize%22%3A%7B%22mvt%22%3A0%7D%2C%22layers%22%3A%5B%5D%2C%22dataviews%22%3A%7B%7D%2C%22analyses%22%3A%5B%5D%7D';
@@ -78,7 +78,7 @@ describe('Engine', function () {
       engine.reload();
     });
 
-    it('Should perform a request with the state encoded in a payload (single layer) ', function (done) {
+    it('should perform a request with the state encoded in a payload (single layer) ', function (done) {
       spyOn($, 'ajax').and.callFake(function (params) {
         var actual = params.url;
         var expected = 'http://example.com/api/v1/map?config=%7B%22buffersize%22%3A%7B%22mvt%22%3A0%7D%2C%22layers%22%3A%5B%7B%22type%22%3A%22mapnik%22%2C%22options%22%3A%7B%22cartocss_version%22%3A%222.1.0%22%2C%22source%22%3A%7B%22id%22%3A%22a1%22%7D%2C%22interactivity%22%3A%5B%22cartodb_id%22%5D%7D%7D%5D%2C%22dataviews%22%3A%7B%7D%2C%22analyses%22%3A%5B%7B%22id%22%3A%22a1%22%2C%22type%22%3A%22source%22%2C%22params%22%3A%7B%22query%22%3A%22SELECT%20*%20FROM%20table%22%7D%7D%5D%7D';
@@ -90,7 +90,7 @@ describe('Engine', function () {
       engine.reload();
     });
 
-    it('Should trigger a RELOAD_SUCCESS event when the server returns a successful response ', function () {
+    it('should trigger a RELOAD_SUCCESS event when the server returns a successful response ', function () {
       // Successfull server response
       spyOn($, 'ajax').and.callFake(function (params) { params.success(FAKE_RESPONSE); });
       // Fake model updater.
@@ -103,7 +103,7 @@ describe('Engine', function () {
       expect(spy).toHaveBeenCalled();
     });
 
-    it('Should trigger a RELOAD_ERROR event when the server returns an error response ', function () {
+    it('should trigger a RELOAD_ERROR event when the server returns an error response ', function () {
       // Error server response
       spyOn($, 'ajax').and.callFake(function (params) { params.error({}); });
       // Fake model updater.
@@ -116,33 +116,38 @@ describe('Engine', function () {
       expect(spy).toHaveBeenCalled();
     });
 
-    it('Should use the sourceID parameter', function () {
+    it('should use the sourceID parameter', function (done) {
       // Error server response
       spyOn($, 'ajax').and.callFake(function (params) { params.success(FAKE_RESPONSE); });
       // Spy on modelupdater to ensure thats called with fakesourceId
-      var spy = spyOn(engine._modelUpdater, 'updateModels');
-      // Attach the error event to a spy.
-      engine.on(Engine.Events.RELOAD_SUCCESS, spy);
+      var updateModelsSpy = spyOn(engine._modelUpdater, 'updateModels');
 
+      // When the reload is finished, check the spy.
+      engine.on(Engine.Events.RELOAD_SUCCESS, function () {
+        expect(updateModelsSpy).toHaveBeenCalledWith(jasmine.anything(), 'fakeSourceId', undefined);
+        done();
+      });
       engine.reload('fakeSourceId');
-      expect(spy).toHaveBeenCalledWith(jasmine.anything(), 'fakeSourceId', undefined);
     });
 
-    it('Should use the forceFetch parameter', function () {
+    it('should use the forceFetch parameter', function (done) {
       // Error server response
       spyOn($, 'ajax').and.callFake(function (params) { params.success(FAKE_RESPONSE); });
       // Spy on modelupdater to ensure thats called with fakesourceId
-      var spy = spyOn(engine._modelUpdater, 'updateModels');
-      // Attach the error event to a spy.
-      engine.on(Engine.Events.RELOAD_SUCCESS, spy);
+      var updateModelsSpy = spyOn(engine._modelUpdater, 'updateModels');
+
+      // When the reload is finished, check the spy.
+      engine.on(Engine.Events.RELOAD_SUCCESS, function () {
+        expect(updateModelsSpy).toHaveBeenCalledWith(jasmine.anything(), 'fakeSourceId', true);
+        done();
+      });
 
       engine.reload('fakeSourceId', true);
-      expect(spy).toHaveBeenCalledWith(jasmine.anything(), 'fakeSourceId', true);
     });
 
     // The following tests overlaps the model-updater tests.
 
-    it('Should update the layer metadata according to the server response', function (done) {
+    it('should update the layer metadata according to the server response', function (done) {
       // Successfull server response
       spyOn($, 'ajax').and.callFake(function (params) { params.success(FAKE_RESPONSE); });
 
@@ -157,7 +162,7 @@ describe('Engine', function () {
       engine.reload();
     });
 
-    it('Should update the cartolayerGroup metadata according to the server response', function (done) {
+    it('should update the cartolayerGroup metadata according to the server response', function (done) {
       // Successfull server response
       spyOn($, 'ajax').and.callFake(function (params) { params.success(FAKE_RESPONSE); });
 
@@ -175,11 +180,11 @@ describe('Engine', function () {
       engine.reload();
     });
 
-    it('Should update the analyses metadata according to the server response', function (done) {
+    it('should update the analyses metadata according to the server response', function (done) {
       pending('Test not implemented');
     });
 
-    it('Should update the dataview metadata according to the server response', function (done) {
+    it('should update the dataview metadata according to the server response', function (done) {
       pending('Test not implemented');
     });
   });

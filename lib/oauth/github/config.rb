45ee41bd553ff79fa638bb7e534b73ceae6a1967
Javier Torres <jtorres@carto.com>
2017-10-20 09:52:09 +0200

    Add google

--- lib/oauth/github/config.rb
+++ lib/oauth/github/config.rb
@@ -1,34 +1,46 @@
+require_dependency 'oauth/client'
+
 module Carto
   module Github
     class Config
-      attr_reader :client_id, :client_secret, :state
+      GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize'.freeze
+      GITHUB_TOKEN_URL = 'https://github.com/login/oauth/access_token'.freeze
 
-      def self.instance(state, organization_name: nil, invitation_token: nil)
-        Github::Config.new(state, organization_name, invitation_token) if Cartodb.get_config(:oauth, 'github', 'client_id').present?
+      attr_reader :client
+
+      def self.instance(state, controller, organization_name: nil, invitation_token: nil)
+        Github::Config.new(state, controller, organization_name, invitation_token) if Cartodb.get_config(:oauth, 'github', 'client_id').present?
       end
 
-      def initialize(state, organization_name, invitation_token)
-        @client_id = Cartodb.get_config(:oauth, 'github', 'client_id')
-        @client_secret = Cartodb.get_config(:oauth, 'github', 'client_secret')
-        @state = state
-        @invitation_token = invitation_token
-        @organization_name = organization_name
+      def initialize(state, controller, organization_name, invitation_token)
+        @client = Carto::OAuth2Client.new(
+          auth_url: GITHUB_AUTH_URL,
+          token_url: GITHUB_TOKEN_URL,
+          client_id: Cartodb.get_config(:oauth, 'github', 'client_id'),
+          client_secret: Cartodb.get_config(:oauth, 'github', 'client_secret'),
+          state: state,
+          redirect_uri: redirect_uri(controller, organization_name, invitation_token),
+          scopes: ['user:email']
+        )
       end
 
-      def github_url(controller)
-        escaped_state = Rack::Utils.escape(state)
-        url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&state=#{escaped_state}&scope=user:email"
+      def github_url
+        client.authorize_url
+      end
 
+      private
+
+      def redirect_uri(controller, organization_name, invitation_token)
         params = {}
-        params[:invitation_token] = @invitation_token if @invitation_token
-        params[:organization] = @organization_name if @organization_name
-        redirect_uri = "#{base_callback_url(controller)}?#{URI.encode_www_form(params)}" unless params.empty?
-        url += "&redirect_uri=#{CGI.escape(redirect_uri)}" if redirect_uri
+        params[:invitation_token] = invitation_token if invitation_token
+        params[:organization] = organization_name if organization_name
+        redirect_uri = base_callback_uri(controller)
+        redirect_uri += "?#{URI.encode_www_form(params)}" unless params.empty?
 
-        url
+        redirect_uri
       end
 
-      def base_callback_url(controller)
+      def base_callback_uri(controller)
         if Cartodb::Central.sync_data_with_cartodb_central?
           Cartodb::Central.new.host + '/github'
         else
---
+++ lib/oauth/google/api.rb
@@ -0,0 +1,72 @@
+require 'typhoeus'
+require_dependency 'oauth/github/config'
+
+module Google
+  class Api
+    attr_reader :access_token
+
+    def self.with_code(config, code)
+      token = config.client.exchange_code_for_token(code)
+      raise 'Could not initialize Google API' unless token
+      Google::Api.new(config, token)
+    end
+
+    def initialize(config, token)
+      @config = config
+      @access_token = token
+    end
+
+    def id
+      user_data['id']
+    end
+
+    def first_name
+      user_data.fetch('name', {}).fetch('givenName', nil)
+    end
+
+    def last_name
+      user_data.fetch('name', {}).fetch('familyName', nil)
+    end
+
+    def email
+      user_data['emails'].select { |mail| mail['type'] == 'account' }.first['value']
+    end
+
+    def user_params
+      {
+        username: email.split('@')[0],
+        email: email,
+        name: first_name,
+        last_name: last_name,
+        google_sign_in: true
+      }
+    end
+
+    private
+
+    def user_data
+      @user_data ||= get_user_data
+    rescue => e
+      CartodbCentral::Logger.error(message: 'Error obtaining GitHub user data', exception: e, access_token: access_token)
+      nil
+    end
+
+    def get_user_data
+      response = Typhoeus::Request.new(
+        "https://www.googleapis.com/plus/v1/people/me?access_token=#{@access_token}",
+        method: 'GET',
+        ssl_verifypeer: true,
+        timeout: 600
+      ).run
+
+      raise 'Invalid response code' unless response.code == 200
+        JSON.parse(response.body)
+    rescue => e
+      CartodbCentral::Logger.error(message: 'Error in request to Google', exception: e,
+                                   method: method, url: url, body: body, headers: headers,
+                                   response_code: response.code, response_headers: response.headers,
+                                   response_body: response.body, return_code: response.return_code)
+      nil
+    end
+  end
+end

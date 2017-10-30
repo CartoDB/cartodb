2e7a2dbdcc999129e82630f3adb3917855b3273a
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-10-30 13:25:38 +0100

    Fix error propagations

--- src/engine.js
+++ src/engine.js
@@ -227,8 +227,8 @@ Engine.prototype._onReloadError = function (serverResponse, options) {
   var windshaftErrors = parseWindshaftErrors(serverResponse);
   var error = _.find(windshaftErrors, function (error) { return error.isGlobalError(); });
   this._modelUpdater.setErrors(windshaftErrors);
-  this._eventEmmitter.trigger(Engine.Events.RELOAD_ERROR, windshaftErrors);
-  options.error && options.error(error);
+  this._eventEmmitter.trigger(Engine.Events.RELOAD_ERROR, error);
+  options.error && options.error();
 };
 
 /**

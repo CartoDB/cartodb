89a46b00b8d745c537fddaf8c6a7d3db12053f32
IagoLast <iago.lastra@gmail.com>
2017-11-03 11:35:41 +0100

    Add failing test cases

--- test/spec/api/v4/client.spec.js
+++ test/spec/api/v4/client.spec.js
@@ -14,27 +14,43 @@ describe('client', function () {
   });
 
   describe('.addLayer', function () {
-    it('should add a new layer', function () {});
-    it('should add a new layer triggering a reload cycle by default', function () {});
-    it('should add a new layer without triggering a reload cycle when opts.reload is false', function () {});
-    it('should return a rejected promise when some error happened', function () {});
+    var client;
+    beforeEach(function () {
+      client = new carto.Client({
+        apiKey: '84fdbd587e4a942510270a48e843b4c1baa11e18',
+        serverUrl: 'https://{user}.carto.com:443',
+        username: 'cartojs-test'
+      });
+    });
+    it('should add a new layer', function () {
+      var source = new carto.source.Dataset('e_10m_populated_places_simple');
+      var style = new carto.style.CartoCSS(`#layer {  marker-fill: red; }`);
+      var layer = new carto.layer.Layer(source, style);
+
+      client.addLayer(layer);
+
+      expect(client.getLayers()[0]).toEqual(layer);
+    });
+    it('should add a new layer triggering a reload cycle by default', function () { });
+    it('should add a new layer without triggering a reload cycle when opts.reload is false', function () { });
+    it('should return a rejected promise when some error happened', function () { });
   });
 
   describe('.addLayers', function () {
-    it('should add a layers array', function () {});
-    it('should add a layer array triggering a reload cycle by default', function () {});
-    it('should add a layers array without triggering a reload cycle when opts.reload is false', function () {});
-    it('should return a rejected promise when some error happened', function () {});
+    it('should add a layers array', function () { });
+    it('should add a layer array triggering a reload cycle by default', function () { });
+    it('should add a layers array without triggering a reload cycle when opts.reload is false', function () { });
+    it('should return a rejected promise when some error happened', function () { });
   });
 
   describe('.getLayers', function () {
-    it('should return an empty array when there are no layers', function () {});
-    it('should return the layers stored in the client', function () {});
+    it('should return an empty array when there are no layers', function () { });
+    it('should return the layers stored in the client', function () { });
   });
 
   describe('.removeLayer', function () {
-    it('should throw a descriptive error when the parameter is invalid', function () {});
-    it('should throw a descriptive error when layer is not in the client', function () {});
-    it('should remove the layer when is in the client', function () {});
+    it('should throw a descriptive error when the parameter is invalid', function () { });
+    it('should throw a descriptive error when layer is not in the client', function () { });
+    it('should remove the layer when is in the client', function () { });
   });
 });

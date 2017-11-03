99219c0ed4b6193e0f2621aa387d85f6553c5bde
IagoLast <iago.lastra@gmail.com>
2017-11-03 13:03:08 +0100

    Add client.addLayer tests

--- test/spec/api/v4/client.spec.js
+++ test/spec/api/v4/client.spec.js
@@ -15,25 +15,54 @@ describe('client', function () {
 
   describe('.addLayer', function () {
     var client;
+    var source;
+    var style;
+    var layer;
     beforeEach(function () {
       client = new carto.Client({
         apiKey: '84fdbd587e4a942510270a48e843b4c1baa11e18',
         serverUrl: 'https://{user}.carto.com:443',
         username: 'cartojs-test'
       });
+      source = new carto.source.Dataset('ne_10m_populated_places_simple');
+      style = new carto.style.CartoCSS(`#layer {  marker-fill: red; }`);
+      layer = new carto.layer.Layer('id', source, style, {});
     });
-    it('should add a new layer', function () {
-      var source = new carto.source.Dataset('e_10m_populated_places_simple');
-      var style = new carto.style.CartoCSS(`#layer {  marker-fill: red; }`);
-      var layer = new carto.layer.Layer(source, style);
 
+    it('should add a new layer', function () {
       client.addLayer(layer);
 
       expect(client.getLayers()[0]).toEqual(layer);
     });
-    it('should add a new layer triggering a reload cycle by default', function () { });
-    it('should add a new layer without triggering a reload cycle when opts.reload is false', function () { });
-    it('should return a rejected promise when some error happened', function () { });
+
+    it('should add a new layer triggering a reload cycle by default', function (done) {
+      spyOn(client._engine, 'reload').and.callThrough();
+
+      client.addLayer(layer).then(function () {
+        expect(client._engine.reload).toHaveBeenCalled();
+        expect(client._engine.reload.calls.count()).toEqual(1);
+        done();
+      });
+    });
+
+    it('should add a new layer without triggering a reload cycle when opts.reload is false', function (done) {
+      spyOn(client._engine, 'reload').and.callThrough();
+
+      client.addLayer(layer, { reload: false }).then(function () {
+        expect(client._engine.reload).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('should return a rejected promise when some error happened', function (done) {
+      var errorMock = new Error('Error-Mock');
+      spyOn(client._engine, 'reload').and.returnValue(Promise.reject(errorMock));
+
+      client.addLayer(layer).catch(function (error) {
+        expect(error).toEqual(errorMock);
+        done();
+      });
+    });
   });
 
   describe('.addLayers', function () {
--- test/spec/api/v4/layer.spec.js
+++ test/spec/api/v4/layer.spec.js
@@ -4,7 +4,7 @@ describe('layer', function () {
   // Layer constructor is polymorphic and has 4 different forms.
   describe('constructor', function () {
     it('should build a new Layer params: (id, source, style)', function () {
-      var source = new carto.source.Dataset('e_10m_populated_places_simple');
+      var source = new carto.source.Dataset('ne_10m_populated_places_simple');
       var style = new carto.style.CartoCSS(`#layer {  marker-fill: red; }`);
       var layer = new carto.layer.Layer('l0', source, style);
 
@@ -14,7 +14,7 @@ describe('layer', function () {
     });
 
     it('should build a new Layer params: (source, style)', function () {
-      var source = new carto.source.Dataset('e_10m_populated_places_simple');
+      var source = new carto.source.Dataset('ne_10m_populated_places_simple');
       var style = new carto.style.CartoCSS(`#layer {  marker-fill: red; }`);
       var layer = new carto.layer.Layer('source', style);
 

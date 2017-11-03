472ce7cee2719441db59e703452bbd60f78d5403
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-11-03 14:01:06 +0100

    Improve dataview/base tests

--- test/spec/api/v4/dataview/base.spec.js
+++ test/spec/api/v4/dataview/base.spec.js
@@ -1,7 +1,7 @@
 var DataviewBase = require('../../../../../src/api/v4/dataview/base');
 var STATUS = require('../../../../../src/api/v4/constants').STATUS;
 
-describe('dataview/base', function () {
+describe('base dataview public v4 API', function () {
   var base = new DataviewBase();
 
   it('.getStatus should return the internal status', function () {
@@ -34,9 +34,17 @@ describe('dataview/base', function () {
     expect(base._enabled).toBe(true);
   });
 
+  it('.enable should return the dataview', function () {
+    expect(base.enable()).toBe(base);
+  });
+
   it('.disable should disable the dataview', function () {
     base.disable();
     expect(base._enabled).toBe(false);
+  });
+
+  it('.disable should return the dataview', function () {
+    expect(base.disable()).toBe(base);
   });
 
   it('.isEnabled should return true if enabled and false otherwise', function () {
@@ -53,9 +61,14 @@ describe('dataview/base', function () {
   });
 
   it('.setColumn should throw an error if the argument is not string or undefined', function () {
-    expect(function () { base.setColumn(); }).toThrow(new TypeError('Column property is required.'));
-    expect(function () { base.setColumn(12); }).toThrow(new TypeError('Column property must be a string.'));
-    expect(function () { base.setColumn(''); }).toThrow(new TypeError('Column property must be not empty.'));
+    expect(function () { base.setColumn(); }).toThrowError(TypeError, 'Column property is required.');
+    expect(function () { base.setColumn(12); }).toThrowError(TypeError, 'Column property must be a string.');
+    expect(function () { base.setColumn(''); }).toThrowError(TypeError, 'Column property must be not empty.');
+  });
+
+  it('.setColumn should return the dataview', function () {
+    var column = 'column-test';
+    expect(base.setColumn(column)).toBe(base);
   });
 
   it('.getColumn should return the column name', function () {
@@ -65,6 +78,6 @@ describe('dataview/base', function () {
   });
 
   it('.getData should not be defined in the base dataview', function () {
-    expect(function () { base.getData(); }).toThrow(new Error('getData must be implemented by the particular dataview.'));
+    expect(function () { base.getData(); }).toThrowError(Error, 'getData must be implemented by the particular dataview.');
   });
 });

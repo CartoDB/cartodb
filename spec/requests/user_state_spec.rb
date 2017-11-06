a119fffee14c490e18e4019f70d9a336ae7d54bd
Mario de Frutos <ethervoid@gmail.com>
2017-11-06 14:51:03 +0100

    Improved and add tests for the non-locked scenario

--- spec/requests/user_state_spec.rb
+++ spec/requests/user_state_spec.rb
@@ -9,35 +9,46 @@ def login(user)
   host! "#{user.username}.localhost.lan"
 end
 
+def follow_redirects(limit = 10)
+  while response.redirect? && (limit -= 1) > 0 do
+      follow_redirect!
+  end
+end
+
 describe "UserState"  do
 
   before(:all) do
-    @locked_user = FactoryGirl.create(:carto_locked_user)
-    @map, @table, @table_visualization, @visualization = create_full_visualization(@locked_user)
-    @non_locked_user = FactoryGirl.create(:carto_user)
+    @locked_user = FactoryGirl.create(:locked_user)
+    @map, @table, @table_visualization, @visualization = create_full_builder_vis(@locked_user)
+    @visualization.create_mapcap!
+    @non_locked_user = FactoryGirl.create(:valid_user)
     @dashboard_endpoints = ['/dashboard', '/dashboard/tables', '/dashboard/datasets', '/dashboard/visualizations',
                           '/dashboard/maps'].freeze
-    @user_endpoints = ['/me', '/account', '/profile'].freeze
+    @public_user_endpoints = ['/me'].freeze
+    @user_endpoints = ['/account', '/profile'].freeze
     @tables_endpoints = ["/tables/#{@table.id}", "/tables/#{@table.id}/public",
-                         "/tables/#{@table.id}/embed_map", "/viz/#{@visualization.id}"].freeze
-    @viz_endpoints = ["/viz/#{@visualization.id}/public", "/viz/#{@visualization.id}/embed_map",
-                      "/viz/#{@visualization.id}/public_map", "/builder/#{@visualization.id}",
-                      "/builder/#{@visualization.id}/embed"].freeze
-    @api_endpoints = ["/api/v1/viz", "/api/v1/viz/#{@visualization.id}", "/api/v1/tables/#{@table.id}",
-                      "/api/v1/tables/#{@table.id}/columns",
-                      "/api/v1/imports", "/api/v1/users/#{@locked_user.id}/layers", "/api/v1/synchronizations",
-                      "/api/v1/geocodings", "/api/v1/users/#{@locked_user.id}", "/api/v2/viz/#{@visualization.id}/viz",
-                      "/api/v3/me", "/api/v3/viz/#{@visualization.id}/viz"].freeze
+                         "/tables/#{@table.id}/embed_map"].freeze
+    @viz_endpoints = ["/viz/#{@visualization.id}/public",
+                      "/viz/#{@visualization.id}/embed_map", "/viz/#{@visualization.id}/public_map",
+                      "/builder/#{@visualization.id}", "/builder/#{@visualization.id}/embed"].freeze
+    @public_api_endpoints = ["/api/v1/viz", "/api/v1/viz/#{@visualization.id}",
+                             "/api/v2/viz/#{@visualization.id}/viz",
+                             "/api/v3/me", "/api/v3/viz/#{@visualization.id}/viz"].freeze
+    @private_api_endpoints = ["/api/v1/tables/#{@table.id}", "/api/v1/tables/#{@table.id}/columns",
+                              "/api/v1/imports", "/api/v1/users/#{@locked_user.id}/layers",
+                              "/api/v1/synchronizations", "/api/v1/geocodings",
+                              "/api/v1/users/#{@locked_user.id}"]
     @headers = {}
   end
 
   after(:all) do
-    @user.destroy
+    @locked_user.destroy
+    @non_locked_user.destroy
   end
 
-  describe '#locked user' do
-    it 'owner accessing their resources' do
-      login(@locked_user)
+    describe '#locked user' do
+      it 'owner accessing their resources' do
+        login(@locked_user)
       @dashboard_endpoints.each do |endpoint|
         get endpoint, {}, @headers
         response.status.should == 302
@@ -52,6 +63,13 @@ describe "UserState"  do
         request.path.should == '/upgrade_trial'
         response.status.should == 200
       end
+      @public_user_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        response.status.should == 302
+        follow_redirect!
+        request.path.should == '/upgrade_trial'
+        response.status.should == 200
+      end
       @tables_endpoints.each do |endpoint|
         get endpoint, {}, @headers
         response.status.should == 302
@@ -67,8 +85,14 @@ describe "UserState"  do
         response.status.should == 200
       end
       api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
-      @api_endpoints.each do |endpoint|
+      @private_api_endpoints.each do |endpoint|
         get "#{endpoint}?api_key=#{@locked_user.api_key}", {}, @headers
+        request.path == endpoint
+        response.status.should == 404
+      end
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
         response.status.should == 404
       end
     end
@@ -79,6 +103,10 @@ describe "UserState"  do
         get endpoint, {}, @headers
         response.status.should == 404
       end
+      @public_user_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        response.status.should == 404
+      end
       @tables_endpoints.each do |endpoint|
         get endpoint, {}, @headers
         response.status.should == 404
@@ -88,14 +116,15 @@ describe "UserState"  do
         response.status.should == 404
       end
       api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
-      @api_endpoints.each do |endpoint|
-        get "#{endpoint}", {}, @headers
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
         response.status.should == 404
       end
     end
     it 'non-logged user accessing a locked user resources' do
       host! "#{@locked_user.username}.localhost.lan"
-      @user_endpoints.each do |endpoint|
+      @public_user_endpoints.each do |endpoint|
         get endpoint, {}, @headers
         response.status.should == 404
       end
@@ -108,9 +137,122 @@ describe "UserState"  do
         response.status.should == 404
       end
       api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
-      @api_endpoints.each do |endpoint|
-        get "#{endpoint}", {}, @headers
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
         response.status.should == 404
+      end
+    end
+  end
+  describe '#non locked user' do
+    before(:all) do
+      @locked_user.state = 'active'
+      @locked_user.save
+    end
+    after(:all) do
+      @locked_user.state = 'locked'
+      @locked_user.save
+    end
+    it 'owner accessing their resources' do
+      login(@locked_user)
+      host! "#{@locked_user.username}.localhost.lan"
+      @dashboard_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        request.path.should_not == '/upgrade_trial'
+        response.status.should == 200
+      end
+      @user_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        request.path.should_not == '/upgrade_trial'
+        response.status.should == 200
+      end
+      @public_user_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        request.path.should_not == '/upgrade_trial'
+        response.status.should == 200
+      end
+      @tables_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        request.path.should_not == '/upgrade_trial'
+        response.status.should == 200
+      end
+      @viz_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        request.path.should_not == '/upgrade_trial'
+        response.status.should == 200
+      end
+      api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
+      @private_api_endpoints.each do |endpoint|
+        get "#{endpoint}?api_key=#{@locked_user.api_key}", {}, @headers
+        request.path == endpoint
+        response.status.should == 200
+      end
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
+        response.status.should == 200
+      end
+    end
+    it 'non locked user accessing a locked user resources' do
+      login(@non_locked_user)
+      @user_endpoints.each do |endpoint|
+        host! "#{@locked_user.username}.localhost.lan"
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      @public_user_endpoints.each do |endpoint|
+        host! "#{@locked_user.username}.localhost.lan"
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      @tables_endpoints.each do |endpoint|
+        host! "#{@locked_user.username}.localhost.lan"
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      @viz_endpoints.each do |endpoint|
+        host! "#{@locked_user.username}.localhost.lan"
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
+        response.status.should == 200
+      end
+    end
+    it 'non-logged user accessing a locked user resources' do
+      host! "#{@locked_user.username}.localhost.lan"
+      @public_user_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      @tables_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      @viz_endpoints.each do |endpoint|
+        get endpoint, {}, @headers
+        follow_redirects
+        response.status.should == 200
+      end
+      api_headers = {'CONTENT_TYPE'  => 'application/json', :format => "json" }
+      @public_api_endpoints.each do |endpoint|
+        get endpoint, {}, @api_headers
+        request.path == endpoint
+        response.status.should == 200
       end
     end
   end

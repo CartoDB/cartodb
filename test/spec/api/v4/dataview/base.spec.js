3eb87b7ddfcce450ccf6b9c035f20c83d01dea26
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-11-13 13:30:30 +0100

    Refactor dataview base tests

--- test/spec/api/v4/dataview/base.spec.js
+++ test/spec/api/v4/dataview/base.spec.js
@@ -8,77 +8,93 @@ describe('api/v4/dataview/base', function () {
     expect(base.getStatus()).toEqual(base._status);
   });
 
-  it('.isLoading should return true if loading and false otherwise', function () {
-    base._status = status.NOT_LOADED;
-    expect(base.isLoading()).toBe(false);
-    base._status = status.LOADING;
-    expect(base.isLoading()).toBe(true);
+  describe('.isLoading', function () {
+    it('should return true if loading and false otherwise', function () {
+      base._status = status.NOT_LOADED;
+      expect(base.isLoading()).toBe(false);
+      base._status = status.LOADING;
+      expect(base.isLoading()).toBe(true);
+    });
+
+    it('should return true if loading and false otherwise', function () {
+      base._status = status.NOT_LOADED;
+      expect(base.isLoaded()).toBe(false);
+      base._status = status.LOADED;
+      expect(base.isLoaded()).toBe(true);
+    });
   });
 
-  it('.isLoaded should return true if loading and false otherwise', function () {
-    base._status = status.NOT_LOADED;
-    expect(base.isLoaded()).toBe(false);
-    base._status = status.LOADED;
-    expect(base.isLoaded()).toBe(true);
+  describe('.hasError', function () {
+    it('should return true if loading and false otherwise', function () {
+      base._status = status.NOT_LOADED;
+      expect(base.hasError()).toBe(false);
+      base._status = status.ERROR;
+      expect(base.hasError()).toBe(true);
+    });
   });
 
-  it('.hasError should return true if loading and false otherwise', function () {
-    base._status = status.NOT_LOADED;
-    expect(base.hasError()).toBe(false);
-    base._status = status.ERROR;
-    expect(base.hasError()).toBe(true);
+  describe('.enable', function () {
+    it('should enable the dataview', function () {
+      base.enable();
+      expect(base._enabled).toBe(true);
+    });
+
+    it('should return the dataview', function () {
+      expect(base.enable()).toBe(base);
+    });
   });
 
-  it('.enable should enable the dataview', function () {
-    base.enable();
-    expect(base._enabled).toBe(true);
+  describe('.disable', function () {
+    it('should disable the dataview', function () {
+      base.disable();
+      expect(base._enabled).toBe(false);
+    });
+
+    it('should return the dataview', function () {
+      expect(base.disable()).toBe(base);
+    });
   });
 
-  it('.enable should return the dataview', function () {
-    expect(base.enable()).toBe(base);
+  describe('.isEnabled', function () {
+    it('should return true if enabled and false otherwise', function () {
+      base.disable();
+      expect(base.isEnabled()).toBe(false);
+      base.enable();
+      expect(base.isEnabled()).toBe(true);
+    });
   });
 
-  it('.disable should disable the dataview', function () {
-    base.disable();
-    expect(base._enabled).toBe(false);
+  describe('.setColumn', function () {
+    it('should set the column name as string', function () {
+      var column = 'column-test';
+      base.setColumn(column);
+      expect(base._column).toBe(column);
+    });
+
+    it('should throw an error if the argument is not string or undefined', function () {
+      expect(function () { base.setColumn(); }).toThrowError(TypeError, 'Column property is required.');
+      expect(function () { base.setColumn(12); }).toThrowError(TypeError, 'Column property must be a string.');
+      expect(function () { base.setColumn(''); }).toThrowError(TypeError, 'Column property must be not empty.');
+    });
+
+    it('should return the dataview', function () {
+      var column = 'column-test';
+      expect(base.setColumn(column)).toBe(base);
+    });
   });
 
-  it('.disable should return the dataview', function () {
-    expect(base.disable()).toBe(base);
+  describe('.getColumn', function () {
+    it('should return the column name', function () {
+      var column = 'column-test2';
+      base._column = column;
+      expect(base.getColumn()).toBe(column);
+    });
   });
 
-  it('.isEnabled should return true if enabled and false otherwise', function () {
-    base.disable();
-    expect(base.isEnabled()).toBe(false);
-    base.enable();
-    expect(base.isEnabled()).toBe(true);
-  });
-
-  it('.setColumn should set the column name as string', function () {
-    var column = 'column-test';
-    base.setColumn(column);
-    expect(base._column).toBe(column);
-  });
-
-  it('.setColumn should throw an error if the argument is not string or undefined', function () {
-    expect(function () { base.setColumn(); }).toThrowError(TypeError, 'Column property is required.');
-    expect(function () { base.setColumn(12); }).toThrowError(TypeError, 'Column property must be a string.');
-    expect(function () { base.setColumn(''); }).toThrowError(TypeError, 'Column property must be not empty.');
-  });
-
-  it('.setColumn should return the dataview', function () {
-    var column = 'column-test';
-    expect(base.setColumn(column)).toBe(base);
-  });
-
-  it('.getColumn should return the column name', function () {
-    var column = 'column-test2';
-    base._column = column;
-    expect(base.getColumn()).toBe(column);
-  });
-
-  it('.getData should not be defined in the base dataview', function () {
-    expect(function () { base.getData(); }).toThrowError(Error, 'getData must be implemented by the particular dataview.');
+  describe('.getData', function () {
+    it('.getData should not be defined in the base dataview', function () {
+      expect(function () { base.getData(); }).toThrowError(Error, 'getData must be implemented by the particular dataview.');
+    });
   });
 
   describe('._changeProperty', function () {

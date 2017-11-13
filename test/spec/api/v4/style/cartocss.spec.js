cbe54a7b04a1fed20b7fa09935d3f8c2a8a2e1df
IagoLast <iago.lastra@gmail.com>
2017-11-13 17:59:48 +0100

    CartoCSS throws CartoErrors when invalid

--- test/spec/api/v4/style/cartocss.spec.js
+++ test/spec/api/v4/style/cartocss.spec.js
@@ -1,6 +1,6 @@
 var carto = require('../../../../../src/api/v4');
 
-fdescribe('api/v4/style/cartocss', function () {
+describe('api/v4/style/cartocss', function () {
   var cartoCSS;
 
   beforeEach(function () {
@@ -37,15 +37,34 @@ fdescribe('api/v4/style/cartocss', function () {
         apiKey: '84fdbd587e4a942510270a48e843b4c1baa11e18',
         username: 'cartojs-test'
       });
-      var source = carto.source.Dataset('ne_10m_populated_places_simple');
+      var source = new carto.source.Dataset('ne_10m_populated_places_simple');
       var invalidCartoCSS = new carto.style.CartoCSS('#layer { invalid-property: 10; }');
 
       invalidCartoCSS.on('error', function (cartoError) {
-        expect(cartoError.message).toEqual('');
+        expect(cartoError.message).toMatch(/Unrecognized rule: invalid-property/);
         done();
       });
       var layer = new carto.layer.Layer(source, invalidCartoCSS);
-      client.addLayer(layer);
+      client.addLayer(layer)
+        .catch(function () { }); // Prevent console "uncaught error" warning.
+    });
+
+    it('should NOT trigger a CartoError when the style is valid but there are some other errors', function (done) {
+      var client = new carto.Client({
+        apiKey: '84fdbd587e4a942510270a48e843b4c1baa11e18',
+        username: 'cartojs-test'
+      });
+      var source = new carto.source.Dataset('invalid_dataset');
+      var invalidCartoCSS = new carto.style.CartoCSS('#layer { marker-with: 10; }');
+      var errorCallbackSpy = jasmine.createSpy('errorCallbackSpy').and.callThrough();
+      invalidCartoCSS.on('error', errorCallbackSpy);
+      var layer = new carto.layer.Layer(source, invalidCartoCSS);
+
+      client.addLayer(layer)
+        .catch(function () {
+          expect(errorCallbackSpy).not.toHaveBeenCalled();
+          done();
+        });
     });
   });
 });

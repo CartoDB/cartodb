9f9477afa1b95d5d751602c8d396d328531eba2d
rubenmoya <ruben@carto.com>
2017-11-13 12:17:17 +0100

    Add DraggedNode event

--- spec/lib/carto/tracking/events_spec.rb
+++ spec/lib/carto/tracking/events_spec.rb
@@ -1382,6 +1382,29 @@ module Carto
             end
           end
         end
+
+        describe DraggedNode do
+          before (:all) { @event_class = self.class.description.constantize }
+          after  (:all) { @event_class = nil }
+
+          describe '#properties validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'requires a user_id' do
+              @event = @event_class.new(@user.id, visualization_id: @visualization.id)
+            end
+
+            it 'requires a visualization_id' do
+              @event = @event_class.new(@user.id, user_id: @user.id)
+            end
+          end
+        end
       end
     end
   end

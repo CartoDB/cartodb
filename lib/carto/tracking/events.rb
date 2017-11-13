aed18776d67dfa318607a20376b11e0bf1de4cea
rubenmoya <ruben@carto.com>
2017-11-13 12:59:42 +0100

    Add AddedLayer event

--- lib/carto/tracking/events.rb
+++ lib/carto/tracking/events.rb
@@ -241,6 +241,15 @@ module Carto
 
         required_properties :user_id, :visualization_id
       end
+
+      class AddedLayer < Event
+        include Carto::Tracking::Services::Segment
+
+        include Carto::Tracking::Validators::Visualization::Writable
+        include Carto::Tracking::Validators::User
+
+        required_properties :user_id, :visualization_id, :layer_id
+      end
     end
   end
 end
--- spec/lib/carto/tracking/events_spec.rb
+++ spec/lib/carto/tracking/events_spec.rb
@@ -1334,55 +1334,6 @@ module Carto
           end
         end
 
-        describe DownloadLayer do
-          before (:all) { @event_class = self.class.description.constantize }
-          after  (:all) { @event_class = nil }
-
-          describe '#properties validation' do
-            after(:each) do
-              expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
-            end
-
-            after(:all) do
-              @event = nil
-            end
-
-            it 'requires a user_id' do
-              @event = @event_class.new(@user.id,
-                                        visualization_id: @visualization.id,
-                                        layer_id: @visualization.data_layers.first.id,
-                                        format: 'csv',
-                                        source: 'd0',
-                                        table_name: 'test',
-                                        visible: true)
-            end
-
-            it 'requires a visualization_id' do
-              @event = @event_class.new(@user.id,
-                                        user_id: @user.id,
-                                        layer_id: @visualization.data_layers.first.id,
-                                        format: 'csv',
-                                        source: 'd0',
-                                        table_name: 'test',
-                                        visible: true)
-            end
-
-            it 'requires an attribute' do
-              @event = @event_class.new(@user.id,
-                                        visualization_id: @visualization.id,
-                                        user_id: @user.id,
-                                        attribute_type: 'string')
-            end
-
-            it 'requires an attribute_type' do
-              @event = @event_class.new(@user.id,
-                                        visualization_id: @visualization.id,
-                                        user_id: @user.id,
-                                        attribute: 'my_column')
-            end
-          end
-        end
-
         describe DraggedNode do
           before (:all) { @event_class = self.class.description.constantize }
           after  (:all) { @event_class = nil }
@@ -1402,6 +1353,39 @@ module Carto
 
             it 'requires a visualization_id' do
               @event = @event_class.new(@user.id, user_id: @user.id)
+            end
+          end
+        end
+
+        describe AddedLayer do
+          before (:all) { @event_class = self.class.description.constantize }
+          after  (:all) { @event_class = nil }
+
+          describe '#properties validation' do
+            after(:each) do
+              expect { @event.report! }.to raise_error(Carto::UnprocesableEntityError)
+            end
+
+            after(:all) do
+              @event = nil
+            end
+
+            it 'requires a user_id' do
+              @event = @event_class.new(@user.id,
+                                        layer_id: @visualization.data_layers.first.id,
+                                        visualization_id: @visualization.id)
+            end
+
+            it 'requires a visualization_id' do
+              @event = @event_class.new(@user.id,
+                                        layer_id: @visualization.data_layers.first.id,
+                                        user_id: @user.id)
+            end
+
+            it 'requires a layer_id' do
+              @event = @event_class.new(@user.id,
+                                        user_id: @user.id,
+                                        visualization_id: @visualization.id)
             end
           end
         end

f327db66714491e503f1a9e02dfdb74cca6fdb88
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-11-14 19:32:02 +0100

    Deploy examples and docs to gh-pages

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,8 +2,7 @@
 
 # Pull requests and commits to other branches shouldn't try to deploy
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "public-api" ]; then
-  echo "The branch $TRAVIS_BRANCH does not generate docs"
-  echo "Skip deploy."
+  echo "Skip deploy: $TRAVIS_BRANCH"
   exit 0
 fi
 
@@ -11,18 +10,26 @@ echo "Starting deployment"
 echo "Target: gh-pages branch"
 
 DOCS_DIR="docs"
+EXAMPLES_DIR="examples"
 TMP_DOCS_DIR="tmp_docs"
+TMP_EXAMPLES_DIR="tmp_examples"
+
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
 
 echo "Generating documentation"
 npm run docs
-npm run docs:all
-cp config/jsdoc/index.html $DOCS_DIR/index.html || exit 1
+npm run docs:internal
 
-echo "Move to tmp file"
+echo "Move documentation to tmp file"
 mv $DOCS_DIR $TMP_DOCS_DIR
+
+echo "Move examples to tmp file"
+mv $EXAMPLES_DIR $TMP_EXAMPLES_DIR
+
+echo "Copy index.html"
+cp config/jsdoc/index.html $TMP_DOCS_DIR/index.html || exit 1
 
 echo "Fetching gh-pages branch"
 git fetch origin gh-pages:refs/remotes/origin/gh-pages || exit 1
@@ -31,9 +38,13 @@ echo "Checking out gh-pages branch"
 git checkout -b gh-pages origin/gh-pages || exit 1
 
 echo "Copying source content to root"
-rm -rf $DOCS_DIR || exit 1
-mv $TMP_DOCS_DIR $DOCS_DIR || exit 1
-mv $DOCS_DIR/index.html index.html || exit 1
+rm -rf $DOCS_DIR/v4 || exit 1
+mv $TMP_DOCS_DIR/public $DOCS_DIR/v4 || exit 1
+rm -rf $DOCS_DIR/v4-internal || exit 1
+mv $TMP_DOCS_DIR/internal $DOCS_DIR/v4-internal || exit 1
+rm -rf $EXAMPLES_DIR/v4 || exit 1
+mv $TMP_EXAMPLES_DIR/public $EXAMPLES_DIR/v4 || exit 1
+mv $TMP_DOCS_DIR/index.html index.html || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Cartofante" || exit 1
@@ -41,11 +52,9 @@ git config user.email "systems@cartodb.com" || exit 1
 
 git add index.html || exit 1
 git add $DOCS_DIR || exit 1
-git commit --allow-empty -m "Update docs for $CURRENT_COMMIT" || exit 1
+git add $EXAMPLES_DIR || exit 1
+git commit --allow-empty -m "Update docs/examples for $CURRENT_COMMIT" || exit 1
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-
-echo "Cleaning up temp files"
-rm -rf $DOCS_DIR
 
 echo "Deployed successfully."
 exit 0

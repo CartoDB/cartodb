eb84291d7ad7e2578b11e9240b4c1f80b4e69151
IagoLast <iago.lastra@gmail.com>
2017-11-15 09:37:45 +0100

    Dataset trigger errors

--- test/spec/api/v4/source/dataset.spec.js
+++ test/spec/api/v4/source/dataset.spec.js
@@ -1,6 +1,6 @@
 var carto = require('../../../../../src/api/v4');
 
-describe('api/v4/source/dataset', function () {
+fdescribe('api/v4/source/dataset', function () {
   describe('constructor', function () {
     it('should return a new Dataset object', function () {
       var populatedPlacesDataset = new carto.source.Dataset('ne_10m_populated_places_simple');
@@ -41,6 +41,25 @@ describe('api/v4/source/dataset', function () {
       expect(internalModel.get('id')).toEqual(populatedPlacesDataset.getId());
       expect(internalModel.get('query')).toEqual('SELECT * from ne_10m_populated_places_simple');
       expect(internalModel._engine).toEqual('fakeEngine');
+    });
+  });
+
+  describe('errors', function () {
+    it('should trigger an error when invalid', function (done) {
+      var client = new carto.Client({
+        apiKey: '84fdbd587e4a942510270a48e843b4c1baa11e18',
+        username: 'cartojs-test'
+      });
+      var invalidSource = new carto.source.Dataset('invalid_dataset');
+      var cartoCSS = new carto.style.CartoCSS('#layer { marker-fill: red; }');
+
+      invalidSource.on('error', function (cartoError) {
+        expect(cartoError.message).toMatch(/relation "invalid_dataset" does not exist/);
+        done();
+      });
+      var layer = new carto.layer.Layer(invalidSource, cartoCSS);
+
+      client.addLayer(layer).catch(function () { }); // Prevent console "uncaught error" warning.
     });
   });
 });

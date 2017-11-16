64cbc19099906001b60c8299a2bfdb735193bf9a
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-11-16 13:00:40 +0100

    Reorder deploy script actions

--- scripts/deploy-dist-docs-examples.sh
+++ scripts/deploy-dist-docs-examples.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-# Deploy only semver tags (branches)
-
 SEMVER_PATTERN="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$"
+
+DIST_DIR="dist"
+DOCS_DIR="docs"
+EXAMPLES_DIR="examples"
+TMP_DIST_DIR="tmp_dist"
+TMP_DOCS_DIR="tmp_docs"
+TMP_EXAMPLES_DIR="tmp_examples"
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
+
+# - Deploy only semver tags
 
 if [[ ! $TRAVIS_BRANCH =~ $SEMVER_PATTERN ]]; then
   echo "Skip deploy: $TRAVIS_BRANCH"
   exit 0
 fi
 
-# - Dist deployment
-
-echo "Starting dist deployment"
-echo "Target: dist branch"
-
-DIST_DIR="dist"
-TMP_DIST_DIR="tmp_dist"
-PUBLIC_DIR="public"
-INTERNAL_DIR="internal"
-CURRENT_COMMIT=`git rev-parse HEAD`
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
+# - Generation
 
 echo "Generating dist"
 npm run dist
 
-echo "Move to tmp file"
+echo "Generating docs"
+npm run docs
+npm run docs:internal
+
+echo "Move dist to tmp file"
 mv $DIST_DIR $TMP_DIST_DIR
+
+echo "Move docs to tmp file"
+mv $DOCS_DIR $TMP_DOCS_DIR
+
+echo "Move examples to tmp file"
+mv $EXAMPLES_DIR $TMP_EXAMPLES_DIR
+
+echo "Copy index.html"
+cp config/jsdoc/index.html $TMP_DOCS_DIR/index.html || exit 1
+
+# - Deployment
+
+echo "Starting dist deployment"
+echo "Target: dist branch"
 
 echo "Fetching dist branch"
 git fetch origin dist:refs/remotes/origin/dist || exit 1
@@ -37,7 +55,7 @@ git checkout -b dist origin/dist || exit 1
 
 echo "Copying source content to root"
 rm -rf *.js *.map || exit 1
-cp $TMP_DIST_DIR/$PUBLIC_DIR/* . || exit 1
+cp $TMP_DIST_DIR/public/* . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Cartofante" || exit 1
@@ -51,38 +69,8 @@ git push "$ORIGIN_URL_WITH_CREDENTIALS" "@${TRAVIS_BRANCH:1}"
 
 echo "Dist deployed successfully."
 
-# - Checking out main branch
-
-echo "Checking out $TRAVIS_BRANCH"
-git checkout -- . || exit 1
-git checkout -b $TRAVIS_BRANCH || exit 1
-
-# - Docs/examples deployment
-
 echo "Starting docs/examples deployment"
 echo "Target: gh-pages branch"
-
-DOCS_DIR="docs"
-EXAMPLES_DIR="examples"
-TMP_DOCS_DIR="tmp_docs"
-TMP_EXAMPLES_DIR="tmp_examples"
-
-CURRENT_COMMIT=`git rev-parse HEAD`
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS="https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG"
-
-echo "Generating documentation"
-npm run docs
-npm run docs:internal
-
-echo "Move documentation to tmp file"
-mv $DOCS_DIR $TMP_DOCS_DIR
-
-echo "Move examples to tmp file"
-mv $EXAMPLES_DIR $TMP_EXAMPLES_DIR
-
-echo "Copy index.html"
-cp config/jsdoc/index.html $TMP_DOCS_DIR/index.html || exit 1
 
 echo "Fetching gh-pages branch"
 git fetch origin gh-pages:refs/remotes/origin/gh-pages || exit 1
@@ -116,4 +104,5 @@ git commit --allow-empty -m "Update docs/examples for $CURRENT_COMMIT" || exit 1
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
 
 echo "Docs/examples deployed successfully."
+
 exit 0

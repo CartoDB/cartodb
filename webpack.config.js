e95def35d75f797cb9c2514bc04da41fc0c14038
IagoLast <iago.lastra@gmail.com>
2017-12-05 16:45:15 +0100

    Add webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,13 @@ var path = require('path');
 module.exports = {
   entry: './src/api/v4/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/public'),
     filename: 'carto.bundle.js',
     library: 'carto',
     libraryTarget: 'umd'
+  },
+  // Tell webpack not to include Leaflet in the bundle. Leaflet should will be included manually
+  externals: {
+    leaflet: 'L'
   }
 };

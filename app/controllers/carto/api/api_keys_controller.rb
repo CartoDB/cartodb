3aede493d118c3f2722ef2eadda5b1582a9942bb
Alex Martín <amartin@carto.com>
2017-12-12 20:13:30 +0100

    update controller

--- app/controllers/carto/api/api_keys_controller.rb
+++ app/controllers/carto/api/api_keys_controller.rb
@@ -1,13 +1,14 @@
 class Carto::Api::ApiKeysController < ::Api::ApplicationController
-    ssl_required :create
-    before_filter :optional_api_authorization
+  ssl_required :create
+  before_filter :optional_api_authorization
 
-    def create
-      api_key = ::Carto::ApiKey.new
-      api_key.user_id = current_user.id
-      api_key.type = ::Carto::ApiKey::TYPE_REGULAR
-      api_key.name = params[:name]
-      api_key.save!
-      render_jsonp(::Carto::Api::ApiKeyPresenter.new(api_key).to_poro)
-    end
+  def create
+    api_key = ::Carto::ApiKey.new
+    api_key.user_id = current_user.id
+    api_key.type = ::Carto::ApiKey::TYPE_REGULAR
+    api_key.name = params[:name]
+    api_key.grants_json = params[:grants]
+    api_key.save!
+    render_jsonp(::Carto::Api::ApiKeyPresenter.new(api_key).to_poro)
+  end
 end

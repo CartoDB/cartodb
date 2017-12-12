02817229317e1eb019591a22a48144d4d9bae0e1
Alex Martín <amartin@carto.com>
2017-12-12 10:32:35 +0100

    Basic ApiKey creation

--- app/controllers/carto/api/api_keys_controller.rb
+++ app/controllers/carto/api/api_keys_controller.rb
@@ -1,11 +1,13 @@
-module Carto
-    module Api
-        class ApiKeysController < ::Api::ApplicationController
-            ssl_required: :create
+class Carto::Api::ApiKeysController < ::Api::ApplicationController
+    ssl_required :create
+    before_filter :optional_api_authorization
 
-            def create
-                
-            end
-        end
+    def create
+      api_key = ::Carto::ApiKey.new
+      api_key.user_id = current_user.id
+      api_key.type = ::Carto::ApiKey::TYPE_REGULAR
+      api_key.name = params[:name]
+      api_key.save!
+      render_jsonp(::Carto::Api::ApiKeyPresenter.new(api_key).to_poro)
     end
 end

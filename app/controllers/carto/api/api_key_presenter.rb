a8c615b56fd508a6b2e2cae1c0a36a1e93eb1096
Alex Martín <amartin@carto.com>
2017-12-12 20:13:19 +0100

    Update presenter

--- app/controllers/carto/api/api_key_presenter.rb
+++ app/controllers/carto/api/api_key_presenter.rb
@@ -6,15 +6,19 @@ module Carto
       def initialize(api_key)
         @api_key = api_key
       end
+
       def to_poro
         return {} unless @api_key
-
         {
           id: @api_key.id,
           user: @api_key.user.username,
           type: @api_key.type,
           token: @api_key.token,
-          grants: JSON.parse(@api_key.grants_json || '[]'),
+          grants: @api_key.grants_hash,
+          databaseConfig: {
+            role: @api_key.db_role,
+            password: @api_key.db_password
+          },
           createdAt: @api_key.created_at.to_s,
           updatedAt: @api_key.updated_at.to_s
         }

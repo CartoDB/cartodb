c46b19dabda87dd4c31ad2fb17354fd576b950f4
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-12-21 07:02:43 -0600

    Remove unused DI files

--- deep-insights/NEWS.md
+++
@@ -1,8 +0,0 @@
-# 0.1.0 ()
-* Features
-    - Widget menu uses toggle buttons
-
-# 0.0.1 (04/06/2016)
-
-* First release
-
--- deep-insights/grunt-tasks/gh-pages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    base: '.'
-  },
-  src: [
-    'dist/**/*',
-    'examples/**/*'
-  ]
-};

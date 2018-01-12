5cd250f4d3733aab193ab00e41ee6430c1b942f8
Javier Torres <jtorres@carto.com>
2018-01-12 12:04:26 +0100

    Fix platform_controller tests in parallel

--- spec/requests/superadmin/platform_controller_spec.rb
+++ spec/requests/superadmin/platform_controller_spec.rb
@@ -4,27 +4,27 @@ require_relative '../../acceptance_helper'
 require_relative '../../../app/controllers/superadmin/platform_controller'
 
 describe Superadmin::PlatformController do
-  before(:each) do
+  before(:all) do
+    @dbhost = 'platform_controller_spec.localhost'
     CartoDB::UserModule::DBService.any_instance.stubs(:enable_remote_db_user).returns(true)
-    @user1 = FactoryGirl.create(:valid_user)
-    @user2 = FactoryGirl.create(:valid_user)
+    @user1 = FactoryGirl.create(:valid_user, database_host: @dbhost)
+    @user2 = FactoryGirl.create(:valid_user, database_host: @dbhost)
   end
 
-  after(:each) do
+  after(:all) do
     @user1.destroy
     @user2.destroy
   end
 
   describe '#databases info' do
     it 'returns just one database info if passed as argument' do
-      old_db_host = @user1.database_host
       @user1.database_host = 'test1.dbhost.com'
       get superadmin_get_databases_info_url, { database_host: @user1.database_host }, superadmin_headers
       response.status.should == 200
       body = JSON.parse(response.body)
       body.has_key?('test1.dbhost.com').should == true
       body.size.should == 1
-      @user1.database_host = old_db_host
+      @user1.database_host = @dbhost
     end
 
     it 'returns just two databases info if none passed as argument' do
@@ -42,7 +42,7 @@ describe Superadmin::PlatformController do
     end
 
     it 'validate for database checking for active users using metadata' do
-      get superadmin_database_validation_url, { database_host: 'localhost' }, superadmin_headers
+      get superadmin_database_validation_url, { database_host: @dbhost }, superadmin_headers
       response.status.should == 200
       body = JSON.parse(response.body)
       body['carto_users'].size.should == 2
@@ -54,7 +54,7 @@ describe Superadmin::PlatformController do
       old_db_host = @user2.database_host
       @user2.database_host = 'test2.dbhost.com'
       @user2.save
-      get superadmin_database_validation_url, { database_host: 'localhost' }, superadmin_headers
+      get superadmin_database_validation_url, { database_host: @dbhost }, superadmin_headers
       response.status.should == 200
       body = JSON.parse(response.body)
       body['carto_users'].size.should == 1

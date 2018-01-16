24335a66ca49e0a06c9980fbb51fd35a3342e2fc
IagoLast <iago.lastra@gmail.com>
2018-01-16 15:06:24 +0100

    Add aggregation docs

--- src/api/v4/layer/aggregation.js
+++ src/api/v4/layer/aggregation.js
@@ -19,6 +19,46 @@ var VALID_PLACEMENTS = {
 
 var VALID_RESOLUTIONS = [0.5, 1, 2, 4, 8, 16, 32, 64, 128, 256];
 
+/**
+ * An aggregation can be passed to a {@link carto.layer.Layer} to reduce the number of visible points 
+ * increasing the performance.
+ * 
+ * See {@link https://carto.com/docs/carto-engine/maps-api/tile-aggregation} for more info.
+ * 
+ * @param {object} opts 
+ * @param {number} opts.threshold - The minimum number of rows in the dataset for aggregation to be applied
+ * @param {number} opts.resolution - The cell-size of the spatial aggregation grid [more info]{@link https://carto.com/docs/carto-engine/maps-api/tile-aggregation/#resolution}
+ * @param {string} opts.placement - The kind of [aggregated geometry]{@link https://carto.com/docs/carto-engine/maps-api/tile-aggregation/#placement} generated
+ * @param {object} opts.columns - The new columns are computed by a applying an aggregate function to all the points in each group
+ * @param {string} opts.columns.aggregatedFunction - The Function used to aggregate the points: avg (average), min (minimum), max (maximum) and mode (the most frequent value in the group)
+ * @param {string} opts.columns.aggregatedColumn - The name of the original column to be aggregated.
+ * 
+ * @example
+ * // Create a layer with aggregated data.
+ * const aggregationOptions = {
+ *   // Always apply aggregation
+ *   threshold: 1,
+ *   // 
+ *   resolution: 1,
+ *   // New points will be displayed at the center of the grid
+ *   placement: 'point-grid',
+ *   columns: {
+ *     // The key is the name of the new generated column
+ *     agg_population: {
+ *     // Points will display the average of the aggregated data.
+ *     aggregateFunction: 'avg',
+ *     // The column to be aggregated
+ *     aggregatedColumn: 'population'
+ *    },
+ *   }
+ * };
+ * const aggregation = new Aggregation(options);
+ * const layer = new carto.layer.Layer(source, style, { aggregation: aggregation });
+ * 
+ * @constructor
+ * @api
+ * @memberof carto.layer
+ */
 function Aggregation (opts) {
   if (!_.isFinite(opts.threshold)) {
     throw _getValidationError('thresholdRequired');

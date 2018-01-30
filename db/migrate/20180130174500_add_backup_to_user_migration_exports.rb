7a192f4514dd8df7138be72533cfcaacc2d81e16
Alex Martín <amartin@carto.com>
2018-01-30 19:58:28 +0100

    Fix table name

--- db/migrate/20180130174500_add_backup_to_user_migration_exports.rb
+++ db/migrate/20180130174500_add_backup_to_user_migration_exports.rb
@@ -4,9 +4,9 @@ include Carto::Db::MigrationHelper
 
 migration(
   Proc.new do
-    add_column :user_migration_imports, :backup, :boolean, default: false
+    add_column :user_migration_exports, :backup, :boolean, default: false
   end,
   Proc.new do
-    drop_column :user_migration_imports, :backup
+    drop_column :user_migration_export, :backup
   end
 )

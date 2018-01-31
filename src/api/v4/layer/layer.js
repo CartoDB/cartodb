39ccf3f3e4c4b6915ef5d0003d5d676656d00cfc
IagoLast <iago.lastra@gmail.com>
2018-01-31 16:53:38 +0100

    Revert "Hide aggregation from public api"
    This reverts commit 8ca44a760e74de2eb8c7d3bfb257e27f2cb87905.

--- src/api/v4/layer/layer.js
+++ src/api/v4/layer/layer.js
@@ -31,6 +31,7 @@ var metadataParser = require('./metadata/parser');
  * @param {object} [options]
  * @param {Array<string>} [options.featureClickColumns=[]] - Columns that will be available for `featureClick` events
  * @param {Array<string>} [options.featureOverColumns=[]] - Columns that will be available for `featureOver` events
+ * @param {carto.layer.Aggregation} [options.aggregation={}] - Specify {@link carto.layer.Aggregation|aggregation } options
  * @param {string} [options.id] - An unique identifier for the layer
  * @fires metadataChanged
  * @fires featureClicked

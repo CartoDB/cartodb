5dc4115116fd001825ab9372d90a8ec0deb63201
Alberto Romeu <alrocar@cartodb.com>
2018-02-01 19:06:19 +0100

    Hound

--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -2,22 +2,14 @@ FactoryGirl.define do
   factory :api_key_apis, class: Carto::ApiKey do
     type Carto::ApiKey::TYPE_REGULAR
     name { unique_name('regular api key for apis') }
-    grants [
-             {
-               type: "apis",
-               apis: ["sql", "maps"]
-             }
-           ]
+    grants [{ type: "apis",
+              apis: ["sql", "maps"] }]
   end
 
   factory :master_api_key, class: Carto::ApiKey do
     type Carto::ApiKey::TYPE_MASTER
     name Carto::ApiKey::MASTER_NAME
-    grants [
-             {
-               type: "apis",
-               apis: ["sql", "maps"]
-             }
-           ]
+    grants [{ type: "apis",
+              apis: ["sql", "maps"] }]
   end
 end

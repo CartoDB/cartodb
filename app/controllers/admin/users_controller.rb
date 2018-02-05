efa3c1edaf160b308795500be88f68e61b87ffdd
Carlos Matallín <matallo@carto.com>
2018-02-05 18:40:15 +0100

    revert

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -36,7 +36,7 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def account
-    return render(file: "public/static/account/index.html", layout: false) # if current_user.has_feature_flag?('static_account')
+    return render(file: "public/static/account/index.html", layout: false) if current_user.has_feature_flag?('static_account')
 
     respond_to do |format|
       format.html { render 'account' }

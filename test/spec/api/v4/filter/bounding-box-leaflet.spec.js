c2ba2f960e6cec7a195a4f791c2934e1d023cc67
IagoLast <iago.lastra@gmail.com>
2018-02-07 10:13:17 +0100

    Pr fixes

--- test/spec/api/v4/filter/bounding-box-leaflet.spec.js
+++ test/spec/api/v4/filter/bounding-box-leaflet.spec.js
@@ -5,7 +5,7 @@ describe('api/v4/filter/bounding-box-leaflet', function () {
     it('should throw a descriptive error when initialized with invalid parameters', function () {
       expect(function () {
         new carto.filter.BoundingBoxLeaflet(undefined); // eslint-disable-line
-      }).toThrowError('Bounding box requires a leaflet map but got: undefined');
+      }).toThrowError('Bounding box requires a Leaflet map but got: undefined');
     });
   });
 });

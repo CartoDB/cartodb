cf1ab594679c528e08caacd48858ded2a808a702
Alberto Romeu <alrocar@cartodb.com>
2018-02-27 18:19:25 +0100

    add support for multiple rate limits

--- app/models/carto/rate_limit_values.rb
+++ app/models/carto/rate_limit_values.rb
@@ -1,28 +1,42 @@
 module Carto
   class RateLimitValues
-    attr_accessor :max_burst, :count_per_period, :period
+    attr_accessor :rate_limits
+
+    VALUES_PER_RATE_LIMIT = 3
 
     def initialize(values)
+      @rate_limits = []
       values = convert_to_array(values)
 
-      return if !values || values.length != 3
+      return if !values || values.length % VALUES_PER_RATE_LIMIT != 0
 
-      self.max_burst = values[0].to_i
-      self.count_per_period = values[1].to_i
-      self.period = values[2].to_i
+      values.each_slice(VALUES_PER_RATE_LIMIT) do |slice|
+        @rate_limits.push(RateLimitValue.new(slice))
+      end
     end
 
     def to_redis_array
-      [period,
-       count_per_period,
-       max_burst]
+      result = []
+      @rate_limits.reverse.each do |rate_limit|
+        result.push(rate_limit.period)
+              .push(rate_limit.count_per_period)
+              .push(rate_limit.max_burst)
+      end
+
+      result
     end
 
     def self.dump(rate_limit_values)
       return [] if rate_limit_values.nil?
-      [rate_limit_values.max_burst,
-       rate_limit_values.count_per_period,
-       rate_limit_values.period]
+
+      result = []
+      rate_limit_values.rate_limits.each_value do |rate_limit|
+        result.push(rate_limit.max_burst)
+              .push(rate_limit.count_per_period)
+              .push(rate_limit.period)
+      end
+
+      result
     end
 
     def self.load(values)
@@ -34,6 +48,17 @@ module Carto
     def convert_to_array(values)
       return [] if values.nil? || values.empty?
       return values.delete('{}').split(',') if values.is_a? String
+      values
+    end
+  end
+
+  class RateLimitValue
+    attr_accessor :max_burst, :count_per_period, :period
+
+    def initialize(values)
+      self.max_burst = values[0].to_i
+      self.count_per_period = values[1].to_i
+      self.period = values[2].to_i
     end
   end
 end

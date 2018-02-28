f9b48af7bd174a2f6185d50e1cebccb8811605d5
Alberto Romeu <alrocar@cartodb.com>
2018-02-28 14:23:02 +0100

    use sequel notation for migration

--- db/migrate/20180227084432_create_rate_limits.rb
+++ db/migrate/20180227084432_create_rate_limits.rb
@@ -6,23 +6,23 @@ migration(
   Proc.new do
     create_table :rate_limits do
       Uuid        :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+      column      :maps_anonymous, "integer[]"
+      column      :maps_static, "integer[]"
+      column      :maps_static_named, "integer[]"
+      column      :maps_dataview, "integer[]"
+      column      :maps_analysis, "integer[]"
+      column      :maps_tile, "integer[]"
+      column      :maps_attributes, "integer[]"
+      column      :maps_named_list, "integer[]"
+      column      :maps_named_create, "integer[]"
+      column      :maps_named_get, "integer[]"
+      column      :maps_named, "integer[]"
+      column      :maps_named_update, "integer[]"
+      column      :maps_named_delete, "integer[]"
+      column      :maps_named_tiles, "integer[]"
       DateTime    :created_at, null: false
       DateTime    :updated_at, null: false
     end
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_anonymous integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_static integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_static_named integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_dataview integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_analysis integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_tile integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_attributes integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_list integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_create integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_get integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_update integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_delete integer[]')
-    SequelRails.connection.run('ALTER TABLE "rate_limits" ADD COLUMN maps_named_tiles integer[]')
   end,
   Proc.new do
     drop_table :rate_limits

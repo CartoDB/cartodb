5e9a734a8a1b6540f3ca2c51ac6b83d202cabe5d
Alex Martín <amartin@carto.com>
2018-03-01 16:39:36 +0100

    Make transaction lose weight

--- app/controllers/carto/api/widgets_controller.rb
+++ app/controllers/carto/api/widgets_controller.rb
@@ -58,13 +58,14 @@ module Carto
       end
 
       def update_many
-        ActiveRecord::Base.transaction do
-          result = params[:_json].map do |json_widget|
-            widget = widget_with_validations(json_widget[:id])
-            WidgetPresenter.new(update_widget!(widget, json_widget)).to_poro
-          end
-          render_jsonp(result)
+        entries = params[:_json].map do |json_widget|
+          {widget: widget_with_validations(json_widget[:id]), json: json_widget }
         end
+        result = []
+        ActiveRecord::Base.transaction do
+          result = entries.map { |entry| update_widget!(entry[:widget], entry[:json])}
+        end
+        render_jsonp(result.map { |widget| WidgetPresenter.new(widget).to_poro })
       end
 
       def destroy

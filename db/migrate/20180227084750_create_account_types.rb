1ddb23b08e5370d3375da11b4b4223bc17d7fb75
Alberto Romeu <alrocar@cartodb.com>
2018-03-01 09:55:15 +0100

    change account_type db model

--- db/migrate/20180227084750_create_account_types.rb
+++ db/migrate/20180227084750_create_account_types.rb
@@ -5,14 +5,10 @@ include Carto::Db::MigrationHelper
 migration(
   Proc.new do
     create_table :account_types do
-      String      :account_type, null: false
-      foreign_key :rate_limit_id, :rate_limits, type: :uuid, on_delete: :cascade, null: false
+      String      :account_type, primary_key: true
+      foreign_key :rate_limit_id, :rate_limits, type: :uuid, on_delete: :restrict, null: false
       DateTime    :created_at, null: false
       DateTime    :updated_at, null: false
-    end
-
-    alter_table :account_types do
-      add_index :account_type, unique: true
     end
   end,
   Proc.new do

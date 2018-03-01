5795d2c5173d28bb8850eed0ad818b8658522ca7
Alberto Romeu <alrocar@cartodb.com>
2018-03-01 16:47:01 +0100

    add defaults in dates

--- db/migrate/20180227084750_create_account_types.rb
+++ db/migrate/20180227084750_create_account_types.rb
@@ -7,8 +7,8 @@ migration(
     create_table :account_types do
       String      :account_type, primary_key: true
       foreign_key :rate_limit_id, :rate_limits, type: :uuid, on_delete: :restrict, null: false
-      DateTime    :created_at, null: false
-      DateTime    :updated_at, null: false
+      DateTime    :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime    :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
     end
   end,
   Proc.new do

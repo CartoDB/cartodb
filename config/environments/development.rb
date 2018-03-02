0b90ca19de0b4fbc5a8dc9e9fe3ff59cf96e38a8
Javier Torres <jtorres@carto.com>
2018-03-02 08:44:40 +0100

    Restore X-Sendfile config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ CartoDB::Application.configure do
   config.action_controller.perform_caching = false
 
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'

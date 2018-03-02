b3568297df2eafde3afb17d92fb49fb40a7d060c
Ivan Malagon <hacheka@gmail.com>
2018-03-02 11:14:38 +0100

    Add ajax and query model fixtures

--- webpack/common/alias.js
+++ webpack/common/alias.js
@@ -1,6 +1,7 @@
 const {resolve} = require('path');
 
 module.exports = {
+  'fixtures': resolve(resolve('.'), 'lib/assets/test/spec/fixtures'),
   'backbone/core-view$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/backbone/core-view.js'),
   'backbone/core-model$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/backbone/core-model.js'),
   'jquery-ui$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/jquery-ui.js'),

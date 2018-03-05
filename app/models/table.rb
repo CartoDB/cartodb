800cb5718057a65d1e6ddae02c3015062459212f
Alex Martín <amartin@carto.com>
2018-03-05 19:45:17 +0100

    Check that cartodb_id candidate has not null values and add objectid to candidates

--- app/models/table.rb
+++ app/models/table.rb
@@ -256,15 +256,12 @@ class Table
       # In that case:
       #  - If cartodb_id already exists, remove ogc_fid
       #  - If cartodb_id does not exist, treat this field as the auxiliary column
-      aux_cartodb_id_column = nil
-      flattened_schema = schema.present? ? schema.flatten : []
 
-      if schema.present?
-        if flattened_schema.include?(:ogc_fid)
-          aux_cartodb_id_column = 'ogc_fid'
-        elsif flattened_schema.include?(:gid)
-          aux_cartodb_id_column = 'gid'
-        end
+      aux_cartodb_id_column = nil
+
+      [:objectid, :ogc_fid, :gid].each do |candidate_column|
+        break if aux_cartodb_id_column
+        aux_cartodb_id_column = candidate_column if valid_cartodb_id_candidate?(candidate_column)
       end
 
       # Remove primary key
@@ -1210,6 +1207,17 @@ class Table
   end
 
   private
+
+  def valid_cartodb_id_candidate?(col_name)
+    return false unless flattened_schema.include?(col_name)
+    owner.transaction_with_timeout(statement_timeout: STATEMENT_TIMEOUT, as: :superuser) do |db|
+      return db["SELECT COUNT(*) from #{qualified_table_name} where objectid is null"].first[:count] == 0
+    end
+  end
+
+  def flattened_schema
+    @flattened_schema ||= schema.present? ? schema.flatten : []
+  end
 
   def related_visualizations
     carto_layers = layers.map do |layer|

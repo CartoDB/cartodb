796ab59eb0cef26d70ea097d2078b10a24f5ffb0
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-03-09 18:07:01 +0100

    Visualization search API call

--- app/models/common_data.rb
+++ app/models/common_data.rb
@@ -1,13 +1,9 @@
 require_relative '../../lib/carto/http/client'
 require_relative '../../services/sql-api/sql_api'
 require_relative '../helpers/common_data_redis_cache'
+require_relative '../../lib/carto_api/json_client'
 
 class CommonData
-
-  # seconds
-  CONNECT_TIMEOUT = 45
-  DEFAULT_TIMEOUT = 60
-  NO_PAGE_LIMIT = 100000
 
   def initialize(visualizations_api_url)
     @datasets = nil
@@ -23,7 +19,13 @@ class CommonData
         is_https_request = (@visualizations_api_url =~ /^https:\/\//)
         cached_data = redis_cache.get(is_https_request)
         if cached_data.nil?
-          response = get_datasets_json('common_data', @visualizations_api_url)
+          client = CartoAPI::JsonClient.new(http_client_tag: 'common_data')
+          response = begin
+                       client.get_visualizations_v1_from_url(@visualizations_api_url)
+                     rescue Exception => e
+                       CartoDB.notify_exception(e)
+                       nil
+                     end
           if response.code == 200
             redis_cache.set(is_https_request, response.headers, response.response_body)
             @datasets = get_datasets(response.response_body)
@@ -78,24 +80,6 @@ class CommonData
         "created_at" => row["created_at"],
         "updated_at" => row["updated_at"]
       }
-  end
-
-  def get_datasets_json(http_client_tag, visualizations_api_url)
-    begin
-      http_client = Carto::Http::Client.get(http_client_tag, log_requests: true)
-      request = http_client.request(
-        visualizations_api_url,
-        method: :get,
-        connecttimeout: CONNECT_TIMEOUT,
-        timeout: DEFAULT_TIMEOUT,
-        params: { per_page: NO_PAGE_LIMIT },
-        followlocation: true
-      )
-      request.run
-    rescue Exception => e
-      CartoDB.notify_exception(e)
-      nil
-    end
   end
 
   def export_url(table_name)

89663573e977a4b7e10f8076f36cc0f2598b07bf
jesusbotella <jesus.botella@gmail.com>
2018-03-12 12:30:14 +0100

    Restore account.html.erb template, and change users_controller condition

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -36,9 +36,7 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def account
-    @has_new_dashboard = current_user.builder_enabled? && current_user.has_feature_flag?('dashboard_migration')
-
-    return render(file: "public/static/account/index.html", layout: false) if current_user.has_feature_flag?('static_account')
+    return render(file: "public/static/account/index.html", layout: false) if current_user.has_feature_flag?('dashboard_migration')
 
     respond_to do |format|
       format.html { render 'account' }

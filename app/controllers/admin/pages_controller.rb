22ee711243f8cdf74ebc9958db4043a0d6ef5e47
Román Jiménez <roman@cartodb.com>
2018-03-13 18:08:44 +0100

    builder_enabled? missing question mark

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -318,10 +318,10 @@ class Admin::PagesController < Admin::AdminController
 
   def set_vars_for_public_dashboard
     @has_new_dashboard = if @viewed_user.nil?
-                           @org.builder_enabled \
+                           @org.builder_enabled? \
                            && @org.owner.has_feature_flag?('dashboard_migration')
                          else
-                           @viewed_user.builder_enabled \
+                           @viewed_user.builder_enabled? \
                            && @viewed_user.has_feature_flag?('dashboard_migration')
                          end
 

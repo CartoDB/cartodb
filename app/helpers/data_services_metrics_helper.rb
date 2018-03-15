33f16b8711cc76a48cf6e1533464ad679926c55f
Mario de Frutos <ethervoid@gmail.com>
2018-03-15 12:30:50 +0100

    Data services metrics are now aware of the defined provider for the user

--- app/helpers/data_services_metrics_helper.rb
+++ app/helpers/data_services_metrics_helper.rb
@@ -17,21 +17,21 @@ module DataServicesMetricsHelper
   end
 
   def get_user_here_isolines_data(user, from, to)
-    get_here_isolines_data(user, from, to)
+    get_isolines_data(user, from, to)
   end
 
   def get_organization_here_isolines_data(organization, from, to)
     organization.require_organization_owner_presence!
-    get_here_isolines_data(organization.owner, from, to)
+    get_isolines_data(organization.owner, from, to)
   end
 
   def get_user_mapzen_routing_data(user, from, to)
-    get_mapzen_routing_data(user, from, to)
+    get_routing_data(user, from, to)
   end
 
   def get_organization_mapzen_routing_data(organization, from, to)
     organization.require_organization_owner_presence!
-    get_mapzen_routing_data(organization.owner, from, to)
+    get_routing_data(organization.owner, from, to)
   end
 
   def get_user_obs_snapshot_data(user, from, to)
@@ -59,7 +59,13 @@ module DataServicesMetricsHelper
     usage_metrics = CartoDB::GeocoderUsageMetrics.new(user.username, orgname)
     # FIXME removed once we have fixed to charge google geocoder users for overquota
     return 0 if user.google_maps_geocoder_enabled?
-    geocoder_key = user.google_maps_geocoder_enabled? ? :geocoder_google : :geocoder_here
+    if user.geocoder_provider.blank? || user.geocoder_provider == 'mapbox'
+      geocoder_key = :geocoder_mapbox
+    elsif user.geocoder_provider == 'heremaps'
+      geocoder_key = :geocoder_here
+    elsif user.geocoder_provider == 'mapzen'
+      geocoder_key = :geocoder_mapzen
+    end
     cache_hits = 0
     success = usage_metrics.get_sum_by_date_range(geocoder_key, :success_responses, from, to)
     empty = usage_metrics.get_sum_by_date_range(geocoder_key, :empty_responses, from, to)
@@ -67,12 +73,19 @@ module DataServicesMetricsHelper
     success + empty + hit
   end
 
-  def get_here_isolines_data(user, from, to)
+  def get_isolines_data(user, from, to)
     orgname = user.organization.nil? ? nil : user.organization.name
     usage_metrics = CartoDB::IsolinesUsageMetrics.new(user.username, orgname)
     here_isolines_key = :here_isolines
-    success = usage_metrics.get_sum_by_date_range(here_isolines_key, :isolines_generated, from, to)
-    empty = usage_metrics.get_sum_by_date_range(here_isolines_key, :empty_responses, from, to)
+    if user.isolines_provider.blank? || user.isolines_provider == 'mapbox'
+      isolines_key = :mapbox_isolines
+    elsif user.isolines_provider == 'heremaps'
+      isolines_key = :here_isolines
+    elsif user.isolines_provider == 'mapzen'
+      isolines_key = :mapzen_isolines
+    end
+    success = usage_metrics.get_sum_by_date_range(isolines_key, :isolines_generated, from, to)
+    empty = usage_metrics.get_sum_by_date_range(isolines_key, :empty_responses, from, to)
     success + empty
   end
 
@@ -94,12 +107,16 @@ module DataServicesMetricsHelper
     success + empty
   end
 
-  def get_mapzen_routing_data(user, from, to)
+  def get_routing_data(user, from, to)
     orgname = user.organization.nil? ? nil : user.organization.name
     usage_metrics = CartoDB::RoutingUsageMetrics.new(user.username, orgname)
-    mapzen_routing_key = :routing_mapzen
-    success = usage_metrics.get_sum_by_date_range(mapzen_routing_key, :success_responses, from, to)
-    empty = usage_metrics.get_sum_by_date_range(mapzen_routing_key, :empty_responses, from, to)
+    if user.routing_provider.blank? || user.routing_provider == 'mapbox'
+      routing_key = :routing_mapbox
+    elsif user.routing_provider == 'mapzen'
+      routing_key = :routing_mapzen
+    end
+    success = usage_metrics.get_sum_by_date_range(routing_key, :success_responses, from, to)
+    empty = usage_metrics.get_sum_by_date_range(routing_key, :empty_responses, from, to)
     success + empty
   end
 end

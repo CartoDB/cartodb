9ab96a1064f3a8716af855e333247be797afb58b
Javier Torres <jtorres@carto.com>
2018-03-15 18:10:24 +0100

    Use built-ins for importer unit tests, avoid loading SequelRails and its dependencies

--- services/importer/spec/factories/pg_connection.rb
+++ services/importer/spec/factories/pg_connection.rb
@@ -9,7 +9,7 @@ module CartoDB
     module Factories
       class PGConnection
         def initialize(options = {})
-          @options = SequelRails.configuration.environment_for(Rails.env).merge(options)
+          @options = read_config.merge(options)
           create_db if options[:create_db]
         end
 
@@ -23,11 +23,19 @@ module CartoDB
 
         private
 
+        def read_config
+          begin
+            load_from_json
+          rescue
+            load_from_yml
+          end
+        end
+
         def create_db
           begin
-            connection.run("CREATE DATABASE \"#{@options[:create_db]}\"
+            connection.run("CREATE DATABASE \"#{ @options[:create_db] }\"
             WITH TEMPLATE = template_postgis
-            OWNER = #{@options[:username]}
+            OWNER = #{ @options[:user] }
             ENCODING = 'UTF8'
             CONNECTION LIMIT=-1")
           rescue Sequel::DatabaseError => e
@@ -38,6 +46,29 @@ module CartoDB
           rescue Sequel::DatabaseError => e
             raise unless e.message =~ /extension \"postgis\" already exists/
           end
+        end
+
+        def load_from_json
+          json_config = File.join(File.dirname("#{__FILE__}"), 'database.json')
+            ::JSON.parse(File.read(json_config)).each_with_object({}){ |(k,v), h|
+            h[k.to_sym] = v
+          }
+        end
+
+        def load_from_yml
+          if ENV['RAILS_DATABASE_FILE']
+            yml_config = "#{File.dirname(__FILE__)}/../../../../config/#{ENV['RAILS_DATABASE_FILE']}"
+          else
+            yml_config = "#{File.dirname(__FILE__)}/../../../../config/database.yml"
+          end
+          yml_config = YAML.load_file(yml_config)['test'].each_with_object({}){ |(k,v), h|
+            h[k.to_sym] = v
+          }
+          yml_config[:user] = yml_config.delete :username
+          yml_config[:adapter].sub!('postgresql', 'postgres')
+          yml_config
+        rescue
+          raise("Configure database settings in RAILS_ROOT/config/database.yml or spec/factories/database.json")
         end
       end
     end

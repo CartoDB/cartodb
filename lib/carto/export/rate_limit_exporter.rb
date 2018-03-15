ba32ca6f21018023e3704a6a1b4e1f0cc4bf62b0
Alberto Romeu <alrocar@cartodb.com>
2018-03-15 14:12:30 +0100

    use rate_limit methods to export

--- lib/carto/export/rate_limit_exporter.rb
+++ lib/carto/export/rate_limit_exporter.rb
@@ -1,40 +1,11 @@
 module Carto
-  module RateLimitExporterConfiguration
-    EXPORTED_RATE_LIMIT_ATTRIBUTES = [:id,
-                                      :maps_anonymous,
-                                      :maps_static,
-                                      :maps_static_named,
-                                      :maps_dataview,
-                                      :maps_dataview_search,
-                                      :maps_analysis,
-                                      :maps_tile,
-                                      :maps_attributes,
-                                      :maps_named_list,
-                                      :maps_named_create,
-                                      :maps_named_get,
-                                      :maps_named,
-                                      :maps_named_update,
-                                      :maps_named_delete,
-                                      :maps_named_tiles,
-                                      :sql_query,
-                                      :sql_query_format,
-                                      :sql_job_create,
-                                      :sql_job_get,
-                                      :sql_job_delete].freeze
-  end
-
   module RateLimitImporter
-    include RateLimitExporterConfiguration
-
     private
 
     def build_rate_limit_from_hash(exported_hash)
       return unless exported_hash
 
-      attributes = exported_hash.slice(*EXPORTED_RATE_LIMIT_ATTRIBUTES)
-      rate_limit_attributes = attributes.slice(*Carto::RateLimit::RATE_LIMIT_ATTRIBUTES)
-      rate_limit = RateLimit.new
-      rate_limit_attributes.each { |k, v| rate_limit[k] = RateLimitValues.new(v) }
+      rate_limit = Carto::RateLimit.from_api_attributes(exported_hash[:limits])
       rate_limit.id = exported_hash[:id]
 
       rate_limit.save!
@@ -43,18 +14,15 @@ module Carto
   end
 
   module RateLimitExporter
-    include RateLimitExporterConfiguration
-
     private
 
     def export_rate_limit(rate_limit)
-      other_attributes = EXPORTED_RATE_LIMIT_ATTRIBUTES - Carto::RateLimit::RATE_LIMIT_ATTRIBUTES
-      rate_limit_attributes = EXPORTED_RATE_LIMIT_ATTRIBUTES - other_attributes
+      return unless rate_limit
 
-      attrs_hash = other_attributes.map { |att| [att, rate_limit.attributes[att.to_s]] }.to_h
-      rate_limit_hash = rate_limit_attributes.map { |att| [att, rate_limit.attributes[att.to_s].to_array] }.to_h
-
-      attrs_hash.merge(rate_limit_hash)
+      {
+        id: rate_limit.id,
+        limits: rate_limit.api_attributes
+      }
     end
   end
 end

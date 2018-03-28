ef2f3110a683959c7ab2d48845fc3136b6e258d7
Alegoiko <alex@carto.com>
2018-03-28 19:04:05 +0200

    hound compliance

--- spec/services/carto/user_metadata_export_service_spec.rb
+++ spec/services/carto/user_metadata_export_service_spec.rb
@@ -186,8 +186,9 @@ describe Carto::UserMetadataExportService do
 
         # At this point, the user database is still there, but the tables got destroyed.
         # We recreate a dummy one for the visualization we did export
-        canonical_with_table = source_visualizations.find { |v| v['type'] == 'table' &&\
-           v['name'] != canonical_without_table['name'] }
+        canonical_with_table = source_visualizations.find do |v|
+                                 v['type'] == 'table' && v['name'] != canonical_without_table['name']
+                               end
         @user.in_database.execute("CREATE TABLE #{canonical_with_table['name']} (cartodb_id int)")
 
         # We import the visualizations

fd557199afc3f94599fd962579b3a152760d744e
Alberto Romeu <alrocar@cartodb.com>
2018-03-29 12:47:04 +0200

    add default account types

--- db/migrate/20180329095112_add_account_type_ref_to_users.rb
+++ db/migrate/20180329095112_add_account_type_ref_to_users.rb
@@ -6,63 +6,75 @@ migration(
   Proc.new do
     run "DO $$
          DECLARE
-         accounts text[] := (select array_agg(distinct(account_type)) from users where account_type not in (select account_type from account_types));
+         missing_accounts text[] := (SELECT ARRAY_AGG(DISTINCT(account_type))
+                                      FROM users
+                                      WHERE account_type NOT IN
+                                        (SELECT account_type FROM account_types));
+         default_accounts text[] := '{\"FREE\",
+                                      \"enterprise\",
+                                      \"[DEDICATED]\",
+                                      \"ORGANIZATION USER\"}';
 
-        BEGIN
-          IF accounts IS NULL THEN
-            RETURN;
-          END IF;
-          FOR i in 1 .. array_length(accounts, 1) LOOP
-            WITH rate_limit AS (
-              INSERT INTO rate_limits
-                (maps_anonymous,
-                maps_static,
-                maps_static_named,
-                maps_dataview,
-                maps_dataview_search,
-                maps_analysis,
-                maps_tile,
-                maps_attributes,
-                maps_named_list,
-                maps_named_create,
-                maps_named_get,
-                maps_named,
-                maps_named_update,
-                maps_named_delete,
-                maps_named_tiles,
-                maps_analysis_catalog,
-                sql_query,
-                sql_query_format,
-                sql_job_create,
-                sql_job_get,
-                sql_job_delete)
-              VALUES
-                ('{2, 2, 1}',
-                 '{1, 1, 1}',
-                 '{2, 2, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{30, 30, 1, 75, 150, 60}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{4, 4, 1}',
-                 '{1, 1, 1}',
-                 '{2, 2, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}',
-                 '{2, 2, 1}',
-                 '{1, 1, 1}',
-                 '{2, 2, 1}',
-                 '{1, 1, 1}',
-                 '{1, 1, 1}')
-              RETURNING id)
-            INSERT INTO account_types (account_type, rate_limit_id)
-              VALUES (accounts[i], (select id from rate_limit));
-          END LOOP;
-        END $$;"
+         accounts text[] := (SELECT ARRAY_AGG(a ORDER BY a)
+                              FROM (
+                                  SELECT DISTINCT UNNEST(missing_accounts || default_accounts) AS a
+                              ) s);
+
+         BEGIN
+           IF accounts IS NULL THEN
+             RETURN;
+           END IF;
+           FOR i in 1 .. array_length(accounts, 1) LOOP
+             WITH rate_limit AS (
+               INSERT INTO rate_limits
+                 (maps_anonymous,
+                 maps_static,
+                 maps_static_named,
+                 maps_dataview,
+                 maps_dataview_search,
+                 maps_analysis,
+                 maps_tile,
+                 maps_attributes,
+                 maps_named_list,
+                 maps_named_create,
+                 maps_named_get,
+                 maps_named,
+                 maps_named_update,
+                 maps_named_delete,
+                 maps_named_tiles,
+                 maps_analysis_catalog,
+                 sql_query,
+                 sql_query_format,
+                 sql_job_create,
+                 sql_job_get,
+                 sql_job_delete)
+               VALUES
+                 ('{2, 2, 1}',
+                  '{1, 1, 1}',
+                  '{2, 2, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{30, 30, 1, 75, 150, 60}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{4, 4, 1}',
+                  '{1, 1, 1}',
+                  '{2, 2, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}',
+                  '{2, 2, 1}',
+                  '{1, 1, 1}',
+                  '{2, 2, 1}',
+                  '{1, 1, 1}',
+                  '{1, 1, 1}')
+               RETURNING id)
+             INSERT INTO account_types (account_type, rate_limit_id)
+               VALUES (accounts[i], (select id from rate_limit));
+           END LOOP;
+         END $$;"
     alter_table :users do
       add_foreign_key [:account_type], :account_types, on_delete: :restrict, null: false
       add_index :account_type

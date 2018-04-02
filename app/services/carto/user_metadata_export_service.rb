1fa4b1c6f776975d6173caecf546288b9a605ef4
Alegoiko <alex@carto.com>
2018-04-02 11:53:56 +0200

    dont use constants outside the class

--- app/services/carto/user_metadata_export_service.rb
+++ app/services/carto/user_metadata_export_service.rb
@@ -287,7 +287,7 @@ module Carto
     def export_user_visualizations_to_directory(user, type, path)
       root_dir = Pathname.new(path)
       user.visualizations.where(type: type).each do |visualization|
-        next if visualization.type == Carto::Visualization::TYPE_CANONICAL && visualization.user_table.nil?
+        next if visualization.canonical? && visualization.table.nil?
 
         visualization_export = Carto::VisualizationsExportService2.new.export_visualization_json_string(
           visualization.id, user

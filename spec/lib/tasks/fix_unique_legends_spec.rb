ceabbbc1813f1f56857ef6e674b7dc981a57ac24
Alex Martín <amartin@carto.com>
2018-04-12 17:48:26 +0200

    Add a test to validate it does not remove more legends than needed

--- spec/lib/tasks/fix_unique_legends_spec.rb
+++ spec/lib/tasks/fix_unique_legends_spec.rb
@@ -12,20 +12,43 @@ describe 'cartodb:fix_unique_legends' do
     user = FactoryGirl.create(:carto_user)
     _, _, _, visualization = create_full_visualization(user)
     @layer = visualization.layers.find(&:data_layer?)
-    legend = Carto::Legend.create!(layer: @layer,
-                                   type: 'bubble',
-                                   definition: { color: '#abc' })
-    legend2 = legend.dup
-    @updated_at = legend2.updated_at = legend.updated_at - 1.minute
-    legend2.save(validate: false)
+    @legend = Carto::Legend.create!(layer: @layer,
+                                    title: "bird_name",
+                                    pre_html: "",
+                                    post_html: "",
+                                    type: "torque",
+                                    definition: {
+                                      "categories"=>[
+                                        {"title"=>"Nico", "color"=>"#ff382a", "icon"=>""},
+                                        {"title"=>"Sanne", "color"=>"#0038d1", "icon"=>""},
+                                        {"title"=>"Eric", "color"=>"#d4cf34", "icon"=>""}
+                                      ]
+                                    },
+                                    conf: {"columns"=>[]})
   end
 
   it 'deletes duplicate legend of unique type' do
+    legend2 = @legend.dup
+    legend2.type = 'choropleth'
+    updated_at = legend2.updated_at = @legend.updated_at - 1.minute
+    legend2.save(validate: false)
     @layer.reload.legends.count.should eq 2
 
     Rake.application['cartodb:fix_unique_legends'].invoke
 
     @layer.reload.legends.count.should eq 1
-    @layer.reload.legends.first.updated_at.to_s.should eq @updated_at.to_s
+    @layer.reload.legends.first.updated_at.to_s.should eq updated_at.to_s
+  end
+
+  it 'keeps layers if different type' do
+    legend2 = @legend.dup
+    updated_at = legend2.updated_at = @legend.updated_at - 1.minute
+    legend2.type = 'bubble'
+    legend2.save(validate: false)
+    @layer.reload.legends.count.should eq 2
+
+    Rake.application['cartodb:fix_unique_legends'].invoke
+
+    @layer.reload.legends.count.should eq 2
   end
 end

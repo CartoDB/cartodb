ea4ced688e6afe864aa714a9cf2b75b89d53b77e
Alberto Romeu <alrocar@cartodb.com>
2018-04-12 10:35:43 +0200

    add job role

--- db/migrate/20180412060415_add_personal_data_fields_to_users.rb
+++ db/migrate/20180412060415_add_personal_data_fields_to_users.rb
@@ -7,10 +7,12 @@ migration(
     add_column :users, :company, String
     add_column :users, :phone, String
     add_column :users, :industry, String
+    add_column :users, :job_rol, String
   end,
   Proc.new do
     drop_column :users, :company
     drop_column :users, :phone
     drop_column :users, :industry
+    drop_column :users, :job_role
   end
 )

942b13cb3e36d5fdd76885fe434652fcc04b0fa1
Alberto Romeu <alrocar@cartodb.com>
2018-04-16 14:09:38 +0200

    fix indentation

--- app/models/carto/user.rb
+++ app/models/carto/user.rb
@@ -39,12 +39,12 @@ class Carto::User < ActiveRecord::Base
 
   # INFO: select filter is done for security and performance reasons. Add new columns if needed.
   DEFAULT_SELECT = "users.email, users.username, users.admin, users.organization_id, users.id, users.avatar_url," \
-                 "users.api_key, users.database_schema, users.database_name, users.name, users.location," \
-                 "users.disqus_shortname, users.account_type, users.twitter_username, users.google_maps_key, " \
-                 "users.viewer, users.quota_in_bytes, users.database_host, users.crypted_password, " \
-                 "users.builder_enabled, users.private_tables_enabled, users.private_maps_enabled, " \
-                 "users.org_admin, users.last_name, users.google_maps_private_key, users.website, " \
-                 "users.description, users.available_for_hire, users.frontend_version, users.asset_host".freeze
+                   "users.api_key, users.database_schema, users.database_name, users.name, users.location," \
+                   "users.disqus_shortname, users.account_type, users.twitter_username, users.google_maps_key, " \
+                   "users.viewer, users.quota_in_bytes, users.database_host, users.crypted_password, " \
+                   "users.builder_enabled, users.private_tables_enabled, users.private_maps_enabled, " \
+                   "users.org_admin, users.last_name, users.google_maps_private_key, users.website, " \
+                   "users.description, users.available_for_hire, users.frontend_version, users.asset_host".freeze
 
   has_many :tables, class_name: Carto::UserTable, inverse_of: :user
   has_many :visualizations, inverse_of: :user

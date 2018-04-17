d3f7afdec637c917e33c2bb9c6507fd2c06152f7
Javier Torres <jtorres@carto.com>
2018-04-17 16:38:25 +0200

    Fix importing data imports

--- spec/services/carto/visualizations_export_service_2_spec.rb
+++ spec/services/carto/visualizations_export_service_2_spec.rb
@@ -1696,7 +1696,7 @@ describe Carto::VisualizationsExportService2 do
       it 'imports an exported dataset with external data import without a synchronization' do
         @table.data_import = FactoryGirl.create(:data_import, user: @user2, table_id: @table.id)
         @table.save!
-        FactoryGirl.create(:external_data_import_with_external_source, data_import: @table.data_import)
+        edi = FactoryGirl.create(:external_data_import_with_external_source, data_import: @table.data_import)
         exported_string = export_service.export_visualization_json_string(@table_visualization.id, @user2)
         built_viz = export_service.build_visualization_from_json_export(exported_string)
         @user2.in_database.execute("CREATE TABLE #{@table_visualization.name} (cartodb_id int)")

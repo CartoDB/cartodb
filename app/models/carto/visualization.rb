dfc69bc739b77a8441d83f6fbba96c8bc55d8811
Román Jiménez <roman@cartodb.com>
2018-04-20 13:52:38 +0200

    stop forcing named maps for builder

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -270,7 +270,7 @@ class Carto::Visualization < ActiveRecord::Base
 
   # Despite storing always a named map, no need to retrieve it for "public" visualizations
   def retrieve_named_map?
-    password_protected? || has_private_tables? || builder?
+    password_protected? || has_private_tables?
   end
 
   def has_password?

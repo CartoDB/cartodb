94716b2bb0f0cf639d1c654b34e4f915573ed596
Javier Goizueta <jgoizueta@gmail.com>
2018-04-27 10:14:50 +0200

    Make overview functions work with both AR & Sequel connectxions
    So far they required a Sequel connection.ActiveRecord is admitted now too.

--- app/services/carto/overviews_service.rb
+++ app/services/carto/overviews_service.rb
@@ -1,10 +1,12 @@
 # Create/Drop/Rename overviews...
 
+require 'carto/db/sql_interface'
+
 module Carto
   class OverviewsService
 
     def initialize(database)
-      @database = database
+      @database = Carto::Db::SqlInterface.new(database)
     end
 
     def create_overviews(table_name, tolerance_px)
@@ -17,7 +19,7 @@ module Carto
       # TODO: use CDB_RenameOverviews
       overview_tables(table_name).each do |overview_table|
         new_overview_table = overview_table.sub(table_name, new_table_name)
-        @database.execute %{
+        @database.run %{
           ALTER TABLE "#{overview_table}" RENAME TO "#{new_overview_table}"
         }
       end
@@ -27,18 +29,18 @@ module Carto
       # TODO: this is not very elegant, we could detect the existence of the
       # table some otherway or have a CDB_DropOverviews(text)  function...
       @database.run(%{SELECT cartodb.CDB_DropOverviews('#{table_name}'::REGCLASS)})
-    rescue Sequel::DatabaseError => e
+    rescue Carto::Db::SqlInterface::Error => e
       raise unless e.to_s.match /relation .+ does not exist/
     end
 
     def overview_tables(table_name)
       overviews_data = @database.fetch(%{SELECT * FROM cartodb.CDB_Overviews('#{table_name}'::REGCLASS)})
       if overviews_data
-        overviews_data.map(:overview_table).to_a
+        overviews_data.map { |row|  row[:overview_table] }
       else
         []
       end
-    rescue Sequel::DatabaseError => e
+    rescue Carto::Db::SqlInterface::Error => e
       raise unless e.to_s.match /relation .does not exist/
       []
     end

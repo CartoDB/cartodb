91ec5f1988dcbcbb54d22b735eb191e3f0876867
Pablo Alonso Garcia <pablo@cartodb.com>
2018-05-11 12:13:25 +0200

    Hound

--- app/helpers/google_tag_manager_helper.rb
+++ app/helpers/google_tag_manager_helper.rb
@@ -1,15 +1,15 @@
 module GoogleTagManagerHelper
-  def insert_google_tag_manager(container_name='primary')
+  def insert_google_tag_manager(container_name = 'primary')
     container_id = get_container_id(container_name)
     if container_id.present? && params[:cookies] != '0'
-      render(:partial => 'shared/google_tag_manager', :locals => { container_id: container_id })
+      render(partial: 'shared/google_tag_manager', locals: { container_id: container_id })
     end
   end
 
-  def insert_google_tag_manager_no_script(container_name='primary')
+  def insert_google_tag_manager_no_script(container_name = 'primary')
     container_id = get_container_id(container_name)
     if container_id.present? && params[:cookies] != '0'
-      render(:partial => 'shared/google_tag_manager_no_script', :locals => { container_id: container_id })
+      render(partial: 'shared/google_tag_manager_no_script', locals: { container_id: container_id })
     end
   end
 

1134971c23e6ceb8155aa1484a033de229a0533b
Ivan Malagon <hacheka@gmail.com>
2018-05-11 15:03:28 +0200

    Apply CR suggestion

--- src/api/v4/client.js
+++ src/api/v4/client.js
@@ -163,12 +163,8 @@ Client.prototype.removeLayer = function (layer) {
  * @api
  */
 Client.prototype.removeLayers = function (layers) {
-  var clone = layers.slice(0);
-  var length = layers.length;
-  var i = 0;
-  for (i = 0; i < length; i++) {
-    this._removeLayer(clone[i]);
-  }
+  var layersToRemove = layers.slice(0);
+  layersToRemove.forEach(this._removeLayer, this);
 
   return this._reload();
 };

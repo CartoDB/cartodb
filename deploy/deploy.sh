251d91e4a3d52caddb8dde0286058b147d6c208f
Ian Yon <ian.alonyon@gmail.com>
2018-05-31 12:10:01 -0400

    added sshpass deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 clear
-echo "Good morning, world."
+echo "Good morning, world. Deploying"
+
+#sshpass -p $HOST_PASS scp -o stricthostkeychecking=no golang-ci-example root@$SSH_HOST:~/
+#sshpass -p $HOST_PASS ssh -o StrictHostKeyChecking=no root@$SSH_HOST DB_HOST=$DB_HOST DB_USER=$DB_USER DB_PASS=$DB_PASS DB_BUCKET=$DB_BUCKET ./golang-ci-example
+
 ansible-playbook ./deploy/install.yml -i ./deploy/hosts

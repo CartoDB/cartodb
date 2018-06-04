dc7bb6a2ac0ac4309de7f063509a4867afbd70a1
Giuliano P. Tognarelli Buono-core <gtognarelli@entel.cl>
2018-06-04 16:41:01 -0400

    create dev user

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # this could be any runnable code or shell script, really
+
+# Create a development user
+sh /root/cartodb/script/create_dev_user &
+
 bundle exec script/resque &
 bundle exec thin start --threaded -p 3000 --threadpool-size 5 &
 cd /root/CartoDB-SQL-API && node app.js &

ff9b6f007099f50bfdd89f4cd37e50117a480a7f
Giuliano P. Tognarelli Buono-core <gtognarelli@entel.cl>
2018-06-12 16:00:43 -0400

    Ansible verbose mode

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,4 +5,4 @@ echo "Good morning, world. Deploying"
 #sshpass -p $HOST_PASS scp -o stricthostkeychecking=no golang-ci-example root@$SSH_HOST:~/
 #sshpass -p $HOST_PASS ssh -o StrictHostKeyChecking=no root@$SSH_HOST DB_HOST=$DB_HOST DB_USER=$DB_USER DB_PASS=$DB_PASS DB_BUCKET=$DB_BUCKET ./golang-ci-example
 
-ansible-playbook ./deploy/install.yml -i ./deploy/hosts
+ansible-playbook ./deploy/install.yml -vvvv -i ./deploy/hosts

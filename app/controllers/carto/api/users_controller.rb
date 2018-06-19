282ad5e3665a03477ab8dc4ecdc9e05e1c33c235
jesusbotella <jesus.botella@gmail.com>
2018-06-19 16:44:11 +0200

    Return [] if there are no organization notifications in /api/v3/me

--- app/controllers/carto/api/users_controller.rb
+++ app/controllers/carto/api/users_controller.rb
@@ -37,14 +37,14 @@ module Carto
 
         cant_be_deleted_reason = carto_viewer.try(:cant_be_deleted_reason)
         can_be_deleted = carto_viewer.present? ? cant_be_deleted_reason.nil? : nil
-        organization_notifications = carto_viewer ? organization_notifications(carto_viewer) : {}
+        viewer_organization_notifications = carto_viewer ? organization_notifications(carto_viewer) : []
 
         render json: {
           user_data: carto_viewer.present? ? Carto::Api::UserPresenter.new(carto_viewer).data : nil,
           default_fallback_basemap: carto_viewer.try(:default_basemap),
           config: frontend_config_hash(current_viewer),
           dashboard_notifications: carto_viewer.try(:notifications_for_category, :dashboard),
-          organization_notifications: organization_notifications,
+          organization_notifications: viewer_organization_notifications,
           is_just_logged_in: carto_viewer.present? ? !!flash['logged'] : nil,
           is_first_time_viewing_dashboard: !carto_viewer.try(:dashboard_viewed_at),
           can_change_email: carto_viewer.try(:can_change_email?),

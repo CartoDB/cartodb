1a0b63f4df122cd1bc018ad3036aee8bfc0e1080
rubenmoya <ruben@carto.com>
2018-06-26 11:56:10 +0200

    Add spec for ._getOverlayIndex

--- test/spec/geo/gmaps/gmaps-cartodb-layer-group.spec.js
+++ test/spec/geo/gmaps/gmaps-cartodb-layer-group.spec.js
@@ -1,6 +1,9 @@
 /* global google */
 var GoogleCartoDBLayerGroupClass = require('../../../../src/geo/gmaps/gmaps-cartodb-layer-group-view');
 var cartoLayerGroupViewTests = require('../shared-tests-for-carto-layer-group');
+var CartoDBLayer = require('../../../../src/geo/map/cartodb-layer');
+var LayersCollection = require('../../../../src/geo/map/layers');
+var CartoDBLayerGroup = require('../../../../src/geo/cartodb-layer-group');
 
 describe('gmaps-cartodb-layer-group-view', function () {
   /**
@@ -37,5 +40,82 @@ describe('gmaps-cartodb-layer-group-view', function () {
     pixel: { x: 243, y: 274 }
   };
 
+  // -- Shared tests for carto layer group
   cartoLayerGroupViewTests(createNativeMap, GoogleCartoDBLayerGroupClass, getTileUrl, event);
+
+  // -- Google maps specific tests
+  describe('GMapsCartoDBLayerGroupView', function () {
+    var nativeMap;
+    var container;
+    var cartoDbLayer0;
+    var cartoDbLayer1;
+    var layerGroupView;
+
+    var layerGroupModelMock;
+    var layersCollection;
+
+    var engineMock = {
+      on: jasmine.createSpy('on')
+    };
+
+    var mapModelMock = {
+      on: jasmine.createSpy('on'),
+      isFeatureInteractivityEnabled: jasmine.createSpy('isFeatureInteractivityEnabled').and.returnValue(false)
+    };
+
+    beforeEach(function () {
+      container = document.createElement('div');
+      nativeMap = createNativeMap(container);
+
+      cartoDbLayer0 = new CartoDBLayer({}, { engine: engineMock });
+      cartoDbLayer1 = new CartoDBLayer({}, { engine: engineMock });
+
+      layersCollection = new LayersCollection([cartoDbLayer0, cartoDbLayer1]);
+
+      layerGroupModelMock = new CartoDBLayerGroup({
+        urls: {
+          'subdomains': [0, 1, 2, 3],
+          'tiles': 'http://{s}.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/{layerIndexes}/{z}/{x}/{y}.png',
+          'grids': [
+            [
+              'http://0.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/0/{z}/{x}/{y}.grid.json',
+              'http://1.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/0/{z}/{x}/{y}.grid.json',
+              'http://2.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/0/{z}/{x}/{y}.grid.json',
+              'http://3.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/0/{z}/{x}/{y}.grid.json'
+            ],
+            [
+              'http://0.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/1/{z}/{x}/{y}.grid.json',
+              'http://1.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/1/{z}/{x}/{y}.grid.json',
+              'http://2.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/1/{z}/{x}/{y}.grid.json',
+              'http://3.ashbu.cartocdn.com/documentation/api/v1/map/0123456789/1/{z}/{x}/{y}.grid.json'
+            ]
+          ],
+          'attributes': [
+            'http://ashbu.cartocdn.com/documentation/api/v1/map/0123456789/0/attributes',
+            'http://ashbu.cartocdn.com/documentation/api/v1/map/0123456789/1/attributes'
+          ]
+        },
+        indexOfLayersInWindshaft: [1, 2]
+      }, {
+        layersCollection: layersCollection
+      });
+    });
+
+    describe('._getOverlayIndex', function () {
+      it('returns the index of the given layour group', function () {
+        layerGroupView = new GoogleCartoDBLayerGroupClass(layerGroupModelMock, {
+          nativeMap: nativeMap,
+          mapModel: mapModelMock
+        });
+
+        nativeMap.overlayMapTypes.setAt(2, layerGroupView);
+
+        expect(layerGroupView._getOverlayIndex()).toEqual(2);
+      });
+    });
+
+    afterEach(function () {
+      document.body.removeChild(container);
+    });
+  });
 });

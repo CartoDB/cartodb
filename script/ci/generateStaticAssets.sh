8d2d7660add7cc8f838b8cb8cf11a58adcd91cc8
Alberto Romeu <alrocar@cartodb.com>
2018-06-26 14:20:46 +0200

    install only dev dependencies

--- script/ci/generateStaticAssets.sh
+++ script/ci/generateStaticAssets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # static assets live at public/static/
-npm install webpack webpack-cli html-webpack-plugin && npm run build:static
+npm install --only=dev && \
+npm run carto-node && \
+npm run build:static

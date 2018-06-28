9f2e78006fde202e55adfb7cb62fed6c4e74bd3a
jesusbotella <jesus.botella@gmail.com>
2018-06-28 12:30:40 +0200

    Export Category and Range filter to filters

--- src/api/v4/filter/index.js
+++ src/api/v4/filter/index.js
@@ -1,13 +1,17 @@
-var BoundingBox = require('./bounding-box');
-var BoundingBoxLeaflet = require('./bounding-box-leaflet');
-var BoundingBoxGoogleMaps = require('./bounding-box-gmaps');
+const BoundingBox = require('./bounding-box');
+const BoundingBoxLeaflet = require('./bounding-box-leaflet');
+const BoundingBoxGoogleMaps = require('./bounding-box-gmaps');
+const Category = require('./sql/category');
+const Range = require('./sql/range');
 
 /**
  *  @namespace carto.filter
  *  @api
  */
 module.exports = {
-  BoundingBox: BoundingBox,
-  BoundingBoxLeaflet: BoundingBoxLeaflet,
-  BoundingBoxGoogleMaps: BoundingBoxGoogleMaps
+  BoundingBox,
+  BoundingBoxLeaflet,
+  BoundingBoxGoogleMaps,
+  Category,
+  Range
 };

91a770f36b4780957c268a0c903e6596530d8e61
Rafa de la Torre <rtorre@carto.com>
2018-07-03 16:40:58 +0200

    Delete legacy removal of `cartodb_id_#{oid}_seq`
    We no longer create a sequence with that convention and that was
    modified a long time ago.
    
    ```
    CREATE TABLE test();
    SELECT cdb_cartodbfytable('test');
    
    \d test
                                                   Table "public.test"
            Column        |          Type           | Collation | Nullable |                 Default
    ----------------------+-------------------------+-----------+----------+------------------------------------------
     cartodb_id           | bigint                  |           | not null | nextval('test_cartodb_id_seq'::regclass)
     the_geom             | geometry(Geometry,4326) |           |          |
     the_geom_webmercator | geometry(Geometry,3857) |           |          |
    Indexes:
        "test_pkey" PRIMARY KEY, btree (cartodb_id)
        "test_the_geom_idx" gist (the_geom)
        "test_the_geom_webmercator_idx" gist (the_geom_webmercator)
    Triggers:
        test_quota BEFORE INSERT OR UPDATE ON test FOR EACH STATEMENT EXECUTE PROCEDURE cdb_checkquota('0.1', '-1', 'public')
        test_quota_per_row BEFORE INSERT OR UPDATE ON test FOR EACH ROW EXECUTE PROCEDURE cdb_checkquota('0.001', '-1', 'public')
        track_updates AFTER INSERT OR DELETE OR UPDATE OR TRUNCATE ON test FOR EACH STATEMENT EXECUTE PROCEDURE cdb_tablemetadata_trigger()
        update_the_geom_webmercator_trigger BEFORE INSERT OR UPDATE OF the_geom ON test FOR EACH ROW EXECUTE PROCEDURE _cdb_update_the_geom_webmercator()
    
    \ds test_cartodb_id_seq
                     List of relations
     Schema |        Name         |   Type   |  Owner
    --------+---------------------+----------+----------
     public | test_cartodb_id_seq | sequence | postgres
    (1 row)
    ```
    
    and since the sequence is owned by the table, deleting it will delete the sequence (as inteded):
    ```
    DROP TABLE test;
    
    \ds test_cartodb_id_seq
    Did not find any relation named "test_cartodb_id_seq".
    ```

--- app/models/table.rb
+++ app/models/table.rb
@@ -458,7 +458,6 @@ class Table
       user_database.transaction do
         # Give up if it cannot get ExclusiveLocks for DDL operations in a reasonable time
         user_database.run(%{SET LOCAL lock_timeout = '1s'})
-        user_database.run("DROP SEQUENCE IF EXISTS cartodb_id_#{oid}_seq")
         Carto::OverviewsService.new(user_database).delete_overviews qualified_table_name
         user_database.run(%{DROP TABLE IF EXISTS #{qualified_table_name}})
       end

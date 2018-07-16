78909feda6f6475f1303f51e02a761342b9ce03b
Javier Torres <jtorres@carto.com>
2018-07-16 14:45:29 +0200

    Set eager load config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,8 @@ CartoDB::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  config.eager_load = true
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true

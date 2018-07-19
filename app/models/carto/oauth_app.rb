e1514c6b20700ff9cd4722f2366248c82174f77a
Javier Torres <jtorres@carto.com>
2018-07-19 16:01:36 +0200

    Callback URL validation

--- app/models/carto/oauth_app.rb
+++ app/models/carto/oauth_app.rb
@@ -9,6 +9,7 @@ module Carto
     validates :client_id, presence: true
     validates :client_secret, presence: true
     validates :redirect_urls, presence: true
+    validate :validate_urls
 
     before_validation :generate_keys
 
@@ -17,6 +18,18 @@ module Carto
     def generate_keys
       self.client_id ||= SecureRandom.urlsafe_base64(9)
       self.client_secret ||= SecureRandom.urlsafe_base64(18)
+    end
+
+    def validate_urls
+      redirect_urls.each { |url| validate_url(url) }
+    end
+
+    def validate_url(url)
+      uri = URI.parse(url)
+      return errors.add(:callback_urls, "#{url} must be absolute") unless uri.absolute?
+      return errors.add(:callback_urls, "#{url} must be https") unless uri.scheme == 'https'
+    rescue
+      errors.add(:callback_urls, "#{url} must be valid")
     end
   end
 end

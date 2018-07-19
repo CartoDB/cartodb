9633b364aba4c5a6dff4a80c28d3db2f061cd5d4
Javier Torres <jtorres@carto.com>
2018-07-19 15:00:59 +0200

    Create oauth app users

---
+++ db/migrate/20180719125312_create_oauth_apps_users.rb
@@ -0,0 +1,20 @@
+require 'carto/db/migration_helper'
+
+include Carto::Db::MigrationHelper
+
+migration(
+  Proc.new do
+    create_table :oauth_apps_users do
+      foreign_key :oauth_app_id, :oauth_apps, type: :uuid, null: false, index: true
+      foreign_key :user_id, :users, type: :uuid, null: false, index: true
+      foreign_key :api_key_id, :api_keys, type: :uuid, null: false, index: true
+      column      :scopes, 'text[]', null: false, default: "'{}'".lit
+      String      :code, null: true
+      DateTime    :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime    :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end,
+  Proc.new do
+    drop_table :oauth_apps_users
+  end
+)

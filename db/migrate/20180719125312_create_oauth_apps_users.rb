f12235854a277120adc5ee952cb1055c217756a0
Javier Torres <jtorres@carto.com>
2018-07-24 20:00:04 +0200

    Fix migration

--- db/migrate/20180719125312_create_oauth_apps_users.rb
+++ db/migrate/20180719125312_create_oauth_apps_users.rb
@@ -8,9 +8,9 @@ migration(
       Uuid        :id, primary_key: true, default: 'uuid_generate_v4()'.lit
       foreign_key :oauth_app_id, :oauth_apps, type: :uuid, null: false, index: true, on_delete: :cascade
       foreign_key :user_id, :users, type: :uuid, null: false, index: true, on_delete: :cascade
-      foreign_key :api_key_id, :api_keys, type: :uuid, null: false, index: true, on_delete: :restrict
+      foreign_key :api_key_id, :api_keys, type: :uuid, null: true, index: true, on_delete: :restrict
       column      :scopes, 'text[]', null: false, default: "'{}'".lit
-      String      :code, null: true, index: { where: 'NOT NULL' }
+      String      :code, null: true, index: { where: 'code IS NOT NULL' }
       DateTime    :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
       DateTime    :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
     end

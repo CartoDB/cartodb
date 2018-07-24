94945fb47d14ae7b4653cbe50203dbb76952401d
Javier Torres <jtorres@carto.com>
2018-07-24 19:28:37 +0200

    Oauth app user no longer has redirect uri

--- app/models/carto/oauth_app_user.rb
+++ app/models/carto/oauth_app_user.rb
@@ -11,21 +11,12 @@ module Carto
     validates :user, presence: true
     validates :oauth_app, presence: true
     validate :code_or_api_key_present
-    validate :redirect_url_matches
 
     private
 
     def code_or_api_key_present
-      # You can have either code + redirect_url if the token is not exchanged yet, or else, an api_key
-      errors.add(:code, 'must be present if redirect_url is') if code.blank? && redirect_url.present?
-      errors.add(:redirect_url, 'must be present if code is') if code.present? && redirect_url.blank?
+      # You can have either code if the token is not exchanged yet, or an api_key
       errors.add(:api_key, 'must be present if code is missing') if code.blank? && api_key.blank?
-    end
-
-    def redirect_url_matches
-      if oauth_app && redirect_url
-        errors.add(:redirect_url, 'is not registered') unless oauth_app.redirect_urls.include?(redirect_url)
-      end
     end
   end
 end

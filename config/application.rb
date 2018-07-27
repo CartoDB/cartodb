36e7e3335c6d4c2f318f746a6c03b51bed2ef5fe
oskardolch <pavel@pamsoft.cz>
2018-07-27 17:20:58 +0200

    Adding support for backend i18n

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module CartoDB
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.available_locales = [:en]
+    I18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)

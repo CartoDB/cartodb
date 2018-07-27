4dd62501c62364972d14561dcf29f50e4e19e709
Javier Torres <jtorres@carto.com>
2018-07-27 18:09:40 +0200

    Split authorizations to a different table, so we can keep track of authorized apps without tokens

---
+++ db/migrate/20180727174523_create_oauth_authorizations.rb
@@ -0,0 +1,21 @@
+require 'carto/db/migration_helper'
+
+include Carto::Db::MigrationHelper
+
+migration(
+  Proc.new do
+    create_table :oauth_authorizations do
+      Uuid        :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+      foreign_key :oauth_app_user_id, :oauth_apps_users, type: :uuid, null: false, index: true, on_delete: :cascade
+      foreign_key :api_key_id, :api_keys, type: :uuid, null: true, on_delete: :restrict,
+                                          index: { where: 'api_key_id IS NOT NULL', unique: true }
+      column      :scopes, 'text[]', null: false, default: "'{}'".lit
+      String      :code, null: true, index: { where: 'code IS NOT NULL' }
+      DateTime    :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime    :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end,
+  Proc.new do
+    drop_table :oauth_authorizations
+  end
+)

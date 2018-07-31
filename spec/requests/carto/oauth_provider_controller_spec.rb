05fbb78dc9e594da9e4388e895c36ea3d631cbb8
Javier Torres <jtorres@carto.com>
2018-07-31 12:40:43 +0200

    Tests for authorize endpoint

--- spec/requests/carto/oauth_provider_controller_spec.rb
+++ spec/requests/carto/oauth_provider_controller_spec.rb
@@ -26,13 +26,64 @@ describe Carto::OauthProviderController do
     host!("#{@user.username}.localhost.lan")
   end
 
+  let(:valid_payload) do
+    {
+      client_id: @oauth_app.client_id,
+      response_type: 'code',
+      state: 'random_state_thingy',
+      accept: true
+    }
+  end
+
+  shared_examples_for 'authorization parameter validation' do
+    it 'returns a 404 error if application cannot be found' do
+      request_endpoint(valid_payload.merge(client_id: 'e'))
+
+      expect(response.status).to(eq(404))
+    end
+
+    it 'redirects with an error if invalid response_type' do
+      request_endpoint(valid_payload.merge(response_type: 'err'))
+
+      expect(response.status).to(eq(302))
+      expect(response.location).to(start_with(@oauth_app.redirect_uri))
+      expect(Addressable::URI.parse(response.location).query_values['error']).to(eq('unsupported_response_type'))
+    end
+
+    it 'redirects with an error if missing state' do
+      request_endpoint(valid_payload.merge(state: ''))
+
+      expect(response.status).to(eq(302))
+      expect(response.location).to(start_with(@oauth_app.redirect_uri))
+      qs = Addressable::URI.parse(response.location).query_values
+      expect(qs['error']).to(eq('invalid_request'))
+      expect(qs['error_description']).to(eq('state is mandatory'))
+    end
+
+    it 'redirects with an error if requesting unknown scopes' do
+      request_endpoint(valid_payload.merge(scope: 'invalid wadus'))
+
+      expect(response.status).to(eq(302))
+      expect(response.location).to(start_with(@oauth_app.redirect_uri))
+      expect(Addressable::URI.parse(response.location).query_values['error']).to(eq('invalid_scope'))
+    end
+
+    it 'redirects with an error if requesting with an invalid redirect_uri' do
+      request_endpoint(valid_payload.merge(redirect_uri: 'invalid'))
+
+      expect(response.status).to(eq(302))
+      expect(response.location).to(start_with(@oauth_app.redirect_uri))
+      qs = Addressable::URI.parse(response.location).query_values
+      expect(qs['error']).to(eq('invalid_request'))
+      expect(qs['error_description']).to(eq('The redirect_uri is not authorized for this application'))
+    end
+  end
+
   describe '#consent' do
-    let(:valid_payload) do
-      {
-        client_id: @oauth_app.client_id,
-        response_type: 'code',
-        state: 'random_state_thingy'
-      }
+    it_behaves_like 'authorization parameter validation' do
+      def request_endpoint(parameters)
+        get oauth_provider_authorize_url(parameters)
+      end
     end
 
     it 'logged out, redirects to login' do
@@ -43,54 +94,40 @@ describe Carto::OauthProviderController do
       expect(response.location).to(include('/login'))
     end
 
-    it 'shows the consent form' do
+    it 'with valid payload, shows the consent form' do
       get oauth_provider_authorize_url(valid_payload)
 
       expect(response.status).to(eq(200))
       expect(response.body).to(include(valid_payload[:client_id]))
       expect(response.body).to(include(valid_payload[:state]))
     end
+  end
 
-    it 'returns a 404 error if application cannot be found' do
-      get oauth_provider_authorize_url(valid_payload.merge(client_id: 'e'))
-
-      expect(response.status).to(eq(404))
+  describe '#authorize' do
+    it_behaves_like 'authorization parameter validation' do
+      def request_endpoint(parameters)
+        post oauth_provider_authorize_url(parameters)
+      end
     end
 
-    it 'redirects with an error if invalid response_type' do
-      get oauth_provider_authorize_url(valid_payload.merge(response_type: 'err'))
+    it 'logged out, redirects to login' do
+      logout
+      post oauth_provider_authorize_url(valid_payload)
 
       expect(response.status).to(eq(302))
-      expect(response.location).to(start_with(@oauth_app.redirect_uri))
-      expect(Addressable::URI.parse(response.location).query_values['error']).to(eq('unsupported_response_type'))
+      expect(response.location).to(include('/login'))
     end
 
-    it 'redirects with an error if missing state' do
-      get oauth_provider_authorize_url(valid_payload.merge(state: ''))
+    it 'with valid payload, creates an authorization and redirects back to the application with a code' do
+      post oauth_provider_authorize_url(valid_payload)
+
+      authorization = @oauth_app.oauth_app_users.find_by_user_id!(@user.id).oauth_authorizations.first
+      expect(authorization).to(be)
+      expect(authorization.code).to(be_present)
+      expect(authorization.api_key).not_to(be)
 
       expect(response.status).to(eq(302))
-      expect(response.location).to(start_with(@oauth_app.redirect_uri))
-      qs = Addressable::URI.parse(response.location).query_values
-      expect(qs['error']).to(eq('invalid_request'))
-      expect(qs['error_description']).to(eq('state is mandatory'))
-    end
-
-    it 'redirects with an error if requesting unknown scopes' do
-      get oauth_provider_authorize_url(valid_payload.merge(scope: 'invalid wadus'))
-
-      expect(response.status).to(eq(302))
-      expect(response.location).to(start_with(@oauth_app.redirect_uri))
-      expect(Addressable::URI.parse(response.location).query_values['error']).to(eq('invalid_scope'))
-    end
-
-    it 'redirects with an error if requesting with an invalid redirect_uri' do
-      get oauth_provider_authorize_url(valid_payload.merge(redirect_uri: 'invalid'))
-
-      expect(response.status).to(eq(302))
-      expect(response.location).to(start_with(@oauth_app.redirect_uri))
-      qs = Addressable::URI.parse(response.location).query_values
-      expect(qs['error']).to(eq('invalid_request'))
-      expect(qs['error_description']).to(eq('The redirect_uri is not authorized for this application'))
+      expect(Addressable::URI.parse(response.location).query_values['code']).to(eq(authorization.code))
     end
   end
 end

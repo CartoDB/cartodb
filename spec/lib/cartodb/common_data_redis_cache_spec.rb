182940d8e77246e2cce593fb6781e635d8cbd163
Mario de Frutos <ethervoid@gmail.com>
2018-08-01 16:05:04 +0200

    Test outside helpers folder

--- spec/lib/cartodb/common_data_redis_cache_spec.rb
+++ spec/lib/cartodb/common_data_redis_cache_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../../app/helpers/common_data_redis_cache'
-require_relative '../spec_helper_min'
+require_relative '../../../app/helpers/common_data_redis_cache'
+require_relative '../../spec_helper_min'
 require 'mock_redis'
 
 describe CommonDataRedisCache do

260006ba05d90d18979742cfd0eceec79ca13f03
Mario de Frutos <ethervoid@gmail.com>
2018-08-01 12:20:27 +0200

    Fix test

--- spec/helpers/common_data_redis_cache_spec.rb
+++ spec/helpers/common_data_redis_cache_spec.rb
@@ -1,15 +1,9 @@
-require_relative '../simplecov_helper'
-require_relative '../rspec_configuration'
 require_relative '../../app/helpers/common_data_redis_cache'
-require 'active_support/dependencies/autoload'
-require 'active_support/core_ext/numeric'
+require_relative '../spec_helper_min'
 require 'mock_redis'
 
 describe CommonDataRedisCache do
   it 'handles encoding issues' do
-    # visualizations_api_url = "https://common-data.carto.com/api/v1/viz?privacy=public&type=table"
-    # cd = CommonData.new(visualizations_api_url)
-    # cd.datasets
     string_with_wrong_encoding = "\xc2\xa0".force_encoding('ascii-8bit')
     expected_result = { headers: {}, body: string_with_wrong_encoding }
     @mock_redis = MockRedis.new

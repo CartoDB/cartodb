0661c95bf3c73b2c7a9acb2232efcc0f465f7bbe
Javier Torres <jtorres@carto.com>
2018-08-21 17:38:35 +0200

    Add restricted flag to support apps without restrictions

--- db/migrate/20180821144500_create_oauth_app_organizations.rb
+++ db/migrate/20180821144500_create_oauth_app_organizations.rb
@@ -12,8 +12,12 @@ migration(
       DateTime    :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
       DateTime    :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
     end
+
+    add_column :oauth_apps, :restricted, :boolean, null: false, default: false
   end,
   Proc.new do
+    drop_column :oauth_apps, :restricted
+
     drop_table :oauth_app_organizations
   end
 )

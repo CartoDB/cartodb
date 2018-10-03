906c786be40b8196c746fc86fbcec7554597aafc
Javier Torres <jtorres@carto.com>
2018-10-03 18:21:38 +0200

    level not supported until ruby 2.4

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,7 +25,7 @@ CartoDB::Application.configure do
   # Note that we pass the desired log level to the logger's constructor;
   # assigning to `config.log_level` would have no effect here, since we have set the logger explicitly.
   config.logger = ActiveSupport::TaggedLogging.new(
-    ActiveSupport::Logger.new(Carto::Conf.new.log_file_path('staging.log'), level: Logger::INFO)
+    ActiveSupport::Logger.new(Carto::Conf.new.log_file_path('staging.log'))
   )
 
   config.log_level = :info

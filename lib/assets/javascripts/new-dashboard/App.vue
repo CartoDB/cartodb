bc67170dbfad57aac3f76eff1b06103073700e01
jesusbotella <jesus.botella@gmail.com>
2018-10-17 12:58:56 +0200

    Resolve bad import issues. Remove src/ folder.

--- lib/assets/javascripts/new-dashboard/App.vue
+++ lib/assets/javascripts/new-dashboard/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -13,11 +12,11 @@ export default {
 
 <style>
 #app {
+  margin-top: 60px;
+  color: #2C3E50;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

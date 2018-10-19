c673a89826395f4a1faaf045a8c49b13300b0665
Gonzalo Riestra <gonzalor@cartodb.com>
2018-10-19 14:56:48 +0200

    use timeout in AR

--- spec/models/carto/user_service_spec.rb
+++ spec/models/carto/user_service_spec.rb
@@ -16,101 +16,118 @@ describe Carto::UserService do
     @user.destroy
   end
 
-  it "Tests in_database() settimeout option" do
-    custom_timeout = 123456
-    expected_returned_custom_timeout = { statement_timeout: "#{custom_timeout}ms" }
+  describe "#in_database" do
+    it "initializes the connection with the expected options" do
+      carto_user = Carto::User.find(@user.id)
+      default_opts = {
+        username: carto_user.database_username,
+        password: carto_user.database_password,
+        user_schema: carto_user.database_schema,
+        connect_timeout: -1
+      }
+      expected_params = [carto_user.database_host, carto_user.database_name, default_opts]
 
-    @returned_timeout = nil
-    @default_timeout = nil
-    @returned_timeout_new = nil
-    @default_timeout_new = nil
+      Carto::Db::Connection.expects(:connect).with(*expected_params)
 
-    @user.in_database do |db|
-      @default_timeout = db[%{SHOW statement_timeout}].first
+      carto_user.in_database
     end
 
-    @user.in_database(statement_timeout: custom_timeout) do |db|
-      @returned_timeout = db[%{SHOW statement_timeout}].first
+    it "sets statement timeout option" do
+      custom_timeout = 123456
+      expected_returned_custom_timeout = { statement_timeout: "#{custom_timeout}ms" }
+
+      @returned_timeout = nil
+      @default_timeout = nil
+      @returned_timeout_new = nil
+      @default_timeout_new = nil
+
+      @user.in_database do |db|
+        @default_timeout = db[%{SHOW statement_timeout}].first
+      end
+
+      @user.in_database(statement_timeout: custom_timeout) do |db|
+        @returned_timeout = db[%{SHOW statement_timeout}].first
+      end
+
+      @returned_timeout.should eq expected_returned_custom_timeout
+      @default_timeout.should_not eq @returned_timeout
+
+      @user.in_database do |db|
+        @default_timeout.should eq db[%{SHOW statement_timeout}].first
+      end
+
+      # Now test with CARTO user
+      carto_user = Carto::User.find(@user.id)
+
+      carto_user.in_database do |db|
+        @default_timeout_new = db.execute(%{SHOW statement_timeout}).first
+      end
+
+      carto_user.in_database(statement_timeout: custom_timeout) do |db|
+        @returned_timeout_new = db.execute(%{SHOW statement_timeout}).first
+      end
+
+      @returned_timeout_new.symbolize_keys!
+      @default_timeout_new .symbolize_keys!
+
+      @returned_timeout_new.should eq expected_returned_custom_timeout
+      @default_timeout_new.should_not eq @returned_timeout_new
+
+      carto_user.in_database do |db|
+        @default_timeout_new.should eq db.execute(%{SHOW statement_timeout}).first.symbolize_keys
+      end
+
+      @default_timeout_new .symbolize_keys!
+
+      @returned_timeout_new.should eq @returned_timeout
+      @default_timeout_new.should eq @default_timeout
     end
 
-    @returned_timeout.should eq expected_returned_custom_timeout
-    @default_timeout.should_not eq @returned_timeout
+    it "sets search_path correctly" do
+      expected_returned_normal_search_path = { search_path: "#{@user.database_schema}, cartodb, cdb_dataservices_client, public" }
 
-    @user.in_database do |db|
-      @default_timeout.should eq db[%{SHOW statement_timeout}].first
+      @normal_search_path = nil
+      @normal_search_path_new = nil
+      @user.in_database do |db|
+        @normal_search_path = db[%{SHOW search_path}].first
+      end
+      @normal_search_path.should eq expected_returned_normal_search_path
+
+      carto_user = Carto::User.find(@user.id)
+
+      carto_user.in_database do |db|
+        @normal_search_path_new = db.execute(%{SHOW search_path}).first
+      end
+      @normal_search_path_new.symbolize_keys!
+      @normal_search_path_new.should eq expected_returned_normal_search_path
+
+      @normal_search_path_new.should eq @normal_search_path
     end
 
-    # Now test with CARTO user
-    carto_user = Carto::User.find(@user.id)
-
-    carto_user.in_database do |db|
-      @default_timeout_new = db.execute(%{SHOW statement_timeout}).first
-    end
-
-    carto_user.in_database(statement_timeout: custom_timeout) do |db|
-      @returned_timeout_new = db.execute(%{SHOW statement_timeout}).first
-    end
-
-    @returned_timeout_new.symbolize_keys!
-    @default_timeout_new .symbolize_keys!
-
-    @returned_timeout_new.should eq expected_returned_custom_timeout
-    @default_timeout_new.should_not eq @returned_timeout_new
-
-    carto_user.in_database do |db|
-      @default_timeout_new.should eq db.execute(%{SHOW statement_timeout}).first.symbolize_keys
-    end
-
-    @default_timeout_new .symbolize_keys!
-
-    @returned_timeout_new.should eq @returned_timeout
-    @default_timeout_new.should eq @default_timeout
-  end
-
-  it "Tests search_path correctly set" do
-    expected_returned_normal_search_path = { search_path: "#{@user.database_schema}, cartodb, cdb_dataservices_client, public" }
-
-    @normal_search_path = nil
-    @normal_search_path_new = nil
-    @user.in_database do |db|
-      @normal_search_path = db[%{SHOW search_path}].first
-    end
-    @normal_search_path.should eq expected_returned_normal_search_path
-
-    carto_user = Carto::User.find(@user.id)
-
-    carto_user.in_database do |db|
-      @normal_search_path_new = db.execute(%{SHOW search_path}).first
-    end
-    @normal_search_path_new.symbolize_keys!
-    @normal_search_path_new.should eq expected_returned_normal_search_path
-
-    @normal_search_path_new.should eq @normal_search_path
-  end
-
-  it "tests in_database() with superadmin only operations" do
-    carto_user = Carto::User.find(@user.id)
-    expect {
-      @user.in_database do |conn|
+    it "only allows superadmin operations to the expected roles" do
+      carto_user = Carto::User.find(@user.id)
+      expect {
+        @user.in_database do |conn|
+          conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
+        end
+      }.to raise_exception(Sequel::DatabaseError, /permission denied to set parameter "log_statement_stats"/)
+      expect {
+        carto_user.in_database do |conn|
+          conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
+        end
+      }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied to set parameter "log_statement_stats"/)
+      @user.in_database(as: :superuser) do |conn|
         conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
       end
-    }.to raise_exception(Sequel::DatabaseError, /permission denied to set parameter "log_statement_stats"/)
-    expect {
-      carto_user.in_database do |conn|
+      carto_user.in_database(as: :superuser) do |conn|
         conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
       end
-    }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied to set parameter "log_statement_stats"/)
-    @user.in_database(as: :superuser) do |conn|
-      conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
-    end
-    carto_user.in_database(as: :superuser) do |conn|
-      conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
-    end
-    @user.in_database(as: :cluster_admin) do |conn|
-      conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
-    end
-    carto_user.in_database(as: :cluster_admin) do |conn|
-      conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
+      @user.in_database(as: :cluster_admin) do |conn|
+        conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
+      end
+      carto_user.in_database(as: :cluster_admin) do |conn|
+        conn.execute(%{SELECT set_config('log_statement_stats', 'off', false)})
+      end
     end
   end
 end

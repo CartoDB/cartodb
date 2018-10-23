b21de68b69e52ad93b793209fa3e03078e2388e8
csubira <cillas@cartodb.com>
2018-10-23 16:08:06 +0200

    Add featured favorited maps and page num draft to maps page

--- lib/assets/javascripts/new-dashboard/pages/Maps.vue
+++ lib/assets/javascripts/new-dashboard/pages/Maps.vue
@@ -1,26 +1,48 @@
 <template>
   <section>
     <h1>Maps Page</h1>
+    <h2>Favorited maps</h2>
+    <p v-if="isFetchingFeaturedFavoritedMaps">Loading favorited maps</p>
+    <ul v-else>
+      <li v-for="featuredFavoritedMap in featuredFavoritedMaps" v-bind:key="featuredFavoritedMap.id">
+        {{ featuredFavoritedMap.id }}
+      </li>
+    </ul>
+
+    <h2>All maps</h2>
     <p v-if="isFetchingMaps">Loading</p>
     <ul v-else>
       <li v-for="map in maps" v-bind:key="map.id">
         {{ map.id }}
       </li>
     </ul>
+
+    <button v-on:click="goToPage(2)">Page 2</button>
+    <button v-on:click="goToPage(3)">Page 3</button>
   </section>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 
 export default {
   name: 'MapsPage',
   computed: {
-    isFetchingMaps() {
+    isFetchingMaps () {
       return this.$store.state.maps.isFetching;
     },
-    maps() {
+    maps () {
       return this.$store.state.maps.list;
+    },
+    isFetchingFeaturedFavoritedMaps () {
+      return this.$store.state.maps.featuredFavoritedMaps.isFetching;
+    },
+    featuredFavoritedMaps () {
+      return this.$store.state.maps.featuredFavoritedMaps.list;
+    }
+  },
+  methods: {
+    goToPage (page) {
+      this.$store.dispatch('maps/goToPage', page);
     }
   }
 };

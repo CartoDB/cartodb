a5609e14cc651e7e2cafb090211de8f788c1bc15
jesusbotella <jesus.botella@gmail.com>
2018-10-23 09:48:14 +0200

    Retrieve maps from API

--- lib/assets/javascripts/new-dashboard/pages/Maps.vue
+++ lib/assets/javascripts/new-dashboard/pages/Maps.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1>Maps Page</h1>
+  <section>
+    <h1>Maps Page</h1>
+    <p v-if="isFetchingMaps">Loading</p>
+    <ul v-else>
+      <li v-for="map in maps" v-bind:key="map.id">
+        {{ map.id }}
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  name: 'MapsPage'
+  name: 'MapsPage',
+  computed: {
+    isFetchingMaps() {
+      return this.$store.state.maps.isFetching;
+    },
+    maps() {
+      return this.$store.state.maps.list;
+    }
+  }
 };
 </script>
 

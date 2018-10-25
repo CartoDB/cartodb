46ab2cdb321cdfe041ab1eb9211c2517a6f59201
csubira <cillas@cartodb.com>
2018-10-25 12:58:25 +0200

    Add tests to maps store

--- lib/assets/javascripts/new-dashboard/pages/Maps.vue
+++ lib/assets/javascripts/new-dashboard/pages/Maps.vue
@@ -1,24 +1,43 @@
 <template>
-  <section>
-    <h1>Maps Page</h1>
-    <h2>Favorited maps</h2>
-    <p v-if="isFetchingFeaturedFavoritedMaps">Loading favorited maps</p>
-    <ul v-else>
-      <li v-for="featuredFavoritedMap in featuredFavoritedMaps" v-bind:key="featuredFavoritedMap.id">
-        {{ featuredFavoritedMap.id }}
+  <section class="temp">
+    <h1 class="title--h1">Maps Page</h1>
+    <h2 class="title--h2">Favorited maps</h2>
+    <span class="loading" v-if="isFetchingFeaturedFavoritedMaps">Loading favorited maps</span>
+    <ul class="maplist" v-else>
+      <li class="maplist-element" v-for="featuredFavoritedMap in featuredFavoritedMaps" v-bind:key="featuredFavoritedMap.id">
+        <span class="maplist-detail">Title: {{ featuredFavoritedMap.name }}</span>
+        <span class="maplist-detail">Id: {{ featuredFavoritedMap.id }}</span>
+        <span class="maplist-detail">Locked: {{ featuredFavoritedMap.locked }}</span>
+        <span class="maplist-detail">Liked: {{ featuredFavoritedMap.liked }}</span>
       </li>
     </ul>
 
-    <h2>All maps</h2>
-    <p v-if="isFetchingMaps">Loading</p>
-    <ul v-else>
-      <li v-for="map in maps" v-bind:key="map.id">
-        {{ map.id }}
+    <h2 class="title--h2">All maps</h2>
+    <div>
+      <button class="button" v-on:click="filterLockedMaps()">LOCKED MAPS</button>
+      <button class="button" v-on:click="filterSharedMaps()">SHARED MAPS</button>
+      <button class="button" v-on:click="resetFilters()">RESET</button>
+    </div>
+    <span class="loading" v-if="isFetchingMaps">Loading</span>
+    <ul class="maplist" v-else>
+      <li class="maplist-element" v-for="map in maps" v-bind:key="map.id">
+        <span class="maplist-detail" >Title: {{ map.name }}</span>
+        <span class="maplist-detail">Id: {{ map.id }}</span>
+        <span class="maplist-detail" >Locked: {{ map.locked }}</span>
+        <span class="maplist-detail">Liked: {{ map.liked }}</span>
       </li>
     </ul>
 
-    <button v-on:click="goToPage(2)">Page 2</button>
-    <button v-on:click="goToPage(3)">Page 3</button>
+    <div>
+      <span>Current Page: {{ currentPage }}</span>
+      <span>Num Pages: {{ numPages }}</span>
+    </div>
+    <ul class="pageslist">
+      <li class="pageslist-element" v-for="page in numPages" v-bind:key="page">
+        <button class="button button--page" v-on:click="goToPage(page)">Page {{ page }}</button>
+      </li>
+    </ul>
+
   </section>
 </template>
 
@@ -33,6 +52,12 @@ export default {
     maps () {
       return this.$store.state.maps.list;
     },
+    currentPage () {
+      return this.$store.state.maps.page;
+    },
+    numPages () {
+      return this.$store.state.maps.numPages;
+    },
     isFetchingFeaturedFavoritedMaps () {
       return this.$store.state.maps.featuredFavoritedMaps.isFetching;
     },
@@ -43,6 +68,15 @@ export default {
   methods: {
     goToPage (page) {
       this.$store.dispatch('maps/goToPage', page);
+    },
+    filterLockedMaps () {
+      this.$store.dispatch('maps/filterLockedMaps');
+    },
+    filterSharedMaps () {
+      this.$store.dispatch('maps/filterSharedMaps');
+    },
+    resetFilters () {
+      this.$store.dispatch('maps/resetFilters');
     }
   }
 };
@@ -50,4 +84,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import 'stylesheets/new-dashboard/variables';
+.temp {
+  .title--h1 {
+    font-size: 32px;
+    margin-bottom: 16px;
+  }
+  .title--h2 {
+    font-size: 24px;
+    margin-bottom: 12px;
+  }
+  .maplist, .pageslist {
+    display: flex;
+  }
+  .maplist-element {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    border: 1px solid $light-grey;
+    box-sizing: border-box;
+  }
+  .maplist-detail {
+    margin-bottom: 8px;
+  }
+  .pageslist-element {
+    padding: 16px;
+  }
+  .button {
+    padding: 8px 20px;
+    border-radius: 4px;
+    margin: 8px;
+    cursor: pointer;
+    box-sizing: border-box;
+    background-color: $primaryColor;
+    color: $white;
+  }
+  .button--page {
+    background-color: $light-grey;
+  }
+}
 </style>

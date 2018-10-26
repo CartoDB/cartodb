8ed0c49bc4e19e7d420f7875b30625ac404de540
Gonzalo Riestra <gonzalor@cartodb.com>
2018-10-26 11:47:28 +0200

    add new specs to makefile and use spec_helper_min

--- spec/models/carto/feature_flag_spec.rb
+++ spec/models/carto/feature_flag_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require_relative '../../spec_helper'
+require_relative '../../spec_helper_min'
 
 describe Carto::FeatureFlag do
 
--- spec/requests/password_resets_controller_spec.rb
+++ spec/requests/password_resets_controller_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../spec_helper'
+require_relative '../spec_helper_min'
 
 describe PasswordResetsController do
 

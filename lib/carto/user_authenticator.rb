28aa5caf5b5e96a1f9e7cb977fd52d7fbfdba186
Alberto Romeu <alrocar@cartodb.com>
2018-10-26 19:02:46 +0200

    change method name

--- lib/carto/user_authenticator.rb
+++ lib/carto/user_authenticator.rb
@@ -3,7 +3,7 @@ module Carto
     def authenticate(email, password)
       sanitized_input = email.strip.downcase
       if candidate = ::User.filter("email = ? OR username = ?", sanitized_input, sanitized_input).first
-        check_password_locked(candidate)
+        login_attempt(candidate)
         return candidate if valid_password?(candidate, password)
       end
     end
@@ -12,7 +12,7 @@ module Carto
       candidate.crypted_password == password_digest(password, candidate.salt)
     end
 
-    def check_password_locked(user)
+    def login_attempt(user)
       retry_after = user.password_login_attempt
       if retry_after != ::User::LOGIN_NOT_RATE_LIMITED
         throw(:warden, action: :password_locked, retry_after: retry_after)

ff7db7230d5c0c8587e6ea047a2a654b18515df7
csubira <cillas@cartodb.com>
2018-11-05 18:51:52 +0100

    Setup datasets store

--- lib/assets/javascripts/new-dashboard/pages/Data.vue
+++ lib/assets/javascripts/new-dashboard/pages/Data.vue
@@ -1,10 +1,91 @@
 <template>
-  <h1>Data Page</h1>
+  <section class="section">
+    <div class="container grid">
+     <div class="grid-cell grid-cell--col12">
+      <SectionTitle title='Your Data' description="This is a description test">
+        <template slot="icon">
+          <img src="../assets/icons/section-title/data.svg" />
+        </template>
+        <template slot="actionButton">
+          <CreateButton visualizationType="datasets">New Dataset</CreateButton>
+        </template>
+      </SectionTitle>
+     </div>
+
+    <ul v-if="isFetchingDatasets">
+      <li v-for="n in 12" :key="n">
+        Loading
+      </li>
+    </ul>
+
+    <ul v-if="!isFetchingDatasets">
+      <li v-for="dataset in datasets" :key="dataset.id">
+        <span>{{dataset.name}}</span>
+        <span>FAV: {{ dataset.liked }} - </span>
+        <span>Last Modified: {{dataset.updated_at }} - </span>
+        <span>Rows: {{dataset.table.row_count }} - </span>
+        <span>Size: {{ dataset.table.size }} - </span>
+        <span>Privacy: {{dataset.table.privacy }} - </span>
+        <span>Geometry Types: {{ dataset.table.geometry_types }}</span>
+      </li>
+    </ul>
+    </div>
+
+    <Pagination v-if="!isFetchingDatasets" :page=currentPage :numPages=numPages @pageChange="goToPage"></Pagination>
+  </section>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import Pagination from 'new-dashboard/components/Pagination';
+import SectionTitle from 'new-dashboard/components/SectionTitle';
+import CreateButton from 'new-dashboard/components/CreateButton.vue';
+import { isAllowed } from '../store/datasets/filters';
+
 export default {
-  name: 'DataPage'
+  name: 'DataPage',
+  components: {
+    SectionTitle,
+    CreateButton,
+    Pagination
+  },
+  beforeRouteUpdate (to, from, next) {
+    const urlOptions = { ...to.params, ...to.query };
+
+    if (urlOptions.filter && !isAllowed(urlOptions.filter)) {
+      return next({ name: 'datasets' });
+    }
+
+    this.$store.dispatch('datasets/setURLOptions', urlOptions);
+    next();
+  },
+  computed: {
+    ...mapState({
+      numPages: state => state.datasets.numPages,
+      currentPage: state => state.datasets.page,
+      appliedFilter: state => state.datasets.filterType,
+      appliedOrder: state => state.datasets.order,
+      datasets: state => state.datasets.list,
+      datasetsMetadata: state => state.datasets.metadata,
+      isFetchingDatasets: state => state.datasets.isFetching
+    }),
+    pageTitle () {
+      return this.$t(`DataPage.header.title['${this.appliedFilter}']`);
+    }
+  },
+  methods: {
+    goToPage (page) {
+      window.scroll({ top: 0, left: 0 });
+      this.$router.push({
+        name: 'datasets',
+        params: this.$route.params,
+        query: {...this.$route.query, page}
+      });
+    },
+    applyFilter (filter) {
+      this.$router.push({ name: 'maps', params: { filter } });
+    }
+  }
 };
 </script>
 

d439702917a1a21e5bb562d0ac285404393e6bef
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-11-08 15:52:49 +0100

    Do not override schema variable

--- app/services/carto/table_and_friends.rb
+++ app/services/carto/table_and_friends.rb
@@ -9,7 +9,7 @@ module Carto
       block[schema, table_name, qualified_name]
       overviews_service = Carto::OverviewsService.new(db_connection)
       overviews_service.overview_tables(qualified_name).each do |overview_table|
-        table, schema = Table.table_and_schema(overview_table)
+        table, _ = Table.table_and_schema(overview_table)
         block[schema, table, qualified_table_name(schema, table)]
       end
       # TODO: should we apply also to raster overview tables?

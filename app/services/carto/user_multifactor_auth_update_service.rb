8d4af34d8a96d44c784a71325a3413af04e8b26a
Gonzalo Riestra <gonzalor@cartodb.com>
2018-11-12 16:28:48 +0100

    improve code style

--- app/services/carto/user_multifactor_auth_update_service.rb
+++ app/services/carto/user_multifactor_auth_update_service.rb
@@ -6,19 +6,21 @@ module Carto
     end
 
     def update(enabled:, type: Carto::UserMultifactorAuth::TYPE_TOTP)
-      if enabled
-        create_user_multifactor_auth(type)
-      else
-        Carto::UserMultifactorAuth.where(user_id: @user_id).each(&:destroy!)
-      end
+      enabled ? create_user_multifactor_auth(type) : destroy_user_multifactor_auth(type)
     end
 
     private
 
     def create_user_multifactor_auth(type)
-      unless Carto::UserMultifactorAuth.where(user_id: @user_id, type: type).present?
-        Carto::UserMultifactorAuth.create!(user_id: @user_id, type: type)
-      end
+      Carto::UserMultifactorAuth.create!(user_id: @user_id, type: type) unless user_multifactor_auth(type).present?
+    end
+
+    def destroy_user_multifactor_auth(type)
+      user_multifactor_auth(type).each(&:destroy!)
+    end
+
+    def user_multifactor_auth(type)
+      Carto::UserMultifactorAuth.where(user_id: @user_id, type: type)
     end
 
   end

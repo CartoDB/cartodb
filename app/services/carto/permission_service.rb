30d1b905054da8fbd2b6ab300efce6afd12f6170
Simon Martín <oleurud@gmail.com>
2018-11-19 21:28:34 +0100

    fix and refactoring prioritize perms

--- app/services/carto/permission_service.rb
+++ app/services/carto/permission_service.rb
@@ -1,5 +1,5 @@
 module Carto
-  module PermissionService
+  class PermissionService
     TYPE_USER  = 'user'.freeze
     TYPE_ORG   = 'org'.freeze
     TYPE_GROUP = 'group'.freeze
@@ -8,16 +8,37 @@ module Carto
       old_acl = hashing_acl(old_acl)
       new_acl = hashing_acl(new_acl)
 
-      diff_by_types = diff_by_types(old_acl, new_acl)
-      diff = diff_by_types[TYPE_USER] || {}
+      diff_by_types = diff_by_types(old_acl, new_acl, table_owner_id)
+      diff_prioritize_by_user(diff_by_types, new_acl)
+    end
 
-      # diff by org users
-      diff_by_org_or_group(TYPE_ORG, diff, diff_by_types, table_owner_id)
+    private
 
-      # diff by group users
-      diff_by_org_or_group(TYPE_GROUP, diff, diff_by_types, table_owner_id)
+    def self.diff_by_types(old_acl, new_acl, table_owner_id)
+      diff_by_types = {}
+      old_acl.each do |type, id_access|
+        id_access.each do |id, access|
+          if access == 'rw'
+            if new_acl[type].nil? || new_acl[type][id].nil?
+              revoke = 'rw'
+            elsif new_acl[type][id] == 'r'
+              revoke = 'w'
+            end
+          elsif access == 'r' && (new_acl[type].nil? || new_acl[type][id].nil?)
+            revoke = 'r'
+          end
 
-      diff
+          if revoke.present?
+            user_ids(type, id).each do |uid|
+              if table_owner_id != uid
+                diff_by_types[uid] ||= {}
+                diff_by_types[uid][type] = revoke
+              end
+            end
+          end
+        end
+      end
+      diff_by_types
     end
 
     def self.hashing_acl(acl)
@@ -29,66 +50,38 @@ module Carto
       acl_hash
     end
 
-    def self.diff_by_types(old_acl, new_acl)
+    def self.user_ids(type, id)
+      if type == TYPE_USER
+        [id]
+      elsif type == TYPE_ORG
+        Carto::Organization.find(id).users.map(&:id)
+      else
+        Carto::Group.find(id).users.map(&:id)
+      end
+    end
+
+    def self.diff_prioritize_by_user(diff_by_types, new_acl)
       diff = {}
-      old_acl.each do |type, id_access|
-        id_access.each do |id, access|
-          if access == 'rw'
-            if new_acl[type].nil? || new_acl[type][id].nil?
-              diff[type] ||= {}
-              diff[type][id] ||= 'rw'
-            elsif new_acl[type][id] == 'r'
-              diff[type] ||= {}
-              diff[type][id] ||= 'w'
-            end
-          elsif access == 'r' && (new_acl[type].nil? || new_acl[type][id].nil?)
-            diff[type] ||= {}
-            diff[type][id] ||= 'r'
-          end
+      diff_by_types.each do |uid, revoke_by_type|
+        revoke = if revoke_by_type[TYPE_USER].present?
+          revoke_by_type[TYPE_USER]
+        elsif revoke_by_type.length == 1
+          revoke_by_type[TYPE_ORG] || revoke_by_type[TYPE_GROUP]
+        elsif revoke_by_type[TYPE_ORG] == 'w' || revoke_by_type[TYPE_GROUP] == 'w'
+          'w'
+        elsif revoke_by_type[TYPE_ORG] == 'r' || revoke_by_type[TYPE_GROUP] == 'r'
+          'r'
+        else
+          'rw'
+        end
+
+        if new_acl[TYPE_USER].nil? || new_acl[TYPE_USER][uid].nil?
+          diff[uid] = revoke
+        elsif new_acl[TYPE_USER][uid] == 'r' && (revoke == 'w' && revoke_by_type[TYPE_USER].present?)
+          diff[uid] = 'w'
         end
       end
       diff
-    end
-
-    # Set the revoked permissions by user
-    # Returns a hash with user ids as hash keys and revokes as values
-    # users param is an array with the users from an organization or group
-    def self.diff_by_users(users, revoke, new_acl)
-      diff = {}
-      users.each do |user|
-        if new_acl[TYPE_USER].nil? || new_acl[TYPE_USER][user.id].nil?
-          diff[TYPE_USER] ||= {}
-          diff[TYPE_USER][user.id] = revoke
-        elsif (revoke == 'rw' || revoke == 'w') && new_acl[TYPE_USER][user.id] == 'r'
-          diff[TYPE_USER] ||= {}
-          diff[TYPE_USER][user.id] = 'w'
-        end
-      end
-      diff
-    end
-
-    def self.diff_by_org_or_group(type, diff, diff_by_types, table_owner_id)
-      if diff_by_types[type].present?
-        diff_by_types[type].each do |id, revoke|
-          begin
-            users = type == TYPE_ORG ? Carto::Organization.find(id).users : Carto::Group.find(id).users
-          rescue ActiveRecord::RecordNotFound
-          end
-
-          if users.present?
-            users_diff = diff_by_users(users, revoke, new_acl)
-            add_users_to_diff(diff, users_diff, table_owner_id)
-          end
-        end
-      end
-    end
-
-    def self.add_users_to_diff(diff, users_diff, table_owner_id)
-      unless users_diff.blank?
-        users_diff[TYPE_USER].each do |user_id, revoke|
-          diff[user_id] = revoke if diff[user_id].nil? && user_id != table_owner_id
-        end
-      end
     end
   end
 end
--- spec/services/carto/permission_service_spec.rb
+++ spec/services/carto/permission_service_spec.rb
@@ -1,295 +1,187 @@
 require 'spec_helper_min'
 
-class FakeUser
-  def initialize(id)
-    @id = id
-  end
-
-  def id
-    @id
+class PermissionServiceMock < Carto::PermissionService
+  def self.user_ids(type, id)
+    if type == 'user'
+      [id]
+    else
+      ["1", "2", "3", "4"]
+    end
   end
 end
 
 describe Carto::PermissionService do
-  def create_acl_hash(acl)
-    Carto::PermissionService.hashing_acl(acl)
-  end
-
-  describe '#diff_by_types' do
+  describe '#revokes_by_user' do
     it 'should be empty with grants' do
-      old_acl = create_acl_hash([])
-      new_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = []
+      new_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
 
-      old_acl = create_acl_hash([])
-      new_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "rw" }])
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = []
+      new_acl = [{ "type": "user", "id": "1", "access": "rw" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
 
-      old_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      new_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      new_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
 
-      old_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      new_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "rw" }])
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      new_acl = [{ "type": "user", "id": "1", "access": "rw" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
 
-      old_acl = create_acl_hash([])
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "r" }, { "type": "user", "id": "2", "access": "rw" }
-        ]
-      )
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = []
+      new_acl = [{ "type": "user", "id": "1", "access": "r" }, { "type": "user", "id": "2", "access": "rw" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
 
-      old_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "r" },
-          { "type": "org", "id": "1", "access": "r" }
-        ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "org", "id": "1", "access": "r" }
-        ]
-      )
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to be_blank
+      old_acl = [{ "type": "user", "id": "1", "access": "r" }, { "type": "org", "id": "1", "access": "r" }]
+      new_acl = [{ "type": "user", "id": "1", "access": "rw" }, { "type": "org", "id": "1", "access": "r" }]
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to be_blank
     end
 
     it 'should not be empty with revokes by type' do
-      old_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      new_acl = create_acl_hash([])
-      expected = { "user" => { "1" => "r" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      new_acl = []
+      expected = { "1" => "r" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
 
-      old_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "rw" }])
-      new_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "r" }])
-      expected = { "user" => { "1" => "w" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "user", "id": "1", "access": "rw" }]
+      new_acl = [{ "type": "user", "id": "1", "access": "r" }]
+      expected = { "1" => "w" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
 
-      old_acl = create_acl_hash([{ "type": "user", "id": "1", "access": "rw" }])
-      new_acl = create_acl_hash([])
-      expected = { "user" => { "1" => "rw" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "user", "id": "1", "access": "rw" }]
+      new_acl = []
+      expected = { "1" => "rw" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
 
-      old_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "r" },
-          { "type": "user", "id": "2", "access": "rw" }
-        ]
-      )
-      new_acl = create_acl_hash([])
-      expected = { "user" => { "1" => "r", "2" => "rw" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "user", "id": "1", "access": "r" }, { "type": "user", "id": "2", "access": "rw" }]
+      new_acl = []
+      expected = { "1" => "r", "2" => "rw" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
 
-      old_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "org", "id": "1", "access": "r" }
-        ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "r" },
-          { "type": "org", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "user" => { "1" => "w" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
-
-      old_acl = create_acl_hash(
-        [
-          { "type": "group", "id": "1", "access": "rw" },
-          { "type": "org", "id": "1", "access": "rw" }
-        ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "group", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "group" => { "1" => "w" }, "org" => { "1" => "rw" } }
-      revokes = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      expect(revokes).to eq(expected)
-    end
-  end
-
-  describe '#diff_by_users' do
-    before :all do
-      @users = [FakeUser.new("1"), FakeUser.new("2"), FakeUser.new("3"), FakeUser.new("4")]
+      old_acl = [{ "type": "user", "id": "1", "access": "rw" }, { "type": "org", "id": "1", "access": "r" }]
+      new_acl = [{ "type": "user", "id": "1", "access": "r" }, { "type": "org", "id": "1", "access": "r" }]
+      expected = { "1" => "w" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
     end
 
     it 'org: rw ---> r' do
-      org_revoke = "w"
-      new_acl = create_acl_hash([{ "type": "org", "id": "1", "access": "r" }])
-      expected = { "user" => { "1" => org_revoke, "2" => org_revoke, "3" => org_revoke, "4" => org_revoke } }
-      revokes = Carto::PermissionService.diff_by_users(@users, org_revoke, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "org", "id": "1", "access": "rw" }]
+      new_acl = [{ "type": "org", "id": "1", "access": "r" }]
+      expected = { "1" => "w", "2" => "w", "3" => "w", "4" => "w" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
     end
 
     it 'org: rw ---> none' do
-      org_revoke = "rw"
-      new_acl = create_acl_hash([])
-      revokes = Carto::PermissionService.diff_by_users(@users, org_revoke, new_acl)
-      expected = { "user" => { "1" => org_revoke, "2" => org_revoke, "3" => org_revoke, "4" => org_revoke } }
-      expect(revokes).to eq(expected)
+      old_acl = [{ "type": "org", "id": "1", "access": "rw" }]
+      new_acl = []
+      expected = { "1" => "rw", "2" => "rw", "3" => "rw", "4" => "rw" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
     end
 
     it 'user1: rw, user2: r, org: rw ---> r' do
-      org_revoke = "w"
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "user", "id": "2", "access": "r" },
-          { "type": "org", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "user" => { "2" => org_revoke, "3" => org_revoke, "4" => org_revoke } }
-      revokes = Carto::PermissionService.diff_by_users(@users, org_revoke, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [
+        { "type": "user", "id": "1", "access": "rw" },
+        { "type": "user", "id": "2", "access": "r" },
+        { "type": "org", "id": "1", "access": "rw" }
+      ]
+      new_acl = [
+        { "type": "user", "id": "1", "access": "rw" },
+        { "type": "user", "id": "2", "access": "r" },
+        { "type": "org", "id": "1", "access": "r" }
+      ]
+      expected = { "3" => "w", "4" => "w" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
     end
 
     it 'user1: rw, user2: r, org: r ---> none' do
-      org_revoke = "r"
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "user", "id": "2", "access": "r" }
-        ]
-      )
-      expected = { "user" => { "3" => org_revoke, "4" => org_revoke } }
-      revokes = Carto::PermissionService.diff_by_users(@users, org_revoke, new_acl)
-      expect(revokes).to eq(expected)
+      old_acl = [
+        { "type": "user", "id": "1", "access": "rw" },
+        { "type": "user", "id": "2", "access": "r" },
+        { "type": "org", "id": "1", "access": "r" }
+      ]
+      new_acl = [{ "type": "user", "id": "1", "access": "rw" }, { "type": "user", "id": "2", "access": "r" }]
+      expected = { "3" => "r", "4" => "r" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+      expect(diff).to eq(expected)
     end
 
     it 'user1: rw, user2: r, org: rw ---> none' do
-      org_revoke = "rw"
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "user", "id": "2", "access": "r" }
-        ]
-      )
-      expected = { "user" => { "2" => "w", "3" => org_revoke, "4" => org_revoke } }
-      revokes = Carto::PermissionService.diff_by_users(@users, org_revoke, new_acl)
-      expect(revokes).to eq(expected)
-    end
-  end
-
-  describe '#revokes_by_user' do
-    def revokes_by_user_mock(old_acl, new_acl)
-      diff_by_types = Carto::PermissionService.diff_by_types(old_acl, new_acl)
-      diff = diff_by_types['user'] || {}
-
-      # diff by org users
-      diff_by_types['org'].each do |_, revoke|
-        org_diff = Carto::PermissionService.diff_by_users(@users, revoke, new_acl)
-        Carto::PermissionService.add_users_to_diff(diff, org_diff, 'fake_table_owner_id')
-      end
-
-      # diff by group users
-      diff_by_types['group'].each do |_, revoke|
-        group_diff = Carto::PermissionService.diff_by_users(@users, revoke, new_acl)
-        Carto::PermissionService.add_users_to_diff(diff, group_diff, 'fake_table_owner_id')
-      end
-
-      diff
-    end
-
-    before :all do
-      @users = [FakeUser.new("1"), FakeUser.new("2"), FakeUser.new("3"), FakeUser.new("4")]
-    end
-
-    it 'org: rw ---> none; group: rw ---> r' do
-      old_acl = create_acl_hash(
-        [
-          { "type": "group", "id": "1", "access": "rw" },
-          { "type": "org", "id": "1", "access": "rw" }
-        ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "group", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "1" => "w", "2" => "w", "3" => "w", "4" => "w" }
-
-      diff = revokes_by_user_mock(old_acl, new_acl)
+      old_acl = [
+        { "type": "user", "id": "1", "access": "rw" },
+        { "type": "user", "id": "2", "access": "r" },
+        { "type": "org", "id": "1", "access": "rw" }
+      ]
+      new_acl = [{ "type": "user", "id": "1", "access": "rw" }, { "type": "user", "id": "2", "access": "r" }]
+      expected = { "3" => "rw", "4" => "rw" }
+      diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
       expect(diff).to eq(expected)
     end
 
-    it 'user1: rw; org: rw ---> none; group: rw ---> r' do
-      old_acl = create_acl_hash(
-        [
+    describe '#users, orgs and groups' do
+      it 'org: rw ---> none; group: rw ---> r' do
+        old_acl = [{ "type": "group", "id": "1", "access": "rw" }, { "type": "org", "id": "1", "access": "rw" }]
+        new_acl = [{ "type": "group", "id": "1", "access": "r" }]
+        expected = { "1" => "w", "2" => "w", "3" => "w", "4" => "w" }
+        diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+        expect(diff).to eq(expected)
+      end
+
+      it 'user1: rw; org: rw ---> none; group: rw ---> r' do
+        old_acl = [
           { "type": "user", "id": "1", "access": "rw" },
           { "type": "group", "id": "1", "access": "rw" },
           { "type": "org", "id": "1", "access": "rw" }
         ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "group", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "2" => "w", "3" => "w", "4" => "w" }
+        new_acl = [{ "type": "user", "id": "1", "access": "rw" },{ "type": "group", "id": "1", "access": "r" }]
+        expected = { "2" => "w", "3" => "w", "4" => "w" }
+        diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+        expect(diff).to eq(expected)
+      end
 
-      diff = revokes_by_user_mock(old_acl, new_acl)
-      expect(diff).to eq(expected)
-    end
-
-    it 'user1: rw; user2: rw ---> none; org: rw ---> none; group: rw ---> r' do
-      old_acl = create_acl_hash(
-        [
+      it 'user1: rw; user2: rw ---> none; org: rw ---> none; group: rw ---> r' do
+        old_acl = [
           { "type": "user", "id": "1", "access": "rw" },
           { "type": "user", "id": "2", "access": "rw" },
           { "type": "group", "id": "1", "access": "rw" },
           { "type": "org", "id": "1", "access": "rw" }
         ]
-      )
-      new_acl = create_acl_hash(
-        [
-          { "type": "user", "id": "1", "access": "rw" },
-          { "type": "group", "id": "1", "access": "r" }
-        ]
-      )
-      expected = { "2" => "rw", "3" => "w", "4" => "w" }
+        new_acl = [{ "type": "user", "id": "1", "access": "rw" }, { "type": "group", "id": "1", "access": "r" }]
+        expected = { "2" => "rw", "3" => "w", "4" => "w" }
+        diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+        expect(diff).to eq(expected)
+      end
 
-      diff = revokes_by_user_mock(old_acl, new_acl)
-      expect(diff).to eq(expected)
-    end
-
-    it 'user1: rw; user2: rw ---> none; org: rw ---> none; group: rw ---> r' do
-      old_acl = create_acl_hash(
-        [
+      it 'user1: rw; user2: rw ---> none; org: rw ---> none; group: rw ---> r' do
+        old_acl = [
           { "type": "user", "id": "1", "access": "rw" },
           { "type": "user", "id": "2", "access": "rw" },
           { "type": "group", "id": "1", "access": "rw" },
           { "type": "org", "id": "1", "access": "rw" }
         ]
-      )
-      new_acl = create_acl_hash(
-        [
+        new_acl = [
           { "type": "user", "id": "1", "access": "rw" },
           { "type": "user", "id": "2", "access": "r" },
           { "type": "group", "id": "1", "access": "r" }
         ]
-      )
-      expected = { "2" => "w", "3" => "w", "4" => "w" }
-
-      diff = revokes_by_user_mock(old_acl, new_acl)
-      expect(diff).to eq(expected)
+        expected = { "2" => "w", "3" => "w", "4" => "w" }
+        diff = PermissionServiceMock.revokes_by_user(old_acl, new_acl, 'fake_table_owner_id')
+        expect(diff).to eq(expected)
+      end
     end
   end
 end

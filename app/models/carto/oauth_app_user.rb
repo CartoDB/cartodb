a4a471c25db49716adbd36fdf2b96b7b3c3afc35
Simon Martín <oleurud@gmail.com>
2018-11-20 17:09:17 +0100

    dont move dataset_role_name

--- app/models/carto/oauth_app_user.rb
+++ app/models/carto/oauth_app_user.rb
@@ -34,10 +34,6 @@ module Carto
       no_dataset_scopes + dataset_scopes
     end
 
-    def dataset_role_name
-      "carto_oauth_app_#{id}"
-    end
-
     def revoke_permissions(table, revoked_permissions)
       db_connection = user.in_database(as: :superuser)
       Carto::TableAndFriends.apply(db_connection, table.database_schema, table.name) do |_s, _t, qualified_name|
@@ -168,6 +164,10 @@ module Carto
       }
 
       user.in_database.execute(query).map { |r| "\"#{r['nspname']}\".#{r['relname']}" }
+    end
+
+    def dataset_role_name
+      "carto_oauth_app_#{id}"
     end
   end
 end

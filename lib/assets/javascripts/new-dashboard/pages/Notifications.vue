570e54a0c9f89eb7c35bdc0bd719fd33ff354fed
csubira <cillas@cartodb.com>
2018-11-21 12:26:20 +0100

    Add empty state and new tests to notifications page

--- lib/assets/javascripts/new-dashboard/pages/Notifications.vue
+++ lib/assets/javascripts/new-dashboard/pages/Notifications.vue
@@ -7,60 +7,68 @@
             <img src="../assets/icons/section-title/envelope.svg">
           </template>
         </SectionTitle>
-        <section class="notifications-list--empty"  v-if="false">
-          <h3 class="text is-caption ">There are no notifications yet</h3>
-        </section>
-        <section class="notifications-list" v-if="hasNotifications">
-          <NotificationCard 
-            v-for="notification in notifications"
-            :key="notification.id"
-            :receivedAt='notification.received_at'
-            :readAt='notification.read_at'
-            :htmlBody='notification.html_body'>
-          </NotificationCard>
-        </section>
+        <div class="grid" v-if="!emptyState">
+          <ul class="notifications-list grid-cell  grid-cell--col9 grid-cell--col12--tablet">
+            <li class="notification-item">
+              <NotificationCard
+                v-for="notification in notifications"
+                :key="notification.id"
+                :receivedAt='notification.received_at'
+                :readAt='notification.read_at'
+                :htmlBody='notification.html_body'>
+              </NotificationCard>
+            </li>
+          </ul>
+        </div>
+        <EmptyState
+          :text="$t('NotificationsPage.emptyState')"
+          v-if="emptyState">
+          <img svg-inline src="../assets/icons/maps/compass.svg">
+        </EmptyState>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import SectionTitle from "../components/SectionTitle";
-import NotificationCard from "../components/NotificationCard";
+import SectionTitle from '../components/SectionTitle';
+import EmptyState from '../components/States/EmptyState';
+import NotificationCard from '../components/NotificationCard';
 export default {
-  name: "NotificationsPage",
+  name: 'NotificationsPage',
   components: {
     SectionTitle,
+    EmptyState,
     NotificationCard
   },
   props: {},
   computed: {
-    pageTitle() {
+    pageTitle () {
       return this.$t(`NotificationsPage.header.title`);
     },
-    notifications() {
+    notifications () {
       // return [];
-      // return this.$store.state.user.organizationNotifications;
-      return [
-        {
-          html_body:
-            "<p>This is an unread notification, font will be bolder and the green dot should be visible.</p>",
-          icon: "alert",
-          id: "288dfe6e-1a9d-4157-bd36-41cd8459af62",
-          read_at: null,
-          received_at: "2018-11-20T15:28:21.792Z"
-        },
-        {
-          html_body: "<p>This notification has been read and should be lighter</p>",
-          icon: "alert",
-          id: "091a04a7-28d6-4fcf-bd8a-b1c34842bdb4",
-          read_at: "2018-11-20T17:11:42.285Z",
-          received_at: "2018-11-19T17:11:42.285Z"
-        }
-      ];
+      return this.$store.state.user.organizationNotifications;
+      // return [
+      //   {
+      //     html_body:
+      //       "<p>This is an unread notification, font will be bolder and the green dot should be visible.</p>",
+      //     icon: "alert",
+      //     id: "288dfe6e-1a9d-4157-bd36-41cd8459af62",
+      //     read_at: null,
+      //     received_at: "2018-11-20T15:28:21.792Z"
+      //   },
+      //   {
+      //     html_body: "<p>This notification has been read and should be lighter</p>",
+      //     icon: "alert",
+      //     id: "091a04a7-28d6-4fcf-bd8a-b1c34842bdb4",
+      //     read_at: "2018-11-20T17:11:42.285Z",
+      //     received_at: "2018-11-19T17:11:42.285Z"
+      //   }
+      // ];
     },
-    hasNotifications() {
-      return this.notifications && this.notifications.length > 0;
+    emptyState () {
+      return !this.notifications || !this.notifications.length;
     }
   }
 };
@@ -74,12 +82,15 @@ export default {
   margin-bottom: 44px;
 }
 
-.notifications-list--empty {
-  color: $text-secondary-color;
-  text-align: center;
-}
-
 .full-width {
   width: 100%;
+}
+
+.notification-item {
+  border-bottom: 1px solid $softblue;
+
+  &:last-child {
+    border-bottom: none;
+  }
 }
 </style>

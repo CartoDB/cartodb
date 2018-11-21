f61f15aa355a0052623ed9b2f10fb1ac27e3c1a2
IagoLast <iago.lastra@gmail.com>
2018-11-21 14:19:28 +0100

    refactor: Simplify code and share test mocks

--- lib/assets/javascripts/new-dashboard/pages/Notifications.vue
+++ lib/assets/javascripts/new-dashboard/pages/Notifications.vue
@@ -8,12 +8,10 @@
           </template>
         </SectionTitle>
 
-        <div class="grid" v-if="!emptyState">
+        <div class="grid" v-if="hasNotifications">
           <ul class="notifications-list grid-cell  grid-cell--col9 grid-cell--col12--tablet">
-            <li class="notification-item">
+            <li class="notification-item" v-for="notification in notifications" :key="notification.id">
               <NotificationCard
-                v-for="notification in notifications"
-                :key="notification.id"
                 :receivedAt='notification.received_at'
                 :readAt='notification.read_at'
                 :htmlBody='notification.html_body'>
@@ -22,10 +20,7 @@
           </ul>
         </div>
 
-        <EmptyState
-          v-if="emptyState"
-          :text="emptyStateText"
-          >
+        <EmptyState v-if="!hasNotifications" :text="emptyStateText">
           <img svg-inline src="../assets/icons/maps/compass.svg">
         </EmptyState>
       </div>
@@ -50,13 +45,30 @@ export default {
       return this.$t(`NotificationsPage.header.title`);
     },
     notifications() {
+      return [
+        {
+          html_body:
+            "<p>Cupcake Ipsum dolor gingerbread chocolate. <em>Pudding</em> wafer ice cream. Powder ice cream carrot cake <strong>liquorice</strong> cookie oat cake.</p>",
+          icon: "alert",
+          id: "288dfe6e-1a9d-4157-bd36-41cd8459af62",
+          read_at: null,
+          received_at: "2018-11-20T15:28:21.792Z"
+        },
+        {
+          html_body: "<p>Holi parte 2</p>",
+          icon: "alert",
+          id: "091a04a7-28d6-4fcf-bd8a-b1c34842bdb4",
+          read_at: "2018-11-20T17:11:42.285Z",
+          received_at: "2018-11-19T17:11:42.285Z"
+        }
+      ];
       return this.$store.state.user.organizationNotifications;
     },
-    emptyState() {
-      return !this.notifications || !this.notifications.length;
+    hasNotifications() {
+      return this.notifications || this.notifications.length > 0;
     },
     emptyStateText() {
-      return $t('NotificationsPage.emptyState');
+      return $t("NotificationsPage.emptyState");
     }
   }
 };
@@ -75,10 +87,15 @@ export default {
 }
 
 .notification-item {
+  margin-bottom: 36px;
   border-bottom: 1px solid $softblue;
 
   &:last-child {
     border-bottom: none;
+  }
+
+  .notification {
+    margin-bottom: 36px;
   }
 }
 </style>

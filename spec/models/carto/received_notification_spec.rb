14edde7ab017ff144d9717f319c91a9d80b19b1e
IagoLast <iago.lastra@gmail.com>
2018-11-27 12:30:42 +0100

    test: Recover user controller specs

--- spec/models/carto/received_notification_spec.rb
+++ spec/models/carto/received_notification_spec.rb
@@ -29,5 +29,22 @@ module Carto
       )
     end
 
+    describe 'User#unread_received_notifications' do	
+      it 'does not list read notifications' do	
+        unread = create_received_notification(false)	
+        create_received_notification(true)	
+         expect(@user.received_notifications.unread).to eq [unread]	
+      end
+
+      it 'sorted in decreasing date order' do	
+        unread1 = create_received_notification(false)	
+        Delorean.jump(5.seconds)	
+        unread2 = create_received_notification(false)	
+        Delorean.jump(5.seconds)	
+        unread3 = create_received_notification(false)	
+        Delorean.back_to_the_present	
+        expect(@user.received_notifications.unread).to eq [unread3, unread2, unread1]	
+      end	
+    end
   end
 end

c27245b8e3feb696ca312ea3296df942769c731f
IagoLast <iago.lastra@gmail.com>
2018-12-12 10:46:27 +0100

    feat: Make maps responsive

--- lib/assets/javascripts/new-dashboard/components/Home/Maps/MapList.vue
+++ lib/assets/javascripts/new-dashboard/components/Home/Maps/MapList.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="map-list">
-    <li v-if="!maps.length" class="card">
+    <li v-if="!maps.length" class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile map-element">
       <CreateMapCard></CreateMapCard>
     </li>
-    <li v-if="maps.length" class="card" v-for="map in maps" :key="map.id">
+    <li v-if="maps.length" class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile map-element" v-for="map in maps" :key="map.id">
       <MapCard @dataChanged="onDataChanged" :canHover="false" :map="map" :isSelected="isMapSelected(map)" @toggleSelection="toggleSelected"></MapCard>
     </li>
   </ul>
@@ -48,19 +48,15 @@ export default {
 .map-list {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
   width: 100%;
 
   .card {
-    min-width: 280px;
-    margin: 12px auto;
+    
   }
 
   @media all and (min-width: 656px) {
     .card {
-      flex: 1;
-      min-width: 288px;
-      margin: 0 12px 36px;
+     
     }
   }
 }

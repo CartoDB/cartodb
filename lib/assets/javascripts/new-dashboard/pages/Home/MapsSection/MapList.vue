7692b8b3451f276aa6a1b6f1a1b24ffd7b8fbfc4
IagoLast <iago.lastra@gmail.com>
2018-12-13 15:28:35 +0100

    feat: Remove selecting props from mapList

--- lib/assets/javascripts/new-dashboard/pages/Home/MapsSection/MapList.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/MapsSection/MapList.vue
@@ -4,7 +4,7 @@
       <CreateMapCard></CreateMapCard>
     </li>
     <li v-if="hasMaps" class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile map-element" v-for="map in maps" :key="map.id">
-      <MapCard @dataChanged="onDataChanged" :canHover="false" :map="map" :isSelected="isMapSelected(map)" @toggleSelection="toggleSelected"></MapCard>
+      <MapCard :canHover="false" :map="map"></MapCard>
     </li>
   </ul>
 </template>
@@ -17,25 +17,10 @@ export default {
   name: 'MapList',
   props: {
     maps: Object,
-    selectedMaps: {
-      type: Set,
-      default: () => new Set()
-    }
   },
   components: {
     MapCard,
     CreateMapCard
-  },
-  methods: {
-    toggleSelected (event) {
-      this.$emit('toggleSelection', event.map);
-    },
-    isMapSelected (map) {
-      return this.$props.selectedMaps.has(map);
-    },
-    onDataChanged () {
-      this.$emit('dataChanged');
-    }
   },
   computed: {
     hasMaps () {

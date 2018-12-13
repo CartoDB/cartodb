4e94a64df210659524e8f8165bd4a3c88585dcaf
Simon Martín <oleurud@gmail.com>
2018-12-13 11:02:55 +0100

    hound happier? 2

--- spec/services/carto/user_metadata_export_service_spec.rb
+++ spec/services/carto/user_metadata_export_service_spec.rb
@@ -382,13 +382,13 @@ describe Carto::UserMetadataExportService do
       expect(user.user_multifactor_auths).to be_empty
     end
 
-    if export[:oauth_apps]
+    if export[:oauth_apps] && !export[:oauth_apps].blank?
       expect_export_matches_oauth_apps(export[:oauth_apps].first, user.oauth_apps.first)
     else
       expect(user.oauth_apps).to be_empty
     end
 
-    if export[:oauth_app_users]
+    if export[:oauth_app_users] && !export[:oauth_app_users].blank?
       expect_export_matches_oauth_app_users(export[:oauth_app_users].first, user.oauth_app_users.first)
     else
       expect(user.oauth_app_users).to be_empty
@@ -518,8 +518,6 @@ describe Carto::UserMetadataExportService do
   end
 
   def expect_export_matches_oauth_apps(exported_oauth_app, oauth_app)
-    return unless oauth_app
-
     expect(exported_oauth_app[:id]).to eq oauth_app.id
     expect(exported_oauth_app[:user_id]).to eq oauth_app.user_id
     expect(exported_oauth_app[:name]).to eq oauth_app.name
@@ -543,8 +541,6 @@ describe Carto::UserMetadataExportService do
   end
 
   def expect_export_matches_oauth_app_users(exported_oauth_app_user, oauth_app_user)
-    expect(exported_oauth_app_user).to be_nil && return unless oauth_app_user
-
     expect(exported_oauth_app_user[:id]).to eq oauth_app_user.id
     expect(exported_oauth_app_user[:oauth_app_id]).to eq oauth_app_user.oauth_app_id
     expect(exported_oauth_app_user[:user_id]).to eq oauth_app_user.user_id

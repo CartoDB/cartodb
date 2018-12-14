199efbd823038ac546931f2ce9755160ebf5dd7b
Gonzalo Riestra <gonzalor@cartodb.com>
2018-12-14 19:49:52 +0100

    eager load permissions for dependent visualizations

--- app/queries/carto/visualization_query_builder.rb
+++ app/queries/carto/visualization_query_builder.rb
@@ -117,7 +117,7 @@ class Carto::VisualizationQueryBuilder
   end
 
   def with_prefetch_dependent_visualizations
-    inner_visualization = { visualization: { map: { layers: :layers_user_tables } } }
+    inner_visualization = { visualization: { map: { layers: :layers_user_tables }, permission: :owner } }
     nested_association = { map: { user_table: { layers: { maps: inner_visualization } } } }
     with_eager_load_of(nested_association)
   end

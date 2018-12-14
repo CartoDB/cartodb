48816afc373e7bbe12d96e58cf63654f81423a39
IagoLast <iago.lastra@gmail.com>
2018-12-14 11:31:03 +0100

    feat: Use CreateMapCard horizontal component in maps and welcome>maps

--- lib/assets/javascripts/new-dashboard/pages/Home/MapsSection/MapList.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/MapsSection/MapList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="map-list">
-    <li v-if="!hasMaps" class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile map-element">
+    <li class="grid-cell--col12" v-if="!hasMaps">
       <CreateMapCard></CreateMapCard>
     </li>
     <li v-if="hasMaps" class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile map-element" v-for="map in maps" :key="map.id">
@@ -11,7 +11,7 @@
 
 <script>
 import MapCard from 'new-dashboard/components/MapCard.vue';
-import CreateMapCard from './CreateMapCard.vue';
+import CreateMapCard from 'new-dashboard/components/CreateMapCard.vue';
 
 export default {
   name: 'MapList',

2356226d7f24b7be178b2f49334e114e7648846e
Gonzalo Riestra <gonzalor@cartodb.com>
2018-12-18 08:15:17 +0100

    next_billing_cycle

--- app/models/carto/helpers/billing_cycle.rb
+++ app/models/carto/helpers/billing_cycle.rb
@@ -4,7 +4,18 @@ module Carto
   module BillingCycle
     def last_billing_cycle
       day = period_end_date.day
-      date = (day > Date.today.day ? (Date.today - 1.month) : Date.today)
+      date = day > Date.today.day ? (Date.today - 1.month) : Date.today
+      begin
+        Date.new(date.year, date.month, day)
+      rescue ArgumentError
+        day = day - 1
+        retry
+      end
+    end
+
+    def next_billing_cycle
+      day = period_end_date.day
+      date = day > Date.today.day ? Date.today : Date.today + 1.month
       begin
         Date.new(date.year, date.month, day)
       rescue ArgumentError
--- spec/models/carto/helpers/billing_cycle_spec.rb
+++ spec/models/carto/helpers/billing_cycle_spec.rb
@@ -3,17 +3,13 @@ require 'ostruct'
 require 'delorean'
 
 describe Carto::BillingCycle do
-  describe :last_billing_cycle do
+  describe "#last_billing_cycle" do
     class MyDummyUser < OpenStruct
       include Carto::BillingCycle
     end
 
     before(:each) do
       @user = MyDummyUser.new
-    end
-
-    after(:each) do
-      @user = nil
     end
 
     [
@@ -68,6 +64,72 @@ describe Carto::BillingCycle do
         @user.period_end_date = Date.parse(period_end_date)
         Delorean.time_travel_to(today) do
           @user.last_billing_cycle.should eq Date.parse(expected)
+        end
+      end
+    end
+  end
+
+  describe "#next_billing_cycle" do
+    class MyDummyUser < OpenStruct
+      include Carto::BillingCycle
+    end
+
+    before(:each) do
+      @user = MyDummyUser.new
+    end
+
+    [
+      { today: '2017-03-10', period_end_date: '2017-02-21', expected: '2017-03-21' },
+      { today: '2017-03-10', period_end_date: '2017-07-31', expected: '2017-03-31' },
+      { today: '2017-03-10', period_end_date: '2017-01-11', expected: '2017-03-11' },
+      { today: '2017-01-10', period_end_date: '2016-10-31', expected: '2017-01-31' },
+      { today: '2017-03-01', period_end_date: '2016-10-02', expected: '2017-03-02' }
+    ].each do |example|
+      today = example[:today]
+      period_end_date = example[:period_end_date]
+      expected = example[:expected]
+      it "returns the current month if period_end_date.day > today.day" \
+      " (today = #{today}, period_end_date = #{period_end_date}, expected = #{expected})" do
+        @user.period_end_date = Date.parse(period_end_date)
+        Delorean.time_travel_to(today) do
+          @user.next_billing_cycle.should eq Date.parse(expected)
+        end
+      end
+    end
+
+    [
+      { today: '2017-03-01', period_end_date: '2016-10-01', expected: '2017-04-01' },
+      { today: '2017-03-10', period_end_date: '2017-02-01', expected: '2017-04-01' },
+      { today: '2017-02-01', period_end_date: '2017-02-01', expected: '2017-03-01' },
+      { today: '2017-01-18', period_end_date: '2016-10-15', expected: '2017-02-15' },
+      { today: '2017-03-12', period_end_date: '2015-07-04', expected: '2017-04-04' }
+    ].each do |example|
+      today = example[:today]
+      period_end_date = example[:period_end_date]
+      expected = example[:expected]
+      it "returns the next month if period_end_date.day >= today.day" \
+      " (today = #{today}, period_end_date = #{period_end_date}, expected = #{expected})" do
+        @user.period_end_date = Date.parse(period_end_date)
+        Delorean.time_travel_to(today) do
+          @user.next_billing_cycle.should eq Date.parse(expected)
+        end
+      end
+    end
+
+    [
+      { today: '2017-03-21', period_end_date: '2017-01-31', expected: '2017-03-31' },
+      # 2020 is a leap year
+      { today: '2020-02-21', period_end_date: '2017-01-31', expected: '2020-02-29' },
+      { today: '2017-04-21', period_end_date: '2017-01-31', expected: '2017-04-30' }
+    ].each do |example|
+      today = example[:today]
+      period_end_date = example[:period_end_date]
+      expected = example[:expected]
+      it "returns the previous valid day when dealing with 28, 29 or 30-day month corner cases" \
+      " (today = #{today}, period_end_date = #{period_end_date}, expected = #{expected})" do
+        @user.period_end_date = Date.parse(period_end_date)
+        Delorean.time_travel_to(today) do
+          @user.next_billing_cycle.should eq Date.parse(expected)
         end
       end
     end

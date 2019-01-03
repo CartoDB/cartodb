74b1f9ece183ac69b8543752540752014ef9d900
jesusbotella <jesus.botella@gmail.com>
2019-01-03 17:42:47 +0100

    Create TrialExpired bundle

---
+++ lib/assets/javascripts/new-dashboard/bundles/trial-expired/TrialExpiredApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <Footer />
+</template>
+
+<script>
+import Footer from 'new-dashboard/components/Footer';
+
+export default {
+  name: 'TrialExpiredApp',
+  components: {
+    Footer
+  }
+};
+</script>

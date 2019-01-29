9395201ffdd613bae8cc48ef40c40511168ca4c3
jesusbotella <jesus.botella@gmail.com>
2019-01-29 15:30:13 +0100

    Remove unused code from RecentSection

--- lib/assets/javascripts/new-dashboard/pages/Home/RecentSection/RecentSection.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/RecentSection/RecentSection.vue
@@ -20,8 +20,6 @@
 import { mapState } from 'vuex';
 import SectionTitle from 'new-dashboard/components/SectionTitle';
 import SimpleMapCard from 'new-dashboard/components/MapCard/SimpleMapCard.vue';
-// import compareAsc from 'date-fns/compare_asc';
-// import toObject from 'new-dashboard/utils/to-object';
 
 export default {
   name: 'RecentSection',
@@ -34,36 +32,11 @@ export default {
   },
   computed: {
     ...mapState({
-      // datasets: state => state.datasets.list,
-      // maps: state => state.maps.list,
-      // areMapsFiltered: state => state.maps.isFiltered,
-      // areDatasetsFiltered: state => state.datasets.isFiltered,
-      // areMapsFetching: state => state.maps.isFetching,
-      // areDatasetsFetching: state => state.datasets.isFetching
       recentContent: state => state.recentContent.list
     }),
     visibleSections () {
       return ['privacy', 'lastModification'];
     }
-    // recentContent () {
-    //   if (this.areMapsFetching || this.areDatasetsFetching) {
-    //     return [];
-    //   }
-
-    //   const recentContent = {
-    //     ...this.maps,
-    //     ...this.datasets
-    //   };
-
-    //   const recentContentIds = Object.keys(recentContent)
-    //     .sort((elementA, elementB) =>
-    //       compareAsc(recentContent[elementB].updated_at, recentContent[elementA].updated_at)
-    //     );
-
-    //   return recentContentIds
-    //     .slice(0, 3)
-    //     .map(id => recentContent[id]);
-    // }
   }
 };
 </script>

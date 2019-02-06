13b7b909c00154ef4b547a54ee1ad8630c8442de
jesusbotella <jesus.botella@gmail.com>
2019-02-06 15:15:38 +0100

    Setup different states in TagSection

--- lib/assets/javascripts/new-dashboard/pages/Home/TagsSection/TagsSection.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/TagsSection/TagsSection.vue
@@ -14,58 +14,109 @@
         </template>
       </SectionTitle>
 
-      <ul class="grid">
+      <LoadingState v-if="isFetching" :text="$t('HomePage.TagsSection.loading')" class="loading"></LoadingState>
+
+      <EmptyState
+        :text="$t('HomePage.TagsSection.emptyState.title')"
+        :subtitle="$t('HomePage.TagsSection.emptyState.subtitle')"
+        v-if="isEmptyState">
+      </EmptyState>
+
+      <ul class="grid tags" v-if="hasTags">
         <li class="card grid-cell grid-cell--col4 grid-cell--col6--tablet grid-cell--col12--mobile tag" v-for="tag in tags" :key="tag.tag">
           <TagCard :tag="tag" />
         </li>
       </ul>
+
+      <Pagination class="pagination-element" v-if="shouldShowPagination" :page=currentPage :numPages=numPages @pageChange="goToPage"></Pagination>
     </div>
   </section>
 </template>
 
 <script>
 import SectionTitle from 'new-dashboard/components/SectionTitle';
-import TagCard from 'new-dashboard/components/TagCard.vue';
+import TagCard from 'new-dashboard/components/TagCard';
+import Pagination from 'new-dashboard/components/Pagination';
+import EmptyState from 'new-dashboard/components/States/EmptyState';
+import LoadingState from 'new-dashboard/components/States/LoadingState';
 
 export default {
   name: 'TagsSection',
   components: {
     SectionTitle,
-    TagCard
+    TagCard,
+    Pagination,
+    EmptyState,
+    LoadingState
   },
   mounted () {
     this.getTags();
   },
   data () {
     return {
-      tags: []
+      isFetching: false,
+      tags: [],
+      currentPage: 1,
+      perPage: 6,
+      totalTags: 0
     };
+  },
+  computed: {
+    hasTags () {
+      return !this.isFetching && this.totalTags > 0;
+    },
+    isEmptyState () {
+      return !this.isFetching && this.totalTags <= 0;
+    },
+    numPages () {
+      return Math.ceil(this.totalTags / this.perPage);
+    },
+    shouldShowPagination () {
+      return this.numPages > 1;
+    }
   },
   methods: {
     goToRecentSection () {
       this.$emit('sectionChange', 'RecentSection');
     },
     getTags (options = {}) {
+      this.isFetching = true;
+
       this.$store.state.client.getTags(options,
         (err, _, tags) => {
+          this.isFetching = false;
+
           if (err) {
             return;
           }
 
-          this.tags = tags;
+          this.tags = tags.result;
+          this.totalTags = tags.total;
         }
       );
+    },
+    goToPage (page) {
+      this.currentPage = page;
+      this.getTags({ page });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.tags {
+  margin-bottom: 10px;
+}
+
 .tag {
   margin-bottom: 24px;
 }
 
 .tags__action {
   margin-right: 12px;
+}
+
+.loading {
+  margin: 154px 0 85px;
 }
 </style>

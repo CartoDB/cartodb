dc335f963016ce5abf4e808a4d292fe042716601
Mario de Frutos Dieguez <mario@defrutos.org>
2019-02-06 13:18:09 +0100

    Comment corrections

--- config/initializers/zz_patch_activerecord_type_loading.rb
+++ config/initializers/zz_patch_activerecord_type_loading.rb
@@ -1,18 +1,20 @@
-# Activerecord every time it mades a connection brings all the `pg_type`
-# data into a cache in order to do the relation between pg types and rails
-# types
+# Every time Activerecord makes a connection, it brings all the `pg_type`
+# data into a cache in order to do mapping between pg types and rails types
 #
 # What is the problem?
 #
 # Well in our users database we have 300K type. AR picks ~142K which leads to
-# spent ~1.3s picking the data and ~0.5s in processing those records so we end
-# spending about ~2s or more without needed in all our in_database operations
+# spending ~1.3s picking the data and ~0.5s in processing those records so we end
+# spending about ~2s or more without a need in all our in_database operations
 #
-# How this fix works?
+# How does this fix work?
 #
 # Well, we filter all the table types (table, analysis tables, overviews) because
-# in our app they aren't going to be used and in case they're need, Rails is going
+# in our app they aren't going to be used and in case they're needed, Rails is going
 # to query the database for it, so no problem.
+#
+# This patch was developed for rails 4.2.10, in case of update you need to review it
+# to verify if it needs to be modified or even removed.
 module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL

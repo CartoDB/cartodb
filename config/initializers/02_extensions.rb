c1fe93dfa7f5dbe98c07ccba936834c2f55bbed3
Mario de Frutos Dieguez <mario@defrutos.org>
2019-03-05 15:55:30 +0100

    Remove monkey patches for rails 3 and ruby 2.4 (#14667)
    Remove monkey patches for rails 3 and ruby 2.4

--- config/initializers/02_extensions.rb
+++ config/initializers/02_extensions.rb
@@ -8,32 +8,3 @@ module SequelRails
     @db ||= ::Sequel.synchronize { ::Sequel::DATABASES.first }
   end
 end
-
-# Patches for Ruby 2.4
-# TODO: remove .dup when we no longer support buggy ruby version 2.2.4p230
-if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new("2.4")
-  # ActiveSupport dates, e.g: 3.days (fixed in Rails 5, and no sooner)
-  class ActiveSupport::Duration
-    def coerce(other)
-      [other, to_i]
-    end
-  end
-
-  # ActiveRecord 3.2. Fixed in Arel 7.1.0
-  require 'arel'
-  module Arel
-    module Visitors
-      class Dot
-        alias :visit_Integer :visit_String
-      end
-
-      class ToSql
-        alias :visit_Integer :literal
-      end
-
-      class DepthFirst
-        alias :visit_Integer :terminal
-      end
-    end
-  end
-end

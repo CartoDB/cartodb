1ae2e40813aa4102813d7265eb30143cfbed373e
jesusbotella <jesus.botella@gmail.com>
2019-03-11 11:52:43 +0100

    Add header and footer entry points to split Webpack configuration

--- webpack/v4/new-dashboard/entryPoints.js
+++ webpack/v4/new-dashboard/entryPoints.js
@@ -9,5 +9,15 @@ module.exports = {
     rootDir('lib/assets/javascripts/new-dashboard/main.js'),
     ...glob.sync(rootDir('assets/stylesheets/new-dashboard/main.scss')),
     rootDir('node_modules/internal-carto.js/themes/scss/entry.scss')
+  ],
+
+  header: [
+    rootDir('lib/assets/javascripts/new-dashboard/bundles/header/header.js'),
+    rootDir('lib/assets/javascripts/new-dashboard/styles/bundles/header.scss')
+  ],
+
+  footer: [
+    rootDir('lib/assets/javascripts/new-dashboard/bundles/footer/footer.js'),
+    rootDir('lib/assets/javascripts/new-dashboard/styles/bundles/footer.scss')
   ]
 };

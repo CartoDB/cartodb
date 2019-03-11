39631bb836559769b302154ec6148514a6d4484e
Gonzalo Riestra <gonzalor@cartodb.com>
2019-03-11 18:43:48 +0100

    upgrade extension to 0.26.0

--- app/models/user/db_service.rb
+++ app/models/user/db_service.rb
@@ -594,7 +594,7 @@ module CartoDB
       # Upgrade the cartodb postgresql extension
       def upgrade_cartodb_postgres_extension(statement_timeout = nil, cdb_extension_target_version = nil)
         if cdb_extension_target_version.nil?
-          cdb_extension_target_version = '0.25.0'
+          cdb_extension_target_version = '0.26.0'
         end
 
         @user.in_database(as: :superuser, no_cartodb_in_schema: true) do |db|

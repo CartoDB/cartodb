a513e131c9fbc53957a3f39d54ba7006eddf3242
Simon Martín <oleurud@gmail.com>
2019-03-13 16:59:32 +0100

    visualization_backups migration

---
+++ db/migrate/20190313144818_revamp_visualization_backup.rb
@@ -0,0 +1,24 @@
+require 'carto/db/migration_helper'
+
+include Carto::Db::MigrationHelper
+
+migration(
+  Proc.new do
+    run "TRUNCATE TABLE visualization_backups"
+
+    run "ALTER TABLE visualization_backups DROP CONSTRAINT visualization_backups_pkey"
+    drop_column :visualization_backups, :export_vizjson
+
+    add_column :visualization_backups, :id, :uuid, default: Sequel.lit('uuid_generate_v4()'), null: false
+    add_column :visualization_backups, :export, String, null: false
+    run "ALTER TABLE visualization_backups ADD PRIMARY KEY (id)"
+  end,
+  Proc.new do
+    run "ALTER TABLE visualization_backups DROP CONSTRAINT visualization_backups_pkey"
+    drop_column :visualization_backups, :id
+    drop_column :visualization_backups, :export
+
+    add_column :visualization_backups, :export_vizjson, String, null: false
+    run "ALTER TABLE visualization_backups ADD PRIMARY KEY (visualization)"
+  end
+)

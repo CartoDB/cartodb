9bd28aa4d52367dc3a7b2f8ba2afbd163d3bf74b
Mario de Frutos Dieguez <mario@defrutos.org>
2019-03-14 18:47:47 +0100

    New tests with more delay

--- spec/lib/carto/bolt_spec.rb
+++ spec/lib/carto/bolt_spec.rb
@@ -44,7 +44,7 @@ module Carto
     it 'should wait for execution and exit without complete it if timeout and retries reach the limit' do
       main = Thread.new do
         @bolt.run_locked {
-          sleep(0.1)
+          sleep(1)
         }.should be_true
       end
       thr = Thread.new do
@@ -61,7 +61,7 @@ module Carto
         flag = 0
         @bolt.run_locked(rerun_func: lambda { flag += 1 }) {
           flag += 1
-          sleep(0.1)
+          sleep(1)
         }.should be_true
         flag.should eq(2)
       end
@@ -76,19 +76,19 @@ module Carto
       main = Thread.new do
         flag = 0
         rerun_func = lambda do
-          sleep(0.1)
+          sleep(1)
           flag += 1
         end
         @bolt.run_locked(rerun_func: rerun_func) {
           flag += 1
-          sleep(0.1)
+          sleep(1)
         }.should be_true
         flag.should > 2
       end
       6.times do
         t = Thread.new do
           Carto::Bolt.new('manolo_bolt_locked').run_locked {}
-          sleep(0.025)
+          sleep(0.25)
         end
         t.join
       end

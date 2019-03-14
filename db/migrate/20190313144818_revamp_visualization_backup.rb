b19784c97a9b03c51fde56f474dc5336d315b8e8
Simon Martín <oleurud@gmail.com>
2019-03-14 16:30:16 +0100

    changing migration removing table

--- db/migrate/20190313144818_revamp_visualization_backup.rb
+++ db/migrate/20190313144818_revamp_visualization_backup.rb
@@ -4,23 +4,14 @@ include Carto::Db::MigrationHelper
 
 migration(
   Proc.new do
-    run "TRUNCATE TABLE visualization_backups"
-
-    run "ALTER TABLE visualization_backups DROP CONSTRAINT visualization_backups_pkey"
-    drop_column :visualization_backups, :export_vizjson
-
-    add_column :visualization_backups, :id, :uuid, default: Sequel.lit('uuid_generate_v4()'), null: false
-    add_column :visualization_backups, :export, JSON, null: false
-    add_column :visualization_backups, :type, String, null: false
-    run "ALTER TABLE visualization_backups ADD PRIMARY KEY (id)"
+    drop_table :visualization_backups
   end,
   Proc.new do
-    run "ALTER TABLE visualization_backups DROP CONSTRAINT visualization_backups_pkey"
-    drop_column :visualization_backups, :id
-    drop_column :visualization_backups, :export
-    drop_column :visualization_backups, :type
-
-    add_column :visualization_backups, :export_vizjson, String, null: false
-    run "ALTER TABLE visualization_backups ADD PRIMARY KEY (visualization)"
+    create_table :visualization_backups do
+      String    :username,        null: false
+      Uuid      :visualization,   null: false, primary_key: true
+      String    :export_vizjson,  null: false
+      DateTime  :created_at,      default: Sequel::CURRENT_TIMESTAMP
+    end
   end
 )

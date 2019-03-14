9fed05bd6c4794e504c571758570a30c272573f3
Simon Martín <oleurud@gmail.com>
2019-03-14 13:35:15 +0100

    new FF

--- app/models/carto/visualization_backup.rb
+++ app/models/carto/visualization_backup.rb
@@ -11,15 +11,17 @@ module Carto
     # @param JSON export
     # @param DateTime created_at (Self-generated)
 
+    FEATURE_FLAG_NAME = "visualizations_backup_2"
+
     TYPE_VISUALIZATION = 'visualization'.freeze
     TYPE_LAYER = 'layer'.freeze
 
     VALID_TYPES = [TYPE_VISUALIZATION, TYPE_LAYER].freeze
 
     # Allow mass-setting upon .new
-    attr_accessible :username, :visualization, :export_vizjson
+    attr_accessible :username, :visualization, :export
 
-    validates :username, :visualization, :export_vizjson, presence: true
+    validates :username, :visualization, :export, presence: true
 
   end
 end

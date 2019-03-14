e8f6f58549f5e7b8b091a16c18efe024dc2016a6
Simon Martín <oleurud@gmail.com>
2019-03-14 13:39:25 +0100

    adding type col

--- db/migrate/20190313144818_revamp_visualization_backup.rb
+++ db/migrate/20190313144818_revamp_visualization_backup.rb
@@ -11,12 +11,14 @@ migration(
 
     add_column :visualization_backups, :id, :uuid, default: Sequel.lit('uuid_generate_v4()'), null: false
     add_column :visualization_backups, :export, JSON, null: false
+    add_column :visualization_backups, :type, String, null: false
     run "ALTER TABLE visualization_backups ADD PRIMARY KEY (id)"
   end,
   Proc.new do
     run "ALTER TABLE visualization_backups DROP CONSTRAINT visualization_backups_pkey"
     drop_column :visualization_backups, :id
     drop_column :visualization_backups, :export
+    drop_column :visualization_backups, :type
 
     add_column :visualization_backups, :export_vizjson, String, null: false
     run "ALTER TABLE visualization_backups ADD PRIMARY KEY (visualization)"

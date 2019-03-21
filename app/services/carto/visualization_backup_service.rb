55e6a1d920643e4740863f152d9496709f56d3da
Simon Martín <oleurud@gmail.com>
2019-03-21 16:12:46 +0100

    create_visualization_backup

--- app/services/carto/visualization_backup_service.rb
+++ app/services/carto/visualization_backup_service.rb
@@ -3,7 +3,7 @@ require_dependency 'carto/visualizations_export_service_2'
 module Carto
   module VisualizationBackupService
     include Carto::VisualizationsExportService2Exporter
-    # include Carto::VisualizationsExportService2Importer
+    include Carto::VisualizationsExportService2Importer
 
     def create_visualization_backup(visualization_id, user, category, with_mapcaps: true, with_password: true)
       export_json = export_visualization_json_hash(
@@ -21,22 +21,24 @@ module Carto
     rescue StandardError => exception
       # The backup should not break the flow
       CartoDB::Logger.error(
-        message: 'Error backing up visualization',
+        message: 'Error creating a visualization backup',
         exception: exception,
         visualization_id: visualization_id
       )
     end
 
-    # def restore_backup(visualization_backup_id)
-    #     visualization_backup = Carto::VisualizationBackup.find(visualization_backup_id)
-    #     return false if visualization_backup.nil?
-
-    #     user = Carto::User.find(visualization_backup.user_id)
-    #     return false if user.nil?
-
-    #     visualization_json = build_visualization_from_json_export(JSON.dump(visualization_backup.export))
-
-    #     Carto::VisualizationsExportPersistenceService.new.save_import(user, visualization_json)
-    # end
+    def restore_visualization_backup(visualization_backup_id)
+      visualization_backup = Carto::VisualizationBackup.find(visualization_backup_id)
+      user = Carto::User.find(visualization_backup.user_id)
+      visualization_json = build_visualization_from_json_export(JSON.dump(visualization_backup.export))
+      Carto::VisualizationsExportPersistenceService.new.save_import(user, visualization_json, full_restore:true)
+    rescue StandardError => exception
+      # The backup should not break the flow
+      CartoDB::Logger.error(
+        message: 'Error restoring a visualization backup',
+        exception: exception,
+        visualization_backup_id: visualization_backup_id
+      )
+    end
   end
 end

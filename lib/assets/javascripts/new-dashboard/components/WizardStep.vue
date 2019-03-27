0590c23292ee5311d12218d518ffadc4b851a009
csubira <cillas@cartodb.com>
2019-03-27 13:11:49 +0100

    Draft for wizard structure

---
+++ lib/assets/javascripts/new-dashboard/components/WizardStep.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wizard-step">
+    <h2>{{title}}</h2>
+    <h2>{{subTitle}}</h2>
+     <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WizardStep',
+  props: {
+    title: String,
+    subTitle: String,
+  },
+}
+</script>
+<style scoped lang="scss">
+@import 'new-dashboard/styles/variables';
+
+</style>
---
+++ lib/assets/javascripts/new-dashboard/pages/TestWizard.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="page">
+    <Wizard :step="2"></Wizard>
+  </section>
+</template>
+
+<script>
+
+import Wizard from 'new-dashboard/components/Wizard.vue';
+import WizardStep from 'new-dashboard/components/WizardStep.vue';
+
+export default {
+  name: 'TestWizard',
+  components: {
+    Wizard,
+    WizardStep
+  }
+};
+</script>

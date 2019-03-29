51f67758526506ad2e4628f6bb12ed0dd399b39f
csubira <cillas@cartodb.com>
2019-03-29 15:18:30 +0100

    Undo remove route

--- config/routes.rb
+++ config/routes.rb
@@ -280,6 +280,7 @@ CartoDB::Application.routes.draw do
     get '(/user/:user_domain)(/u/:user_domain)/dashboard/notifications/'    => 'visualizations#index', as: :notifications
     get '(/user/:user_domain)(/u/:user_domain)/dashboard/notifications'     => 'visualizations#index', as: :notifications_bis
     get '(/user/:user_domain)(/u/:user_domain)/dashboard/solutions/'        => 'visualizations#index', as: :solutions
+    get '(/user/:user_domain)(/u/:user_domain)/dashboard/solutions'         => 'visualizations#index', as: :solutions_bis
 
     # Public dashboard
     # root also goes to 'pages#public', as: public_visualizations_home

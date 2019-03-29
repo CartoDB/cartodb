9b236121ba9e5860eaede2b3f94b7f8eaaaeca53
csubira <cillas@cartodb.com>
2019-03-29 10:39:17 +0100

    Add new codeblock component

---
+++ lib/assets/javascripts/new-dashboard/components/CodeBlock.vue
@@ -0,0 +1,39 @@
+<template>
+  <textarea ref="code" v-model="code"></textarea>
+</template>
+
+<script>
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+export default {
+  name: 'CodeBlock',
+  mounted () {
+    this.initialize();
+  },
+  props: {
+    code: {
+      type: String
+    }
+  },
+  methods: {
+    initialize () {
+       CodeMirror.fromTextArea(this.$refs.code, defaultOptions);
+    }
+  }
+}
+
+const defaultOptions = {
+  mode: 'javascript',
+  theme: 'material',
+  readOnly: true,
+  lineNumbers: true
+};
+
+</script>
+<style scoped lang="scss">
+@import 'new-dashboard/styles/variables';
+
+</style>

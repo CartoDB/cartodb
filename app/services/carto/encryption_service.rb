abc438f2b30d0ce79a7794114568d3b7d8f953f1
Gonzalo Riestra <gonzalor@cartodb.com>
2019-04-08 15:40:11 +0200

    refactor default digest keys

--- app/services/carto/encryption_service.rb
+++ app/services/carto/encryption_service.rb
@@ -1,9 +1,12 @@
 module Carto
   class EncryptionService
 
-    AUTH_DIGEST_KEY_SHA1 = '47f940ec20a0993b5e9e4310461cc8a6a7fb84e3'.freeze
-    AUTH_DIGEST_KEY_SHA256 = '1211b3e77138f6e1724721f1ab740c9c70e66ba6fec5e989bb6640c4541ed15d06dbd5fdcbd3052b'.freeze
     AUTH_TOKEN_DIGEST_KEY = '6da98b2da1b38c5ada2547ad2c3268caa1eb58dc20c9144ead844a2eda1917067a06dcb54833ba2'.freeze
+
+    DEFAULT_AUTH_DIGEST_KEYS = {
+      Digest::SHA1 => '47f940ec20a0993b5e9e4310461cc8a6a7fb84e3',
+      Digest::SHA256 => '1211b3e77138f6e1724721f1ab740c9c70e66ba6fec5e989bb6640c4541ed15d06dbd5fdcbd3052b'
+    }.freeze
 
     DEFAULT_SHA_CLASS = Digest::SHA1
 
@@ -56,7 +59,7 @@ module Carto
     end
 
     def sha_digest(sha_class: DEFAULT_SHA_CLASS, initial_digest: nil, digest_key: nil, args: [])
-      digest_key ||= default_digest_key(sha_class)
+      digest_key ||= DEFAULT_AUTH_DIGEST_KEYS[sha_class]
       digest = initial_digest || digest_key
       args_join = '--' if sha_class == Digest::SHA1
 
@@ -65,13 +68,6 @@ module Carto
         digest = sha_class.hexdigest(joined_args)
       end
       digest
-    end
-
-    def default_digest_key(sha_class)
-      case sha_class.to_s
-      when "Digest::SHA1" then AUTH_DIGEST_KEY_SHA1
-      when "Digest::SHA256" then AUTH_DIGEST_KEY_SHA256
-      end
     end
 
   end

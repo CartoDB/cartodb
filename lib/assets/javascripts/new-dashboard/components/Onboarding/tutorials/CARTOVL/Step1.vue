41d5b3f3a6824892ed64b4cd338cfd9526f686af
jesusbotella <jesus.botella@gmail.com>
2019-04-10 18:38:53 +0200

    Almost ready CARTO-VL tutorial

--- lib/assets/javascripts/new-dashboard/components/Onboarding/tutorials/CARTOVL/Step1.vue
+++ lib/assets/javascripts/new-dashboard/components/Onboarding/tutorials/CARTOVL/Step1.vue
@@ -5,9 +5,12 @@
           <img svg-inline src="../../../../assets/icons/onboarding/summary.svg"/>
       </template>
     </StepTitle>
+
     <InjectableIframe :content="map" width="940" height="416"></InjectableIframe>
-    <p v-html="$t(`Wizards.cartovl.step1.text1`)" class="text is-caption u-mb--16 u-mt--24"></p>
-    <p v-html="$t(`Wizards.cartovl.step1.text2`)" class="text is-caption u-mb--64"></p>
+
+    <p v-html="$t(`Wizards.cartovl.step1.text1`)" class="text is-body u-mb--16 u-mt--24"></p>
+    <p v-html="$t(`Wizards.cartovl.step1.text2`)" class="text is-body u-mb--16 u-mt--24"></p>
+    <p v-html="$t(`Wizards.cartovl.step1.text3`)" class="text is-body u-mb--64"></p>
   </div>
 </template>
 

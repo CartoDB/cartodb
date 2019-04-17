65fc79be88ad6ca46aa90b4def1beb2ce043b5a1
csubira <cillas@cartodb.com>
2019-04-17 14:15:34 +0200

    Add not-visited badge to onboarding button

--- lib/assets/javascripts/new-dashboard/components/Onboarding/OnboardingButton.vue
+++ lib/assets/javascripts/new-dashboard/components/Onboarding/OnboardingButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="button"
-    :class="[isFirstTimeViewingDashboard ? 'button--cta' : 'button--ghost']"
+    :class="[{'not-visited': !(hasVisitedOnboarding || isFirstTimeViewingDashboard)}, isFirstTimeViewingDashboard ? 'button--cta' : 'button--ghost']"
     @click="openOnboarding()">
     {{ $t('Wizards.Distributor.cta') }}
   </button>
@@ -16,10 +16,48 @@ export default {
       default: false
     }
   },
+  data () {
+    return {
+      hasVisitedOnboarding: false
+    }
+  },
+  mounted () {
+    this.hasVisitedOnboarding = this.setHasVisitedOnboarding();
+  },
   methods: {
     openOnboarding () {
+      this.updateHasVisitedOnboarding(true);
       this.$router.push({ name: 'onboarding' });
+    },
+    setHasVisitedOnboarding () {
+      if (localStorage.hasOwnProperty('hasVisitedOnboarding')) {
+        return JSON.parse(window.localStorage.getItem('hasVisitedOnboarding'));
+      } else {
+        return false
+      }
+    },
+    updateHasVisitedOnboarding (visited) {
+      this.hasVisitedOnboarding = visited;
+      localStorage.hasVisitedOnboarding = JSON.stringify(visited);
     }
   }
 };
 </script>
+<style scoped lang="scss">
+@import 'new-dashboard/styles/variables';
+
+button.button--ghost {
+  position: relative;
+
+  &.not-visited::after {
+    content: '';
+    position: absolute;
+    top: -4px;
+    right: -12px;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: $onboarding-notification;
+  }
+}
+</style>

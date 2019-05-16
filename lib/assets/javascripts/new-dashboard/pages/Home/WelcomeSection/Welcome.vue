7118bb50cd8bed2abf95f46454038d0c9511b514
alejandraarri <alejandraarri@carto.com>
2019-05-16 14:47:59 +0200

    Update account types

--- lib/assets/javascripts/new-dashboard/pages/Home/WelcomeSection/Welcome.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/WelcomeSection/Welcome.vue
@@ -76,7 +76,7 @@ export default {
       return freeUser.includes(this.user.account_type);
     },
     isProUser () {
-      const proUsers = ['professional-2019', 'professional-2019 LUMP-SUM'];
+      const proUsers = ['Professional', 'Annual Professional'];
       return proUsers.includes(this.user.account_type);
     },
     isOrganizationAdmin () {

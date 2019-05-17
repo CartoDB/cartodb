99b73a9334067ab3fd7b0fc65547a193d0382eb2
Gonzalo Riestra <gonzalor@cartodb.com>
2019-05-17 14:16:43 +0200

    update also user_creations

--- db/migrate/20190517114808_add_default_salt.rb
+++ db/migrate/20190517114808_add_default_salt.rb
@@ -7,9 +7,15 @@ migration(
     alter_table(:users) do
       set_column_default :salt, ''
     end
+    alter_table(:user_creations) do
+      set_column_default :salt, ''
+    end
   end,
   Proc.new do
     alter_table(:users) do
+      set_column_default :salt, nil
+    end
+    alter_table(:user_creations) do
       set_column_default :salt, nil
     end
   end

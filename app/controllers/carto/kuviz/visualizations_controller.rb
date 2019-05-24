f5bd60304adeb880fb72fac8ff93b048c54b9340
Mario de Frutos Dieguez <mario@defrutos.org>
2019-05-24 13:32:36 +0200

    Password protected kuvizs

--- app/controllers/carto/kuviz/visualizations_controller.rb
+++ app/controllers/carto/kuviz/visualizations_controller.rb
@@ -2,10 +2,16 @@ module Carto
   module Kuviz
     class VisualizationsController < ApplicationController
       def show
-        debugger
-        kuviz = Carto::Asset.find(params[:id])
-        @source = open(kuviz.public_url).read
+        kuviz_source = Carto::Asset.find(params[:id])
+        return public_map_protected if kuviz_source.visualization.password_protected?
+        @source = open(kuviz_source.public_url).read
         render :layout => false
+      end
+
+      private
+
+      def kuviz_password_protected
+        render 'public_map_password', :layout => 'application_password_layout'
       end
     end
   end

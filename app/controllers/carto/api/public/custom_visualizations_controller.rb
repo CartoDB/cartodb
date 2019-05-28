b4062f78d75ea44405291e7d7abdf9539a4f2011
Mario de Frutos Dieguez <mario@defrutos.org>
2019-05-28 16:03:17 +0200

    Added list kuviz endpoint

--- app/controllers/carto/api/public/custom_visualizations_controller.rb
+++ app/controllers/carto/api/public/custom_visualizations_controller.rb
@@ -1,12 +1,40 @@
+require_relative '../visualization_searcher'
+require_relative '../paged_searcher'
+
 class Carto::Api::Public::CustomVisualizationsController < Carto::Api::Public::ApplicationController
+  include Carto::Api::VisualizationSearcher
+  include Carto::Api::PagedSearcher
+
   CONTENT_LENGTH_LIMIT_IN_BYTES = 20000
+  VALID_ORDER_PARAMS = %i(name updated_at privacy).freeze
 
   ssl_required
 
   before_action :validate_input_data, only: [:create, :update]
 
   def index
-    head 501
+    offdatabase_orders = Carto::VisualizationQueryOrderer::SUPPORTED_OFFDATABASE_ORDERS.map(&:to_sym)
+    valid_order_combinations = VALID_ORDER_PARAMS - offdatabase_orders
+    opts = { valid_order_combinations: valid_order_combinations }
+    page, per_page, order, order_direction = page_per_page_order_params(VALID_ORDER_PARAMS, opts)
+    params[:type] = Carto::Visualization::TYPE_KUVIZ
+    vqb = query_builder_with_filter_from_hash(params)
+
+    visualizations = vqb.with_order(order, order_direction)
+                        .build_paged(page, per_page).map do |v|
+      asset = Carto::Asset.find_by_visualization_id(v.id)
+      Carto::Api::Public::KuvizPresenter.new(self, v.user, v, asset).to_hash
+    end
+    response = {
+      visualizations: visualizations,
+      total_entries: vqb.build.size
+    }
+    render_jsonp(response)
+  rescue Carto::ParamInvalidError, Carto::ParamCombinationInvalidError => e
+    render_jsonp({ error: e.message }, e.status)
+  rescue StandardError => e
+    CartoDB::Logger.error(exception: e)
+    render_jsonp({ error: e.message }, 500)
   end
 
   def create
@@ -17,6 +45,9 @@ class Carto::Api::Public::CustomVisualizationsController < Carto::Api::Public::A
     asset.save
 
     render_jsonp(Carto::Api::Public::KuvizPresenter.new(self, user, kuviz, asset).to_hash, 200)
+  rescue StandardError => e
+    CartoDB::Logger.error(exception: e)
+    render_jsonp({ error: 'cant create the kuviz' }, 500)
   end
 
   def update
--- app/controllers/carto/api/public/kuviz_presenter.rb
+++ app/controllers/carto/api/public/kuviz_presenter.rb
@@ -13,6 +13,10 @@ module Carto
         def to_hash
           {
             visualization: @kuviz.id,
+            name: @kuviz.name,
+            privacy: @kuviz.privacy,
+            created_at: @kuviz.created_at,
+            updated_at: @kuviz.updated_at,
             url: CartoDB.url(@context, 'kuviz_show',
                              params: { id: @kuviz.id },
                              user: @user)

7f1ca1f1c6731b8ef925f9531ab015fd80fdac64
csubira <cillas@cartodb.com>
2019-06-13 17:35:52 +0200

    Add dummy header to maintenance and lockout page

--- lib/assets/javascripts/new-dashboard/bundles/maintenance/MaintenanceApp.vue
+++ lib/assets/javascripts/new-dashboard/bundles/maintenance/MaintenanceApp.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <DummyHeader />
     <Maintenance />
     <Footer :user="user"/>
   </div>
@@ -7,12 +8,14 @@
 
 <script>
 import Maintenance from 'new-dashboard/pages/Maintenance';
+import DummyHeader from 'new-dashboard/components/DummyHeader';
 import Footer from 'new-dashboard/components/Footer';
 
 export default {
   name: 'MaintenanceApp',
   components: {
     Maintenance,
+    DummyHeader,
     Footer
   },
   computed: {

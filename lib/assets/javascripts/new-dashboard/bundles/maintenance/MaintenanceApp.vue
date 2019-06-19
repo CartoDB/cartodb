bc72f89017d2fc99dee23d2d8607d1ee6c3f6a18
csubira <cillas@cartodb.com>
2019-06-19 18:21:11 +0200

    Add dummy footer

--- lib/assets/javascripts/new-dashboard/bundles/maintenance/MaintenanceApp.vue
+++ lib/assets/javascripts/new-dashboard/bundles/maintenance/MaintenanceApp.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <DummyHeader />
     <Maintenance />
+    <DummyFooter />
   </div>
 </template>
 
 <script>
 import Maintenance from 'new-dashboard/pages/Maintenance';
 import DummyHeader from 'new-dashboard/components/DummyHeader';
+import DummyFooter from 'new-dashboard/components/DummyFooter';
 
 export default {
   name: 'MaintenanceApp',
   components: {
     Maintenance,
-    DummyHeader
+    DummyHeader,
+    DummyFooter
   }
 };
 </script>

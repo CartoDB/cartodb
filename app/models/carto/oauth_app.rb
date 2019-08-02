efe87c56d0093a018b6699d00a3b785936c69e71
Gonzalo Riestra <gonzalor@cartodb.com>
2019-08-02 13:22:07 +0200

    do not require icon_url in rails

--- app/models/carto/oauth_app.rb
+++ app/models/carto/oauth_app.rb
@@ -18,7 +18,7 @@ module Carto
     validates :client_id, presence: true
     validates :client_secret, presence: true
     validates :redirect_uris, presence: true
-    validates :icon_url, presence: true, url: true
+    validates :icon_url, url: true, if: -> { icon_url.present? }
     validates :oauth_app_organizations, absence: true, unless: :restricted?
     validate :validate_uris
 
@@ -28,8 +28,8 @@ module Carto
     after_update :update_central, if: :sync_with_central?
     after_destroy :delete_central, if: :sync_with_central?
 
-    ALLOWED_SYNC_ATTRIBUTES = %i[id name client_id client_secret redirect_uris
-      icon_url restricted description website_url].freeze
+    ALLOWED_SYNC_ATTRIBUTES = %i[id name client_id client_secret redirect_uris icon_url restricted description 
+                                 website_url].freeze
 
     attr_accessor :avoid_sync_central
 

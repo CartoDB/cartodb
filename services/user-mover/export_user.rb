80b2956ae29f528496b43325d714ae57c53ff0f3
Simon Martín <oleurud@gmail.com>
2019-10-28 10:04:56 +0100

    add schema to tigger functions

--- services/user-mover/export_user.rb
+++ services/user-mover/export_user.rb
@@ -481,11 +481,11 @@ module CartoDB
       end
 
       def disable_ghost_tables_event_trigger
-        pg_conn.exec("SELECT CDB_DisableGhostTablesTrigger()")
+        pg_conn.exec("SELECT cartodb.CDB_DisableGhostTablesTrigger()")
       end
 
       def enable_ghost_tables_event_trigger
-        pg_conn.exec("SELECT CDB_EnableGhostTablesTrigger()")
+        pg_conn.exec("SELECT cartodb.CDB_EnableGhostTablesTrigger()")
       end
 
       def initialize(options)

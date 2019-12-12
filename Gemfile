74205d2ab837846c601aa86239f6fccfe2789b88
Esther Lozano <esloho@gmail.com>
2019-12-12 11:21:04 +0100

    Add pubsub tracker

--- Gemfile
+++ Gemfile
@@ -58,11 +58,14 @@ gem 'state_machines-activerecord', '~> 0.5.0'
 gem 'typhoeus',                '0.7.2'
 gem 'charlock_holmes',         '0.7.6'
 gem 'dbf',                     '2.0.6'
-gem 'faraday',                 '0.9.0'
-gem 'google-api-client',       '0.28.4'
-gem 'dropbox_api',             '0.1.6'
-gem 'instagram',               '1.1.6'
+gem 'faraday',                 '0.11.0'
+gem 'google-api-client',       '0.32.1'
+gem 'dropbox_api',             '0.1.17'
+gem 'instagram-continued',     '1.4.0'
 gem 'gibbon',                  '1.1.4'
+
+# GCloud
+gem 'google-cloud-pubsub', '0.27.2'
 
 # Service components (/services)
 gem 'virtus',                   '1.0.5'
@@ -79,6 +82,7 @@ gem 'resque',                '1.25.2'
 gem 'resque-metrics',        '0.1.1'
 
 gem 'net-telnet'
+
 
 # This is weird. In ruby 2 test-unit is required. We don't know why for sure
 gem 'test-unit'
@@ -118,6 +122,7 @@ group :development, :test do
   gem 'rspec-rails',           '2.12.0'
   gem 'rb-readline'
   gem 'byebug'
+  gem 'pry-byebug',            '3.3.0'
   gem 'rack'
   gem 'zeus'
 
---
+++ lib/carto/tracking/services/pubsub.rb
@@ -0,0 +1,89 @@
+require 'singleton'
+require 'google/cloud/pubsub'
+
+module Carto
+  module Tracking
+    module Services
+      class PubSubTracker
+        include Singleton
+
+        MAX_BYTES_BATCH = 500_000
+        MAX_MESSAGES_BATCH = 5
+
+        def initialize
+          @topic_name = Cartodb.get_config(:pubsub, 'topic')
+          @pubsub = init_pubsub
+          @errored = false
+
+          @topics = {
+            metrics: metrics_topic
+          }
+
+        rescue StandardError => e
+          @errored = true
+          CartoDB::Logger.error(message: 'PubSubTracker: initialization error', exception: e)
+        end
+
+        def init_pubsub
+          project_id = Cartodb.get_config(:pubsub, 'project_id')
+          credentials = Cartodb.get_config(:pubsub, 'credentials')
+
+          return Google::Cloud::Pubsub.new unless project_id.present?
+
+          return Google::Cloud::Pubsub.new(project_id: project_id) unless credentials.present?
+
+          @pubsub = Google::Cloud::Pubsub.new(project_id: project_id, credentials: credentials)
+        end
+
+        def metrics_topic
+          @pubsub.topic(@topic_name, async: {
+            max_bytes: MAX_BYTES_BATCH,
+            max_messages: MAX_MESSAGES_BATCH
+          })
+        end
+
+        def enabled?
+          @pubsub.present? && !@errored
+        end
+
+        def send_event(topic_key, user_id, event, properties = {})
+          return unless user_id.present? && enabled?
+
+          topic = @topics.fetch(topic_key)
+          attributes = {user_id: user_id}.merge(properties)
+
+          topic.publish_async(event, attributes) do |result|
+            raise "PubSubTracker: error publishing to topic #{@topic_name} for event #{event}" unless result&.succeeded?
+          end
+
+          attributes
+
+        rescue KeyError => e
+          CartoDB::Logger.error(message: "Error:  #{topic_key} does not exist")
+        rescue StandardError => e
+          CartoDB::Logger.error(message: e.message, exception: e)
+        end
+
+        def graceful_shutdown
+          return unless enabled?
+
+          @topics.each_value do |topic|
+            begin
+              stop_publisher(topic) unless topic.async_publisher.stopped?
+            rescue StandardError => e
+              CartoDB::Logger.error(message: e.message, exeption: e)
+            end
+          end
+        rescue StandardError => e
+          CartoDB::Logger.error(message: e.message, exeption: e)
+        end
+
+        # Method needed for testing purposes
+        def stop_publisher(topic)
+          topic.async_publisher.stop.wait!
+        end
+
+      end
+    end
+  end
+end

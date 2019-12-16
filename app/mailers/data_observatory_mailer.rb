20eca4d299740184dacbb9ad24f708cdc68835f0
Gonzalo Riestra <gonzalor@cartodb.com>
2019-12-16 13:35:22 +0100

    new mailer and mail templates

---
+++ app/mailers/data_observatory_mailer.rb
@@ -0,0 +1,25 @@
+class DataObservatoryMailer < ActionMailer::Base
+
+  CARTO_REQUEST_RECIPIENT = 'dataobservatory@carto.com'.freeze
+
+  default from: Cartodb.get_config(:mailer, 'from')
+  layout 'mail'
+
+  def user_request(user, dataset_id)
+    subject = 'Your Data Observatory request'
+    @user_name = user.name
+    @dataset_id = dataset_id
+
+    mail to: user.email, subject: subject
+  end
+
+  def carto_request(user, dataset_id, delivery_days)
+    subject = 'Data Observatory request from CARTOframes'
+    @user_name = user.name
+    @user_email = user.email
+    @dataset_id = dataset_id
+    @delivery_days = delivery_days
+
+    mail to: CARTO_REQUEST_RECIPIENT, subject: subject
+  end
+end

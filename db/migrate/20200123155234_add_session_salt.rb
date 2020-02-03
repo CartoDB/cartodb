fe958c0c39386340a0b1586ef9d75e5ca19ac57b
Esther Lozano <esloho@gmail.com>
2020-02-03 13:08:55 +0100

    Add session_salt to user_creations

--- db/migrate/20200123155234_add_session_salt.rb
+++ db/migrate/20200123155234_add_session_salt.rb
@@ -5,8 +5,10 @@ include Carto::Db::MigrationHelper
 migration(
   Proc.new do
     add_column :users, :session_salt, String
+    add_column :user_creations, :session_salt, String
   end,
   Proc.new do
     drop_column :users, :session_salt
+    drop_column :user_creations, :session_salt
   end
 )

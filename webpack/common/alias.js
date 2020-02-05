8a0391e7c0425408ce0c7b8fa6547bf9f8e8a3f6
elenatorro <elenatorro@gmail.com>
2020-02-05 12:40:31 +0100

    Remove jquery-ui when not necessary

--- webpack/common/alias.js
+++ webpack/common/alias.js
@@ -5,7 +5,6 @@ module.exports = {
   'stylesheets': resolve(resolve('.'), 'assets/stylesheets'),
   'backbone/core-view$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/backbone/core-view.js'),
   'backbone/core-model$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/backbone/core-model.js'),
-  // 'jquery-ui$': resolve(resolve('.'), 'lib/assets/javascripts/vendor/jquery-ui.js'),
   'backbone': resolve(resolve('.'), 'node_modules/backbone'), // Avoid two different `backbone` modules when linking `cartodb.js`  'fixtures/dashboard'
   'vue$': 'vue/dist/vue.esm.js'
 };

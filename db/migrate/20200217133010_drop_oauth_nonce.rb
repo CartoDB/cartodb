3416c96dc1e06d60337039ac60ffa7a06ca310fd
Gonzalo Riestra <gonzalor@cartodb.com>
2020-02-17 19:16:52 +0100

    fix migrations

--- db/migrate/20200217133010_drop_oauth_nonce.rb
+++ db/migrate/20200217133010_drop_oauth_nonce.rb
@@ -1,16 +1,19 @@
-Sequel.migration do
-  up do
-    drop_table :oauth_nonces
-  end
+require 'carto/db/migration_helper'
 
-  down do
+include Carto::Db::MigrationHelper
+
+migration(
+  Proc.new do
+    drop_table :oauth_nonces
+  end,
+  Proc.new do
     create_table :oauth_nonces do
       Uuid :id, primary_key: true
       String :nonce
       Integer :timestamp
-      DateTime :created_at, :null => false
-      DateTime :updated_at, :null => false
-      index [:nonce, :timestamp], :unique => true
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
+      index [:nonce, :timestamp], unique: true
     end
   end
-end
+)

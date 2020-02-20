cb1ae4315ef6c21d99b6d9c09da0bd064a4a287c
ibrahim <ibrahimmenem@gmail.com>
2020-02-20 13:11:50 +0100

    Revert "check that canceling still working"
    This reverts commit cae99cbb58a1befb8729e8fd54d31c0593964f1d.

--- config.rb
+++ config.rb
@@ -1,7 +1,6 @@
 # Compass configuration, used by cartodb UI grunt task.
 # Require any additional compass plugins here.
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "dist/css"

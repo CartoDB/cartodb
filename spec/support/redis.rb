7b28f658a0660ee68c58ef370de540e6402bf136
Esther Lozano <esloho@gmail.com>
2020-03-11 18:45:15 +0100

    Use get_config method

--- spec/support/redis.rb
+++ spec/support/redis.rb
@@ -51,7 +51,7 @@ module CartoDB
         new_logfile = "/tmp/redis-#{ENV['REDIS_PORT']}/stdout"
         Dir.mkdir "/tmp/redis-#{ENV['REDIS_PORT']}" unless File.exists?("/tmp/redis-#{ENV['REDIS_PORT']}")
       else
-        port = Cartodb.config[:redis]["port"]
+        port = Cartodb.get_config(:redis, 'port')
       end
       print "[redis] Starting test server on port #{port}... "
 

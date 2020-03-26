cf0026567ca8510b45102d8fde40cc86a2ac3796
antoniocarlon <antoniocarlon@gmail.com>
2020-03-26 10:14:38 +0100

    Better qualified_table_name

--- app/models/carto/analysis.rb
+++ app/models/carto/analysis.rb
@@ -42,15 +42,7 @@ class Carto::Analysis < ActiveRecord::Base
     username = layer_options[:user_name] || layer.user.username
     table_name = layer_options[:table_name]
 
-    qualified_table_name = if layer.user.organization_user?
-                             safe_schema_and_table_quoting(username, table_name)
-                           else
-                             if table_name.present?
-                               safe_schema_and_table_quoting('public', table_name)
-                             else
-                               table_name
-                             end
-                           end
+    qualified_table_name = safe_schema_and_table_quoting(layer.user.database_schema, table_name)
 
     analysis_definition = {
       id: 'abcdefghijklmnopqrstuvwxyz'[index] + '0',

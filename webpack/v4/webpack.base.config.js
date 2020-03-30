722988ef8e796f7a1b83dc9c4ad650caf914ed6b
jesusbotella <jesus.botella@gmail.com>
2020-03-30 19:01:01 +0200

    Split and Lazy loading for routes, and webpack optimizations

--- webpack/v4/webpack.base.config.js
+++ webpack/v4/webpack.base.config.js
@@ -18,7 +18,8 @@ module.exports = {
   entry: entryPoints,
   output: {
     filename: `${version}/javascripts/[name].js`,
-    path: rootDir('public/assets')
+    path: rootDir('public/assets'),
+    publicPath: http_path_prefix + '/assets/'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json', '.scss'],
@@ -28,6 +29,8 @@ module.exports = {
   },
   devtool: 'source-map',
   plugins: [
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en/),
+
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',

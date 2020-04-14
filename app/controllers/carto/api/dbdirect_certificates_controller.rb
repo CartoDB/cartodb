7992d1fb19b156797d308a95e171da6802c852fc
Javier Goizueta <jgoizueta@gmail.com>
2020-04-14 09:52:26 +0200

    Update app/controllers/carto/api/dbdirect_certificates_controller.rb
    Co-Authored-By: Gonzalo Riestra <gonzalor@cartodb.com>

--- app/controllers/carto/api/dbdirect_certificates_controller.rb
+++ app/controllers/carto/api/dbdirect_certificates_controller.rb
@@ -62,8 +62,8 @@ module Carto
         # TODO: should the user be an organization owner?
         api_key = Carto::ApiKey.find_by_token(params["api_key"])
         if api_key.present?
-          raise UnauthorizedError unless api_key&.master?
-          raise UnauthorizedError unless api_key.user_id === @user.id
+          raise UnauthorizedError unless api_key.master?
+          raise UnauthorizedError unless api_key.user_id == @user.id
         end
         unless @user.has_feature_flag?('dbdirect')
           raise UnauthorizedError.new("DBDirect not enabled for user #{@user.username}")

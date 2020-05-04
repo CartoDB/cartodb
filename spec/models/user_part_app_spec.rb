8db59c6c6d8c667bb6b1c9fee3abc7af6d369c09
Alberto Miedes Garcés <albertomg994@gmail.com>
2020-05-04 12:19:23 +0200

    Improve logging when salt change fails

--- spec/models/user_part_app_spec.rb
+++ spec/models/user_part_app_spec.rb
@@ -335,18 +335,55 @@ describe User do
         Cartodb::Central.any_instance.unstub(:send_request)
       end
 
-      it 'updates the session_salt' do
-        initial_session_salt = @user.session_salt
+      context 'when everything succeeds' do
+        it 'updates the session_salt' do
+          initial_session_salt = @user.session_salt
 
-        @user.invalidate_all_sessions!
+          @user.invalidate_all_sessions!
 
-        initial_session_salt.should_not == @user.reload.session_salt
+          initial_session_salt.should_not == @user.reload.session_salt
+        end
+
+        it 'updates the user in Central' do
+          Cartodb::Central.any_instance.expects(:send_request).once
+
+          @user.invalidate_all_sessions!
+        end
       end
 
-      it 'updates the user in Central' do
-        Cartodb::Central.any_instance.expects(:send_request).once
+      context 'when syncing to Central fails' do
+        context 'due to a newtork error' do
+          before do
+            User.any_instance.stubs(:update_in_central).raises(CartoDB::CentralCommunicationFailure, 'Error')
+          end
 
-        @user.invalidate_all_sessions!
+          it 'logs an error' do
+            CartoDB::Logger.expects(:error).once
+
+            @user.invalidate_all_sessions!
+          end
+        end
+
+        context 'due to anything else' do
+          before do
+            User.any_instance.stubs(:update_in_central).returns(false)
+          end
+
+          it 'logs an error' do
+            CartoDB::Logger.expects(:error).once
+
+            @user.invalidate_all_sessions!
+          end
+        end
+      end
+
+      context 'when saving in local fails' do
+        it 'logs an error' do
+          @user.email = nil
+          CartoDB::Logger.expects(:error).once
+
+          @user.invalidate_all_sessions!
+        end
       end
     end
   end

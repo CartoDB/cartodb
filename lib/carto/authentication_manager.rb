b2301e79160b7be96947d4772cb154fd99ad2aba
Alberto Miedes Garcés <albertomg994@gmail.com>
2020-05-05 12:58:15 +0200

    Readability improvements

--- lib/carto/authentication_manager.rb
+++ lib/carto/authentication_manager.rb
@@ -2,21 +2,19 @@ module Carto
   class AuthenticationManager
 
     def self.validate_session(warden_context, request, user, reset_session_on_error = true)
-      if session_security_token_valid?(warden_context, user)
-        true
-      else
-        request.reset_session if reset_session_on_error
-        false
-      end
+      return true if session_security_token_valid?(warden_context, user)
+
+      request.reset_session if reset_session_on_error
+      false
     end
 
     def self.session_security_token_valid?(warden_context, user)
       session = warden_context.session(user.username)
 
       return false unless session.key?(:sec_token)
+      return true if session[:sec_token] == user.security_token
 
-      raise Carto::ExpiredSessionError if session[:sec_token] != user.security_token
-      true
+      raise Carto::ExpiredSessionError
     rescue Warden::NotAuthenticated
       false
     end

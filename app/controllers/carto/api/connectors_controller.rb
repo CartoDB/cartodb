cefcb18e8745995c6bcb4cd7b58dc4a91c8152e8
Javier Goizueta <jgoizueta@gmail.com>
2020-05-29 17:26:12 +0200

    Fix typo

--- app/controllers/carto/api/connectors_controller.rb
+++ app/controllers/carto/api/connectors_controller.rb
@@ -118,7 +118,7 @@ module Carto
         rescue Carto::Connector::InvalidParametersError => e
           render_jsonp({ errors: e.message }, 422)
         rescue => e
-          render_jsonp({ errors: "Error connecting to provider #{provider_id}, #{e}" }, 400)
+          render_jsonp({ errors: "Error connecting to provider #{provider_id}: #{e}" }, 400)
         end
       end
 

844acb2321af1548c0c59ebedcd71308a4371613
Javier Goizueta <jgoizueta@gmail.com>
2020-06-28 18:58:00 +0200

    Change DO sync status

--- app/services/carto/do_sync_service.rb
+++ app/services/carto/do_sync_service.rb
@@ -1,7 +1,24 @@
 module Carto
   class DoSyncService
-    DO_SYNC_PROVIDER = 'do'.freeze
+    DO_SYNC_PROVIDER = 'do-v2'.freeze
     DO_SYNC_INTERVAL = 86400
+
+    # Sync Attributes
+    DO_SYNC_STATUS              = 'sync_status'.freeze
+    DO_SYNC_UNSYNCABLE_REASON   = 'unsyncable_reason'.freeze
+    DO_SYNC_UNSYNCED_ERRORS     = 'unsynced_errors'.freeze
+    DO_SYNC_SYNCED_WARNINGS     = 'synced_warnings'.freeze
+    DO_SYNC_TABLE               = 'sync_table'.freeze
+    DO_SYNC_TABLE_ID            = 'sync_table_id'.freeze
+    DO_SYNC_SYNCHRONIZATION_ID  = 'synchronization_id'.freeze
+    DO_SYNC_ESTIMATED_SIZE      = 'estimated_size'.freeze
+    DO_SYNC_ESTIMATED_ROW_COUNT = 'estimated_row_count'.freeze
+
+    # Values of DO_SYNC_STATUS attribute
+    DO_SYNC_STATUS_UNSYNCABLE   = 'unsyncable'.freeze
+    DO_SYNC_STATUS_UNSYNCED     = 'unsynced'.freeze
+    DO_SYNC_STATUS_SYNCING      = 'syncing'.freeze
+    DO_SYNC_STATUS_SYNCED       = 'synced'.freeze
 
     def initialize(user)
       @user = user
@@ -9,28 +26,38 @@ module Carto
 
     # subscription -> existing sync; returns nil for invalid subscription or sync not created
     def sync(subscription_id)
+      sync_info = {
+        DO_SYNC_STATUS => DO_SYNC_STATUS_UNSYNCED
+      }
+      # TODO: define result for non-existing/expired subscription: nil? {}? {sync_status: 'unsyncable',...}
+      # TODO: obtain estimated_size, estimated_row_count
+      # TODO: check size, rows & columns limits, set state to DO_SYNC_STATUS_UNSYNCABLE and DO_SYNC_UNSYNCABLE_REASON properly
       condition = %{
           service_name = 'connector'
           AND service_item_id::jsonb @> '{"provider":"#{DO_SYNC_PROVIDER}","subscription_id":"#{subscription_id}"}'::jsonb
+          AND EXISTS (SELECT id FROM synchronizations WHERE synchronizations.id = data_imports.synchronization_id)
       }
       data_import = Carto::DataImport.where(user_id: @user.id).where(condition).order('created_at desc').first
       if data_import
-        state = case data_import.state
-        when DataImport::STATE_ENQUEUED,  DataImport::STATE_PENDING, DataImport::STATE_UNPACKING,
-          DataImport::STATE_IMPORTING, DataImport::STATE_UPLOADING
-          'connecting'
+        case data_import.state
+        when Carto::DataImport::STATE_ENQUEUED,  Carto::DataImport::STATE_PENDING, Carto::DataImport::STATE_UNPACKING,
+          Carto::DataImport::STATE_IMPORTING, Carto::DataImport::STATE_UPLOADING
+          sync_info[DO_SYNC_STATUS] = DO_SYNC_STATUS_SYNCING
         when DataImport::STATE_COMPLETE
-          'connected'
+          sync_info[DO_SYNC_STATUS] = DO_SYNC_STATUS_SYNCED
+          sync_info[DO_SYNC_TABLE] = data_import.table_name
+          sync_info[DO_SYNC_TABLE_ID] = data_import.table_id
+          sync_info[DO_SYNC_SYNCHRONIZATION_ID] = data_import.synchronization_id
+          # TODO: data_import.id too?
+          # TODO: we could add DO_SYNC_SYNCED_WARNINGS if synchronization has failed
         else
-          'error'
+          sync_info[DO_SYNC_STATUS] = DO_SYNC_STATUS_UNSYNCED
+          sync_info[DO_SYNC_UNSYNCED_ERRORS] = [data_import.error_code]
+          # TODO: proper error message
+          # but note that data_import.get_error_text is intended for UI imports and not approriate here
         end
-        {
-          sync_table: data_import.table_name, # empty while connecting
-          sync_status: state,
-          synchronization_id: data_import.synchronization_id,
-          sync_table_id: data_import.table_id
-        }
       end
+      sync_info.with_indifferent_access
     end
 
     # sync table name --> subscription_id; returns nil for non-subscription-sync table
@@ -50,8 +77,12 @@ module Carto
 
     # create sync for subscription if it does not exist, or return existing sync
     def create_sync(subscription_id, force=false)
+      # TODO: tracking
+
       sync_data = sync(subscription_id)
-      if sync_data.blank? || (force && sync_data[:sync_status] == 'error')
+      return sync_data unless sync_data[DO_SYNC_STATUS] == DO_SYNC_STATUS_UNSYNCED
+
+      if force || sync_data[DO_SYNC_UNSYNCED_ERRORS].blank?
         create_new_sync_for_subscription! subscription_id
         sync_data = sync(subscription_id)
       end
@@ -60,11 +91,15 @@ module Carto
 
     # stop sync'ing a subscription
     def remove_sync(subscription_id)
+      # TODO: tracking, e.g. Carto::Tracking::Events::DeletedDataset.new(@user.id, ...).report
+
       sync_data = sync(subscription_id)
-      if sync_data.present? && sync_data[:sync_status] != 'error' && sync_data[:synchronization_id].present?
-        raise "Cannot remove sync while connecting" if sync_data[:sync_status] == 'connecting'
-        # FIXME: should we also check the state of the synchronization?
-        synchronization = CartoDB::Synchronization::Member.new(id: sync_data[:synchronization_id]).fetch
+      raise "Cannot remove sync while syncing" if sync_data[DO_SYNC_STATUS] == DO_SYNC_STATUS_SYNCING
+      # FIXME: should we also check the state of the synchronization? what if it's being synchronized?
+
+      if sync_data[DO_SYNC_STATUS] == DO_SYNC_STATUS_SYNCED
+        # Stop the synchronization
+        synchronization = CartoDB::Synchronization::Member.new(id: sync_data[DO_SYNC_SYNCHRONIZATION_ID]).fetch
         synchronization.delete
       end
     end

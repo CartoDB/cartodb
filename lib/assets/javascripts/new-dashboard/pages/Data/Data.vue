e33556c6afc3eb01902fc518148a5346ff50b302
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2020-07-13 12:01:29 +0200

    Enable subscriptions UI

--- lib/assets/javascripts/new-dashboard/pages/Data/Data.vue
+++ lib/assets/javascripts/new-dashboard/pages/Data/Data.vue
@@ -5,9 +5,9 @@
         <router-link :to="{ name: 'datasets' }" class="tabs__item title is-small" exact active-class="is-active" :class="{'is-active': isDatasetPage }">
           <span>{{ $t('DataPage.tabs.yourDatasets') }}</span>
         </router-link>
-        <!-- <router-link :to="{ name: 'subscriptions' }" class="tabs__item title is-small" exact active-class="is-active">
+        <router-link :to="{ name: 'subscriptions' }" class="tabs__item title is-small" exact active-class="is-active">
           <span>{{ $t('DataPage.tabs.yourSubscriptions') }}</span>
-        </router-link> -->
+        </router-link>
       </div>
       <router-link :to="{ name: 'do-catalog' }" class="tabs__item title is-small" exact active-class="is-active" style="margin-left: auto;">
         <span>{{ $t('Subscriptions.do') }}</span>

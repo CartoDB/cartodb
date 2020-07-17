0e579731921650ee295fc166aee8122c36acdc46
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2020-07-17 14:58:39 +0200

    Remove do sync table mock

--- app/models/carto/visualization.rb
+++ app/models/carto/visualization.rb
@@ -660,11 +660,9 @@ class Carto::Visualization < ActiveRecord::Base
 
   def subscription
     if user_table
-      # TEMP MOCK
-      { type: 'do-v2', id: 'ags_businesscou_8dc7d1e0' } if user_table.name == 'my_do_subscription'
-      # doss = Carto::DoSyncService.new(user)
-      # subscription_id = doss.subscription_from_sync_table(user_table.name)
-      # { type: 'do-v2', id: subscription_id } if subscription_id.present?
+      doss = Carto::DoSyncService.new(user)
+      subscription_id = doss.subscription_from_sync_table(user_table.name)
+      { type: 'do-v2', id: subscription_id } if subscription_id.present?
     end
   end
 

baada51ff5a54d92a26c2e2090f699a4da48efe9
Rafa de la Torre <rtorre@carto.com>
2020-07-22 18:58:23 +0200

    Do not truncate json response for further analysis

--- services/datasources/lib/datasources/url/arcgis.rb
+++ services/datasources/lib/datasources/url/arcgis.rb
@@ -458,7 +458,7 @@ module CartoDB
               # HACK: JSON spec does not cover Infinity
               body = ::JSON.parse(response.body.gsub(':INF,', ':"Infinity",'))
             rescue JSON::ParserError
-              @logger.append_and_store("JSON parsing error: #{response.body}")
+              @logger.append_and_store("JSON parsing error: #{response.body}", _truncate=false)
               raise ResponseError.new("JSON parsing error. URL: #{prepared_url} #{to_s}")
             end
           end

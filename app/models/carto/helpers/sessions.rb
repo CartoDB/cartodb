62be1972dbdb662664dfa1082863c4b09a4c1cfe
Rafa de la Torre <rtorre@carto.com>
2020-09-02 16:41:11 +0200

    Make the code Sequel/AR compat

--- app/models/carto/helpers/sessions.rb
+++ app/models/carto/helpers/sessions.rb
@@ -3,11 +3,12 @@ module Carto::Sessions
     self.session_salt = SecureRandom.hex
 
     if update_in_central
-      save(raise_on_failure: true)
+      # NOTE: this is a hack for making the code AR/Sequel compatible
+      save(raise_on_failure: true) || raise(ActiveRecord::RecordNotSaved.new("Failed to save the record", self))
     else
       CartoDB::Logger.error(message: "Cannot invalidate session")
     end
-  rescue CartoDB::CentralCommunicationFailure, Sequel::ValidationFailed => e
+  rescue CartoDB::CentralCommunicationFailure, Sequel::ValidationFailed, ActiveRecord::RecordNotSaved => e
     CartoDB::Logger.error(exception: e, message: "Cannot invalidate session")
   end
 end

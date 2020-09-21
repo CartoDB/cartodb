1cf56bc12a3fb6b91a6eb1d07af7eab48b76009d
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2020-09-21 14:56:53 +0200

    Update DO user_request mail

--- app/mailers/data_observatory_mailer.rb
+++ app/mailers/data_observatory_mailer.rb
@@ -1,13 +1,9 @@
 class DataObservatoryMailer < ActionMailer::Base
-
-  CARTO_REQUEST_RECIPIENT = 'dataobservatory@carto.com'.freeze
-  EXCLUDED_ORGS = %w(team solutionscdb).freeze
-
   default from: Cartodb.get_config(:mailer, 'from')
   layout 'mail'
 
   def user_request(user, dataset_id, dataset_name)
-    subject = 'Your dataset request to CARTO'
+    subject = 'Your premium dataset request to CARTO'
     @user_name = user.name
     @dataset_id = dataset_id
     @dataset_name = dataset_name
@@ -21,6 +17,8 @@ class DataObservatoryMailer < ActionMailer::Base
     @user_email = user.email
     @dataset_id = dataset_id
     @delivery_days = delivery_days
+    EXCLUDED_ORGS = %w(team solutionscdb).freeze
+    CARTO_REQUEST_RECIPIENT = 'dataobservatory@carto.com'.freeze
 
     unless Rails.env.staging? || EXCLUDED_ORGS.include?(user.organization&.name)
       mail to: CARTO_REQUEST_RECIPIENT, subject: subject

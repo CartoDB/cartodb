963ce8d80aba74085dd95138938d4817565877e8
Rafa de la Torre <rtorre@carto.com>
2020-09-24 16:38:00 +0200

    Greatly simplify the graph by avoiding duplicate edges
    They are exactly not duplicated (different kinds of layers taken into
    account in the Map model, but yet, they provide little value)

--- script/sequel-model-graph.rb
+++ script/sequel-model-graph.rb
@@ -3,18 +3,25 @@
 # Usage:
 #   bundle exec rails runner script/sequel-model-graph.rb
 
+require 'set'
 
 # Load all the model classes (autoloader might not have loaded all of
 # them yet)
 Dir.glob('./app/models/**/*.rb').each {|file| require file }
+
+graph = Set.new
 
 # List Sequel through reflection
 Sequel::Model.subclasses.each do |model|
   # Get its associations
   associations = model.association_reflections.values
   associations.each do |assoc|
-    # Graphviz dot output
-    puts "#{model.to_s.demodulize} -> #{assoc.associated_class.to_s} [ label=\"#{assoc[:type].to_s}\" ];"
+    # Graphviz dot output format
+    graph << "#{model.to_s.demodulize} -> #{assoc.associated_class.to_s} [ label=\"#{assoc[:type].to_s}\" ];"
   end
-  puts "#{model.to_s.demodulize} -> {}" if associations.empty?
+  graph << "#{model.to_s.demodulize} -> {}" if associations.empty?
+end
+
+graph.each do |edge|
+  puts edge
 end

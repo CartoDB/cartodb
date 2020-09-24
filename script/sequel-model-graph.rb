0543b7c6112e59009ee8b67b8cddf5015711cd19
Rafa de la Torre <rtorre@carto.com>
2020-09-24 17:01:19 +0200

    Disable association type from graph with a flag

--- script/sequel-model-graph.rb
+++ script/sequel-model-graph.rb
@@ -13,6 +13,7 @@
 require 'set'
 
 OUTPUT_FILE = '/tmp/sequel-graph.dot'
+INCLUDE_ASSOCIATION_TYPE = false
 
 # Load all the model classes (autoloader might not have loaded all of
 # them yet)
@@ -27,7 +28,8 @@ Sequel::Model.subclasses.each do |model|
   associations = model.association_reflections.values
   associations.each do |assoc|
     # Graphviz dot output format
-    graph << "#{model.to_s.demodulize} -> #{assoc.associated_class.to_s} [ label=\"#{assoc[:type].to_s}\" ];"
+    association_type = INCLUDE_ASSOCIATION_TYPE ? %Q([ label=\"#{assoc[:type].to_s}\" ]) : ''
+    graph << "#{model.to_s.demodulize} -> #{assoc.associated_class.to_s} #{association_type};"
   end
   graph << "#{model.to_s.demodulize} -> {}" if associations.empty?
 end

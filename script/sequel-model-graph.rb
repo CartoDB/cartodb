67c69fadbf1da871d638a9109010ece5cf92f8ed
Rafa de la Torre <rtorre@carto.com>
2020-09-24 16:32:36 +0200

    Include models without associations

--- script/sequel-model-graph.rb
+++ script/sequel-model-graph.rb
@@ -11,8 +11,10 @@ Dir.glob('./app/models/**/*.rb').each {|file| require file }
 # List Sequel through reflection
 Sequel::Model.subclasses.each do |model|
   # Get its associations
-  model.association_reflections.values.each do |assoc|
+  associations = model.association_reflections.values
+  associations.each do |assoc|
     # Graphviz dot output
     puts "#{model.to_s.demodulize} -> #{assoc.associated_class.to_s} [ label=\"#{assoc[:type].to_s}\" ];"
   end
+  puts "#{model.to_s.demodulize} -> {}" if associations.empty?
 end

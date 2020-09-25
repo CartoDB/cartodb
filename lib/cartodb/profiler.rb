9b4e756da643bcdbaa39bf18902ad3ebaf372090
Rafa de la Torre <rtorre@carto.com>
2020-09-25 17:50:19 +0200

    Remove support for other printers (not using them)
    The API has changed, so not worth dragging this unused and untested
    code.

--- lib/cartodb/profiler.rb
+++ lib/cartodb/profiler.rb
@@ -6,8 +6,7 @@ module CartoDB
   # A profiler based on https://github.com/justinweiss/request_profiler/
   class Profiler
 
-    def initialize(printer: nil, exclude: nil)
-      @printer = printer || ::RubyProf::CallTreePrinter
+    def initialize(exclude: nil)
       @exclusions = exclude
     end
 
@@ -35,39 +34,11 @@ module CartoDB
       end
     end
 
-    def format(printer)
-      case printer
-      when ::RubyProf::FlatPrinter
-        'txt'
-      when ::RubyProf::GraphPrinter
-        'txt'
-      when ::RubyProf::GraphHtmlPrinter
-        'html'
-      when ::RubyProf::DotPrinter
-        'dot'
-      when ::RubyProf::CallTreePrinter
-        "out.#{Process.pid}"
-      when ::RubyProf::CallStackPrinter
-        'html'
-      else
-        'txt'
-      end
-    end
-
-    def prefix(printer)
-      case printer
-      when ::RubyProf::CallTreePrinter
-        "callgrind."
-      else
-        ""
-      end
-    end
-
     def write_result(result, request, response)
       result.eliminate_methods!(@exclusions) if @exclusions
-      printer = @printer.new(result)
+      printer = ::RubyProf::CallTreePrinter.new(result)
       url = request.fullpath.gsub(/[?\/]/, '-')
-      base_name = "#{prefix(printer)}#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}-#{url.slice(0, 50)}"
+      base_name = "callgrind.#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}-#{url.slice(0, 50)}"
       printer.print(path: Dir.tmpdir, profile: base_name)
 
       # see https://github.com/ruby-prof/ruby-prof/blob/1.4.1/lib/ruby-prof/printers/call_tree_printer.rb#L115

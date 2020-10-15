bb733579d8070b11bbaef40e8b6a595f76e4bfac
Javier Goizueta <jgoizueta@gmail.com>
2020-10-15 20:11:26 +0200

    Follow (some) of rubocop advice

--- app/models/carto/helpers/data_import_commons.rb
+++ app/models/carto/helpers/data_import_commons.rb
@@ -1,30 +1,35 @@
 require_dependency 'cartodb/errors'
 require_dependency 'cartodb/import_error_codes'
 
-module Carto::DataImportCommons
-  # Notice that this returns the entire error hash, not just the text
-  def get_error_text
-    if self.error_code == CartoDB::NO_ERROR_CODE
-      CartoDB::NO_ERROR_CODE
-    elsif self.error_code.blank? || self.error_code == 99999
-      connector_error_message || CartoDB::IMPORTER_ERROR_CODES[9999]
-    else
-      CartoDB::IMPORTER_ERROR_CODES[self.error_code]
+module Carto
+
+  module DataImportCommons
+
+    # Notice that this returns the entire error hash, not just the text
+    def get_error_text
+      if error_code == CartoDB::NO_ERROR_CODE
+        CartoDB::NO_ERROR_CODE
+      elsif error_code.blank? || error_code == 99999
+        connector_error_message || CartoDB::IMPORTER_ERROR_CODES[9999]
+      else
+        CartoDB::IMPORTER_ERROR_CODES[error_code]
+      end
     end
-  end
 
-  private
+    private
 
-  CONNECTOR_ERROR_PATTERN = /Connector Error [^\s]+: ERROR:\s+(.+?)^\d\d\d\d-\d\d-\d\d/mi.freeze
+    CONNECTOR_ERROR_PATTERN = /Connector Error [^\s]+: ERROR:\s+(.+?)^\d\d\d\d-\d\d-\d\d/mi.freeze
 
-  def connector_error_message
-    match = CONNECTOR_ERROR_PATTERN.match(log&.entries)
-    if match.present?
+    def connector_error_message
+      match = CONNECTOR_ERROR_PATTERN.match(log&.entries)
+      return unless match.present?
       {
         title: 'Connector Error',
         what_about: match[1],
         source: CartoDB::ERROR_SOURCE_CARTODB # FIXME, should it typically be ERROR_SOURCE_USER ?
       }
     end
+
   end
+
 end

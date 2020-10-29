dadf33dd59bb59e3ac1eb860359cd9a987b4c90d
Javier Goizueta <jgoizueta@gmail.com>
2020-10-29 20:43:40 +0100

    Preparation for DO sample metadata

--- lib/resque/do_sample_jobs.rb
+++ lib/resque/do_sample_jobs.rb
@@ -8,8 +8,13 @@ module Resque
       run_action(options, @queue, lambda { |options|
         data_import = DataImport[options.symbolize_keys[:job_id]]
         data_import.run_import!
-        # TODO: tag imported table (data_import.table_name) as do-sample if succeeded
+        # TODO: tag imported table (data_import.table_name) as do-sample if succeeded and save also get_dataset_id associated to it
       })
     end
+  end
+
+  def self.get_dataset_id(data_import)
+    service_item_id = data_import.service_item_id
+    JSON.parse(service_item_id)['dataset_id']
   end
 end

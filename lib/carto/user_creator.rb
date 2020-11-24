7fed628e99f7abd55bb47d26768ed6e594d8a0cb
Alberto Miedes Garcés <albertomg994@gmail.com>
2020-11-24 12:24:32 +0100

    Typo in params names

--- lib/carto/user_creator.rb
+++ lib/carto/user_creator.rb
@@ -1,20 +1,20 @@
 module Carto
   class UserCreator
+
     # NOTE copied from the superadmin users_controller.rb
-    def create(user_param)
+    def create(params)
       user = ::User.new
 
-      user_param = params[:user]
-      user.set_fields_from_central(user_param, :create)
+      user.set_fields_from_central(params, :create)
       user.enabled = true
 
-      if user_param[:rate_limit].present?
-        user.rate_limit_id = Carto::RateLimitsHelper.create_rate_limits(user_param[:rate_limit]).id
+      if params[:rate_limit].present?
+        user.rate_limit_id = Carto::RateLimitsHelper.create_rate_limits(params[:rate_limit]).id
       end
       if user.save
         user.reload
         CartoDB::Visualization::CommonDataService.load_common_data(user, self) if user.should_load_common_data?
-        user.update_feature_flags(user_param[:feature_flags])
+        user.update_feature_flags(params[:feature_flags])
       end
       CartoGearsApi::Events::EventManager.instance.notify(
         CartoGearsApi::Events::UserCreationEvent.new(
@@ -24,5 +24,6 @@ module Carto
 
       user
     end
+
   end
 end

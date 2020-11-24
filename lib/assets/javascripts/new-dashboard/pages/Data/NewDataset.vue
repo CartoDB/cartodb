c2d6db93605b90ee07ca83ec706a4e98ba5052c8
Javier Aragón <javier.aragon@geographica.gs>
2020-11-24 18:56:46 +0100

    first commit

--- lib/assets/javascripts/new-dashboard/pages/Data/NewDataset.vue
+++ lib/assets/javascripts/new-dashboard/pages/Data/NewDataset.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-    New Dataset
-  </div>
+  <Dialog>
+    <DialogHeader
+      :title="'Add new dataset'"
+      :image="require('../../assets/icons/catalog/modal/subsc-add-icon.svg')"
+    ></DialogHeader>
+    <div class="js-content-container Dialog-body Dialog-body--expanded Dialog-body--create Dialog-body--noPaddingTop Dialog-body--withoutBorder"></div>
+  </Dialog>
 </template>
 
 <script>
 
+import Dialog from 'new-dashboard/components/Backbone/Dialog.vue';
+import DialogHeader from 'new-dashboard/components/Dialogs/DialogHeader.vue';
+
 export default {
   name: 'NewDataset',
-  components: {},
+  components: {
+    Dialog,
+    DialogHeader
+  },
   computed: {},
   methods: {}
 };

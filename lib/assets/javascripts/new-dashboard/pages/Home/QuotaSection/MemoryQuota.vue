e800ed861bd0a9f392ae992913a3adb76e386c4c
Iván Moreno <ivan@geographica.gs>
2020-11-25 15:18:16 +0100

    added tooltips

--- lib/assets/javascripts/new-dashboard/pages/Home/QuotaSection/MemoryQuota.vue
+++ lib/assets/javascripts/new-dashboard/pages/Home/QuotaSection/MemoryQuota.vue
@@ -6,6 +6,7 @@
       :availableQuota="getAmountInUnit(availableQuota)"
       :unit="getUnit(availableQuota)"
       :formatToLocale="false"
+      helpText="TODO"
       :helpLink="storageHelpLink"/>
     <StackedQuotaWidget
       v-if="subscriptionsPremiumSizeInBytes"

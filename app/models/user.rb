f81474d98bcc3e57675d5f3a66ba20bdcff690b3
manmorjim <manmorjim@gmail.com>
2020-11-26 16:12:24 +0100

    Prevent add nil values to the new_attributes hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1451,10 +1451,9 @@ class User < Sequel::Model
 
   def sync_enabled_api_keys
     if previous_changes.present?
-      new_attributes = {
-        state: previous_changes[:state],
-        engine_enabled: previous_changes[:engine_enabled]
-      }
+      new_attributes = {}
+      new_attributes[:state] = previous_changes[:state] if previous_changes[:state].present?
+      new_attributes[:engine_enabled] = previous_changes[:engine_enabled] if previous_changes[:engine_enabled].present?
     end
 
     api_keys.each { |api_key| api_key.set_enabled_for_engine(new_attributes) }

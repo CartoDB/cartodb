f15d0764b05bb638ea5bd3786d87e26ff0409b4b
Javier Aragón <javier.aragon@geographica.gs>
2020-11-30 19:39:40 +0100

    fist commit

---
+++ lib/assets/javascripts/new-dashboard/components/Connector/ConnectorsList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    List of connectors
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ConnectorList',
+  components: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+@import "new-dashboard/styles/variables";
+</style>

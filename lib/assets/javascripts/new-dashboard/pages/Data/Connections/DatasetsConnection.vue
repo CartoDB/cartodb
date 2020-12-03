27d1466d61f494ea3be1306c79ec10cfa7590ec5
albertoac <albert.a.g@gmail.com>
2020-12-03 18:43:39 +0100

    Fixed design

--- lib/assets/javascripts/new-dashboard/pages/Data/Connections/DatasetsConnection.vue
+++ lib/assets/javascripts/new-dashboard/pages/Data/Connections/DatasetsConnection.vue
@@ -5,36 +5,41 @@
     :showSubHeader="true"
   >
     <template #default>
-      <h3 class="title is-small">{{$t('DataPage.addConnector.importFrom', { datasource: "Snowflake instance" })}}</h3>
-      <div class="u-flex u-mt--24">
-        <p class="text is-small u-mt--24 label-text">{{$t('DataPage.addConnector.query')}}</p>
-        <div class="query-container u-ml--16">
-          <div class="codeblock-container">
-            <CodeBlock language="text/x-plsql"
-              :readOnly="false"
-              placeholder="SELECT *, ST_GeogPoint(longitude, latitude) AS the_geom FROM mytable"
-              v-model="query"/>
+      <div class="u-flex u-flex__justify--center">
+        <div class="forms-container">
+          <h3 class="title is-small">{{$t('DataPage.addConnector.importFrom', { datasource: "Snowflake instance" })}}</h3>
+          <div class="u-flex u-mt--24">
+            <p class="text is-small u-mt--12 label-text">{{$t('DataPage.addConnector.query')}}</p>
+            <div class="query-container u-ml--16">
+              <div class="codeblock-container">
+                <CodeBlock language="text/x-plsql"
+                  :readOnly="false"
+                  placeholder="SELECT *, ST_GeogPoint(longitude, latitude) AS the_geom 
+FROM mytable"
+                  v-model="query"/>
+              </div>
+              <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.sqlNote')}}</p>
+            </div>
           </div>
-          <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.sqlNote')}}</p>
+          <div class="u-flex u-mt--20">
+            <p class="text is-small u-mt--24 label-text">{{$t('DataPage.addConnector.destinationDataset')}}</p>
+            <div class="dataset-container u-ml--16">
+              <FormInput
+                :optional="true"
+                placeholder=""
+                v-model="datasetName"/>
+              <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.destinationDatasetNote')}}</p>
+            </div>
+          </div>
+          <div class="u-flex u-flex__justify--end u-mt--24">
+            <Button
+              color="is-primary"
+              class="underline-animation"
+              :disabled="!(query && datasetName)">
+              <span>{{$t('DataPage.addConnector.runQuery')}}</span>
+            </Button>
+          </div>
         </div>
-      </div>
-      <div class="u-flex u-mt--20">
-        <p class="text is-small u-mt--24 label-text">{{$t('DataPage.addConnector.destinationDataset')}}</p>
-        <div class="dataset-container u-ml--16">
-          <FormInput
-            :optional="true"
-            placeholder=""
-            v-model="datasetName"/>
-          <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.destinationDatasetNote')}}</p>
-        </div>
-      </div>
-      <div class="u-flex u-flex__justify--end u-mt--24">
-        <Button
-          color="is-primary"
-          class="underline-animation"
-          :disabled="!(query && datasetName)">
-          <span>{{$t('DataPage.addConnector.runQuery')}}</span>
-        </Button>
       </div>
     </template>
   </Dialog>
@@ -69,6 +74,11 @@ export default {
 <style scoped lang="scss">
 @import "new-dashboard/styles/variables";
 
+.forms-container {
+  width: 620px;
+  max-width: 100%;
+}
+
 .query-container {
   flex-grow: 1;
 
@@ -78,15 +88,18 @@ export default {
     border-radius: 4px;
 
     /deep/ .CodeMirror {
+      max-width: 100%;
       height: 100%;
       margin: 0;
+      padding: 0;
     }
   }
 }
 
 .label-text {
   flex-shrink: 0;
-  width: 100px;
+  width: 90px;
+  text-align: right;
 }
 
 .dataset-container {

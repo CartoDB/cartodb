d5a236237dd8599658a4a5ee53d1f9fd031aa5a3
albertoac <albert.a.g@gmail.com>
2020-12-03 17:45:14 +0100

    Added query page for connections

--- lib/assets/javascripts/new-dashboard/components/Code/CodeBlock.vue
+++ lib/assets/javascripts/new-dashboard/components/Code/CodeBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea ref="code" v-model="code"></textarea>
+  <textarea ref="code" :placeholder="placeholder"></textarea>
 </template>
 
 <script>
@@ -7,14 +7,19 @@ import CodeMirror from 'codemirror';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/shell/shell';
+import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 
 export default {
   name: 'CodeBlock',
   data () {
     return {
-      codemirror: null
+      codemirror: null,
     };
+  },
+  model: {
+    prop: 'code',
+    event: 'inputChange'
   },
   mounted () {
     this.initialize();
@@ -23,7 +28,11 @@ export default {
     this.destroy();
   },
   props: {
-    code: String,
+    code: {
+      type: String,
+      default: ''
+    },
+    placeholder: String,
     language: {
       type: String,
       default: 'javascript'
@@ -35,32 +44,37 @@ export default {
     theme: {
       type: String,
       default: 'material'
+    },
+    readOnly: {
+      type: Boolean,
+      default: true
     }
   },
   methods: {
+    onInputChange (e) {
+      this.$emit('inputChange', e.getValue());
+    },
     initialize () {
       const allOptions = {
         mode: this.language,
         lineNumbers: this.lineNumbers,
         theme: this.theme,
+        readOnly: this.readOnly,
         ...defaultOptions
       };
       this.codemirror = CodeMirror.fromTextArea(this.$refs.code, allOptions);
+      if(this.code)
+        this.codemirror.setValue(this.code);
+      this.codemirror.on("change", this.onInputChange)
     },
     destroy () {
       const codemirrorElement = this.codemirror.doc.cm.getWrapperElement();
       codemirrorElement.remove && codemirrorElement.remove();
     }
-  },
-  watch: {
-    code () {
-      this.codemirror && this.codemirror.setValue(this.code);
-    }
   }
 };
 
 const defaultOptions = {
-  readOnly: true,
   addModeClass: true
 };
 
--- lib/assets/javascripts/new-dashboard/pages/Data/Connections/DatasetsConnection.vue
+++ lib/assets/javascripts/new-dashboard/pages/Data/Connections/DatasetsConnection.vue
@@ -5,7 +5,37 @@
     :showSubHeader="true"
   >
     <template #default>
-      Datasets Connection
+      <h3 class="title is-small">{{$t('DataPage.addConnector.importFrom', { datasource: "Snowflake instance" })}}</h3>
+      <div class="u-flex u-mt--24">
+        <p class="text is-small u-mt--24 label-text">{{$t('DataPage.addConnector.query')}}</p>
+        <div class="query-container u-ml--16">
+          <div class="codeblock-container">
+            <CodeBlock language="text/x-plsql"
+              :readOnly="false"
+              placeholder="SELECT *, ST_GeogPoint(longitude, latitude) AS the_geom FROM mytable"
+              v-model="query"/>
+          </div>
+          <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.sqlNote')}}</p>
+        </div>
+      </div>
+      <div class="u-flex u-mt--20">
+        <p class="text is-small u-mt--24 label-text">{{$t('DataPage.addConnector.destinationDataset')}}</p>
+        <div class="dataset-container u-ml--16">
+          <FormInput
+            :optional="true"
+            placeholder=""
+            v-model="datasetName"/>
+          <p class="text is-small is-txtSoftGrey u-mt--12">{{$t('DataPage.addConnector.destinationDatasetNote')}}</p>
+        </div>
+      </div>
+      <div class="u-flex u-flex__justify--end u-mt--24">
+        <Button
+          color="is-primary"
+          class="underline-animation"
+          :disabled="!(query && datasetName)">
+          <span>{{$t('DataPage.addConnector.runQuery')}}</span>
+        </Button>
+      </div>
     </template>
   </Dialog>
 </template>
@@ -13,12 +43,24 @@
 <script>
 
 import Dialog from 'new-dashboard/components/Dialogs/Dialog.vue';
+import CodeBlock from 'new-dashboard/components/code/CodeBlock.vue'
+import FormInput from 'new-dashboard/components/forms/FormInput';
+import Button from 'new-dashboard/components/Button';
 
 export default {
   name: 'DatasetsConnection',
   components: {
-    Dialog
+    Button,
+    CodeBlock,
+    Dialog,
+    FormInput
   },
+  data () {
+    return {
+      query: '',
+      datasetName: ''
+    }
+  }, 
   computed: {},
   methods: {}
 };
@@ -26,4 +68,28 @@ export default {
 
 <style scoped lang="scss">
 @import "new-dashboard/styles/variables";
+
+.query-container {
+  flex-grow: 1;
+
+  .codeblock-container {
+    height: 120px;
+    overflow: hidden;
+    border-radius: 4px;
+
+    /deep/ .CodeMirror {
+      height: 100%;
+      margin: 0;
+    }
+  }
+}
+
+.label-text {
+  flex-shrink: 0;
+  width: 100px;
+}
+
+.dataset-container {
+  flex-grow: 1;
+}
 </style>

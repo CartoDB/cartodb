910f24138ca288bc429fd24810785824df306c5d
juanrmn <juanr.gonzalez@gmail.com>
2020-12-03 13:17:20 +0100

    chore: rubocop

--- app/models/carto/user_map_views.rb
+++ app/models/carto/user_map_views.rb
@@ -3,9 +3,12 @@ module Carto
 
     belongs_to :user
     validates :user, :metric_date, presence: true
+    # rubocop:disable Rails/ScopeArgs
+
     scope :last_billing_cycle, lambda do |start_date|
       where("metric_date >= DATE('#{start_date}')").order('metric_date DESC')
     end
+    # rubocop:enable Rails/ScopeArgs
 
   end
 end

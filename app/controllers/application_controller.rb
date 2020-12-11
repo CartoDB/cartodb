245d29c91101c94fe13860326a9b1f2c98cf2444
Javier Goizueta <jgoizueta@gmail.com>
2020-12-11 12:20:55 +0100

    Allow all CORS origins provisionally for staging tests from local dev.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -160,7 +160,7 @@ class ApplicationController < ActionController::Base
     if request.method == :options && check_cors_headers_for_whitelisted_origin
       common_cors_headers
       response.headers['Access-Control-Max-Age'] = '3600'
-    elsif !Rails.env.production? && !Rails.env.staging?
+    elsif !Rails.env.production? # && !Rails.env.staging? FIXME: this is provisional
       development_cors_headers
     end
   end

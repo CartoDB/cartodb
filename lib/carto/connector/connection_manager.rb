4ebf14c9293c642490569524f0c52f32b944a535
Javier Goizueta <jgoizueta@gmail.com>
2020-12-17 12:27:15 +0100

    OAuth tokens must be revoked before deletion

--- lib/carto/connector/connection_manager.rb
+++ lib/carto/connector/connection_manager.rb
@@ -168,6 +168,9 @@ module Carto
 
     def delete_connection(id)
       connection = fetch_connection(id)
+      if connection.connection_type == Carto::Connection::TYPE_OAUTH_SERVICE
+        conection.get_service_datasource.revoke_token
+      end
       connection.destroy!
       @user.reload
     end

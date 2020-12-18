5fdb73a17caef88bf7590076cc8a2ecbeb2811bd
alberhander <albertoh@carto.com>
2020-12-18 13:20:36 +0100

    update ci init script

--- script/ci/init.sh
+++ script/ci/init.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+set -e
+
+DB_IP=${DB_IP:-localhost}
+TIMEOUT_CONN=${TIMEOUT_CONN:-120}
+MAX_RETRIES=${MAX_RETRIES:-10}
+SLEEP_RETRY=$((TIMEOUT_CONN/MAX_RETRIES))
+SLEEP_DEBUGING=${SLEEP_DEBUGING:-120}
+
 cd /cartodb
 mkdir -p log && chmod -R 777 log/
-createdb -T template0 -O postgres -h localhost -U postgres -E UTF8 template_postgis || true
-psql -h localhost -U postgres template_postgis -c 'CREATE EXTENSION IF NOT EXISTS postgis;CREATE EXTENSION IF NOT EXISTS postgis_topology;'
-REDIS_PORT=6335 RAILS_ENV=test bundle exec rake cartodb:test:prepare
-cd -
+
+echo "Waiting for the database to be ready. Retrying every $SLEEP_RETRY seconds until it reaches a total of $MAX_RETRIES retries"
+n=0
+until [[ $n -eq $MAX_RETRIES ]]; do
+  echo "Try $((n+1))..."
+  pg_isready -U postgres -h "$DB_IP" && break
+  n=$((n+1))
+  sleep "$SLEEP_RETRY"
+done
+
+createdb -T template0 -O postgres -h "${DB_IP}" -U postgres -E UTF8 template_postgis || true
+psql -h "${DB_IP}" -U postgres -d template_postgis -c 'CREATE EXTENSION IF NOT EXISTS postgis;CREATE EXTENSION IF NOT EXISTS postgis_topology;CREATE EXTENSION IF NOT EXISTS postgis_raster;'
+RAILS_ENV=test bundle exec rake cartodb:test:prepare
 

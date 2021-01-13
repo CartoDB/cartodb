bdf9fa26ee28f1f4d8a3dcfd2d01135f93b5587f
Rafa de la Torre <rtorre@carto.com>
2021-01-13 17:02:13 +0100

    Adapt BaseCommand and OrganizationCommands to subscriber callback
    Adapt BaseCommand and OrganizationCommands to new subscriber callback
    API (receiving a message object instead of plain payload hash)

--- app/commands/base_command.rb
+++ app/commands/base_command.rb
@@ -7,13 +7,13 @@ class BaseCommand
     :request_id
   )
 
-  def self.run(params = {})
-    new(params).run
+  def self.run(message = nil)
+    new(message).run
   end
 
-  def initialize(params = {}, extra_context = {})
-    self.params = params.with_indifferent_access
-    self.request_id = Carto::Common::CurrentRequest.request_id || params.delete(:request_id)
+  def initialize(message = nil, extra_context = {})
+    self.params = message.payload
+    self.request_id = Carto::Common::CurrentRequest.request_id || message.request_id
     self.notifications_topic = extra_context[:notifications_topic]
     self.logger = extra_context[:logger] || Rails.logger
   end

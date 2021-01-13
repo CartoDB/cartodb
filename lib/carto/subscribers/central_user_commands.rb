6da20baf45562bfa04906d2f5f60f21d95ea4a98
Rafa de la Torre <rtorre@carto.com>
2021-01-13 16:53:07 +0100

    Adapt CentralUserCommands to new subscriber callback message API

--- lib/carto/subscribers/central_user_commands.rb
+++ lib/carto/subscribers/central_user_commands.rb
@@ -13,8 +13,9 @@ module Carto
         @logger = logger
       end
 
-      def update_user(payload)
-        Carto::Common::CurrentRequest.with_request_id(payload[:request_id]) do
+      def update_user(message)
+        payload = message.payload
+        Carto::Common::CurrentRequest.with_request_id(message.request_id) do
           logger.info(message: 'Processing :update_user', class_name: self.class.name)
 
           user_id = payload.delete('remote_user_id')
@@ -22,17 +23,17 @@ module Carto
           return unless user_id.present? && payload.any?
 
           user = ::User.where(id: user_id).first
-          Carto::UserUpdater.new(user).update(payload.except(:request_id))
+          Carto::UserUpdater.new(user).update(payload)
 
           logger.info(message: 'User updated', current_user: user, class_name: self.class.name)
         end
       end
 
-      def create_user(payload)
-        Carto::Common::CurrentRequest.with_request_id(payload[:request_id]) do
+      def create_user(message)
+        Carto::Common::CurrentRequest.with_request_id(message.request_id) do
           logger.info(message: 'Processing :create_user', class_name: self.class.name)
 
-          user = Carto::UserCreator.new.create(payload.except(:request_id))
+          user = Carto::UserCreator.new.create(message.payload)
           notifications_topic.publish(:user_created, {
                                         username: user.username,
                                         id: user.id
@@ -42,8 +43,9 @@ module Carto
         end
       end
 
-      def delete_user(payload)
-        Carto::Common::CurrentRequest.with_request_id(payload[:request_id]) do
+      def delete_user(message)
+        payload = message.payload
+        Carto::Common::CurrentRequest.with_request_id(message.request_id) do
           logger.info(message: 'Processing :delete_user', class_name: self.class.name)
 
           user = ::User.where(id: payload[:id]).first
@@ -53,7 +55,7 @@ module Carto
           logger.info(message: 'User deleted', current_user: user.username, class_name: self.class.name)
         end
       rescue CartoDB::SharedEntitiesError
-        Carto::Common::CurrentRequest.with_request_id(payload[:request_id]) do
+        Carto::Common::CurrentRequest.with_request_id(message.request_id) do
           notifications_topic.publish(:user_could_not_be_deleted, {
                                         username: user.username,
                                         reason: 'user has shared entities'

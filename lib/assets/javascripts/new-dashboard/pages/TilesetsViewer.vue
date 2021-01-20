4c7b38ee5973cc4b2237e112aa7b718a74868c4b
Javier Aragón <javier.aragon@geographica.gs>
2021-01-20 19:26:59 +0100

    viewer integration

--- lib/assets/javascripts/new-dashboard/pages/TilesetsViewer.vue
+++ lib/assets/javascripts/new-dashboard/pages/TilesetsViewer.vue
@@ -20,19 +20,45 @@ export default {
       username: state => state.user.username
     })
   },
-  mounted () {
+  async mounted () {
+
     const element = this.$refs.viewer;
+    const tileset = this.$route.params.id;
+    const source = 'bigquery';
+    let api_key = 'default_public';
+    let tilejson;
+
+    try {
+      tilejson = await this.getTilejson(source, tileset, api_key);
+    } catch (error) {
+      if (error.message === '401') {
+        api_key = this.apiKey;
+        try {
+          tilejson = await this.getTilejson(source, tileset, api_key);
+        } catch (error) {
+          console.error(`ERROR: ${error}`);
+        }
+      }
+    }
+
     const props = {
       username: this.username,
-      type: 'bigquery',
-      query: new URLSearchParams(`?data=${this.$route.params.id}${this.getApiKeyParam()}`)
+      type: source,
+      query: new URLSearchParams(`?data=${tileset}&api_key=${api_key}${this.getColorByValue(tilejson)}`)
     };
     init(element, props);
   },
   methods: {
-    getApiKeyParam () {
-      // return `&api_key=${this.apiKey}`;
-      return '&api_key=default_public';
+    getTilejson (source, tileset, apiKey) {
+      return this.$store.dispatch('tilesets/getTilejson', { source, tileset, apiKey });
+    },
+    getColorByValue (tilejson) {
+      return (tilejson &&
+        tilejson.vector_layers &&
+        tilejson.vector_layers[0] &&
+        tilejson.vector_layers[0].fields &&
+        tilejson.vector_layers[0].fields.aggregated_total &&
+        `&color_by_value=aggregated_total`) || '';
     }
   }
 };

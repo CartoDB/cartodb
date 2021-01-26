51edf728322e276f8fc430f2ae64b2fbe82c18f0
Iván Moreno <ivan@geographica.gs>
2021-01-26 12:03:24 +0100

    finished changes in BQ Conn.

--- lib/assets/javascripts/new-dashboard/components/Connector/BigQueryConnectionForm.vue
+++ lib/assets/javascripts/new-dashboard/components/Connector/BigQueryConnectionForm.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="u-flex u-flex__justify--center">
     <div class="main u-flex u-flex__align--center u-flex__direction--column">
-      <div v-if="showDisclaimer">
-        <h4 class="is-subtitle is-semibold u-mt--16">Lorem ipsum dolor sit</h4>
+      <!-- DISCLAIMER -->
+      <div v-if="showDisclaimer" class="disclaimer">
+        <h4 class="is-subtitle is-semibold u-mt--16">{{$t('ConnectorsPage.BigQuery.title')}}</h4>
         <ul class="u-mt--16">
           <li class="u-mb--16">
-            <span class="text is-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et.</span>
+            <span class="text is-caption">{{$t('ConnectorsPage.BigQuery.disclaimer1')}}</span>
           </li>
           <li class="u-mb--16">
-            <span class="text is-caption">Aenean eu enim justo. Vestibulum aliquam hendrerit molestie.</span>
+            <span class="text is-caption">{{$t('ConnectorsPage.BigQuery.disclaimer2')}}</span>
           </li>
           <li class="u-mb--16">
-            <span class="text is-caption">Maecenas tincidunt, velit ac porttitor pulvinar, tortor eros facilisis libero, vitae commodo nunc quam et ligula.</span>
-          </li>
-          <li class="u-mb--16">
-            <span class="text is-caption">Ut nec ipsum sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus.</span>
+            <span class="text is-caption" v-html="$t('ConnectorsPage.BigQuery.disclaimer3')"></span>
           </li>
         </ul>
         <div class="u-mt--48 u-flex u-flex__justify--center u-flex__align--center">
           <button @click="cancel" class="is-small is-semibold is-txtPrimary">{{$t('ConnectorsPage.BigQuery.conditions.cancel')}}</button>
-          <button @click="accept" class="button is-primary u-ml--36">{{$t('ConnectorsPage.BigQuery.conditions.accept')}}</button>
+          <button @click="accept" class="button is-primary u-ml--36">{{$t('DataPage.continue')}}</button>
         </div>
       </div>
-      <div v-else-if="!showDisclaimer && !hasProjects">
+      <!-- UPLOAD FILE -->
+      <div v-else-if="!showDisclaimer && !isServiceAccountValid">
         <div class="section-header is-semibold is-small u-mb--4">
           <span class="u-flex u-flex__justify--center u-flex__align--center number">1</span>
           {{$t('ConnectorsPage.BigQuery.fileInputLabel')}}
@@ -39,43 +38,32 @@
           {{$t('ConnectorsPage.BigQuery.emailHelpText')}}
         </div>
         <input v-model="connectionModel.email" id="email" type="text" placeholder="user@organization.com">
-
+        <div class="error-wrapper text is-small u-mt--16" v-if="error">
+          <div class="is-small is-semibold">{{$t('ConnectorsPage.BigQuery.errorTitle')}}</div>
+          <div class="text is-small">{{error}}</div>
+        </div>
         <div class="u-flex u-flex__justify--end u-mt--32">
-          <!-- <button @click="cancel" class="u-mr--28 is-small is-semibold is-txtPrimary">{{$t('ConnectorsPage.cancel')}}</button> -->
-          <button @click="uploadServiceAccount" class="CDB-Button CDB-Button--primary CDB-Button--big" :class="{'is-disabled': !isFileSelected}">
+          <button @click="cancel" class="u-mr--28 is-small is-semibold is-txtPrimary">{{$t('ConnectorsPage.cancel')}}</button>
+          <button @click="uploadServiceAccount" class="CDB-Button CDB-Button--primary CDB-Button--big" :class="{'is-disabled': !isFileSelectedAndEmail}">
             <span class="CDB-Button-Text CDB-Text is-semibold CDB-Size-medium">
               {{ $t('DataPage.continue') }}
             </span>
           </button>
         </div>
       </div>
-      <div v-else-if="!showDisclaimer && hasProjects" class="u-flex u-flex__direction--column">
-        <h4 class="is-small is-semibold">{{$t('ConnectorsPage.BigQuery.title')}}</h4>
-        <div class="u-flex u-flex__justify--between u-flex__align--start u-mt--24 input-wrapper">
-          <div class="u-flex u-flex__direction--column u-flex__align--end u-flex__grow--1  u-mr--16">
-            <label class="is-small u-mt--12">{{$t('ConnectorsPage.BigQuery.connName')}}</label>
-          </div>
-          <input v-model="connectionModel.name" type="text" :placeholder="$t('DataPage.imports.database.placeholder-name')">
+      <!-- LAST STEP -->
+      <div v-else-if="!showDisclaimer && isServiceAccountValid" class="u-flex u-flex__direction--column">
+        <div class="section-header is-semibold is-small u-mb--4">
+          <span class="u-flex u-flex__justify--center u-flex__align--center number">3</span>
+          {{$t('ConnectorsPage.BigQuery.billingProject')}}
         </div>
-        <div class="u-flex u-flex__justify--between u-flex__align--start u-mt--24 input-wrapper">
-          <div class="u-flex u-flex__direction--column u-flex__align--end u-flex__grow--1  u-mr--16">
-            <label class="is-small u-mt--12">{{$t('ConnectorsPage.BigQuery.billingProject')}}</label>
-          </div>
-          <div>
-            <SelectComponent v-model="connectionModel.billing_project" :elements="projects"></SelectComponent>
-            <div class="text is-small is-txtMidGrey u-mt--8 helper" :html="$t('ConnectorsPage.BigQuery.billingHelper')"></div>
-          </div>
-        </div>
-        <div class="u-flex u-flex__align--center u-flex__justify--between u-mt--16 input-wrapper">
-          <div class="u-flex u-flex__direction--column u-flex__align--end u-flex__grow--1  u-mr--16">
-            <label class="is-small">{{$t('ConnectorsPage.BigQuery.defaultProject')}}</label>
-          </div>
-          <SelectComponent v-model="connectionModel.default_project" :elements="projects"></SelectComponent>
-        </div>
+        <div class="text is-small is-txtMidGrey u-mb--24" v-html="$t('ConnectorsPage.BigQuery.billingHelper')"></div>
+        <SelectComponent v-model="connectionModel.billing_project" :elements="projects"></SelectComponent>
         <div class="error-wrapper text is-small is-txtAlert u-flex u-flex__justify--start u-mt--16" v-if="error">
           {{ error }}
         </div>
         <div class="u-flex u-flex__justify--end u-mt--32">
+          <button @click="cancel" class="u-mr--28 is-small is-semibold is-txtPrimary">{{$t('ConnectorsPage.cancel')}}</button>
           <button @click="connect" class="CDB-Button CDB-Button--primary CDB-Button--big" :class="{'is-disabled': (!connectionModelIsValid || submited)}">
             <span class="CDB-Button-Text CDB-Text is-semibold CDB-Size-medium">
               {{ $t('DataPage.connect') }}
@@ -92,6 +80,7 @@
 import FileInput from 'new-dashboard/components/forms/FileInput';
 import SelectComponent from 'new-dashboard/components/forms/SelectComponent';
 import uploadData from '../../mixins/connector/uploadData';
+import { validateEmail } from 'new-dashboard/utils/email-validation';
 
 const SERVICE_ACCOUNT_EXTENSION = 'json';
 
@@ -106,14 +95,14 @@ export default {
     return {
       error: '',
       file: null,
-      showDisclaimer: false, // FIxME
+      showDisclaimer: true,
       dragged: false,
       submited: false,
       projects: null,
       userAccount: null,
       serviceAccount: null,
       connectionModel: {
-        name: '',
+        name: 'BigQuery',
         billing_project: null,
         default_project: null
       },
@@ -126,12 +115,15 @@ export default {
   },
   computed: {
     connectionModelIsValid () {
-      return this.connectionModel.name && this.connectionModel.billing_project && this.connectionModel.default_project;
+      return this.connectionModel.name &&
+        this.connectionModel.billing_project &&
+        this.connectionModel.email;
     },
-    isFileSelected () {
-      return this.file;
+    isFileSelectedAndEmail () {
+      return this.file && this.connectionModel.email && this.validateEmail(this.connectionModel.email);
     },
-    hasProjects () {
+    isServiceAccountValid () {
+      // If there are projects, means that ServiceAccount is valid :)
       return this.projects && this.projects.length;
     }
   },
@@ -145,10 +137,14 @@ export default {
     onFileChange (file) {
       this.file = file;
     },
+    validateEmail (email) {
+      return validateEmail(email);
+    },
     async connect () {
       try {
         this.error = '';
         this.submited = true;
+        this.connectionModel.default_project = this.connectionModel.billing_project;
         const response = await this.$store.dispatch('connectors/createNewBQConnection', { ...this.serviceAccount, ...this.connectionModel });
         this.submited = false;
         this.$emit('connectionSuccess', response.id);
@@ -167,6 +163,7 @@ export default {
       try {
         this.error = '';
         this.projects = (await this.$store.dispatch('connectors/checkServiceAccount', serviceAccount)).map(p => ({ id: p.id, label: p.friendly_name }));
+        this.connectionModel.billing_project = this.projects[0].id;
       } catch (e) {
         this.error = this.$t('ConnectorsPage.BigQuery.serviceAccountError');
       }
@@ -178,21 +175,19 @@ export default {
 <style scoped lang="scss">
 @import "new-dashboard/styles/variables";
 
-ul {
-  max-width: 588px;
-  list-style: disc;
-
-  li {
-    font-size: 12px;
-  }
-}
-
 .helper {
   max-width: 512px;
 }
 
 .main {
   max-width: 460px;
+}
+.disclaimer {
+  width: 620px;
+
+  h4 {
+    text-align: center;
+  }
 }
 
 .section-header {
@@ -209,6 +204,32 @@ ul {
   }
 }
 
+.error-wrapper {
+  position: relative;
+  padding: 16px 16px 16px 44px;
+  background-color: #fde8e7;
+  border-radius: 4px;
+
+  &:before {
+    content: '';
+    position: absolute;
+    display: block;
+    background-image: url("../../assets/icons/common/error.svg");
+    height: 24px;
+    width: 24px;
+    top: 12px;
+    left: 12px;
+  }
+}
+
+.select-wrapper {
+  &::v-deep {
+    select {
+      width: 460px;
+    }
+  }
+}
+
 input {
   width: 512px;
   border: solid 1px #dddddd;
@@ -220,6 +241,7 @@ input {
   height: 40px;
 
   &#email {
+    cursor: text;
     width: 100%;
   }
 

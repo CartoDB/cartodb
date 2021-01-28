06ab5e943f05efff64ca01d8a8ad7a1a1057f01f
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-01-28 11:03:48 +0100

    Fix spec and linter issues

--- spec/requests/superadmin/organizations_spec.rb
+++ spec/requests/superadmin/organizations_spec.rb
@@ -2,40 +2,53 @@
 
 require_relative '../../acceptance_helper'
 
-feature "Superadmin's organization API" do
-  before(:all) do
-    # Capybara.current_driver = :rack_test
+describe Superadmin::OrganizationsController do
+  let(:authentication_headers) do
+    {
+      'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(
+        Cartodb.config[:superadmin]['username'],
+        Cartodb.config[:superadmin]['password']
+      ),
+      'HTTP_ACCEPT' => 'application/json'
+    }
+  end
+
+  before do
     @organization1 = create_organization_with_users
     @organization2 = create_organization_with_users
     @org_atts = @organization1.values
   end
 
-  scenario "Http auth is needed" do
-    post_json superadmin_users_path, { :format => "json" } do |response|
+  it 'requires HTTP authentication' do
+    get_json superadmin_organization_path(@organization1), {} do |response|
       response.status.should == 401
     end
   end
 
-  scenario "organization get info success" do
-    get_json superadmin_organization_path(@organization1), {}, superadmin_headers do |response|
-      response.status.should == 200
-      response.body[:id].should == @organization1.id
+  # GET /superadmin/organization/:id
+  describe '#show' do
+    it 'returns the specified organization' do
+      get_json superadmin_organization_path(@organization1), {}, authentication_headers
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)['id']).to eq(@organization1.id)
     end
   end
 
-  describe "GET /superadmin/organization" do
-
+  # GET /superadmin/organization
+  describe '#index' do
     it "gets all organizations" do
       Carto::Organization.where(owner_id: nil).destroy_all
-      get_json superadmin_organizations_path, {}, superadmin_headers do |response|
+      get_json superadmin_organizations_path, {}, authentication_headers do |response|
         response.status.should == 200
         response.body.map { |u| u["name"] }.should include(@organization1.name, @organization2.name)
         response.body.length.should >= 2
       end
     end
+
     it "gets overquota organizations" do
       Carto::Organization.stubs(:overquota).returns [@organization1]
-      get_json superadmin_organizations_path, { overquota: true }, superadmin_headers do |response|
+      get_json superadmin_organizations_path, { overquota: true }, authentication_headers do |response|
         response.status.should == 200
         response.body[0]["name"].should == @organization1.name
         response.body.length.should == 1
@@ -47,7 +60,7 @@ feature "Superadmin's organization API" do
       Carto::Organization.stubs(:overquota).returns [@organization1]
       ::User.any_instance.stubs(:get_geocoding_calls).returns(100)
       ::User.any_instance.stubs(:map_views_count).returns (0..30).to_a
-      get_json superadmin_organizations_path, { overquota: true }, superadmin_headers do |response|
+      get_json superadmin_organizations_path, { overquota: true }, authentication_headers do |response|
         response.status.should == 200
         response.body[0]['name'].should == @organization1.name
         response.body[0]['geocoding']['quota'].should == @organization1.geocoding_quota
@@ -58,15 +71,6 @@ feature "Superadmin's organization API" do
       end
     end
     # rubocop:enable Lint/Void
-  end
-
-  private
-
-  def superadmin_headers(user = Cartodb.config[:superadmin]["username"], password = Cartodb.config[:superadmin]["password"])
-    {
-      'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(user, password),
-      'HTTP_ACCEPT' => "application/json"
-    }
   end
 end
 # rubocop:enable RSpec/InstanceVariable

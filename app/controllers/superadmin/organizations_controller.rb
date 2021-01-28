d749f21822472834f5298348234b953934518ce1
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-01-28 11:29:49 +0100

    Refactor spec to drop the shared state

--- app/controllers/superadmin/organizations_controller.rb
+++ app/controllers/superadmin/organizations_controller.rb
@@ -1,27 +1,23 @@
-class Superadmin::OrganizationsController < Superadmin::SuperadminController
-  respond_to :json
+module Superadmin
+  class OrganizationsController < ::Superadmin::SuperadminController
 
-  ssl_required :show, :create, :update, :destroy, :index
-  before_filter :get_organization, only: [:update, :destroy, :show]
+    respond_to :json
+    ssl_required :show, :create, :update, :destroy, :index
+    layout 'application'
 
-  layout 'application'
+    def show
+      organization = Carto::Organization.find_by(id: params[:id])
 
-  def show
-    respond_with(::OrganizationPresenter.new(@organization, extended: true).data)
+      render(json: { error: 'Organization not found' }, status: :not_found) unless organization
+      respond_with(::OrganizationPresenter.new(organization, extended: true).data)
+    end
+
+    def index
+      organizations = params[:overquota].present? ? Carto::Organization.overquota(0.20) : Carto::Organization.all
+      organizations_data = organizations.map { |organization| ::OrganizationPresenter.new(organization) }.map(&:data)
+
+      respond_with(:superadmin, organizations_data)
+    end
+
   end
-
-  def index
-    organizations = params[:overquota].present? ? Carto::Organization.overquota(0.20) : Carto::Organization.all
-    organizations_data = organizations.map { |organization| ::OrganizationPresenter.new(organization) }.map(&:data)
-
-    respond_with(:superadmin, organizations_data)
-  end
-
-  private
-
-  def get_organization
-    @organization = Carto::Organization.find_by(id: params[:id])
-    render json: { error: 'Organization not found' }, status: 404 unless @organization
-  end
-
 end
--- spec/requests/superadmin/organizations_controller_spec.rb
+++ spec/requests/superadmin/organizations_controller_spec.rb
@@ -1,5 +1,3 @@
-# rubocop:disable RSpec/InstanceVariable
-
 require_relative '../../acceptance_helper'
 
 describe Superadmin::OrganizationsController do
@@ -12,17 +10,15 @@ describe Superadmin::OrganizationsController do
       'HTTP_ACCEPT' => 'application/json'
     }
   end
+  let!(:organization) { create_organization_with_users }
 
-  before do
-    @organization1 = create_organization_with_users
-    @organization2 = create_organization_with_users
-    @org_atts = @organization1.values
-  end
+  # TODO: shouldn't be necessary once we setup DatabaseCleaner
+  after { Carto::Organization.delete_all }
 
   it 'requires HTTP authentication' do
-    get_json superadmin_organization_path(@organization1), {} do |response|
-      response.status.should == 401
-    end
+    get_json superadmin_organization_path(organization), {}
+
+    expect(response.status).to eq(401)
   end
 
   # GET /superadmin/organization/:id
@@ -30,51 +26,51 @@ describe Superadmin::OrganizationsController do
     let(:response_body) { JSON.parse(response.body).with_indifferent_access }
 
     it 'returns the specified organization' do
-      get_json superadmin_organization_path(@organization1), {}, authentication_headers
+      get_json superadmin_organization_path(organization), {}, authentication_headers
 
       expect(response.status).to eq(200)
-      expect(response_body[:id]).to eq(@organization1.id)
+      expect(response_body[:id]).to eq(organization.id)
     end
   end
 
   # GET /superadmin/organization
   describe '#index' do
     let(:response_body) { JSON.parse(response.body).map(&:with_indifferent_access) }
+    let(:first_organization) { response_body.first }
+
+    before { create_organization_with_users }
 
     it 'returns all the organizations' do
-      Carto::Organization.where(owner_id: nil).destroy_all
       get_json superadmin_organizations_path, {}, authentication_headers
 
       expect(response.status).to eq(200)
-      expect(response_body.map { |o| o[:name] }).to include(@organization1.name, @organization2.name)
+      expect(first_organization[:name]).to eq(organization.name)
       expect(response_body.size).to eq(2)
     end
 
     it 'returns organizations overquota' do
-      Carto::Organization.stubs(:overquota).returns [@organization1]
+      Carto::Organization.stubs(:overquota).returns [organization]
       get_json superadmin_organizations_path, { overquota: true }, authentication_headers
 
       expect(response.status).to eq(200)
-      expect(response_body.first[:name]).to eq(@organization1.name)
+      expect(response_body.first[:name]).to eq(organization.name)
       expect(response_body.size).to eq(1)
     end
 
     it 'returns geocoding and mapviews quotas and uses for all organizations' do
-      Carto::Organization.stubs(:overquota).returns [@organization1]
+      Carto::Organization.stubs(:overquota).returns [organization]
       ::User.any_instance.stubs(:get_geocoding_calls).returns(100)
       ::User.any_instance.stubs(:map_views_count).returns (0..30).to_a
 
       get_json superadmin_organizations_path, { overquota: true }, authentication_headers
 
       expect(response.status).to eq(200)
-      first_organization = response_body.first
-      expect(first_organization[:name]).to eq(@organization1.name)
-      expect(first_organization[:geocoding][:quota]).to eq(@organization1.geocoding_quota)
-      expect(first_organization[:geocoding][:monthly_use]).to eq(@organization1.get_geocoding_calls)
-      expect(first_organization[:map_views_quota]).to eq(@organization1.map_views_quota)
-      expect(first_organization[:map_views]).to eq(@organization1.map_views_count)
+      expect(first_organization[:name]).to eq(organization.name)
+      expect(first_organization[:geocoding][:quota]).to eq(organization.geocoding_quota)
+      expect(first_organization[:geocoding][:monthly_use]).to eq(organization.get_geocoding_calls)
+      expect(first_organization[:map_views_quota]).to eq(organization.map_views_quota)
+      expect(first_organization[:map_views]).to eq(organization.map_views_count)
       expect(response_body.size).to eq(1)
     end
   end
 end
-# rubocop:enable RSpec/InstanceVariable

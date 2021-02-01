9e19577e758ed6c27130f3dc6f5b3c11437742e1
Javier Aragón <javier.aragon@geographica.gs>
2021-02-01 12:28:13 +0100

    integration

--- lib/assets/javascripts/new-dashboard/pages/TilesetsViewer.vue
+++ lib/assets/javascripts/new-dashboard/pages/TilesetsViewer.vue
@@ -1,15 +1,22 @@
 <template>
-  <div ref="viewer"></div>
+  <div ref="viewer">
+    <section class="page">
+      <LoadingState primary/>
+    </section>
+  </div>
 </template>
 
 <script>
 
 import init from '@carto/viewer/src/init';
 import { mapState } from 'vuex';
+import LoadingState from 'new-dashboard/components/States/LoadingState';
 
 export default {
   name: 'TilesetsViewer',
-  components: {},
+  components: {
+    LoadingState
+  },
   props: {},
   data () {
     return {};
@@ -18,50 +25,56 @@ export default {
     ...mapState({
       apiKey: state => state.user.api_key,
       username: state => state.user.username
-    })
+    }),
+    source () {
+      return 'bigquery';
+    },
+    tileset_id () {
+      return this.$route.params.id;
+    }
   },
   async mounted () {
     const element = this.$refs.viewer;
-    const tileset = this.$route.params.id;
-    const source = 'bigquery';
-    // let tilejson;
     const backRoute = this.$router.resolve({name: 'tilesets'});
+    const tileset = await this.$store.dispatch('tilesets/getTileset', { source: this.source, tileset_id: this.tileset_id, ...this.$route.query });
+    const center = tileset.metadata && tileset.metadata.center && tileset.metadata.center.split(',');
+    const longitude = center && parseFloat(center[0]);
+    const latitude = center && parseFloat(center[1]);
+    const zoom = center && parseFloat(center[2]);
+    const initialViewState = { longitude, latitude, zoom };
 
-    // try {
-    //   tilejson = await this.getTilejson(source, tileset, this.apiKey);
-    // } catch (error) {
-    //   if (error.message === '401') {
-    //     try {
-    //       tilejson = await this.getTilejson(source, tileset, this.apiKey);
-    //     } catch (error) {
-    //       console.error(`ERROR: ${error}`);
-    //     }
-    //   }
-    // }
-
-    const props = {
+    this.props = {
       username: this.username,
-      type: source,
-      // query: new URLSearchParams(`?data=${tileset}&api_key=${this.apiKey}${this.getColorByValue(tilejson)}`),
-      query: new URLSearchParams(`?data=${tileset}&api_key=${this.apiKey}`),
+      type: this.source,
+      query: new URLSearchParams(`?data=${this.tileset_id}&api_key=${this.apiKey}${this.getColorByValue(tileset)}&initialViewState=${JSON.stringify(initialViewState)}`),
       backRoute: backRoute && backRoute.href,
       shareOptions: {
-        baseUrl: 'https://viewer.carto.com'
+        baseUrl: 'https://viewer.carto.com',
+        privacy: tileset.privacy,
+        setPrivacy: this.setPrivacy
       }
     };
-    init(element, props);
+    init(element, this.props);
   },
   methods: {
-    getTilejson (source, tileset, apiKey) {
-      return this.$store.dispatch('tilesets/getTilejson', { source, tileset, apiKey });
-    },
-    getColorByValue (tilejson) {
-      return (tilejson &&
-        tilejson.vector_layers &&
-        tilejson.vector_layers[0] &&
-        tilejson.vector_layers[0].fields &&
-        tilejson.vector_layers[0].fields.aggregated_total &&
+    getColorByValue (tileset) {
+      return (tileset.metadata &&
+        tileset.metadata.tilestats &&
+        tileset.metadata.tilestats.layers &&
+        tileset.metadata.tilestats.layers[0] &&
+        tileset.metadata.tilestats.layers[0].attributes &&
+        tileset.metadata.tilestats.layers[0].attributes.find(a => a.attribute === 'aggregated_total') &&
         `&color_by_value=aggregated_total`) || '';
+    },
+    async setPrivacy (privacy) {
+      // try {
+      //   await this.$store.dispatch('tilesets/setPrivacy', { source: this.source, tileset_id: this.tileset_id, ...this.$route.query, privacy });
+      // } catch (error) {
+      //   debugger;
+      // }
+      await this.$store.dispatch('tilesets/setPrivacy', { source: this.source, tileset_id: this.tileset_id, ...this.$route.query, privacy });
+      this.props.shareOptions.privacy = privacy;
+      // this.props.shareOptions.privacy = privacy;
     }
   }
 };
@@ -69,4 +82,8 @@ export default {
 
 <style scoped lang="scss">
 @import "new-dashboard/styles/variables";
+.page {
+  height: 100vh;
+  border: none;
+}
 </style>

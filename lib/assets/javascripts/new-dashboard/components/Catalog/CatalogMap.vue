0e3acfc91f12c2bffa700fdc5952cc8151247e53
Javier Aragón <javier.aragon@geographica.gs>
2021-02-12 17:49:04 +0100

    linter

--- lib/assets/javascripts/new-dashboard/components/Catalog/CatalogMap.vue
+++ lib/assets/javascripts/new-dashboard/components/Catalog/CatalogMap.vue
@@ -33,7 +33,7 @@ import { mapState } from 'vuex';
 import mapboxgl from 'mapbox-gl';
 import { Deck } from '@deck.gl/core';
 import { CartoBQTilerLayer, BASEMAP } from '@deck.gl/carto';
-import {MVTLoader} from '@loaders.gl/mvt';
+import { MVTLoader } from '@loaders.gl/mvt';
 
 import { generateColorStyleProps, resetColorStyleProps } from './map-styles/colorStyles';
 import { getQuantiles, formatNumber, capitalize, compare } from './map-styles/utils';

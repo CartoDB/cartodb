f18a0230d9b279dc749294df6144518a878660ec
cgonzalez <cgonzalez@cartodb.com>
2021-02-22 12:04:19 +0000

    Fix tests

--- spec/requests/carto/api/visualizations_controller_spec_part_1.rb
+++ spec/requests/carto/api/visualizations_controller_spec_part_1.rb
@@ -294,9 +294,7 @@ describe Carto::Api::VisualizationsController do
         'total_likes' => 0,
         'total_user_entries' => 0,
         'total_shared' => 0,
-        'total_locked' => 0,
-        'total_subscriptions' => 0,
-        'total_samples' => 0
+        'total_locked' => 0
       }
       response_body.should == expected_response
     end

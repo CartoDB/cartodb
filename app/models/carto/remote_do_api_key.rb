60fb07f938af3845c5a8ce6ec1b6f05d7a9541b7
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-03-02 14:16:56 +0100

    Sync onprem DO API keys with CARTO clouds

---
+++ app/models/carto/remote_do_api_key.rb
@@ -0,0 +1,31 @@
+module Carto
+  class RemoteDoApiKey
+
+    attr_accessor :token, :type, :username
+    attr_reader :redis_client
+
+    def initialize(attributes = {})
+      attributes = attributes.with_indifferent_access
+      @token = attributes[:token]
+      @username = attributes[:username]
+      @type = attributes[:type]
+      @redis_client = $users_metadata
+    end
+
+    def save!
+      Rails.logger.info(message: 'Saving to Redis', redis_key: redis_key, redis_value: redis_hash_as_array)
+      redis_client.hmset(redis_key, redis_hash_as_array)
+    end
+
+    private
+
+    def redis_key
+      "#{Carto::ApiKey::REDIS_KEY_PREFIX}#{username}:#{token}"
+    end
+
+    def redis_hash_as_array
+      ['user', username, 'type', type]
+    end
+
+  end
+end

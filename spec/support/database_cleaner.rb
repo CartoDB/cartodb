803980b5503a967062cc92e57b2dfb22be510f89
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-03-04 11:10:28 +0100

    Fix shared state in spec

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,7 +3,12 @@
 # Use with caution until its integration is more spreaded
 shared_context 'with DatabaseCleaner' do
   around do |example|
-    DatabaseCleaner[:active_record].strategy = :truncation, { only: %w(account_types) }
+    DatabaseCleaner[:active_record].strategy = :truncation, { only: %w(
+      account_types
+      users
+      user_tables
+      maps
+    ) }
     DatabaseCleaner.start
 
     example.run

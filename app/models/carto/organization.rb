4c39a67dae761c64a75415ddd6ddf507a89ba3da
Javier Goizueta <jgoizueta@gmail.com>
2021-03-04 13:00:17 +0100

    Remove comment

--- app/models/carto/organization.rb
+++ app/models/carto/organization.rb
@@ -22,7 +22,6 @@ module Carto
     has_many :connector_configurations, inverse_of: :organization, dependent: :destroy
     has_many :oauth_app_organizations, inverse_of: :oauth_app, dependent: :destroy
 
-    # Global Connections
     has_many :connections, class_name: 'Carto::Connection', inverse_of: :organization, dependent: :destroy
     delegate :oauth_connections, to: :connections
     delegate :db_connections, to: :connections

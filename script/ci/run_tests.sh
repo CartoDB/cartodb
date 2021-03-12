f86333114ad6a97b8e66fa056c4d45c3505edada
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-03-12 13:37:51 +0100

    run cato model tests

--- script/ci/run_tests.sh
+++ script/ci/run_tests.sh
@@ -38,8 +38,7 @@ bundle exec rake parallel:setup --trace
 
 set +e
 # bundle exec rake parallel:spec['spec\/commands'] [OK]
-# bundle exec rake parallel:spec['spec\/models\/carto']
-bundle exec rake parallel:spec['spec\/models\/carto\/(visualization_spec).rb']
+bundle exec rake parallel:spec['spec\/models\/carto']
 tests_exit_code=$?
 
 echo $tests_exit_code >> tmp/tests_exit_code

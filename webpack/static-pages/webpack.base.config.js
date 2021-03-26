7c0c7b5541b357e791dbb10dd8dd758bd45055e9
Víctor Velarde <victor.velarde@gmail.com>
2021-03-26 16:59:12 +0100

    Read config for public statics compilation

--- webpack/static-pages/webpack.base.config.js
+++ webpack/static-pages/webpack.base.config.js
@@ -5,6 +5,7 @@ const webpackFiles = require('../../lib/build/files/webpack_files');
 const Package = require('./../../package.json');
 
 const VERSION = Package.version;
+const PUBLIC_STATICS_CONFIG = require('../../config/public_statics_config');
 
 module.exports = {
   entry: Object.assign(
@@ -19,7 +20,9 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      __ASSETS_VERSION__: `'${VERSION}'`
+      __ASSETS_VERSION__: `'${VERSION}'`,
+      __CARTO_BUILDER_ASSET_HOST__: PUBLIC_STATICS_CONFIG.CARTO_BUILDER_ASSET_HOST,
+      __CARTO_MAPS_API_V2_EXTERNAL_URL__: PUBLIC_STATICS_CONFIG.CARTO_MAPS_API_V2_EXTERNAL_URL
     }),
 
     ...Object.keys(webpackFiles.htmlFiles).map((entryName) => {

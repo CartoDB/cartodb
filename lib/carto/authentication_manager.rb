37f3e521f0f18c8e191625610193b0a089371dd5
manmorjim <manmorjim@gmail.com>
2021-06-02 13:09:02 +0200

    include LoggerHelper and new logs

--- lib/carto/authentication_manager.rb
+++ lib/carto/authentication_manager.rb
@@ -1,5 +1,6 @@
 module Carto
   class AuthenticationManager
+    extend ::LoggerHelper
 
     def self.validate_session(warden_context, request, user)
       return true if session_security_token_valid?(warden_context, user)
@@ -15,6 +16,7 @@ module Carto
       log_info(message: "sec_token #{session.key?(:sec_token)}")
       return false unless session.key?(:sec_token)
 
+      log_info(message: "session sec_token #{session[:sec_token]}")
       log_info(message: "user sec_token #{user.security_token}")
       return true if session[:sec_token] == user.security_token
 

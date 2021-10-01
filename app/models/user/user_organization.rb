75138054eb552e3161109dfb676532a9afaca41d
cgonzalez <cgonzalez@cartodb.com>
2021-10-01 08:14:48 +0100

    Avoid overwrite user data when a it is promoted to admin

--- app/models/user/user_organization.rb
+++ app/models/user/user_organization.rb
@@ -22,6 +22,7 @@ module CartoDB
     def promote_user_to_admin
       raise "Organization is already active. You can't assign an admin" if @active
       @owner.organization_id = @organization.id
+      @owner.this.update organization_id: @organization.id
       @owner.db_service.move_to_own_schema
       @organization.owner_id = @owner.id
       @organization.admin_email = @owner.email
@@ -32,7 +33,6 @@ module CartoDB
       @owner.db_service.reset_user_schema_permissions
 
       @owner.db_service.setup_organization_user_schema
-      @owner.save
       @owner.db_service.monitor_user_notification
       @active = true
     end

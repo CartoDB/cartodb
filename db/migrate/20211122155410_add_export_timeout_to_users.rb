2fd1b2113a593c6f4464df1bc5c4e0ee6760b2c1
cgonzalez <cgonzalez@cartodb.com>
2021-12-28 13:37:16 +0000

    Fix Rubocop offenses

--- db/migrate/20211122155410_add_export_timeout_to_users.rb
+++ db/migrate/20211122155410_add_export_timeout_to_users.rb
@@ -1,13 +1,15 @@
 require 'carto/db/migration_helper'
 
+# rubocop:disable Style/MixinUsage
 include Carto::Db::MigrationHelper
+# rubocop:enable Style/MixinUsage
 
 migration(
-  Proc.new do
+  proc do
     # The 0 value means: "apply default export timeout" (defined by the SQL API)
     add_column :users, :export_timeout, :integer, default: 0, null: false
   end,
-  Proc.new do
+  proc do
     drop_column :users, :export_timeout
   end
 )

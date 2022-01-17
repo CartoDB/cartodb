5aa761b6ed13aef12fb46860cb3c51a540044e8f
Moisés Calzado <mcalzado@cartodb.com>
2022-01-17 11:19:36 +0100

    Fix some rubocop ofenses

--- services/importer/lib/importer/arcgis_autoguessing.rb
+++ services/importer/lib/importer/arcgis_autoguessing.rb
@@ -5,12 +5,12 @@ module CartoDB
   module Importer2
     class ArcGISAutoguessing
 
-        def initialize(db, schema_name, table_name, fields_metadata)
-            @db              = db
-            @schema_name     = schema_name
-            @table_name      = table_name
-            @fields_metadata = fields_metadata
-          end
+      def initialize(db, schema_name, table_name, fields_metadata)
+        @db              = db
+        @schema_name     = schema_name
+        @table_name      = table_name
+        @fields_metadata = fields_metadata
+      end
 
       def run
         autoguess_dates
@@ -19,10 +19,10 @@ module CartoDB
       def autoguess_dates
         date_fields = @fields_metadata.select { |field| field['type'] == 'esriFieldTypeDate' }
         date_fields.each do |field|
-            @db.run(%Q{
-              ALTER TABLE #{@schema_name}.#{@table_name} ALTER COLUMN #{field['name'].downcase} TYPE DATE 
-              using to_timestamp(cast(#{field['name'].downcase}::bigint/1000 as bigint))::date
-            })
+          @db.run(%{
+            ALTER TABLE #{@schema_name}.#{@table_name} ALTER COLUMN #{field['name'].downcase} TYPE DATE
+            using to_timestamp(cast(#{field['name'].downcase}::bigint/1000 as bigint))::date
+          })
         end
       end
 

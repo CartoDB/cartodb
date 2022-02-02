b82d261dfd6689bcfbd28df685261280871b8267
Mmoncadaisla <mmoncada@cartodb.com>
2022-02-02 17:12:13 +0100

    Apply Rubocop suggestions

--- app/connectors/importer.rb
+++ app/connectors/importer.rb
@@ -73,10 +73,7 @@ module CartoDB
         public_datasets = visualizations.select do |v|
           v.type == Carto::Visualization::TYPE_CANONICAL && v.privacy != Carto::Visualization::PRIVACY_PRIVATE
         end
-        if public_datasets.count == 0 then
-          return
-        end
-
+        return if public_datasets.zero?
         quota_checker = CartoDB::QuotaChecker.new(data_import.user)
         return unless quota_checker.will_be_over_public_dataset_quota?(public_datasets.count)
 

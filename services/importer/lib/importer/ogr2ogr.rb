081212ca45964e2549a76266185fbabe43a8161a
Moisés Calzado <mcalzado@cartodb.com>
2022-07-29 17:01:04 +0200

    Add AUTODETECT_SIZE_LIMIT option when importing CSVs

--- services/importer/lib/importer/ogr2ogr.rb
+++ services/importer/lib/importer/ogr2ogr.rb
@@ -180,6 +180,7 @@ module CartoDB
           # Inverse of the selection: if I want guessing I must NOT leave quoted fields as string
           [
             '-oo', 'AUTODETECT_TYPE=YES',
+            '-oo', 'AUTODETECT_SIZE_LIMIT=20000000',
             '-oo', "QUOTED_FIELDS_AS_STRING=#{quoted_fields_guessing ? 'NO' : 'YES'}"
           ] + x_y_possible_names_option + ['-s_srs', 'EPSG:4326', '-t_srs', 'EPSG:4326']
         end
